4a625c2121945c997be0ce00683be6afa0a11bc0
Jan Verschelde <jan@math.uic.edu>
2014-06-24 09:54:57 -0500

    random polynomials with same support with double double complex coefficients

---
+++ src/Ada/Math_Lib/Polynomials/dobldobl_complex_poly_randomizers.ads
@@ -0,0 +1,26 @@
+with DoblDobl_Complex_Polynomials;       use DoblDobl_Complex_Polynomials;
+with DoblDobl_Complex_Poly_Systems;      use DoblDobl_Complex_Poly_Systems;
+
+package DoblDobl_Complex_Poly_Randomizers is
+
+-- DESCRIPTION :
+--   This package offers routines for randomizing and perturbing
+--   the coefficients of polynomials.
+--   Except for the last three functions, the monomial structure 
+--   remains the same, only random (real or complex) coefficients 
+--   will replace the existing ones.
+
+  function Complex_Randomize ( p : Poly ) return Poly;
+  function Complex_Randomize ( p : Poly_Sys ) return Poly_Sys;
+ 
+  -- DESCRIPTION :
+  --   The real and imaginary part of the randomly generated
+  --   coefficients are in [-1.0,1.0]
+
+  function Complex_Randomize1 ( p : Poly ) return Poly;
+  function Complex_Randomize1 ( p : Poly_Sys ) return Poly_Sys;
+
+  -- DESCRIPTION :
+  --   Generates random complex coefficients with modulus one.
+
+end DoblDobl_Complex_Poly_Randomizers;
