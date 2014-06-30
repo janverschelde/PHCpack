015286ea2b2957d8dc739c029798cd4507d5abb8
Jan Verschelde <jan@math.uic.edu>
2014-06-30 10:24:34 -0500

    prepared greetings for version 2.3.89

--- src/Ada/Main/greeting_banners.adb
+++ src/Ada/Main/greeting_banners.adb
@@ -2,7 +2,7 @@ package body Greeting_Banners is
 
   function Version return string is
 
-    res : constant string := "PHCv2.3.88 released 2014-06-06";
+    res : constant string := "PHCv2.3.89 released 2014-06-30";
 
   begin
     return res;
--- src/Ada/Main/greeting_banners.ads
+++ src/Ada/Main/greeting_banners.ads
@@ -4,7 +4,7 @@ package Greeting_Banners is
 --   Defines banners to greet the user of phc.
 
   welcome : constant string :=
-    "Welcome to PHC (Polynomial Homotopy Continuation) v2.3.88  6 Jun 2014";
+    "Welcome to PHC (Polynomial Homotopy Continuation) v2.3.89 30 Jun 2014";
 
   compban : constant string :=
     "A numerical irreducible decomposition for the solution components.";
