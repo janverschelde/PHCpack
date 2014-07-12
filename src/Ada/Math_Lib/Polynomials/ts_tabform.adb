ac56eb6ee3d3aa26c5720d84f80aa161cd0fd2f1
Jan Verschelde <jan@math.uic.edu>
2014-07-12 09:51:42 -0500

    new package to work with tableau formats of polynomials and systems with double double complex coefficients

--- src/Ada/Math_Lib/Polynomials/ts_tabform.adb
+++ src/Ada/Math_Lib/Polynomials/ts_tabform.adb
@@ -1,4 +1,7 @@
+with text_io;                           use text_io;
+with Communications_with_User;          use Communications_with_User;
 with Standard_Tableau_Formats;
+with DoblDobl_Tableau_Formats;
 
 procedure ts_tabform is
 
@@ -6,6 +9,17 @@ procedure ts_tabform is
 --   This simple procedure allows the conversion between polynomials in
 --   symbolic (PHCpack) format and a more basic tableau format.
 
+  ans : character;
+
 begin
-  Standard_Tableau_Formats.Main_Interactive_Driver;
+  new_line;
+  put_line("MENU to convert polynomials to tableau format : ");
+  put_line("  1. with coefficients in standard double precision");
+  put_line("  2. with coefficients in double double precision");
+  put("Type 1 or 2 to select the precision : ");
+  Ask_Alternative(ans,"12");
+  if ans = '1'
+   then Standard_Tableau_Formats.Main_Interactive_Driver;
+   else DoblDobl_Tableau_Formats.Main_Interactive_Driver;
+  end if;
 end ts_tabform;
