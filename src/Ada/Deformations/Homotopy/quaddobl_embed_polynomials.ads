5a4dc28fd99061f9296aae9afa57cd8c4adc47c9
Jan Verschelde <jan@math.uic.edu>
2014-08-05 05:28:24 -0500

    embedding polynomials with double double and quad double complex coefficients in a larger space

---
+++ src/Ada/Deformations/Homotopy/quaddobl_embed_polynomials.ads
@@ -0,0 +1,23 @@
+with Standard_Natural_Numbers;          use Standard_Natural_Numbers;
+with QuadDobl_Complex_Polynomials;      use QuadDobl_Complex_Polynomials;
+with QuadDobl_Complex_Poly_Systems;     use QuadDobl_Complex_Poly_Systems;
+with QuadDobl_Complex_Jaco_Matrices;    use QuadDobl_Complex_Jaco_Matrices;
+with QuadDobl_Complex_Poly_Matrices;    use QuadDobl_Complex_Poly_Matrices;
+
+package QuadDobl_Embed_Polynomials is
+
+-- DESCRIPTION :
+--   The operations in this package extend the polynomials
+--   with extra variables.  This corresponds to the embedding
+--   of the polynomials into a larger space.
+
+  function Add_Variables ( p : Poly; k : natural32 ) return Poly;
+  function Add_Variables ( p : Poly_Sys; k : natural32 ) return Poly_Sys;
+  function Add_Variables ( p : Jaco_Mat; k : natural32 ) return Jaco_Mat;
+  function Add_Variables ( p : Matrix; k : natural32 ) return Matrix;
+
+  -- DESCRIPTION :
+  --   Extends the representation of the degrees with k more variables,
+  --   whose exponent equals 0.
+
+end QuadDobl_Embed_Polynomials;
