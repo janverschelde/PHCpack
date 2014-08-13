888b8b4bf13de5762789aa7c2214364677962755
Jan Verschelde <jan@math.uic.edu>
2014-08-13 16:21:13 -0500

    upgrades for projective transformations for systems and solutions with double double and quad double complex coefficients

--- src/Ada/Deformations/Homotopy/projective_transformations.adb
+++ src/Ada/Deformations/Homotopy/projective_transformations.adb
@@ -1,12 +1,21 @@
 with Standard_Natural_Numbers;           use Standard_Natural_Numbers;
 with Standard_Integer_Numbers;           use Standard_Integer_Numbers;
 with Standard_Floating_Numbers;          use Standard_Floating_Numbers;
-with Standard_Complex_Numbers;           use Standard_Complex_Numbers;
+with Double_Double_Numbers;              use Double_Double_Numbers;
+with Quad_Double_Numbers;                use Quad_Double_Numbers;
+with Standard_Complex_Numbers;
+with DoblDobl_Complex_Numbers;
+with QuadDobl_Complex_Numbers;
 with Standard_Natural_Vectors;
 
 package body Projective_Transformations is
 
-  function Projective_Transformation ( p : Poly ) return Poly is
+  function Projective_Transformation
+             ( p : Standard_Complex_Polynomials.Poly )
+             return Standard_Complex_Polynomials.Poly is
+
+    use Standard_Complex_Numbers;
+    use Standard_Complex_Polynomials;
   
     deg : constant integer32 := Degree(p);
     htd : Degrees
@@ -38,7 +47,84 @@ package body Projective_Transformations is
     return res;
   end Projective_Transformation;
 
-  procedure Projective_Transformation ( p : in out Poly ) is
+  function Projective_Transformation
+             ( p : DoblDobl_Complex_Polynomials.Poly )
+             return DoblDobl_Complex_Polynomials.Poly is
+
+    use DoblDobl_Complex_Numbers;
+    use DoblDobl_Complex_Polynomials;
+  
+    deg : constant integer32 := Degree(p);
+    htd : Degrees
+        := new Standard_Natural_Vectors.Vector
+                 (1..integer32(Number_of_Unknowns(p))+1);
+    res : Poly := Null_Poly;
+
+    procedure Embed_Term ( t : in Term; continue : out boolean ) is
+
+      ht : Term;
+      sum : natural32 := 0;
+
+    begin
+      ht.cf := t.cf;
+      for i in t.dg'range loop
+        sum := sum + t.dg(i);
+        htd(i) := t.dg(i);
+      end loop;
+      htd(htd'last) := natural32(deg)-sum;
+      ht.dg := htd;
+      Add(res,ht);
+      continue := true;
+    end Embed_Term;
+    procedure Embed_Terms is new Visiting_Iterator(Embed_Term);
+
+  begin
+    Embed_Terms(p);
+    Clear(htd);
+    return res;
+  end Projective_Transformation;
+
+  function Projective_Transformation
+             ( p : QuadDobl_Complex_Polynomials.Poly )
+             return QuadDobl_Complex_Polynomials.Poly is
+
+    use QuadDobl_Complex_Numbers;
+    use QuadDobl_Complex_Polynomials;
+  
+    deg : constant integer32 := Degree(p);
+    htd : Degrees
+        := new Standard_Natural_Vectors.Vector
+                 (1..integer32(Number_of_Unknowns(p))+1);
+    res : Poly := Null_Poly;
+
+    procedure Embed_Term ( t : in Term; continue : out boolean ) is
+
+      ht : Term;
+      sum : natural32 := 0;
+
+    begin
+      ht.cf := t.cf;
+      for i in t.dg'range loop
+        sum := sum + t.dg(i);
+        htd(i) := t.dg(i);
+      end loop;
+      htd(htd'last) := natural32(deg)-sum;
+      ht.dg := htd;
+      Add(res,ht);
+      continue := true;
+    end Embed_Term;
+    procedure Embed_Terms is new Visiting_Iterator(Embed_Term);
+
+  begin
+    Embed_Terms(p);
+    Clear(htd);
+    return res;
+  end Projective_Transformation;
+
+  procedure Projective_Transformation
+              ( p : in out Standard_Complex_Polynomials.Poly ) is
+
+    use Standard_Complex_Polynomials;
   
     res : Poly := Projective_Transformation(p);
 
@@ -46,9 +132,33 @@ package body Projective_Transformations is
     Copy(res,p); Clear(res);
   end Projective_Transformation;
 
-  function Projective_Transformation ( p : Poly_Sys ) return Poly_Sys is
+  procedure Projective_Transformation
+              ( p : in out DoblDobl_Complex_Polynomials.Poly ) is
 
-    res : Poly_Sys(p'range);
+    use DoblDobl_Complex_Polynomials;
+  
+    res : Poly := Projective_Transformation(p);
+
+  begin
+    Copy(res,p); Clear(res);
+  end Projective_Transformation;
+
+  procedure Projective_Transformation
+              ( p : in out QuadDobl_Complex_Polynomials.Poly ) is
+
+    use QuadDobl_Complex_Polynomials;
+  
+    res : Poly := Projective_Transformation(p);
+
+  begin
+    Copy(res,p); Clear(res);
+  end Projective_Transformation;
+
+  function Projective_Transformation
+             ( p : Standard_Complex_Poly_Systems.Poly_Sys )
+             return Standard_Complex_Poly_Systems.Poly_Sys is
+
+    res : Standard_Complex_Poly_Systems.Poly_Sys(p'range);
 
   begin
     for k in p'range loop
@@ -57,14 +167,62 @@ package body Projective_Transformations is
     return res;
   end Projective_Transformation;
 
-  procedure Projective_Transformation ( p : in out Poly_Sys ) is
+  function Projective_Transformation
+             ( p : DoblDobl_Complex_Poly_Systems.Poly_Sys )
+             return DoblDobl_Complex_Poly_Systems.Poly_Sys is
+
+    res : DoblDobl_Complex_Poly_Systems.Poly_Sys(p'range);
+
+  begin
+    for k in p'range loop
+      res(k) := Projective_Transformation(p(k));
+    end loop;
+    return res;
+  end Projective_Transformation;
+
+  function Projective_Transformation
+             ( p : QuadDobl_Complex_Poly_Systems.Poly_Sys )
+             return QuadDobl_Complex_Poly_Systems.Poly_Sys is
+
+    res : QuadDobl_Complex_Poly_Systems.Poly_Sys(p'range);
+
+  begin
+    for k in p'range loop
+      res(k) := Projective_Transformation(p(k));
+    end loop;
+    return res;
+  end Projective_Transformation;
+
+  procedure Projective_Transformation
+              ( p : in out Standard_Complex_Poly_Systems.Poly_Sys ) is
   begin
     for k in p'range loop
       Projective_Transformation(p(k));
     end loop;
   end Projective_Transformation;
 
-  function Projective_Transformation ( s : Solution ) return Solution is
+  procedure Projective_Transformation
+              ( p : in out DoblDobl_Complex_Poly_Systems.Poly_Sys ) is
+  begin
+    for k in p'range loop
+      Projective_Transformation(p(k));
+    end loop;
+  end Projective_Transformation;
+
+  procedure Projective_Transformation
+              ( p : in out QuadDobl_Complex_Poly_Systems.Poly_Sys ) is
+  begin
+    for k in p'range loop
+      Projective_Transformation(p(k));
+    end loop;
+  end Projective_Transformation;
+
+  function Projective_Transformation
+             ( s : Standard_Complex_Solutions.Solution )
+             return Standard_Complex_Solutions.Solution is
+
+    use Standard_Complex_Numbers;
+    use Standard_Complex_Solutions;
 
     n : constant integer32 := s.n;
     r : Solution(n+1);
@@ -81,7 +239,54 @@ package body Projective_Transformations is
   end Projective_Transformation;
 
   function Projective_Transformation
-             ( sols : Solution_List ) return Solution_List is
+             ( s : DoblDobl_Complex_Solutions.Solution )
+             return DoblDobl_Complex_Solutions.Solution is
+
+    use DoblDobl_Complex_Numbers;
+    use DoblDobl_Complex_Solutions;
+
+    n : constant integer32 := s.n;
+    r : Solution(n+1);
+    one : constant double_double := create(1.0);
+
+  begin
+    r.v(1..n) := s.v(1..n);
+    r.v(n+1) := Create(one);
+    r.t := s.t;
+    r.m := s.m;
+    r.err := s.err;
+    r.rco := s.rco;
+    r.res := s.res;
+    return r;
+  end Projective_Transformation;
+
+  function Projective_Transformation
+             ( s : QuadDobl_Complex_Solutions.Solution )
+             return QuadDobl_Complex_Solutions.Solution is
+
+    use QuadDobl_Complex_Numbers;
+    use QuadDobl_Complex_Solutions;
+
+    n : constant integer32 := s.n;
+    r : Solution(n+1);
+    one : constant quad_double := create(1.0);
+
+  begin
+    r.v(1..n) := s.v(1..n);
+    r.v(n+1) := Create(one);
+    r.t := s.t;
+    r.m := s.m;
+    r.err := s.err;
+    r.rco := s.rco;
+    r.res := s.res;
+    return r;
+  end Projective_Transformation;
+
+  function Projective_Transformation
+             ( sols : Standard_Complex_Solutions.Solution_List )
+             return Standard_Complex_Solutions.Solution_List is
+
+    use Standard_Complex_Solutions;
 
     res,res_last : Solution_List;
     tmp : Solution_List := sols;
@@ -96,7 +301,49 @@ package body Projective_Transformations is
     return res;
   end Projective_Transformation;
 
-  procedure Projective_Transformation ( sols : in out Solution_List ) is
+  function Projective_Transformation
+             ( sols : DoblDobl_Complex_Solutions.Solution_List )
+             return DoblDobl_Complex_Solutions.Solution_List is
+
+    use DoblDobl_Complex_Solutions;
+
+    res,res_last : Solution_List;
+    tmp : Solution_List := sols;
+    ls : Link_to_Solution;
+
+  begin
+    while not Is_Null(tmp) loop
+      ls := Head_Of(tmp);
+      Append(res,res_last,Projective_Transformation(ls.all));
+      tmp := Tail_Of(tmp);
+    end loop;
+    return res;
+  end Projective_Transformation;
+
+  function Projective_Transformation
+             ( sols : QuadDobl_Complex_Solutions.Solution_List )
+             return QuadDobl_Complex_Solutions.Solution_List is
+
+    use QuadDobl_Complex_Solutions;
+
+    res,res_last : Solution_List;
+    tmp : Solution_List := sols;
+    ls : Link_to_Solution;
+
+  begin
+    while not Is_Null(tmp) loop
+      ls := Head_Of(tmp);
+      Append(res,res_last,Projective_Transformation(ls.all));
+      tmp := Tail_Of(tmp);
+    end loop;
+    return res;
+  end Projective_Transformation;
+
+  procedure Projective_Transformation 
+              ( sols : in out Standard_Complex_Solutions.Solution_List ) is
+
+    use Standard_Complex_Solutions;
+
   begin
     if Is_Null(sols) then
       null;
@@ -121,7 +368,70 @@ package body Projective_Transformations is
     end if;
   end Projective_Transformation;
 
-  function Affine_Transformation ( s : Solution ) return Solution is
+  procedure Projective_Transformation 
+              ( sols : in out DoblDobl_Complex_Solutions.Solution_List ) is
+
+    use DoblDobl_Complex_Solutions;
+
+  begin
+    if Is_Null(sols) then
+      null;
+    else
+      declare
+        temp : Solution_List := sols;
+        n : constant integer32 := Head_Of(sols).n;
+        ls : Link_To_Solution;
+        s : Solution(n);
+        s2 : Solution(n+1);
+      begin
+        while not Is_Null(temp) loop
+          ls := Head_Of(temp);
+          s := ls.all;
+          s2 := Projective_Transformation(s);
+          Clear(ls);
+          ls := new Solution'(s2);
+          Set_Head(temp,ls);
+          temp := Tail_Of(temp);
+        end loop;
+      end;
+    end if;
+  end Projective_Transformation;
+
+  procedure Projective_Transformation 
+              ( sols : in out QuadDobl_Complex_Solutions.Solution_List ) is
+
+    use QuadDobl_Complex_Solutions;
+
+  begin
+    if Is_Null(sols) then
+      null;
+    else
+      declare
+        temp : Solution_List := sols;
+        n : constant integer32 := Head_Of(sols).n;
+        ls : Link_To_Solution;
+        s : Solution(n);
+        s2 : Solution(n+1);
+      begin
+        while not Is_Null(temp) loop
+          ls := Head_Of(temp);
+          s := ls.all;
+          s2 := Projective_Transformation(s);
+          Clear(ls);
+          ls := new Solution'(s2);
+          Set_Head(temp,ls);
+          temp := Tail_Of(temp);
+        end loop;
+      end;
+    end if;
+  end Projective_Transformation;
+
+  function Affine_Transformation
+             ( s : Standard_Complex_Solutions.Solution )
+             return Standard_Complex_Solutions.Solution is
+
+    use Standard_Complex_Numbers;
+    use Standard_Complex_Solutions;
 
     n : constant integer32 := s.n;
     r : Solution(n-1);
@@ -129,8 +439,8 @@ package body Projective_Transformations is
 
   begin
     for i in 1..(n-1) loop
-      if absvn + Create(1.0) = Create(1.0)
-       then r.v(i) := Create(10.0**10);
+      if absvn + 1.0 = 1.0 
+       then r.v(i) := Create(1.0E+10);
        else r.v(i) := s.v(i) / s.v(n);
       end if;
      end loop;
@@ -142,11 +452,137 @@ package body Projective_Transformations is
      return r;
   exception
     when constraint_error =>
-       r.v(1..(n-1)) := (1..(n-1) => Create(10.0**10));
+       r.v(1..(n-1)) := (1..(n-1) => Create(1.0E+10));
        return r;
   end Affine_Transformation;
 
-  procedure Affine_Transformation ( sols : in out Solution_List ) is
+  function Affine_Transformation
+             ( s : DoblDobl_Complex_Solutions.Solution )
+             return DoblDobl_Complex_Solutions.Solution is
+
+    use DoblDobl_Complex_Numbers;
+    use DoblDobl_Complex_Solutions;
+
+    n : constant integer32 := s.n;
+    r : Solution(n-1);
+    absvn : constant double_double := AbsVal(s.v(n));
+    largecst : constant double_double := create(1.0E+20);
+    one : constant double_double := create(1.0);
+
+  begin
+    for i in 1..(n-1) loop
+      if absvn + one = one
+       then r.v(i) := Create(largecst);
+       else r.v(i) := s.v(i) / s.v(n);
+      end if;
+     end loop;
+     r.t := s.t;
+     r.m := s.m;
+     r.err := s.err;
+     r.rco := s.rco;
+     r.res := s.res;
+     return r;
+  exception
+    when constraint_error =>
+       r.v(1..(n-1)) := (1..(n-1) => Create(largecst));
+       return r;
+  end Affine_Transformation;
+
+  function Affine_Transformation
+             ( s : QuadDobl_Complex_Solutions.Solution )
+             return QuadDobl_Complex_Solutions.Solution is
+
+    use QuadDobl_Complex_Numbers;
+    use QuadDobl_Complex_Solutions;
+
+    n : constant integer32 := s.n;
+    r : Solution(n-1);
+    absvn : constant quad_double := AbsVal(s.v(n));
+    largecst : constant quad_double := create(1.0E+20);
+    one : constant quad_double := create(1.0);
+
+  begin
+    for i in 1..(n-1) loop
+      if absvn + one = one
+       then r.v(i) := Create(largecst);
+       else r.v(i) := s.v(i) / s.v(n);
+      end if;
+     end loop;
+     r.t := s.t;
+     r.m := s.m;
+     r.err := s.err;
+     r.rco := s.rco;
+     r.res := s.res;
+     return r;
+  exception
+    when constraint_error =>
+       r.v(1..(n-1)) := (1..(n-1) => Create(largecst));
+       return r;
+  end Affine_Transformation;
+
+  procedure Affine_Transformation
+              ( sols : in out Standard_Complex_Solutions.Solution_List ) is
+
+    use Standard_Complex_Solutions;
+
+  begin
+    if Is_Null(sols) then
+      null;
+    else
+      declare
+        n : constant integer32 := Head_Of(sols).n;
+        s1 : Solution(n);
+        s2 : Solution(n-1);
+        temp : Solution_List := sols;
+        ls : Link_To_Solution;
+      begin
+        while not Is_Null(temp) loop
+          ls := Head_Of(temp);
+          s1 := ls.all;
+          s2 := Affine_Transformation(s1);
+          Clear(ls);
+          ls := new Solution'(s2);
+          Set_Head(temp,ls);
+          temp := Tail_Of(temp);
+        end loop;
+      end;
+    end if;
+  end Affine_Transformation;
+
+  procedure Affine_Transformation
+              ( sols : in out DoblDobl_Complex_Solutions.Solution_List ) is
+
+    use DoblDobl_Complex_Solutions;
+
+  begin
+    if Is_Null(sols) then
+      null;
+    else
+      declare
+        n : constant integer32 := Head_Of(sols).n;
+        s1 : Solution(n);
+        s2 : Solution(n-1);
+        temp : Solution_List := sols;
+        ls : Link_To_Solution;
+      begin
+        while not Is_Null(temp) loop
+          ls := Head_Of(temp);
+          s1 := ls.all;
+          s2 := Affine_Transformation(s1);
+          Clear(ls);
+          ls := new Solution'(s2);
+          Set_Head(temp,ls);
+          temp := Tail_Of(temp);
+        end loop;
+      end;
+    end if;
+  end Affine_Transformation;
+
+  procedure Affine_Transformation
+              ( sols : in out QuadDobl_Complex_Solutions.Solution_List ) is
+
+    use QuadDobl_Complex_Solutions;
+
   begin
     if Is_Null(sols) then
       null;
--- src/Ada/Deformations/Homotopy/projective_transformations.ads
+++ src/Ada/Deformations/Homotopy/projective_transformations.ads
@@ -1,37 +1,102 @@
-with Standard_Complex_Polynomials;       use Standard_Complex_Polynomials;
-with Standard_Complex_Poly_Systems;      use Standard_Complex_Poly_Systems;
-with Standard_Complex_Solutions;         use Standard_Complex_Solutions;
+with Standard_Complex_Polynomials;
+with Standard_Complex_Poly_Systems;
+with DoblDobl_Complex_Polynomials;
+with DoblDobl_Complex_Poly_Systems;
+with QuadDobl_Complex_Polynomials;
+with QuadDobl_Complex_Poly_Systems;
+with Standard_Complex_Solutions;
+with DoblDobl_Complex_Solutions;
+with QuadDobl_Complex_Solutions;
 
 package Projective_Transformations is
 
 -- DESCRIPTION :
---   This package provides routines for performing a projective
---   transformation on polynomials and polynomial systems, on
---   solutions and on lists of solutions.
+--   This package exports methods to perform projective transformations
+--   on polynomials, polynomial systems, solutions, and lists of solutions,
+--   in standard double, double double, and quad double precision.
 
-  function Projective_Transformation ( p : Poly ) return Poly;
-  function Projective_Transformation ( p : Poly_Sys ) return Poly_Sys;
+  function Projective_Transformation 
+             ( p : Standard_Complex_Polynomials.Poly )
+             return Standard_Complex_Polynomials.Poly;
+  function Projective_Transformation 
+             ( p : DoblDobl_Complex_Polynomials.Poly )
+             return DoblDobl_Complex_Polynomials.Poly;
+  function Projective_Transformation 
+             ( p : QuadDobl_Complex_Polynomials.Poly )
+             return QuadDobl_Complex_Polynomials.Poly;
+  function Projective_Transformation
+             ( p : Standard_Complex_Poly_Systems.Poly_Sys )
+             return Standard_Complex_Poly_Systems.Poly_Sys;
+  function Projective_Transformation
+             ( p : DoblDobl_Complex_Poly_Systems.Poly_Sys )
+             return DoblDobl_Complex_Poly_Systems.Poly_Sys;
+  function Projective_Transformation
+             ( p : QuadDobl_Complex_Poly_Systems.Poly_Sys )
+             return QuadDobl_Complex_Poly_Systems.Poly_Sys;
 
-  procedure Projective_Transformation ( p : in out Poly );
-  procedure Projective_Transformation ( p : in out Poly_Sys );
+  procedure Projective_Transformation
+              ( p : in out Standard_Complex_Polynomials.Poly );
+  procedure Projective_Transformation
+              ( p : in out Standard_Complex_Poly_Systems.Poly_Sys );
+  procedure Projective_Transformation
+              ( p : in out DoblDobl_Complex_Polynomials.Poly );
+  procedure Projective_Transformation
+              ( p : in out DoblDobl_Complex_Poly_Systems.Poly_Sys );
+  procedure Projective_Transformation
+              ( p : in out QuadDobl_Complex_Polynomials.Poly );
+  procedure Projective_Transformation
+              ( p : in out QuadDobl_Complex_Poly_Systems.Poly_Sys );
 
   -- DESCRIPTION :
   --   An additional unknown is added so that each term has the same degree.
 
-  function Projective_Transformation ( s : Solution ) return Solution;
+  function Projective_Transformation
+             ( s : Standard_Complex_Solutions.Solution )
+             return Standard_Complex_Solutions.Solution;
+  function Projective_Transformation
+             ( s : DoblDobl_Complex_Solutions.Solution )
+             return DoblDobl_Complex_Solutions.Solution;
+  function Projective_Transformation
+             ( s : QuadDobl_Complex_Solutions.Solution )
+             return QuadDobl_Complex_Solutions.Solution;
 
   -- DESCRIPTION :
   --   Adds 1 as last component to the solution.
 
   function Projective_Transformation
-             ( sols : Solution_List ) return Solution_List;
-  procedure Projective_Transformation ( sols : in out Solution_List );
+             ( sols : Standard_Complex_Solutions.Solution_List )
+           return Standard_Complex_Solutions.Solution_List;
+  function Projective_Transformation
+             ( sols : DoblDobl_Complex_Solutions.Solution_List )
+           return DoblDobl_Complex_Solutions.Solution_List;
+  function Projective_Transformation
+             ( sols : QuadDobl_Complex_Solutions.Solution_List )
+           return QuadDobl_Complex_Solutions.Solution_List;
+  procedure Projective_Transformation
+              ( sols : in out Standard_Complex_Solutions.Solution_List );
+  procedure Projective_Transformation
+              ( sols : in out DoblDobl_Complex_Solutions.Solution_List );
+  procedure Projective_Transformation
+              ( sols : in out QuadDobl_Complex_Solutions.Solution_List );
 
   -- DESCRIPTION :
   --   An additional component, equal to 1, is added to the solution vector.
 
-  function  Affine_Transformation ( s : Solution ) return Solution;
-  procedure Affine_Transformation ( sols : in out Solution_List );
+  function Affine_Transformation
+             ( s : Standard_Complex_Solutions.Solution )
+           return Standard_Complex_Solutions.Solution;
+  function Affine_Transformation
+             ( s : DoblDobl_Complex_Solutions.Solution )
+           return DoblDobl_Complex_Solutions.Solution;
+  function Affine_Transformation
+             ( s : QuadDobl_Complex_Solutions.Solution )
+           return QuadDobl_Complex_Solutions.Solution;
+  procedure Affine_Transformation
+              ( sols : in out Standard_Complex_Solutions.Solution_List );
+  procedure Affine_Transformation
+              ( sols : in out DoblDobl_Complex_Solutions.Solution_List );
+  procedure Affine_Transformation
+              ( sols : in out QuadDobl_Complex_Solutions.Solution_List );
 
   -- DESCRIPTION :
   --   All components of the solution vector will be divided by the last
