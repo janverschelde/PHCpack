4983d63c047c7704caec39d4ed251353ab066b0a
Jan Verschelde <jan@math.uic.edu>
2014-09-08 20:04:26 -0500

    extended the polytopes module with a convex hull function in 3-space and 4-space

--- src/Python/PHCpy/phcpy/polytopes.py
+++ src/Python/PHCpy/phcpy/polytopes.py
@@ -29,6 +29,33 @@ def planar_convex_hull(points):
     hull = eval(strhull)
     return hull
 
+def convex_hull(dim, points):
+    """
+    Returns the list of facets of the convex hull of the points,
+    given in points.  The dimension of the ambient space is in dim.
+    """
+    from phcpy2c import py2c_giftwrap_convex_hull
+    from phcpy2c import py2c_giftwrap_number_of_facets
+    from phcpy2c import py2c_giftwrap_retrieve_facet
+    from phcpy2c import py2c_giftwrap_clear_3d_facets
+    from phcpy2c import py2c_giftwrap_clear_3d_facets
+    strpoints = str(points)
+    fail = py2c_giftwrap_convex_hull(len(strpoints), strpoints)
+    nbrfacets = py2c_giftwrap_number_of_facets(dim)
+    print 'computed', nbrfacets, 'facets'
+    result = []
+    for k in range(nbrfacets):
+        strfacet = py2c_giftwrap_retrieve_facet(dim, k)
+        facet = eval(strfacet)
+        result.append(facet)
+    if(dim == 3):
+        from phcpy2c import py2c_giftwrap_clear_3d_facets
+        fail = py2c_giftwrap_clear_3d_facets()
+    if(dim == 4):
+        from phcpy2c import py2c_giftwrap_clear_4d_facets
+        fail = py2c_giftwrap_clear_4d_facets()
+    return result
+
 def test_planar_hull():
     """
     Generates a random point configuration in the plane
@@ -40,5 +67,18 @@ def test_planar_hull():
     print 'the vertices :', vertices
     print 'inner normals :', normals
 
+def test_convex_hull():
+    """
+    Generates a random point configuration in 3-space
+    and then computes its convex hull.
+    """
+    pts = random_points(3, 10, -9, 9)
+    print 'the points :', pts
+    facets = convex_hull(3, pts)
+    print 'the facets :'
+    for facet in facets:
+        print facet
+
 if __name__ == "__main__":
     test_planar_hull()
+    test_convex_hull()
