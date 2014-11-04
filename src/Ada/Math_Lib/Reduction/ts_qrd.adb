09ddf5c5463d7a5bf00089d272c6ab670edf89d3
Jan Verschelde <jan@math.uic.edu>
2014-11-04 13:59:30 -0600

    new package for QR and least squares solving of quad double matrices

--- src/Ada/Math_Lib/Reduction/ts_qrd.adb
+++ src/Ada/Math_Lib/Reduction/ts_qrd.adb
@@ -44,6 +44,12 @@ with DoblDobl_Complex_Vector_Norms;      use DoblDobl_Complex_Vector_Norms;
 with DoblDobl_Random_Vectors;            use DoblDobl_Random_Vectors;
 with DoblDobl_Random_Matrices;           use DoblDobl_Random_Matrices;
 with DoblDobl_Complex_QR_Least_Squares;  use DoblDobl_Complex_QR_Least_Squares;
+with Quad_Double_Vectors;
+with Quad_Double_Vectors_io;             use Quad_Double_Vectors_io;
+with Quad_Double_Matrices;
+with Quad_Double_Matrices_io;            use Quad_Double_Matrices_io;
+with Quad_Double_Vector_Norms;           use Quad_Double_Vector_Norms;
+with Quad_Double_QR_Least_Squares;       use Quad_Double_QR_Least_Squares;
 with QuadDobl_Complex_Vectors;
 with QuadDobl_Complex_Vectors_io;        use QuadDobl_Complex_Vectors_io;
 with QuadDobl_Complex_Matrices;
@@ -102,6 +108,28 @@ procedure ts_qrd is
 
     res : Double_Double_Matrices.Matrix(a'range(1),a'range(2));
     zero : constant double_double := create(0.0);
+
+  begin
+    for i in a'range(1) loop
+      for j in a'first(2)..(i-1) loop
+        res(i,j) := zero;
+      end loop;
+      for j in i..a'last(2) loop
+        res(i,j) := a(i,j);
+      end loop;
+    end loop;
+    return res;
+  end Extract_Upper_Triangular;
+
+  function Extract_Upper_Triangular
+                ( a : Quad_Double_Matrices.Matrix )
+                return Quad_Double_Matrices.Matrix is
+
+  -- DESCRIPTION :
+  --   Returns the upper triangular part of the matrix a.
+
+    res : Quad_Double_Matrices.Matrix(a'range(1),a'range(2));
+    zero : constant quad_double := create(0.0);
 
   begin
     for i in a'range(1) loop
@@ -239,6 +267,23 @@ procedure ts_qrd is
     return sum;
   end Differences;
 
+  function Differences ( a,b : in Quad_Double_Matrices.Matrix )
+                       return quad_double is
+
+  -- DESCRIPTION :
+  --   Returns the sum of the differences of all elements |a(i,j)-b(i,j)|.
+
+    sum : quad_double := create(0.0);
+
+  begin
+    for i in a'range(1) loop
+      for j in a'range(2) loop
+        sum := sum + abs(a(i,j)-b(i,j));
+      end loop;
+    end loop;
+    return sum;
+  end Differences;
+
   function Differences ( a,b : in Standard_Complex_Matrices.Matrix )
                        return double_float is
 
@@ -342,8 +387,7 @@ procedure ts_qrd is
   end Orthogonality_Check_Sum;
 
   function Orthogonality_Check_Sum
-             ( q : Double_Double_Matrices.Matrix )
-             return double_double is
+             ( q : Double_Double_Matrices.Matrix ) return double_double is
 
   -- DESCRIPTION :
   --   Tests whether the columns are orthogonal w.r.t. each other,
@@ -352,6 +396,31 @@ procedure ts_qrd is
 
     sum,ip : double_double;
     zero : constant double_double := create(0.0);
+
+  begin
+    sum := zero;
+    for j in q'range(2) loop
+      for k in j+1..q'last(2) loop
+        ip := zero;
+        for i in q'range(1) loop
+          ip := ip + q(i,j)*q(i,k);
+        end loop;
+        sum := sum + abs(ip);
+      end loop;
+    end loop;
+    return sum;
+  end Orthogonality_Check_Sum;
+
+  function Orthogonality_Check_Sum
+             ( q : Quad_Double_Matrices.Matrix ) return quad_double is
+
+  -- DESCRIPTION :
+  --   Tests whether the columns are orthogonal w.r.t. each other,
+  --   returns the sum of all inner products of any column in q 
+  --   with all its following columns.
+
+    sum,ip : quad_double;
+    zero : constant quad_double := create(0.0);
 
   begin
     sum := zero;
@@ -520,6 +589,26 @@ procedure ts_qrd is
     put(Orthogonality_Check_Sum(q),3); new_line;
   end Test_QRD;
 
+  procedure Test_QRD ( a,q,r : in Quad_Double_Matrices.Matrix;
+                       output : in boolean ) is
+
+    wrk : Quad_Double_Matrices.Matrix(a'range(1),a'range(2));
+    use Quad_Double_Matrices;
+
+  begin
+    if output
+     then put_line("The upper triangular part R :"); put(r,3);
+    end if;
+    wrk := q*r;
+    if output
+     then put_line("q*r :"); put(wrk,3); 
+    end if;
+    put("Difference in 1-norm between the matrix and q*r : ");
+    put(Differences(a,wrk),3); new_line;
+    put("Orthogonality check sum : ");
+    put(Orthogonality_Check_Sum(q),3); new_line;
+  end Test_QRD;
+
   procedure Test_QRD ( a,q,r : in Standard_Complex_Matrices.Matrix;
                        output : in boolean ) is
 
@@ -628,13 +717,13 @@ procedure ts_qrd is
      then put_line("The matrix : "); put(a,3);
     end if;
     QRD(wrk,qraux,jpvt,piv);
-    if output
-     then put_line("The matrix after QR : "); put(wrk,3);
-          put_line("The vector qraux : "); put(qraux,3); new_line;
+    if output then
+      put_line("The matrix after QR : "); put(wrk,3);
+      put_line("The vector qraux : "); put(qraux,3); new_line;
     end if;
-    if piv
-     then put("The vector jpvt : "); put(jpvt); new_line;
-          Permute_Columns(wrk,jpvt);
+    if piv then
+      put("The vector jpvt : "); put(jpvt); new_line;
+      Permute_Columns(wrk,jpvt);
     end if;
     QRLS(wrk,n,n,m,qraux,b,dum2,dum3,sol,rsd,dum,110,info);
     if piv
@@ -644,9 +733,9 @@ procedure ts_qrd is
      then put_line("The solution : "); put(sol,3); new_line;
     end if;
     dum := b - a*sol;
-    if output
-     then put_line("right-hand size - matrix*solution : "); 
-          put(dum,3); new_line;
+    if output then
+      put_line("right-hand size - matrix*solution : "); 
+      put(dum,3); new_line;
     end if;
     put("The norm of residual : "); put(Sum_Norm(dum),3,3,3); new_line;
   end Standard_Real_LS_Test;          
@@ -666,13 +755,13 @@ procedure ts_qrd is
      then put_line("The matrix : "); put(a,3);
     end if;
     QRD(wrk,qraux,jpvt,piv);
-    if output
-     then put_line("The matrix after QR : "); put(wrk,3);
-          put_line("The vector qraux : "); put(qraux,3); new_line;
+    if output then
+      put_line("The matrix after QR : "); put(wrk,3);
+      put_line("The vector qraux : "); put(qraux,3); new_line;
     end if;
-    if piv
-     then put("The vector jpvt : "); put(jpvt); new_line;
-          Permute_Columns(wrk,jpvt);
+    if piv then
+      put("The vector jpvt : "); put(jpvt); new_line;
+      Permute_Columns(wrk,jpvt);
     end if;
     for i in wrk'range(1) loop
       for j in wrk'range(2) loop
@@ -931,6 +1020,170 @@ procedure ts_qrd is
     new_line;
     print_times(Standard_Output,timer,"Testing DoblDobl Real Least Squares");
   end DoblDobl_Random_Real_LS_Test;
+
+-- QUADDOBL REAL TEST DRIVERS :
+
+  procedure QuadDobl_Real_LS_Test
+              ( n,m : in integer32; piv : in boolean;
+                a : in Quad_Double_Matrices.Matrix;
+                b : in Quad_Double_Vectors.Vector;
+                output : in boolean ) is
+
+    zero : constant quad_double := create(0.0);
+    wrk : Quad_Double_Matrices.Matrix(1..n,1..m) := a;
+    qraux : Quad_Double_Vectors.Vector(1..m) := (1..m => zero);
+    jpvt : Standard_Integer_Vectors.Vector(1..m) := (1..m => 0);
+    sol : Quad_Double_Vectors.Vector(1..m);
+    rsd,dum,dum2,dum3 : Quad_Double_Vectors.Vector(1..n);
+    info : integer32;
+    use Quad_Double_Matrices;
+    use Quad_Double_Vectors;
+
+  begin
+    if output
+     then put_line("The matrix : "); put(a,3);
+    end if;
+    QRD(wrk,qraux,jpvt,piv);
+    if output then
+      put_line("The matrix after QR : "); put(wrk,3);
+      put_line("The vector qraux : "); put(qraux,3); new_line;
+    end if;
+    if piv then
+      put("The vector jpvt : "); put(jpvt); new_line;
+      Permute_Columns(wrk,jpvt);
+    end if;
+    QRLS(wrk,n,n,m,qraux,b,dum2,dum3,sol,rsd,dum,110,info);
+    if piv
+     then Permute(sol,jpvt);
+    end if;
+    if output
+     then put_line("The solution : "); put(sol,3); new_line;
+    end if;
+    dum := b - a*sol;
+    if output then
+      put_line("right-hand size - matrix*solution : "); 
+      put(dum,3); new_line;
+    end if;
+    put("The norm of residual : "); put(Sum_Norm(dum),3); new_line;
+  end QuadDobl_Real_LS_Test;          
+
+  procedure QuadDobl_Real_QR_Test
+              ( n,m : in integer32; piv : in boolean;
+                a : in Quad_Double_Matrices.Matrix;
+                output : in boolean ) is
+
+    zero : constant quad_double := create(0.0);
+    wrk : Quad_Double_Matrices.Matrix(1..n,1..m) := a;
+    bas : Quad_Double_Matrices.Matrix(1..n,1..n);
+    qraux : Quad_Double_Vectors.Vector(1..m) := (1..m => zero);
+    jpvt : Standard_Integer_Vectors.Vector(1..m) := (1..m => 0);
+
+  begin
+    if output
+     then put_line("The matrix : "); put(a,3);
+    end if;
+    QRD(wrk,qraux,jpvt,piv);
+    if output then
+      put_line("The matrix after QR : "); put(wrk,3);
+      put_line("The vector qraux : "); put(qraux,3); new_line;
+    end if;
+    if piv then
+      put("The vector jpvt : "); put(jpvt); new_line;
+      Permute_Columns(wrk,jpvt);
+    end if;
+    for i in wrk'range(1) loop
+      for j in wrk'range(2) loop
+        bas(i,j) := wrk(i,j);
+      end loop;
+      for j in n+1..m loop
+        bas(i,j) := zero;
+      end loop;
+    end loop;
+    Basis(bas,a);
+    if output
+     then put_line("The orthogonal part Q of QR  :"); put(bas,3);
+    end if;
+    Test_QRD(a,bas,Extract_Upper_Triangular(wrk),output);
+  end QuadDobl_Real_QR_Test;
+
+  procedure QuadDobl_Interactive_Real_QR_Test
+              ( n,m : in integer32; piv : in boolean ) is
+
+    a : Quad_Double_Matrices.Matrix(1..n,1..m);
+
+  begin
+    put("Give a "); put(n,1); put("x"); put(m,1);   
+    put_line(" matrix : "); get(a);
+    QuadDobl_Real_QR_Test(n,m,piv,a,true);
+  end QuadDobl_Interactive_Real_QR_Test;
+
+  procedure QuadDobl_Interactive_Real_LS_Test
+              ( n,m : in integer32; piv : in boolean ) is
+
+    a : Quad_Double_Matrices.Matrix(1..n,1..m);
+    b : Quad_Double_Vectors.Vector(1..n);
+
+  begin
+    put("Give a "); put(n,1); put("x"); put(m,1);   
+    put_line(" matrix : "); get(a);
+    put("Give right-hand size "); put(n,1);
+    put_line("-vector : "); get(b);
+    QuadDobl_Real_LS_Test(n,m,piv,a,b,true);
+  end QuadDobl_Interactive_Real_LS_Test;
+
+  procedure QuadDobl_Random_Real_QR_Test
+              ( n,m : in integer32; piv : in boolean ) is
+
+    a : Quad_Double_Matrices.Matrix(1..n,1..m);
+    nb : integer32 := 0;
+    output : boolean;
+    ans : character;
+    timer : Timing_Widget;
+
+  begin
+    put("Give the number of tests : "); get(nb);
+    put("Do you want to see all matrices and vectors ? (y/n) ");
+    Ask_Yes_or_No(ans);
+    output := (ans = 'y');
+    tstart(timer);
+    for i in 1..nb loop
+      a := Random_Matrix(natural32(n),natural32(m));
+      QuadDobl_Real_QR_Test(n,m,piv,a,output);
+    end loop;
+    tstop(timer);
+    put("Tested "); put(nb,1);
+    put_line(" QR factoriziations on quaddobl random real matrices.");
+    new_line;
+    print_times(Standard_Output,timer,"Random QuadDobl Real QR Factorizations");
+  end QuadDobl_Random_Real_QR_Test;
+
+  procedure QuadDobl_Random_Real_LS_Test
+              ( n,m : in integer32; piv : in boolean ) is
+
+    a : Quad_Double_Matrices.Matrix(1..n,1..m);
+    b : Quad_Double_Vectors.Vector(1..n);
+    nb : integer32 := 0;
+    ans : character;
+    output : boolean;
+    timer : Timing_Widget;
+
+  begin
+    put("Give the number of tests : "); get(nb);
+    put("Do you want to see all matrices and vectors ? (y/n) ");
+    Ask_Yes_or_No(ans);
+    output := (ans = 'y'); 
+    tstart(timer);
+    for i in 1..nb loop
+      a := Random_Matrix(natural32(n),natural32(m));
+      b := Random_Vector(1,n);
+      QuadDobl_Real_LS_Test(n,m,piv,a,b,output);
+    end loop;
+    tstop(timer);
+    put("Tested "); put(nb,1);
+    put_line(" real least squares on quaddobl random real matrices.");
+    new_line;
+    print_times(Standard_Output,timer,"Testing QuadDobl Real Least Squares");
+  end QuadDobl_Random_Real_LS_Test;
 
 -- STANDARD COMPLEX TEST DRIVERS :
 
@@ -1680,22 +1933,26 @@ procedure ts_qrd is
       put_line("  F.               on random dobldobl real matrix.");
       put_line("  G.                                  complex matrix.");
       put_line("*** QR and Least Squares in quad double precision ***");
-      put_line("  H. QR-decomposition on given quaddobl complex matrix.");
-      put_line("  I.                  on random quaddobl complex matrix.");
-      put_line("  J. Least Squares on given quaddobl complex matrix.");
-      put_line("  K.               on random quaddobl complex matrix.");
+      put_line("  H. QR-decomposition on given quaddobl real matrix.");
+      put_line("  I.                                    complex matrix.");
+      put_line("  J.                  on random quaddobl real matrix.");
+      put_line("  K.                                     complex matrix.");
+      put_line("  L. Least Squares on given quaddobl real matrix.");
+      put_line("  M.                                 complex matrix.");
+      put_line("  N.               on random quaddobl real matrix.");
+      put_line("  O.                                  complex matrix.");
       put_line("*** QR and Least Squares in arbitrary multiprecision ***");
-      put_line("  L. QR-decomposition on given multprec complex matrix.");
-      put_line("  M.                  on random multprec complex matrix.");
-      put_line("  N. Least Squares on given multprec complex matrix.");
-      put_line("  O.               on random multprec complex matrix.");
-      put("Make your choice (0, 1, .. , A, B, .., O) : ");
-      Ask_Alternative(choice,"0123456789ABCDEFGHIJKLMNO");
+      put_line("  P. QR-decomposition on given multprec complex matrix.");
+      put_line("  Q.                  on random multprec complex matrix.");
+      put_line("  R. Least Squares on given multprec complex matrix.");
+      put_line("  S.               on random multprec complex matrix.");
+      put("Make your choice (0, 1, .. , A, B, .., S) : ");
+      Ask_Alternative(choice,"0123456789ABCDEFGHIJKLMNOPQRS");
       exit when (choice = '0');
       new_line;
       put("Give the number of rows of the matrix : "); get(n);
       put("Give the number of columns of the matrix : "); get(m);
-      if choice = 'H' or choice = 'I' or choice = 'J' or choice = 'K'
+      if choice = 'P' or choice = 'Q' or choice = 'R' or choice = 'S'
        then put("Give the size of the numbers : "); get(sz);
       end if;
       case choice is
@@ -1718,15 +1975,19 @@ procedure ts_qrd is
         when 'F' => DoblDobl_Random_Real_LS_Test(n,m,piv);
         when 'G' => DoblDobl_Random_Complex_LS_Test(n,m,piv);
        -- QR and Least Squares in quad double arithmetic
-        when 'H' => QuadDobl_Interactive_Complex_QR_Test(n,m,piv);
-        when 'I' => QuadDobl_Random_Complex_QR_Test(n,m,piv);
-        when 'J' => QuadDobl_Interactive_Complex_LS_Test(n,m,piv);
-        when 'K' => QuadDobl_Random_Complex_LS_Test(n,m,piv);
+        when 'H' => QuadDobl_Interactive_Real_QR_Test(n,m,piv);
+        when 'I' => QuadDobl_Interactive_Complex_QR_Test(n,m,piv);
+        when 'J' => QuadDobl_Random_Real_QR_Test(n,m,piv);
+        when 'K' => QuadDobl_Random_Complex_QR_Test(n,m,piv);
+        when 'L' => QuadDobl_Interactive_Real_LS_Test(n,m,piv);
+        when 'M' => QuadDobl_Interactive_Complex_LS_Test(n,m,piv);
+        when 'N' => QuadDobl_Random_Real_LS_Test(n,m,piv);
+        when 'O' => QuadDobl_Random_Complex_LS_Test(n,m,piv);
        -- QR and Least Squares in arbitrary precision arithmetic
-        when 'L' => Multprec_Interactive_Complex_QR_Test(n,m,piv);
-        when 'M' => Multprec_Random_Complex_QR_Test(n,m,sz,piv);
-        when 'N' => Multprec_Interactive_Complex_LS_Test(n,m,piv);
-        when 'O' => Multprec_Random_Complex_LS_Test(n,m,sz,piv);
+        when 'P' => Multprec_Interactive_Complex_QR_Test(n,m,piv);
+        when 'Q' => Multprec_Random_Complex_QR_Test(n,m,sz,piv);
+        when 'R' => Multprec_Interactive_Complex_LS_Test(n,m,piv);
+        when 'S' => Multprec_Random_Complex_LS_Test(n,m,sz,piv);
         when others => null;
       end case;
     end loop;
