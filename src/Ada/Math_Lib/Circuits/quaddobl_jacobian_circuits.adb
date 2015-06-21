557a1c7adb00b791dc1ae6a02b7733362ed7e0bc
janv@uic.edu <jan@math.uic.edu>
2015-06-21 16:08:15 -0500

    encapsulation packages for circuits to evaluate Jacobian matrices in double double and quad double precision

---
+++ src/Ada/Math_Lib/Circuits/quaddobl_jacobian_circuits.adb
@@ -0,0 +1,226 @@
+with unchecked_deallocation;
+with Standard_Natural_VecVecs;
+with Standard_Integer_VecVecs;
+with Lists_of_Integer_Vectors;
+with Lexicographical_Supports;
+with QuadDobl_Complex_Polynomials;       use QuadDobl_Complex_Polynomials;
+with QuadDobl_Polynomial_Flatteners;
+with Coefficient_Supported_Polynomials;
+with Standard_Jacobian_Evaluations;
+with QuadDobl_Jacobian_Evaluations;      use QuadDobl_Jacobian_Evaluations;
+
+package body QuadDobl_Jacobian_Circuits is
+
+-- DATA STRUCTURE :
+
+  type Circuit_Rep ( m,nq,nv : integer32 ) is record
+    -- m  : distinct number of monomials 
+    -- nq : the number of equations 
+    -- nv : the number of variables
+    b : Standard_Natural_VecVecs.VecVec(1..m);   -- positions are bits
+    f : Standard_Natural_VecVecs.Link_to_VecVec; -- common factors
+    c : QuadDobl_Complex_VecVecs.VecVec(1..nq);  -- coefficients
+    k : Standard_Natural_VecVecs.VecVec(1..nq);  -- indices
+  end record;
+
+-- CONSTRUCTORS :
+
+  function Create ( p : Poly_Sys ) return Circuit is
+
+    res : Circuit;
+    nq : constant integer32 := p'last;
+    nv : constant integer32 := integer32(Number_of_Unknowns(p(p'first)));
+    c : QuadDobl_Complex_VecVecs.VecVec(p'range);
+    k : Standard_Natural_VecVecs.VecVec(p'range);
+    s,e : Lists_of_Integer_Vectors.List;
+    nof : boolean;
+
+  begin
+    s := QuadDobl_Polynomial_Flatteners.Distinct_Supports(p);
+    e := Lexicographical_Supports.Sort(s);
+    declare
+      m : constant integer32
+        := integer32(Lists_of_Integer_Vectors.Length_Of(e));
+      v : Standard_Integer_VecVecs.VecVec(1..m)
+        := Lists_of_Integer_Vectors.Shallow_Create(e);
+      w : Standard_Natural_VecVecs.VecVec(1..m)
+        := Standard_Jacobian_Evaluations.Integer_to_Natural(v);
+      b,f : Standard_Natural_VecVecs.VecVec(1..m);
+      rep : Circuit_Rep(m,nq,nv);
+    begin
+      Coefficient_Supported_Polynomials.Split_Common_Factors(w,f,b,nof);
+      rep.b := b;
+      if nof then
+        Standard_Natural_VecVecs.Clear(f);
+        rep.f := null;
+      else
+        rep.f := new Standard_Natural_VecVecs.VecVec'(f);
+      end if;
+      QuadDobl_Polynomial_Flatteners.Coefficients_of_Supports(p,v,c,k);
+      rep.k := k;
+      rep.c := c;
+      res := new Circuit_Rep'(rep);
+    end;
+    return res;
+  end Create;
+
+-- SELECTORS :
+
+  function Number_of_Polynomials ( c : Circuit ) return natural32 is
+  begin
+    if c = null
+     then return 0;
+     else return natural32(c.nq);
+    end if;
+  end Number_of_Polynomials;
+
+  function Number_of_Variables ( c : Circuit ) return natural32 is
+  begin
+    if c = null
+     then return 0;
+     else return natural32(c.nv);
+    end if;
+  end Number_of_Variables;
+
+  function Number_of_Monomials ( c : Circuit ) return natural32 is
+  begin
+    if c = null
+     then return 0;
+     else return natural32(c.m);
+    end if;
+  end Number_of_Monomials;
+
+  function Number_of_Terms ( c : Circuit; i : integer32 ) return natural32 is
+  begin
+    if c = null then
+      return 0;
+    elsif ((i < 1) or (i > c.nq)) then
+      return 0;
+    else
+      return natural32(c.k(i)'last);
+    end if;
+  end Number_of_Terms;
+
+  function Coefficients ( c : Circuit; i : integer32 )
+                        return QuadDobl_Complex_Vectors.Link_to_Vector is
+  begin
+    if c = null then
+      return null;
+    elsif ((i < 1) or (i > c.nq)) then
+      return null;
+    else
+      return c.c(i);
+    end if;
+  end Coefficients;
+
+  function Coefficient ( c : Circuit; i,j : integer32 )
+                       return Complex_Number is
+
+    res : Complex_Number;
+    cff : constant QuadDobl_Complex_Vectors.Link_to_Vector
+        := Coefficients(c,i);
+
+    use QuadDobl_Complex_Vectors;
+
+  begin
+    if cff = null
+     then res := Create(integer(0));
+     else res := cff(j); 
+    end if;
+    return res;
+  end Coefficient;
+
+  function Product ( c : Circuit; i,j : integer32 )
+                   return Standard_Natural_Vectors.Link_to_Vector is
+
+    ind : integer32;
+
+  begin
+    if c = null then
+      return null;
+    elsif ((i < 1) or (i > c.nq)) then
+      return null;
+    else
+      ind := integer32(c.k(i)(j));
+      return c.b(ind);
+    end if;
+  end Product;
+
+  function Factor ( c : Circuit; i,j : integer32 )
+                  return Standard_Natural_Vectors.Link_to_Vector is
+
+    ind : integer32;
+
+    use Standard_Natural_VecVecs;
+
+  begin
+    if c = null then
+      return null;
+    elsif ((i < 1) or (i > c.nq)) then
+      return null;
+    elsif c.f = null then
+      return null;
+    else
+      ind := integer32(c.k(i)(j));
+      return c.f(ind);
+    end if;
+  end Factor;
+
+-- EVALUATION AND DIFFERENTIATION :
+
+  function WorkSpace ( c : Circuit )
+                     return QuadDobl_Complex_VecVecs.VecVec is
+
+    res : QuadDobl_Complex_VecVecs.VecVec(1..c.m);
+    dim : integer32 := integer32(c.nv);
+
+  begin
+    for k in res'range loop
+      res(k) := new QuadDobl_Complex_Vectors.Vector(0..dim);
+    end loop;
+    return res;
+  end WorkSpace;
+
+  procedure EvalDiff ( c : in Circuit;
+                       x : in QuadDobl_Complex_Vectors.Vector;
+                       wrk : in out QuadDobl_Complex_VecVecs.VecVec;
+                       y : out QuadDobl_Complex_Vectors.Vector;
+                       A : out QuadDobl_Complex_Matrices.Matrix ) is
+
+    use Standard_Natural_VecVecs;
+
+  begin
+    if c.f = null
+     then EvalDiff(c.b,c.c,c.k,x,y,wrk,A);
+     else EvalDiff(c.f.all,c.b,c.c,c.k,x,y,wrk,A);
+    end if;
+  end EvalDiff;
+
+  procedure EvalDiff ( c : in Circuit;
+                       x : in QuadDobl_Complex_Vectors.Vector;
+                       wrk : in out QuadDobl_Complex_VecVecs.VecVec;
+                       y : out QuadDobl_Complex_Vectors.Vector;
+                       A : in QuadDobl_Complex_VecVecs.VecVec ) is
+
+    use Standard_Natural_VecVecs;
+
+  begin
+    if c.f = null
+     then EvalDiff(c.b,c.c,c.k,x,y,wrk,A);
+     else EvalDiff(c.f.all,c.b,c.c,c.k,x,y,wrk,A);
+    end if;
+  end EvalDiff;
+
+-- DESTRUCTOR :
+
+  procedure Clear ( c : in out Circuit ) is
+
+    procedure free is new unchecked_deallocation(Circuit_Rep,Circuit);
+
+  begin
+    if c /= null then
+      free(c);
+    end if;
+  end Clear;
+
+end QuadDobl_Jacobian_Circuits;
--- src/Ada/Math_Lib/Circuits/ts_gradcirc.adb
+++ src/Ada/Math_Lib/Circuits/ts_gradcirc.adb
@@ -20,19 +20,23 @@ with QuadDobl_Complex_Numbers;
 with QuadDobl_Complex_Numbers_io;        use QuadDobl_Complex_Numbers_io;
 with Multprec_Complex_Numbers;
 with Multprec_Complex_Numbers_io;        use Multprec_Complex_Numbers_io;
-with Standard_Complex_Vectors;
-with Standard_Random_Vectors;
-with DoblDobl_Complex_Vectors;
-with DoblDobl_Random_Vectors;
-with QuadDobl_Complex_Vectors;
-with QuadDobl_Random_Vectors;
-with Multprec_Complex_Vectors;
-with Multprec_Random_Vectors;
 with Standard_Natural_Vectors;
 with Standard_Natural_Vectors_io;        use Standard_Natural_Vectors_io;
 with Standard_Natural_VecVecs;
+with Standard_Complex_Vectors;
 with Standard_Complex_VecVecs;
 with Standard_Complex_Matrices;
+with Standard_Random_Vectors;
+with DoblDobl_Complex_Vectors;
+with DoblDobl_Random_Vectors;
+with DoblDobl_Complex_VecVecs;
+with DoblDobl_Complex_Matrices;
+with QuadDobl_Complex_Vectors;
+with QuadDobl_Random_Vectors;
+with QuadDobl_Complex_VecVecs;
+with QuadDobl_Complex_Matrices;
+with Multprec_Complex_Vectors;
+with Multprec_Random_Vectors;
 with Symbol_Table;
 with Standard_Complex_Polynomials;       use Standard_Complex_Polynomials;
 with Standard_Complex_Polynomials_io;    use Standard_Complex_Polynomials_io;
@@ -44,16 +48,26 @@ with Standard_Complex_Jaco_Matrices;
 with DoblDobl_Complex_Polynomials;       use DoblDobl_Complex_Polynomials;
 with DoblDobl_Complex_Polynomials_io;    use DoblDobl_Complex_Polynomials_io;
 with DoblDobl_Complex_Poly_Functions;
+with DoblDobl_Complex_Poly_Systems;
+with DoblDobl_Complex_Poly_Systems_io;   use DoblDobl_Complex_Poly_Systems_io;
+with DoblDobl_Complex_Poly_SysFun;
+with DoblDobl_Complex_Jaco_Matrices;
 with QuadDobl_Complex_Polynomials;       use QuadDobl_Complex_Polynomials;
 with QuadDobl_Complex_Polynomials_io;    use QuadDobl_Complex_Polynomials_io;
 with QuadDobl_Complex_Poly_Functions;
+with QuadDobl_Complex_Poly_Systems;
+with QuadDobl_Complex_Poly_Systems_io;   use QuadDobl_Complex_Poly_Systems_io;
+with QuadDobl_Complex_Poly_SysFun;
+with QuadDobl_Complex_Jaco_Matrices;
 with Multprec_Complex_Polynomials;       use Multprec_Complex_Polynomials;
 with Multprec_Complex_Polynomials_io;    use Multprec_Complex_Polynomials_io;
 with Multprec_Complex_Poly_Functions;
 with Standard_Gradient_Circuits;
 with Standard_Jacobian_Circuits;
 with DoblDobl_Gradient_Circuits;
+with DoblDobl_Jacobian_Circuits;
 with QuadDobl_Gradient_Circuits;
+with QuadDobl_Jacobian_Circuits;
 with Multprec_Gradient_Circuits;
 
 procedure ts_gradcirc is
@@ -179,6 +193,68 @@ procedure ts_gradcirc is
     end loop;
   end Compare;
 
+  procedure Compare ( x : in DoblDobl_Complex_Matrices.Matrix;
+                      y : in DoblDobl_Complex_Matrices.Matrix;
+                      tol : in double_float; output : in boolean ) is
+
+  -- DESCRIPTION :
+  --   Compares the values in x with the values in y.
+  --   If the differences between the corresponding values is larger
+  --   than the tolerance tol, then an error message is written.
+  --   If the flag output is true, then all differences are written.
+
+    use DoblDobl_Complex_Numbers;
+
+    dff : Complex_Number;
+    val : double_double;
+ 
+  begin
+    for i in x'range(1) loop
+      for j in x'range(2) loop
+        dff := x(i,j) - y(i,j);
+        val := AbsVal(dff);
+        if val > tol or output then
+          put("difference at ("); put(i,1); put(",");
+          put(j,1); put_line(") :");
+          put(x(i,j)); new_line;
+          put(y(i,j)); new_line;
+          put("error : "); put(val,3); new_line;
+        end if;
+      end loop;
+    end loop;
+  end Compare;
+
+  procedure Compare ( x : in DoblDobl_Complex_Matrices.Matrix;
+                      y : in DoblDobl_Complex_VecVecs.VecVec;
+                      tol : in double_float; output : in boolean ) is
+
+  -- DESCRIPTION :
+  --   Compares the values in x with the values in y.
+  --   If the differences between the corresponding values is larger
+  --   than the tolerance tol, then an error message is written.
+  --   If the flag output is true, then all differences are written.
+
+    use DoblDobl_Complex_Numbers;
+
+    dff : Complex_Number;
+    val : double_double;
+ 
+  begin
+    for i in x'range(1) loop
+      for j in x'range(2) loop
+        dff := x(i,j) - y(i)(j);
+        val := AbsVal(dff);
+        if val > tol or output then
+          put("difference at ("); put(i,1); put(",");
+          put(j,1); put_line(") :");
+          put(x(i,j)); new_line;
+          put(y(i)(j)); new_line;
+          put("error : "); put(val,3); new_line;
+        end if;
+      end loop;
+    end loop;
+  end Compare;
+
   procedure Compare ( x : in QuadDobl_Complex_Vectors.Vector;
                       y : in QuadDobl_Complex_Vectors.Vector;
                       tol : in double_float; output : in boolean ) is
@@ -204,6 +280,68 @@ procedure ts_gradcirc is
         put(y(i)); new_line;
         put("error : "); put(val,3); new_line;
       end if;
+    end loop;
+  end Compare;
+
+  procedure Compare ( x : in QuadDobl_Complex_Matrices.Matrix;
+                      y : in QuadDobl_Complex_Matrices.Matrix;
+                      tol : in double_float; output : in boolean ) is
+
+  -- DESCRIPTION :
+  --   Compares the values in x with the values in y.
+  --   If the differences between the corresponding values is larger
+  --   than the tolerance tol, then an error message is written.
+  --   If the flag output is true, then all differences are written.
+
+    use QuadDobl_Complex_Numbers;
+
+    dff : Complex_Number;
+    val : quad_double;
+ 
+  begin
+    for i in x'range(1) loop
+      for j in x'range(2) loop
+        dff := x(i,j) - y(i,j);
+        val := AbsVal(dff);
+        if val > tol or output then
+          put("difference at ("); put(i,1); put(",");
+          put(j,1); put_line(") :");
+          put(x(i,j)); new_line;
+          put(y(i,j)); new_line;
+          put("error : "); put(val,3); new_line;
+        end if;
+      end loop;
+    end loop;
+  end Compare;
+
+  procedure Compare ( x : in QuadDobl_Complex_Matrices.Matrix;
+                      y : in QuadDobl_Complex_VecVecs.VecVec;
+                      tol : in double_float; output : in boolean ) is
+
+  -- DESCRIPTION :
+  --   Compares the values in x with the values in y.
+  --   If the differences between the corresponding values is larger
+  --   than the tolerance tol, then an error message is written.
+  --   If the flag output is true, then all differences are written.
+
+    use QuadDobl_Complex_Numbers;
+
+    dff : Complex_Number;
+    val : quad_double;
+ 
+  begin
+    for i in x'range(1) loop
+      for j in x'range(2) loop
+        dff := x(i,j) - y(i)(j);
+        val := AbsVal(dff);
+        if val > tol or output then
+          put("difference at ("); put(i,1); put(",");
+          put(j,1); put_line(") :");
+          put(x(i,j)); new_line;
+          put(y(i)(j)); new_line;
+          put("error : "); put(val,3); new_line;
+        end if;
+      end loop;
     end loop;
   end Compare;
 
@@ -587,11 +725,96 @@ procedure ts_gradcirc is
     Standard_Complex_Jaco_Matrices.Clear(jm);
   end Standard_Jacobian_Test;
 
+  procedure DoblDobl_Jacobian_Test 
+              ( p : in DoblDobl_Complex_Poly_Systems.Poly_Sys;
+                c : in DoblDobl_Jacobian_Circuits.Circuit ) is
+
+  -- DESCRIPTION :
+  --   Tests the evaluation at a random point using the circuit c
+  --   defined by a polynomial system p,
+  --   with comparisons to the straightforward evaluations.
+
+    use DoblDobl_Jacobian_Circuits;
+
+    nm : constant integer32 := integer32(Number_of_Monomials(c));
+    nq : constant integer32 := integer32(Number_of_Polynomials(c));
+    nv : constant integer32 := integer32(Number_of_Variables(c));
+    x : constant DoblDobl_Complex_Vectors.Vector(1..nv)
+      := DoblDobl_Random_Vectors.Random_Vector(1,nv);
+    px : constant DoblDobl_Complex_Vectors.Vector(1..nq)
+       := DoblDobl_Complex_Poly_SysFun.Eval(p,x);
+    jm : DoblDobl_Complex_Jaco_Matrices.Jaco_Mat(1..nq,1..nv)
+       := DoblDobl_Complex_Jaco_Matrices.Create(p);
+    Ap : DoblDobl_Complex_Matrices.Matrix(jm'range(1),jm'range(2))
+       := DoblDobl_Complex_Jaco_Matrices.Eval(jm,x);
+    wrk : DoblDobl_Complex_VecVecs.VecVec(1..nm) := WorkSpace(c);
+    y : DoblDobl_Complex_Vectors.Vector(1..nq);
+    A : DoblDobl_Complex_Matrices.Matrix(1..nq,1..nv);
+    B : DoblDobl_Complex_VecVecs.VecVec(1..nv);
+
+  begin
+    EvalDiff(c,x,wrk,y,A);
+    for i in 1..nv loop
+      B(i) := new DoblDobl_Complex_Vectors.Vector(1..nq);
+    end loop;
+    EvalDiff(c,x,wrk,y,B);
+    put_line("Comparing the evaluation :");
+    Compare(px,y,1.0E-8,true);
+    put_line("Comparing the differentiation :");
+    Compare(Ap,A,1.0E-8,true);
+    put_line("Comparing the vectors of vectors :");
+    Compare(Ap,B,1.0E-8,true);
+    DoblDobl_Complex_Jaco_Matrices.Clear(jm);
+  end DoblDobl_Jacobian_Test;
+
+  procedure QuadDobl_Jacobian_Test 
+              ( p : in QuadDobl_Complex_Poly_Systems.Poly_Sys;
+                c : in QuadDobl_Jacobian_Circuits.Circuit ) is
+
+  -- DESCRIPTION :
+  --   Tests the evaluation at a random point using the circuit c
+  --   defined by a polynomial system p,
+  --   with comparisons to the straightforward evaluations.
+
+    use QuadDobl_Jacobian_Circuits;
+
+    nm : constant integer32 := integer32(Number_of_Monomials(c));
+    nq : constant integer32 := integer32(Number_of_Polynomials(c));
+    nv : constant integer32 := integer32(Number_of_Variables(c));
+    x : constant QuadDobl_Complex_Vectors.Vector(1..nv)
+      := QuadDobl_Random_Vectors.Random_Vector(1,nv);
+    px : constant QuadDobl_Complex_Vectors.Vector(1..nq)
+       := QuadDobl_Complex_Poly_SysFun.Eval(p,x);
+    jm : QuadDobl_Complex_Jaco_Matrices.Jaco_Mat(1..nq,1..nv)
+       := QuadDobl_Complex_Jaco_Matrices.Create(p);
+    Ap : QuadDobl_Complex_Matrices.Matrix(jm'range(1),jm'range(2))
+       := QuadDobl_Complex_Jaco_Matrices.Eval(jm,x);
+    wrk : QuadDobl_Complex_VecVecs.VecVec(1..nm) := WorkSpace(c);
+    y : QuadDobl_Complex_Vectors.Vector(1..nq);
+    A : QuadDobl_Complex_Matrices.Matrix(1..nq,1..nv);
+    B : QuadDobl_Complex_VecVecs.VecVec(1..nv);
+
+  begin
+    EvalDiff(c,x,wrk,y,A);
+    for i in 1..nv loop
+      B(i) := new QuadDobl_Complex_Vectors.Vector(1..nq);
+    end loop;
+    EvalDiff(c,x,wrk,y,B);
+    put_line("Comparing the evaluation :");
+    Compare(px,y,1.0E-8,true);
+    put_line("Comparing the differentiation :");
+    Compare(Ap,A,1.0E-8,true);
+    put_line("Comparing the vectors of vectors :");
+    Compare(Ap,B,1.0E-8,true);
+    QuadDobl_Complex_Jaco_Matrices.Clear(jm);
+  end QuadDobl_Jacobian_Test;
+
   procedure Standard_System_Test is
 
   -- DESCRIPTION :
   --   Prompts the user for a polynomial system and then
-  --   tests the operations on the circuit representing the system.
+  --   tests the operations on the circuit representing the system,
+  --   in standard double precision.
 
     use Standard_Jacobian_Circuits;
     use Standard_Natural_Vectors;
@@ -630,6 +853,96 @@ procedure ts_gradcirc is
     end loop;
     Standard_Jacobian_Test(p.all,c);
   end Standard_System_Test;
+
+  procedure DoblDobl_System_Test is
+
+  -- DESCRIPTION :
+  --   Prompts the user for a polynomial system and then
+  --   tests the operations on the circuit representing the system,
+  --   in double double precision.
+
+    use DoblDobl_Jacobian_Circuits;
+    use Standard_Natural_Vectors;
+
+    p : DoblDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
+    comfac : Standard_Natural_Vectors.Link_to_Vector;
+    c : Circuit;
+    nq : integer32;
+
+  begin
+    new_line;
+    put_line("Reading a polynomial system ..."); get(p);
+    c := Create(p.all);
+    put("-> number of equations : ");
+    put(Number_of_Polynomials(c),1); new_line;
+    put("-> number of variables : ");
+    put(Number_of_Variables(c),1); new_line;
+    put("-> number of monomials : ");
+    put(Number_of_Monomials(c),1); new_line;
+    nq := integer32(Number_of_Polynomials(c));
+    for k in 1..nq loop
+      put("-> polynomial "); put(k,1); put(" has ");
+      put(Number_of_Terms(c,k),1); put_line(" terms");
+    end loop;
+    for k in 1..nq loop
+      put("-> polynomial "); put(k,1); put_line(" : ");
+      for i in 1..integer32(Number_of_Terms(c,k)) loop
+        put(Coefficient(c,k,i));
+        put(Product(c,k,i).all);
+        comfac := Factor(c,k,i);
+        if comfac /= null then
+          put(" +"); put(comfac.all);
+        end if;
+        new_line;
+      end loop;
+    end loop;
+    DoblDobl_Jacobian_Test(p.all,c);
+  end DoblDobl_System_Test;
+
+  procedure QuadDobl_System_Test is
+
+  -- DESCRIPTION :
+  --   Prompts the user for a polynomial system and then
+  --   tests the operations on the circuit representing the system,
+  --   in quad double precision.
+
+    use QuadDobl_Jacobian_Circuits;
+    use Standard_Natural_Vectors;
+
+    p : QuadDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
+    comfac : Standard_Natural_Vectors.Link_to_Vector;
+    c : Circuit;
+    nq : integer32;
+
+  begin
+    new_line;
+    put_line("Reading a polynomial system ..."); get(p);
+    c := Create(p.all);
+    put("-> number of equations : ");
+    put(Number_of_Polynomials(c),1); new_line;
+    put("-> number of variables : ");
+    put(Number_of_Variables(c),1); new_line;
+    put("-> number of monomials : ");
+    put(Number_of_Monomials(c),1); new_line;
+    nq := integer32(Number_of_Polynomials(c));
+    for k in 1..nq loop
+      put("-> polynomial "); put(k,1); put(" has ");
+      put(Number_of_Terms(c,k),1); put_line(" terms");
+    end loop;
+    for k in 1..nq loop
+      put("-> polynomial "); put(k,1); put_line(" : ");
+      for i in 1..integer32(Number_of_Terms(c,k)) loop
+        put(Coefficient(c,k,i));
+        put(Product(c,k,i).all);
+        comfac := Factor(c,k,i);
+        if comfac /= null then
+          put(" +"); put(comfac.all);
+        end if;
+        new_line;
+      end loop;
+    end loop;
+    QuadDobl_Jacobian_Test(p.all,c);
+  end QuadDobl_System_Test;
 
   procedure Main is
 
@@ -671,7 +984,12 @@ procedure ts_gradcirc is
         when others => null;
       end case;
     else
-      Standard_System_Test;
+      case ans is
+        when '0' => Standard_System_Test;
+        when '1' => DoblDobl_System_Test;
+        when '2' => QuadDobl_System_Test;
+        when others => null;
+      end case;
     end if;
   end Main;
 
