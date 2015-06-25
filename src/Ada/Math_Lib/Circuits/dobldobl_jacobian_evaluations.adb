cf3db1dd8dfd4ee5cc426f1df4d8d25e43e19006
janv@uic.edu <jan@math.uic.edu>
2015-06-25 17:22:11 -0500

    forgot to stage and commit the most recent versions of some packages

--- src/Ada/Math_Lib/Circuits/dobldobl_jacobian_evaluations.adb
+++ src/Ada/Math_Lib/Circuits/dobldobl_jacobian_evaluations.adb
@@ -1,4 +1,5 @@
 with Standard_Integer_Numbers;           use Standard_Integer_Numbers;
+with Double_Double_Numbers;              use Double_Double_Numbers;
 with DoblDobl_Complex_Numbers;
 with Coefficient_Supported_Polynomials;  use Coefficient_Supported_Polynomials;
 with Standard_Gradient_Evaluations;
@@ -6,6 +7,142 @@ with Standard_Jacobian_Evaluations;
 with DoblDobl_Gradient_Evaluations;
 
 package body DoblDobl_Jacobian_Evaluations is
+
+  procedure EvalDiff
+              ( b : in Standard_Natural_VecVecs.VecVec;
+                c : in DoblDobl_Complex_VecVecs.VecVec;
+                k : in Standard_Natural_VecVecs.VecVec;
+                x : in DoblDobl_Complex_Vectors.Vector;
+                z : out DoblDobl_Complex_Vectors.Vector;
+                y : out DoblDobl_Complex_VecVecs.VecVec;
+                A : out DoblDobl_Complex_Matrices.Matrix ) is
+
+    ind : integer32;
+    use DoblDobl_Complex_Numbers;
+    cff : Complex_Number;
+    zero : constant double_double := create(0.0);
+
+  begin
+    DoblDobl_Gradient_Evaluations.Gradient_Monomials(b,x,y);
+    for i in z'range loop
+      z(i) := Create(zero);
+      for j in A'range(2) loop
+        A(i,j) := Create(zero);
+      end loop;
+      for j in c(i)'range loop
+        ind := integer32(k(i)(j));
+        cff := c(i)(j);
+        z(i) := z(i) + cff*y(ind)(0);
+        for k in A'range(2) loop
+          A(i,k) := A(i,k) + cff*y(ind)(k);
+        end loop;
+      end loop;
+    end loop;
+  end EvalDiff;
+
+  procedure EvalDiff
+              ( f,b : in Standard_Natural_VecVecs.VecVec;
+                c : in DoblDobl_Complex_VecVecs.VecVec;
+                k : in Standard_Natural_VecVecs.VecVec;
+                x : in DoblDobl_Complex_Vectors.Vector;
+                z : out DoblDobl_Complex_Vectors.Vector;
+                y : out DoblDobl_Complex_VecVecs.VecVec;
+                A : out DoblDobl_Complex_Matrices.Matrix ) is
+
+    ind : integer32;
+    use DoblDobl_Complex_Numbers;
+    cff : Complex_Number;
+    zero : constant double_double := create(0.0);
+
+  begin
+    DoblDobl_Gradient_Evaluations.Gradient_Monomials(f,b,x,y);
+    for i in z'range loop
+      z(i) := Create(zero);
+      for j in A'range(2) loop
+        A(i,j) := Create(zero);
+      end loop;
+      for j in c(i)'range loop
+        ind := integer32(k(i)(j));
+        cff := c(i)(j);
+        z(i) := z(i) + cff*y(ind)(0);
+        for k in A'range(2) loop
+          A(i,k) := A(i,k) + cff*y(ind)(k);
+        end loop;
+      end loop;
+    end loop;
+  end EvalDiff;
+
+  procedure EvalDiff
+              ( b : in Standard_Natural_VecVecs.VecVec;
+                c : in DoblDobl_Complex_VecVecs.VecVec;
+                k : in Standard_Natural_VecVecs.VecVec;
+                x : in DoblDobl_Complex_Vectors.Vector;
+                z : out DoblDobl_Complex_Vectors.Vector;
+                y : out DoblDobl_Complex_VecVecs.VecVec;
+                A : in DoblDobl_Complex_VecVecs.VecVec ) is
+
+    ind : integer32;
+    use DoblDobl_Complex_Numbers;
+    cff : Complex_Number;
+    yind,Arow : DoblDobl_Complex_Vectors.Link_to_Vector;
+    zero : constant double_double := create(0.0);
+
+  begin
+    DoblDobl_Gradient_Evaluations.Gradient_Monomials(b,x,y);
+    for i in z'range loop
+      z(i) := Create(zero);
+      Arow := A(i);
+      for j in Arow'range loop
+        Arow(j) := Create(zero);
+      end loop;
+      for j in c(i)'range loop
+        ind := integer32(k(i)(j));
+        cff := c(i)(j);
+        yind := y(ind);
+        Arow := A(i);
+        z(i) := z(i) + cff*yind(0);
+        for k in Arow'range loop
+          Arow(k) := Arow(k) + cff*yind(k);
+        end loop;
+      end loop;
+    end loop;
+  end EvalDiff;
+
+  procedure EvalDiff
+              ( f,b : in Standard_Natural_VecVecs.VecVec;
+                c : in DoblDobl_Complex_VecVecs.VecVec;
+                k : in Standard_Natural_VecVecs.VecVec;
+                x : in DoblDobl_Complex_Vectors.Vector;
+                z : out DoblDobl_Complex_Vectors.Vector;
+                y : out DoblDobl_Complex_VecVecs.VecVec;
+                A : in DoblDobl_Complex_VecVecs.VecVec ) is
+
+    ind : integer32;
+    use DoblDobl_Complex_Numbers;
+    cff : Complex_Number;
+    yind,Arow : DoblDobl_Complex_Vectors.Link_to_Vector;
+    zero : constant double_double := create(0.0);
+
+  begin
+    DoblDobl_Gradient_Evaluations.Gradient_Monomials(f,b,x,y);
+    for i in z'range loop
+      z(i) := Create(zero);
+      Arow := A(i);
+      for j in Arow'range loop
+        Arow(j) := Create(zero);
+      end loop;
+      for j in c(i)'range loop
+        ind := integer32(k(i)(j));
+        cff := c(i)(j);
+        yind := y(ind);
+        Arow := A(i);
+        z(i) := z(i) + cff*yind(0);
+        for k in Arow'range loop
+          Arow(k) := Arow(k) + cff*yind(k);
+        end loop;
+      end loop;
+    end loop;
+  end EvalDiff;
 
   procedure DoblDobl_Jacobian_Evaluation
               ( v : in Standard_Natural_VecVecs.VecVec;
--- src/Ada/Math_Lib/Circuits/dobldobl_jacobian_evaluations.ads
+++ src/Ada/Math_Lib/Circuits/dobldobl_jacobian_evaluations.ads
@@ -12,6 +12,81 @@ package DoblDobl_Jacobian_Evaluations is
 --   The system is assumed to be sparse but all polynomials may share
 --   the same support, or share some monomials.
 
+  procedure EvalDiff
+              ( b : in Standard_Natural_VecVecs.VecVec;
+                c : in DoblDobl_Complex_VecVecs.VecVec;
+                k : in Standard_Natural_VecVecs.VecVec;
+                x : in DoblDobl_Complex_Vectors.Vector;
+                z : out DoblDobl_Complex_Vectors.Vector;
+                y : out DoblDobl_Complex_VecVecs.VecVec;
+                A : out DoblDobl_Complex_Matrices.Matrix );
+  procedure EvalDiff
+              ( f,b : in Standard_Natural_VecVecs.VecVec;
+                c : in DoblDobl_Complex_VecVecs.VecVec;
+                k : in Standard_Natural_VecVecs.VecVec;
+                x : in DoblDobl_Complex_Vectors.Vector;
+                z : out DoblDobl_Complex_Vectors.Vector;
+                y : out DoblDobl_Complex_VecVecs.VecVec;
+                A : out DoblDobl_Complex_Matrices.Matrix );
+
+  -- DESCRIPTION :
+  --   Evaluates and differentiates the system defined by (f,b,c).
+  --   The common factors of the exponent vectors are in f
+  --   and the Speelpenning products are defined by the vectors in b.
+  --   In this version, the matrix A is the two dimensional structure.
+
+  -- ON ENTRY :
+  --   f        common factors in the exponent vectors,
+  --            if omitted, then there are no common factors;
+  --   b        bit vectors defining the positions in the products;
+  --   c        coefficients of the terms in the system;
+  --   k        indices in the flattened representation;
+  --   x        point where to evaluate and differentiate the system;
+  --   y        allocated work space for the evaluated monomials.
+
+  -- ON RETURN :
+  --   z        evaluated system at x;
+  --   y        work space;
+  --   A        evaluated Jacobian matrix.
+
+  procedure EvalDiff
+              ( b : in Standard_Natural_VecVecs.VecVec;
+                c : in DoblDobl_Complex_VecVecs.VecVec;
+                k : in Standard_Natural_VecVecs.VecVec;
+                x : in DoblDobl_Complex_Vectors.Vector;
+                z : out DoblDobl_Complex_Vectors.Vector;
+                y : out DoblDobl_Complex_VecVecs.VecVec;
+                A : in DoblDobl_Complex_VecVecs.VecVec );
+  procedure EvalDiff
+              ( f,b : in Standard_Natural_VecVecs.VecVec;
+                c : in DoblDobl_Complex_VecVecs.VecVec;
+                k : in Standard_Natural_VecVecs.VecVec;
+                x : in DoblDobl_Complex_Vectors.Vector;
+                z : out DoblDobl_Complex_Vectors.Vector;
+                y : out DoblDobl_Complex_VecVecs.VecVec;
+                A : in DoblDobl_Complex_VecVecs.VecVec );
+
+  -- DESCRIPTION :
+  --   Evaluates and differentiates the system defined by (f,b,c).
+  --   The common factors of the exponent vectors are in f
+  --   and the Speelpenning products are defined by the vectors in b.
+  --   In this version, the matrix is stored as a vector of columns.
+
+  -- ON ENTRY :
+  --   f        common factors in the exponent vectors,
+  --            if omitted, then there are no common factors;
+  --   b        bit vectors defining the positions in the products;
+  --   c        coefficients of the terms in the system;
+  --   k        indices in the flattened representation;
+  --   x        point where to evaluate and differentiate the system;
+  --   y        allocated work space for the evaluated monomials;
+  --   A        allocated array of columns of the Jacobian matrix.
+
+  -- ON RETURN :
+  --   z        evaluated system at x;
+  --   y        work space;
+  --   A        evaluated Jacobian matrix.
+
   procedure DoblDobl_Jacobian_Evaluation
               ( v : in Standard_Natural_VecVecs.VecVec;
                 c : in DoblDobl_Complex_VecVecs.VecVec;
