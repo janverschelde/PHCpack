b2b950bf99b351498f63bb9f808bf80216dfbbd2
janv@uic.edu <jan@math.uic.edu>
2015-08-06 15:07:18 -0500

    calling accelerated Newton's method for quad doubles

--- src/Ada/PHCtoC/ts_gpunewton_qd.adb
+++ src/Ada/PHCtoC/ts_gpunewton_qd.adb
@@ -1,41 +1,41 @@
 with text_io;                            use text_io;
 with Standard_Natural_Numbers_io;        use Standard_Natural_Numbers_io;
 with Standard_Integer_Numbers;           use Standard_Integer_Numbers;
-with DoblDobl_Complex_Poly_Systems;
-with DoblDobl_Complex_Solutions;
-with DoblDobl_System_and_Solutions_io;
-with DoblDobl_PolySys_Container;
-with DoblDobl_Solutions_Container;
+with QuadDobl_Complex_Poly_Systems;
+with QuadDobl_Complex_Solutions;
+with QuadDobl_System_and_Solutions_io;
+with QuadDobl_PolySys_Container;
+with QuadDobl_Solutions_Container;
 
-procedure ts_gpunewton_dd is
+procedure ts_gpunewton_qd is
 
 -- DESCRIPTION :
 --   Procedure to test the development of the acceleration of Newton's method
 --   as called from an Ada procedure.  The Ada procedure remains in control.
 --   Data is passed to the C++ code via the systems and solutions containers.
 
-  procedure DoblDobl_GPU_Newton is
+  procedure QuadDobl_GPU_Newton is
 
   -- DESCRIPTION :
-  --   Calls the accelerated Newton's method in double double precision.
+  --   Calls the accelerated Newton's method in quad double precision.
 
     return_of_call : integer32;
 
     function newton return integer32;
-    pragma import(C, newton, "gpunewton_dd");
+    pragma import(C, newton, "gpunewton_qd");
 
   begin
     return_of_call := newton;
-  end DoblDobl_GPU_Newton;
+  end QuadDobl_GPU_Newton;
 
-  procedure DoblDobl_Newton is
+  procedure QuadDobl_Newton is
 
   -- DESCRIPTION :
   --   Reads a polynomial system and a corresponding list of solutions
-  --   in double double precision.
+  --   in quad double precision.
 
-    use DoblDobl_Complex_Poly_Systems;
-    use DoblDobl_Complex_Solutions;
+    use QuadDobl_Complex_Poly_Systems;
+    use QuadDobl_Complex_Solutions;
 
     p : Link_to_Poly_Sys;
     sols : Solution_List;
@@ -43,21 +43,21 @@ procedure ts_gpunewton_dd is
   begin
     new_line;
     put_line("Reading a system with solutions ...");
-    DoblDobl_System_and_Solutions_io.get(p,sols);
+    QuadDobl_System_and_Solutions_io.get(p,sols);
     new_line;
     put("Read "); put(Length_Of(sols),1); put_line(" solutions.");
     put_line("Initializing the systems container ...");
-    DoblDobl_PolySys_Container.Initialize(p.all);
+    QuadDobl_PolySys_Container.Initialize(p.all);
     put_line("Initializing the solutions container ...");
-    DoblDobl_Solutions_Container.Initialize(sols);
-    DoblDobl_GPU_Newton;
-  end DoblDobl_Newton;
+    QuadDobl_Solutions_Container.Initialize(sols);
+    QuadDobl_GPU_Newton;
+  end QuadDobl_Newton;
 
   procedure Main is
   begin
-    DoblDobl_Newton;
+    QuadDobl_Newton;
   end Main;
 
 begin
   Main;
-end ts_gpunewton_dd;
+end ts_gpunewton_qd;
