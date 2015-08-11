00de6fffaa38e9456d323dc016f0174fac6632c5
janv@uic.edu <jan@math.uic.edu>
2015-08-11 16:43:46 -0500

    added mode and verbose flags to the C wrappers and the Ada callers to accelerate Newton's method

--- src/Ada/PHCtoC/gpunewton_dd.cpp
+++ src/Ada/PHCtoC/gpunewton_dd.cpp
@@ -12,41 +12,54 @@
 
 using namespace std;
 
-int newton ( PolySys& p, PolySolSet& s );
+int newton ( int mode, int verbose, PolySys& p, PolySolSet& s );
 /*
  * DESCRIPTION :
- *   Applies Newton's method to the first solution in s. */
+ *   Applies Newton's method to the first solution in s,
+ *   on the polynomial system p, and with execution mode
+ *   equal to 0 (CPU+GPU), 1 (CPU), or 2 (GPU).
+ *   If verbose > 0, then additional output is written to screen. */
 
-extern "C" int gpunewton_dd ( void )
+extern "C" int gpunewton_dd ( int mode, int verbose )
 /*
  * DESCRIPTION :
  *   A C++ function to accelerate Newton's method,
- *   encapsulated as a C function for to be called from Ada. */
+ *   encapsulated as a C function for to be called from Ada.
+ *   If mode = 0, then both CPU and GPU will execute,
+ *   if mode = 1, then only CPU runs Newton's method,
+ *   if mode = 2, then only GPU runs Newton's method.
+ *   If verbose > 0, then additional output will be written. */
 {
-   int fail,dim,len;
+   int fail;
    PolySys ps;
    PolySolSet sols;
 
-   cout << endl;
-   cout << "Acceleration of Newton's method ..." << endl;
+   if(verbose > 0)
+   {
+      int dim, len;
 
-   fail = syscon_number_of_dobldobl_polynomials(&dim);
-   cout << "number of polynomials : " << dim << endl;
-   fail = solcon_number_of_dobldobl_solutions(&len);
-   cout << "number of solutions : " << len << endl;
+      cout << endl;
+      cout << "Acceleration of Newton's method ..." << endl;
+      fail = syscon_number_of_dobldobl_polynomials(&dim);
+      cout << "number of polynomials : " << dim << endl;
+      fail = solcon_number_of_dobldobl_solutions(&len);
+      cout << "number of solutions : " << len << endl;
+   }
 
-   ada_read_sys(ps);
+   ada_read_sys(verbose,ps);
    ada_read_sols(ps,sols);
 
-   fail = newton(ps,sols);
+   fail = newton(mode,verbose,ps,sols);
 
-   cout << "writing the solutions to the container ..." << endl;
+   if(verbose > 0)
+      cout << "writing the solutions to the container ..." << endl;
+
    ada_write_sols(sols);
 
    return 0;
 }
 
-int newton ( PolySys& p, PolySolSet& s )
+int newton ( int mode, int verbose, PolySys& p, PolySolSet& s )
 {
    double teval,tmgs;
    bool success;
@@ -60,32 +73,53 @@ int newton ( PolySys& p, PolySolSet& s )
       ERR_MAX_FIRST_DELTA_X, ERR_MIN_ROUND_OFF, MAX_IT_REFINE, \
       ERR_MIN_ROUND_OFF_REFINE, STEP_INCREASE, STEP_DECREASE);
 
-   cout << "The first solution on input :" << endl;
-   for(int k=0; k<p.dim; k++)
+   if(verbose > 0)
    {
-      cout << k << " :";
-      cout << setw(40) << scientific << setprecision(32) << sol[k];
+      cout << "The first solution on input :" << endl;
+      for(int k=0; k<p.dim; k++)
+      {
+         cout << k << " :";
+         cout << setw(40) << scientific << setprecision(32) << sol[k];
+      }
    }
 
    alpha = CT(1,0);
    cpu_inst_hom.init(p,p,p.dim,p.n_eq,1,alpha);
    cpu_inst_hom.init_workspace(workspace_cpu);
-   workspace_cpu.init_x_t_idx();
-   workspace_cpu.update_x_t_value(sol,alpha);
 
-   success = CPU_Newton(workspace_cpu,cpu_inst_hom,path_parameter,teval,tmgs);
-   cout.precision(32);
-   cout << "The first solution after CPU_Newton :" << endl;
-   for(int k=0; k<p.dim; k++)
-      cout << k << " :" << setw(40) << setprecision(32) << workspace_cpu.x[k];
+   if(mode == 0 || mode == 1)
+   {
+      workspace_cpu.init_x_t_idx();
+      workspace_cpu.update_x_t_value(sol,alpha);
 
-   alpha = CT(1,0);
-   success = GPU_Newton(cpu_inst_hom,path_parameter,sol,alpha,x_gpu);
-   cout << "The first solution after GPU_Newton :" << endl;
-   for(int k=0; k<p.dim; k++)
-      cout << k << " :" << setw(40) << setprecision(32) << x_gpu[k];
+      success = CPU_Newton
+                   (workspace_cpu,cpu_inst_hom,path_parameter,teval,tmgs);
+      if(verbose > 0)
+      {
+         cout.precision(32);
+         cout << "The first solution after CPU_Newton :" << endl;
+         for(int k=0; k<p.dim; k++)
+            cout << k << " :" << setw(40) << setprecision(32)
+                 << workspace_cpu.x[k];
+      }
+   }
 
-   s.change_sol(0,x_gpu);
+   if(mode == 0 || mode == 2)
+   {
+      alpha = CT(1,0);
+      success = GPU_Newton(cpu_inst_hom,path_parameter,sol,alpha,x_gpu);
+      if(verbose > 0)
+      {
+         cout << "The first solution after GPU_Newton :" << endl;
+         for(int k=0; k<p.dim; k++)
+            cout << k << " :" << setw(40) << setprecision(32) << x_gpu[k];
+      }
+   }
+
+   if(mode == 1)
+      s.change_sol(0,workspace_cpu.x);
+   else
+      s.change_sol(0,x_gpu);
 
    return 0;
 }
--- src/Ada/PHCtoC/ts_gpunewton.adb
+++ src/Ada/PHCtoC/ts_gpunewton.adb
@@ -1,4 +1,5 @@
 with text_io;                            use text_io;
+with Communications_with_User;           use Communications_with_User;
 with Standard_Natural_Numbers;           use Standard_Natural_Numbers;
 with Standard_Natural_Numbers_io;        use Standard_Natural_Numbers_io;
 with Standard_Integer_Numbers;           use Standard_Integer_Numbers;
@@ -16,19 +17,62 @@ procedure ts_gpunewton is
 --   as called from an Ada procedure.  The Ada procedure remains in control.
 --   Data is passed to the C++ code via the systems and solutions containers.
 
-  procedure Standard_GPU_Newton is
+  procedure Standard_GPU_Newton ( execmode,verbose : integer32 ) is
 
   -- DESCRIPTION :
   --   Calls the accelerated Newton's method in standard double precision.
+  --   The first input parameter is the mode of execution: 0, 1, or 2.
+  --   If verbose > 0, then additional output is written to screen.
 
     return_of_call : integer32;
 
-    function newton return integer32;
+    function newton ( m,v : integer32 ) return integer32;
     pragma import(C, newton, "gpunewton_d");
 
   begin
-    return_of_call := newton;
+    return_of_call := newton(execmode,verbose);
   end Standard_GPU_Newton;
+
+  function Prompt_for_Mode return integer32 is
+
+  -- DESCRIPTION :
+  --   Shows the user the menu and prompts for a mode of execution,
+  --   returned as 0, 1, or 2.
+
+    ans : character;
+
+  begin
+    new_line;
+    put_line("MENU for the mode of execution : ");
+    put_line("  0. Both the CPU and GPU will execute Newton's method.");
+    put_line("  1. Only the CPU will execute Newton's method.");
+    put_line("  2. Only the GPU will execute Newton's method.");
+    put("Type 0, 1, or 2 : ");
+    Ask_Alternative(ans,"012");
+    case ans is
+      when '0' => return 0;
+      when '1' => return 1;
+      when '2' => return 2;
+      when others => return -1;
+    end case;
+  end Prompt_for_Mode;
+
+  function Prompt_for_Verbose return integer32 is
+
+  -- DESCRIPTION :
+  --   Asks the user if extra output is wanted and returns 0 or 1.
+
+    ans : character;
+
+  begin
+    new_line;
+    put("Extra output before and after the computations ? (y/n) ");
+    Ask_Yes_or_No(ans);
+    if ans = 'y'
+     then return 1;
+     else return 0;
+    end if;
+  end Prompt_for_Verbose;
 
   procedure Standard_Newton is
 
@@ -41,6 +85,7 @@ procedure ts_gpunewton is
 
     p : Link_to_Poly_Sys;
     sols,newtsols : Solution_List;
+    execmode,verbose : integer32;
 
   begin
     new_line;
@@ -52,7 +97,9 @@ procedure ts_gpunewton is
     Standard_PolySys_Container.Initialize(p.all);
     put_line("Initializing the solutions container ...");
     Standard_Solutions_Container.Initialize(sols);
-    Standard_GPU_Newton;
+    execmode := Prompt_for_Mode;
+    verbose := Prompt_for_Verbose;
+    Standard_GPU_Newton(execmode,verbose);
     newtsols := Standard_Solutions_Container.Retrieve;
     put_line("The solutions after Newton's method :");
     put(standard_output,Length_Of(newtsols),
