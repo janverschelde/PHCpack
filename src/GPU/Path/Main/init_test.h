f37bba04a42f9f15cb1a67437f89dba0496e3d10
janv@uic.edu <jan@math.uic.edu>
2015-08-15 20:50:42 -0500

    formatting edits in the initialization and the calling of the path trackers

--- src/GPU/Path/Main/init_test.h
+++ src/GPU/Path/Main/init_test.h
@@ -1,9 +1,4 @@
-/*
- * init_test.h
- *
- *  Created on: Feb 8, 2015
- *      Author: yxc
- */
+/* init_test.h, created on Feb 8, 2015 by yxc with edits by jv */
 
 #ifndef INIT_TEST_H_
 #define INIT_TEST_H_
@@ -11,21 +6,24 @@
 #include "families.h"
 #include "eval_host.h"
 
-bool init_homotopy_test(PolySys& Target_Sys, PolySys& Start_Sys, PolySolSet& sol_set, int dim, int& n_eq, \
-		       CT*& sol0, CPUInstHom& cpu_inst_hom, Workspace& workspace_cpu, int test, int n_predictor, \
-		       string start_sys_filename, string target_sys_filename);
+bool init_homotopy_test
+ ( PolySys& Target_Sys, PolySys& Start_Sys, PolySolSet& sol_set,
+   int dim, int& n_eq, CT*& sol0,
+   CPUInstHom& cpu_inst_hom, Workspace& workspace_cpu, int test, 
+   int n_predictor, string start_sys_filename, string target_sys_filename );
 
-void init_cpu_inst_workspace(PolySys& Target_Sys, PolySys& Start_Sys, \
-		                     int dim, int n_eq, int n_predictor, \
-	                         CPUInstHom& cpu_inst_hom, Workspace& workspace_cpu, \
-	                         int test);
+void init_cpu_inst_workspace
+ ( PolySys& Target_Sys, PolySys& Start_Sys, int dim, int n_eq,
+   int n_predictor, CPUInstHom& cpu_inst_hom, Workspace& workspace_cpu,
+   int test );
 
-bool read_homotopy_file(PolySys& Target_Sys, PolySys& Start_Sys,\
-		int dim, int& n_eq, \
-		string Start_Sys_filename, string Target_Sys_file_name, \
-		PolySolSet* sol_set=NULL);
+bool read_homotopy_file
+ ( PolySys& Target_Sys, PolySys& Start_Sys, int dim, int& n_eq,
+   string Start_Sys_filename, string Target_Sys_file_name,
+   PolySolSet* sol_set=NULL );
 
-bool init_homotopy_test_ada(PolySys& Target_Sys, PolySys& Start_Sys, \
-		       CPUInstHom& cpu_inst_hom, Workspace& workspace_cpu, int n_predictor);
+bool init_homotopy_test_ada
+ ( PolySys& Target_Sys, PolySys& Start_Sys, 
+   CPUInstHom& cpu_inst_hom, Workspace& workspace_cpu, int n_predictor );
 
 #endif /* INIT_TEST_H_ */
--- src/GPU/Path/Main/path_test.h
+++ src/GPU/Path/Main/path_test.h
@@ -1,9 +1,4 @@
-/*
- * path_test.h
- *
- *  Created on: Feb 8, 2015
- *      Author: yxc
- */
+/* path_test.h, created on Feb 8, 2015 by yxc with edits by jv */
 
 #ifndef PATH_TEST_H_
 #define PATH_TEST_H_
@@ -14,10 +9,13 @@
 
 #include "path_host.h"
 
-bool path_test(Workspace& workspace_cpu, CPUInstHom& cpu_inst_hom, Parameter path_parameter, CT* sol0,  CT*& sol_new, CT t, PolySys& Target_Sys, int device_option = 0);
+bool path_test
+ ( Workspace& workspace_cpu, CPUInstHom& cpu_inst_hom,
+   Parameter path_parameter, CT* sol0, CT*& sol_new, CT t,
+   PolySys& Target_Sys, int device_option = 0 );
 
-T1 path_test_reverse(Workspace& workspace_cpu, CPUInstHom& cpu_inst_hom, Parameter path_parameter, CT* sol0, CT t, int device_option = 1);
-
-
+T1 path_test_reverse
+ ( Workspace& workspace_cpu, CPUInstHom& cpu_inst_hom,
+   Parameter path_parameter, CT* sol0, CT t, int device_option = 1 );
 
 #endif /* PATH_TEST_H_ */
