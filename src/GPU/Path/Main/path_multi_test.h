869009cfd614822c6c9c2672ae079728dba7f294
janv@uic.edu <jan@math.uic.edu>
2015-08-16 16:35:53 -0500

    reformatting code to call the trackers of many solution paths

--- src/GPU/Path/Main/path_multi_test.h
+++ src/GPU/Path/Main/path_multi_test.h
@@ -1,17 +1,13 @@
-/*
- * multi_path_test.h
- *
- *  Created on: Feb 12, 2015
- *      Author: yxc
- */
+/* multi_path_test.h, created on Feb 12, 2015 by yxc with edits by jv */
 
 #ifndef PATH_MULTI_TEST_H_
 #define PATH_MULTI_TEST_H_
 
 #include "path_test.h"
 
-bool path_multiple_test(Workspace& workspace_cpu, CPUInstHom& cpu_inst_hom, Parameter path_parameter,\
-		PolySolSet& sol_set, PolySys& Target_Sys, int device_option, int n_path=-1);
-
+bool path_multiple_test
+ ( Workspace& workspace_cpu, CPUInstHom& cpu_inst_hom,
+   Parameter path_parameter, PolySolSet& sol_set, PolySys& Target_Sys,
+   int device_option, int n_path=-1 );
 
 #endif /* PATH_MULTI_TEST_H_ */
