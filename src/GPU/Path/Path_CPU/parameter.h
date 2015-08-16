ef98755ace91dc5ba4be70e36e8ec8129e1f4b70
janv@uic.edu <jan@math.uic.edu>
2015-08-15 20:09:50 -0500

    more edits in the code for the host, for the Newton extrapolation in the predictor

--- src/GPU/Path/Path_CPU/parameter.h
+++ src/GPU/Path/Path_CPU/parameter.h
@@ -1,9 +1,4 @@
-/*
- * parameter.h
- *
- *  Created on: Dec 25, 2014
- *      Author: yxc
- */
+/* parameter.h, created on Dec 25, 2014 by yxc and edited by jv */
 
 #ifndef PARAMETER_H_
 #define PARAMETER_H_
@@ -12,49 +7,52 @@
 
 #include <iostream>
 
-class Parameter{
-public:
-	int n_predictor;
+class Parameter
+{
+   public:
 
-	int max_step;
+      int n_predictor;
 
-	int max_it;
-	double max_delta_t;
-	double max_delta_t_end;
-	double min_delta_t;
-	double err_max_res;
-	double err_max_delta_x;
-	double err_max_first_delta_x;
-	double err_min_round_off;
-	int max_it_refine;
-	double err_min_round_off_refine;
-	double step_increase;
-	double step_decrease;
+      int max_step;
 
-	Parameter(int n_predictor, int max_step, int max_it, \
-			double max_delta_t, double max_delta_t_end, double min_delta_t, \
-			double err_max_res, double err_max_delta_x, \
-			double err_max_first_delta_x, double err_min_round_off, \
-			double max_it_refine, double err_min_round_off_refine, \
-			double step_increase, double step_decrease){
-		this->n_predictor = n_predictor;
-		this->max_step = max_step;
-		this->max_it = max_it;
-		this->max_delta_t = max_delta_t;
-		this->max_delta_t_end = max_delta_t_end;
-		this->min_delta_t = min_delta_t;
-		this->err_max_res = err_max_res;
-		this->err_max_delta_x = err_max_delta_x;
-		this->err_max_first_delta_x = err_max_first_delta_x;
-		this->err_min_round_off = err_min_round_off;
-		this->max_it_refine = max_it_refine;
-		this->err_min_round_off_refine = err_min_round_off_refine;
-		this->step_increase = step_increase;
-		this->step_decrease = step_decrease;
+      int max_it;
+      double max_delta_t;
+      double max_delta_t_end;
+      double min_delta_t;
+      double err_max_res;
+      double err_max_delta_x;
+      double err_max_first_delta_x;
+      double err_min_round_off;
+      int max_it_refine;
+      double err_min_round_off_refine;
+      double step_increase;
+      double step_decrease;
 
-		std::cout << "path_parameter.err_min_round_off = " << this->err_min_round_off << std::endl;
-	}
+      Parameter
+      ( int n_predictor, int max_step, int max_it,
+        double max_delta_t, double max_delta_t_end, double min_delta_t,
+        double err_max_res, double err_max_delta_x,
+        double err_max_first_delta_x, double err_min_round_off,
+        double max_it_refine, double err_min_round_off_refine,
+        double step_increase, double step_decrease )
+      {
+         this->n_predictor = n_predictor;
+         this->max_step = max_step;
+         this->max_it = max_it;
+         this->max_delta_t = max_delta_t;
+         this->max_delta_t_end = max_delta_t_end;
+         this->min_delta_t = min_delta_t;
+         this->err_max_res = err_max_res;
+         this->err_max_delta_x = err_max_delta_x;
+         this->err_max_first_delta_x = err_max_first_delta_x;
+         this->err_min_round_off = err_min_round_off;
+         this->max_it_refine = max_it_refine;
+         this->err_min_round_off_refine = err_min_round_off_refine;
+         this->step_increase = step_increase;
+         this->step_decrease = step_decrease;
+         std::cout << "path_parameter.err_min_round_off = "
+                   << this->err_min_round_off << std::endl;
+      }
 };
-
 
 #endif /* PARAMETER_H_ */
--- src/GPU/Path/Path_CPU/predictor_host.h
+++ src/GPU/Path/Path_CPU/predictor_host.h
@@ -1,16 +1,15 @@
-/*
- * predictor.h
- *
- *  Created on: Dec 6, 2014
- *      Author: yxc
- */
+/* predictor.h, created on Dec 6, 2014 by yxc with edits by jv */
 
 #ifndef PREDICTOR_HOST_H_
 #define PREDICTOR_HOST_H_
 
 #include "DefineType_Host.h"
 
-void predictor_newton(CT** x_array, CT* t_array_orig, int x_t_idx, int np_predictor, int dim);
-
+void predictor_newton
+ ( CT** x_array, CT* t_array_orig, int x_t_idx, int np_predictor, int dim );
+/*
+ * DESCRIPTION :
+ *   Applies Newton extrapolation to predict the next point
+ *   on a solution path. */
 
 #endif /* PREDICTOR_HOST_H_ */
