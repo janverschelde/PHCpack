ef3705248710fc7f139a21a2f54970afd0df13a4
janv@uic.edu <jan@math.uic.edu>
2015-08-16 16:47:53 -0500

    reformatted code to call Newton's method on the CPU and GPU

--- src/GPU/Path/Main/newton_test.h
+++ src/GPU/Path/Main/newton_test.h
@@ -1,9 +1,4 @@
-/*
- * newton_test.h
- *
- *  Created on: Feb 8, 2015
- *      Author: yxc
- */
+/* newton_test.h, created on Feb 8, 2015 by yxc with edits by jv */
 
 #ifndef NEWTON_TEST_H_
 #define NEWTON_TEST_H_
@@ -14,7 +9,11 @@
 
 #include "newton_host.h"
 
-T1 newton_test(Workspace& workspace_cpu, CPUInstHom& cpu_inst_hom, Parameter path_parameter, CT* sol0, CT t);
-
+T1 newton_test
+ ( Workspace& workspace_cpu, CPUInstHom& cpu_inst_hom,
+   Parameter path_parameter, CT* sol0, CT t );
+/*
+ * DESCRIPTION :
+ *   Tests the calling of Newton's method. */
 
 #endif /* NEWTON_TEST_H_ */
