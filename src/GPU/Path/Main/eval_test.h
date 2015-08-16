3ebe1f3f0f93580600828839e6725b1338319b15
janv@uic.edu <jan@math.uic.edu>
2015-08-16 17:49:09 -0500

    reformatted code to test evaluation and differentiation on CPU and GPU

--- src/GPU/Path/Main/eval_test.h
+++ src/GPU/Path/Main/eval_test.h
@@ -1,9 +1,4 @@
-/*
- * eval_test.h
- *
- *  Created on: Feb 8, 2015
- *      Author: yxc
- */
+/* eval_test.h, created on Feb 8, 2015 by yxc with edits by jv */
 
 #ifndef EVAL_TEST_H_
 #define EVAL_TEST_H_
@@ -12,8 +7,12 @@
 #include "path_gpu.h"
 #include "err_check.h"
 
-T1 eval_test(const CPUInstHom& cpu_inst_hom, CT* host_sol0, CT t, const CT* cpu_workspace, const CT* cpu_matrix);
+T1 eval_test
+ ( const CPUInstHom& cpu_inst_hom, CT* host_sol0, CT t,
+   const CT* cpu_workspace, const CT* cpu_matrix );
 
-T1 eval_test_classic(Workspace& workspace_cpu, CPUInstHom& cpu_inst_hom, CT* sol0, CT t, PolySys& Target_Sys, int n_path=-1);
+T1 eval_test_classic
+ ( Workspace& workspace_cpu, CPUInstHom& cpu_inst_hom, CT* sol0, CT t,
+   PolySys& Target_Sys, int n_path=-1 );
 
 #endif /* EVAL_TEST_H_ */
