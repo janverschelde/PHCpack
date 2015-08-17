d8b804fd29c4013c180094ea62ee0e18c1388836
janv@uic.edu <jan@math.uic.edu>
2015-08-17 10:23:11 -0500

    reformatted code to test the modified Gram-Schmidt method on cpu and gpu

--- src/GPU/Path/Main/mgs_test.h
+++ src/GPU/Path/Main/mgs_test.h
@@ -1,9 +1,4 @@
-/*
- * mgs_test.h
- *
- *  Created on: Feb 8, 2015
- *      Author: yxc
- */
+/* mgs_test.h created on Feb 8, 2015 by yxc with edits by jv */
 
 #ifndef MGS_TEST_H_
 #define MGS_TEST_H_
@@ -14,11 +9,12 @@
 
 #include "mgs_host.h"
 
-T1 mgs_test(Workspace& workspace_cpu, CPUInstHom& cpu_inst_hom);
+T1 mgs_test ( Workspace& workspace_cpu, CPUInstHom& cpu_inst_hom );
 
-T1 mgs_test_large(Workspace& workspace_cpu, CPUInstHom& cpu_inst_hom);
+T1 mgs_test_large ( Workspace& workspace_cpu, CPUInstHom& cpu_inst_hom );
 
-T1 mgs_test_any(Workspace& workspace_cpu, CPUInstHom& cpu_inst_hom, int device_option = 0, int n_path=-1);
-
+T1 mgs_test_any
+ ( Workspace& workspace_cpu, CPUInstHom& cpu_inst_hom,
+   int device_option = 0, int n_path=-1 );
 
 #endif /* MGS_TEST_H_ */
