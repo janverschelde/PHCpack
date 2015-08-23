9f74ec5352958aba3f475bdaa8fdb155a7678f3c
janv@uic.edu <jan@math.uic.edu>
2015-08-22 20:37:47 -0500

    wrappers to call the c++ code to apply algorithm differerentiation in Newton's method and to track paths

---
+++ src/Ada/PHCtoC/standard_algodiffeval_trackers.ads
@@ -0,0 +1,50 @@
+with Standard_Integer_Numbers;           use Standard_Integer_Numbers;
+
+package Standard_AlgoDiffEval_Trackers is
+
+-- DESCRIPTION :
+--   Wraps the C++ code to apply algorithmic differentiation for evaluation
+--   in Newton's method and path tracking in standard double precision.
+--   The Ada procedures are in control.
+--   Data is passed to the C++ code via the systems and solutions containers.
+
+  procedure Standard_ADE_Newton ( verbose : integer32 );
+
+  -- DESCRIPTION :
+  --   Calls Newton's method in standard double precision.
+ 
+  -- REQUIRED :
+  --   The containers for polynomial systems and for solutions
+  --   in standard double precision contain a system and a solution.
+  --   The computed solution is place in the solutions container.
+
+  -- ON ENTRY :
+  --   verbose    if > 0, then additional output is written to screen.
+
+  procedure Standard_ADE_Track_One ( verbose : integer32 );
+
+  -- DESCRIPTION :
+  --   Calls the path tracker in standard double precision,
+  --   for tracking one solution path.
+
+  -- REQUIRED :
+  --   A target and start system are defined in PHCpack_Operations
+  --   and the solutions container contains one start solution.
+
+  -- ON ENTRY :
+  --   verbose    if > 0, then additional output is written to screen.
+
+  procedure Standard_ADE_Track_Many ( verbose : integer32 );
+
+  -- DESCRIPTION :
+  --   Calls the path tracker in standard double precision,
+  --   for tracking many solution paths.
+
+  -- REQUIRED :
+  --   A target and start system are defined in PHCpack_Operations
+  --   and the start solutions are stored in the solutions container.
+
+  -- ON ENTRY :
+  --   verbose    if > 0, then additional output is written to screen.
+
+end Standard_AlgoDiffEval_Trackers; 
