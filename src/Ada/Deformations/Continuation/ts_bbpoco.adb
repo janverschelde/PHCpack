893221b5db9852b8e19b659f3d0713908a2077fb
janv@uic.edu <jan@math.uic.edu>
2015-09-05 20:36:47 -0500

    running polynomial continuation with quad doubles in blackbox mode

--- src/Ada/Deformations/Continuation/ts_bbpoco.adb
+++ src/Ada/Deformations/Continuation/ts_bbpoco.adb
@@ -7,17 +7,25 @@ with Standard_Complex_Numbers;
 with Standard_Random_Numbers;            use Standard_Random_Numbers;
 with DoblDobl_Complex_Numbers;
 with DoblDobl_Random_Numbers;            use DoblDobl_Random_Numbers;
+with QuadDobl_Complex_Numbers;
+with QuadDobl_Random_Numbers;            use QuadDobl_Random_Numbers;
 with Standard_Complex_Poly_Systems;
 with Standard_Complex_Poly_Systems_io;   use Standard_Complex_Poly_Systems_io;
 with DoblDobl_Complex_Poly_Systems;
 with DoblDobl_Complex_Poly_Systems_io;   use DoblDobl_Complex_Poly_Systems_io;
+with QuadDobl_Complex_Poly_Systems;
+with QuadDobl_Complex_Poly_Systems_io;   use QuadDobl_Complex_Poly_Systems_io;
 with Standard_Complex_Solutions;
 with Standard_System_and_Solutions_io;   use Standard_System_and_Solutions_io;
 with DoblDobl_Complex_Solutions;
 with DoblDobl_Complex_Solutions_io;      use DoblDobl_Complex_Solutions_io;
 with DoblDobl_System_and_Solutions_io;   use DoblDobl_System_and_Solutions_io;
+with QuadDobl_Complex_Solutions;
+with QuadDobl_Complex_Solutions_io;      use QuadDobl_Complex_Solutions_io;
+with QuadDobl_System_and_Solutions_io;   use QuadDobl_System_and_Solutions_io;
 with Black_Box_Poly_Continuations;       use Black_Box_Poly_Continuations;
 with DoblDobl_Blackbox_Continuations;    use DoblDobl_Blackbox_Continuations;
+with QuadDobl_Blackbox_Continuations;    use QuadDobl_Blackbox_Continuations;
 
 procedure ts_bbpoco is
 
@@ -93,6 +101,42 @@ procedure ts_bbpoco is
     put(file,Length_Of(sols),natural32(Head_Of(sols).n),sols);
   end DoblDobl_Continuation;
 
+  procedure QuadDobl_Continuation is
+
+  -- DESCRIPTION :
+  --   Prompts the user for a target and start system
+  --   and then calls the black box polynomal continuation
+  --   in quad double precision.
+
+    use QuadDobl_Complex_Numbers;
+    use QuadDobl_Complex_Poly_Systems;
+    use QuadDobl_Complex_Solutions;
+
+    file : file_type;
+    lp,lq : Link_to_Poly_Sys;
+    sols : Solution_List;
+    gamma : constant Complex_Number := Random1;
+    nt : integer32 := 0;
+    elap : duration;
+
+  begin
+    new_line;
+    put_line("Reading the target system ...");
+    get(lp);
+    new_line;
+    put_line("Reading the start system and start solutions ...");
+    get(lq,sols);
+    new_line;
+    put_line("Reading the name of the output file.");
+    Read_Name_and_Create_File(file);
+    new_line;
+    put("Give the number of tasks : "); get(nt);
+    Black_Box_Polynomial_Continuation(file,nt,lp.all,lq.all,gamma,sols,elap);
+    new_line(file);
+    put_line(file,"THE SOLUTIONS :");
+    put(file,Length_Of(sols),natural32(Head_Of(sols).n),sols);
+  end QuadDobl_Continuation;
+
   procedure Main is
 
   -- DESCRIPTION :
@@ -106,14 +150,12 @@ procedure ts_bbpoco is
     put_line("  1. use standard double float arithmetic;");
     put_line("  2. use double double float arithmetic;");
     put_line("  3. use quad double float arithmetic;");
-    put_line("  4. use multiprecision arithmetic.");
-    put("Type 1, 2, 3, or 4 to make your choice : ");
-    Ask_Alternative(ans,"1234");
+    put("Type 1, 2, or 3 to make your choice : ");
+    Ask_Alternative(ans,"123");
     case ans is
       when '1' => Standard_Continuation;
       when '2' => DoblDobl_Continuation;
-     -- when '3' => QuadDobl_Continuation;
-     -- when '4' => Multprec_Continuation;
+      when '3' => QuadDobl_Continuation;
       when others => null;
     end case;
   end Main;
