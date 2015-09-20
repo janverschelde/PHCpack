3ae500b574cc9fe5850bf7919fb0de344e5cf259
janv@uic.edu <jan@math.uic.edu>
2015-09-20 15:46:21 -0500

    black box factorization runs in double double and quad double precision

--- src/Ada/Components/Factorization/black_box_factorization.adb
+++ src/Ada/Components/Factorization/black_box_factorization.adb
@@ -1,25 +1,85 @@
 with Standard_Natural_Numbers;          use Standard_Natural_Numbers;
 with Standard_Natural_Numbers_io;       use Standard_Natural_Numbers_io;
 with Standard_Complex_Polynomials_io;   use Standard_Complex_Polynomials_io;
-with Standard_Complex_Poly_Systems;     use Standard_Complex_Poly_Systems;
+with Standard_Complex_Poly_Systems;
+with DoblDobl_Complex_Polynomials_io;   use DoblDobl_Complex_Polynomials_io;
+with DoblDobl_Complex_Poly_Systems;
+with QuadDobl_Complex_Polynomials_io;   use QuadDobl_Complex_Polynomials_io;
+with QuadDobl_Complex_Poly_Systems;
 with Drivers_to_Factor_Polynomials;     use Drivers_to_Factor_Polynomials;
 
-procedure Black_Box_Factorization
-            ( infilename : in string; file : in file_type; p : in Poly ) is
+package body Black_Box_Factorization is
 
-  n : constant natural32 := Number_of_Unknowns(p);
-  fail : boolean;
-  factors : Link_to_Poly_Sys;
+  procedure Standard_Black_Box_Factorization
+              ( infilename : in string; file : in file_type;
+                p : in Standard_Complex_Polynomials.Poly ) is
 
-begin
-  put(file,"1 ");
-  put(file,n,1);
-  new_line(file);
-  put(file,p);
-  new_line(file);
-  new_line(file);
-  Driver_to_Factor(file,false,false,n,p,fail,factors);
-  if factors /= null 
-   then Write_Factors(infilename,factors.all);
-  end if;
+    use Standard_Complex_Poly_Systems;
+
+    n : constant natural32
+      := Standard_Complex_Polynomials.Number_of_Unknowns(p);
+    fail : boolean;
+    factors : Standard_Complex_Poly_Systems.Link_to_Poly_Sys;
+
+  begin
+    put(file,"1 ");
+    put(file,n,1);
+    new_line(file);
+    put(file,p);
+    new_line(file);
+    new_line(file);
+    Driver_to_Factor(file,false,false,n,p,fail,factors);
+    if factors /= null 
+     then Write_Factors(infilename,factors.all);
+    end if;
+  end Standard_Black_Box_Factorization;
+
+  procedure DoblDobl_Black_Box_Factorization
+              ( infilename : in string; file : in file_type;
+                p : in DoblDobl_Complex_Polynomials.Poly ) is
+
+    use DoblDobl_Complex_Poly_Systems;
+
+    n : constant natural32
+      := DoblDobl_Complex_Polynomials.Number_of_Unknowns(p);
+    fail : boolean;
+    factors : DoblDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
+
+  begin
+    put(file,"1 ");
+    put(file,n,1);
+    new_line(file);
+    put(file,p);
+    new_line(file);
+    new_line(file);
+    Driver_to_Factor(file,false,false,n,p,fail,factors);
+    if factors /= null 
+     then Write_Factors(infilename,factors.all);
+    end if;
+  end DoblDobl_Black_Box_Factorization;
+
+  procedure QuadDobl_Black_Box_Factorization
+              ( infilename : in string; file : in file_type;
+                p : in QuadDobl_Complex_Polynomials.Poly ) is
+
+    use QuadDobl_Complex_Poly_Systems;
+
+    n : constant natural32
+      := QuadDobl_Complex_Polynomials.Number_of_Unknowns(p);
+    fail : boolean;
+    factors : QuadDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
+
+  begin
+    put(file,"1 ");
+    put(file,n,1);
+    new_line(file);
+    put(file,p);
+    new_line(file);
+    new_line(file);
+    Driver_to_Factor(file,false,false,n,p,fail,factors);
+    if factors /= null 
+     then Write_Factors(infilename,factors.all);
+    end if;
+  end QuadDobl_Black_Box_Factorization;
+
 end Black_Box_Factorization;
--- src/Ada/Components/Factorization/black_box_factorization.ads
+++ src/Ada/Components/Factorization/black_box_factorization.ads
@@ -1,9 +1,26 @@
 with text_io;                          use text_io;
-with Standard_Complex_Polynomials;     use Standard_Complex_Polynomials;
+with Standard_Complex_Polynomials;
+with DoblDobl_Complex_Polynomials;
+with QuadDobl_Complex_Polynomials;
 
-procedure Black_Box_Factorization
-            ( infilename : in string; file : in file_type; p : in Poly );
+package Black_Box_Factorization is
 
 -- DESCRIPTION :
---   Black-box factorization of one polynomial in several variables,
---   invoked as phc -b on an input which contains p as system.
+--   This package offers the factorization routines in blackbox mode.
+
+  procedure Standard_Black_Box_Factorization
+              ( infilename : in string; file : in file_type;
+                p : in Standard_Complex_Polynomials.Poly );
+  procedure DoblDobl_Black_Box_Factorization
+              ( infilename : in string; file : in file_type;
+                p : in DoblDobl_Complex_Polynomials.Poly );
+  procedure QuadDobl_Black_Box_Factorization
+              ( infilename : in string; file : in file_type;
+                p : in QuadDobl_Complex_Polynomials.Poly );
+
+  -- DESCRIPTION :
+  --   Black-box factorization of one polynomial in several variables,
+  --   in standard double, double double, or quad double precision,
+  --   invoked as phc -b on an input which contains p as system.
+
+end Black_Box_Factorization;
