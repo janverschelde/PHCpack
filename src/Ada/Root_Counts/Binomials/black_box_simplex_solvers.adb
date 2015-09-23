086ac67e38e298388684c4b8ae3749a7b1c4dad6
janv@uic.edu <jan@math.uic.edu>
2015-09-23 09:00:36 -0500

    extended version of the solvers of binomial systems in quad double precision in black box mode

--- src/Ada/Root_Counts/Binomials/black_box_simplex_solvers.adb
+++ src/Ada/Root_Counts/Binomials/black_box_simplex_solvers.adb
@@ -1,10 +1,13 @@
 with Standard_Natural_Numbers;           use Standard_Natural_Numbers;
 with Standard_Floating_Numbers;          use Standard_Floating_Numbers;
 with Double_Double_Numbers;              use Double_Double_Numbers;
+with Quad_Double_Numbers;                use Quad_Double_Numbers;
 with Standard_Root_Refiners;             use Standard_Root_Refiners;
 with DoblDobl_Root_Refiners;             use DoblDobl_Root_Refiners;
+with QuadDobl_Root_Refiners;             use QuadDobl_Root_Refiners;
 with Standard_Simpomial_Solvers;
 with DoblDobl_Simpomial_Solvers;
+with QuadDobl_Simpomial_Solvers;
 
 package body Black_Box_Simplex_Solvers is
 
@@ -42,14 +45,37 @@ package body Black_Box_Simplex_Solvers is
 
     epsxa,epsfa : double_double;
     numit,max : natural32;
-    tol_zero : constant double_double := create(1.0E-12);
+    tol_zero : constant double_double := create(1.0E-24);
     zero_y : boolean;
 
   begin
     DoblDobl_Simpomial_Solvers.Solve(p,tol_zero,sols,fail,zero_y);
     if (not fail and then (Length_Of(sols) > 0)) then
-      epsxa := create(1.0E-12);
-      epsfa := create(1.0E-12);
+      epsxa := create(1.0E-24);
+      epsfa := create(1.0E-24);
+      max := 5;
+      numit := 0;
+      Silent_Root_Refiner(p,sols,epsxa,epsfa,numit,max);
+    end if;
+  end Black_Box_Simplex_Solver;
+
+  procedure Black_Box_Simplex_Solver
+              ( p : in QuadDobl_Complex_Poly_Systems.Poly_Sys;
+                sols : out QuadDobl_Complex_Solutions.Solution_List;
+                fail : out boolean ) is
+
+    use QuadDobl_Complex_Solutions;
+
+    epsxa,epsfa : quad_double;
+    numit,max : natural32;
+    tol_zero : constant quad_double := create(1.0E-48);
+    zero_y : boolean;
+
+  begin
+    QuadDobl_Simpomial_Solvers.Solve(p,tol_zero,sols,fail,zero_y);
+    if (not fail and then (Length_Of(sols) > 0)) then
+      epsxa := create(1.0E-48);
+      epsfa := create(1.0E-48);
       max := 5;
       numit := 0;
       Silent_Root_Refiner(p,sols,epsxa,epsfa,numit,max);
@@ -77,11 +103,25 @@ package body Black_Box_Simplex_Solvers is
 
     use DoblDobl_Complex_Solutions;
 
-    tol_zero : constant double_double := create(1.0E-12);
+    tol_zero : constant double_double := create(1.0E-24);
     zero_y : boolean;
 
   begin
     DoblDobl_Simpomial_Solvers.Solve(p,tol_zero,sols,fail,zero_y);
+  end Black_Box_Simplex_Solver;
+
+  procedure Black_Box_Simplex_Solver
+              ( p : in QuadDobl_Complex_Laur_Systems.Laur_Sys;
+                sols : out QuadDobl_Complex_Solutions.Solution_List;
+                fail : out boolean ) is
+
+    use QuadDobl_Complex_Solutions;
+
+    tol_zero : constant quad_double := create(1.0E-48);
+    zero_y : boolean;
+
+  begin
+    QuadDobl_Simpomial_Solvers.Solve(p,tol_zero,sols,fail,zero_y);
   end Black_Box_Simplex_Solver;
 
   procedure Black_Box_Simplex_Solver
@@ -131,7 +171,7 @@ package body Black_Box_Simplex_Solvers is
 
     epsxa,epsfa : double_double;
     numit,max : natural32;
-    tol_zero : constant double_double := create(1.0E-12);
+    tol_zero : constant double_double := create(1.0E-24);
     zero_y : boolean;
     deflate : boolean := false;
 
@@ -147,8 +187,43 @@ package body Black_Box_Simplex_Solvers is
         put_line(file,
     "remove the perturbations then again via homotopy continuation.");
       elsif Length_Of(sols) > 0 then
-        epsxa := create(1.0E-12);
-        epsfa := create(1.0E-12);
+        epsxa := create(1.0E-24);
+        epsfa := create(1.0E-24);
+        max := 5;
+        numit := 0;
+        Reporting_Root_Refiner(file,p,sols,epsxa,epsfa,numit,max,false);
+      end if;
+    end if;
+  end Black_Box_Simplex_Solver;
+
+  procedure Black_Box_Simplex_Solver
+              ( file : in file_type;
+                p : in QuadDobl_Complex_Poly_Systems.Poly_Sys;
+                sols : out QuadDobl_Complex_Solutions.Solution_List;
+                fail : out boolean ) is
+
+    use QuadDobl_Complex_Solutions;
+
+    epsxa,epsfa : quad_double;
+    numit,max : natural32;
+    tol_zero : constant quad_double := create(1.0E-48);
+    zero_y : boolean;
+    deflate : boolean := false;
+
+  begin
+    QuadDobl_Simpomial_Solvers.Solve(p,tol_zero,sols,fail,zero_y);
+    if not fail then
+      if zero_y then
+        new_line(file);
+        put_line(file,
+    "The system has no solutions with all components different from zero.");
+        put_line(file,
+    "Try perturbing the constant terms, solve the perturbed system, and");
+        put_line(file,
+    "remove the perturbations then again via homotopy continuation.");
+      elsif Length_Of(sols) > 0 then
+        epsxa := create(1.0E-48);
+        epsfa := create(1.0E-48);
         max := 5;
         numit := 0;
         Reporting_Root_Refiner(file,p,sols,epsxa,epsfa,numit,max,false);
@@ -182,6 +257,48 @@ package body Black_Box_Simplex_Solvers is
           epsxa,epsfa,tolsing,numit,max,false);
       end if;
     end if;
+    if fail
+     then put_line(file,"Blackbox simpomial solver reports failure.");
+    end if;
+    if zero_y
+     then put_line(file,"Blackbox simpomial solver reports zero component.");
+    end if;
+  end Black_Box_Simplex_Solver;
+
+  procedure Black_Box_Simplex_Solver
+              ( file : in file_type;
+                p : in DoblDobl_Complex_Laur_Systems.Laur_Sys;
+                sols : out DoblDobl_Complex_Solutions.Solution_List;
+                fail : out boolean ) is
+
+    use DoblDobl_Complex_Solutions;
+
+    tol_zero : constant double_double := create(1.0E-24);
+    zero_y : boolean;
+
+  begin
+    DoblDobl_Simpomial_Solvers.Solve(p,tol_zero,sols,fail,zero_y);
+    if fail
+     then put_line(file,"Blackbox simpomial solver reports failure.");
+    end if;
+    if zero_y
+     then put_line(file,"Blackbox simpomial solver reports zero component.");
+    end if;
+  end Black_Box_Simplex_Solver;
+
+  procedure Black_Box_Simplex_Solver
+              ( file : in file_type;
+                p : in QuadDobl_Complex_Laur_Systems.Laur_Sys;
+                sols : out QuadDobl_Complex_Solutions.Solution_List;
+                fail : out boolean ) is
+
+    use QuadDobl_Complex_Solutions;
+
+    tol_zero : constant quad_double := create(1.0E-48);
+    zero_y : boolean;
+
+  begin
+    QuadDobl_Simpomial_Solvers.Solve(p,tol_zero,sols,fail,zero_y);
     if fail
      then put_line(file,"Blackbox simpomial solver reports failure.");
     end if;
--- src/Ada/Root_Counts/Binomials/black_box_simplex_solvers.ads
+++ src/Ada/Root_Counts/Binomials/black_box_simplex_solvers.ads
@@ -3,8 +3,11 @@ with Standard_Complex_Poly_Systems;
 with Standard_Complex_Laur_Systems;
 with DoblDobl_Complex_Poly_Systems;
 with DoblDobl_Complex_Laur_Systems;
+with QuadDobl_Complex_Poly_Systems;
+with QuadDobl_Complex_Laur_Systems;
 with Standard_Complex_Solutions;
 with DoblDobl_Complex_Solutions;
+with QuadDobl_Complex_Solutions;
 
 package Black_Box_Simplex_Solvers is
 
@@ -20,6 +23,10 @@ package Black_Box_Simplex_Solvers is
               ( p : in DoblDobl_Complex_Poly_Systems.Poly_Sys;
                 sols : out DoblDobl_Complex_Solutions.Solution_List;
                 fail : out boolean );
+  procedure Black_Box_Simplex_Solver
+              ( p : in QuadDobl_Complex_Poly_Systems.Poly_Sys;
+                sols : out QuadDobl_Complex_Solutions.Solution_List;
+                fail : out boolean );
 
   procedure Black_Box_Simplex_Solver
               ( p : in Standard_Complex_Laur_Systems.Laur_Sys;
@@ -29,6 +36,10 @@ package Black_Box_Simplex_Solvers is
               ( p : in DoblDobl_Complex_Laur_Systems.Laur_Sys;
                 sols : out DoblDobl_Complex_Solutions.Solution_List;
                 fail : out boolean );
+  procedure Black_Box_Simplex_Solver
+              ( p : in QuadDobl_Complex_Laur_Systems.Laur_Sys;
+                sols : out QuadDobl_Complex_Solutions.Solution_List;
+                fail : out boolean );
 
   procedure Black_Box_Simplex_Solver
               ( file : in file_type;
@@ -37,8 +48,29 @@ package Black_Box_Simplex_Solvers is
                 fail : out boolean );
   procedure Black_Box_Simplex_Solver
               ( file : in file_type;
+                p : in DoblDobl_Complex_Poly_Systems.Poly_Sys;
+                sols : out DoblDobl_Complex_Solutions.Solution_List;
+                fail : out boolean );
+  procedure Black_Box_Simplex_Solver
+              ( file : in file_type;
+                p : in QuadDobl_Complex_Poly_Systems.Poly_Sys;
+                sols : out QuadDobl_Complex_Solutions.Solution_List;
+                fail : out boolean );
+
+  procedure Black_Box_Simplex_Solver
+              ( file : in file_type;
                 p : in Standard_Complex_Laur_Systems.Laur_Sys;
                 sols : out Standard_Complex_Solutions.Solution_List;
+                fail : out boolean );
+  procedure Black_Box_Simplex_Solver
+              ( file : in file_type;
+                p : in DoblDobl_Complex_Laur_Systems.Laur_Sys;
+                sols : out DoblDobl_Complex_Solutions.Solution_List;
+                fail : out boolean );
+  procedure Black_Box_Simplex_Solver
+              ( file : in file_type;
+                p : in QuadDobl_Complex_Laur_Systems.Laur_Sys;
+                sols : out QuadDobl_Complex_Solutions.Solution_List;
                 fail : out boolean );
 
   -- DESCRIPTION :
