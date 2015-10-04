8315734f92618de424c50be6a077d7c0997b0674
janv@uic.edu <jan@math.uic.edu>
2015-10-04 18:04:09 -0500

    change to write seed and version number in output file of phc -r

--- src/Ada/Main/mainroco.adb
+++ src/Ada/Main/mainroco.adb
@@ -7,6 +7,8 @@ with Standard_Complex_Poly_Systems;      use Standard_Complex_Poly_Systems;
 with Standard_Complex_Poly_Systems_io;   use Standard_Complex_Poly_Systems_io;
 with Standard_Complex_Solutions;         use Standard_Complex_Solutions;
 with Drivers_for_Root_Counts;            use Drivers_for_Root_Counts;
+with Write_Seed_Number;
+with Greeting_Banners;
 with Bye_Bye_Message;
 
 procedure mainroco ( infilename,outfilename : in string ) is
@@ -34,21 +36,29 @@ procedure mainroco ( infilename,outfilename : in string ) is
       lp := null; return;
   end Read_System;
 
-begin
-  Read_System(infilename);
-  if lp = null
-   then new_line; get(lp);
-  end if;
-  Create_Output_File(outft,outfilename);
-  put(outft,lp.all);
-  declare
-    q : Poly_Sys(lp'range);
-    qsols : Solution_List;
-    rc : natural32;
+  procedure Main is
+
   begin
-    Driver_for_Root_Counts(outft,lp.all,q,false,qsols,rc);
-  end; 
-  new_line(outft);
-  put(outft,Bye_Bye_Message);
-  Close(outft);
+    Read_System(infilename);
+    if lp = null
+     then new_line; get(lp);
+    end if;
+    Create_Output_File(outft,outfilename);
+    put(outft,lp.all);
+    declare
+      q : Poly_Sys(lp'range);
+      qsols : Solution_List;
+      rc : natural32;
+    begin
+      Driver_for_Root_Counts(outft,lp.all,q,false,qsols,rc);
+    end; 
+    new_line(outft);
+    put_line(outft,Bye_Bye_Message);
+    Write_Seed_Number(outft);
+    put_line(outft,Greeting_Banners.Version);
+    Close(outft);
+  end Main;
+
+begin
+  Main;
 end mainroco;
