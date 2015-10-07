dbae4aa8d4aa52e96d7a99d077d9a4067a595db0
janv@uic.edu <jan@math.uic.edu>
2015-10-07 06:28:53 -0500

    updated version to 2.4.02 in its greetings

--- src/Ada/Main/greeting_banners.adb
+++ src/Ada/Main/greeting_banners.adb
@@ -2,7 +2,7 @@ package body Greeting_Banners is
 
   function Version return string is
 
-    res : constant string := "PHCv2.4.01 released 2015-09-24";
+    res : constant string := "PHCv2.4.01 released 2015-10-07";
 
   begin
     return res;
