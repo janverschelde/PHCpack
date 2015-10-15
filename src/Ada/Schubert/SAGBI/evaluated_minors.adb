f279ca9dbe9f39224e0df748e67fe4d28691663c
janv@uic.edu <jan@math.uic.edu>
2015-10-15 11:09:46 -0500

    extensions for double double and quad double precision

--- src/Ada/Schubert/SAGBI/evaluated_minors.adb
+++ src/Ada/Schubert/SAGBI/evaluated_minors.adb
@@ -2,6 +2,10 @@ with Standard_Integer_Numbers;           use Standard_Integer_Numbers;
 with Standard_Integer_Vectors;           use Standard_Integer_Vectors;
 with Standard_Floating_Linear_Solvers;   use Standard_Floating_Linear_Solvers;
 with Standard_Complex_Linear_Solvers;    use Standard_Complex_Linear_Solvers;
+with Double_Double_Linear_Solvers;       use Double_Double_Linear_Solvers;
+with DoblDobl_Complex_Linear_Solvers;    use DoblDobl_Complex_Linear_Solvers;
+with Quad_Double_Linear_Solvers;         use Quad_Double_Linear_Solvers;
+with QuadDobl_Complex_Linear_Solvers;    use QuadDobl_Complex_Linear_Solvers;
 
 package body Evaluated_Minors is
 
@@ -25,6 +29,70 @@ package body Evaluated_Minors is
       res := 0.0;
     else
       res := 1.0;
+      for i in m'range(1) loop
+        res := res*wrk(i,i);
+      end loop;
+      for i in piv'range loop
+        if piv(i) > i
+         then res := -res;
+        end if;
+      end loop;
+    end if;
+    return res;
+  end Determinant;
+
+  function Determinant ( m : Double_Double_Matrices.Matrix )
+                       return double_double is
+
+    res : double_double;
+    wrk : Double_Double_Matrices.Matrix(m'range(1),m'range(2));
+    piv : Standard_Integer_Vectors.Vector(m'range(1));
+    inf : integer32;
+
+  begin
+    for i in m'range(1) loop
+      piv(i) := i;
+      for j in m'range(2) loop
+        wrk(i,j) := m(i,j);
+      end loop;
+    end loop;
+    lufac(wrk,m'last(1),piv,inf);
+    if inf /= 0 then
+      res := create(0.0);
+    else
+      res := create(1.0);
+      for i in m'range(1) loop
+        res := res*wrk(i,i);
+      end loop;
+      for i in piv'range loop
+        if piv(i) > i
+         then res := -res;
+        end if;
+      end loop;
+    end if;
+    return res;
+  end Determinant;
+
+  function Determinant ( m : Quad_Double_Matrices.Matrix )
+                       return quad_double is
+
+    res : quad_double;
+    wrk : Quad_Double_Matrices.Matrix(m'range(1),m'range(2));
+    piv : Standard_Integer_Vectors.Vector(m'range(1));
+    inf : integer32;
+
+  begin
+    for i in m'range(1) loop
+      piv(i) := i;
+      for j in m'range(2) loop
+        wrk(i,j) := m(i,j);
+      end loop;
+    end loop;
+    lufac(wrk,m'last(1),piv,inf);
+    if inf /= 0 then
+      res := create(0.0);
+    else
+      res := create(1.0);
       for i in m'range(1) loop
         res := res*wrk(i,i);
       end loop;
@@ -69,8 +137,74 @@ package body Evaluated_Minors is
     return res;
   end Determinant;
 
+  function Determinant ( m : Double_Double_Matrices.Matrix; b : Bracket )
+                       return double_double is
+
+    res : double_double;
+    sqm : Double_Double_Matrices.Matrix(b'range,b'range);
+    piv : Standard_Integer_Vectors.Vector(b'range);
+    inf : integer32;
+
+  begin
+    for i in b'range loop
+      piv(i) := i;
+      for j in b'range loop
+        sqm(i,j) := m(integer32(b(i)),j);
+      end loop;
+    end loop;
+    lufac(sqm,b'last,piv,inf);
+    if inf /= 0 then
+      res := create(0.0);
+    else
+      res := create(1.0);
+      for i in b'range loop
+        res := res*sqm(i,i);
+      end loop;
+      for i in piv'range loop
+        if piv(i) > i
+         then res := -res;
+        end if;
+      end loop;
+    end if;
+    return res;
+  end Determinant;
+
+  function Determinant ( m : Quad_Double_Matrices.Matrix; b : Bracket )
+                       return quad_double is
+
+    res : quad_double;
+    sqm : Quad_Double_Matrices.Matrix(b'range,b'range);
+    piv : Standard_Integer_Vectors.Vector(b'range);
+    inf : integer32;
+
+  begin
+    for i in b'range loop
+      piv(i) := i;
+      for j in b'range loop
+        sqm(i,j) := m(integer32(b(i)),j);
+      end loop;
+    end loop;
+    lufac(sqm,b'last,piv,inf);
+    if inf /= 0 then
+      res := create(0.0);
+    else
+      res := create(1.0);
+      for i in b'range loop
+        res := res*sqm(i,i);
+      end loop;
+      for i in piv'range loop
+        if piv(i) > i
+         then res := -res;
+        end if;
+      end loop;
+    end if;
+    return res;
+  end Determinant;
+
   function Determinant ( m : Standard_Complex_Matrices.Matrix )
-                       return Complex_Number is
+                       return Standard_Complex_Numbers.Complex_Number is
+
+    use Standard_Complex_Numbers;
 
     res : Complex_Number;
     wrk : Standard_Complex_Matrices.Matrix(m'range(1),m'range(2));
@@ -101,8 +235,82 @@ package body Evaluated_Minors is
     return res;
   end Determinant;
 
+  function Determinant ( m : DoblDobl_Complex_Matrices.Matrix )
+                       return DoblDobl_Complex_Numbers.Complex_Number is
+
+    use DoblDobl_Complex_Numbers;
+
+    res : Complex_Number;
+    wrk : DoblDobl_Complex_Matrices.Matrix(m'range(1),m'range(2));
+    piv : Standard_Integer_Vectors.Vector(m'range(1));
+    inf : integer32;
+    zero : constant double_double := create(0.0);
+    one : constant double_double := create(1.0);
+
+  begin
+    for i in m'range(1) loop
+      piv(i) := i;
+      for j in m'range(2) loop
+        wrk(i,j) := m(i,j);
+      end loop;
+    end loop;
+    lufac(wrk,wrk'last(1),piv,inf);
+    if inf /= 0 then
+      res := Create(zero);
+    else
+      res := Create(one);
+      for i in wrk'range(1) loop
+        res := res*wrk(i,i);
+      end loop;
+      for i in piv'range loop
+        if piv(i) > i
+         then res := -res;
+        end if;
+      end loop;
+    end if;
+    return res;
+  end Determinant;
+
+  function Determinant ( m : QuadDobl_Complex_Matrices.Matrix )
+                       return QuadDobl_Complex_Numbers.Complex_Number is
+
+    use QuadDobl_Complex_Numbers;
+
+    res : Complex_Number;
+    wrk : QuadDobl_Complex_Matrices.Matrix(m'range(1),m'range(2));
+    piv : Standard_Integer_Vectors.Vector(m'range(1));
+    inf : integer32;
+    zero : constant quad_double := create(0.0);
+    one : constant quad_double := create(1.0);
+
+  begin
+    for i in m'range(1) loop
+      piv(i) := i;
+      for j in m'range(2) loop
+        wrk(i,j) := m(i,j);
+      end loop;
+    end loop;
+    lufac(wrk,wrk'last(1),piv,inf);
+    if inf /= 0 then
+      res := Create(zero);
+    else
+      res := Create(one);
+      for i in wrk'range(1) loop
+        res := res*wrk(i,i);
+      end loop;
+      for i in piv'range loop
+        if piv(i) > i
+         then res := -res;
+        end if;
+      end loop;
+    end if;
+    return res;
+  end Determinant;
+
   function Determinant ( m : Standard_Complex_Matrices.Matrix; b : Bracket )
-                       return Complex_Number is
+                       return Standard_Complex_Numbers.Complex_Number is
+
+    use Standard_Complex_Numbers;
 
     res : Complex_Number;
     sqm : Standard_Complex_Matrices.Matrix(b'range,b'range);
@@ -133,9 +341,83 @@ package body Evaluated_Minors is
     return res;
   end Determinant;
 
+  function Determinant ( m : DoblDobl_Complex_Matrices.Matrix; b : Bracket )
+                       return DoblDobl_Complex_Numbers.Complex_Number is
+
+    use DoblDobl_Complex_Numbers;
+
+    res : Complex_Number;
+    sqm : DoblDobl_Complex_Matrices.Matrix(b'range,b'range);
+    piv : Standard_Integer_Vectors.Vector(b'range);
+    inf : integer32;
+    zero : constant double_double := create(0.0);
+    one : constant double_double := create(1.0);
+
+  begin
+    for i in b'range loop
+      piv(i) := i;
+      for j in b'range loop
+        sqm(i,j) := m(integer32(b(i)),j);
+      end loop;
+    end loop;
+    lufac(sqm,b'last,piv,inf);
+    if inf /= 0 then
+      res := Create(zero);
+    else
+      res := Create(one);
+      for i in sqm'range(1) loop
+        res := res*sqm(i,i);
+      end loop;
+      for i in piv'range loop
+        if piv(i) > i
+         then res := -res;
+        end if;
+      end loop;
+    end if;
+    return res;
+  end Determinant;
+
+  function Determinant ( m : QuadDobl_Complex_Matrices.Matrix; b : Bracket )
+                       return QuadDobl_Complex_Numbers.Complex_Number is
+
+    use QuadDobl_Complex_Numbers;
+
+    res : Complex_Number;
+    sqm : QuadDobl_Complex_Matrices.Matrix(b'range,b'range);
+    piv : Standard_Integer_Vectors.Vector(b'range);
+    inf : integer32;
+    zero : constant quad_double := create(0.0);
+    one : constant quad_double := create(1.0);
+
+  begin
+    for i in b'range loop
+      piv(i) := i;
+      for j in b'range loop
+        sqm(i,j) := m(integer32(b(i)),j);
+      end loop;
+    end loop;
+    lufac(sqm,b'last,piv,inf);
+    if inf /= 0 then
+      res := Create(zero);
+    else
+      res := Create(one);
+      for i in sqm'range(1) loop
+        res := res*sqm(i,i);
+      end loop;
+      for i in piv'range loop
+        if piv(i) > i
+         then res := -res;
+        end if;
+      end loop;
+    end if;
+    return res;
+  end Determinant;
+
   function Determinant ( m : Standard_Complex_Matrices.Matrix;
                          rows,cols : Bracket )
-                       return Complex_Number is
+                       return Standard_Complex_Numbers.Complex_Number is
+
+    use Standard_Complex_Numbers;
 
     res : Complex_Number;
     sqm : Standard_Complex_Matrices.Matrix(rows'range,cols'range);
@@ -154,6 +436,80 @@ package body Evaluated_Minors is
       res := Create(0.0);
     else
       res := Create(1.0);
+      for i in sqm'range(1) loop
+        res := res*sqm(i,i);
+      end loop;
+      for i in piv'range loop
+        if piv(i) > i
+         then res := -res;
+        end if;
+      end loop;
+    end if;
+    return res;
+  end Determinant;
+
+  function Determinant ( m : DoblDobl_Complex_Matrices.Matrix;
+                         rows,cols : Bracket )
+                       return DoblDobl_Complex_Numbers.Complex_Number is
+
+    use DoblDobl_Complex_Numbers;
+
+    res : Complex_Number;
+    sqm : DoblDobl_Complex_Matrices.Matrix(rows'range,cols'range);
+    piv : Standard_Integer_Vectors.Vector(rows'range);
+    inf : integer32;
+    zero : constant double_double := create(0.0);
+    one : constant double_double := create(1.0);
+
+  begin
+    for i in rows'range loop
+      piv(i) := i;
+      for j in cols'range loop
+        sqm(i,j) := m(integer32(rows(i)),integer32(cols(j)));
+      end loop;
+    end loop;
+    lufac(sqm,rows'last,piv,inf);
+    if inf /= 0 then
+      res := Create(zero);
+    else
+      res := Create(one);
+      for i in sqm'range(1) loop
+        res := res*sqm(i,i);
+      end loop;
+      for i in piv'range loop
+        if piv(i) > i
+         then res := -res;
+        end if;
+      end loop;
+    end if;
+    return res;
+  end Determinant;
+
+  function Determinant ( m : QuadDobl_Complex_Matrices.Matrix;
+                         rows,cols : Bracket )
+                       return QuadDobl_Complex_Numbers.Complex_Number is
+
+    use QuadDobl_Complex_Numbers;
+
+    res : Complex_Number;
+    sqm : QuadDobl_Complex_Matrices.Matrix(rows'range,cols'range);
+    piv : Standard_Integer_Vectors.Vector(rows'range);
+    inf : integer32;
+    zero : constant quad_double := create(0.0);
+    one : constant quad_double := create(1.0);
+
+  begin
+    for i in rows'range loop
+      piv(i) := i;
+      for j in cols'range loop
+        sqm(i,j) := m(integer32(rows(i)),integer32(cols(j)));
+      end loop;
+    end loop;
+    lufac(sqm,rows'last,piv,inf);
+    if inf /= 0 then
+      res := Create(zero);
+    else
+      res := Create(one);
       for i in sqm'range(1) loop
         res := res*sqm(i,i);
       end loop;
--- src/Ada/Schubert/SAGBI/evaluated_minors.ads
+++ src/Ada/Schubert/SAGBI/evaluated_minors.ads
@@ -1,7 +1,15 @@
 with Standard_Floating_Numbers;          use Standard_Floating_Numbers;
-with Standard_Complex_Numbers;           use Standard_Complex_Numbers;
+with Double_Double_Numbers;              use Double_Double_Numbers;
+with Quad_Double_Numbers;                use Quad_Double_Numbers;
+with Standard_Complex_Numbers;
+with DoblDobl_Complex_Numbers;
+with QuadDobl_Complex_Numbers;
 with Standard_Floating_Matrices;
 with Standard_Complex_Matrices;
+with Double_Double_Matrices;
+with DoblDobl_Complex_Matrices;
+with Quad_Double_Matrices;
+with QuadDobl_Complex_Matrices;
 with Brackets;                           use Brackets;
 
 package Evaluated_Minors is
@@ -11,31 +19,76 @@ package Evaluated_Minors is
 
   function Determinant ( m : Standard_Floating_Matrices.Matrix )
                        return double_float;
+  function Determinant ( m : Double_Double_Matrices.Matrix )
+                       return double_double;
+  function Determinant ( m : Quad_Double_Matrices.Matrix )
+                       return quad_double;
+
+  -- DESCRIPTION :
+  --   Returns the determinant of the real matrix m, 
+  --   computed in double, double double, or quad double precision.
+
+  -- REQUIRED : m'range(1) = m'range(2), the matrix is square.
 
   function Determinant ( m : Standard_Floating_Matrices.Matrix; b : Bracket )
                        return double_float;
+  function Determinant ( m : Double_Double_Matrices.Matrix; b : Bracket )
+                       return double_double;
+  function Determinant ( m : Quad_Double_Matrices.Matrix; b : Bracket )
+                       return quad_double;
+
+  -- DESCRIPTION :
+  --   Computes the determinant of the submatrix of m, obtained by selecting
+  --   those rows from the matrix m that are entries in the bracket b,
+  --   in double, double double, or quad double precision.
+
+  -- REQUIRED :
+  --   The matrix m should at least have as many columns as b'last and
+  --   as many rows as b(b'last).
 
   function Determinant ( m : Standard_Complex_Matrices.Matrix )
-                       return Complex_Number;
+                       return Standard_Complex_Numbers.Complex_Number;
+  function Determinant ( m : DoblDobl_Complex_Matrices.Matrix )
+                       return DoblDobl_Complex_Numbers.Complex_Number;
+  function Determinant ( m : QuadDobl_Complex_Matrices.Matrix )
+                       return QuadDobl_Complex_Numbers.Complex_Number;
+
+  -- DESCRIPTION :
+  --   Returns the determinant of the complex matrix m, 
+  --   computed in double, double double, or quad double precision.
+
+  -- REQUIRED : m'range(1) = m'range(2), the matrix is square.
 
   function Determinant ( m : Standard_Complex_Matrices.Matrix; b : Bracket )
-                       return Complex_Number;
+                       return Standard_Complex_Numbers.Complex_Number;
+  function Determinant ( m : DoblDobl_Complex_Matrices.Matrix; b : Bracket )
+                       return DoblDobl_Complex_Numbers.Complex_Number;
+  function Determinant ( m : QuadDobl_Complex_Matrices.Matrix; b : Bracket )
+                       return QuadDobl_Complex_Numbers.Complex_Number;
 
   -- DESCRIPTION :
   --   Computes the determinant of the sub-matrix of m, obtained by selecting
-  --   those rows from the matrix m that are entries in the bracket b.
-  --   If b is omitted, then the determinant of m is returned.
+  --   those rows from the matrix m that are entries in the bracket b,
+  --   in double, double double, or quad double precision.
 
   -- REQUIRED :
-  --   The matrix m should at least have as many columns as b'range and
-  --   as many rows as b(b'last).  If b is omitted, then m must be square.
+  --   The matrix m should at least have as many columns as b'last and
+  --   as many rows as b(b'last).
 
   function Determinant ( m : Standard_Complex_Matrices.Matrix;
-                         rows,cols : Bracket ) return Complex_Number;
+                         rows,cols : Bracket )
+                       return Standard_Complex_Numbers.Complex_Number;
+  function Determinant ( m : DoblDobl_Complex_Matrices.Matrix;
+                         rows,cols : Bracket )
+                       return DoblDobl_Complex_Numbers.Complex_Number;
+  function Determinant ( m : QuadDobl_Complex_Matrices.Matrix;
+                         rows,cols : Bracket )
+                       return QuadDobl_Complex_Numbers.Complex_Number;
 
   -- DESCRIPTION :
   --   Returns the determinant of the matrix obtained from selecting
-  --   those rows and columns as defined by the brackets rows and cols.
+  --   those rows and columns as defined by the brackets rows and cols,
+  --   in double, double double, and quad double precision.
 
   -- REQUIRED : m'range(1) contains all indices of rows
   --   and m'range(2) contains all indices of cols.
