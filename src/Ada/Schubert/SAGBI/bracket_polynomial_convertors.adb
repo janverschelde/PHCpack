6c4b77d4f0b545f18addd8677eae7d3eb7aa6d9a
janv@uic.edu <jan@math.uic.edu>
2015-10-18 12:04:18 -0500

    converting bracket polynomials to have coefficients of double double and quad double precision

---
+++ src/Ada/Schubert/SAGBI/bracket_polynomial_convertors.adb
@@ -0,0 +1,59 @@
+with Bracket_Monomials;                use Bracket_Monomials;
+with DoblDobl_Complex_Numbers_cv;      use DoblDobl_Complex_Numbers_cv; 
+with QuadDobl_Complex_Numbers_cv;      use QuadDobl_Complex_Numbers_cv; 
+
+package body Bracket_Polynomial_Convertors is
+
+  function Convert
+             ( p : Bracket_Polynomials.Bracket_Polynomial )
+             return  DoblDobl_Bracket_Polynomials.Bracket_Polynomial is
+
+    res : DoblDobl_Bracket_Polynomials.Bracket_Polynomial
+        := DoblDobl_Bracket_Polynomials.Null_Bracket_Poly;
+
+    procedure Convert_Term
+                ( t : Bracket_Polynomials.Bracket_Term; c : out boolean ) is
+
+      dd_t : DoblDobl_Bracket_Polynomials.Bracket_Term;
+
+    begin
+      dd_t.coeff := Standard_to_DoblDobl_Complex(t.coeff);
+      Copy_Append(t.monom,dd_t.monom);
+      DoblDobl_Bracket_Polynomials.Add(res,dd_t);
+      c := true;
+    end Convert_Term;
+    procedure Convert_Terms is new
+      Bracket_Polynomials.Enumerate_Terms(Convert_Term);
+
+  begin
+    Convert_Terms(p);
+    return res;
+  end Convert;
+
+  function Convert
+             ( p : Bracket_Polynomials.Bracket_Polynomial )
+             return QuadDobl_Bracket_Polynomials.Bracket_Polynomial is
+
+    res : QuadDobl_Bracket_Polynomials.Bracket_Polynomial
+        := QuadDobl_Bracket_Polynomials.Null_Bracket_Poly;
+
+    procedure Convert_Term
+                ( t : Bracket_Polynomials.Bracket_Term; c : out boolean ) is
+
+      dd_t : QuadDobl_Bracket_Polynomials.Bracket_Term;
+
+    begin
+      dd_t.coeff := Standard_to_QuadDobl_Complex(t.coeff);
+      Copy_Append(t.monom,dd_t.monom);
+      QuadDobl_Bracket_Polynomials.Add(res,dd_t);
+      c := true;
+    end Convert_Term;
+    procedure Convert_Terms is new
+      Bracket_Polynomials.Enumerate_Terms(Convert_Term);
+
+  begin
+    Convert_Terms(p);
+    return res;
+  end Convert;
+
+end Bracket_Polynomial_Convertors;
---
+++ src/Ada/Schubert/SAGBI/bracket_polynomial_convertors.ads
@@ -0,0 +1,23 @@
+with Bracket_Polynomials;
+with DoblDobl_Bracket_Polynomials;
+with QuadDobl_Bracket_Polynomials;
+
+package Bracket_Polynomial_Convertors is
+
+-- DESCRIPTION :
+--   Converts bracket polynomials with coefficients in standard double
+--   precision into brackt polynomials with coefficients in double double
+--   or quad double precision.
+
+  function Convert
+             ( p : Bracket_Polynomials.Bracket_Polynomial )
+             return DoblDobl_Bracket_Polynomials.Bracket_Polynomial;
+  function Convert
+             ( p : Bracket_Polynomials.Bracket_Polynomial )
+             return QuadDobl_Bracket_Polynomials.Bracket_Polynomial;
+
+  -- DESCRIPTION :
+  --   On return is a bracket polynomial with the same monomials,
+  --   but with coefficients in double double or quad double precision.
+
+end Bracket_Polynomial_Convertors;
