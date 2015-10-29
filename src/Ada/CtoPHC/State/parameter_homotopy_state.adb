3f73e7b08061a9b0975764035998b23c3c7afe26
janv@uic.edu <jan@math.uic.edu>
2015-10-29 18:23:44 -0500

    adding management of values for start and target values in a sweep homotopy

--- src/Ada/CtoPHC/State/parameter_homotopy_state.adb
+++ src/Ada/CtoPHC/State/parameter_homotopy_state.adb
@@ -7,6 +7,12 @@ package body Parameter_Homotopy_State is
 
   nbequ,nbvar,nbpar : integer32;
   paridx : Standard_Integer_Vectors.Link_to_Vector;
+  st_startv : Standard_Complex_Vectors.Link_to_Vector;
+  dd_startv : DoblDobl_Complex_Vectors.Link_to_Vector;
+  qd_startv : QuadDobl_Complex_Vectors.Link_to_Vector;
+  st_target : Standard_Complex_Vectors.Link_to_Vector;
+  dd_target : DoblDobl_Complex_Vectors.Link_to_Vector;
+  qd_target : QuadDobl_Complex_Vectors.Link_to_Vector;
 
 -- CONSTRUCTORS :
 
@@ -31,6 +37,36 @@ package body Parameter_Homotopy_State is
     Standard_Parameter_Systems.Sort(paridx.all);
   end Set_Indices;
 
+  procedure Set_Start ( v : in Standard_Complex_Vectors.Vector ) is
+  begin
+    st_startv := new Standard_Complex_Vectors.Vector'(v);
+  end Set_Start;
+
+  procedure Set_Start ( v : in DoblDobl_Complex_Vectors.Vector ) is
+  begin
+    dd_startv := new DoblDobl_Complex_Vectors.Vector'(v);
+  end Set_Start;
+
+  procedure Set_Start ( v : in QuadDobl_Complex_Vectors.Vector ) is
+  begin
+    qd_startv := new QuadDobl_Complex_Vectors.Vector'(v);
+  end Set_Start;
+
+  procedure Set_Target ( v : in Standard_Complex_Vectors.Vector ) is
+  begin
+    st_target := new Standard_Complex_Vectors.Vector'(v);
+  end Set_Target;
+
+  procedure Set_Target ( v : in DoblDobl_Complex_Vectors.Vector ) is
+  begin
+    dd_target := new DoblDobl_Complex_Vectors.Vector'(v);
+  end Set_Target;
+
+  procedure Set_Target ( v : in QuadDobl_Complex_Vectors.Vector ) is
+  begin
+    qd_target := new QuadDobl_Complex_Vectors.Vector'(v);
+  end Set_Target;
+
 -- SELECTORS :
 
   function Get_Number_of_Equations return integer32 is
@@ -53,11 +89,44 @@ package body Parameter_Homotopy_State is
     return paridx;
   end Get_Indices;
 
+  function Get_Start return Standard_Complex_Vectors.Link_to_Vector is
+  begin
+    return st_startv;
+  end Get_Start;
+
+  function Get_Start return DoblDobl_Complex_Vectors.Link_to_Vector is
+  begin
+    return dd_startv;
+  end Get_Start;
+
+  function Get_Start return QuadDobl_Complex_Vectors.Link_to_Vector is
+  begin
+    return qd_startv;
+  end Get_Start;
+
+  function Get_Target return Standard_Complex_Vectors.Link_to_Vector is
+  begin
+    return st_target;
+  end Get_Target;
+
+  function Get_Target return DoblDobl_Complex_Vectors.Link_to_Vector is
+  begin
+    return dd_target;
+  end Get_Target;
+
+  function Get_Target return QuadDobl_Complex_Vectors.Link_to_Vector is
+  begin
+    return qd_target;
+  end Get_Target;
+
 -- DESTRUCTOR :
 
   procedure Clear is
 
     use Standard_Integer_Vectors;
+    use Standard_Complex_Vectors;
+    use DoblDobl_Complex_Vectors;
+    use QuadDobl_Complex_Vectors;
 
   begin
     nbequ := 0;
@@ -65,6 +134,24 @@ package body Parameter_Homotopy_State is
     nbpar := 0;
     if paridx /= null
      then Standard_Integer_Vectors.Clear(paridx);
+    end if;
+    if st_startv /= null
+     then Standard_Complex_Vectors.Clear(st_startv);
+    end if;
+    if dd_startv /= null
+     then DoblDobl_Complex_Vectors.Clear(dd_startv);
+    end if;
+    if qd_startv /= null
+     then QuadDobl_Complex_Vectors.Clear(qd_startv);
+    end if;
+    if st_target /= null
+     then Standard_Complex_Vectors.Clear(st_target);
+    end if;
+    if dd_target /= null
+     then DoblDobl_Complex_Vectors.Clear(dd_target);
+    end if;
+    if qd_target /= null
+     then QuadDobl_Complex_Vectors.Clear(qd_target);
     end if;
   end Clear;
 
--- src/Ada/CtoPHC/State/parameter_homotopy_state.ads
+++ src/Ada/CtoPHC/State/parameter_homotopy_state.ads
@@ -1,5 +1,8 @@
 with Standard_Integer_Numbers;          use Standard_Integer_Numbers;
 with Standard_Integer_Vectors;
+with Standard_Complex_Vectors;
+with DoblDobl_Complex_Vectors;
+with QuadDobl_Complex_Vectors;
 
 package Parameter_Homotopy_State is
 
@@ -9,6 +12,8 @@ package Parameter_Homotopy_State is
 --   2) the number of variables, which includes the parameters;
 --   3) the number of parameters;
 --   4) the index set of which variables are parameters.
+--   In addition, the start and target values for the parameters are stored,
+--   in standard double, double double, or quad double precision.
 
 -- CONSTRUCTORS :
 
@@ -32,6 +37,28 @@ package Parameter_Homotopy_State is
   -- DESCRIPTION :
   --   Defines the indices to the parameters in the sweep homotopy.
 
+  -- REQUIRED : idx'range goes from 1 till the number of parameters.
+
+  procedure Set_Start ( v : in Standard_Complex_Vectors.Vector ); 
+  procedure Set_Start ( v : in DoblDobl_Complex_Vectors.Vector ); 
+  procedure Set_Start ( v : in QuadDobl_Complex_Vectors.Vector ); 
+
+  -- DESCRIPTION :
+  --   Sets the start values for the parameters,
+  --   in standard double, double double, or quad double precision.
+
+  -- REQUIRED : v'range is the range of the indices to the parameter.
+
+  procedure Set_Target ( v : in Standard_Complex_Vectors.Vector ); 
+  procedure Set_Target ( v : in DoblDobl_Complex_Vectors.Vector ); 
+  procedure Set_Target ( v : in QuadDobl_Complex_Vectors.Vector ); 
+
+  -- DESCRIPTION :
+  --   Sets the target values for the parameters,
+  --   in standard double, double double, or quad double precision.
+
+  -- REQUIRED : v'range is the range of the indices to the parameter.
+
 -- SELECTORS :
 
   function Get_Number_of_Equations return integer32;
@@ -53,6 +80,22 @@ package Parameter_Homotopy_State is
 
   -- DESCRIPTION :
   --   Returns the indices to the parameters.
+
+  function Get_Start return Standard_Complex_Vectors.Link_to_Vector; 
+  function Get_Start return DoblDobl_Complex_Vectors.Link_to_Vector; 
+  function Get_Start return QuadDobl_Complex_Vectors.Link_to_Vector; 
+
+  -- DESCRIPTION :
+  --   Gets the start values for the parameters,
+  --   in standard double, double double, or quad double precision.
+
+  function Get_Target return Standard_Complex_Vectors.Link_to_Vector; 
+  function Get_Target return DoblDobl_Complex_Vectors.Link_to_Vector; 
+  function Get_Target return QuadDobl_Complex_Vectors.Link_to_Vector; 
+
+  -- DESCRIPTION :
+  --   Gets the target values for the parameters,
+  --   in standard double, double double, or quad double precision.
 
 -- DESTRUCTOR :
 
