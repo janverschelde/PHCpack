92c616d395957c395707e738e5c7cfcbeeacc7f5
janv@uic.edu <jan@math.uic.edu>
2015-10-30 11:34:58 -0500

    factored out code into a new package for the special case of complex convex parameter continuation

---
+++ src/Ada/Deformations/Sweep/complex_convex_continuation.adb
@@ -0,0 +1,463 @@
+with Standard_Complex_Matrices;
+with Standard_Complex_Norms_Equals;     use Standard_Complex_Norms_Equals;
+with DoblDobl_Complex_Matrices;
+with DoblDobl_Complex_Vector_Norms;     use DoblDobl_Complex_Vector_Norms;
+with QuadDobl_Complex_Matrices;
+with QuadDobl_Complex_Vector_Norms;     use QuadDobl_Complex_Vector_Norms;
+with Standard_Complex_Poly_Functions;
+with Standard_Complex_Poly_SysFun;
+with Standard_Complex_Jaco_Matrices;
+with DoblDobl_Complex_Poly_Functions;
+with DoblDobl_Complex_Poly_SysFun;
+with DoblDobl_Complex_Jaco_Matrices;
+with QuadDobl_Complex_Poly_Functions;
+with QuadDobl_Complex_Poly_SysFun;
+with QuadDobl_Complex_Jaco_Matrices;
+with Standard_IncFix_Continuation;
+with DoblDobl_IncFix_Continuation;
+with QuadDobl_IncFix_Continuation;
+
+package body Complex_Convex_Continuation is
+
+  function Interpolate ( a,b : Standard_Complex_Vectors.Vector;
+                         t : Standard_Complex_Numbers.Complex_Number )
+                       return Standard_Complex_Vectors.Vector is
+
+    use Standard_Complex_Numbers;
+
+    res : Standard_Complex_Vectors.Vector(a'range);
+    zero : constant Complex_Number := Create(0.0);
+    one : Complex_Number := Create(1.0);
+
+  begin
+    if t = zero then
+       res := a;
+    elsif t = one then
+       res := b;
+    else
+       one := one - t;
+       for i in a'range loop
+         res(i) := one*a(i) + t*b(i);  
+       end loop;
+    end if;
+    return res;
+  end Interpolate;
+
+  function Interpolate ( a,b : DoblDobl_Complex_Vectors.Vector;
+                         t : DoblDobl_Complex_Numbers.Complex_Number )
+                       return DoblDobl_Complex_Vectors.Vector is
+
+    use DoblDobl_Complex_Numbers;
+
+    res : DoblDobl_Complex_Vectors.Vector(a'range);
+    zero : constant Complex_Number := Create(integer(0));
+    one : Complex_Number := Create(integer(1));
+
+  begin
+    if t = zero then
+       res := a;
+    elsif t = one then
+       res := b;
+    else
+       one := one - t;
+       for i in a'range loop
+         res(i) := one*a(i) + t*b(i);  
+       end loop;
+    end if;
+    return res;
+  end Interpolate;
+
+  function Interpolate ( a,b : QuadDobl_Complex_Vectors.Vector;
+                         t : QuadDobl_Complex_Numbers.Complex_Number )
+                       return QuadDobl_Complex_Vectors.Vector is
+
+    use QuadDobl_Complex_Numbers;
+
+    res : QuadDobl_Complex_Vectors.Vector(a'range);
+    zero : constant Complex_Number := Create(integer(0));
+    one : Complex_Number := Create(integer(1));
+
+  begin
+    if t = zero then
+       res := a;
+    elsif t = one then
+       res := b;
+    else
+       one := one - t;
+       for i in a'range loop
+         res(i) := one*a(i) + t*b(i);  
+       end loop;
+    end if;
+    return res;
+  end Interpolate;
+
+  function Circulate ( a,b : Standard_Complex_Vectors.Vector;
+                       gamma,t : Standard_Complex_Numbers.Complex_Number )
+                     return Standard_Complex_Vectors.Vector is
+
+    use Standard_Complex_Numbers;
+
+    s : constant Complex_Number := t + gamma*t*(Create(1.0)-t);
+
+  begin
+    return Interpolate(a,b,s);
+  end Circulate;
+
+  function Circulate ( a,b : DoblDobl_Complex_Vectors.Vector;
+                       gamma,t : DoblDobl_Complex_Numbers.Complex_Number )
+                     return DoblDobl_Complex_Vectors.Vector is
+
+    use DoblDobl_Complex_Numbers;
+
+    s : constant Complex_Number := t + gamma*t*(Create(integer(1))-t);
+
+  begin
+    return Interpolate(a,b,s);
+  end Circulate;
+
+  function Circulate ( a,b : QuadDobl_Complex_Vectors.Vector;
+                       gamma,t : QuadDobl_Complex_Numbers.Complex_Number )
+                     return QuadDobl_Complex_Vectors.Vector is
+
+    use QuadDobl_Complex_Numbers;
+
+    s : constant Complex_Number := t + gamma*t*(Create(integer(1))-t);
+
+  begin
+    return Interpolate(a,b,s);
+  end Circulate;
+
+  function Differentiate ( a,b : Standard_Complex_Vectors.Vector )
+                         return Standard_Complex_Vectors.Vector is
+
+    use Standard_Complex_Vectors;
+
+    res : constant Vector(a'range) := b-a;
+
+  begin
+    return res;
+  end Differentiate;
+
+  function Differentiate ( a,b : DoblDobl_Complex_Vectors.Vector )
+                         return DoblDobl_Complex_Vectors.Vector is
+
+    use DoblDobl_Complex_Vectors;
+
+    res : constant Vector(a'range) := b-a;
+
+  begin
+    return res;
+  end Differentiate;
+
+  function Differentiate ( a,b : QuadDobl_Complex_Vectors.Vector )
+                         return QuadDobl_Complex_Vectors.Vector is
+
+    use QuadDobl_Complex_Vectors;
+
+    res : constant Vector(a'range) := b-a;
+
+  begin
+    return res;
+  end Differentiate;
+
+  procedure Standard_Parameter_Continuation
+              ( file : in file_type;
+                n : in integer32;
+                p : in Standard_Complex_Poly_Systems.Poly_Sys;
+                pars : in Standard_Integer_Vectors.Vector;
+                vars : in Standard_Integer_Vectors.Vector;
+		sols : in out Standard_Complex_Solutions.Solution_List;
+                output : in boolean ) is
+
+    use Standard_Complex_Numbers;
+    use Standard_Complex_Poly_Functions;
+    use Standard_Complex_Poly_SysFun;
+    use Standard_Complex_Jaco_Matrices;
+    use Standard_Complex_Solutions;
+    use Standard_IncFix_Continuation;
+
+    ep : Eval_Poly_Sys(p'range) := Create(p);
+    jm : Jaco_Mat(p'range,1..n) := Create(p);
+    ejm : Eval_Jaco_Mat(p'range,1..n) := Create(jm);
+  
+    function xvt ( x : Standard_Complex_Vectors.Vector;
+                   t : Standard_Complex_Numbers.Complex_Number )
+                 return Standard_Complex_Vectors.Vector is
+
+    -- DESCRIPTION :
+    --   Returns the value of all variables and parameters at
+    --   the current value of the continuation parameter t.
+
+      z : Standard_Complex_Vectors.Vector(1..n);
+      v : constant Standard_Complex_Vectors.Vector(pars'range)
+        := Evaluate_Parameters(t);
+
+    begin
+      for i in pars'range loop
+        z(integer32(pars(i))) := v(i);
+      end loop;
+      for i in vars'range loop
+        z(integer32(vars(i))) := x(i);
+      end loop;
+      return z;
+    end xvt;
+
+    function Eval ( x : Standard_Complex_Vectors.Vector;
+                    t : Standard_Complex_Numbers.Complex_Number )
+                  return Standard_Complex_Vectors.Vector is
+
+      z : constant Standard_Complex_Vectors.Vector(1..n) := xvt(x,t);
+
+    begin
+      return Eval(ep,z);
+    end Eval;
+
+    function Diff ( x : Standard_Complex_Vectors.Vector;
+                    t : Standard_Complex_Numbers.Complex_Number )
+                  return Standard_Complex_Matrices.Matrix is
+
+      res : Standard_Complex_Matrices.Matrix(p'range,x'range);
+      z : constant Standard_Complex_Vectors.Vector(1..n) := xvt(x,t);
+
+    begin
+      for i in p'range loop
+        for j in x'range loop
+          res(i,j) := Eval(ejm(i,integer32(vars(j))),z);
+        end loop;
+      end loop;
+      return res;
+    end Diff;
+
+    function Diff_t ( x : Standard_Complex_Vectors.Vector;
+                      t : Standard_Complex_Numbers.Complex_Number )
+                    return Standard_Complex_Vectors.Vector is
+
+      res : Standard_Complex_Vectors.Vector(p'range)
+          := (p'range => Create(0.0));
+      z : constant Standard_Complex_Vectors.Vector(1..n) := xvt(x,t);
+      d : constant Standard_Complex_Vectors.Vector
+        := Differentiate_Parameters(t);
+
+    begin
+      for i in p'range loop
+        for j in pars'range loop
+          res(i) := res(i) + Eval(ejm(i,integer32(pars(j))),z)*d(j);
+        end loop;
+      end loop;
+      return res;
+    end Diff_t;
+
+    procedure Sil_Cont is
+      new Silent_Continue(Max_Norm,Eval,Diff_t,Diff);
+    procedure Rep_Cont is
+      new Reporting_Continue(Max_Norm,Eval,Diff_t,Diff);
+
+  begin
+    if output
+     then Rep_Cont(file,sols,false,Create(1.0));
+     else Sil_Cont(sols,false,Create(1.0));
+    end if;
+    Clear(ep); Clear(jm); Clear(ejm);
+  end Standard_Parameter_Continuation;
+
+  procedure DoblDobl_Parameter_Continuation
+              ( file : in file_type;
+                n : in integer32;
+                p : in DoblDobl_Complex_Poly_Systems.Poly_Sys;
+                pars : in Standard_Integer_Vectors.Vector;
+                vars : in Standard_Integer_Vectors.Vector;
+		sols : in out DoblDobl_Complex_Solutions.Solution_List;
+                output : in boolean ) is
+
+    use DoblDobl_Complex_Numbers;
+    use DoblDobl_Complex_Poly_Functions;
+    use DoblDobl_Complex_Poly_SysFun;
+    use DoblDobl_Complex_Jaco_Matrices;
+    use DoblDobl_Complex_Solutions;
+    use DoblDobl_IncFix_Continuation;
+
+    ep : Eval_Poly_Sys(p'range) := Create(p);
+    jm : Jaco_Mat(p'range,1..n) := Create(p);
+    ejm : Eval_Jaco_Mat(p'range,1..n) := Create(jm);
+  
+    function xvt ( x : DoblDobl_Complex_Vectors.Vector;
+                   t : DoblDobl_Complex_Numbers.Complex_Number )
+                 return DoblDobl_Complex_Vectors.Vector is
+
+    -- DESCRIPTION :
+    --   Returns the value of all variables and parameters at
+    --   the current value of the continuation parameter t.
+
+      z : DoblDobl_Complex_Vectors.Vector(1..n);
+      v : constant DoblDobl_Complex_Vectors.Vector(pars'range)
+        := Evaluate_Parameters(t);
+
+    begin
+      for i in pars'range loop
+        z(integer32(pars(i))) := v(i);
+      end loop;
+      for i in vars'range loop
+        z(integer32(vars(i))) := x(i);
+      end loop;
+      return z;
+    end xvt;
+
+    function Eval ( x : DoblDobl_Complex_Vectors.Vector;
+                    t : DoblDobl_Complex_Numbers.Complex_Number )
+                  return DoblDobl_Complex_Vectors.Vector is
+
+      z : constant DoblDobl_Complex_Vectors.Vector(1..n) := xvt(x,t);
+
+    begin
+      return Eval(ep,z);
+    end Eval;
+
+    function Diff ( x : DoblDobl_Complex_Vectors.Vector;
+                    t : DoblDobl_Complex_Numbers.Complex_Number )
+                  return DoblDobl_Complex_Matrices.Matrix is
+
+      res : DoblDobl_Complex_Matrices.Matrix(p'range,x'range);
+      z : constant DoblDobl_Complex_Vectors.Vector(1..n) := xvt(x,t);
+
+    begin
+      for i in p'range loop
+        for j in x'range loop
+          res(i,j) := Eval(ejm(i,integer32(vars(j))),z);
+        end loop;
+      end loop;
+      return res;
+    end Diff;
+
+    function Diff_t ( x : DoblDobl_Complex_Vectors.Vector;
+                      t : DoblDobl_Complex_Numbers.Complex_Number )
+                    return DoblDobl_Complex_Vectors.Vector is
+
+      res : DoblDobl_Complex_Vectors.Vector(p'range)
+          := (p'range => Create(integer(0)));
+      z : constant DoblDobl_Complex_Vectors.Vector(1..n) := xvt(x,t);
+      d : constant DoblDobl_Complex_Vectors.Vector
+        := Differentiate_Parameters(t);
+
+    begin
+      for i in p'range loop
+        for j in pars'range loop
+          res(i) := res(i) + Eval(ejm(i,integer32(pars(j))),z)*d(j);
+        end loop;
+      end loop;
+      return res;
+    end Diff_t;
+
+    procedure Sil_Cont is
+      new Silent_Continue(Max_Norm,Eval,Diff_t,Diff);
+    procedure Rep_Cont is
+      new Reporting_Continue(Max_Norm,Eval,Diff_t,Diff);
+
+  begin
+    if output
+     then Rep_Cont(file,sols,Create(integer(1)));
+     else Sil_Cont(sols,Create(integer(1)));
+    end if;
+    Clear(ep); Clear(jm); Clear(ejm);
+  end DoblDobl_Parameter_Continuation;
+
+  procedure QuadDobl_Parameter_Continuation
+              ( file : in file_type;
+                n : in integer32;
+                p : in QuadDobl_Complex_Poly_Systems.Poly_Sys;
+                pars : in Standard_Integer_Vectors.Vector;
+                vars : in Standard_Integer_Vectors.Vector;
+		sols : in out QuadDobl_Complex_Solutions.Solution_List;
+                output : in boolean ) is
+
+    use QuadDobl_Complex_Numbers;
+    use QuadDobl_Complex_Poly_Functions;
+    use QuadDobl_Complex_Poly_SysFun;
+    use QuadDobl_Complex_Jaco_Matrices;
+    use QuadDobl_Complex_Solutions;
+    use QuadDobl_IncFix_Continuation;
+
+    ep : Eval_Poly_Sys(p'range) := Create(p);
+    jm : Jaco_Mat(p'range,1..n) := Create(p);
+    ejm : Eval_Jaco_Mat(p'range,1..n) := Create(jm);
+  
+    function xvt ( x : QuadDobl_Complex_Vectors.Vector;
+                   t : QuadDobl_Complex_Numbers.Complex_Number )
+                 return QuadDobl_Complex_Vectors.Vector is
+
+    -- DESCRIPTION :
+    --   Returns the value of all variables and parameters at
+    --   the current value of the continuation parameter t.
+
+      z : QuadDobl_Complex_Vectors.Vector(1..n);
+      v : constant QuadDobl_Complex_Vectors.Vector(pars'range)
+        := Evaluate_Parameters(t);
+
+    begin
+      for i in pars'range loop
+        z(integer32(pars(i))) := v(i);
+      end loop;
+      for i in vars'range loop
+        z(integer32(vars(i))) := x(i);
+      end loop;
+      return z;
+    end xvt;
+
+    function Eval ( x : QuadDobl_Complex_Vectors.Vector;
+                    t : QuadDobl_Complex_Numbers.Complex_Number )
+                  return QuadDobl_Complex_Vectors.Vector is
+
+      z : constant QuadDobl_Complex_Vectors.Vector(1..n) := xvt(x,t);
+
+    begin
+      return Eval(ep,z);
+    end Eval;
+
+    function Diff ( x : QuadDobl_Complex_Vectors.Vector;
+                    t : QuadDobl_Complex_Numbers.Complex_Number )
+                  return QuadDobl_Complex_Matrices.Matrix is
+
+      res : QuadDobl_Complex_Matrices.Matrix(p'range,x'range);
+      z : constant QuadDobl_Complex_Vectors.Vector(1..n) := xvt(x,t);
+
+    begin
+      for i in p'range loop
+        for j in x'range loop
+          res(i,j) := Eval(ejm(i,integer32(vars(j))),z);
+        end loop;
+      end loop;
+      return res;
+    end Diff;
+
+    function Diff_t ( x : QuadDobl_Complex_Vectors.Vector;
+                      t : QuadDobl_Complex_Numbers.Complex_Number )
+                    return QuadDobl_Complex_Vectors.Vector is
+
+      res : QuadDobl_Complex_Vectors.Vector(p'range)
+          := (p'range => Create(integer(0)));
+      z : constant QuadDobl_Complex_Vectors.Vector(1..n) := xvt(x,t);
+      d : constant QuadDobl_Complex_Vectors.Vector
+        := Differentiate_Parameters(t);
+
+    begin
+      for i in p'range loop
+        for j in pars'range loop
+          res(i) := res(i) + Eval(ejm(i,integer32(pars(j))),z)*d(j);
+        end loop;
+      end loop;
+      return res;
+    end Diff_t;
+
+    procedure Sil_Cont is
+      new Silent_Continue(Max_Norm,Eval,Diff_t,Diff);
+    procedure Rep_Cont is
+      new Reporting_Continue(Max_Norm,Eval,Diff_t,Diff);
+
+  begin
+    if output
+     then Rep_Cont(file,sols,Create(integer(1)));
+     else Sil_Cont(sols,Create(integer(1)));
+    end if;
+    Clear(ep); Clear(jm); Clear(ejm);
+  end QuadDobl_Parameter_Continuation;
+
+end Complex_Convex_Continuation;
---
+++ src/Ada/Deformations/Sweep/complex_convex_continuation.ads
@@ -0,0 +1,144 @@
+with text_io;                           use text_io;
+with Standard_Integer_Numbers;          use Standard_Integer_Numbers;
+with Standard_Complex_Numbers;
+with DoblDobl_Complex_Numbers;
+with QuadDobl_Complex_Numbers;
+with Standard_Integer_Vectors;
+with Standard_Complex_Vectors;
+with DoblDobl_Complex_Vectors;
+with QuadDobl_Complex_Vectors;
+with Standard_Complex_Poly_Systems;
+with DoblDobl_Complex_Poly_Systems;
+with DoblDobl_Complex_Jaco_Matrices;
+with QuadDobl_Complex_Poly_Systems;
+with Standard_Complex_Solutions;
+with DoblDobl_Complex_Solutions;
+with QuadDobl_Complex_Solutions;
+
+package Complex_Convex_Continuation is
+
+-- DESCRIPTION :
+--   A special case of Coefficient Parameter Homotopy Continuation 
+--   is when the parameter space is convex and we can move from one
+--   set of parameter values to another via convex-linear complex arcs,
+--   made generic by the gamma trick, so singularities can be avoided.
+--   The definition of start and target values for the parameters
+--   leads then naturally to interpolation between the two.
+--   This interpolation may be seen as the default function to instantiate
+--   the general parameter continuation routine with.
+--   For efficiency -- to reduce the number of variables -- interpolating
+--   between start and target is not expressed by explicit equations,
+--   but via direct substitution in the increment-and-fix continuation.
+
+  function Interpolate ( a,b : Standard_Complex_Vectors.Vector;
+                         t : Standard_Complex_Numbers.Complex_Number )
+                       return Standard_Complex_Vectors.Vector;
+  function Interpolate ( a,b : DoblDobl_Complex_Vectors.Vector;
+                         t : DoblDobl_Complex_Numbers.Complex_Number )
+                       return DoblDobl_Complex_Vectors.Vector;
+  function Interpolate ( a,b : QuadDobl_Complex_Vectors.Vector;
+                         t : QuadDobl_Complex_Numbers.Complex_Number )
+                       return QuadDobl_Complex_Vectors.Vector;
+
+  -- DESCRIPTION :
+  --   Returns (1-t)*a + t*b, for t between 0 and 1.  Is the 1st
+  --   default to use in the instantiation of Parameter_Continuation.
+
+  function Circulate ( a,b : Standard_Complex_Vectors.Vector;
+                       gamma,t : Standard_Complex_Numbers.Complex_Number )
+                     return Standard_Complex_Vectors.Vector;
+  function Circulate ( a,b : DoblDobl_Complex_Vectors.Vector;
+                       gamma,t : DoblDobl_Complex_Numbers.Complex_Number )
+                     return DoblDobl_Complex_Vectors.Vector;
+  function Circulate ( a,b : QuadDobl_Complex_Vectors.Vector;
+                       gamma,t : QuadDobl_Complex_Numbers.Complex_Number )
+                     return QuadDobl_Complex_Vectors.Vector;
+
+  -- DESCRIPTION :
+  --   Returns (1-s)*a + s*b, for s = t + gamma*t*(1-t).  This move
+  --   through the parameter space ensures complex arithmetic,
+  --   in standard double, double double, or quad double arithmetic.
+
+  function Differentiate ( a,b : Standard_Complex_Vectors.Vector )
+                         return Standard_Complex_Vectors.Vector;
+  function Differentiate ( a,b : DoblDobl_Complex_Vectors.Vector )
+                         return DoblDobl_Complex_Vectors.Vector;
+  function Differentiate ( a,b : QuadDobl_Complex_Vectors.Vector )
+                         return QuadDobl_Complex_Vectors.Vector;
+
+  -- DESCRIPTION :
+  --   Just returns b-a, in standard double, double double, or quad
+  --   double precision, as this is the derivative of the Interpolate
+  --   function of the parameters.  Is the 2nd default to use in the
+  --   instantiation of Parameter_Continuation.
+
+  generic
+    with function Evaluate_Parameters 
+                    ( t : Standard_Complex_Numbers.Complex_Number )
+                    return Standard_Complex_Vectors.Vector;
+    -- returns value of parameters at t
+    with function Differentiate_Parameters
+                    ( t : Standard_Complex_Numbers.Complex_Number )
+                    return Standard_Complex_Vectors.Vector;
+    -- returns derivatives of parameters with respect to t
+  procedure Standard_Parameter_Continuation
+              ( file : in file_type;
+                n : in integer32;
+                p : in Standard_Complex_Poly_Systems.Poly_Sys;
+                pars : in Standard_Integer_Vectors.Vector;
+                vars : in Standard_Integer_Vectors.Vector;
+		sols : in out Standard_Complex_Solutions.Solution_List;
+                output : in boolean );
+  generic
+    with function Evaluate_Parameters 
+                    ( t : DoblDobl_Complex_Numbers.Complex_Number )
+                    return DoblDobl_Complex_Vectors.Vector;
+    -- returns value of parameters at t
+    with function Differentiate_Parameters
+                    ( t : DoblDobl_Complex_Numbers.Complex_Number )
+                    return DoblDobl_Complex_Vectors.Vector;
+    -- returns derivatives of parameters with respect to t
+  procedure DoblDobl_Parameter_Continuation
+              ( file : in file_type;
+                n : in integer32;
+                p : in DoblDobl_Complex_Poly_Systems.Poly_Sys;
+                pars : in Standard_Integer_Vectors.Vector;
+                vars : in Standard_Integer_Vectors.Vector;
+		sols : in out DoblDobl_Complex_Solutions.Solution_List;
+                output : in boolean );
+  generic
+    with function Evaluate_Parameters 
+                    ( t : QuadDobl_Complex_Numbers.Complex_Number )
+                    return QuadDobl_Complex_Vectors.Vector;
+    -- returns value of parameters at t
+    with function Differentiate_Parameters
+                    ( t : QuadDobl_Complex_Numbers.Complex_Number )
+                    return QuadDobl_Complex_Vectors.Vector;
+    -- returns derivatives of parameters with respect to t
+  procedure QuadDobl_Parameter_Continuation
+              ( file : in file_type;
+                n : in integer32;
+                p : in QuadDobl_Complex_Poly_Systems.Poly_Sys;
+                pars : in Standard_Integer_Vectors.Vector;
+                vars : in Standard_Integer_Vectors.Vector;
+		sols : in out QuadDobl_Complex_Solutions.Solution_List;
+                output : in boolean );
+
+  -- DESCRIPTION :
+  --   Executes the parameter continuation for the homotopy defined by
+  --   the system p and for parameters defined by the generics functions,
+  --   in standard double, double double, or quad double precision.
+
+  -- ON ENTRY :
+  --   file     for intermediate output and diagnostics;
+  --   n        number of unknowns and parameters in p.
+  --   p        polynomial system with parameters;
+  --   pars     indices to the parameter variables in p;
+  --   vars     indices to the unknown variables in p;
+  --   sols     start solutions for start values of parameters;
+  --   output   true if intermediate output during continuation.
+
+  -- ON RETURN :
+  --   sols     solutions for target values of parameters.
+
+end Complex_Convex_Continuation;
