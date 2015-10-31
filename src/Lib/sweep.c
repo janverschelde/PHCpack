1394dd49df6c51097803bdc9551f79e56393d116
janv@uic.edu <jan@math.uic.edu>
2015-10-31 15:46:01 -0500

    all functionality of use_sweep is available in C via the functions with prototypes in sweep.h

--- src/Lib/sweep.c
+++ src/Lib/sweep.c
@@ -17,6 +17,25 @@ int sweep_define_parameters_numerically ( int nq, int nv, int np, int *pars )
    return fail;
 }
 
+int sweep_define_parameters_symbolically
+ ( int nq, int nv, int np, int nc, char *pars )
+{
+   int k,fail = 0;
+   int nqvp[4];
+   int idx[np];
+   double *c;
+
+   nqvp[0] = nq;
+   nqvp[1] = nv;
+   nqvp[2] = np;
+   nqvp[3] = nc;
+   for(k=0; k<np; k++) idx[k] = (int) pars[k];
+
+   fail = _ada_use_c2phc(611,nqvp,idx,c);
+
+   return fail;
+}
+
 int sweep_get_number_of_equations ( int *n )
 {
    int fail,*b;
@@ -57,12 +76,231 @@ int sweep_get_indices_numerically ( int *idx )
    return fail;
 }
 
+int sweep_get_indices_symbolically ( int *nc, char *pars )
+{
+   int fail,np;
+   double *c;
+
+   fail = sweep_get_number_of_parameters(&np);
+   {
+      int k,buf[np*20]; /* assumes no more than 20 characters per symbol */
+
+      fail = _ada_use_c2phc(617,nc,buf,c);
+
+      for(k=0; k<(*nc); k++) pars[k] = (char) buf[k];
+   }
+   return fail;
+}
+
 int sweep_clear_definitions ( void )
 {
    int fail,*a,*b;
    double *c;
 
    fail = _ada_use_c2phc(617,a,b,c);
+
+   return fail;
+}
+
+int sweep_set_standard_start ( int n, double *c )
+{
+   int fail,ptos[2];
+
+   ptos[0] = 0; /* standard precision */
+   ptos[1] = 0; /* start values */
+
+   fail = _ada_use_c2phc(618,ptos,&n,c);
+
+   return fail;
+}
+
+int sweep_set_standard_target ( int n, double *c )
+{
+   int fail,ptos[2];
+
+   ptos[0] = 0; /* standard precision */
+   ptos[1] = 1; /* target values */
+
+   fail = _ada_use_c2phc(618,ptos,&n,c);
+
+   return fail;
+}
+
+int sweep_set_dobldobl_start ( int n, double *c )
+{
+   int fail,ptos[2];
+
+   ptos[0] = 1; /* double double precision */
+   ptos[1] = 0; /* start values */
+
+   fail = _ada_use_c2phc(618,ptos,&n,c);
+
+   return fail;
+}
+
+int sweep_set_dobldobl_target ( int n, double *c )
+{
+   int fail,ptos[2];
+
+   ptos[0] = 1; /* double double precision */
+   ptos[1] = 1; /* target values */
+
+   fail = _ada_use_c2phc(618,ptos,&n,c);
+
+   return fail;
+}
+
+int sweep_set_quaddobl_start ( int n, double *c )
+{
+   int fail,ptos[2];
+
+   ptos[0] = 2; /* quad double precision */
+   ptos[1] = 0; /* start values */
+
+   fail = _ada_use_c2phc(618,ptos,&n,c);
+
+   return fail;
+}
+
+int sweep_set_quaddobl_target ( int n, double *c )
+{
+   int fail,ptos[2];
+
+   ptos[0] = 2; /* quad double precision */
+   ptos[1] = 1; /* target values */
+
+   fail = _ada_use_c2phc(618,ptos,&n,c);
+
+   return fail;
+}
+
+int sweep_get_standard_start ( int n, double *c )
+{
+   int fail,ptos[2];
+
+   ptos[0] = 0; /* standard precision */
+   ptos[1] = 0; /* start values */
+
+   fail = _ada_use_c2phc(619,ptos,&n,c);
+
+   return fail;
+}
+
+int sweep_get_standard_target ( int n, double *c )
+{
+   int fail,ptos[2];
+
+   ptos[0] = 0; /* standard precision */
+   ptos[1] = 1; /* target values */
+
+   fail = _ada_use_c2phc(619,ptos,&n,c);
+
+   return fail;
+}
+
+int sweep_get_dobldobl_start ( int n, double *c )
+{
+   int fail,ptos[2];
+
+   ptos[0] = 1; /* double double precision */
+   ptos[1] = 0; /* start values */
+
+   fail = _ada_use_c2phc(619,ptos,&n,c);
+
+   return fail;
+}
+
+int sweep_get_dobldobl_target ( int n, double *c )
+{
+   int fail,ptos[2];
+
+   ptos[0] = 1; /* double double precision */
+   ptos[1] = 1; /* target values */
+
+   fail = _ada_use_c2phc(619,ptos,&n,c);
+
+   return fail;
+}
+
+int sweep_get_quaddobl_start ( int n, double *c )
+{
+   int fail,ptos[2];
+
+   ptos[0] = 2; /* quad double precision */
+   ptos[1] = 0; /* start values */
+
+   fail = _ada_use_c2phc(619,ptos,&n,c);
+
+   return fail;
+}
+
+int sweep_get_quaddobl_target ( int n, double *c )
+{
+   int fail,ptos[2];
+
+   ptos[0] = 2; /* quad double precision */
+   ptos[1] = 1; /* target values */
+
+   fail = _ada_use_c2phc(619,ptos,&n,c);
+
+   return fail;
+}
+
+int sweep_standard_run ( int gchoice, double *regamma, double *imgamma )
+{
+   int fail;
+   int pg[2];
+   int *b;
+   double c[2];
+
+   pg[0] = 0; /* standard double precision */
+   pg[1] = gchoice; /* type of choice for gamma */
+   if(gchoice == 2)
+   {
+      c[0] = *regamma;
+      c[1] = *imgamma;
+   }
+   fail = _ada_use_c2phc(620,pg,b,c);
+
+   return fail;
+}
+
+int sweep_dobldobl_run ( int gchoice, double *regamma, double *imgamma )
+{
+   int fail;
+   int pg[2];
+   int *b;
+   double c[4];
+
+   pg[0] = 1; /* double double precision */
+   pg[1] = gchoice;   /* type of choice for gamma */
+   if(gchoice == 2)
+   {
+      c[0] = regamma[0]; c[1] = regamma[1]; /* regamma is a double double */
+      c[2] = imgamma[0]; c[3] = imgamma[1]; /* imgamma is a double double */
+   }
+   fail = _ada_use_c2phc(620,pg,b,c);
+
+   return fail;
+}
+
+int sweep_quaddobl_run ( int gchoice, double *regamma, double *imgamma )
+{
+   int fail;
+   int pg[2];
+   int *b;
+   double c[8];
+
+   pg[0] = 2; /* quad double precision */
+   pg[1] = gchoice;   /* type of choice for gamma */
+   if(gchoice == 2)
+   {
+      c[0] = regamma[0]; c[1] = regamma[1]; /* regamma is a quad double */
+      c[2] = regamma[2]; c[3] = regamma[3];
+      c[0] = imgamma[0]; c[1] = imgamma[1]; /* imgamma is a quad double */
+      c[2] = imgamma[2]; c[3] = imgamma[3];
+   }
+   fail = _ada_use_c2phc(620,pg,b,c);
 
    return fail;
 }
--- src/Lib/sweep.h
+++ src/Lib/sweep.h
@@ -28,6 +28,22 @@ int sweep_define_parameters_numerically ( int nq, int nv, int np, int *pars );
  *   np      the number of parameters;
  *   pars    indices to the parameter variables, should be np in number. */
 
+int sweep_define_parameters_symbolically
+ ( int nq, int nv, int np, int nc, char *pars );
+/*
+ * DESCRIPTION :
+ *   Defines the indices to the variables that serve as parameters
+ *   symbolically, that is, as names of variables.
+ *   For this to work, the symbol table must be initialized.
+ *
+ * ON ENTRY :
+ *   nq      the number of equations;
+ *   nv      the number of variables, which includes the number of parameters;
+ *   np      the number of parameters;
+ *   nc      the number of characters in the string pars;
+ *   pars    names of the parameter variables, should be np in number,
+ *           the names are separated by one space each. */
+
 int sweep_get_number_of_equations ( int *n );
 /*
  * DESCRIPTION :
@@ -49,9 +65,143 @@ int sweep_get_indices_numerically ( int *idx );
  *   Returns in idx the indices of the variables that are parameters,
  *   if the argument contains sufficiently allocated space. */
 
+int sweep_get_indices_symbolically ( int *nc, char *pars );
+/*
+ * DESCRIPTION :
+ *   Returns in the string pars with nc characters the names
+ *   of the parameters, each separated by one space. */
+
 int sweep_clear_definitions ( void );
 /*
  * DESCRIPTION :
  *   Clears the definitions of the parameters. */
+
+int sweep_set_standard_start ( int n, double *c );
+/*
+ * DESCRIPTION :
+ *   Sets the start values for the parameters in standard double precision,
+ *   giving on input n doubles in c, with the consecutive real and imaginary
+ *   parts for the start values of all parameters. */
+
+int sweep_set_standard_target ( int n, double *c );
+/*
+ * DESCRIPTION :
+ *   Sets the target values for the parameters in standard double precision,
+ *   giving on input n doubles in c, with the consecutive real and imaginary
+ *   parts for the target values of all parameters. */
+
+int sweep_set_dobldobl_start ( int n, double *c );
+/*
+ * DESCRIPTION :
+ *   Sets the start values for the parameters in double double precision,
+ *   giving on input n doubles in c, with the consecutive real and imaginary
+ *   parts for the start values of all parameters. */
+
+int sweep_set_dobldobl_target ( int n, double *c );
+/*
+ * DESCRIPTION :
+ *   Sets the target values for the parameters in double double precision,
+ *   giving on input n doubles in c, with the consecutive real and imaginary
+ *   parts for the target values of all parameters. */
+
+int sweep_set_quaddobl_start ( int n, double *c );
+/*
+ * DESCRIPTION :
+ *   Sets the start values for the parameters in quad double precision,
+ *   giving on input n doubles in c, with the consecutive real and imaginary
+ *   parts for the start values of all parameters. */
+
+int sweep_set_quaddobl_target ( int n, double *c );
+/*
+ * DESCRIPTION :
+ *   Sets the target values for the parameters in quad double precision,
+ *   giving on input n doubles in c, with the consecutive real and imaginary
+ *   parts for the target values of all parameters. */
+
+int sweep_get_standard_start ( int n, double *c );
+/*
+ * DESCRIPTION :
+ *   Gets the start values for the parameters in standard double precision,
+ *   giving on input in n the number doubles that need to be returned in c.
+ *   On return in c will be n doubles, for the consecutive real and imaginary
+ *   parts for the start values of all parameters. */
+
+int sweep_get_standard_target ( int n, double *c );
+/*
+ * DESCRIPTION :
+ *   Gets the target values for the parameters in standard double precision,
+ *   giving on input in n the number doubles that need to be returned in c.
+ *   On return in c will be n doubles, for the consecutive real and imaginary
+ *   parts for the target values of all parameters. */
+
+int sweep_get_dobldobl_start ( int n, double *c );
+/*
+ * DESCRIPTION :
+ *   Gets the start values for the parameters in double double precision,
+ *   giving on input in n the number doubles that need to be returned in c.
+ *   On return in c will be n doubles, for the consecutive real and imaginary
+ *   parts for the start values of all parameters. */
+
+int sweep_get_dobldobl_target ( int n, double *c );
+/*
+ * DESCRIPTION :
+ *   Gets the target values for the parameters in double double precision,
+ *   giving on input in n the number doubles that need to be returned in c.
+ *   On return in c will be n doubles, for the consecutive real and imaginary
+ *   parts for the target values of all parameters. */
+
+int sweep_get_quaddobl_start ( int n, double *c );
+/*
+ * DESCRIPTION :
+ *   Gets the start values for the parameters in quad double precision,
+ *   giving on input in n the number doubles that need to be returned in c.
+ *   On return in c will be n doubles, for the consecutive real and imaginary
+ *   parts for the start values of all parameters. */
+
+int sweep_get_quaddobl_target ( int n, double *c );
+/*
+ * DESCRIPTION :
+ *   Gets the target values for the parameters in quad double precision,
+ *   giving on input in n the number doubles that need to be returned in c.
+ *   On return in c will be n doubles, for the consecutive real and imaginary
+ *   parts for the target values of all parameters. */
+
+int sweep_standard_run ( int gchoice, double *regamma, double *imgamma );
+/*
+ * DESCRIPTION :
+ *   Starts the trackers in a complex convex parameter homotopy,
+ *   in standard double precision, where the indices to the parameters,
+ *   start and target values are already defined.  Moreover, the containers
+ *   of systems and solutions in standard double precision have been
+ *   initialized with a parametric systems and start solutions.
+ *   The input parameter gchoice is 0, 1, or 2, for respectively
+ *   a randomly generated gamma (0), or no gamma (1), or a user given
+ *   gamma with real and imaginary parts in regamma and imgamma. */
+
+int sweep_dobldobl_run ( int gchoice, double *regamma, double *imgamma );
+/*
+ * DESCRIPTION :
+ *   Starts the trackers in a complex convex parameter homotopy,
+ *   in double double precision, where the indices to the parameters,
+ *   start and target values are already defined.  Moreover, the containers
+ *   of systems and solutions in double double precision have been
+ *   initialized with a parametric systems and start solutions.
+ *   The input parameter gchoice is 0, 1, or 2, for respectively
+ *   a randomly generated gamma (0), or no gamma (1), or a user given
+ *   gamma with real and imaginary parts in regamma and imgamma.
+ *   For gchoice == 2, regamma and imgamma are double doubles. */
+
+int sweep_quaddobl_run ( int gchoice, double *regamma, double *imgamma );
+/*
+ * DESCRIPTION :
+ *   Starts the trackers in a complex convex parameter homotopy,
+ *   in quad double precision, where the indices to the parameters,
+ *   start and target values are already defined.  Moreover, the containers
+ *   of systems and solutions in quad double precision have been
+ *   initialized with a parametric systems and start solutions.
+ *   The input parameter gchoice is 0, 1, or 2, for respectively
+ *   a randomly generated gamma (0), or no gamma (1), or a user given
+ *   gamma with real and imaginary parts in regamma and imgamma.
+ *   For gchoice == 2, regamma and imgamma are quad doubles. */
 
 #endif
