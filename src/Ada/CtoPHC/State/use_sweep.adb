d092ec68ce34eccd77d1922821ce038bed1e0812
janv@uic.edu <jan@math.uic.edu>
2015-11-05 16:45:19 -0600

    added double double and quad double versions to the main interface function for the sweep

--- src/Ada/CtoPHC/State/use_sweep.adb
+++ src/Ada/CtoPHC/State/use_sweep.adb
@@ -4,6 +4,8 @@ with String_Splitters;
 with Standard_Natural_Numbers;          use Standard_Natural_Numbers;
 with Standard_Floating_Numbers;         use Standard_Floating_Numbers;
 with Standard_Complex_Numbers;
+with Double_Double_Numbers;             use Double_Double_Numbers;
+with Quad_Double_Numbers;               use Quad_Double_Numbers;
 with DoblDobl_Complex_Numbers;
 with QuadDobl_Complex_Numbers;
 with Standard_Random_Numbers;
@@ -14,6 +16,8 @@ with Assignments_in_Ada_and_C;          use Assignments_in_Ada_and_C;
 with Standard_Integer_Vectors;
 with Standard_Complex_Vectors;
 with Standard_Floating_Vectors;
+with Double_Double_Vectors;
+with Quad_Double_Vectors;
 with DoblDobl_Complex_Vectors;
 with QuadDobl_Complex_Vectors;
 with Standard_Complex_Poly_Systems;
@@ -24,18 +28,34 @@ with Standard_Floating_Poly_Systems;
 with Standard_Floating_Poly_SysFun;
 with Standard_Floating_Jaco_Matrices;
 with DoblDobl_Complex_Poly_Systems;
+with DoblDobl_Complex_Poly_SysFun;
+with DoblDobl_Complex_Jaco_Matrices;
+with DoblDobl_Complex_to_Real_Poly;
+with Double_Double_Poly_Systems;
+with Double_Double_Poly_SysFun;
+with Double_Double_Jaco_Matrices;
 with QuadDobl_Complex_Poly_Systems;
+with QuadDobl_Complex_Poly_SysFun;
+with QuadDobl_Complex_Jaco_Matrices;
+with QuadDobl_Complex_to_Real_Poly;
+with Quad_Double_Poly_Systems;
+with Quad_Double_Poly_SysFun;
+with Quad_Double_Jaco_Matrices;
 with Standard_Complex_Solutions;
 with Standard_Solution_Diagnostics;
 with DoblDobl_Complex_Solutions;
+with DoblDobl_Solution_Diagnostics;
 with QuadDobl_Complex_Solutions;
+with QuadDobl_Solution_Diagnostics;
 with Standard_Parameter_Systems;
 with Standard_Parameter_Solutions;
 with Standard_Quad_Sweepers;
 with DoblDobl_Parameter_Systems;
 with DoblDobl_Parameter_Solutions;
+with DoblDobl_Quad_Sweepers;
 with QuadDobl_Parameter_Systems;
 with QuadDobl_Parameter_Solutions;
+with QuadDobl_Quad_Sweepers;
 with Complex_Convex_Continuation;
 with Parameter_Homotopy_State;
 with Standard_PolySys_Container;
@@ -676,6 +696,212 @@ function use_sweep ( job : integer32;
     return 0;
   end Standard_Real_Sweep;
 
+  function DoblDobl_Real_Sweep return integer32 is
+
+  -- DESCRIPTION :
+  --   Runs through the container for the solution in double double
+  --   precision and runs a natural-parameter sweep defined by the
+  --   family of polynomial systems in the dobldobl systems container.
+  --   The sweep runs from the start to the target values of the
+  --   parameters, or until a singularity is encountered.
+
+    lp : constant DoblDobl_Complex_Poly_Systems.Link_to_Poly_Sys
+       := DoblDobl_PolySys_Container.Retrieve;
+    sols : DoblDobl_Complex_Solutions.Solution_List
+         := DoblDobl_Solutions_Container.Retrieve;
+    nb_equ : constant integer32
+           := Parameter_Homotopy_State.Get_Number_of_Equations;
+    nb_var : constant integer32
+           := Parameter_Homotopy_State.Get_Number_of_Variables;
+    nb_par : constant integer32
+           := Parameter_Homotopy_State.Get_Number_of_Parameters;
+    indpar : constant Standard_Integer_Vectors.Link_to_Vector
+           := Parameter_Homotopy_State.Get_Indices;
+    startv : DoblDobl_Complex_Vectors.Link_to_Vector
+           := Parameter_Homotopy_State.Get_Start;
+    target : DoblDobl_Complex_Vectors.Link_to_Vector
+           := Parameter_Homotopy_State.Get_Target;
+    dx : DoblDobl_Complex_Vectors.Vector(1..nb_var);
+    rx,rdx : Double_Double_Vectors.Vector(1..nb_var);
+
+    use DoblDobl_Complex_Numbers;
+    use DoblDobl_Complex_Solutions;
+    use DoblDobl_Quad_Sweepers;
+
+    tmp : Solution_List := sols;
+    ls : Link_to_Solution;
+    isreal : constant boolean := DoblDobl_Complex_to_Real_Poly.Is_Real(lp.all);
+    realvals : boolean; -- true if everything is real
+
+    spf : DoblDobl_Complex_Poly_SysFun.Eval_Poly_Sys(lp'range);
+    sjm : DoblDobl_Complex_Jaco_Matrices.Jaco_Mat(lp'range,1..nb_var);
+    sjf : DoblDobl_Complex_Jaco_Matrices.Eval_Jaco_Mat(lp'range,1..nb_var);
+    rp : Double_Double_Poly_Systems.Poly_Sys(lp'range);
+    rpf : Double_Double_Poly_SysFun.Eval_Poly_Sys(lp'range);
+    rjm : Double_Double_Jaco_Matrices.Jaco_Mat(lp'range,1..nb_var);
+    rjf : Double_Double_Jaco_Matrices.Eval_Jaco_Mat(lp'range,1..nb_var);
+    zero : constant double_double := create(0.0);
+    one : constant double_double := create(1.0);
+
+  begin
+    spf := DoblDobl_Complex_Poly_SysFun.Create(lp.all);
+    sjm := DoblDobl_Complex_Jaco_Matrices.Create(lp.all);
+    sjf := DoblDobl_Complex_Jaco_Matrices.Create(sjm);
+    if isreal then
+      rp := DoblDobl_Complex_to_Real_Poly.Convert_Complex_to_Real(lp.all);
+      rpf := Double_Double_Poly_SysFun.Create(rp);
+      rjm := Double_Double_Jaco_Matrices.Create(rp);
+      rjf := Double_Double_Jaco_Matrices.Create(rjm);
+    end if;
+    while not Is_Null(tmp) loop
+      ls := Head_Of(tmp);
+      ls.t := Create(integer(0));
+      if not isreal then
+        realvals := false;
+      else
+        if(IMAG_Part(startv(startv'first)) /= zero) then
+          realvals := false;
+        elsif(IMAG_Part(target(target'first)) /= zero) then
+          realvals := false;
+        else
+          realvals := DoblDobl_Solution_Diagnostics.Is_Real(ls.all,1.0E-14);
+        end if;
+      end if;
+      if realvals then
+        for i in ls.v'range loop
+          rx(i) := REAL_PART(ls.v(i));
+          rdx(i) := create(0.0);
+        end loop;
+       -- Silent_Real_Sweep
+        Start_Real_Sweep
+          (true,natural32(nb_equ),natural32(nb_var),one,rpf,rjf,rx,rdx);
+        for i in rx'range loop
+          ls.v(i) := Create(rx(i));
+        end loop;
+      else
+        for i in ls.v'range loop
+          dx(i) := Create(integer(0));
+        end loop;
+        Start_Complex_Sweep
+          (natural32(nb_equ),natural32(nb_var),one,spf,sjf,ls.v,dx);
+      end if;
+      tmp := Tail_Of(tmp);
+    end loop;
+    DoblDobl_Complex_Poly_SysFun.Clear(spf);
+    DoblDobl_Complex_Jaco_Matrices.Clear(sjm);
+    DoblDobl_Complex_Jaco_Matrices.Clear(sjf);
+    if isreal then
+      Double_Double_Poly_Systems.Clear(rp);
+      Double_Double_Poly_SysFun.Clear(rpf);
+      Double_Double_Jaco_Matrices.Clear(rjm);
+      Double_Double_Jaco_Matrices.Clear(rjf);
+    end if;
+    return 0;
+  end DoblDobl_Real_Sweep;
+
+  function QuadDobl_Real_Sweep return integer32 is
+
+  -- DESCRIPTION :
+  --   Runs through the container for the solution in quad double
+  --   precision and runs a natural-parameter sweep defined by the
+  --   family of polynomial systems in the quaddobl systems container.
+  --   The sweep runs from the start to the target values of the
+  --   parameters, or until a singularity is encountered.
+
+    lp : constant QuadDobl_Complex_Poly_Systems.Link_to_Poly_Sys
+       := QuadDobl_PolySys_Container.Retrieve;
+    sols : QuadDobl_Complex_Solutions.Solution_List
+         := QuadDobl_Solutions_Container.Retrieve;
+    nb_equ : constant integer32
+           := Parameter_Homotopy_State.Get_Number_of_Equations;
+    nb_var : constant integer32
+           := Parameter_Homotopy_State.Get_Number_of_Variables;
+    nb_par : constant integer32
+           := Parameter_Homotopy_State.Get_Number_of_Parameters;
+    indpar : constant Standard_Integer_Vectors.Link_to_Vector
+           := Parameter_Homotopy_State.Get_Indices;
+    startv : QuadDobl_Complex_Vectors.Link_to_Vector
+           := Parameter_Homotopy_State.Get_Start;
+    target : QuadDobl_Complex_Vectors.Link_to_Vector
+           := Parameter_Homotopy_State.Get_Target;
+    dx : QuadDobl_Complex_Vectors.Vector(1..nb_var);
+    rx,rdx : Quad_Double_Vectors.Vector(1..nb_var);
+
+    use QuadDobl_Complex_Numbers;
+    use QuadDobl_Complex_Solutions;
+    use QuadDobl_Quad_Sweepers;
+
+    tmp : Solution_List := sols;
+    ls : Link_to_Solution;
+    isreal : constant boolean := QuadDobl_Complex_to_Real_Poly.Is_Real(lp.all);
+    realvals : boolean; -- true if everything is real
+
+    spf : QuadDobl_Complex_Poly_SysFun.Eval_Poly_Sys(lp'range);
+    sjm : QuadDobl_Complex_Jaco_Matrices.Jaco_Mat(lp'range,1..nb_var);
+    sjf : QuadDobl_Complex_Jaco_Matrices.Eval_Jaco_Mat(lp'range,1..nb_var);
+    rp : Quad_Double_Poly_Systems.Poly_Sys(lp'range);
+    rpf : Quad_Double_Poly_SysFun.Eval_Poly_Sys(lp'range);
+    rjm : Quad_Double_Jaco_Matrices.Jaco_Mat(lp'range,1..nb_var);
+    rjf : Quad_Double_Jaco_Matrices.Eval_Jaco_Mat(lp'range,1..nb_var);
+    zero : constant quad_double := create(0.0);
+    one : constant quad_double := create(1.0);
+
+  begin
+    spf := QuadDobl_Complex_Poly_SysFun.Create(lp.all);
+    sjm := QuadDobl_Complex_Jaco_Matrices.Create(lp.all);
+    sjf := QuadDobl_Complex_Jaco_Matrices.Create(sjm);
+    if isreal then
+      rp := QuadDobl_Complex_to_Real_Poly.Convert_Complex_to_Real(lp.all);
+      rpf := Quad_Double_Poly_SysFun.Create(rp);
+      rjm := Quad_Double_Jaco_Matrices.Create(rp);
+      rjf := Quad_Double_Jaco_Matrices.Create(rjm);
+    end if;
+    while not Is_Null(tmp) loop
+      ls := Head_Of(tmp);
+      ls.t := Create(integer(0));
+      if not isreal then
+        realvals := false;
+      else
+        if(IMAG_Part(startv(startv'first)) /= zero) then
+          realvals := false;
+        elsif(IMAG_Part(target(target'first)) /= zero) then
+          realvals := false;
+        else
+          realvals := QuadDobl_Solution_Diagnostics.Is_Real(ls.all,1.0E-14);
+        end if;
+      end if;
+      if realvals then
+        for i in ls.v'range loop
+          rx(i) := REAL_PART(ls.v(i));
+          rdx(i) := create(0.0);
+        end loop;
+       -- Silent_Real_Sweep
+        Start_Real_Sweep
+          (true,natural32(nb_equ),natural32(nb_var),one,rpf,rjf,rx,rdx);
+        for i in rx'range loop
+          ls.v(i) := Create(rx(i));
+        end loop;
+      else
+        for i in ls.v'range loop
+          dx(i) := Create(integer(0));
+        end loop;
+        Start_Complex_Sweep
+          (natural32(nb_equ),natural32(nb_var),one,spf,sjf,ls.v,dx);
+      end if;
+      tmp := Tail_Of(tmp);
+    end loop;
+    QuadDobl_Complex_Poly_SysFun.Clear(spf);
+    QuadDobl_Complex_Jaco_Matrices.Clear(sjm);
+    QuadDobl_Complex_Jaco_Matrices.Clear(sjf);
+    if isreal then
+      Quad_Double_Poly_Systems.Clear(rp);
+      Quad_Double_Poly_SysFun.Clear(rpf);
+      Quad_Double_Jaco_Matrices.Clear(rjm);
+      Quad_Double_Jaco_Matrices.Clear(rjf);
+    end if;
+    return 0;
+  end QuadDobl_Real_Sweep;
+
   function Job11 return integer32 is -- real natural-parameter sweep
 
     v_a : constant C_Integer_Array
@@ -685,6 +911,8 @@ function use_sweep ( job : integer32;
   begin
     case precision is
       when 0 => return Standard_Real_Sweep;
+      when 1 => return DoblDobl_Real_Sweep;
+      when 2 => return QuadDobl_Real_Sweep;
       when others => null;
     end case;
     return 0;
