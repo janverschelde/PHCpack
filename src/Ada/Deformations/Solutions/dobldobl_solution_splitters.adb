e108a028a8b5b083e35ecaa1c46dc9da94b772be
janv@uic.edu <jan@math.uic.edu>
2015-11-17 21:10:07 -0600

    added versions in double double and quad double precision of splitters of solution lists as needed in the cascades of homotopies

---
+++ src/Ada/Deformations/Solutions/dobldobl_solution_splitters.adb
@@ -0,0 +1,129 @@
+with Standard_Natural_Numbers;            use Standard_Natural_Numbers;
+with Standard_Natural_Numbers_io;         use Standard_Natural_Numbers_io;
+with Standard_Integer_Numbers_io;         use Standard_Integer_Numbers_io;
+with Double_Double_Numbers;               use Double_Double_Numbers;
+with Double_Double_Numbers_io;            use Double_Double_Numbers_io;
+with DoblDobl_Complex_Numbers;            use DoblDobl_Complex_Numbers;
+with Standard_Natural_Vectors;            use Standard_Natural_Vectors;
+with DoblDobl_Condition_Tables;           use DoblDobl_Condition_Tables;
+with DoblDobl_Complex_Solutions_io;       use DoblDobl_Complex_Solutions_io;
+with DoblDobl_Solution_Filters;           use DoblDobl_Solution_Filters;
+
+package body DoblDobl_Solution_Splitters is
+
+  procedure Filter_and_Split_Solutions
+              ( file : in file_type; sols : in Solution_List;
+                n,k : in integer32; tol : in double_float;
+                sols0,sols1 : out Solution_List ) is
+
+    tar_sols : constant Solution_List
+             := On_Target_Filter(sols,Create(integer(1)),tol);
+    van_sols : constant Solution_List := Vanishing_Filter(tar_sols,tol);
+
+  begin
+    new_line(file);
+    put(file,"FILTERED "); put(file,Length_Of(sols),1);
+    put(file," computed vectors and found "); 
+    put(file,Length_Of(van_sols),1);
+    put_line(file," vanishing solutions.");
+    if k = 0 then
+      sols1 := van_sols;
+      if not Is_Null(van_sols)
+       then put(file,Length_Of(sols1),natural32(Head_Of(sols1).n),sols1);
+      end if;
+    else
+      sols0 := Zero_Component_Filter(van_sols,natural32(n+k),tol);
+      if Is_Null(sols0) then
+        put(file,"NO SOLUTIONS WITH zz");
+        put(file,k,1); put_line(file," = 0.");
+      else
+        put(file,"THE SOLUTIONS WITH zz");
+        put(file,k,1); put_line(file," = 0 :");
+        put(file,Length_Of(sols0),natural32(Head_Of(sols0).n),sols0);
+      end if;
+      sols1 := Free_Component_Filter(van_sols,natural32(n+k),tol);
+      if Is_Null(sols1) then
+        put(file,"NO SOLUTIONS WITH zz");
+        put(file,k,1); put_line(file," /= 0.");
+      else
+        put(file,"THE SOLUTIONS WITH zz");
+        put(file,k,1); put_line(file," /= 0 :");
+        put(file,Length_Of(sols1),natural32(Head_Of(sols1).n),sols1);
+      end if;
+    end if;
+  end Filter_and_Split_Solutions;
+
+  procedure Filter_and_Split_Solutions
+                ( sols : in Solution_List;
+                  n,k : in integer32; tol : in double_float;
+                  sols0,sols1 : out Solution_List ) is
+
+    tar_sols : constant Solution_List
+             := On_Target_Filter(sols,Create(integer(1)),tol);
+    van_sols : constant Solution_List := Vanishing_Filter(tar_sols,tol);
+
+  begin
+    if k = 0 then
+      sols1 := van_sols;
+    else
+      sols0 := Zero_Component_Filter(van_sols,natural32(n+k),tol);
+      sols1 := Free_Component_Filter(van_sols,natural32(n+k),tol);
+    end if;
+  end Filter_and_Split_Solutions;
+
+  procedure S_Singular_Filter
+              ( sols : in Solution_List; tol : in double_float;
+                sinsols,regsols : out Solution_List ) is
+
+    sin_last,reg_last : Solution_List;
+    tmp : Solution_List := sols;
+    ls : Link_to_Solution;
+
+  begin
+    for i in 1..Length_Of(sols) loop
+      ls := Head_Of(tmp);
+      if ((ls.err <= tol) or (ls.res <= tol)) then
+        if ls.rco > tol
+         then Append(regsols,reg_last,ls.all);
+         else Append(sinsols,sin_last,ls.all);
+        end if;
+      end if;
+      tmp := Tail_Of(tmp);
+    end loop;
+  end S_Singular_Filter;
+
+  procedure R_Singular_Filter
+              ( file : in file_type;
+                sols : in Solution_List; tol : in double_float;
+                sinsols,regsols : out Solution_List ) is
+
+    sin_last,reg_last : Solution_List;
+    tmp : Solution_List := sols;
+    ls : Link_to_Solution;
+    table : Vector(0..15) := Create(15);
+
+  begin
+    for i in 1..Length_Of(sols) loop
+      ls := Head_Of(tmp);
+      put(file,"Solution "); put(file,i,1); put(file," : ");
+      put(file,"  err ="); put(file,ls.err,3);
+      put(file,"  rco ="); put(file,ls.rco,3);
+      put(file,"  res ="); put(file,ls.res,3);
+      if ((ls.err <= tol) or (ls.res <= tol)) then
+        if ls.rco <= tol then
+          put_line(file,"  singular");
+          Append(sinsols,sin_last,ls.all);
+        else
+          put_line(file,"  regular");
+          Append(regsols,reg_last,ls.all);
+        end if;
+      else
+        put_line(file,"  no solution");
+      end if;
+      Update_Condition(table,ls.all);
+      tmp := Tail_Of(tmp);
+    end loop;
+    Write_Condition_Table(file,table);
+  end R_Singular_Filter;
+
+end DoblDobl_Solution_Splitters;
---
+++ src/Ada/Deformations/Solutions/dobldobl_solution_splitters.ads
@@ -0,0 +1,61 @@
+with text_io;                             use text_io;
+with Standard_Integer_Numbers;            use Standard_Integer_Numbers;
+with Standard_Floating_Numbers;           use Standard_Floating_Numbers;
+with DoblDobl_Complex_Solutions;          use DoblDobl_Complex_Solutions;
+
+package DoblDobl_Solution_Splitters is
+
+-- DESCRIPTION :
+--   A splitter divides a solution list in two, depending on
+--    1) whether a solution has a zero slack variable; or
+--    2) whether a solution is regular or singular.
+--   The operations in this package are drivers to the filters
+--   in Standard_Solution_Filters.
+
+  procedure Filter_and_Split_Solutions
+                ( file : in file_type; sols : in Solution_List;
+                  n,k : in integer32; tol : in double_float;
+                  sols0,sols1 : out Solution_List );
+
+  procedure Filter_and_Split_Solutions
+                ( sols : in Solution_List;
+                  n,k : in integer32; tol : in double_float;
+                  sols0,sols1 : out Solution_List );
+
+  -- DESCRIPTION :
+  --   Removes the spurious solutions from the list sols and splits it
+  --   into two lists, depending on the (n+k)-th value to be zero.
+
+  -- ON ENTRY :
+  --   file       to write the results on, if omitted, then silent;
+  --   sols       computed solutions at the end of the paths;
+  --   n          original dimension;
+  --   k          number of random slices and variables added;
+  --   tol        everything that has absolute value less than tol is zero.
+
+  -- ON RETURN :
+  --   sols0      solutions with n-th component less than tol;
+  --   sols1      solutions with n-th component larger than tol.
+
+  procedure S_Singular_Filter
+              ( sols : in Solution_List; tol : in double_float;
+                sinsols,regsols : out Solution_List );
+
+  procedure R_Singular_Filter
+              ( file : in file_type;
+                sols : in Solution_List; tol : in double_float;
+                sinsols,regsols : out Solution_List );
+
+  -- DESCRIPTION :
+  --   Removes those solutions whose conditions number is too large.
+
+  -- ON ENTRY :
+  --   file     for intermediate output;
+  --   sols     list of solutions;
+  --   tol      tolerance on inverse condition number rco.
+
+  -- ON RETURN :
+  --   sinsols  solutions with rco <= tol.
+  --   regsols  solutions with rco > tol.
+
+end DoblDobl_Solution_Splitters;
