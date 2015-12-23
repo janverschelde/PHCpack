2ca9394913e0ce4e89932af1b6b1e6d74345789c
janv@uic.edu <jan@math.uic.edu>
2015-12-23 11:38:17 -0600

    added extra functionality to the Python module to deal with positive dimensional solution sets

--- src/Python/PHCpy/phcpy/sets.py
+++ src/Python/PHCpy/phcpy/sets.py
@@ -335,10 +335,10 @@ def test_cascade():
     for sol in ws1f2:
         print sol
 
-def decomposition(deg):
+def standard_decomposition(deg):
     """
-    Returns the decomposition as a list of labels
-    of witness points on the components.
+    Returns the decomposition as a list of labels of witness points
+    on the components, computed in standard double precision.
     """
     from phcpy.phcpy2c import py2c_factor_number_of_components
     from phcpy.phcpy2c import py2c_factor_witness_points_of_component
@@ -350,6 +350,54 @@ def decomposition(deg):
         tsd = py2c_factor_trace_sum_difference(deg, i, len(compnt), compnt)
         result.append((eval(compnt), tsd))
     return result
+
+def dobldobl_decomposition(deg):
+    """
+    Returns the decomposition as a list of labels of witness points
+    on the components, computed in double double precision.
+    """
+    from phcpy.phcpy2c import py2c_factor_number_of_dobldobl_components
+    from phcpy.phcpy2c import py2c_factor_witness_points_of_dobldobl_component
+    from phcpy.phcpy2c import py2c_factor_dobldobl_trace_sum_difference as dtf
+    nbcmp = py2c_factor_number_of_dobldobl_components()
+    result = []
+    for i in range(1, nbcmp+1):
+        compnt = py2c_factor_witness_points_of_dobldobl_component(deg, i)
+        tsd = dtf(deg, i, len(compnt), compnt)
+        result.append((eval(compnt), tsd))
+    return result
+
+def quaddobl_decomposition(deg):
+    """
+    Returns the decomposition as a list of labels of witness points
+    on the components, computed in quad double precision.
+    """
+    from phcpy.phcpy2c import py2c_factor_number_of_quaddobl_components
+    from phcpy.phcpy2c import py2c_factor_witness_points_of_quaddobl_component
+    from phcpy.phcpy2c import py2c_factor_quaddobl_trace_sum_difference as qtf
+    nbcmp = py2c_factor_number_of_quaddobl_components()
+    result = []
+    for i in range(1, nbcmp+1):
+        compnt = py2c_factor_witness_points_of_quaddobl_component(deg, i)
+        tsd = qtf(deg, i, len(compnt), compnt)
+        result.append((eval(compnt), tsd))
+    return result
+
+def decomposition(deg, precision='d'):
+    """
+    Returns the decomposition as a list of labels of witness points
+    on the components, computed in precision 'd', 'dd', or 'qd',
+    respectively for double, double double, or quad double.
+    """
+    if(precision == 'd'):
+        return standard_decomposition(deg)
+    elif(precision == 'dd'):
+        return dobldobl_decomposition(deg)
+    elif(precision == 'qd'):
+        return quaddobl_decomposition(deg)
+    else:
+        print 'wrong level of precision'
+        return None
 
 def standard_monodromy_breakup(embsys, esols, dim, verbose=True, nbloops=0):
     """
@@ -374,6 +422,7 @@ def standard_monodromy_breakup(embsys, esols, dim, verbose=True, nbloops=0):
     from phcpy.phcpy2c import py2c_factor_permutation_after_loop
     from phcpy.phcpy2c import py2c_factor_number_of_components
     from phcpy.phcpy2c import py2c_factor_update_decomposition
+    from phcpy.phcpy2c import py2c_solcon_write_standard_solutions
     from phcpy.phcpy2c import py2c_solcon_clear_standard_solutions
     from phcpy.interface import store_standard_solutions
     if(verbose):
@@ -385,7 +434,8 @@ def standard_monodromy_breakup(embsys, esols, dim, verbose=True, nbloops=0):
         print 'dim =', dim
     store_standard_solutions(nvar, esols)
     py2c_factor_assign_labels(nvar, deg)
-    # py2c_solcon_write_solutions()
+    if(verbose):
+        py2c_solcon_write_standard_solutions()
     py2c_factor_initialize_sampler(dim)
     if(nbloops == 0):
         strnbloops = raw_input('give the maximum number of loops : ')
@@ -448,18 +498,20 @@ def dobldobl_monodromy_breakup(embsys, esols, dim, verbose=True, nbloops=0):
     from phcpy.phcpy2c import py2c_factor_permutation_after_dobldobl_loop
     from phcpy.phcpy2c import py2c_factor_number_of_dobldobl_components
     from phcpy.phcpy2c import py2c_factor_update_dobldobl_decomposition
+    from phcpy.phcpy2c import py2c_solcon_write_dobldobl_solutions
     from phcpy.phcpy2c import py2c_solcon_clear_dobldobl_solutions
     from phcpy.interface import store_dobldobl_solutions
     if(verbose):
-        print '... applying monodromy factorization ...'
-    py2c_factor_set_to_mute()
+        print '... applying monodromy factorization with double doubles ...'
+    # py2c_factor_set_to_mute()
     deg = len(esols)
     nvar = len(embsys)
     if(verbose):
-        print 'dim =', dim
+        print 'nvar =', nvar, 'dim =', dim, 'deg =', deg
     store_dobldobl_solutions(nvar, esols)
     py2c_factor_dobldobl_assign_labels(nvar, deg)
-    # py2c_solcon_write_dobldobl_solutions()
+    if(verbose):
+        py2c_solcon_write_dobldobl_solutions()
     py2c_factor_initialize_dobldobl_sampler(dim)
     if(nbloops == 0):
         strnbloops = raw_input('give the maximum number of loops : ')
@@ -494,7 +546,7 @@ def dobldobl_monodromy_breakup(embsys, esols, dim, verbose=True, nbloops=0):
         nb1 = py2c_factor_number_of_dobldobl_components()
         if(verbose):
             print 'number of factors : %d -> %d' % (nb0, nb1)
-            print 'decomposition :', decomposition(deg)
+            print 'decomposition :', decomposition(deg, 'dd')
         if(done == 1):
             break
         py2c_factor_restore_dobldobl_solutions()
@@ -522,10 +574,11 @@ def quaddobl_monodromy_breakup(embsys, esols, dim, verbose=True, nbloops=0):
     from phcpy.phcpy2c import py2c_factor_permutation_after_quaddobl_loop
     from phcpy.phcpy2c import py2c_factor_number_of_quaddobl_components
     from phcpy.phcpy2c import py2c_factor_update_quaddobl_decomposition
+    from phcpy.phcpy2c import py2c_solcon_write_quaddobl_solutions
     from phcpy.phcpy2c import py2c_solcon_clear_quaddobl_solutions
     from phcpy.interface import store_quaddobl_solutions
     if(verbose):
-        print '... applying monodromy factorization ...'
+        print '... applying monodromy factorization with quad doubles ...'
     py2c_factor_set_to_mute()
     deg = len(esols)
     nvar = len(embsys)
@@ -533,7 +586,8 @@ def quaddobl_monodromy_breakup(embsys, esols, dim, verbose=True, nbloops=0):
         print 'dim =', dim
     store_quaddobl_solutions(nvar, esols)
     py2c_factor_quaddobl_assign_labels(nvar, deg)
-    # py2c_solcon_write_quaddobl_solutions()
+    if(verbose):
+        py2c_solcon_write_quaddobl_solutions()
     py2c_factor_initialize_quaddobl_sampler(dim)
     if(nbloops == 0):
         strnbloops = raw_input('give the maximum number of loops : ')
@@ -568,12 +622,12 @@ def quaddobl_monodromy_breakup(embsys, esols, dim, verbose=True, nbloops=0):
         nb1 = py2c_factor_number_of_quaddobl_components()
         if(verbose):
             print 'number of factors : %d -> %d' % (nb0, nb1)
-            print 'decomposition :', decomposition(deg)
+            print 'decomposition :', decomposition(deg, 'qd')
         if(done == 1):
             break
         py2c_factor_restore_quaddobl_solutions()
 
-def monodromy_breakup(embsys, esols, dim, verbose=True, nbloops=0, prc='d'):
+def monodromy_breakup(embsys, esols, dim, verbose=True, nbloops=0, prec='d'):
     """
     Applies the monodromy breakup algorithm to factor the d-dimensional
     set represented by the embedded system e and its solutions esols.
@@ -581,27 +635,39 @@ def monodromy_breakup(embsys, esols, dim, verbose=True, nbloops=0, prc='d'):
     If nbloops equals zero, then the user is prompted to give
     the maximum number of loops.
     Three different levels of precision are supported: double precision 'd'
-    (for the value for prc) is the default, the two other precisions are
+    (for the value for prec) is the default, the two other precisions are
     double double precision 'dd' and quad double precision 'qd'.
     """
-    if(prc == 'd'):
+    if(prec == 'd'):
         standard_monodromy_breakup(embsys, esols, dim, verbose, nbloops)
-    elif(prc == 'dd'):
+    elif(prec == 'dd'):
         dobldobl_monodromy_breakup(embsys, esols, dim, verbose, nbloops)
-    elif(prc == 'dd'):
+    elif(prec == 'qd'):
         quaddobl_monodromy_breakup(embsys, esols, dim, verbose, nbloops)
     else:
         print 'wrong argument for precision'
 
-def factor(dim, witsys, witsols, verbose=True, nbloops=20):
+def factor(dim, witsys, witsols, verbose=True, nbloops=20, precision='d'):
     """
     Applies monodromy to factor an equidimensional algebraic set,
     given as a witness sets, with the embedded polynomials in witsys,
     and corresponding generic points in witsols.
     The dimension of the algebraic set is given in dim.
+    The default precision is double 'd'.  Other valid values for precision
+    are 'dd' for double double, or 'qd' for quad double.
     """
-    standard_monodromy_breakup(witsys, witsols, dim, verbose, nbloops)
-    return decomposition(len(witsols))
+    if(precision == 'd'):
+        standard_monodromy_breakup(witsys, witsols, dim, verbose, nbloops)
+        return decomposition(len(witsols))
+    elif(precision == 'dd'):
+        dobldobl_monodromy_breakup(witsys, witsols, dim, verbose, nbloops)
+        return decomposition(len(witsols), 'dd')
+    elif(precision == 'dd'):
+        quaddobl_monodromy_breakup(witsys, witsols, dim, verbose, nbloops)
+        return decomposition(len(witsols), 'qd')
+    else:
+        print 'wrong level of precision'
+        return None
 
 def test_monodromy(prc='d'):
     """
@@ -617,7 +683,8 @@ def test_monodromy(prc='d'):
     sols = solve(embsys, silent=True, precision=prc)
     # for sol in sols: print sol
     print 'the degree is', len(sols)
-    monodromy_breakup(embsys, sols, 1, prc)
+    print 'prc = ', prc
+    monodromy_breakup(embsys, sols, 1, prec=prc)
 
 def test_factor():
     """
