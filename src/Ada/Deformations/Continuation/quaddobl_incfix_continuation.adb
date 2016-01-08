99aae47b4035e5b26b27f4269482574d3283b83b
janv@uic.edu <jan@math.uic.edu>
2016-01-08 13:41:40 -0600

    populated the stubs for polyhedral end games in double double and quad double precision with actual code

--- src/Ada/Deformations/Continuation/quaddobl_incfix_continuation.adb
+++ src/Ada/Deformations/Continuation/quaddobl_incfix_continuation.adb
@@ -209,8 +209,36 @@ package body QuadDobl_IncFix_Continuation is
                  v : in out Quad_Double_VecVecs.VecVec;
                  errv : in out Quad_Double_Vectors.Vector;
                  target : in Complex_Number := Create(integer(1)) ) is
+
+    res,res_last : Solution_List;
+    tmp : Solution_List := sols;
+    patpp : constant Pred_Pars := Continuation_Parameters.Create_for_Path;
+    endpp : constant Pred_Pars := Continuation_Parameters.Create_End_Game;
+    patcp : constant Corr_Pars := Continuation_Parameters.Create_for_Path;
+    endcp : constant Corr_Pars := Continuation_Parameters.Create_End_Game;
+    order : constant integer32
+          := integer32(Continuation_Parameters.endext_order);
+
+    procedure Continue_along_Path is
+      new Linear_Single_Normal_Silent_Continue(Norm,H,dH,dH);
+
+    procedure Continue_End_Game is
+      new Linear_Single_Conditioned_Silent_Continue(Norm,H,dH,dH);
+
   begin
-    null;
+    for i in w'range loop
+      declare
+        ls : constant Link_to_Solution := Head_Of(tmp);
+        s : Solu_Info := Deep_Create(ls.all);
+      begin
+        Continue_along_Path(s,target,tol,false,patpp,patcp);
+        Continue_End_Game(s,target,tol,false,order,
+                          w(i),v(i),errv(i),endpp,endcp);
+        Append(res,res_last,Deep_Create(s));
+      end;
+      tmp := Tail_Of(tmp);
+    end loop;
+    Copy(res,sols); Clear(res);
   end Silent_Toric_Continue;
 
   procedure Reporting_Toric_Continue
@@ -220,8 +248,55 @@ package body QuadDobl_IncFix_Continuation is
                  v : in out Quad_Double_VecVecs.VecVec;
                  errv : in out Quad_Double_Vectors.Vector;
                  target : in Complex_Number := Create(integer(1)) ) is
+
+    res,res_last : Solution_List;
+    tmp : Solution_List := sols;
+    patpp : constant Pred_Pars := Continuation_Parameters.Create_for_Path;
+    endpp : constant Pred_Pars := Continuation_Parameters.Create_End_Game;
+    patcp : constant Corr_Pars := Continuation_Parameters.Create_for_Path;
+    endcp : constant Corr_Pars := Continuation_Parameters.Create_End_Game;
+    tol_zero : constant double_float := endcp.epsaf;
+    order : constant integer32
+          := integer32(Continuation_Parameters.endext_order);
+    len : constant natural32 := Length_Of(sols);
+    cnt,nbfail,nbregu,nbsing,kind : natural32 := 0;
+
+    procedure Continue_along_Path is
+      new Linear_Single_Normal_Reporting_Continue(Norm,H,dH,dH);
+
+    procedure Continue_End_Game is
+      new Linear_Single_Conditioned_Reporting_Continue(Norm,H,dH,dH);
+
   begin
-    null;
+    new_line(file);
+    put_line(file,"THE SOLUTIONS :");
+    put(file,len,1); put(file," ");
+    put(file,Head_Of(sols).n,1); new_line(file);
+    for i in 1..76 loop put(file,"="); end loop;
+    new_line(file); flush(file);
+    for i in 1..integer32(len) loop
+      declare
+        ls : constant Link_to_Solution := Head_Of(tmp);
+        s : Solu_Info := Deep_Create(ls.all);
+      begin
+        Continue_along_Path(file,s,target,tol,false,patpp,patcp);
+        Continue_End_Game(file,s,target,tol,false,
+                          order,w(i),v(i),errv(i),endpp,endcp);
+        Write_Next_Solution
+          (file,cnt,s,tol_zero,tol_zero,nbfail,nbregu,nbsing,kind);
+        text_io.flush(file);
+        Append(res,res_last,Deep_Create(s));
+      end;
+      tmp := Tail_Of(tmp);
+    end loop;
+    Copy(res,sols); Clear(res);
+    put(file,"Number of path failures      : ");
+    put(file,nbfail,1); new_line(file);
+    put(file,"Number of regular solutions  : ");
+    put(file,nbregu,1); new_line(file);
+    put(file,"Number of singular solutions : ");
+    put(file,nbsing,1); new_line(file);
+    new_line(file);
   end Reporting_Toric_Continue;
 
 end QuadDobl_IncFix_Continuation;
