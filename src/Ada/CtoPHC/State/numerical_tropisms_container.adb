ea3cc52183e07e4bfb011a7e203de362f62078ad
janv@uic.edu <jan@math.uic.edu>
2016-01-20 09:22:56 -0600

    new container package to pass numerically computed tropisms from Ada to C

---
+++ src/Ada/CtoPHC/State/numerical_tropisms_container.adb
@@ -0,0 +1,214 @@
+package body Numerical_Tropisms_Container is
+
+-- INTERNAL DATA :
+
+  wnd : Standard_Integer_Vectors.Link_to_Vector;
+  st_dir : Standard_Floating_VecVecs.Link_to_VecVec;
+  dd_dir : Double_Double_VecVecs.Link_to_VecVec;
+  qd_dir : Quad_Double_VecVecs.Link_to_VecVec;
+  st_err : Standard_Floating_Vectors.Link_to_Vector;
+  dd_err : Double_Double_Vectors.Link_to_Vector;
+  qd_err : Quad_Double_Vectors.Link_to_Vector;
+
+-- CONSTRUTORS :
+
+  procedure Standard_Initialize 
+              ( w : in Standard_Integer_Vectors.Vector;
+                v : in Standard_Floating_VecVecs.VecVec;
+                e : in Standard_Floating_Vectors.Vector ) is
+  begin
+    wnd := new Standard_Integer_Vectors.Vector'(w);
+    st_dir := new Standard_Floating_VecVecs.VecVec(v'range);
+    for i in v'range loop
+      st_dir(i) := new Standard_Floating_Vectors.Vector'(v(i).all);
+    end loop;
+    st_err := new Standard_Floating_Vectors.Vector'(e);
+  end Standard_Initialize;
+
+  procedure DoblDobl_Initialize 
+              ( w : in Standard_Integer_Vectors.Vector;
+                v : in Double_Double_VecVecs.VecVec;
+                e : in Double_Double_Vectors.Vector ) is
+  begin
+    wnd := new Standard_Integer_Vectors.Vector'(w);
+    dd_dir := new Double_Double_VecVecs.VecVec(v'range);
+    for i in v'range loop
+      dd_dir(i) := new Double_Double_Vectors.Vector'(v(i).all);
+    end loop;
+    dd_err := new Double_Double_Vectors.Vector'(e);
+  end DoblDobl_Initialize;
+
+  procedure QuadDobl_Initialize 
+              ( w : in Standard_Integer_Vectors.Vector;
+                v : in Quad_Double_VecVecs.VecVec;
+                e : in Quad_Double_Vectors.Vector ) is
+  begin
+    wnd := new Standard_Integer_Vectors.Vector'(w);
+    qd_dir := new Quad_Double_VecVecs.VecVec(v'range);
+    for i in v'range loop
+      qd_dir(i) := new Quad_Double_Vectors.Vector'(v(i).all);
+    end loop;
+    qd_err := new Quad_Double_Vectors.Vector'(e);
+  end QuadDobl_Initialize;
+
+  procedure Store_Standard_Tropism
+              ( k : integer32; w : in integer32;
+                v : in Standard_Floating_Vectors.Vector;
+                e : in double_float ) is
+  begin
+    wnd(k) := w;
+    for i in v'range loop
+      st_dir(k)(i) := v(i);
+    end loop;
+    st_err(k) := e;
+  end Store_Standard_Tropism;
+
+  procedure Store_DoblDobl_Tropism
+              ( k : integer32; w : in integer32;
+                v : in Double_Double_Vectors.Vector;
+                e : in double_double ) is
+  begin
+    wnd(k) := w;
+    for i in v'range loop
+      dd_dir(k)(i) := v(i);
+    end loop;
+    dd_err(k) := e;
+  end Store_DoblDobl_Tropism;
+
+  procedure Store_QuadDobl_Tropism
+              ( k : integer32; w : in integer32;
+                v : in Quad_Double_Vectors.Vector;
+                e : in quad_double ) is
+  begin
+    wnd(k) := w;
+    for i in v'range loop
+      qd_dir(k)(i) := v(i);
+    end loop;
+    qd_err(k) := e;
+  end Store_QuadDobl_Tropism;
+
+-- SELECTORS :
+
+  procedure Standard_Retrieve
+              ( w : out Standard_Integer_Vectors.Link_to_Vector;
+                v : out Standard_Floating_VecVecs.Link_to_VecVec;
+                e : out Standard_Floating_Vectors.Link_to_Vector ) is
+  begin
+    w := wnd;
+    v := st_dir;
+    e := st_err;
+  end Standard_Retrieve;
+
+  procedure DoblDobl_Retrieve
+              ( w : out Standard_Integer_Vectors.Link_to_Vector;
+                v : out Double_Double_VecVecs.Link_to_VecVec;
+                e : out Double_Double_Vectors.Link_to_Vector ) is
+  begin
+    w := wnd;
+    v := dd_dir;
+    e := dd_err;
+  end DoblDobl_Retrieve;
+
+  procedure QuadDobl_Retrieve
+              ( w : out Standard_Integer_Vectors.Link_to_Vector;
+                v : out Quad_Double_VecVecs.Link_to_VecVec;
+                e : out Quad_Double_Vectors.Link_to_Vector ) is
+  begin
+    w := wnd;
+    v := qd_dir;
+    e := qd_err;
+  end QuadDobl_Retrieve;
+
+  function Standard_Size return integer32 is
+
+    use Standard_Floating_VecVecs;
+
+  begin
+    if st_dir = null
+     then return 0;
+     else return st_dir'last;
+    end if;
+  end Standard_Size;
+
+  function DoblDobl_Size return integer32 is
+
+    use Double_Double_VecVecs;
+
+  begin
+    if dd_dir = null
+     then return 0;
+     else return dd_dir'last;
+    end if;
+  end DoblDobl_Size;
+
+  function QuadDobl_Size return integer32 is
+
+    use Quad_Double_VecVecs;
+
+  begin
+    if qd_dir = null
+     then return 0;
+     else return qd_dir'last;
+    end if;
+  end QuadDobl_Size;
+
+  procedure Standard_Retrieve_Tropism
+              ( k : in integer32; w : out integer32;
+                v : out Standard_Floating_Vectors.Vector;
+                e : out double_float ) is
+  begin
+    w := wnd(k);
+    for i in v'range loop
+      v(i) := st_dir(k)(i);
+    end loop;
+    e := st_err(k);
+  end Standard_Retrieve_Tropism;
+
+  procedure DoblDobl_Retrieve_Tropism
+              ( k : in integer32; w : out integer32;
+                v : out Double_Double_Vectors.Vector;
+                e : out double_double ) is
+  begin
+    w := wnd(k);
+    for i in v'range loop
+      v(i) := dd_dir(k)(i);
+    end loop;
+    e := dd_err(k);
+  end DoblDobl_Retrieve_Tropism;
+
+  procedure QuadDobl_Retrieve_Tropism
+              ( k : in integer32; w : out integer32;
+                v : out Quad_Double_Vectors.Vector;
+                e : out quad_double ) is
+  begin
+    w := wnd(k);
+    for i in v'range loop
+      v(i) := qd_dir(k)(i);
+    end loop;
+    e := qd_err(k);
+  end QuadDobl_Retrieve_Tropism;
+
+-- DESTRUCTORS :
+
+  procedure Standard_Clear is
+  begin
+    Standard_Integer_Vectors.Clear(wnd);
+    Standard_Floating_VecVecs.Deep_Clear(st_dir);
+    Standard_Floating_Vectors.Clear(st_err);
+  end Standard_Clear;
+
+  procedure DoblDobl_Clear is
+  begin
+    Standard_Integer_Vectors.Clear(wnd);
+    Double_Double_VecVecs.Deep_Clear(dd_dir);
+    Double_Double_Vectors.Clear(dd_err);
+  end DoblDobl_Clear;
+
+  procedure QuadDobl_Clear is
+  begin
+    Standard_Integer_Vectors.Clear(wnd);
+    Quad_Double_VecVecs.Deep_Clear(qd_dir);
+    Quad_Double_Vectors.Clear(qd_err);
+  end QuadDobl_Clear;
+
+end Numerical_Tropisms_Container;
---
+++ src/Ada/CtoPHC/State/numerical_tropisms_container.ads
@@ -0,0 +1,163 @@
+with Standard_Integer_Numbers;           use Standard_Integer_Numbers;
+with Standard_Floating_Numbers;          use Standard_Floating_Numbers;
+with Double_Double_Numbers;              use Double_Double_Numbers;
+with Quad_Double_Numbers;                use Quad_Double_Numbers;
+with Standard_Integer_Vectors;
+with Standard_Floating_VecVecs;
+with Standard_Floating_Vectors;
+with Double_Double_Vectors;
+with Double_Double_VecVecs;
+with Quad_Double_Vectors;
+with Quad_Double_VecVecs;
+
+package Numerical_Tropisms_Container is
+
+-- DESCRIPTION :
+--   A numerical tropisms is a numerical approximation for a tropism,
+--   computed typically via a polyhedral end game.
+--   The data to represent a numerical tropism is a triplet:
+--   1) an estimated winding number, stored as an integer;
+--   2) real floating-point coordinates for the exponents of
+--   the leading powers of the Puiseux series of the path; and
+--   3) one float: an estimate for the error on those exponents.
+--   This package provides a data structure to manage numerical tropisms
+--   in standard double, double double, and quad double precision.
+
+-- CONSTRUCTOES :
+
+  procedure Standard_Initialize 
+              ( w : in Standard_Integer_Vectors.Vector;
+                v : in Standard_Floating_VecVecs.VecVec;
+                e : in Standard_Floating_Vectors.Vector );
+  procedure DoblDobl_Initialize 
+              ( w : in Standard_Integer_Vectors.Vector;
+                v : in Double_Double_VecVecs.VecVec;
+                e : in Double_Double_Vectors.Vector );
+  procedure QuadDobl_Initialize 
+              ( w : in Standard_Integer_Vectors.Vector;
+                v : in Quad_Double_VecVecs.VecVec;
+                e : in Quad_Double_Vectors.Vector );
+
+  -- DESCRIPTION :
+  --   Initializes the data structures to store numerical tropisms
+  --   in standard double, double double, or quad double precision.
+
+  -- REQUIRED : w'range = v'range = e'range = 1..#solutions.
+
+  procedure Store_Standard_Tropism
+              ( k : integer32; w : in integer32;
+                v : in Standard_Floating_Vectors.Vector;
+                e : in double_float ); 
+
+  -- DESCRIPTION :
+  --   Stores the triplet (w, v, e) as the values as the k-th tropism,
+  --   computed in standard double precision.
+
+  -- REQUIRED : Standard_Initialize was executed and Standard_Size >= k.
+  --   Moreover, the range of v fits the allocated data structures
+  --   for the directions.
+
+  procedure Store_DoblDobl_Tropism
+              ( k : integer32; w : in integer32;
+                v : in Double_Double_Vectors.Vector;
+                e : in double_double ); 
+
+  -- DESCRIPTION :
+  --   Stores the triplet (w, v, e) as the values as the k-th tropism,
+  --   computed in double double precision.
+
+  -- REQUIRED : DoblDobl_Initialize was executed and DoblDobl_Size >= k.
+  --   Moreover, the range of v fits the allocated data structures
+  --   for the directions.
+
+  procedure Store_QuadDobl_Tropism
+              ( k : integer32; w : in integer32;
+                v : in Quad_Double_Vectors.Vector;
+                e : in quad_double ); 
+
+  -- DESCRIPTION :
+  --   Stores the triplet (w, v, e) as the values as the k-th tropism,
+  --   computed in quad double precision.
+
+  -- REQUIRED : QuadDobl_Initialize was executed and QuadDobl_Size >= k.
+  --   Moreover, the range of v fits the allocated data structures
+  --   for the directions.
+
+-- SELECTORS :
+
+  procedure Standard_Retrieve
+              ( w : out Standard_Integer_Vectors.Link_to_Vector;
+                v : out Standard_Floating_VecVecs.Link_to_VecVec;
+                e : out Standard_Floating_Vectors.Link_to_Vector );
+  procedure DoblDobl_Retrieve
+              ( w : out Standard_Integer_Vectors.Link_to_Vector;
+                v : out Double_Double_VecVecs.Link_to_VecVec;
+                e : out Double_Double_Vectors.Link_to_Vector );
+  procedure QuadDobl_Retrieve
+              ( w : out Standard_Integer_Vectors.Link_to_Vector;
+                v : out Quad_Double_VecVecs.Link_to_VecVec;
+                e : out Quad_Double_Vectors.Link_to_Vector );
+
+  -- DESCRIPTION :
+  --   Returns the entire data structure to store numerical tropisms
+  --   in standard double, double double, or quad double precision.
+
+  function Standard_Size return integer32;
+  function DoblDobl_Size return integer32;
+  function QuadDobl_Size return integer32;
+
+  -- DESCRIPTION :
+  --   Returns the number of numerical tropisms in standard double,
+  --   double double, or quad double precision.
+  --   Returns 0 if the data structures are cleared or not initialized.
+
+  procedure Standard_Retrieve_Tropism
+              ( k : in integer32; w : out integer32;
+                v : out Standard_Floating_Vectors.Vector;
+                e : out double_float );
+
+  -- DESCRIPTION :
+  --   Retrieves the triplet (w, v, e) as the values as the k-th tropism,
+  --   computed in standard double precision.
+
+  -- REQUIRED : Standard_Initialize was executed and Standard_Size >= k.
+  --   Moreover, the range of v fits the allocated data structures
+  --   for the directions.
+
+  procedure DoblDobl_Retrieve_Tropism
+              ( k : in integer32; w : out integer32;
+                v : out Double_Double_Vectors.Vector;
+                e : out double_double );
+
+  -- DESCRIPTION :
+  --   Retrieves the triplet (w, v, e) as the values as the k-th tropism,
+  --   computed in double double precision.
+
+  -- REQUIRED : DoblDobl_Initialize was executed and DoblDobl_Size >= k.
+  --   Moreover, the range of v fits the allocated data structures
+  --   for the directions.
+
+  procedure QuadDobl_Retrieve_Tropism
+              ( k : in integer32; w : out integer32;
+                v : out Quad_Double_Vectors.Vector;
+                e : out quad_double );
+
+  -- DESCRIPTION :
+  --   Retrieves the triplet (w, v, e) as the values as the k-th tropism,
+  --   computed in quad double precision.
+
+  -- REQUIRED : QuadDobl_Initialize was executed and QuadDobl_Size >= k.
+  --   Moreover, the range of v fits the allocated data structures
+  --   for the directions.
+
+-- DESTRUCTORS :
+
+  procedure Standard_Clear;
+  procedure DoblDobl_Clear;
+  procedure QuadDobl_Clear;
+
+  -- DESCRIPTION :
+  --   Deallocates the data structures used to store numerical tropisms
+  --   in standard double, double double, or quad double precision.
+
+end Numerical_Tropisms_Container;
