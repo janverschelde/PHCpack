1faea9311ca4821547257e25bde03c1c933b71f7
janv@uic.edu <jan@math.uic.edu>
2016-02-12 18:14:43 -0600

    ready for version 0.4.0 of phcpy

--- src/Python/PHCpy2/setup.py
+++ src/Python/PHCpy2/setup.py
@@ -17,12 +17,12 @@ setup(
     author_email = 'jan@math.uic.edu' ,
     description = 'a package for Polynomial Homotopy Continuation' ,
     url = 'https://github.com/janverschelde/PHCpack' ,
-    version = '0.3.9' ,
+    version = '0.4.0' ,
     packages = ['phcpy'] ,
     py_modules = ['phcpy/interface', 'phcpy/solver', 'phcpy/solutions', \
-                  'phcpy/trackers', 'phcpy/sweepers', 'phcpy/sets', \
-                  'phcpy/maps', 'phcpy/schubert' , 'phcpy/polytopes', \
-                  'phcpy/examples', 'phcpy/families' ] ,
+        'phcpy/trackers', 'phcpy/sweepers', 'phcpy/tuning', 'phcpy/tropisms', \
+        'phcpy/sets', 'phcpy/maps', 'phcpy/schubert' , 'phcpy/polytopes', \
+        'phcpy/phcwulf.py', 'phcpy/examples', 'phcpy/families' ] ,
     license = 'GNU GENERAL PUBLIC LICENSE version 2 or higher' ,
     data_files = [(get_python_lib()+'/phcpy', ['phcpy/phcpy2c2.so'])] ,
     platforms = ['linux2'] ,
