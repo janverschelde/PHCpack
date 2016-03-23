a93793b97b80120301c1b62c3503522e9f776eff
janv@uic.edu <jan@math.uic.edu>
2016-03-23 17:12:22 -0500

    added procedures to handle underflow gracefully in double double and quad double precision

--- src/Ada/Deformations/Newton/handle_underflow_gracefully.adb
+++ src/Ada/Deformations/Newton/handle_underflow_gracefully.adb
@@ -12,13 +12,43 @@ package body Handle_Underflow_Gracefully is
     when others => return 0.0;
   end Underflow_to_Zero;
 
-  function Get_Real_Part ( x : Complex_Number ) return double_float is
+  function Underflow_to_Zero ( f : double_double ) return double_double is
+
+    one : constant double_double := create(1.0);
+    zero : constant double_double := create(0.0);
+
+  begin
+    if f + one = one 
+     then return zero;
+     else return f;
+    end if;
+  exception 
+    when others => return zero;
+  end Underflow_to_Zero;
+
+  function Underflow_to_Zero ( f : quad_double ) return quad_double is
+
+    one : constant quad_double := create(1.0);
+    zero : constant quad_double := create(0.0);
+
+  begin
+    if f + one = one 
+     then return zero;
+     else return f;
+    end if;
+  exception 
+    when others => return zero;
+  end Underflow_to_Zero;
+
+  function Get_Real_Part
+             ( x : Standard_Complex_Numbers.Complex_Number ) 
+             return double_float is
 
   -- DESCRIPTION :
   --   Exception handlers wrap Standard_Complex_Numbers.REAL_PART,
   --   taking care of underflow.
  
-    f : constant double_float := REAL_PART(x);
+    f : constant double_float := Standard_Complex_Numbers.REAL_PART(x);
 
   begin
     return Underflow_to_Zero(f);
@@ -26,13 +56,49 @@ package body Handle_Underflow_Gracefully is
     when others => return 0.0;
   end Get_Real_Part;
 
-  function Get_Imaginary_Part ( x : Complex_Number ) return double_float is
+  function Get_Real_Part
+             ( x : DoblDobl_Complex_Numbers.Complex_Number ) 
+             return double_double is
+
+  -- DESCRIPTION :
+  --   Exception handlers wrap DoblDobl_Complex_Numbers.REAL_PART,
+  --   taking care of underflow.
+ 
+    zero : constant double_double := create(0.0);
+    f : constant double_double := DoblDobl_Complex_Numbers.REAL_PART(x);
+
+  begin
+    return Underflow_to_Zero(f);
+  exception 
+    when others => return zero;
+  end Get_Real_Part;
+
+  function Get_Real_Part
+             ( x : QuadDobl_Complex_Numbers.Complex_Number ) 
+             return quad_double is
+
+  -- DESCRIPTION :
+  --   Exception handlers wrap QuadDobl_Complex_Numbers.REAL_PART,
+  --   taking care of underflow.
+ 
+    zero : constant quad_double := create(0.0);
+    f : constant quad_double := QuadDobl_Complex_Numbers.REAL_PART(x);
+
+  begin
+    return Underflow_to_Zero(f);
+  exception 
+    when others => return zero;
+  end Get_Real_Part;
+
+  function Get_Imaginary_Part
+             ( x : Standard_Complex_Numbers.Complex_Number )
+             return double_float is
 
   -- DESCRIPTION :
   --   Exception handlers wrap Standard_Complex_Numbers.IMAG_PART,
   --   taking care of underflow.
 
-    f : constant double_float := IMAG_PART(x);
+    f : constant double_float := Standard_Complex_Numbers.IMAG_PART(x);
 
   begin
     return Underflow_to_Zero(f);
@@ -40,19 +106,101 @@ package body Handle_Underflow_Gracefully is
     when others => return 0.0;
   end Get_Imaginary_Part;
 
-  procedure Underflow_to_Zero ( x : in out Complex_Number ) is
+  function Get_Imaginary_Part
+             ( x : DoblDobl_Complex_Numbers.Complex_Number )
+             return double_double is
+
+  -- DESCRIPTION :
+  --   Exception handlers wrap DoblDobl_Complex_Numbers.IMAG_PART,
+  --   taking care of underflow.
+
+    zero : constant double_double := create(0.0);
+    f : constant double_double := DoblDobl_Complex_Numbers.IMAG_PART(x);
+
+  begin
+    return Underflow_to_Zero(f);
+  exception 
+    when others => return zero;
+  end Get_Imaginary_Part;
+
+  function Get_Imaginary_Part
+             ( x : QuadDobl_Complex_Numbers.Complex_Number )
+             return quad_double is
+
+  -- DESCRIPTION :
+  --   Exception handlers wrap QuadDobl_Complex_Numbers.IMAG_PART,
+  --   taking care of underflow.
+
+    zero : constant quad_double := create(0.0);
+    f : constant quad_double := QuadDobl_Complex_Numbers.IMAG_PART(x);
+
+  begin
+    return Underflow_to_Zero(f);
+  exception 
+    when others => return zero;
+  end Get_Imaginary_Part;
+
+  procedure Underflow_to_Zero
+              ( x : in out Standard_Complex_Numbers.Complex_Number ) is
 
     re : constant double_float := Get_Real_Part(x);
     im : constant double_float := Get_Imaginary_Part(x);
 
   begin
-    x := Create(re,im);
+    x := Standard_Complex_Numbers.Create(re,im);
   exception
     when others => put_line("exception when creating a complex number");
                    raise;
   end Underflow_to_Zero;
 
-  procedure Underflow_to_Zero ( x : in out Vector ) is
+  procedure Underflow_to_Zero
+              ( x : in out DoblDobl_Complex_Numbers.Complex_Number ) is
+
+    re : constant double_double := Get_Real_Part(x);
+    im : constant double_double := Get_Imaginary_Part(x);
+
+  begin
+    x := DoblDobl_Complex_Numbers.Create(re,im);
+  exception
+    when others => put_line("exception when creating a complex number");
+                   raise;
+  end Underflow_to_Zero;
+
+  procedure Underflow_to_Zero
+              ( x : in out QuadDobl_Complex_Numbers.Complex_Number ) is
+
+    re : constant quad_double := Get_Real_Part(x);
+    im : constant quad_double := Get_Imaginary_Part(x);
+
+  begin
+    x := QuadDobl_Complex_Numbers.Create(re,im);
+  exception
+    when others => put_line("exception when creating a complex number");
+                   raise;
+  end Underflow_to_Zero;
+
+  procedure Underflow_to_Zero
+              ( x : in out Standard_Complex_Vectors.Vector ) is
+  begin
+    for i in x'range loop
+      Underflow_to_Zero(x(i));
+    end loop;
+  exception
+    when others => put_line("exception raised in Underflow_to_Zero"); raise;
+  end Underflow_to_Zero;
+
+  procedure Underflow_to_Zero
+              ( x : in out DoblDobl_Complex_Vectors.Vector ) is
+  begin
+    for i in x'range loop
+      Underflow_to_Zero(x(i));
+    end loop;
+  exception
+    when others => put_line("exception raised in Underflow_to_Zero"); raise;
+  end Underflow_to_Zero;
+
+  procedure Underflow_to_Zero
+              ( x : in out QuadDobl_Complex_Vectors.Vector ) is
   begin
     for i in x'range loop
       Underflow_to_Zero(x(i));
--- src/Ada/Deformations/Newton/handle_underflow_gracefully.ads
+++ src/Ada/Deformations/Newton/handle_underflow_gracefully.ads
@@ -1,6 +1,12 @@
 with Standard_Floating_Numbers;          use Standard_Floating_Numbers;
-with Standard_Complex_Numbers;           use Standard_Complex_Numbers;
-with Standard_Complex_Vectors;           use Standard_Complex_Vectors;
+with Double_Double_Numbers;              use Double_Double_Numbers;
+with Quad_Double_Numbers;                use Quad_Double_Numbers;
+with Standard_Complex_Numbers;
+with Standard_Complex_Vectors;
+with DoblDobl_Complex_Numbers;
+with DoblDobl_Complex_Vectors;
+with QuadDobl_Complex_Numbers;
+with QuadDobl_Complex_Vectors;
 
 package Handle_Underflow_Gracefully is
 
@@ -11,17 +17,29 @@ package Handle_Underflow_Gracefully is
 --   exceptions occurring during the application of Newton's method.
 
   function Underflow_to_Zero ( f : double_float ) return double_float;
+  function Underflow_to_Zero ( f : double_double ) return double_double;
+  function Underflow_to_Zero ( f : quad_double ) return quad_double;
 
   -- DESCRIPTION :
   --   Returns 0.0 if f + 1.0 = 1.0 holds, otherwise f is returned.
 
-  procedure Underflow_to_Zero ( x : in out Complex_Number );
+  procedure Underflow_to_Zero 
+              ( x : in out Standard_Complex_Numbers.Complex_Number );
+  procedure Underflow_to_Zero 
+              ( x : in out DoblDobl_Complex_Numbers.Complex_Number );
+  procedure Underflow_to_Zero 
+              ( x : in out QuadDobl_Complex_Numbers.Complex_Number );
 
   -- DESCRIPTION :
   --   Applies the test f + 1.0 = 1.0 to both real and imaginary parts
   --   of the complex number x.
 
-  procedure Underflow_to_Zero ( x : in out Vector );
+  procedure Underflow_to_Zero
+              ( x : in out Standard_Complex_Vectors.Vector );
+  procedure Underflow_to_Zero
+              ( x : in out DoblDobl_Complex_Vectors.Vector );
+  procedure Underflow_to_Zero
+              ( x : in out QuadDobl_Complex_Vectors.Vector );
 
   -- DESCRIPTION :
   --   Sets floating-point numbers in all numbers of x to zero.
