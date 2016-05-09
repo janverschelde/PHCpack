58f9698927127cc2ab9e1857bfdef130ab04d2f0
janv@uic.edu <jan@math.uic.edu>
2016-05-09 09:58:17 -0500

    updated greeting banners for version 2.4.16

--- src/Ada/Main/greeting_banners.adb
+++ src/Ada/Main/greeting_banners.adb
@@ -2,7 +2,7 @@ package body Greeting_Banners is
 
   function Version return string is
 
-    res : constant string := "PHCv2.4.15 released 2016-04-30";
+    res : constant string := "PHCv2.4.16 released 2016-05-09";
 
   begin
     return res;
