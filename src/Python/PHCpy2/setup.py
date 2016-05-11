0fde0b2089c556fb1a33a60789b387f1e5a93658
janv@uic.edu <jan@math.uic.edu>
2016-05-11 09:15:35 -0500

    prepared files for release 0.4.5 of phcpy

--- src/Python/PHCpy2/setup.py
+++ src/Python/PHCpy2/setup.py
@@ -17,7 +17,7 @@ setup(
     author_email = 'jan@math.uic.edu' ,
     description = 'a package for Polynomial Homotopy Continuation' ,
     url = 'https://github.com/janverschelde/PHCpack' ,
-    version = '0.4.4' ,
+    version = '0.4.5' ,
     packages = ['phcpy'] ,
     py_modules = ['phcpy/interface', 'phcpy/solver', 'phcpy/solutions', \
         'phcpy/trackers', 'phcpy/sweepers', 'phcpy/tuning', 'phcpy/tropisms', \
--- src/Python/PHCpy3/phcpy/__init__.py
+++ src/Python/PHCpy3/phcpy/__init__.py
@@ -93,4 +93,4 @@ except:
     print('Is the phcpy2c3.so not suited for this platform?')
 
 # The version number is defined as a data attribute.
-__version__ = '0.4.4'
+__version__ = '0.4.5'
