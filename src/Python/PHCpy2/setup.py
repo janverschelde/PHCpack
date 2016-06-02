04627283ef57410dcdce3c428f77938c7248b98a
janv@uic.edu <jan@math.uic.edu>
2016-06-02 16:50:32 -0500

    python2 version of the new module dashboard

--- src/Python/PHCpy2/setup.py
+++ src/Python/PHCpy2/setup.py
@@ -22,7 +22,8 @@ setup(
     py_modules = ['phcpy/interface', 'phcpy/solver', 'phcpy/solutions', \
         'phcpy/trackers', 'phcpy/sweepers', 'phcpy/tuning', 'phcpy/tropisms', \
         'phcpy/sets', 'phcpy/maps', 'phcpy/schubert' , 'phcpy/polytopes', \
-        'phcpy/phcwulf', 'phcpy/examples', 'phcpy/families' ] ,
+        'phcpy/phcwulf', 'phcpy/examples', 'phcpy/families', \
+        'phcpy/dashboard' ] ,
     license = 'GNU GENERAL PUBLIC LICENSE version 2 or higher' ,
     data_files = [(get_python_lib()+'/phcpy', ['phcpy/phcpy2c2.so'])] ,
     platforms = ['linux2'] ,
