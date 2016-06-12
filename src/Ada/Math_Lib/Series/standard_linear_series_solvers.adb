e875a8c9c090dc4a799e5128c257eeb0981116af
janv@uic.edu <jan@math.uic.edu>
2016-06-12 13:45:41 -0500

    with LU factorization we can solve linear systems of truncated power series

---
+++ src/Ada/Math_Lib/Series/standard_linear_series_solvers.adb
@@ -0,0 +1,104 @@
+package body Standard_Linear_Series_Solvers is
+
+  function cabs ( c : Complex_Number ) return double_float is
+  begin
+    return (ABS(REAL_PART(c)) + ABS(IMAG_PART(c)));
+  end cabs;
+
+  function cabs ( s : Series ) return double_float is
+  begin
+    return cabs(s.cff(0));
+  end cabs;
+
+  procedure LUfac ( A : in out Standard_Dense_Series_Matrices.Matrix;
+                    n : in integer32;
+                    ipvt : out Standard_Integer_Vectors.Vector;
+                    info : out integer32 ) is
+
+    kp1,L,nm1 : integer32;
+    smax,ikabs : double_float;
+    nbr : Complex_Number;
+    temp : Series;
+
+  begin
+    info := 0;
+    nm1 := n - 1;
+    if nm1 >= 1 then
+      for k in 1..nm1 loop
+        kp1 := k + 1;                              -- find the pivot index L
+        L := k;
+        smax := cabs(A(k,k));
+        for i in kp1..n loop
+          ikabs := cabs(A(i,k));
+          if ikabs > smax then
+            L := i;
+            smax := ikabs;
+          end if;
+        end loop;
+        ipvt(k) := L;
+        if smax = 0.0 then          -- this column is already triangularized
+          info := k;
+        else
+          if L /= k then                         -- interchange if necessary
+            temp := A(L,k);
+            A(L,k) := A(k,k);
+            A(k,k) := temp;
+          end if;
+          nbr := Create(-1.0);
+          temp := nbr/A(k,k);                         -- compute multipliers
+          for i in kp1..n loop
+            A(i,k) := temp*A(i,k);
+          end loop;
+          for j in kp1..n loop       -- row elimination with column indexing
+            temp := A(L,j);
+            if L /= k then
+              A(L,j) := A(k,j);
+              A(k,j) := temp;
+            end if;
+            for i in kp1..n loop
+              A(i,j) := A(i,j) + temp*A(i,k);
+            end loop;
+          end loop;
+        end if;
+      end loop;
+    end if;
+    ipvt(n) := n;
+    if cabs(A(n,n)) = 0.0
+     then info := n;
+    end if;
+  end LUfac;
+
+  procedure LUsolve ( A : in Standard_Dense_Series_Matrices.Matrix;
+                      n : in integer32;
+                      ipvt : in Standard_Integer_Vectors.Vector;
+                      b : in out Standard_Dense_Series_Vectors.Vector ) is
+
+    ell,nm1,kb : integer32;
+    temp : Series;
+ 
+  begin
+    nm1 := n-1;
+    if nm1 >= 1 then                                       -- solve L*y = b
+      for k in 1..nm1 loop
+        ell := ipvt(k);
+        temp := b(ell);
+        if ell /= k then
+          b(ell) := b(k);
+          b(k) := temp;
+        end if;
+        for i in (k+1)..n loop
+          b(i) := b(i) + temp*A(i,k);
+        end loop;
+      end loop;
+    end if;
+    for k in 1..n loop                                     -- solve U*x = y
+      kb := n+1-k;
+      b(kb) := b(kb)/A(kb,kb);
+      temp := -b(kb);
+      for j in 1..(kb-1) loop
+        b(j) := b(j) + temp*A(j,kb);
+      end loop;
+    end loop;
+  end LUsolve;
+
+end Standard_Linear_Series_Solvers;
--- src/Ada/Math_Lib/Series/ts_sermat.adb
+++ src/Ada/Math_Lib/Series/ts_sermat.adb
@@ -1,24 +1,32 @@
 with text_io;                             use text_io;
 with Standard_Integer_Numbers;            use Standard_Integer_Numbers;
 with Standard_Integer_Numbers_io;         use Standard_Integer_Numbers_io;
+with Standard_Floating_Numbers;           use Standard_Floating_Numbers;
 with Standard_Complex_Numbers;            use Standard_Complex_Numbers;
+with Standard_Integer_Vectors;
+with Standard_Integer_Vectors_io;         use Standard_Integer_Vectors_io;
+with Standard_Dense_Series;               use Standard_Dense_Series;
 with Standard_Dense_Series_io;            use Standard_Dense_Series_io;
 with Standard_Dense_Series_Vectors;
 with Standard_Dense_Series_Matrices;
 with Standard_Random_Series;
+with Standard_Linear_Series_Solvers;      use Standard_Linear_Series_Solvers;
 
 procedure ts_sermat is
 
 -- DESCRIPTION :
 --   Test on matrices of truncated dense power series.
 
-  procedure Main is
-
-    A : Standard_Dense_Series_Matrices.Matrix(0..2,0..2)
-      := Standard_Random_Series.Random_Series_Matrix(0,2,0,2,4);
-
+  procedure Write ( v : in Standard_Dense_Series_Vectors.Vector ) is
   begin
-    put_line("A 2-by-2 matrix of random series of order 4 :");
+    for i in v'range loop
+      put("Component "); put(i,1); put_line(" :");
+      put(v(i));
+    end loop;
+  end Write;
+
+  procedure Write ( A : in Standard_Dense_Series_Matrices.Matrix ) is
+  begin
     for i in A'range(1) loop
       for j in A'range(2) loop
         put("Component ");
@@ -26,6 +34,51 @@ procedure ts_sermat is
         put(A(i,j));
       end loop;
     end loop;
+  end Write;
+
+  procedure Random_Linear_Solve ( n,order : in integer32 ) is
+
+  -- DESCRIPTION :
+  --   Generates a random matrix A and right hand side vector b
+  --   of dimension n and solves the linear system A*x = b.
+  --   The series are all of the given order.
+
+    A : Standard_Dense_Series_Matrices.Matrix(1..n,1..n)
+      := Standard_Random_Series.Random_Series_Matrix(1,n,1,n,order);
+    wrk : Standard_Dense_Series_Matrices.Matrix(1..n,1..n) := A;
+    b : Standard_Dense_Series_Vectors.Vector(1..n)
+      := Standard_Random_Series.Random_Series_Vector(1,n,order);
+    x : Standard_Dense_Series_Vectors.Vector(1..n) := b;
+    ipvt : Standard_Integer_Vectors.Vector(1..n);
+    info : integer32;
+    rsd : Standard_Dense_Series_Vectors.Vector(1..n);
+
+    use Standard_Dense_Series_Vectors;
+    use Standard_Dense_Series_Matrices;
+
+  begin
+    put_line("The coefficient matrix A :"); Write(A);
+    put_line("The right hand side vector b :"); Write(b);
+    LUfac(wrk,n,ipvt,info);
+    put("The pivots : "); put(ipvt); new_line;
+    LUsolve(wrk,n,ipvt,x);
+    put_line("The solution x to A*x = b :"); Write(x);
+    rsd := b - A*x;
+    put_line("The residual b - A*x :"); Write(rsd);
+  end Random_Linear_Solve;
+
+  procedure Main is
+
+  -- DESCRIPTION :
+  --   Prompts the user for the dimension of the linear system
+  --   and the order of the series.
+
+    dim,ord : integer32 := 0;
+
+  begin
+    put("Give the dimension : "); get(dim);
+    put("Give the order of the series : "); get(ord);
+    Random_Linear_Solve(dim,ord);
   end Main;
 
 begin
