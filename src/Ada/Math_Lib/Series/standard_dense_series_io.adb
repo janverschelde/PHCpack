4efbbee59fbdac74af5439bf5227736870f947f7
janv@uic.edu <jan@math.uic.edu>
2016-06-17 17:33:23 -0500

    basic get() for the input of truncated power series

--- src/Ada/Math_Lib/Series/standard_dense_series_io.adb
+++ src/Ada/Math_Lib/Series/standard_dense_series_io.adb
@@ -1,7 +1,20 @@
+with Standard_Integer_Numbers_io;
 with Standard_Complex_Vectors;
 with Standard_Complex_Vectors_io;
 
 package body Standard_Dense_Series_io is
+
+  procedure get ( s : out Series ) is
+  begin
+    get(standard_input,s);
+  end get;
+
+  procedure get ( file : in file_type; s : out Series ) is
+  begin
+    s.order := 0;
+    Standard_Integer_Numbers_io.get(s.order);
+    Standard_Complex_Vectors_io.get(s.cff(0..s.order));
+  end get;
 
   procedure put ( s : in Series ) is
   begin
@@ -10,8 +23,7 @@ package body Standard_Dense_Series_io is
 
   procedure put ( file : in file_type; s : in Series ) is
   begin
-    Standard_Complex_Vectors_io.put_line
-      (file,Standard_Complex_Vectors.Vector(s));
+    Standard_Complex_Vectors_io.put_line(file,s.cff(0..s.order));
   end put;
 
 end Standard_Dense_Series_io;
--- src/Ada/Math_Lib/Series/standard_dense_series_io.ads
+++ src/Ada/Math_Lib/Series/standard_dense_series_io.ads
@@ -6,6 +6,13 @@ package Standard_Dense_Series_io is
 -- DESCRIPTION :
 --   The package encapsulates the i/o for the coefficient vectors.
 
+  procedure get ( s : out Series );
+  procedure get ( file : in file_type; s : out Series );
+
+  -- DESCRIPTION :
+  --   Prompts for the order (an integer number),
+  --   followed by as many complex numbers as the order plus one.
+
   procedure put ( s : in Series );
   procedure put ( file : in file_type; s : in Series );
 
