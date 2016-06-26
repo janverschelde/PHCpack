4c062da9a0be4537a0c765369f219211be7ab55f
janv@uic.edu <jan@math.uic.edu>
2016-06-26 16:41:20 -0500

    defined vector norms for vectors of truncated power series in double double and quad double precision

---
+++ src/Ada/Math_Lib/Series/dobldobl_series_vector_norms.adb
@@ -0,0 +1,65 @@
+with DoblDobl_Algebraic_Series;
+
+package body DoblDobl_Series_Vector_Norms is
+
+  function Conjugate ( v : Vector ) return Vector is
+
+    res : Vector(v'range);
+
+  begin
+    for i in v'range loop
+      res(i) := DoblDobl_Dense_Series.Conjugate(v(i));
+    end loop;
+    return res;
+  end Conjugate;
+
+  function Inner_Product ( u,v : Vector ) return Series is
+
+    w : constant Vector(u'range) := Conjugate(u);
+    res : Series := Create(0);
+
+  begin
+    for i in w'range loop
+      res := res + w(i)*v(i);
+    end loop;
+    return res;
+  end Inner_Product;
+
+  function Square_of_Norm ( v : Vector ) return Series is
+
+    c : constant Vector(v'range) := Conjugate(v);
+    res : constant Series := c*v;
+
+  begin
+    return res;
+  end Square_of_Norm;
+
+  function Norm ( v : Vector ) return Series is
+
+    sn : constant Series := Square_of_Norm(v);
+    res : constant Series := DoblDobl_Algebraic_Series.sqrt(sn,0);
+
+  begin
+    return res;
+  end Norm;
+
+  procedure Normalize ( v : in out Vector ) is
+
+    sn : constant Series := Norm(v);
+    invsn : constant Series := Inverse(sn);
+
+  begin
+    Mul(v,invsn);
+  end Normalize;
+
+  function Normalize ( v : Vector ) return Vector is
+
+    sn : constant Series := Norm(v);
+    invsn : constant Series := Inverse(sn);
+    res : constant Vector(v'range) := invsn*v;
+
+  begin
+    return res;
+  end Normalize;
+  
+end DoblDobl_Series_Vector_Norms;
--- src/Ada/Math_Lib/Series/ts_servec.adb
+++ src/Ada/Math_Lib/Series/ts_servec.adb
@@ -1,19 +1,31 @@
 with text_io;                             use text_io;
+with Communications_with_User;            use Communications_with_User;
 with Standard_Integer_Numbers;            use Standard_Integer_Numbers;
 with Standard_Integer_Numbers_io;         use Standard_Integer_Numbers_io;
 with Standard_Complex_Numbers;            use Standard_Complex_Numbers;
-with Standard_Dense_Series;               use Standard_Dense_Series;
+with Standard_Dense_Series;
 with Standard_Dense_Series_io;            use Standard_Dense_Series_io;
-with Standard_Dense_Series_Vectors;       use Standard_Dense_Series_Vectors;
+with Standard_Dense_Series_Vectors;
+with Standard_Dense_Series_Vectors_io;    use Standard_Dense_Series_Vectors_io;
+with Standard_Series_Vector_Norms;
+with DoblDobl_Dense_Series;
+with DoblDobl_Dense_Series_io;            use DoblDobl_Dense_Series_io;
+with DoblDobl_Dense_Series_Vectors;
+with DoblDobl_Series_Vector_Norms;
+with QuadDobl_Dense_Series;
+with QuadDobl_Dense_Series_io;            use QuadDobl_Dense_Series_io;
+with QuadDobl_Dense_Series_Vectors;
+with QuadDobl_Series_Vector_Norms;
 with Standard_Random_Series;
-with Standard_Series_Vector_Norms;        use Standard_Series_Vector_Norms;
+with DoblDobl_Random_Series;
+with QuadDobl_Random_Series;
 
 procedure ts_servec is
 
 -- DESCRIPTION :
 --   Test on vectors of truncated power series.
 
-  procedure Write ( v : in Vector ) is
+  procedure Write ( v : in Standard_Dense_Series_Vectors.Vector ) is
 
   -- DESCRIPTION :
   --   Writes the components of the vector to standard output.
@@ -25,10 +37,39 @@ procedure ts_servec is
     end loop;
   end Write;
 
-  procedure Test_Norm ( v : in Vector ) is
+  procedure Write ( v : in DoblDobl_Dense_Series_Vectors.Vector ) is
+
+  -- DESCRIPTION :
+  --   Writes the components of the vector to standard output.
+ 
+  begin
+    for i in v'range loop
+      put("Component "); put(i,1); put_line(" :");
+      put(v(i));
+    end loop;
+  end Write;
+
+  procedure Write ( v : in QuadDobl_Dense_Series_Vectors.Vector ) is
+
+  -- DESCRIPTION :
+  --   Writes the components of the vector to standard output.
+ 
+  begin
+    for i in v'range loop
+      put("Component "); put(i,1); put_line(" :");
+      put(v(i));
+    end loop;
+  end Write;
+
+  procedure Standard_Test_Norm 
+              ( v : in Standard_Dense_Series_Vectors.Vector ) is
 
   -- DESCRIPTION :
   --   Test on normalization of the vector v.
+
+    use Standard_Dense_Series;
+    use Standard_Dense_Series_Vectors;
+    use Standard_Series_Vector_Norms;
 
     sn : constant Series := Norm(v);
     snv : Series;
@@ -41,7 +82,98 @@ procedure ts_servec is
     put_line("The normalized vector : "); Write(nv);
     snv := Square_of_Norm(nv);
     put_line("Square of norm of normalized vector :"); put(snv);
-  end Test_Norm;
+  end Standard_Test_Norm;
+
+  procedure DoblDobl_Test_Norm 
+              ( v : in DoblDobl_Dense_Series_Vectors.Vector ) is
+
+  -- DESCRIPTION :
+  --   Test on normalization of the vector v.
+
+    use DoblDobl_Dense_Series;
+    use DoblDobl_Dense_Series_Vectors;
+    use DoblDobl_Series_Vector_Norms;
+
+    sn : constant Series := Norm(v);
+    snv : Series;
+    nv : Vector(v'range);
+
+  begin
+    new_line;
+    put_line("The norm of v : "); put(sn);
+    nv := Normalize(v);
+    put_line("The normalized vector : "); Write(nv);
+    snv := Square_of_Norm(nv);
+    put_line("Square of norm of normalized vector :"); put(snv);
+  end DoblDobl_Test_Norm;
+
+  procedure QuadDobl_Test_Norm 
+              ( v : in QuadDobl_Dense_Series_Vectors.Vector ) is
+
+  -- DESCRIPTION :
+  --   Test on normalization of the vector v.
+
+    use QuadDobl_Dense_Series;
+    use QuadDobl_Dense_Series_Vectors;
+    use QuadDobl_Series_Vector_Norms;
+
+    sn : constant Series := Norm(v);
+    snv : Series;
+    nv : Vector(v'range);
+
+  begin
+    new_line;
+    put_line("The norm of v : "); put(sn);
+    nv := Normalize(v);
+    put_line("The normalized vector : "); Write(nv);
+    snv := Square_of_Norm(nv);
+    put_line("Square of norm of normalized vector :"); put(snv);
+  end QuadDobl_Test_Norm;
+
+  procedure Standard_Test ( dim,order : in integer32 ) is
+
+  -- DESCRIPTION :
+  --   Generates a random vector of range 1..dim,
+  --   with series of the given order, in standard double precision.
+  --   Tests the computation of the norm and the normalization.
+
+    v : Standard_Dense_Series_Vectors.Vector(1..dim)
+      := Standard_Random_Series.Random_Series_Vector(1,dim,order);
+
+  begin
+    Write(v);
+    Standard_Test_Norm(v);
+  end Standard_Test;
+
+  procedure DoblDobl_Test ( dim,order : in integer32 ) is
+
+  -- DESCRIPTION :
+  --   Generates a random vector of range 1..dim,
+  --   with series of the given order, in double double precision.
+  --   Tests the computation of the norm and the normalization.
+
+    v : DoblDobl_Dense_Series_Vectors.Vector(1..dim)
+      := DoblDobl_Random_Series.Random_Series_Vector(1,dim,order);
+
+  begin
+    Write(v);
+    DoblDobl_Test_Norm(v);
+  end DoblDobl_Test;
+
+  procedure QuadDobl_Test ( dim,order : in integer32 ) is
+
+  -- DESCRIPTION :
+  --   Generates a random vector of range 1..dim,
+  --   with series of the given order, in quad double precision.
+  --   Tests the computation of the norm and the normalization.
+
+    v : QuadDobl_Dense_Series_Vectors.Vector(1..dim)
+      := QuadDobl_Random_Series.Random_Series_Vector(1,dim,order);
+
+  begin
+    Write(v);
+    QuadDobl_Test_Norm(v);
+  end QuadDobl_Test;
 
   procedure Main is
 
@@ -50,18 +182,26 @@ procedure ts_servec is
   --   and then generates a random vector.
 
     dim,order : integer32 := 0;
+    ans : character;
 
   begin
     new_line;
     put("Give the dimension : "); get(dim);
     put("Give the order : "); get(order);
-    declare
-      v : Vector(1..dim)
-        := Standard_Random_Series.Random_Series_Vector(1,dim,order);
-    begin
-      Write(v);
-      Test_Norm(v);
-    end;
+    new_line;
+    put_line("MENU to select the working precision :");
+    put_line("  0. standard double precision;");
+    put_line("  1. double double precision; or");
+    put_line("  2. quad double precision.");
+    put("Type 0, 1, or 2 to select the precision : ");
+    Ask_Alternative(ans,"012");
+    new_line;
+    case ans is
+      when '0' => Standard_Test(dim,order);
+      when '1' => DoblDobl_Test(dim,order);
+      when '2' => QuadDobl_Test(dim,order);
+      when others => null;
+    end case;
   end Main;
 
 begin
