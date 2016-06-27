80ef75a8b1ebbdaa7631a501f916c3e41a48a3f5
janv@uic.edu <jan@math.uic.edu>
2016-06-27 15:43:29 -0500

    added tests on polynomials with series coefficients in double double and quad double precision

--- src/Ada/Math_Lib/Series/ts_serpol.adb
+++ src/Ada/Math_Lib/Series/ts_serpol.adb
@@ -9,12 +9,28 @@ with Standard_Natural_Vectors_io;       use Standard_Natural_Vectors_io;
 with Symbol_Table;
 with Standard_Complex_Polynomials;
 with Standard_Complex_Polynomials_io;   use Standard_Complex_Polynomials_io;
-with Standard_Dense_Series;             use Standard_Dense_Series;
+with DoblDobl_Complex_Polynomials;
+with DoblDobl_Complex_Polynomials_io;   use DoblDobl_Complex_Polynomials_io;
+with QuadDobl_Complex_Polynomials;
+with QuadDobl_Complex_Polynomials_io;   use QuadDobl_Complex_Polynomials_io;
+with Standard_Dense_Series;
 with Standard_Dense_Series_io;          use Standard_Dense_Series_io;
 with Standard_Dense_Series_Vectors;
-with Standard_Random_Series;            use Standard_Random_Series;
 with Standard_Series_Polynomials;
 with Standard_Series_Poly_Functions;
+with DoblDobl_Dense_Series;
+with DoblDobl_Dense_Series_io;          use DoblDobl_Dense_Series_io;
+with DoblDobl_Dense_Series_Vectors;
+with DoblDobl_Series_Polynomials;
+with DoblDobl_Series_Poly_Functions;
+with QuadDobl_Dense_Series;
+with QuadDobl_Dense_Series_io;          use QuadDobl_Dense_Series_io;
+with QuadDobl_Dense_Series_Vectors;
+with QuadDobl_Series_Polynomials;
+with QuadDobl_Series_Poly_Functions;
+with Standard_Random_Series;            use Standard_Random_Series;
+with DoblDobl_Random_Series;            use DoblDobl_Random_Series;
+with QuadDobl_Random_Series;            use QuadDobl_Random_Series;
 with Series_and_Polynomials;            use Series_and_Polynomials;
 with Series_and_Polynomials_io;
 
@@ -27,14 +43,15 @@ procedure ts_serpol is
   procedure Write ( s : in Standard_Series_Polynomials.Poly ) is
 
   -- DESCRIPTION :
-  --   Very simple output of a polynomial with series coefficients.
+  --   Very simple output of a polynomial with series coefficients,
+  --   in standard double precision.
  
     cnt : natural32 := 0;
 
     procedure Visit_Term ( t : in Standard_Series_Polynomials.Term;
                            c : out boolean ) is
    
-      cf : Series := t.cf;
+      cf : constant Standard_Dense_Series.Series := t.cf;
 
     begin
       cnt := cnt + 1;
@@ -50,13 +67,70 @@ procedure ts_serpol is
   begin
     Visit_Terms(s);
   end Write;
+ 
+  procedure Write ( s : in DoblDobl_Series_Polynomials.Poly ) is
 
-  procedure Test_Conversion is 
+  -- DESCRIPTION :
+  --   Very simple output of a polynomial with series coefficients,
+  --   in double double precision.
+ 
+    cnt : natural32 := 0;
+
+    procedure Visit_Term ( t : in DoblDobl_Series_Polynomials.Term;
+                           c : out boolean ) is
+   
+      cf : constant DoblDobl_Dense_Series.Series := t.cf;
+
+    begin
+      cnt := cnt + 1;
+      put("The coefficient of term "); put(cnt); put_line(" :");
+      put(cf);
+      put("has order "); put(cf.order,1);
+      put(" and degrees : "); put(t.dg.all); new_line;
+      c := true;
+    end Visit_Term;
+    procedure Visit_Terms is
+      new DoblDobl_Series_Polynomials.Visiting_Iterator(Visit_Term);
+
+  begin
+    Visit_Terms(s);
+  end Write;
+ 
+  procedure Write ( s : in QuadDobl_Series_Polynomials.Poly ) is
+
+  -- DESCRIPTION :
+  --   Very simple output of a polynomial with series coefficients,
+  --   in quad double precision.
+ 
+    cnt : natural32 := 0;
+
+    procedure Visit_Term ( t : in QuadDobl_Series_Polynomials.Term;
+                           c : out boolean ) is
+   
+      cf : constant QuadDobl_Dense_Series.Series := t.cf;
+
+    begin
+      cnt := cnt + 1;
+      put("The coefficient of term "); put(cnt); put_line(" :");
+      put(cf);
+      put("has order "); put(cf.order,1);
+      put(" and degrees : "); put(t.dg.all); new_line;
+      c := true;
+    end Visit_Term;
+    procedure Visit_Terms is
+      new QuadDobl_Series_Polynomials.Visiting_Iterator(Visit_Term);
+
+  begin
+    Visit_Terms(s);
+  end Write;
+
+  procedure Standard_Test_Conversion is 
 
   -- DESCRIPTION :
   --   Prompts the user for the number of variables.
   --   and reads in a regular polynomial in several variables,
-  --   for conversion into a series polynomial.
+  --   for conversion into a series polynomial,
+  --   in standard double precision.
 
     n : natural32 := 0;
     i : integer32 := 0;
@@ -84,20 +158,94 @@ procedure ts_serpol is
     Write(s);
     q := Series_Polynomial_to_Polynomial(s,i,ans = 'y');
     put("s as poly : "); put(q); new_line;
-  end Test_Conversion;
+  end Standard_Test_Conversion;
 
-  function Factor ( n,k : integer32; s : Series )
+  procedure DoblDobl_Test_Conversion is 
+
+  -- DESCRIPTION :
+  --   Prompts the user for the number of variables.
+  --   and reads in a regular polynomial in several variables,
+  --   for conversion into a series polynomial,
+  --   in double double precision.
+
+    n : natural32 := 0;
+    i : integer32 := 0;
+    p,q : DoblDobl_Complex_Polynomials.Poly;
+    s : DoblDobl_Series_Polynomials.Poly;
+    ans : character;
+ 
+  begin
+    new_line;
+    put("Give the number of variables : "); get(n);
+    Symbol_Table.init(n);
+    put("Give a polynomial : "); get(p);
+    put("> your polynomial : "); put(p); new_line;
+    new_line;
+    put("Give the index of the series parameter : "); get(i);
+    new_line;
+    put("Extra output during the conversion ? (y/n) ");
+    Ask_Yes_or_No(ans);
+    if ans = 'y'
+     then s := Polynomial_to_Series_Polynomial(p,i,true);
+     else s := Polynomial_to_Series_Polynomial(p,i);
+    end if;
+    new_line;
+    put_line("The series polynomial s :");
+    Write(s);
+    q := Series_Polynomial_to_Polynomial(s,i,ans = 'y');
+    put("s as poly : "); put(q); new_line;
+  end DoblDobl_Test_Conversion;
+
+  procedure QuadDobl_Test_Conversion is 
+
+  -- DESCRIPTION :
+  --   Prompts the user for the number of variables.
+  --   and reads in a regular polynomial in several variables,
+  --   for conversion into a series polynomial,
+  --   in quad double precision.
+
+    n : natural32 := 0;
+    i : integer32 := 0;
+    p,q : QuadDobl_Complex_Polynomials.Poly;
+    s : QuadDobl_Series_Polynomials.Poly;
+    ans : character;
+ 
+  begin
+    new_line;
+    put("Give the number of variables : "); get(n);
+    Symbol_Table.init(n);
+    put("Give a polynomial : "); get(p);
+    put("> your polynomial : "); put(p); new_line;
+    new_line;
+    put("Give the index of the series parameter : "); get(i);
+    new_line;
+    put("Extra output during the conversion ? (y/n) ");
+    Ask_Yes_or_No(ans);
+    if ans = 'y'
+     then s := Polynomial_to_Series_Polynomial(p,i,true);
+     else s := Polynomial_to_Series_Polynomial(p,i);
+    end if;
+    new_line;
+    put_line("The series polynomial s :");
+    Write(s);
+    q := Series_Polynomial_to_Polynomial(s,i,ans = 'y');
+    put("s as poly : "); put(q); new_line;
+  end QuadDobl_Test_Conversion;
+
+  function Factor ( n,k : integer32;
+                    s : Standard_Dense_Series.Series )
                   return Standard_Series_Polynomials.Poly is
 
   -- DESCRIPTION :
   --   Returns x[k] - s as a polynomial in n variables.
   --   All coefficients of the polynomial on return have the same order,
-  --   the same as s.order.
+  --   the same as s.order, and are in standard double precision.
 
   -- REQUIRED : k is in range 1..n.
 
     res : Standard_Series_Polynomials.Poly;
-    one : constant Series := Create(1.0); -- Create(1.0,s.order);
+    one : constant Standard_Dense_Series.Series
+        := Standard_Dense_Series.Create(1.0);
     trm : Standard_Series_Polynomials.Term;
 
   begin
@@ -111,12 +259,66 @@ procedure ts_serpol is
     return res;
   end Factor;
 
+  function Factor ( n,k : integer32;
+                    s : DoblDobl_Dense_Series.Series )
+                  return DoblDobl_Series_Polynomials.Poly is
+
+  -- DESCRIPTION :
+  --   Returns x[k] - s as a polynomial in n variables.
+  --   All coefficients of the polynomial on return have the same order,
+  --   the same as s.order, and are in double double precision.
+
+  -- REQUIRED : k is in range 1..n.
+
+    res : DoblDobl_Series_Polynomials.Poly;
+    one : constant DoblDobl_Dense_Series.Series
+        := DoblDobl_Dense_Series.Create(1.0);
+    trm : DoblDobl_Series_Polynomials.Term;
+
+  begin
+    trm.cf := one;
+    trm.dg := new Standard_Natural_Vectors.Vector'(1..n => 0);
+    trm.dg(k) := 1;
+    res := DoblDobl_Series_Polynomials.Create(trm);
+    trm.cf := s;
+    trm.dg(k) := 0;
+    DoblDobl_Series_Polynomials.Sub(res,trm);
+    return res;
+  end Factor;
+
+  function Factor ( n,k : integer32;
+                    s : QuadDobl_Dense_Series.Series )
+                  return QuadDobl_Series_Polynomials.Poly is
+
+  -- DESCRIPTION :
+  --   Returns x[k] - s as a polynomial in n variables.
+  --   All coefficients of the polynomial on return have the same order,
+  --   the same as s.order, and are in quad double precision.
+
+  -- REQUIRED : k is in range 1..n.
+
+    res : QuadDobl_Series_Polynomials.Poly;
+    one : constant QuadDobl_Dense_Series.Series
+        := QuadDobl_Dense_Series.Create(1.0);
+    trm : QuadDobl_Series_Polynomials.Term;
+
+  begin
+    trm.cf := one;
+    trm.dg := new Standard_Natural_Vectors.Vector'(1..n => 0);
+    trm.dg(k) := 1;
+    res := QuadDobl_Series_Polynomials.Create(trm);
+    trm.cf := s;
+    trm.dg(k) := 0;
+    QuadDobl_Series_Polynomials.Sub(res,trm);
+    return res;
+  end Factor;
+
   function Product ( s : Standard_Dense_Series_Vectors.Vector )
                    return Standard_Series_Polynomials.Poly is
 
   -- DESCRIPION :
   --   Returns the product of the factors x[k] - s[k], for k in s'range,
-  --   where s'first = 1.
+  --   where s'first = 1, in standard double precision.
 
     dim : constant integer32 := s'last;
     res : Standard_Series_Polynomials.Poly := Factor(dim,1,s(1));
@@ -131,7 +333,47 @@ procedure ts_serpol is
     return res;
   end Product;
 
-  procedure Test_Evaluation is
+  function Product ( s : DoblDobl_Dense_Series_Vectors.Vector )
+                   return DoblDobl_Series_Polynomials.Poly is
+
+  -- DESCRIPION :
+  --   Returns the product of the factors x[k] - s[k], for k in s'range,
+  --   where s'first = 1, in double double precision.
+
+    dim : constant integer32 := s'last;
+    res : DoblDobl_Series_Polynomials.Poly := Factor(dim,1,s(1));
+    fac : DoblDobl_Series_Polynomials.Poly;
+
+  begin
+    for k in 2..s'last loop
+      fac := Factor(dim,k,s(k));
+      DoblDobl_Series_Polynomials.Mul(res,fac);
+      DoblDobl_Series_Polynomials.Clear(fac);
+    end loop;
+    return res;
+  end Product;
+
+  function Product ( s : QuadDobl_Dense_Series_Vectors.Vector )
+                   return QuadDobl_Series_Polynomials.Poly is
+
+  -- DESCRIPION :
+  --   Returns the product of the factors x[k] - s[k], for k in s'range,
+  --   where s'first = 1, in double double precision.
+
+    dim : constant integer32 := s'last;
+    res : QuadDobl_Series_Polynomials.Poly := Factor(dim,1,s(1));
+    fac : QuadDobl_Series_Polynomials.Poly;
+
+  begin
+    for k in 2..s'last loop
+      fac := Factor(dim,k,s(k));
+      QuadDobl_Series_Polynomials.Mul(res,fac);
+      QuadDobl_Series_Polynomials.Clear(fac);
+    end loop;
+    return res;
+  end Product;
+
+  procedure Standard_Test_Evaluation is
 
   -- DESCRIPTION :
   --   Prompts for the number of variables and the order of the series.
@@ -149,8 +391,9 @@ procedure ts_serpol is
     declare
       rns : constant Standard_Dense_Series_Vectors.Vector(1..dim)
           := Random_Series_Vector(1,dim,order);
-      pol : Standard_Series_Polynomials.Poly := Product(rns);
-      eva : constant Series := Standard_Series_Poly_Functions.Eval(pol,rns);
+      pol : constant Standard_Series_Polynomials.Poly := Product(rns);
+      eva : constant Standard_Dense_Series.Series
+          := Standard_Series_Poly_Functions.Eval(pol,rns);
     begin
       for i in 1..dim loop
         put("random series "); put(i,1); put_line(" :");
@@ -159,14 +402,77 @@ procedure ts_serpol is
       put_line("The polynomial :"); Write(pol);
       put_line("The value at the polynomial :"); put(eva);
     end;
-  end Test_Evaluation;
+  end Standard_Test_Evaluation;
 
-  procedure Test_Input_Output is
+  procedure DoblDobl_Test_Evaluation is
 
   -- DESCRIPTION :
-  --   Reads a series in symbolic format and writes the series back.
+  --   Prompts for the number of variables and the order of the series.
+  --   Then as many random series as the number of variables are generated.
+  --   The polynomial is of the product of x[k] - s[k], where k ranges
+  --   over the number of variables 'x' and series 's'.
+  --   So the evaluation at the series should produce zero.
 
-    s : series;
+    order,dim : integer32 := 0;
+
+  begin
+    new_line;
+    put("Give the number of variables in the polynomial : "); get(dim);
+    put("Give the order of the power series : "); get(order);
+    declare
+      rns : constant DoblDobl_Dense_Series_Vectors.Vector(1..dim)
+          := Random_Series_Vector(1,dim,order);
+      pol : constant DoblDobl_Series_Polynomials.Poly := Product(rns);
+      eva : constant DoblDobl_Dense_Series.Series
+          := DoblDobl_Series_Poly_Functions.Eval(pol,rns);
+    begin
+      for i in 1..dim loop
+        put("random series "); put(i,1); put_line(" :");
+        put(rns(i));
+      end loop;
+      put_line("The polynomial :"); Write(pol);
+      put_line("The value at the polynomial :"); put(eva);
+    end;
+  end DoblDobl_Test_Evaluation;
+
+  procedure QuadDobl_Test_Evaluation is
+
+  -- DESCRIPTION :
+  --   Prompts for the number of variables and the order of the series.
+  --   Then as many random series as the number of variables are generated.
+  --   The polynomial is of the product of x[k] - s[k], where k ranges
+  --   over the number of variables 'x' and series 's'.
+  --   So the evaluation at the series should produce zero.
+
+    order,dim : integer32 := 0;
+
+  begin
+    new_line;
+    put("Give the number of variables in the polynomial : "); get(dim);
+    put("Give the order of the power series : "); get(order);
+    declare
+      rns : constant QuadDobl_Dense_Series_Vectors.Vector(1..dim)
+          := Random_Series_Vector(1,dim,order);
+      pol : constant QuadDobl_Series_Polynomials.Poly := Product(rns);
+      eva : constant QuadDobl_Dense_Series.Series
+          := QuadDobl_Series_Poly_Functions.Eval(pol,rns);
+    begin
+      for i in 1..dim loop
+        put("random series "); put(i,1); put_line(" :");
+        put(rns(i));
+      end loop;
+      put_line("The polynomial :"); Write(pol);
+      put_line("The value at the polynomial :"); put(eva);
+    end;
+  end QuadDobl_Test_Evaluation;
+
+  procedure Standard_Test_Input_Output is
+
+  -- DESCRIPTION :
+  --   Reads a series in symbolic format and writes the series back,
+  --   in standard double precision.
+
+    s : Standard_Dense_Series.Series;
 
   begin
     new_line;
@@ -179,7 +485,49 @@ procedure ts_serpol is
     put_line("The series : ");
     Series_and_Polynomials_io.put(s);
     new_line;
-  end Test_Input_Output;
+  end Standard_Test_Input_Output;
+
+  procedure DoblDobl_Test_Input_Output is
+
+  -- DESCRIPTION :
+  --   Reads a series in symbolic format and writes the series back,
+  --   in double double precision.
+
+    s : DoblDobl_Dense_Series.Series;
+
+  begin
+    new_line;
+    put_line("Give a series, terminate with ;");
+    Series_and_Polynomials_io.get(s);
+    new_line;
+    put_line("The coefficients of the series : ");
+    put(s);
+    new_line;
+    put_line("The series : ");
+    Series_and_Polynomials_io.put(s);
+    new_line;
+  end DoblDobl_Test_Input_Output;
+
+  procedure QuadDobl_Test_Input_Output is
+
+  -- DESCRIPTION :
+  --   Reads a series in symbolic format and writes the series back,
+  --   in quad double precision.
+
+    s : QuadDobl_Dense_Series.Series;
+
+  begin
+    new_line;
+    put_line("Give a series, terminate with ;");
+    Series_and_Polynomials_io.get(s);
+    new_line;
+    put_line("The coefficients of the series : ");
+    put(s);
+    new_line;
+    put_line("The series : ");
+    Series_and_Polynomials_io.put(s);
+    new_line;
+  end QuadDobl_Test_Input_Output;
   
   procedure Main is
 
@@ -187,7 +535,7 @@ procedure ts_serpol is
   --   Displays the menu of tests
   --   and prompts the user to make a choice.
 
-    ans : character;
+    ans,prc : character;
 
   begin
     new_line;
@@ -197,10 +545,35 @@ procedure ts_serpol is
     put_line("  2. test symbolic input and output;");
     put("Type 0, 1, or 2 to select a test : ");
     Ask_Alternative(ans,"012");
+    new_line;
+    put_line("MENU to select the working precision :");
+    put_line("  0. standard double precision;");
+    put_line("  1. double double precision; or");
+    put_line("  2. quad double precision;");
+    put("Type 0, 1, or 2 to select the working precision : ");
+    Ask_Alternative(prc,"012");
     case ans is
-      when '0' => Test_Conversion;
-      when '1' => Test_Evaluation;
-      when '2' => Test_Input_Output;
+      when '0' =>
+        case prc is
+          when '0' => Standard_Test_Conversion;
+          when '1' => DoblDobl_Test_Conversion;
+          when '2' => QuadDobl_Test_Conversion;
+          when others => null;
+        end case;
+      when '1' =>
+        case prc is
+          when '0' => Standard_Test_Evaluation;
+          when '1' => DoblDobl_Test_Evaluation;
+          when '2' => QuadDobl_Test_Evaluation;
+          when others => null;
+        end case;
+      when '2' =>
+        case prc is
+          when '0' => Standard_Test_Input_Output;
+          when '1' => DoblDobl_Test_Input_Output;
+          when '2' => QuadDobl_Test_Input_Output;
+          when others => null;
+        end case;
       when others => null;
     end case;
   end Main;
