2098d3ef8cc10bb9fecc3a1a6f8592daf9bdfb7f
Jan Verschelde <janv@uic.edu>
2016-07-10 14:29:18 -0500

    a very simple, basic path tracker using a power series predictor in double precision

--- src/Ada/Deformations/Curves/ts_serpath.adb
+++ src/Ada/Deformations/Curves/ts_serpath.adb
@@ -3,14 +3,20 @@ with Communications_with_User;           use Communications_with_User;
 with Standard_Natural_Numbers;           use Standard_Natural_Numbers;
 with Standard_Integer_Numbers;           use Standard_Integer_Numbers;
 with Standard_Integer_Numbers_io;        use Standard_Integer_Numbers_io;
+with Standard_Floating_Numbers;          use Standard_Floating_Numbers;
+with Standard_Floating_Numbers_io;       use Standard_Floating_Numbers_io;
 with Standard_Complex_Numbers;
 with DoblDobl_Complex_Numbers;
 with QuadDobl_Complex_Numbers;
 with Standard_Random_Numbers;
 with DoblDobl_Random_Numbers;
 with QuadDobl_Random_Numbers;
+with Standard_Complex_Vectors;
+with Standard_Complex_Vectors_io;        use Standard_Complex_Vectors_io;
 with Standard_Complex_Poly_Systems;
 with Standard_Complex_Poly_Systems_io;   use Standard_Complex_Poly_Systems_io;
+with Standard_Complex_Poly_SysFun;
+with Standard_Complex_Jaco_Matrices;
 with Standard_Complex_Solutions;
 with Standard_System_and_Solutions_io;
 with DoblDobl_Complex_Poly_Systems;
@@ -22,19 +28,130 @@ with QuadDobl_Complex_Poly_Systems_io;   use QuadDobl_Complex_Poly_Systems_io;
 with QuadDobl_Complex_Solutions;
 with QuadDobl_System_and_Solutions_io;
 with Standard_Homotopy;
+with Standard_Root_Refiners;
 with DoblDobl_Homotopy;
 with QuadDobl_Homotopy;
+with Standard_Dense_Series_Vectors;
 with Standard_Series_Poly_Systems;
 with DoblDobl_Series_Poly_Systems;
 with QuadDobl_Series_Poly_Systems;
 with Series_and_Polynomials;
 with Series_and_Polynomials_io;          use Series_and_Polynomials_io;
 with Series_and_Homotopies;
+with Series_and_Predictors;
 
 procedure ts_serpath is
 
 -- DESCRIPTION :
 --   Developing path tracers with power series.
+
+  procedure Standard_Correct
+              ( hom : in Standard_Series_Poly_Systems.Poly_Sys;
+                t : in double_float;
+                sol : in out Standard_Complex_Vectors.Vector;
+                verbose : in boolean := false ) is
+
+  -- DESCRIPTION :
+  --   Applies a couple of Newton iterations to correct the solution sol
+  --   of the system at t.
+
+    p : Standard_Complex_Poly_Systems.Poly_Sys(hom'range)
+      := Series_and_Homotopies.Eval(hom,t);
+    jm : Standard_Complex_Jaco_Matrices.Jaco_Mat(hom'range,sol'range)
+       := Standard_Complex_Jaco_Matrices.Create(p);
+    f : Standard_Complex_Poly_SysFun.Eval_Poly_Sys(p'range)
+       := Standard_Complex_Poly_SysFun.Create(p);
+    jf : Standard_Complex_Jaco_Matrices.Eval_Jaco_Mat(hom'range,sol'range)
+       := Standard_Complex_Jaco_Matrices.Create(jm);
+    err,rco,res : double_float;
+
+    use Standard_Root_Refiners;
+
+  begin
+    for k in 1..3 loop
+      Standard_Newton_Step(f,jf,sol,err,rco,res);
+      put("  err :"); put(err,3);
+      put("  rco :"); put(rco,3);
+      put("  res :"); put(res,3); new_line;
+    end loop;
+    Standard_Complex_Poly_Systems.Clear(p);
+    Standard_Complex_Poly_SysFun.Clear(f);
+    Standard_Complex_Jaco_Matrices.Clear(jm);
+    Standard_Complex_Jaco_Matrices.Clear(jf);
+  end Standard_Correct;
+
+  procedure Standard_Track
+              ( hom : in Standard_Series_Poly_Systems.Poly_Sys;
+                sol : in out Standard_Complex_Solutions.Solution;
+                verbose : in boolean := false ) is
+
+  -- DESCRIPTION :
+  --   Tracks one path starting at the solution sol using the homotopy hom,
+  --   in standard double precision.
+
+    wrk : Standard_Series_Poly_Systems.Poly_Sys(hom'range);
+    nit : integer32 := 4;
+    srv : Standard_Dense_Series_Vectors.Vector(1..sol.n);
+    eva : Standard_Dense_Series_Vectors.Vector(hom'range);
+    tolcff : constant double_float := 1.0E-12;
+    tolres : constant double_float := 1.0E-5;
+    t,step : double_float := 0.0;
+    max_steps : constant natural32 := 500;
+    wrk_sol : Standard_Complex_Vectors.Vector(1..sol.n) := sol.v;
+
+  begin
+    Standard_Series_Poly_Systems.Copy(hom,wrk);
+    for k in 1..max_steps loop
+      Series_and_Predictors.Newton_Prediction(nit,wrk,wrk_sol,srv,eva,verbose);
+      new_line;
+      put_line("Setting the step size based on the power series ...");
+      step := Series_and_Predictors.Set_Step_Size(eva,tolcff,tolres,verbose);
+      put("The computed step size : "); put(step,3);
+      if step > 0.1
+       then step := 0.1;
+      end if;
+      if t + step <= 1.0 then
+        t := t + step;
+      else
+        step := 1.0 - t;
+        t := 1.0; 
+      end if;
+      put(" t = "); put(t,3);  new_line;
+      wrk_sol := Series_and_Predictors.Predicted_Solution(srv,step);
+      put_line("Shifting the polynomial system ...");
+      Standard_Series_Poly_Systems.Clear(wrk);
+      wrk := Series_and_Homotopies.Shift(hom,t);
+      put_line("Correcting the solution ...");
+      Standard_Correct(hom,t,wrk_sol);
+      exit when (t = 1.0);
+    end loop;
+    put_line("The solution vector :"); put_line(wrk_sol);
+    Standard_Series_Poly_Systems.Clear(wrk);
+  end Standard_Track;
+
+  procedure DoblDobl_Track
+              ( h : in DoblDobl_Series_Poly_Systems.Poly_Sys;
+                s : in out DoblDobl_Complex_Solutions.Solution ) is
+
+  -- DESCRIPTION :
+  --   Tracks one path starting at the solution s using the homotopy h,
+  --   in double double precision.
+
+  begin
+    null;
+  end DoblDobl_Track;
+
+  procedure QuadDobl_Track
+              ( h : in QuadDobl_Series_Poly_Systems.Poly_Sys;
+                s : in out Quaddobl_Complex_Solutions.Solution ) is
+
+  -- DESCRIPTION :
+  --   Tracks one path starting at the solution s using the homotopy h,
+  --   in quad double precision.
+
+  begin
+    null;
+  end QuadDobl_Track;
 
   procedure Standard_Test
               ( nq : in integer32;
@@ -54,6 +171,7 @@ procedure ts_serpath is
     tmp : Solution_List := sols;
     len : constant integer32 := integer32(Length_Of(sols));
     ls : Link_to_Solution;
+    ans : character;
 
   begin
     put_line("The homotopy system :"); put_line(h);
@@ -61,6 +179,10 @@ procedure ts_serpath is
     for i in 1..len loop
       ls := Head_Of(tmp);
       put("Tracking path "); put(i,1); put_line(" ...");
+      Standard_Track(s,ls.all);
+      put("Continue to the next path ? (y/n) ");
+      Ask_Yes_or_No(ans);
+      exit when (ans /= 'y');
       tmp := Tail_Of(tmp);
     end loop;
   end Standard_Test;
@@ -90,6 +212,7 @@ procedure ts_serpath is
     for i in 1..len loop
       ls := Head_Of(tmp);
       put("Tracking path "); put(i,1); put_line(" ...");
+      DoblDobl_Track(s,ls.all);
       tmp := Tail_Of(tmp);
     end loop;
   end DoblDobl_Test;
@@ -119,6 +242,7 @@ procedure ts_serpath is
     for i in 1..len loop
       ls := Head_Of(tmp);
       put("Tracking path "); put(i,1); put_line(" ...");
+      QuadDobl_Track(s,ls.all);
       tmp := Tail_Of(tmp);
     end loop;
   end QuadDobl_Test;
