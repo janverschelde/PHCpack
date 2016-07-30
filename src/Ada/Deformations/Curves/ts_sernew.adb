2181fa187e0cf1ee73480cbbaaff2e6af7b6131b
Jan Verschelde <janv@uic.edu>
2016-07-30 13:49:34 -0500

    added test procedures to test the SVD Newton on matrix series

--- src/Ada/Deformations/Curves/ts_sernew.adb
+++ src/Ada/Deformations/Curves/ts_sernew.adb
@@ -3,6 +3,11 @@ with Communications_with_User;          use Communications_with_User;
 with Standard_Integer_Numbers;          use Standard_Integer_Numbers;
 with Standard_Integer_Numbers_io;       use Standard_Integer_Numbers_io;
 with Standard_Floating_Numbers;         use Standard_Floating_Numbers;
+with Standard_Floating_Numbers_io;      use Standard_Floating_Numbers_io;
+with Double_Double_Numbers;             use Double_Double_Numbers;
+with Double_Double_Numbers_io;          use Double_Double_Numbers_io;
+with Quad_Double_Numbers;               use Quad_Double_Numbers;
+with Quad_Double_Numbers_io;            use Quad_Double_Numbers_io;
 with Symbol_Table;
 with Standard_Dense_Series_Vectors;
 with Standard_Series_Polynomials;
@@ -1185,6 +1190,271 @@ procedure ts_sernew is
     end if;
   end Test_QR_Newton_Steps;
 
+  procedure Test_SVD_Newton_Step
+              ( p : in Standard_Series_Poly_Systems.Poly_Sys;
+                degree : in integer32; 
+                x : in Standard_Dense_Series_Vectors.Vector ) is
+
+  -- DESCRIPTION :
+  --  Does one step with Newton's method on the system p,
+  --  calculating with series x of the given degree,
+  --  in standard double precision.
+
+    ans : character;
+    otp : boolean;
+    info : integer32;
+    tol : constant double_float := 1.0E-12;
+    eva : Standard_Dense_Series_Vectors.Vector(p'range);
+    deg : integer32 := degree;
+    z : Standard_Dense_Series_Vectors.Vector(x'range) := x;
+    rcond : double_float;
+
+  begin
+    new_line;
+    put("Do you want intermediate output ? (y/n) ");
+    Ask_Yes_or_No(ans); otp := (ans = 'y');
+    new_line;
+    Series_and_Polynomials.Set_Degree(z,degree);
+    if otp then
+      Standard_Newton_Matrix_Series.SVD_Newton_Step
+        (standard_output,p,deg,z,info,rcond);
+    else
+      Standard_Newton_Matrix_Series.SVD_Newton_Step(p,deg,z,info,rcond);
+    end if;
+    put("rcond = "); put(rcond,3); new_line;
+    if 1.0 + rcond /= 1.0 then
+      Series_and_Polynomials.Filter(z,tol);
+      put_line("The updated power series solution :");
+      Series_and_Polynomials_io.put(z);
+      eva := Standard_Series_Poly_SysFun.Eval(p,z);
+      Series_and_Polynomials.Filter(eva,tol);
+      put_line("The evaluated solution :");
+      Series_and_Polynomials_io.put(eva);
+    end if;
+  end Test_SVD_Newton_Step;
+
+  procedure Test_SVD_Newton_Step
+              ( p : in DoblDobl_Series_Poly_Systems.Poly_Sys;
+                degree : in integer32; 
+                x : in DoblDobl_Dense_Series_Vectors.Vector ) is
+
+  -- DESCRIPTION :
+  --  Does one step with Newton's method on the system p,
+  --  calculating with series x of the given degree,
+  --  in double double precision.
+
+    ans : character;
+    otp : boolean;
+    info : integer32;
+    tol : constant double_float := 1.0E-12;
+    eva : DoblDobl_Dense_Series_Vectors.Vector(p'range);
+    deg : integer32 := degree;
+    z : DoblDobl_Dense_Series_Vectors.Vector(x'range) := x;
+    rcond : double_double;
+    one : constant double_double := create(1.0);
+
+  begin
+    new_line;
+    put("Do you want intermediate output ? (y/n) ");
+    Ask_Yes_or_No(ans); otp := (ans = 'y');
+    new_line;
+    Series_and_Polynomials.Set_Degree(z,degree);
+    if otp then
+      DoblDobl_Newton_Matrix_Series.SVD_Newton_Step
+        (standard_output,p,deg,z,info,rcond);
+    else
+      DoblDobl_Newton_Matrix_Series.SVD_Newton_Step(p,deg,z,info,rcond);
+    end if;
+    put("rcond = "); put(rcond,3); new_line;
+    if one + rcond /= one then
+      Series_and_Polynomials.Filter(z,tol);
+      put_line("The updated power series solution :");
+      Series_and_Polynomials_io.put(z);
+      eva := DoblDobl_Series_Poly_SysFun.Eval(p,z);
+      Series_and_Polynomials.Filter(eva,tol);
+      put_line("The evaluated solution :");
+      Series_and_Polynomials_io.put(eva);
+    end if;
+  end Test_SVD_Newton_Step;
+
+  procedure Test_SVD_Newton_Step
+              ( p : in QuadDobl_Series_Poly_Systems.Poly_Sys;
+                degree : in integer32; 
+                x : in QuadDobl_Dense_Series_Vectors.Vector ) is
+
+  -- DESCRIPTION :
+  --  Does one step with Newton's method on the system p,
+  --  calculating with series x of the given degree,
+  --  in quad double precision.
+
+    ans : character;
+    otp : boolean;
+    info : integer32;
+    tol : constant double_float := 1.0E-12;
+    eva : QuadDobl_Dense_Series_Vectors.Vector(p'range);
+    deg : integer32 := degree;
+    z : QuadDobl_Dense_Series_Vectors.Vector(x'range) := x;
+    rcond : quad_double;
+    one : constant quad_double := create(1.0);
+
+  begin
+    new_line;
+    put("Do you want intermediate output ? (y/n) ");
+    Ask_Yes_or_No(ans); otp := (ans = 'y');
+    new_line;
+    Series_and_Polynomials.Set_Degree(z,degree);
+    if otp then
+      QuadDobl_Newton_Matrix_Series.SVD_Newton_Step
+        (standard_output,p,deg,z,info,rcond);
+    else
+      QuadDobl_Newton_Matrix_Series.SVD_Newton_Step(p,deg,z,info,rcond);
+    end if;
+    put("rcond = "); put(rcond,3); new_line;
+    if one + rcond /= one then
+      Series_and_Polynomials.Filter(z,tol);
+      put_line("The updated power series solution :");
+      Series_and_Polynomials_io.put(z);
+      eva := QuadDobl_Series_Poly_SysFun.Eval(p,z);
+      Series_and_Polynomials.Filter(eva,tol);
+      put_line("The evaluated solution :");
+      Series_and_Polynomials_io.put(eva);
+    end if;
+  end Test_SVD_Newton_Step;
+
+  procedure Test_SVD_Newton_Steps
+              ( p : in Standard_Series_Poly_Systems.Poly_Sys;
+                degree : in integer32; nbrit : in integer32;
+                x : in Standard_Dense_Series_Vectors.Vector ) is
+
+  -- DESCRIPTION :
+  --  Does as many steps with Newton's method on the system p,
+  --  as the value of nbrit, calculating with series x of the given degree,
+  --  in standard double precision.
+
+    ans : character;
+    otp : boolean;
+    info : integer32;
+    tol : constant double_float := 1.0E-12;
+    eva : Standard_Dense_Series_Vectors.Vector(p'range);
+    deg : integer32 := degree;
+    z : Standard_Dense_Series_Vectors.Vector(x'range) := x;
+    rcond : double_float;
+
+  begin
+    new_line;
+    put("Do you want intermediate output ? (y/n) ");
+    Ask_Yes_or_No(ans); otp := (ans = 'y');
+    new_line;
+    Series_and_Polynomials.Set_Degree(z,degree);
+    if otp then
+      Standard_Newton_Matrix_Series.SVD_Newton_Steps
+        (standard_output,p,deg,nbrit,z,info,rcond);
+    else
+      Standard_Newton_Matrix_Series.SVD_Newton_Steps
+        (p,deg,nbrit,z,info,rcond);
+    end if;
+    put("rcond = "); put(rcond,3); new_line;
+    if 1.0 + rcond /= 1.0 then
+      Series_and_Polynomials.Filter(z,tol);
+      put_line("The updated power series solution :");
+      Series_and_Polynomials_io.put(z);
+      eva := Standard_Series_Poly_SysFun.Eval(p,z);
+      Series_and_Polynomials.Filter(eva,tol);
+      put_line("The evaluated solution :");
+      Series_and_Polynomials_io.put(eva);
+    end if;
+  end Test_SVD_Newton_Steps;
+
+  procedure Test_SVD_Newton_Steps
+              ( p : in DoblDobl_Series_Poly_Systems.Poly_Sys;
+                degree : in integer32; nbrit : in integer32;
+                x : in DoblDobl_Dense_Series_Vectors.Vector ) is
+
+  -- DESCRIPTION :
+  --  Does as many steps with Newton's method on the system p,
+  --  as the value of nbrit, calculating with series x of the given degree,
+  --  in double double precision.
+
+    ans : character;
+    otp : boolean;
+    info : integer32;
+    tol : constant double_float := 1.0E-12;
+    eva : DoblDobl_Dense_Series_Vectors.Vector(p'range);
+    deg : integer32 := degree;
+    z : DoblDobl_Dense_Series_Vectors.Vector(x'range) := x;
+    rcond : double_double;
+    one : constant double_double := create(1.0);
+
+  begin
+    new_line;
+    put("Do you want intermediate output ? (y/n) ");
+    Ask_Yes_or_No(ans); otp := (ans = 'y');
+    new_line;
+    Series_and_Polynomials.Set_Degree(z,degree);
+    if otp then
+      DoblDobl_Newton_Matrix_Series.SVD_Newton_Steps
+        (standard_output,p,deg,nbrit,z,info,rcond);
+    else
+      DoblDobl_Newton_Matrix_Series.SVD_Newton_Steps
+        (p,deg,nbrit,z,info,rcond);
+    end if;
+    put("rcond = "); put(rcond,3); new_line;
+    if one + rcond /= one then
+      Series_and_Polynomials.Filter(z,tol);
+      put_line("The updated power series solution :");
+      Series_and_Polynomials_io.put(z);
+      eva := DoblDobl_Series_Poly_SysFun.Eval(p,z);
+      Series_and_Polynomials.Filter(eva,tol);
+      put_line("The evaluated solution :");
+      Series_and_Polynomials_io.put(eva);
+    end if;
+  end Test_SVD_Newton_Steps;
+
+  procedure Test_SVD_Newton_Steps
+              ( p : in QuadDobl_Series_Poly_Systems.Poly_Sys;
+                degree : in integer32; nbrit : in integer32;
+                x : in QuadDobl_Dense_Series_Vectors.Vector ) is
+
+  -- DESCRIPTION :
+  --  Does as many steps with Newton's method on the system p,
+  --  as the value of nbrit, calculating with series x of the given degree,
+  --  in quad double precision.
+
+    ans : character;
+    otp : boolean;
+    info : integer32;
+    tol : constant double_float := 1.0E-12;
+    eva : QuadDobl_Dense_Series_Vectors.Vector(p'range);
+    deg : integer32 := degree;
+    z : QuadDobl_Dense_Series_Vectors.Vector(x'range) := x;
+    rcond : quad_double;
+    one : constant quad_double := create(1.0);
+
+  begin
+    new_line;
+    put("Do you want intermediate output ? (y/n) ");
+    Ask_Yes_or_No(ans); otp := (ans = 'y');
+    new_line;
+    Series_and_Polynomials.Set_Degree(z,degree);
+    if otp then
+      QuadDobl_Newton_Matrix_Series.SVD_Newton_Steps
+        (standard_output,p,deg,nbrit,z,info,rcond);
+    else
+      QuadDobl_Newton_Matrix_Series.SVD_Newton_Steps
+        (p,deg,nbrit,z,info,rcond);
+    end if;
+    put("rcond = "); put(rcond,3); new_line;
+    if one + rcond /= one then
+      Series_and_Polynomials.Filter(z,tol);
+      put_line("The updated power series solution :");
+      Series_and_Polynomials_io.put(z);
+      eva := QuadDobl_Series_Poly_SysFun.Eval(p,z);
+      Series_and_Polynomials.Filter(eva,tol);
+      put_line("The evaluated solution :");
+      Series_and_Polynomials_io.put(eva);
+    end if;
+  end Test_SVD_Newton_Steps;
+
   procedure Standard_Test_LU_Newton is
 
   -- DESCRIPTION :
@@ -1395,6 +1665,114 @@ procedure ts_sernew is
     end if;
   end QuadDobl_Test_QR_Newton;
 
+  procedure Standard_Test_SVD_Newton is
+
+  -- DESCRIPTION :
+  --   Prompts the user for a system of series coefficients
+  --   and for an initial series approximation.
+  --   Then the test on the SVD Newton method are done,
+  --   in standard double precision.
+
+    ls : Standard_Series_Poly_Systems.Link_to_Poly_Sys;
+    sol : Standard_Dense_Series_Vectors.Link_to_Vector;
+    idx,degree,nbr : integer32 := 0;
+    dim : integer32;
+
+    use Standard_Series_Polynomials;
+
+  begin
+    new_line;
+    put("Give the index of the series variable : "); get(idx);
+    new_line;
+    Series_and_Polynomials_io.get(ls,idx);
+    new_line;
+    dim := integer32(Number_of_Unknowns(ls(ls'first)));
+    put("The number of variables : "); put(dim,1); new_line;
+    put_line("The polynomial system : ");
+    Series_and_Polynomials_io.put(ls.all,idx);
+    Read_Series_Vector(sol,dim,idx);
+    new_line;
+    put("Give the start degree of the computations : "); get(degree);
+    new_line;
+    put("Give the number of Newton steps : "); get(nbr);
+    if nbr = 1
+     then Test_SVD_Newton_Step(ls.all,degree,sol.all);
+     else Test_SVD_Newton_Steps(ls.all,degree,nbr,sol.all);
+    end if;
+  end Standard_Test_SVD_Newton;
+
+  procedure DoblDobl_Test_SVD_Newton is
+
+  -- DESCRIPTION :
+  --   Prompts the user for a system of series coefficients
+  --   and for an initial series approximation.
+  --   Then the test on the SVD Newton method are done,
+  --   in double double precision.
+
+    ls : DoblDobl_Series_Poly_Systems.Link_to_Poly_Sys;
+    sol : DoblDobl_Dense_Series_Vectors.Link_to_Vector;
+    idx,degree,nbr : integer32 := 0;
+    dim : integer32;
+
+    use DoblDobl_Series_Polynomials;
+
+  begin
+    new_line;
+    put("Give the index of the series variable : "); get(idx);
+    new_line;
+    Series_and_Polynomials_io.get(ls,idx);
+    new_line;
+    dim := integer32(Number_of_Unknowns(ls(ls'first)));
+    put("The number of variables : "); put(dim,1); new_line;
+    put_line("The polynomial system : ");
+    Series_and_Polynomials_io.put(ls.all,idx);
+    Read_Series_Vector(sol,dim,idx);
+    new_line;
+    put("Give the start degree of the computations : "); get(degree);
+    new_line;
+    put("Give the number of Newton steps : "); get(nbr);
+    if nbr = 1
+     then Test_SVD_Newton_Step(ls.all,degree,sol.all);
+     else Test_SVD_Newton_Steps(ls.all,degree,nbr,sol.all);
+    end if;
+  end DoblDobl_Test_SVD_Newton;
+
+  procedure QuadDobl_Test_SVD_Newton is
+
+  -- DESCRIPTION :
+  --   Prompts the user for a system of series coefficients
+  --   and for an initial series approximation.
+  --   Then the test on the SVD Newton method are done,
+  --   in quad double precision.
+
+    ls : QuadDobl_Series_Poly_Systems.Link_to_Poly_Sys;
+    sol : QuadDobl_Dense_Series_Vectors.Link_to_Vector;
+    idx,degree,nbr : integer32 := 0;
+    dim : integer32;
+
+    use QuadDobl_Series_Polynomials;
+
+  begin
+    new_line;
+    put("Give the index of the series variable : "); get(idx);
+    new_line;
+    Series_and_Polynomials_io.get(ls,idx);
+    new_line;
+    dim := integer32(Number_of_Unknowns(ls(ls'first)));
+    put("The number of variables : "); put(dim,1); new_line;
+    put_line("The polynomial system : ");
+    Series_and_Polynomials_io.put(ls.all,idx);
+    Read_Series_Vector(sol,dim,idx);
+    new_line;
+    put("Give the start degree of the computations : "); get(degree);
+    new_line;
+    put("Give the number of Newton steps : "); get(nbr);
+    if nbr = 1
+     then Test_SVD_Newton_Step(ls.all,degree,sol.all);
+     else Test_SVD_Newton_Steps(ls.all,degree,nbr,sol.all);
+    end if;
+  end QuadDobl_Test_SVD_Newton;
+
   procedure Main is
 
   -- DESCRIPTION :
@@ -1406,9 +1784,10 @@ procedure ts_sernew is
     new_line;
     put_line("MENU to test Newton's method on truncated power series :");
     put_line("  1. test LU factorization for square systems;");
-    put_line("  2. test QR decomposition for overdetermined systems.");
-    put("Type 1 or 2 to select the test : ");
-    Ask_Alternative(ans,"12");
+    put_line("  2. test QR decomposition for overdetermined systems;");
+    put_line("  3. test SVD decomposition om matrix series.");
+    put("Type 1, 2, or 3 to select the test : ");
+    Ask_Alternative(ans,"123");
     new_line;
     put_line("MENU to set the working precision :");
     put_line("  0. standard double precision;");
@@ -1429,6 +1808,13 @@ procedure ts_sernew is
           when '0' => Standard_Test_QR_Newton;
           when '1' => DoblDobl_Test_QR_Newton;
           when '2' => QuadDobl_Test_QR_Newton;
+          when others => null;
+        end case;
+      when '3' =>
+        case prc is
+          when '0' => Standard_Test_SVD_Newton;
+          when '1' => DoblDobl_Test_SVD_Newton;
+          when '2' => QuadDobl_Test_SVD_Newton;
           when others => null;
         end case;
       when others => null;
