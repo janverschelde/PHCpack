d53164dd208da37b37f731970978503c2be3f784
Jan Verschelde <janv@uic.edu>
2016-07-30 20:17:06 -0500

    added wrappers to SVD Newton methods on matrix series

--- src/Ada/Deformations/Curves/power_series_methods.adb
+++ src/Ada/Deformations/Curves/power_series_methods.adb
@@ -1,6 +1,8 @@
 with Communications_with_User;           use Communications_with_User;
 with Standard_Integer_Numbers_io;        use Standard_Integer_Numbers_io;
-with Standard_Floating_Numbers;          use Standard_Floating_Numbers;
+with Standard_Floating_Numbers_io;       use Standard_Floating_Numbers_io;
+with Double_Double_Numbers_io;           use Double_Double_Numbers_io;
+with Quad_Double_Numbers_io;             use Quad_Double_Numbers_io;
 with Standard_Series_Poly_SysFun;
 with DoblDobl_Series_Poly_SysFun;
 with QuadDobl_Series_Poly_SysFun;
@@ -258,6 +260,128 @@ package body Power_Series_Methods is
     end if;
   end Run_QR_Newton;
 
+  procedure Run_SVD_Newton
+             ( nbrit : in integer32;
+               p : in Standard_Series_Poly_Systems.Poly_Sys;
+               s : in out Standard_Dense_Series_Vectors.Vector;
+               rcond : out double_float; verbose : in boolean := false ) is
+  begin
+    Run_SVD_Newton(standard_output,nbrit,p,s,rcond,verbose);
+  end Run_SVD_Newton;
+
+  procedure Run_SVD_Newton
+             ( file : in file_type; nbrit : in integer32;
+               p : in Standard_Series_Poly_Systems.Poly_Sys;
+               s : in out Standard_Dense_Series_Vectors.Vector;
+               rcond : out double_float; verbose : in boolean := false ) is
+
+    use Standard_Newton_Matrix_Series; -- use Standard_Newton_Series;
+
+    order : integer32 := 1;
+    info : integer32;
+    tol : constant double_float := 1.0E-12;
+    eva : Standard_Dense_Series_Vectors.Vector(p'range);
+
+  begin
+    if not verbose then
+      SVD_Newton_Steps(p,order,nbrit,s,info,rcond);
+    else
+      SVD_Newton_Steps(file,p,order,nbrit,s,info,rcond);
+      put(file,"rcond = "); put(file,rcond,3); new_line(file);
+      if 1.0 + rcond /= 1.0 then
+        Series_and_Polynomials.Filter(s,tol);
+        put_line(file,"The updated power series solution :");
+        Series_and_Polynomials_io.put(file,s);
+        eva := Standard_Series_Poly_SysFun.Eval(p,s);
+        Series_and_Polynomials.Filter(eva,tol);
+        put_line(file,"The evaluated solution :");
+        Series_and_Polynomials_io.put(file,eva);
+      end if;
+    end if;
+  end Run_SVD_Newton;
+
+  procedure Run_SVD_Newton
+             ( nbrit : in integer32;
+               p : in DoblDobl_Series_Poly_Systems.Poly_Sys;
+               s : in out DoblDobl_Dense_Series_Vectors.Vector;
+               rcond : out double_double; verbose : in boolean := false ) is
+  begin
+    Run_SVD_Newton(standard_output,nbrit,p,s,rcond,verbose);
+  end Run_SVD_Newton;
+
+  procedure Run_SVD_Newton
+             ( file : in file_type; nbrit : in integer32;
+               p : in DoblDobl_Series_Poly_Systems.Poly_Sys;
+               s : in out DoblDobl_Dense_Series_Vectors.Vector;
+               rcond : out double_double; verbose : in boolean := false ) is
+
+    use DoblDobl_Newton_Matrix_Series; -- use DoblDobl_Newton_Series;
+
+    order : integer32 := 1;
+    info : integer32;
+    tol : constant double_float := 1.0E-24;
+    eva : DoblDobl_Dense_Series_Vectors.Vector(p'range);
+    one : constant double_double := create(1.0);
+
+  begin
+    if not verbose then
+      SVD_Newton_Steps(p,order,nbrit,s,info,rcond);
+    else
+      SVD_Newton_Steps(file,p,order,nbrit,s,info,rcond);
+      put(file,"rcond = "); put(file,rcond,3); new_line(file);
+      if one + rcond /= one then 
+        Series_and_Polynomials.Filter(s,tol);
+        put_line(file,"The updated power series solution :");
+        Series_and_Polynomials_io.put(file,s);
+        eva := DoblDobl_Series_Poly_SysFun.Eval(p,s);
+        Series_and_Polynomials.Filter(eva,tol);
+        put_line(file,"The evaluated solution :");
+        Series_and_Polynomials_io.put(file,eva);
+      end if;
+    end if;
+  end Run_SVD_Newton;
+
+  procedure Run_SVD_Newton
+             ( nbrit : in integer32;
+               p : in QuadDobl_Series_Poly_Systems.Poly_Sys;
+               s : in out QuadDobl_Dense_Series_Vectors.Vector;
+               rcond : out quad_double; verbose : in boolean := false ) is
+  begin
+    Run_SVD_Newton(standard_output,nbrit,p,s,rcond,verbose);
+  end Run_SVD_Newton;
+
+  procedure Run_SVD_Newton
+             ( file : in file_type; nbrit : in integer32;
+               p : in QuadDobl_Series_Poly_Systems.Poly_Sys;
+               s : in out QuadDobl_Dense_Series_Vectors.Vector;
+               rcond : out quad_double; verbose : in boolean := false ) is
+
+    use QuadDobl_Newton_Matrix_Series; -- use QuadDobl_Newton_Series;
+
+    order : integer32 := 1;
+    info : integer32;
+    tol : constant double_float := 1.0E-32;
+    eva : QuadDobl_Dense_Series_Vectors.Vector(p'range);
+    one : constant quad_double := create(1.0);
+
+  begin
+    if not verbose then
+      SVD_Newton_Steps(p,order,nbrit,s,info,rcond);
+    else
+      SVD_Newton_Steps(file,p,order,nbrit,s,info,rcond);
+      put(file,"rcond = "); put(file,rcond,3); new_line(file);
+      if one + rcond /= one then
+        Series_and_Polynomials.Filter(s,tol);
+        put_line(file,"The updated power series solution :");
+        Series_and_Polynomials_io.put(file,s);
+        eva := QuadDobl_Series_Poly_SysFun.Eval(p,s);
+        Series_and_Polynomials.Filter(eva,tol);
+        put_line(file,"The evaluated solution :");
+        Series_and_Polynomials_io.put(file,eva);
+      end if;
+    end if;
+  end Run_SVD_Newton;
+
   procedure Run_LU_Newton
               ( nbrit : in integer32;
                 p : in Standard_Series_Poly_Systems.Poly_Sys;
@@ -491,5 +615,134 @@ package body Power_Series_Methods is
       Run_QR_Newton(file,nbrit,p,v(i).all,verbose);
     end loop;
   end Run_QR_Newton;
+
+  procedure Run_SVD_Newton
+              ( nbrit : in integer32;
+                p : in Standard_Series_Poly_Systems.Poly_Sys;
+                v : in Standard_Dense_Series_VecVecs.VecVec;
+                verbose : in boolean := false;
+                pause : in boolean := false ) is
+
+    ans : character;
+    rcond : double_float;
+
+  begin
+    for i in v'range loop
+      if verbose then
+        put("Running on solution "); put(i,1); put_line(" ...");
+      end if;
+      Run_SVD_Newton(nbrit,p,v(i).all,rcond,verbose);
+      if verbose then
+        if pause then
+          put("Continue ? (y/n) "); Ask_Yes_or_No(ans);
+          exit when (ans /= 'y');
+        end if;
+      end if;
+    end loop;
+  end Run_SVD_Newton;
+
+  procedure Run_SVD_Newton
+              ( nbrit : in integer32;
+                p : in DoblDobl_Series_Poly_Systems.Poly_Sys;
+                v : in DoblDobl_Dense_Series_VecVecs.VecVec;
+                verbose : in boolean := false;
+                pause : in boolean := false ) is
+
+    ans : character;
+    rcond : double_double;
+
+  begin
+    for i in v'range loop
+      if verbose then
+        put("Running on solution "); put(i,1); put_line(" ...");
+      end if;
+      Run_SVD_Newton(nbrit,p,v(i).all,rcond,verbose);
+      if verbose then
+        if pause then
+          put("Continue ? (y/n) "); Ask_Yes_or_No(ans);
+          exit when (ans /= 'y');
+        end if;
+      end if;
+    end loop;
+  end Run_SVD_Newton;
+
+  procedure Run_SVD_Newton
+              ( nbrit : in integer32;
+                p : in QuadDobl_Series_Poly_Systems.Poly_Sys;
+                v : in QuadDobl_Dense_Series_VecVecs.VecVec;
+                verbose : in boolean := false;
+                pause : in boolean := false ) is
+
+    ans : character;
+    rcond : quad_double;
+
+  begin
+    for i in v'range loop
+      if verbose then
+        put("Running on solution "); put(i,1); put_line(" ...");
+      end if;
+      Run_SVD_Newton(nbrit,p,v(i).all,rcond,verbose);
+      if verbose then
+        if pause then
+          put("Continue ? (y/n) "); Ask_Yes_or_No(ans);
+          exit when (ans /= 'y');
+        end if;
+      end if;
+    end loop;
+  end Run_SVD_Newton;
+
+  procedure Run_SVD_Newton
+              ( file : in file_type; nbrit : in integer32;
+                p : in Standard_Series_Poly_Systems.Poly_Sys;
+                v : in Standard_Dense_Series_VecVecs.VecVec;
+                verbose : in boolean := false ) is
+
+    rcond : double_float;
+
+  begin
+    for i in v'range loop
+      if verbose then
+        put(file,"Running on solution ");
+        put(file,i,1); put_line(file," ...");
+      end if;
+      Run_SVD_Newton(file,nbrit,p,v(i).all,rcond,verbose);
+    end loop;
+  end Run_SVD_Newton;
+
+  procedure Run_SVD_Newton
+              ( file : in file_type; nbrit : in integer32;
+                p : in DoblDobl_Series_Poly_Systems.Poly_Sys;
+                v : in DoblDobl_Dense_Series_VecVecs.VecVec;
+                verbose : in boolean := false ) is
+
+    rcond : double_double;
+
+  begin
+    for i in v'range loop
+      if verbose then
+        put(file,"Running on solution ");
+        put(file,i,1); put_line(file," ...");
+      end if;
+      Run_SVD_Newton(file,nbrit,p,v(i).all,rcond,verbose);
+    end loop;
+  end Run_SVD_Newton;
+
+  procedure Run_SVD_Newton
+              ( file : in file_type; nbrit : in integer32;
+                p : in QuadDobl_Series_Poly_Systems.Poly_Sys;
+                v : in QuadDobl_Dense_Series_VecVecs.VecVec;
+                verbose : in boolean := false ) is
+
+    rcond : quad_double;
+
+  begin
+    for i in v'range loop
+      if verbose then
+        put(file,"Running on solution ");
+        put(file,i,1); put_line(file," ...");
+      end if;
+      Run_SVD_Newton(file,nbrit,p,v(i).all,rcond,verbose);
+    end loop;
+  end Run_SVD_Newton;
 
 end Power_Series_Methods;
--- src/Ada/Deformations/Curves/power_series_methods.ads
+++ src/Ada/Deformations/Curves/power_series_methods.ads
@@ -1,5 +1,8 @@
 with text_io;                            use text_io;
 with Standard_Integer_Numbers;           use Standard_Integer_Numbers;
+with Standard_Floating_Numbers;          use Standard_Floating_Numbers;
+with Double_Double_Numbers;              use Double_Double_Numbers;
+with Quad_Double_Numbers;                use Quad_Double_Numbers;
 with Standard_Dense_Series_Vectors;
 with Standard_Dense_Series_VecVecs;
 with DoblDobl_Dense_Series_Vectors;
@@ -18,9 +21,9 @@ package Power_Series_Methods is
 --   The procedures in this packages split in several categories:
 --   (1) whether to be verbose or not;
 --   (2) which precision: double, double double, or quad double;
---   (3) whether to use LU factorization or QR decomposition;
+--   (3) whether to use LU factorization, QR decomposition, or SVD;
 --   (4) whether to work on one vector or on a vector of vectors of series;
---   Depending on the choices, there are 2x3x2x2 = 24 procedures.
+--   Depending on the choices, there are 2x3x3x2 = 36 procedures.
 
 -- NEWTON on ONE VECTOR of POWER SERIES :
 
@@ -122,6 +125,57 @@ package Power_Series_Methods is
   -- ON RETURN :
   --   s        a power series solution to p, up to some order.
 
+  procedure Run_SVD_Newton
+              ( nbrit : in integer32;
+                p : in Standard_Series_Poly_Systems.Poly_Sys;
+                s : in out Standard_Dense_Series_Vectors.Vector;
+                rcond : out double_float; verbose : in boolean := false );
+  procedure Run_SVD_Newton
+              ( file : in file_type; nbrit : in integer32;
+                p : in Standard_Series_Poly_Systems.Poly_Sys;
+                s : in out Standard_Dense_Series_Vectors.Vector;
+                rcond : out double_float; verbose : in boolean := false );
+  procedure Run_SVD_Newton
+              ( nbrit : in integer32;
+                p : in DoblDobl_Series_Poly_Systems.Poly_Sys;
+                s : in out DoblDobl_Dense_Series_Vectors.Vector;
+                rcond : out double_double; verbose : in boolean := false );
+  procedure Run_SVD_Newton
+              ( file : in file_type; nbrit : in integer32;
+                p : in DoblDobl_Series_Poly_Systems.Poly_Sys;
+                s : in out DoblDobl_Dense_Series_Vectors.Vector;
+                rcond : out double_double; verbose : in boolean := false );
+  procedure Run_SVD_Newton
+              ( nbrit : in integer32;
+                p : in QuadDobl_Series_Poly_Systems.Poly_Sys;
+                s : in out QuadDobl_Dense_Series_Vectors.Vector;
+                rcond : out quad_double; verbose : in boolean := false );
+  procedure Run_SVD_Newton
+              ( file : in file_type; nbrit : in integer32;
+                p : in QuadDobl_Series_Poly_Systems.Poly_Sys;
+                s : in out QuadDobl_Dense_Series_Vectors.Vector;
+                rcond : out quad_double; verbose : in boolean := false );
+
+  -- DESCRIPTION :
+  --   Applies as many steps with Newton's method as the value of nbrit,
+  --   starting at the solution in s to the system p,
+  --   applying Singular Value Decomposition to compute the Newton updates,
+  --   in standard double, double double, or quad double precision.
+
+  -- ON ENTRY :
+  --   file     must be opened for output, to write results,
+  --            if not provided, then output is written to screen;
+  --   nbrit    number of new iterations;
+  --   p        a polynomial system with series coefficients;
+  --   s        leading coefficients for a power series solution;
+  --   verbose  indicates if results of intermediate Newton steps
+  --            need to be written to file or to standard output.
+
+  -- ON RETURN :
+  --   s        a power series solution to p, up to some order;
+  --   rcond    the inverse condition number computed from the singular values,
+  --            if 1.0 + rcond = 1.0, then the problem is singular.
+
 -- NEWTON on a VECTOR of VECTOR of POWER SERIES :
 
   procedure Run_LU_Newton
@@ -213,6 +267,58 @@ package Power_Series_Methods is
   -- DESCRIPTION :
   --   Runs Newton's method on the vector of power series in v.
   --   using QR decomposition to compute the updates,
+  --   in double, double double, or quad double precision.
+
+  -- ON ENTRY :
+  --   file     must be opened for output, to write results,
+  --            if not provided, then output is written to screen;
+  --   nbrit    number of new iterations;
+  --   p        a polynomial system with series coefficients;
+  --   v        leading coefficients for power series solutions;
+  --   verbose  indicates if results of intermediate Newton steps
+  --            need to be written to file or to standard output;
+  --   pause   
+
+  -- ON RETURN :
+  --   s        updated power series solutions to p, up to some order.
+
+  procedure Run_SVD_Newton
+              ( nbrit : in integer32;
+                p : in Standard_Series_Poly_Systems.Poly_Sys;
+                v : in Standard_Dense_Series_VecVecs.VecVec;
+                verbose : in boolean := false;
+                pause : in boolean := false );
+  procedure Run_SVD_Newton
+              ( nbrit : in integer32;
+                p : in DoblDobl_Series_Poly_Systems.Poly_Sys;
+                v : in DoblDobl_Dense_Series_VecVecs.VecVec;
+                verbose : in boolean := false;
+                pause : in boolean := false );
+  procedure Run_SVD_Newton
+              ( nbrit : in integer32;
+                p : in QuadDobl_Series_Poly_Systems.Poly_Sys;
+                v : in QuadDobl_Dense_Series_VecVecs.VecVec;
+                verbose : in boolean := false;
+                pause : in boolean := false );
+  procedure Run_SVD_Newton
+              ( file : in file_type; nbrit : in integer32;
+                p : in Standard_Series_Poly_Systems.Poly_Sys;
+                v : in Standard_Dense_Series_VecVecs.VecVec;
+                verbose : in boolean := false );
+  procedure Run_SVD_Newton
+              ( file : in file_type; nbrit : in integer32;
+                p : in DoblDobl_Series_Poly_Systems.Poly_Sys;
+                v : in DoblDobl_Dense_Series_VecVecs.VecVec;
+                verbose : in boolean := false );
+  procedure Run_SVD_Newton
+              ( file : in file_type; nbrit : in integer32;
+                p : in QuadDobl_Series_Poly_Systems.Poly_Sys;
+                v : in QuadDobl_Dense_Series_VecVecs.VecVec;
+                verbose : in boolean := false );
+
+  -- DESCRIPTION :
+  --   Runs Newton's method on the vector of power series in v.
+  --   using the singular value decomposition to compute the updates,
   --   in double, double double, or quad double precision.
 
   -- ON ENTRY :
