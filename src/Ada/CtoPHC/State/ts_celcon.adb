84251ed19d9d216fb9bddd407df8dda097eef7e8
Jan Verschelde <janv@uic.edu>
2016-10-06 23:15:41 -0500

    completed tests on integer cells container

--- src/Ada/CtoPHC/State/ts_celcon.adb
+++ src/Ada/CtoPHC/State/ts_celcon.adb
@@ -11,6 +11,7 @@ with Floating_Lifting_Utilities;
 with Integer_Lifting_Utilities;
 with Arrays_of_Floating_Vector_Lists;
 with Arrays_of_Integer_Vector_Lists;
+with Arrays_of_Integer_Vector_Lists_io;
 with Floating_Mixed_Subdivisions;
 with Floating_Mixed_Subdivisions_io;     use Floating_Mixed_Subdivisions_io;
 with Integer_Mixed_Subdivisions;
@@ -74,12 +75,52 @@ procedure ts_celcon is
     return res;
   end Get_Cell;
 
-  procedure Retrieve_and_Show_Cell
+  procedure Integer_Retrieve_and_Show_Cell
               ( k,len,n : in natural32;
                 mix : in Standard_Integer_Vectors.Link_to_Vector ) is
 
   -- DESCRIPTION :
-  --   Retrieves the k-th cell from the container, and if no failure,
+  --   Retrieves the k-th cell from the container for cells induced
+  --   by an integer valued lifting function, and if no failure,
+  --   the the cell is shown to the user.
+
+  -- ON INPUT :
+  --   k        index to desired cell, must be >= 0;
+  --   len      len = Cells_Container.Length;
+  --   n        dimension of the lifted supports;
+  --   mix      type of mixture needed for volume computation.
+
+    use Integer_Mixed_Subdivisions;
+
+    mic : Mixed_Cell;
+    fail : boolean;
+    mv : natural32;
+    cnt,lab,normal : Standard_Integer_Vectors.Link_to_Vector;
+
+  begin
+    Integer_Cells_Container.Retrieve(k,mic,fail);
+    if fail then
+      put("Retrieval of cell "); put(k,1); put(" failed, is ");
+      put(k,1); put(" > "); put(len,1); put_line(" ?");
+    else
+      put("Retrieval of cell "); put(k,1); put_line(" succeeded.");
+      put(n-1,mix.all,mic,mv);
+      put(" mixed volume of the cell : "); put(mv,1); new_line;
+      Integer_Cells_Container.Retrieve_Mixed_Cell(k,fail,cnt,lab,normal);
+      put("Number of points in each support of the cell :");
+      put(cnt); new_line;
+      put("Labels of the points : "); put(lab); new_line;
+      put_line("The inner normal to the cell : "); put_line(normal);
+    end if;
+  end Integer_Retrieve_and_Show_Cell;
+
+  procedure Floating_Retrieve_and_Show_Cell
+              ( k,len,n : in natural32;
+                mix : in Standard_Integer_Vectors.Link_to_Vector ) is
+
+  -- DESCRIPTION :
+  --   Retrieves the k-th cell from the container for cells induced
+  --   by a floating-point lifting function, and if no failure,
   --   the the cell is shown to the user.
 
   -- ON INPUT :
@@ -111,12 +152,59 @@ procedure ts_celcon is
       put("Labels of the points : "); put(lab); new_line;
       put_line("The inner normal to the cell : "); put_line(normal);
     end if;
-  end Retrieve_and_Show_Cell;
+  end Floating_Retrieve_and_Show_Cell;
 
-  procedure Test_Retrievals is
+  procedure Integer_Test_Retrievals is
 
   -- DESRIPTION :
-  --   Test the retrieval of the cells in the container.
+  --   Test the retrieval of the cells in the container,
+  --   for cells induced by an integer value lifting function.
+
+    use Standard_Integer_Vectors;
+    use Arrays_of_Integer_Vector_Lists;
+    use Arrays_of_Integer_Vector_Lists_io;
+
+    mix : Standard_Integer_Vectors.Link_to_Vector;
+    len,n,k : natural32 := 0;
+    ans : character;
+    lif : Link_to_Array_of_Lists;
+
+  begin
+    new_line;
+    put_line("Testing the retrieval of cells...");
+    new_line;
+    len := Integer_Cells_Container.Length;
+    put("Number of cells in the container : "); put(len,1); new_line;
+    n := Integer_Cells_Container.Dimension;
+    put("Dimension of the points in the cells : "); put(n,1); new_line;
+    mix := Integer_Cells_Container.Type_of_Mixture;
+    if mix = null then
+      put_line("No type of mixture returned, no retrieving.");
+    else
+      put("Type of mixture : "); put(mix); new_line;
+      put("Do you want to see the lifted supports ? (y/n) ");
+      Ask_Yes_or_No(ans);
+      if ans = 'y' then
+        lif := Integer_Cells_Container.Lifted_Supports;
+        if lif /= null
+         then put_line("The lifted supports :"); put(lif.all);
+         else put_line("The lifted supports are empty...");
+        end if;
+      end if;
+      loop
+        put("Give number of a cell to retrieve (0 to exit) : ");
+        get(k);
+        exit when (k = 0);
+        Integer_Retrieve_and_Show_Cell(k,len,n,mix);
+      end loop;
+    end if;
+  end Integer_Test_Retrievals;
+
+  procedure Floating_Test_Retrievals is
+
+  -- DESRIPTION :
+  --   Test the retrieval of the cells in the container,
+  --   for cells induced by a floating-point lifting function.
 
     use Standard_Integer_Vectors;
     use Arrays_of_Floating_Vector_Lists;
@@ -152,16 +240,17 @@ procedure ts_celcon is
         put("Give number of a cell to retrieve (0 to exit) : ");
         get(k);
         exit when (k = 0);
-        Retrieve_and_Show_Cell(k,len,n,mix);
+        Floating_Retrieve_and_Show_Cell(k,len,n,mix);
       end loop;
     end if;
-  end Test_Retrievals;
+  end Floating_Test_Retrievals;
 
-  procedure Read_and_Initialize is
+  procedure Floating_Read_and_Initialize is
 
   -- DESCRIPTION :
   --   Prompts the user to supply a file name for the cells,
-  --   and initializes the cells container.
+  --   and initializes the cells container, for cells induced
+  --   by a floating-point lifting function.
 
     use Arrays_of_Floating_Vector_Lists;
     use Floating_Lifting_Utilities;
@@ -181,7 +270,34 @@ procedure ts_celcon is
     r := mix'last;
     lif := new Array_of_Lists'(Lifted_Supports(r,mcc));
     Cells_Container.Initialize(mix,lif,mcc);
-  end Read_and_Initialize;
+  end Floating_Read_and_Initialize;
+
+  procedure Integer_Read_and_Initialize is
+
+  -- DESCRIPTION :
+  --   Prompts the user to supply a file name for the cells,
+  --   and initializes the cells container, for cells induced
+  --   by an integer valued lifting function.
+
+    use Arrays_of_Integer_Vector_Lists;
+    use Integer_Lifting_Utilities;
+    use Integer_Mixed_Subdivisions;
+
+    file : file_type;
+    n,m : natural32 := 0;
+    r : integer32;
+    mix : Standard_Integer_Vectors.Link_to_Vector;
+    lif : Link_to_Array_of_Lists;
+    mcc : Mixed_Subdivision;
+
+  begin
+    put_line("Reading the file name for a mixed-cell configuration.");
+    Read_Name_and_Open_File(file);
+    get(file,n,m,mix,mcc);
+    r := mix'last;
+    lif := new Array_of_Lists'(Lifted_Supports(r,mcc));
+    Integer_Cells_Container.Initialize(mix,lif,mcc);
+  end Integer_Read_and_Initialize;
 
   procedure Floating_Read_and_Construct is
 
@@ -268,9 +384,21 @@ procedure ts_celcon is
   -- DESCRIPTION :
   --   Test on cell configurations defined by integer lifting functions.
 
+    ans : character;
+
   begin
     new_line;
-    Integer_Read_and_Construct;
+    put_line("Choose one of the following : ");
+    put_line("  1. initialize cells container and test retrievals;");
+    put_line("  2. selectively append cells to the cells container.");
+    put("Type 1 or 2 to make your choice : "); Ask_Alternative(ans,"12");
+    new_line;
+    if ans = '1'
+     then Integer_Read_and_Initialize;
+     else Integer_Read_and_Construct;
+    end if;
+    Integer_Test_Retrievals;
+    Integer_Cells_Container.Clear;
   end Integer_Test;
 
   procedure Floating_Test is
@@ -288,10 +416,10 @@ procedure ts_celcon is
     put("Type 1 or 2 to make your choice : "); Ask_Alternative(ans,"12");
     new_line;
     if ans = '1'
-     then Read_and_Initialize;
+     then Floating_Read_and_Initialize;
      else Floating_Read_and_Construct;
     end if;
-    Test_Retrievals;
+    Floating_Test_Retrievals;
     Cells_Container.Clear;
   end Floating_Test;
 
