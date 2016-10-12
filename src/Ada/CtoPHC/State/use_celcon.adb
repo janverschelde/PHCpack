005c92f4b6116209e0a5bd9bd2596396b30e08e7
Jan Verschelde <janv@uic.edu>
2016-10-11 22:24:04 -0500

    added new jobs to interface to the integer cells container

--- src/Ada/CtoPHC/State/use_celcon.adb
+++ src/Ada/CtoPHC/State/use_celcon.adb
@@ -7,9 +7,10 @@ with Standard_Integer_Numbers_io;       use Standard_Integer_Numbers_io;
 with Standard_Floating_Numbers;         use Standard_Floating_Numbers;
 with Standard_Integer_Vectors;
 with Standard_Floating_Vectors;
-with Lists_of_Floating_Vectors;         use Lists_of_Floating_Vectors;
+with Lists_of_Integer_Vectors;
+with Lists_of_Floating_Vectors;
 with Arrays_of_Integer_Vector_Lists;
-with Arrays_of_Floating_Vector_Lists;   use Arrays_of_Floating_Vector_Lists;
+with Arrays_of_Floating_Vector_Lists;
 with Standard_Complex_Poly_Systems;
 with Standard_Complex_Poly_Systems_io;  use Standard_Complex_Poly_Systems_io;
 with DoblDobl_Complex_Poly_Systems;
@@ -23,10 +24,13 @@ with Standard_Complex_Solutions;
 with DoblDobl_Complex_Solutions;
 with QuadDobl_Complex_Solutions;
 with Supports_of_Polynomial_Systems;
-with Floating_Mixed_Subdivisions;       use Floating_Mixed_Subdivisions;
+with Floating_Mixed_Subdivisions;
 with Floating_Mixed_Subdivisions_io;    use Floating_Mixed_Subdivisions_io;
+with Integer_Mixed_Subdivisions;
+with Integer_Mixed_Subdivisions_io;     use Integer_Mixed_Subdivisions_io;
 with Floating_Lifting_Functions;
 with Floating_Integer_Convertors;
+with Integer_Lifting_Utilities;         use Integer_Lifting_Utilities;
 with Floating_Lifting_Utilities;        use Floating_Lifting_Utilities;
 with Induced_Permutations;
 with Mixed_Volume_Computation;          use Mixed_Volume_Computation;
@@ -40,7 +44,8 @@ with QuadDobl_LaurSys_Container;
 with Standard_Solutions_Container;
 with DoblDobl_Solutions_Container;
 with QuadDobl_Solutions_Container;
-with Cells_Container;                   use Cells_Container;
+with Cells_Container;
+with Integer_Cells_Container;
 with Assignments_in_Ada_and_C;          use Assignments_in_Ada_and_C;
 
 function use_celcon ( job : integer32;
@@ -48,14 +53,38 @@ function use_celcon ( job : integer32;
                       b : C_intarrs.Pointer;
                       c : C_dblarrs.Pointer ) return integer32 is
 
-  function Select_Point ( L : List; k : natural32 )
+  function Select_Point ( L : Lists_of_Integer_Vectors.List; k : natural32 )
+                        return Standard_Integer_Vectors.Link_to_Vector is
+
+  -- DESCRIPTION :
+  --   Returns the k-th point from the list L.
+
+    use Lists_of_Integer_Vectors;
+
+    res : Standard_Integer_Vectors.Link_to_Vector;
+    tmp : List := L;
+
+  begin
+    for i in 1..(k-1) loop
+      exit when Is_Null(tmp);
+      tmp := Tail_Of(tmp);
+    end loop;
+    if not Is_Null(tmp)
+     then res := Head_Of(tmp);
+    end if;
+    return res;
+  end Select_Point;
+
+  function Select_Point ( L : Lists_of_Floating_Vectors.List; k : natural32 )
                         return Standard_Floating_Vectors.Link_to_Vector is
 
   -- DESCRIPTION :
-  --   Returns the k-th point from the list l.
+  --   Returns the k-th point from the list L.
+
+    use Lists_of_Floating_Vectors;
 
     res : Standard_Floating_Vectors.Link_to_Vector;
-    tmp : List := l;
+    tmp : List := L;
 
   begin
     for i in 1..(k-1) loop
@@ -69,6 +98,9 @@ function use_celcon ( job : integer32;
   end Select_Point;
 
   function Job0 return integer32 is  -- read mcc and initialize container
+
+    use Arrays_of_Floating_Vector_Lists;
+    use Floating_mixed_Subdivisions;
   
     file : file_type;
     n,m : natural32;
@@ -90,6 +122,8 @@ function use_celcon ( job : integer32;
   end Job0;
 
   function Job1 return integer32 is -- write the mcc in the container
+
+    use Floating_mixed_Subdivisions;
 
     mcc : Mixed_Subdivision := Cells_Container.Retrieve;
     mix : Standard_Integer_Vectors.Link_to_Vector;
@@ -123,6 +157,9 @@ function use_celcon ( job : integer32;
 
   function Job5 return integer32 is -- return cardinalities of supports
 
+    use Lists_of_Floating_Vectors;
+    use Arrays_of_Floating_Vector_Lists;
+
     lif : Link_to_Array_of_Lists;
     r : integer32;
 
@@ -146,6 +183,8 @@ function use_celcon ( job : integer32;
   end Job5;
 
   function Job6 return integer32 is -- return point in support
+
+    use Arrays_of_Floating_Vector_Lists;
 
     lif : Link_to_Array_of_Lists;
     v_a : constant C_Integer_Array := C_intarrs.Value(a);
@@ -171,6 +210,8 @@ function use_celcon ( job : integer32;
 
   function Job7 return integer32 is -- return innner normal
 
+    use Floating_mixed_Subdivisions;
+
     v : constant C_Integer_Array := C_intarrs.Value(a);
     k : constant natural32 := natural32(v(v'first));
     mic : Mixed_Cell;
@@ -185,6 +226,9 @@ function use_celcon ( job : integer32;
   end Job7;
 
   function Job8 return integer32 is -- return #elements in cell
+
+    use Lists_of_Floating_Vectors;
+    use Floating_Mixed_Subdivisions;
 
     v : constant C_Integer_Array := C_intarrs.Value(a);
     k : constant natural32 := natural32(v(v'first));
@@ -209,6 +253,8 @@ function use_celcon ( job : integer32;
 
   function Job9 return integer32 is -- return point in a cell
 
+    use Floating_Mixed_Subdivisions;
+
     v_a : constant C_Integer_Array := C_intarrs.Value(a);
     v_b : constant C_Integer_Array
         := C_intarrs.Value(b,Interfaces.C.ptrdiff_t(2));
@@ -232,6 +278,8 @@ function use_celcon ( job : integer32;
   end Job9;
 
   function Job10 return integer32 is -- return mixed volume of a cell
+
+    use Floating_Mixed_Subdivisions;
 
     v : constant C_Integer_Array := C_intarrs.Value(a);
     k : constant natural32 := natural32(v(v'first));
@@ -676,6 +724,9 @@ function use_celcon ( job : integer32;
 
   function Job25 return integer32 is -- permute system in st container 
 
+    use Arrays_of_Floating_Vector_Lists;
+    use Floating_mixed_Subdivisions;
+
     mixsub : constant Mixed_Subdivision := Cells_Container.Retrieve;
     mix : constant Standard_Integer_Vectors.Link_to_Vector
         := Cells_Container.Type_of_Mixture;
@@ -731,6 +782,8 @@ function use_celcon ( job : integer32;
 
   function Job35 return integer32 is -- permute system in dd container 
 
+    use Floating_mixed_Subdivisions;
+
     mixsub : constant Mixed_Subdivision := Cells_Container.Retrieve;
     mix : constant Standard_Integer_Vectors.Link_to_Vector
         := Cells_Container.Type_of_Mixture;
@@ -785,6 +838,8 @@ function use_celcon ( job : integer32;
   end Job35;
 
   function Job45 return integer32 is -- permute system in qd container 
+
+    use Floating_mixed_Subdivisions;
 
     mixsub : constant Mixed_Subdivision := Cells_Container.Retrieve;
     mix : constant Standard_Integer_Vectors.Link_to_Vector
@@ -857,6 +912,349 @@ function use_celcon ( job : integer32;
     Cells_Container.Initialize_Supports(nbr);
     return 0;
   end Job47;
+
+  function Job51 return integer32 is  -- read mcc and initialize container
+
+    use Arrays_of_Integer_Vector_Lists;
+    use Integer_Mixed_Subdivisions;
+  
+    file : file_type;
+    n,m : natural32;
+    r : integer32;
+    mix : Standard_Integer_Vectors.Link_to_Vector;
+    lif : Link_to_Array_of_Lists;
+    sub : Mixed_Subdivision;
+
+  begin
+    new_line;
+    put_line("Reading the file name for a mixed-cell configuration.");
+    Read_Name_and_Open_File(file);
+    get(file,n,m,mix,sub);
+    close(file);
+    r := mix'last;
+    lif := new Array_of_Lists'(Lifted_Supports(r,sub));
+    Integer_Cells_Container.Initialize(mix,lif,sub);
+    return 0;
+  end Job51;
+
+  function Job52 return integer32 is -- write the mcc in the container
+
+    use Integer_Mixed_Subdivisions;
+
+    mcc : Mixed_Subdivision := Integer_Cells_Container.Retrieve;
+    mix : Standard_Integer_Vectors.Link_to_Vector;
+    n,mv : natural32;
+
+  begin
+    if not Is_Null(mcc) then
+      n := Integer_Cells_Container.Dimension;
+      mix := Integer_Cells_Container.Type_of_Mixture;
+      put(standard_output,n-1,mix.all,mcc,mv);
+      put("The mixed volume is "); put(mv,1); put_line(".");
+    end if;
+    return 0;
+  end Job52;
+
+  function Job55 return integer32 is -- return type of mixture
+
+    use Standard_Integer_Vectors;
+    mix : Link_to_Vector;
+    r : integer32;
+
+  begin
+    mix := Integer_Cells_Container.Type_of_Mixture;
+    if mix /= null
+     then r := mix'last; Assign(mix.all,b);
+     else r := 0;
+    end if;
+    Assign(r,a);
+    return 0;
+  end Job55;
+
+  function Job56 return integer32 is -- return cardinalities of supports
+
+    use Lists_of_Integer_Vectors;
+    use Arrays_of_Integer_Vector_Lists;
+
+    lif : Link_to_Array_of_Lists;
+    r : integer32;
+
+  begin
+    lif := Integer_Cells_Container.Lifted_Supports;
+    if lif /= null then
+      r := lif'last;
+      declare
+        nl : Standard_Integer_Vectors.Vector(1..r);
+      begin
+        for i in nl'range loop
+          nl(i) := integer32(Length_Of(lif(i)));
+        end loop;
+        Assign(nl,b);
+      end;
+    else
+      r := 0;
+    end if;
+    Assign(r,a);
+    return 0;
+  end Job56;
+
+  function Job57 return integer32 is -- return point in support
+
+    use Arrays_of_Integer_Vector_Lists;
+
+    lif : Link_to_Array_of_Lists;
+    v_a : constant C_Integer_Array := C_intarrs.Value(a);
+    v_b : constant C_Integer_Array := C_intarrs.Value(b);
+    k_a : constant integer32 := integer32(v_a(v_a'first));
+    k_b : constant natural32 := natural32(v_b(v_b'first));
+    use Standard_Integer_Vectors;
+    lpt : Link_to_Vector;
+
+  begin
+    lif := Integer_Cells_Container.Lifted_Supports;
+   -- put("retrieving point "); put(k_b,1);
+   -- put(" from list "); put(k_a,1); put_line(" ...");
+    if lif /= null then
+      lpt := Select_Point(lif(k_a),k_b);
+      if lpt /= null then
+        declare
+          fltlpt : Standard_Floating_Vectors.Vector(lpt'range);
+        begin
+          for k in lpt'range loop
+            fltlpt(k) := double_float(lpt(k));
+          end loop;
+          Assign(fltlpt,c);     
+        end;
+        return 0;
+      end if;
+    end if;
+    return 1;
+  end Job57;
+
+  function Job58 return integer32 is -- return innner normal
+
+    use Integer_Mixed_Subdivisions;
+
+    v : constant C_Integer_Array := C_intarrs.Value(a);
+    k : constant natural32 := natural32(v(v'first));
+    mic : Mixed_Cell;
+    fail : boolean;
+
+  begin
+    Integer_Cells_Container.Retrieve(k,mic,fail);
+    if fail then
+      return 1;
+    else
+      declare
+        fltnor : Standard_Floating_Vectors.Vector(mic.nor'range);
+      begin
+        for k in mic.nor'range loop
+          fltnor(k) := double_float(mic.nor(k));
+        end loop;
+        Assign(fltnor,c); return 0;
+      end;
+    end if;
+  end Job58;
+
+  function Job59 return integer32 is -- return #elements in cell
+
+    use Lists_of_Integer_Vectors;
+    use Integer_Mixed_Subdivisions;
+
+    v : constant C_Integer_Array := C_intarrs.Value(a);
+    k : constant natural32 := natural32(v(v'first));
+    mic : Mixed_Cell;
+    fail : boolean;
+
+  begin
+    Integer_Cells_Container.Retrieve(k,mic,fail);
+    if not fail then
+      declare
+        nl : Standard_Integer_Vectors.Vector(mic.pts'range);
+      begin
+        for i in mic.pts'range loop
+          nl(i) := integer32(Length_Of(mic.pts(i)));
+        end loop;
+        Assign(nl,b);
+      end;
+      return 0;
+    end if;
+    return 1;
+  end Job59;
+
+  function Job60 return integer32 is -- return point in a cell
+
+    use Integer_Mixed_Subdivisions;
+
+    v_a : constant C_Integer_Array := C_intarrs.Value(a);
+    v_b : constant C_Integer_Array
+        := C_intarrs.Value(b,Interfaces.C.ptrdiff_t(2));
+    k : constant natural32 := natural32(v_a(v_a'first));
+    i : constant integer32 := integer32(v_b(v_b'first));
+    j : constant natural32 := natural32(v_b(v_b'first+1));
+    mic : Mixed_Cell;
+    fail : boolean;
+    use Standard_Integer_Vectors;
+    lpt : Link_to_Vector;
+
+  begin
+    Integer_Cells_Container.Retrieve(k,mic,fail);
+    if not fail then
+      lpt := Select_Point(mic.pts(i),j);
+      if lpt /= null then
+        declare
+          fltlpt : Standard_Floating_Vectors.Vector(lpt'range);
+        begin
+          for k in lpt'range loop
+            fltlpt(k) := double_float(lpt(k));
+          end loop;
+          Assign(fltlpt,c);
+        end;
+        return 0;
+      end if;
+    end if;
+    return 1;
+  end Job60;
+
+  function Job61 return integer32 is -- return mixed volume of a cell
+
+    use Integer_Mixed_Subdivisions;
+
+    v : constant C_Integer_Array := C_intarrs.Value(a);
+    k : constant natural32 := natural32(v(v'first));
+    mix : constant Standard_Integer_Vectors.Link_to_Vector
+        := Integer_Cells_Container.Type_of_Mixture;
+    mic : Mixed_Cell;
+    fail : boolean;
+    mv : natural32;
+    n : constant integer32 := integer32(Cells_Container.Dimension)-1;
+
+    use Standard_Integer_Vectors;
+
+  begin
+    Integer_Cells_Container.Retrieve(k,mic,fail);
+    if fail or mix = null then
+      return 1;
+    else
+      Mixed_Volume(n,mix.all,mic,mv);
+      Assign(integer32(mv),b);
+      return 0;
+    end if;
+  end Job61;
+
+  function Job62 return integer32 is -- sets type of mixture
+
+    v : constant C_Integer_Array := C_intarrs.Value(a);
+    r : constant integer32 := integer32(v(v'first));
+    mix : constant Standard_Integer_Vectors.Link_to_Vector
+        := new Standard_Integer_Vectors.Vector(1..r);
+    r1 : constant Interfaces.C.size_t := Interfaces.C.size_t(r-1);
+    mbv : constant C_Integer_Array(0..r1)
+        := C_Intarrs.Value(b,Interfaces.C.ptrdiff_t(r));
+
+  begin
+    for i in 0..r-1 loop
+      mix(i+1) := integer32(mbv(Interfaces.C.size_t(i)));
+    end loop;
+    Integer_Cells_Container.Initialize(mix);
+    return 0;
+  end Job62;
+
+  function Job63 return integer32 is -- append point to a support
+
+    va : constant C_Integer_Array := C_intarrs.Value(a);
+    vb : constant C_Integer_Array := C_intarrs.Value(b);
+    k : constant natural32 := natural32(va(va'first));
+    n : constant integer32 := integer32(vb(vb'first));
+    x : Standard_Integer_Vectors.Vector(1..n);
+    fltx : Standard_Floating_Vectors.Vector(1..n);
+    fail : boolean;
+
+  begin
+    Assign(natural32(n),c,fltx);
+    for k in 1..n loop
+      x(k) := integer32(fltx(k));
+    end loop;
+    fail := Integer_Cells_Container.Append_to_Support(k,x);
+    if fail
+     then return 1;
+     else return 0;
+    end if;
+  end Job63;
+
+  function Job64 return integer32 is -- append mixed cell to container
+
+    va : constant C_Integer_Array
+       := C_intarrs.Value(a,Interfaces.C.ptrdiff_t(3));
+    r : constant integer32 := integer32(va(va'first));
+    n : constant integer32 := integer32(va(va'first+1));
+    d : constant Interfaces.C.size_t := Interfaces.C.size_t(va(2));
+    vb : constant C_Integer_Array(0..d-1)
+       := C_intarrs.Value(b,Interfaces.C.ptrdiff_t(d));
+    m : constant integer32 := integer32(vb(0));
+    x : Standard_Integer_Vectors.Vector(1..n);
+    fltx : Standard_Floating_Vectors.Vector(1..n);
+    cnt : Standard_Integer_Vectors.Vector(1..r);
+    lab : Standard_Integer_Vectors.Vector(1..m);
+
+  begin
+   -- put("  r = "); put(r,1);
+   -- put("  n = "); put(n,1);
+   -- put("  d = "); put(natural(d),1); new_line;
+    for i in cnt'range loop
+      cnt(i) := integer32(vb(Interfaces.C.size_t(i)));
+    end loop;
+    for i in lab'range loop
+      lab(i) := integer32(vb(Interfaces.C.size_t(i+r)));
+    end loop;
+   -- put("the number of points in each support :"); put(cnt); new_line;
+   -- put("the labels of points in each support :"); put(lab); new_line;
+    Assign(natural32(n),c,fltx);
+    for k in 1..n loop
+      x(k) := integer32(fltx(k));
+    end loop;
+    Integer_Cells_Container.Append_Mixed_Cell(cnt,lab,x);
+    return 0;
+  end Job64;
+
+  function Job66 return integer32 is -- retrieve a mixed cell
+
+    va : constant C_Integer_Array := C_intarrs.Value(a);
+    k : constant natural32 := natural32(va(va'first));
+    cnt,lab : Standard_Integer_Vectors.Link_to_Vector;
+    normal : Standard_Integer_Vectors.Link_to_Vector;
+    fail : boolean;
+
+  begin
+    Integer_Cells_Container.Retrieve_Mixed_Cell(k,fail,cnt,lab,normal);
+    if fail then
+      return 1;
+    else
+      declare
+        sum : constant integer32 := Standard_Integer_Vectors.Sum(cnt.all);
+        cntlab : Standard_Integer_Vectors.Vector(1..cnt'last+lab'last+1);
+        ind : integer32 := 0;
+        fltnor : Standard_Floating_Vectors.Vector(normal'range);
+      begin
+        ind := ind + 1;
+        cntlab(ind) := sum;
+        for i in cnt'range loop
+          ind := ind + 1;
+          cntlab(ind) := cnt(i);
+        end loop;
+        for i in lab'range loop
+          ind := ind + 1;
+          cntlab(ind) := lab(i);
+        end loop;
+        Assign(cntlab,b);
+        for k in normal'range loop
+          fltnor(k) := double_float(normal(k));
+        end loop;
+        Assign(fltnor,c);
+      end;
+      return 0;
+    end if;
+  end Job66;
  
   function Handle_Jobs return integer32 is
   begin
@@ -915,6 +1313,24 @@ function use_celcon ( job : integer32;
       when 48 => return Job48; -- copy start solution to st container
       when 49 => return Job49; -- copy start solution to dd container
       when 50 => return Job50; -- copy start solution to qd container
+      when 51 => return Job51; -- read mcc, initialize integer cells
+      when 52 => return Job52; -- write mcc from the integer cells container
+      when 53 => Assign(integer32(Integer_Cells_Container.Length),a);
+                 return 0;
+      when 54 => Assign(integer32(Integer_Cells_Container.Dimension),a);
+                 return 0;
+      when 55 => return Job55; -- return type of mixture for integer cells
+      when 56 => return Job56; -- return cardinalities of supports
+      when 57 => return Job57; -- return integer point in support 
+      when 58 => return Job58; -- return integer innner normal
+      when 59 => return Job59; -- return #elements in integer cell
+      when 60 => return Job60; -- return point in a integer cell
+      when 61 => return Job61; -- return mixed volume of a integer cell
+      when 62 => return Job62; -- set type of mixture
+      when 63 => return Job63; -- append point to a support
+      when 64 => return Job64; -- append mixed cell to container
+      when 65 => Integer_Cells_Container.Clear; return 0;
+      when 66 => return Job66; -- retrieve a mixed cell
       when others => put_line("invalid operation"); return 1;
     end case;
   exception
