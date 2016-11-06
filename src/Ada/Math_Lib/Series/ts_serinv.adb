ffb278b85c10e55367d97ed61f5eb27cc0c9ddfc
Jan Verschelde <janv@uic.edu>
2016-11-06 16:37:37 -0600

    added rank computation via SVD to the test procedure on the stacked matrix

--- src/Ada/Math_Lib/Series/ts_serinv.adb
+++ src/Ada/Math_Lib/Series/ts_serinv.adb
@@ -2,8 +2,11 @@ with text_io;                           use text_io;
 with Standard_Integer_Numbers;          use Standard_Integer_Numbers;
 with Standard_Integer_Numbers_io;       use Standard_Integer_Numbers_io;
 with Standard_Complex_Numbers;          use Standard_Complex_Numbers;
+with Standard_Complex_Vectors;
+with Standard_Complex_Vectors_io;       use Standard_Complex_Vectors_io;
 with Standard_Complex_Matrices;
 with Standard_Complex_Matrices_io;      use Standard_Complex_Matrices_io;
+with Standard_Complex_Singular_Values;
 with Standard_Dense_Matrix_Series;
 
 procedure ts_serinv is
@@ -76,6 +79,33 @@ procedure ts_serinv is
     return res;
   end Stack;
 
+  function Rank ( A : Standard_Complex_Matrices.Matrix;
+                  verbose : boolean := true ) 
+                return integer32 is
+
+  -- DESCRIPTION :
+  --   Returns the numerical rank of the matrix A,
+  --   based on the computation of a singular value decomposition of A.
+
+    n : constant integer32 := A'last(1);
+    p : constant integer32 := A'last(2);
+    res : integer32 := 0;
+    wrk : Standard_Complex_Matrices.Matrix(1..n,1..p) := A;
+    mm : constant integer32 := Standard_Complex_Singular_Values.Min0(n+1,p);
+    s : Standard_Complex_Vectors.Vector(1..mm);
+    e : Standard_Complex_Vectors.Vector(1..p);
+    u : Standard_Complex_Matrices.Matrix(1..n,1..n);
+    v : Standard_Complex_Matrices.Matrix(1..p,1..p);
+    job : constant integer32 := 11;
+    info : integer32;
+
+  begin
+    Standard_Complex_Singular_Values.SVD(wrk,n,p,s,e,u,v,job,info);
+    put_line("The singular values :"); put_line(s);
+    res := Standard_Complex_Singular_Values.Rank(s);
+    return res;
+  end Rank;
+
   procedure Test ( s : in Standard_Dense_Matrix_Series.Matrix ) is
 
   -- DESCRIPTION :
@@ -86,9 +116,12 @@ procedure ts_serinv is
     cls : constant integer32 := dim*(s.deg+1);
     cff : constant Standard_Complex_Matrices.Matrix(1..rws,1..cls)
         := Stack(s);
+    rnk : integer32;
   
   begin
     put_line("The stacked coefficient matrix : "); put(cff,2);
+    rnk := Rank(cff);
+    put("The rank of the stacked matrix : "); put(rnk,1); new_line;
   end Test;
 
   procedure Main is
