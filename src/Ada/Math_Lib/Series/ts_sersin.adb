c1f99ad6a173c90f92e2be132ad48eea5af7450a
Jan Verschelde <janv@uic.edu>
2016-12-08 10:58:52 -0600

    setup of a test procedure to develop the solving of a Hermite-Laurent system

---
+++ src/Ada/Math_Lib/Series/ts_sersin.adb
@@ -0,0 +1,272 @@
+with text_io;                            use text_io;
+with Communications_with_User;           use Communications_with_User;
+with Standard_Integer_Numbers;           use Standard_Integer_Numbers;
+with Standard_Integer_Numbers_io;        use Standard_Integer_Numbers_io;
+with Standard_Floating_Numbers;          use Standard_Floating_Numbers;
+with Double_Double_Numbers;              use Double_Double_Numbers;
+with Quad_Double_Numbers;                use Quad_Double_Numbers;
+with Standard_Complex_Numbers;
+with DoblDobl_Complex_Numbers;
+with QuadDobl_Complex_Numbers;
+with Standard_Random_Numbers;
+with Standard_Complex_Vectors;
+with Standard_Complex_Vectors_io;        use Standard_Complex_Vectors_io;
+with Standard_Complex_Matrices;
+with DoblDobl_Complex_Vectors;
+with DoblDobl_Complex_Vectors_io;        use DoblDobl_Complex_Vectors_io;
+with DoblDobl_Complex_Matrices;
+with QuadDobl_Complex_Vectors;
+with QuadDobl_Complex_Vectors_io;        use QuadDobl_Complex_Vectors_io;
+with QuadDobl_Complex_Matrices;
+with Standard_Dense_Vector_Series;
+with Standard_Dense_Vector_Series_io;    use Standard_Dense_Vector_Series_io;
+with Standard_Random_Series;
+with DoblDobl_Random_Series;
+with QuadDobl_Random_Series;
+with Standard_Dense_Matrix_Series;
+with Standard_Dense_Matrix_Series_io;    use Standard_Dense_Matrix_Series_io;
+with DoblDobl_Dense_Vector_Series;
+with DoblDobl_Dense_Vector_Series_io;    use DoblDobl_Dense_Vector_Series_io;
+with DoblDobl_Dense_Matrix_Series;
+with DoblDobl_Dense_Matrix_Series_io;    use DoblDobl_Dense_Matrix_Series_io;
+with QuadDobl_Dense_Vector_Series;
+with QuadDobl_Dense_Vector_Series_io;    use QuadDobl_Dense_Vector_Series_io;
+with QuadDobl_Dense_Matrix_Series;
+with QuadDobl_Dense_Matrix_Series_io;    use QuadDobl_Dense_Matrix_Series_io;
+with Random_Matrix_Series;               use Random_Matrix_Series;
+with Standard_Interpolating_Series;
+with DoblDobl_Interpolating_Series;
+with QuadDobl_Interpolating_Series;
+
+procedure ts_sersin is
+
+-- DESCRIPTION :
+--   Development of solving linear systems of series where the leading
+--   coefficient matrices are likely to be singular.
+
+  procedure Write_Integer_Matrix
+              ( A : in Standard_Complex_Matrices.Matrix ) is
+
+  -- DESCRIPTION :
+  --   Writes the integer matrix to screen.
+
+    use Standard_Complex_Numbers;
+
+  begin
+    for i in A'range(1) loop
+      for j in A'range(2) loop
+        put(" "); put(integer32(REAL_PART(A(i,j))),1);       
+      end loop;
+      new_line;
+    end loop;
+  end Write_Integer_Matrix;
+
+  procedure Write_Integer_Matrix
+              ( A : in DoblDobl_Complex_Matrices.Matrix ) is
+
+  -- DESCRIPTION :
+  --   Writes the integer matrix to screen.
+
+    use DoblDobl_Complex_Numbers;
+
+  begin
+    for i in A'range(1) loop
+      for j in A'range(2) loop
+        put(" "); put(integer32(hi_part(REAL_PART(A(i,j)))),1);       
+      end loop;
+      new_line;
+    end loop;
+  end Write_Integer_Matrix;
+
+  procedure Write_Integer_Matrix
+              ( A : in QuadDobl_Complex_Matrices.Matrix ) is
+
+  -- DESCRIPTION :
+  --   Writes the integer matrix to screen.
+
+    use QuadDobl_Complex_Numbers;
+
+  begin
+    for i in A'range(1) loop
+      for j in A'range(2) loop
+        put(" "); put(integer32(hihi_part(REAL_PART(A(i,j)))),1);       
+      end loop;
+      new_line;
+    end loop;
+  end Write_Integer_Matrix;
+
+  procedure Standard_Hermite_Laurent
+              ( mat : in Standard_Dense_Matrix_Series.Matrix;
+                rhs : in Standard_Dense_Vector_Series.Vector ) is
+
+  -- DESCRIPTION :
+  --   Solves the linear system defined by the Hermite-Laurent
+  --   interpolation conditions, in standard double precision.
+
+    use Standard_Interpolating_Series;
+
+    deg : constant integer32 := mat.deg;
+    nbr : constant integer32 := mat.cff(0)'last(1);
+    nbc : constant integer32 := mat.cff(0)'last(2);
+    nrows : constant integer32 := nbr*(2*deg+1);
+    ncols : constant integer32 := nbc*(2*deg+1);
+    A : Standard_Complex_Matrices.Matrix(1..nrows,1..ncols)
+      := Hermite_Laurent_Matrix(mat.cff(0..deg));
+    b : Standard_Complex_Vectors.Vector(1..nrows)
+      := Hermite_Laurent_Vector(rhs.cff(0..deg));
+
+  begin
+    put_line("The Hermite-Laurent matrix :");
+    Write_Integer_Matrix(A);
+    put_line("The Hermite-Laurent right hand side vector :");
+    put_line(b);
+  end Standard_Hermite_Laurent;
+
+  procedure DoblDobl_Hermite_Laurent
+              ( mat : in DoblDobl_Dense_Matrix_Series.Matrix;
+                rhs : in DoblDobl_Dense_Vector_Series.Vector ) is
+
+  -- DESCRIPTION :
+  --   Solves the linear system defined by the Hermite-Laurent
+  --   interpolation conditions, in double double precision.
+
+    use DoblDobl_Interpolating_Series;
+
+    deg : constant integer32 := mat.deg;
+    nbr : constant integer32 := mat.cff(0)'last(1);
+    nbc : constant integer32 := mat.cff(0)'last(2);
+    nrows : constant integer32 := nbr*(2*deg+1);
+    ncols : constant integer32 := nbc*(2*deg+1);
+    A : DoblDobl_Complex_Matrices.Matrix(1..nrows,1..ncols)
+      := Hermite_Laurent_Matrix(mat.cff(0..deg));
+    b : DoblDobl_Complex_Vectors.Vector(1..nrows)
+      := Hermite_Laurent_Vector(rhs.cff(0..deg));
+
+  begin
+    put_line("The Hermite-Laurent matrix :");
+    Write_Integer_Matrix(A);
+    put_line("The Hermite-Laurent right hand side vector :");
+    put_line(b);
+  end DoblDobl_Hermite_Laurent;
+
+  procedure QuadDobl_Hermite_Laurent
+              ( mat : in QuadDobl_Dense_Matrix_Series.Matrix;
+                rhs : in QuadDobl_Dense_Vector_Series.Vector ) is
+
+  -- DESCRIPTION :
+  --   Solves the linear system defined by the Hermite-Laurent
+  --   interpolation conditions, in quad double precision.
+
+    use QuadDobl_Interpolating_Series;
+
+    deg : constant integer32 := mat.deg;
+    nbr : constant integer32 := mat.cff(0)'last(1);
+    nbc : constant integer32 := mat.cff(0)'last(2);
+    nrows : constant integer32 := nbr*(2*deg+1);
+    ncols : constant integer32 := nbc*(2*deg+1);
+    A : QuadDobl_Complex_Matrices.Matrix(1..nrows,1..ncols)
+      := Hermite_Laurent_Matrix(mat.cff(0..deg));
+    b : QuadDobl_Complex_Vectors.Vector(1..nrows)
+      := Hermite_Laurent_Vector(rhs.cff(0..deg));
+
+  begin
+    put_line("The Hermite-Laurent matrix :");
+    Write_Integer_Matrix(A);
+    put_line("The Hermite-Laurent right hand side vector :");
+    put_line(b);
+  end QuadDobl_Hermite_Laurent;
+
+  procedure Standard_Test ( deg,dim : integer32 ) is
+
+  -- DESCRIPTION :
+  --   Generates a linear system of the given degree and dimension.
+  --   Solves the problem in standard double precision.
+
+    mat : constant Standard_Dense_Matrix_Series.Matrix
+        := Standard_Random_Matrix_Series(deg,dim);
+    sol : constant Standard_Dense_Vector_Series.Vector
+        := Standard_Random_Series.Random_Vector_Series(1,dim,deg);
+    rhs : constant Standard_Dense_Vector_Series.Vector
+        := Standard_Dense_Matrix_Series.Multiply(mat,sol);
+
+  begin
+    put_line("The generated matrix series :"); put(mat);
+    put_line("The generated solution :"); put(sol);
+    put_line("The multiplied right hand side vector : "); put(rhs);
+    Standard_Hermite_Laurent(mat,rhs);
+  end Standard_Test;
+
+  procedure DoblDobl_Test ( deg,dim : integer32 ) is
+
+  -- DESCRIPTION :
+  --   Generates a linear system of the given degree and dimension.
+  --   Solves the problem in standard double precision.
+
+    mat : constant DoblDobl_Dense_Matrix_Series.Matrix
+        := DoblDobl_Random_Matrix_Series(deg,dim);
+    sol : constant DoblDobl_Dense_Vector_Series.Vector
+        := DoblDobl_Random_Series.Random_Vector_Series(1,dim,deg);
+    rhs : constant DoblDobl_Dense_Vector_Series.Vector
+        := DoblDobl_Dense_Matrix_Series.Multiply(mat,sol);
+
+  begin
+    put_line("The generated matrix series :"); put(mat);
+    put_line("The generated solution :"); put(sol);
+    put_line("The multiplied right hand side vector : "); put(rhs);
+    DoblDobl_Hermite_Laurent(mat,rhs);
+  end DoblDobl_Test;
+
+  procedure QuadDobl_Test ( deg,dim : integer32 ) is
+
+  -- DESCRIPTION :
+  --   Generates a linear system of the given degree and dimension.
+  --   Solves the problem in standard double precision.
+
+    mat : constant QuadDobl_Dense_Matrix_Series.Matrix
+        := QuadDobl_Random_Matrix_Series(deg,dim);
+    sol : constant QuadDobl_Dense_Vector_Series.Vector
+        := QuadDobl_Random_Series.Random_Vector_Series(1,dim,deg);
+    rhs : constant QuadDobl_Dense_Vector_Series.Vector
+        := QuadDobl_Dense_Matrix_Series.Multiply(mat,sol);
+
+  begin
+    put_line("The generated matrix series :"); put(mat);
+    put_line("The generated solution :"); put(sol);
+    put_line("The multiplied right hand side vector : "); put(rhs);
+    QuadDobl_Hermite_Laurent(mat,rhs);
+  end QuadDobl_Test;
+
+  procedure Main is
+
+  -- DESCRIPTION :
+  --   Prompts the user for the degree and the dimension of the series.
+  --   The degree is the highest exponent in the power series.
+  --   The dimension is the number of variables in the series.
+
+    deg,dim : integer32 := 0;
+    ans : character;
+
+  begin
+    new_line;
+    put_line("Testing solving of singular linear series systems.");
+    put("Give the dimension : "); get(dim);
+    put("Give the degree : "); get(deg);
+    new_line;
+    put_line("MENU to select the working precision :");
+    put_line("  0. standard double precision;");
+    put_line("  1. double double precision;");
+    put_line("  2. quad double precision.");
+    put("Type 0, 1, or 2 to select the precision : ");
+    Ask_Alternative(ans,"012");
+    new_line;
+    case ans is
+      when '0' => Standard_Test(deg,dim);
+      when '1' => DoblDobl_Test(deg,dim);
+      when '2' => QuadDobl_Test(deg,dim);
+      when others => null;
+    end case;
+  end Main;
+
+begin
+  Main;
+end ts_sersin;
