0fca79a18683b132870c925467256a6948541c27
Jan Verschelde <janv@uic.edu>
2016-12-08 18:02:33 -0600

    defined finding of pivot and swapping of rows and columns in solver

--- src/Ada/Math_Lib/Series/ts_sersin.adb
+++ src/Ada/Math_Lib/Series/ts_sersin.adb
@@ -9,6 +9,8 @@ with Standard_Complex_Numbers;
 with DoblDobl_Complex_Numbers;
 with QuadDobl_Complex_Numbers;
 with Standard_Random_Numbers;
+with Standard_Integer_Vectors;
+with Standard_Integer_Vectors_io;        use Standard_Integer_Vectors_io;
 with Standard_Complex_Vectors;
 with Standard_Complex_Vectors_io;        use Standard_Complex_Vectors_io;
 with Standard_Complex_Matrices;
@@ -95,6 +97,137 @@ procedure ts_sersin is
     end loop;
   end Write_Integer_Matrix;
 
+  function Is_Zero_Row 
+              ( A : Standard_Complex_Matrices.Matrix;
+                i : integer32; tol : double_float ) return boolean is
+
+  -- DESCRIPTION :
+  --   Returns true if all elements on the i-th row of A
+  --   are less than tol in magnitude.  Returns false otherwise.
+
+  begin
+    for j in A'range(2) loop
+      if Standard_Complex_Numbers.AbsVal(A(i,j)) > tol
+       then return false;
+      end if;
+    end loop;
+    return true;
+  end Is_Zero_Row;
+
+  procedure Swap_Rows
+              ( A : in out Standard_Complex_Matrices.Matrix;
+                i,j : in integer32 ) is
+
+  -- DESCRIPTION :
+  --   Swaps row i with row j in A.
+
+    tmp : Standard_Complex_Numbers.Complex_Number;   
+
+  begin
+    for k in A'range(2) loop
+      tmp := A(i,k);
+      A(i,k) := A(j,k);
+      A(j,k) := tmp;
+    end loop;
+  end Swap_Rows;
+
+  procedure Swap_Elements
+              ( v : in out Standard_Complex_Vectors.Vector;
+                i,j : in integer32 ) is
+
+  -- DESCRIPTION :
+  --   Swaps element i with j in v.
+
+    tmp : Standard_Complex_Numbers.Complex_Number;   
+
+  begin
+    tmp := v(i);
+    v(i) := v(j);
+    v(j) := tmp;
+  end Swap_Elements;
+
+  procedure Swap_Zero_Rows
+              ( A : in out Standard_Complex_Matrices.Matrix;
+                b : in out Standard_Complex_Vectors.Vector;
+                tol : in double_float; pivrow : out integer32 ) is
+
+  -- DESCRIPTION :
+  --   Moves zero rows in A to the top of the matrix,
+  --   swapping also the corresponding entries in the right hand side b.
+
+  -- REQUIRED : A'range(1) = b'range.
+
+  -- ON ENTRY :
+  --   A        block upper triangular coefficient matrix;
+  --   b        corresponding right hand side of a linear system;
+  --   tol      tolerance to decide whether a number is zero or not.
+
+  -- ON RETURN :
+  --   A        matrix with all zero rows on top;
+  --   b        correspondingly swapped right hand side;
+  --   pivrow   index of the first nonzero row in A.
+
+    idx : integer32 := A'first(1); -- first nonzero row
+
+  begin
+    for i in A'range(1) loop
+      if Is_Zero_Row(A,i,tol) then
+        if i /= idx then
+          Swap_Rows(A,i,idx);
+          Swap_Elements(b,i,idx);
+        end if;
+        idx := idx + 1;
+      end if;
+    end loop;
+    pivrow := idx;
+  end Swap_Zero_Rows;
+
+  function Max_on_Row
+             ( A : Standard_Complex_Matrices.Matrix;
+               i,j : integer32 ) return integer32 is
+
+  -- DESCRIPTION :
+  --   Returns the index k >= j on row i for which A(i,k) is largest.
+
+    use Standard_Complex_Numbers;
+
+    res : integer32 := j;
+    maxval : double_float := AbsVal(A(i,j));
+    val : double_float;
+
+  begin
+    for k in j+1..A'last(2) loop
+      val := AbsVal(A(i,k));
+      if val > maxval
+       then maxval := val; res := k;
+      end if;
+    end loop;
+    return res;
+  end Max_on_Row;
+
+  procedure Swap_Columns
+              ( A : in out Standard_Complex_Matrices.Matrix;
+                ipvt : in out Standard_Integer_Vectors.Vector;
+                j,k : in integer32 ) is
+
+  -- DESCRIPTION :
+  --   Swaps the columns j and k in A, and the corresponding pivoting
+  --   information in ipvt as well.
+
+    Atmp : Standard_Complex_Numbers.Complex_Number;
+    itmp : integer32;
+
+  begin
+    for i in A'range(1) loop
+      Atmp := A(i,j);
+      A(i,j) := A(i,k);
+      A(i,k) := Atmp;
+    end loop;
+    itmp := ipvt(j);
+    ipvt(j) := ipvt(k);
+    ipvt(k) := itmp;
+  end Swap_Columns;
+
   procedure Standard_Hermite_Laurent
               ( mat : in Standard_Dense_Matrix_Series.Matrix;
                 rhs : in Standard_Dense_Vector_Series.Vector ) is
@@ -114,12 +247,31 @@ procedure ts_sersin is
       := Hermite_Laurent_Matrix(mat.cff(0..deg));
     b : Standard_Complex_Vectors.Vector(1..nrows)
       := Hermite_Laurent_Vector(rhs.cff(0..deg));
+    tol : constant double_float := 1.0E-12;
+    pivrow,pivcol,idx : integer32;
+    ipvt : Standard_Integer_Vectors.Vector(A'range(2));
 
   begin
     put_line("The Hermite-Laurent matrix :");
     Write_Integer_Matrix(A);
     put_line("The Hermite-Laurent right hand side vector :");
     put_line(b);
+    for k in ipvt'range loop
+      ipvt(k) := k;
+    end loop;
+    Swap_Zero_Rows(A,b,tol,pivrow);
+    put_line("After swapping zero rows :"); 
+    Write_Integer_Matrix(A);
+    put("The pivot row : "); put(pivrow,1); new_line;
+    idx := A'first(2);
+    pivcol := Max_on_Row(A,pivrow,idx);
+    put("The pivot column : "); put(pivcol,1); new_line; 
+    if pivcol /= idx then
+      Swap_Columns(A,ipvt,idx,pivcol);
+      put_line("After swapping columns : ");
+      Write_Integer_Matrix(A);
+      put("The pivoting information : "); put(ipvt); new_line;
+    end if;
   end Standard_Hermite_Laurent;
 
   procedure DoblDobl_Hermite_Laurent
