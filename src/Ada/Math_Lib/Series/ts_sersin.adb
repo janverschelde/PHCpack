da2d3866558ee3540c6e13d2a30848cd86a97218
Jan Verschelde <janv@uic.edu>
2016-12-09 18:50:10 -0600

    added checks on the lower triangular echelon form

--- src/Ada/Math_Lib/Series/ts_sersin.adb
+++ src/Ada/Math_Lib/Series/ts_sersin.adb
@@ -3,17 +3,26 @@ with Communications_with_User;           use Communications_with_User;
 with Standard_Integer_Numbers;           use Standard_Integer_Numbers;
 with Standard_Integer_Numbers_io;        use Standard_Integer_Numbers_io;
 with Standard_Floating_Numbers;          use Standard_Floating_Numbers;
+with Standard_Floating_Numbers_io;       use Standard_Floating_Numbers_io;
 with Double_Double_Numbers;              use Double_Double_Numbers;
+with Double_Double_Numbers_io;           use Double_Double_Numbers_io;
 with Quad_Double_Numbers;                use Quad_Double_Numbers;
+with Quad_Double_Numbers_io;             use Quad_Double_Numbers_io;
 with Standard_Complex_Numbers;
+with Standard_Complex_Numbers_io;        use Standard_Complex_Numbers_io;
 with DoblDobl_Complex_Numbers;
+with DoblDobl_Complex_Numbers_io;        use DoblDobl_Complex_Numbers_io;
 with QuadDobl_Complex_Numbers;
+with QuadDobl_Complex_Numbers_io;        use QuadDobl_Complex_Numbers_io;
 with Standard_Random_Numbers;
 with Standard_Integer_Vectors;
 with Standard_Integer_Vectors_io;        use Standard_Integer_Vectors_io;
+with Standard_Integer_Matrices;
+with Standard_Integer_Matrices_io;       use Standard_Integer_Matrices_io;
 with Standard_Complex_Vectors;
 with Standard_Complex_Vectors_io;        use Standard_Complex_Vectors_io;
 with Standard_Complex_Matrices;
+with Standard_Complex_Matrices_io;       use Standard_Complex_Matrices_io;
 with DoblDobl_Complex_Vectors;
 with DoblDobl_Complex_Vectors_io;        use DoblDobl_Complex_Vectors_io;
 with DoblDobl_Complex_Matrices;
@@ -49,6 +58,195 @@ procedure ts_sersin is
 --   Development of solving linear systems of series where the leading
 --   coefficient matrices are likely to be singular.
 
+  function Standard_Integer2Complex
+             ( A : Standard_Integer_Matrices.Matrix )
+             return Standard_Complex_Matrices.Matrix is
+
+  -- DESCRIPTION :
+  --   Converts the integer matrix to a complex matrix,
+  --   in standard double precision.
+
+    res : Standard_Complex_Matrices.Matrix(A'range(1),A'range(2));
+
+  begin
+    for i in A'range(1) loop
+      for j in A'range(2) loop
+        res(i,j) := Standard_Complex_Numbers.Create(A(i,j));
+      end loop;
+    end loop;
+    return res;
+  end Standard_Integer2Complex;
+
+  function DoblDobl_Integer2Complex
+             ( A : Standard_Integer_Matrices.Matrix )
+             return DoblDobl_Complex_Matrices.Matrix is
+
+  -- DESCRIPTION :
+  --   Converts the integer matrix to a complex matrix,
+  --   in double double precision.
+
+    res : DoblDobl_Complex_Matrices.Matrix(A'range(1),A'range(2));
+
+  begin
+    for i in A'range(1) loop
+      for j in A'range(2) loop
+        res(i,j) := DoblDobl_Complex_Numbers.Create(A(i,j));
+      end loop;
+    end loop;
+    return res;
+  end DoblDobl_Integer2Complex;
+
+  function QuadDobl_Integer2Complex
+             ( A : Standard_Integer_Matrices.Matrix )
+             return QuadDobl_Complex_Matrices.Matrix is
+
+  -- DESCRIPTION :
+  --   Converts the integer matrix to a complex matrix,
+  --   in quad double precision.
+
+    res : QuadDobl_Complex_Matrices.Matrix(A'range(1),A'range(2));
+
+  begin
+    for i in A'range(1) loop
+      for j in A'range(2) loop
+        res(i,j) := QuadDobl_Complex_Numbers.Create(A(i,j));
+      end loop;
+    end loop;
+    return res;
+  end QuadDobl_Integer2Complex;
+
+  procedure Standard_Check
+              ( A,L,U : in Standard_Complex_Matrices.Matrix;
+                rowp,colp : in Standard_Integer_Vectors.Vector ) is
+
+  -- DESCRIPTION :
+  --   Given in L the lower triangular echelon form of A,
+  --   with multipliers in U, row and column pivots in rowp, colp,
+  --   the L is reconstructed from the data in A, U, rowp and colp.
+
+    P : constant Standard_Integer_Matrices.Matrix(rowp'range,rowp'range)
+      := Row_Permutation_Matrix(rowp);
+    Q : constant Standard_Integer_Matrices.Matrix(colp'range,colp'range)
+      := Column_Permutation_Matrix(colp);
+    cP : constant Standard_Complex_Matrices.Matrix(P'range(1),P'range(2))
+       := Standard_Integer2Complex(P);
+    cQ : constant Standard_Complex_Matrices.Matrix(Q'range(1),Q'range(2))
+       := Standard_Integer2Complex(Q);
+    W : Standard_Complex_Matrices.Matrix(A'range(1),A'range(2));
+    checksum : double_float := 0.0;
+   
+    use Standard_Complex_Numbers;
+    use Standard_Complex_Matrices;
+
+  begin
+    put_line("Checking the echelon form ...");
+    put_line("The row permutation matrix : "); put(P);
+    put_line("The column permutation matrix : "); put(Q);
+    put_line("The structure of the multiplier matrix : ");
+    Write_Integer_Matrix(U);
+    W := cP*A;
+    W := W*cQ;
+    W := W*U;
+    for i in L'range(1) loop
+      for j in L'range(2) loop
+        put("L("); put(i,1); put(","); put(j,1); put(") :");
+        put(L(i,j)); new_line;
+        put("W("); put(i,1); put(","); put(j,1); put(") :");
+        put(W(i,j)); new_line;
+        checksum := checksum + AbsVal(L(i,j) - W(i,j));
+      end loop;
+    end loop;
+    put("Check sum of differences : "); put(checksum); new_line;
+  end Standard_Check;
+
+  procedure DoblDobl_Check
+              ( A,L,U : in DoblDobl_Complex_Matrices.Matrix;
+                rowp,colp : in Standard_Integer_Vectors.Vector ) is
+
+  -- DESCRIPTION :
+  --   Given in L the lower triangular echelon form of A,
+  --   with multipliers in U, row and column pivots in rowp, colp,
+  --   the L is reconstructed from the data in A, U, rowp and colp.
+
+    P : constant Standard_Integer_Matrices.Matrix(rowp'range,rowp'range)
+      := Row_Permutation_Matrix(rowp);
+    Q : constant Standard_Integer_Matrices.Matrix(colp'range,colp'range)
+      := Column_Permutation_Matrix(colp);
+    cP : constant DoblDobl_Complex_Matrices.Matrix(P'range(1),P'range(2))
+       := DoblDobl_Integer2Complex(P);
+    cQ : constant DoblDobl_Complex_Matrices.Matrix(Q'range(1),Q'range(2))
+       := DoblDobl_Integer2Complex(Q);
+    W : DoblDobl_Complex_Matrices.Matrix(A'range(1),A'range(2));
+    checksum : double_double := create(0.0);
+
+    use DoblDobl_Complex_Numbers;
+    use DoblDobl_Complex_Matrices;
+
+  begin
+    put_line("Checking the echelon form ...");
+    put_line("The row permutation matrix : "); put(P);
+    put_line("The column permutation matrix : "); put(Q);
+    put_line("The structure of the multiplier matrix : ");
+    Write_Integer_Matrix(U);
+    W := cP*A;
+    W := W*cQ;
+    W := W*U;
+    for i in L'range(1) loop
+      for j in L'range(2) loop
+        put("L("); put(i,1); put(","); put(j,1); put(") : ");
+        put(L(i,j)); new_line;
+        put("W("); put(i,1); put(","); put(j,1); put(") : ");
+        put(W(i,j)); new_line;
+        checksum := checksum + AbsVal(L(i,j) - W(i,j));
+      end loop;
+    end loop;
+    put("Check sum of differences : "); put(checksum); new_line;
+  end DoblDobl_Check;
+
+  procedure QuadDobl_Check
+              ( A,L,U : in QuadDobl_Complex_Matrices.Matrix;
+                rowp,colp : in Standard_Integer_Vectors.Vector ) is
+
+  -- DESCRIPTION :
+  --   Given in L the lower triangular echelon form of A,
+  --   with multipliers in U, row and column pivots in rowp, colp,
+  --   the L is reconstructed from the data in A, U, rowp and colp.
+
+    P : constant Standard_Integer_Matrices.Matrix(rowp'range,rowp'range)
+      := Row_Permutation_Matrix(rowp);
+    Q : constant Standard_Integer_Matrices.Matrix(colp'range,colp'range)
+      := Column_Permutation_Matrix(colp);
+    cP : constant QuadDobl_Complex_Matrices.Matrix(P'range(1),P'range(2))
+       := QuadDobl_Integer2Complex(P);
+    cQ : constant QuadDobl_Complex_Matrices.Matrix(Q'range(1),Q'range(2))
+       := QuadDobl_Integer2Complex(Q);
+    W : QuadDobl_Complex_Matrices.Matrix(A'range(1),A'range(2));
+    checksum : quad_double := create(0.0);
+
+    use QuadDobl_Complex_Numbers;
+    use QuadDobl_Complex_Matrices;
+
+  begin
+    put_line("Checking the echelon form ...");
+    put_line("The row permutation matrix : "); put(P);
+    put_line("The column permutation matrix : "); put(Q);
+    put_line("The structure of the multiplier matrix : ");
+    Write_Integer_Matrix(U);
+    W := cP*A;
+    W := W*cQ;
+    W := W*U;
+    for i in L'range(1) loop
+      for j in L'range(2) loop
+        put("L("); put(i,1); put(","); put(j,1); put(") : ");
+        put(L(i,j)); new_line;
+        put("W("); put(i,1); put(","); put(j,1); put(") : ");
+        put(W(i,j)); new_line;
+        checksum := checksum + AbsVal(L(i,j) - W(i,j));
+      end loop;
+    end loop;
+    put("Check sum of differences : "); put(checksum); new_line;
+  end QuadDobl_Check;
+
   procedure Standard_Hermite_Laurent
               ( mat : in Standard_Dense_Matrix_Series.Matrix;
                 rhs : in Standard_Dense_Vector_Series.Vector ) is
@@ -68,7 +266,7 @@ procedure ts_sersin is
       := Hermite_Laurent_Matrix(mat.cff(0..deg));
     b : Standard_Complex_Vectors.Vector(1..nrows)
       := Hermite_Laurent_Vector(rhs.cff(0..deg));
-    U : Standard_Complex_Matrices.Matrix(1..nrows,1..ncols);
+    L,U : Standard_Complex_Matrices.Matrix(1..nrows,1..ncols);
     row_ipvt : Standard_Integer_Vectors.Vector(1..nrows);
     col_ipvt : Standard_Integer_Vectors.Vector(1..ncols);
 
@@ -76,8 +274,10 @@ procedure ts_sersin is
     put_line("The Hermite-Laurent matrix :"); Write_Integer_Matrix(A);
     put_line("The Hermite-Laurent right hand side vector :");
     put_line(b);
-    Lower_Triangular_Echelon_Form(A,U,row_ipvt,col_ipvt);
-    put_line("The matrix in echelon form :"); Write_Integer_Matrix(A);
+    L := A;
+    Lower_Triangular_Echelon_Form(L,U,row_ipvt,col_ipvt);
+    put_line("The matrix in echelon form :"); Write_Integer_Matrix(L);
+    Standard_Check(A,L,U,row_ipvt,col_ipvt);
   end Standard_Hermite_Laurent;
 
   procedure DoblDobl_Hermite_Laurent
@@ -99,7 +299,7 @@ procedure ts_sersin is
       := Hermite_Laurent_Matrix(mat.cff(0..deg));
     b : DoblDobl_Complex_Vectors.Vector(1..nrows)
       := Hermite_Laurent_Vector(rhs.cff(0..deg));
-    U : DoblDobl_Complex_Matrices.Matrix(1..nrows,1..ncols);
+    L,U : DoblDobl_Complex_Matrices.Matrix(1..nrows,1..ncols);
     row_ipvt : Standard_Integer_Vectors.Vector(1..nrows);
     col_ipvt : Standard_Integer_Vectors.Vector(1..ncols);
 
@@ -108,8 +308,10 @@ procedure ts_sersin is
     Write_Integer_Matrix(A);
     put_line("The Hermite-Laurent right hand side vector :");
     put_line(b);
-    Lower_Triangular_Echelon_Form(A,U,row_ipvt,col_ipvt);
-    put_line("The matrix in echelon form :"); Write_Integer_Matrix(A);
+    L := A;
+    Lower_Triangular_Echelon_Form(L,U,row_ipvt,col_ipvt);
+    put_line("The matrix in echelon form :"); Write_Integer_Matrix(L);
+    DoblDobl_Check(A,L,U,row_ipvt,col_ipvt);
   end DoblDobl_Hermite_Laurent;
 
   procedure QuadDobl_Hermite_Laurent
@@ -131,7 +333,7 @@ procedure ts_sersin is
       := Hermite_Laurent_Matrix(mat.cff(0..deg));
     b : QuadDobl_Complex_Vectors.Vector(1..nrows)
       := Hermite_Laurent_Vector(rhs.cff(0..deg));
-    U : QuadDobl_Complex_Matrices.Matrix(1..nrows,1..ncols);
+    L,U : QuadDobl_Complex_Matrices.Matrix(1..nrows,1..ncols);
     row_ipvt : Standard_Integer_Vectors.Vector(1..nrows);
     col_ipvt : Standard_Integer_Vectors.Vector(1..ncols);
 
@@ -140,8 +342,10 @@ procedure ts_sersin is
     Write_Integer_Matrix(A);
     put_line("The Hermite-Laurent right hand side vector :");
     put_line(b);
-    Lower_Triangular_Echelon_Form(A,U,row_ipvt,col_ipvt);
-    put_line("The matrix in echelon form :"); Write_Integer_Matrix(A);
+    L := A;
+    Lower_Triangular_Echelon_Form(L,U,row_ipvt,col_ipvt);
+    put_line("The matrix in echelon form :"); Write_Integer_Matrix(L);
+    QuadDobl_Check(A,L,U,row_ipvt,col_ipvt);
   end QuadDobl_Hermite_Laurent;
 
   procedure Standard_Integer_Test ( deg,dim : integer32 ) is
