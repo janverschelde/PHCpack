7442c22d8266cac4742e8a9df1507f16b0098487
Jan Verschelde <janv@uic.edu>
2017-01-03 14:19:29 -0600

    fixed blackbox root refiner (phc -b -v or phc -v -b) so it works also for overdetermined polynomial systems

--- src/Ada/Main/bablvali.adb
+++ src/Ada/Main/bablvali.adb
@@ -8,6 +8,7 @@ with Communications_with_User;           use Communications_with_User;
 with File_Scanning;                      use File_Scanning;
 with Standard_Floating_Numbers;          use Standard_Floating_Numbers;
 with Standard_Floating_Numbers_io;       use Standard_Floating_Numbers_io;
+with Standard_Complex_Polynomials;
 with Standard_Complex_Poly_Systems;      use Standard_Complex_Poly_Systems;
 with Standard_Complex_Poly_Systems_io;   use Standard_Complex_Poly_Systems_io;
 with Standard_Complex_Solutions;         use Standard_Complex_Solutions;
@@ -44,6 +45,7 @@ procedure bablvali ( infilename,outfilename : in string ) is
     deflate : boolean := false;
     refsols : Solution_List;
     timer : Timing_Widget;
+    dim : constant integer32 := Head_Of(sols).n;
 
   begin
     new_line(file);
@@ -56,10 +58,17 @@ procedure bablvali ( infilename,outfilename : in string ) is
     put(file,tolsing,2,3,3); new_line(file);
     put(file,"  maximum number of iterations    : ");
     put(file,maxit,2); new_line(file);
-    tstart(timer);
-    Reporting_Root_Refiner
-      (file,lp.all,sols,refsols,epsxa,epsfa,tolsing,numb,maxit,deflate,false);
-    tstop(timer);
+    if lp'last = dim then
+      tstart(timer);
+      Reporting_Root_Refiner
+        (file,lp.all,sols,refsols,epsxa,epsfa,tolsing,numb,maxit,deflate,false);
+      tstop(timer);
+    else
+      tstart(timer);
+      Reporting_Root_Sharpener
+        (file,lp.all,sols,refsols,epsxa,epsfa,tolsing,numb,maxit,deflate,false);
+      tstop(timer);
+    end if;
     new_line(file);
     put_line(file,"THE SOLUTIONS :");
     put(file,Length_Of(refsols),natural32(Head_Of(refsols).n),refsols);
@@ -70,6 +79,7 @@ procedure bablvali ( infilename,outfilename : in string ) is
   procedure Main is
 
     infile,outfile : file_type;
+    nbvar : natural32;
     ans : character;
     sols : Solution_List;
     n : integer32 := 0;
@@ -96,8 +106,12 @@ procedure bablvali ( infilename,outfilename : in string ) is
         sysonfile := false;
       end if;
     end if;
+    nbvar := Standard_Complex_Polynomials.Number_of_Unknowns(lp(lp'first));
     Create_Output_File(outfile,outfilename);
-    put(outfile,natural32(lp'last),lp.all);
+    if lp'last = integer32(nbvar)
+     then put(outfile,natural32(lp'last),lp.all);
+     else put(outfile,natural32(lp'last),nbvar,lp.all);
+    end if;
     if sysonfile then
       Scan_and_Skip(infile,"THE SOLUTIONS",found);
       if found
