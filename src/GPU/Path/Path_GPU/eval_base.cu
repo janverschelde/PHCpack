7dc8b108436a437e74205a1167711ebe15603a62
Jan Verschelde <janv@uic.edu>
2017-01-02 21:38:27 -0600

    reformatted code, replacing the tab positions with three spaces each

--- src/GPU/Path/Path_GPU/eval_base.cu
+++ src/GPU/Path/Path_GPU/eval_base.cu
@@ -1,36 +1,44 @@
-__global__ void eval_base_table_kernel(int dim, int* max_deg_base,\
-		int* base_table_start, GT* x, GT* base_table) {
-    int bidx = (gridDim.x*blockIdx.y+blockIdx.x)*blockDim.x;
-	int tidx = threadIdx.x;
-	int idx = bidx + tidx;
+__global__ void eval_base_table_kernel
+ ( int dim, int* max_deg_base, int* base_table_start, GT* x, GT* base_table )
+{
+   int bidx = (gridDim.x*blockIdx.y+blockIdx.x)*blockDim.x;
+   int tidx = threadIdx.x;
+   int idx = bidx + tidx;
 
-	if(idx < dim && max_deg_base[idx]>0) {
-		GT* var_start = base_table + base_table_start[idx];
-		GT var = x[idx];
-		GT var_term = var;
-		var_start[0] = var_term;
-		for(int deg_idx=1; deg_idx<max_deg_base[idx]; deg_idx++){
-			var_term *= var;
-			var_start[deg_idx] = var_term;
-		}
-	}
+   if(idx < dim && max_deg_base[idx]>0)
+   {
+      GT* var_start = base_table + base_table_start[idx];
+      GT var = x[idx];
+      GT var_term = var;
+      var_start[0] = var_term;
+      for(int deg_idx=1; deg_idx<max_deg_base[idx]; deg_idx++)
+      {
+         var_term *= var;
+         var_start[deg_idx] = var_term;
+      }
+   }
 }
 
-__global__ void eval_base_kernel(int n_mon_base, unsigned short* pos, unsigned short* exp,\
-		int* mon_pos_start, int* base_table_start, GT* base_table, GT* coef) {
-    int bidx = (gridDim.x*blockIdx.y+blockIdx.x)*blockDim.x;
-	int tidx = threadIdx.x;
-	int idx = bidx + tidx;
+__global__ void eval_base_kernel
+ ( int n_mon_base, unsigned short* pos, unsigned short* exp,
+   int* mon_pos_start, int* base_table_start, GT* base_table, GT* coef )
+{
+   int bidx = (gridDim.x*blockIdx.y+blockIdx.x)*blockDim.x;
+   int tidx = threadIdx.x;
+   int idx = bidx + tidx;
 
-	if(idx < n_mon_base) {
-		unsigned short* mon_pos = pos+mon_pos_start[idx];
-		unsigned short* mon_exp = exp+mon_pos_start[idx];
-		int base_start = *mon_exp++;
-		int n_var = *mon_pos++;
-		GT tmp_coef = coef[idx];
-		for(int var_idx=base_start; var_idx<n_var; var_idx++){
-			tmp_coef *= base_table[base_table_start[mon_pos[var_idx]]+mon_exp[var_idx]-2];
-		}
-		coef[idx] = tmp_coef;
-	}
+   if(idx < n_mon_base) 
+   {
+      unsigned short* mon_pos = pos+mon_pos_start[idx];
+      unsigned short* mon_exp = exp+mon_pos_start[idx];
+      int base_start = *mon_exp++;
+      int n_var = *mon_pos++;
+      GT tmp_coef = coef[idx];
+      for(int var_idx=base_start; var_idx<n_var; var_idx++)
+      {
+         tmp_coef *= base_table[base_table_start[mon_pos[var_idx]]
+                   + mon_exp[var_idx]-2];
+      }
+      coef[idx] = tmp_coef;
+   }
 }
--- src/GPU/Path/Path_GPU/eval_coef.cu
+++ src/GPU/Path/Path_GPU/eval_coef.cu
@@ -1,92 +1,114 @@
 // Mon block evalutation and differentiation on GPU
-//template <unsigned int n_th>
-__global__ void eval_coef_kernel(GT* workspace_coef, const GT* coef_orig, int n_coef, \
-		GT* t, GT* one_minor_t, int workspace_size) {
-	//__shared__ GT div_diff_sh[shmemsize];
-    int bidx = (gridDim.x*blockIdx.y+blockIdx.x)*blockDim.x;
-	int tidx = threadIdx.x;
-	int idx = bidx + tidx;
+// template <unsigned int n_th>
 
-	int path_idx = blockIdx.z;
-	//workspace_coef += workspace_size*path_idx;
+__global__ void eval_coef_kernel
+ ( GT* workspace_coef, const GT* coef_orig, int n_coef, 
+   GT* t, GT* one_minor_t, int workspace_size )
+{
+   // __shared__ GT div_diff_sh[shmemsize];
 
-	/*int path_idx = blockIdx.z;
-	 x_predictor += path_idx*np_predictor*dim;
-	 t_predictor += path_idx*np_predictor;
-	 x_new += path_idx*dim;*/
+   int bidx = (gridDim.x*blockIdx.y+blockIdx.x)*blockDim.x;
+   int tidx = threadIdx.x;
+   int idx = bidx + tidx;
 
-	if(idx < n_coef) {
-		//workspace_coef[idx] = coef_orig[idx];
-//		workspace_coef[idx] = (*t)*coef_orig[2*idx] + (*one_minor_t)*coef_orig[2*idx+1];
-		workspace_coef[idx] = (*t)*coef_orig[idx] + (*one_minor_t)*coef_orig[idx+n_coef];
-	}
+   int path_idx = blockIdx.z;
+   // workspace_coef += workspace_size*path_idx;
+
+   /*int path_idx = blockIdx.z;
+     x_predictor += path_idx*np_predictor*dim;
+     t_predictor += path_idx*np_predictor;
+     x_new += path_idx*dim;*/
+
+   if(idx < n_coef) 
+   {
+      // workspace_coef[idx] = coef_orig[idx];
+      // workspace_coef[idx] = (*t)*coef_orig[2*idx]
+      //                     + (*one_minor_t)*coef_orig[2*idx+1];
+      workspace_coef[idx] = (*t)*coef_orig[idx]
+                          + (*one_minor_t)*coef_orig[idx+n_coef];
+   }
 }
 
-__global__ void eval_coef_kernel(GT* workspace_coef, const GT* coef_orig, int n_coef, \
-		GT* t, GT* one_minor_t, int workspace_size, int* x_t_idx){
-	//__shared__ GT div_diff_sh[shmemsize];
-    int bidx = (gridDim.x*blockIdx.y+blockIdx.x)*blockDim.x;
-	int tidx = threadIdx.x;
-	int idx = bidx + tidx;
+__global__ void eval_coef_kernel
+ ( GT* workspace_coef, const GT* coef_orig, int n_coef,
+   GT* t, GT* one_minor_t, int workspace_size, int* x_t_idx )
+{
+   //__shared__ GT div_diff_sh[shmemsize];
+   int bidx = (gridDim.x*blockIdx.y+blockIdx.x)*blockDim.x;
+   int tidx = threadIdx.x;
+   int idx = bidx + tidx;
 
-	int path_idx = blockIdx.z;
-	workspace_coef += workspace_size*path_idx;
-	t += workspace_size*path_idx + x_t_idx[path_idx];
-	one_minor_t += workspace_size*path_idx;
+   int path_idx = blockIdx.z;
+   workspace_coef += workspace_size*path_idx;
+   t += workspace_size*path_idx + x_t_idx[path_idx];
+   one_minor_t += workspace_size*path_idx;
 
-	/*int path_idx = blockIdx.z;
-	 x_predictor += path_idx*np_predictor*dim;
-	 t_predictor += path_idx*np_predictor;
-	 x_new += path_idx*dim;*/
+   /*int path_idx = blockIdx.z;
+     x_predictor += path_idx*np_predictor*dim;
+     t_predictor += path_idx*np_predictor;
+     x_new += path_idx*dim;*/
 
-	if(idx < n_coef) {
-		//workspace_coef[idx] = coef_orig[idx];
-		//workspace_coef[idx] = (*t)*coef_orig[2*idx] + (*one_minor_t)*coef_orig[2*idx+1];
-		workspace_coef[idx] = (*t)*coef_orig[idx] + (*one_minor_t)*coef_orig[idx+n_coef];
-	}
+   if(idx < n_coef)
+   {
+      // workspace_coef[idx] = coef_orig[idx];
+      // workspace_coef[idx] = (*t)*coef_orig[2*idx]
+      //                     + (*one_minor_t)*coef_orig[2*idx+1];
+      workspace_coef[idx] = (*t)*coef_orig[idx]
+                          + (*one_minor_t)*coef_orig[idx+n_coef];
+   }
 }
 
-__global__ void eval_coef_kernel(GT* workspace_coef, const GT* coef_orig, int n_coef, \
-		GT* t, GT* one_minor_t, int workspace_size, int* x_t_idx, int* path_idx_mult) {
-	//__shared__ GT div_diff_sh[shmemsize];
-    int bidx = (gridDim.x*blockIdx.y+blockIdx.x)*blockDim.x;
-	int tidx = threadIdx.x;
-	int idx = bidx + tidx;
+__global__ void eval_coef_kernel
+ ( GT* workspace_coef, const GT* coef_orig, int n_coef, GT* t,
+   GT* one_minor_t, int workspace_size, int* x_t_idx, int* path_idx_mult)
+{
+   //__shared__ GT div_diff_sh[shmemsize];
+   int bidx = (gridDim.x*blockIdx.y+blockIdx.x)*blockDim.x;
+   int tidx = threadIdx.x;
+   int idx = bidx + tidx;
 
-	int path_idx = path_idx_mult[blockIdx.z];
-	workspace_coef += workspace_size*path_idx;
-	t += workspace_size*path_idx + x_t_idx[path_idx];
-	one_minor_t += workspace_size*path_idx;
+   int path_idx = path_idx_mult[blockIdx.z];
+   workspace_coef += workspace_size*path_idx;
+   t += workspace_size*path_idx + x_t_idx[path_idx];
+   one_minor_t += workspace_size*path_idx;
 
-	/*int path_idx = blockIdx.z;
-	 x_predictor += path_idx*np_predictor*dim;
-	 t_predictor += path_idx*np_predictor;
-	 x_new += path_idx*dim;*/
+   /*int path_idx = blockIdx.z;
+     x_predictor += path_idx*np_predictor*dim;
+     t_predictor += path_idx*np_predictor;
+     x_new += path_idx*dim;*/
 
-	if(idx < n_coef) {
-		//workspace_coef[idx] = coef_orig[idx];
-		//workspace_coef[idx] = (*t)*coef_orig[2*idx] + (*one_minor_t)*coef_orig[2*idx+1];
-		workspace_coef[idx] = (*t)*coef_orig[idx] + (*one_minor_t)*coef_orig[idx+n_coef];
-	}
+   if(idx < n_coef)
+   {
+      // workspace_coef[idx] = coef_orig[idx];
+      // workspace_coef[idx] = (*t)*coef_orig[2*idx]
+      //                     + (*one_minor_t)*coef_orig[2*idx+1];
+      workspace_coef[idx] = (*t)*coef_orig[idx]
+                          + (*one_minor_t)*coef_orig[idx+n_coef];
+   }
 }
-//template <unsigned int n_th>
-__global__ void eval_coef_copy_kernel(GT* workspace_coef, const GT* coef_orig, int n_coef) {
-	//__shared__ GT div_diff_sh[shmemsize];
-    int bidx = (gridDim.x*blockIdx.y+blockIdx.x)*blockDim.x;
-	int tidx = threadIdx.x;
-	int idx = bidx + tidx;
 
-	//int path_idx = blockIdx.z;
-	//workspace_coef += workspace_size*path_idx;
+// template <unsigned int n_th>
+__global__ void eval_coef_copy_kernel
+ ( GT* workspace_coef, const GT* coef_orig, int n_coef )
+{
+   //__shared__ GT div_diff_sh[shmemsize];
+   int bidx = (gridDim.x*blockIdx.y+blockIdx.x)*blockDim.x;
+   int tidx = threadIdx.x;
+   int idx = bidx + tidx;
 
-	/*int path_idx = blockIdx.z;
-	 x_predictor += path_idx*np_predictor*dim;
-	 t_predictor += path_idx*np_predictor;
-	 x_new += path_idx*dim;*/
+   // int path_idx = blockIdx.z;
+   // workspace_coef += workspace_size*path_idx;
 
-	if(idx < n_coef) {
-		//workspace_coef[idx] = coef_orig[idx];
-//		workspace_coef[idx] = (*t)*coef_orig[2*idx] + (*one_minor_t)*coef_orig[2*idx+1];
-		workspace_coef[idx] = coef_orig[idx];
-	}
+   /*int path_idx = blockIdx.z;
+     x_predictor += path_idx*np_predictor*dim;
+     t_predictor += path_idx*np_predictor;
+     x_new += path_idx*dim;*/
+
+   if(idx < n_coef)
+   {
+      // workspace_coef[idx] = coef_orig[idx];
+      // workspace_coef[idx] = (*t)*coef_orig[2*idx]
+      //                     + (*one_minor_t)*coef_orig[2*idx+1];
+      workspace_coef[idx] = coef_orig[idx];
+   }
 }
