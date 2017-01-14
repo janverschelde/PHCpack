a595810285cc61b0c334e62b3ad6ee6e63556ecb
Jan Verschelde <janv@uic.edu>
2017-01-14 17:34:23 -0600

    reformatting of code with bug fix in generation of random positions

--- src/GPU/PED/gqd_qd_util.cpp
+++ src/GPU/PED/gqd_qd_util.cpp
@@ -1,3 +1,5 @@
+// definitions of utilities with prototypes in gqd_qd_util.h
+
 #include "gqd_type.h"
 #include "vector_types.h"
 #include <qd/qd_real.h>
@@ -34,22 +36,19 @@ void gqd2qd ( gdd_real *a, dd_real *b )
    b->x[1] = a->y;
 }
 
-
 void qd2gqd ( double *a, double *b )
 {
-  *b = *a;
+   *b = *a;
 }
 
 void gqd2qd ( double *a, double *b )
 {
-  *b = *a;
+   *b = *a;
 }
 
 int parse_arguments
  ( int argc, char *argv[],
    int *BS, int *dim, int *NM, int *NV, int *deg, int *r, int *mode )
-/* Parses the arguments on the command line.
-   Returns 0 if okay, otherwise the function returns 1. */
 {
    if(argc < 8)
    {
@@ -61,7 +60,9 @@ int parse_arguments
       cout << "(5) maximal degree of each variable" << endl;
       cout << "(6) number of repeated runs" << endl;
       cout << "(7) execution mode" << endl;
-      cout << "please try again..." << endl; return 1;
+      cout << "please try again..." << endl;
+
+      return 1;
    }
    *BS = atoi(argv[1]);     // block size
    *dim = atoi(argv[2]);    // dimension
@@ -70,36 +71,57 @@ int parse_arguments
    *deg = atoi(argv[5]);    // maximal degree of each variable
    *r = atoi(argv[6]);      // number of repeated runs
    *mode = atoi(argv[7]);   // execution mode
+
+   return 0;
+}
+
+int parse_args
+ ( int argc, char *argv[], int *prc,
+   int *BS, int *dim, int *NM, int *NV, int *deg, int *r, int *mode )
+{
+   if(argc < 9)
+   {
+      cout << argv[0] << " needs eight parameters :" << endl;
+      cout << "(0) working precision, 0, 1, or 2 for d, dd, or qd" << endl;
+      cout << "(1) block size, number of threads in a block" << endl;
+      cout << "(2) dimension of the problem" << endl;
+      cout << "(3) number of monomials" << endl;
+      cout << "(4) number of variables in each monomial" << endl;
+      cout << "(5) maximal degree of each variable" << endl;
+      cout << "(6) number of repeated runs" << endl;
+      cout << "(7) execution mode" << endl;
+      cout << "please try again..." << endl;
+
+      return 1;
+   }
+   *prc = atoi(argv[1]);    // working precision
+   *BS = atoi(argv[2]);     // block size
+   *dim = atoi(argv[3]);    // dimension
+   *NM = atoi(argv[4]);     // number of monomials
+   *NV = atoi(argv[5]);     // number of variables in each monomial
+   *deg = atoi(argv[6]);    // maximal degree of each variable
+   *r = atoi(argv[7]);      // number of repeated runs
+   *mode = atoi(argv[8]);   // execution mode
+
    return 0;
 }
 
 void random_positions ( int dim, int NM, int NV, int *p_int, char *p_char )
-// Generates random positions for the NM monomials in NV variables,
-// for a system of dimension dim, stored twice in the arrays p_int and p_char.
 {
    for(int i=0; i<NM; i++)
-   {
-      int lp = 0;
       for(int j=0; j<NV; j++)
       {
-         int ia = max(rand() % (dim-lp - (NV-j)),1);
-         lp = lp + ia;
          p_int[NV*i+j] = (i+j) % dim;
          p_char[NV*i+j] = (char) p_int[NV*i+j];
       }
-   }
 }
 
 void random_exponents ( int NM, int NV, int d, int *e_int, char *e_char )
-// Generates random exponents for the NM monomials in NV variables,
-// of degree at most d, stored twice in the arrays e_int and e_char.
 {
    for(int i=0; i<NM; i++)
-   {
-      for(int j=0;j<NV;j++)
+      for(int j=0; j<NV; j++)
       {
          e_int[NV*i+j] = rand() % d;
          e_char[NV*i+j] = (char) e_int[NV*i+j];
       }
-   }
 }
--- src/GPU/PED/gqd_qd_util.h
+++ src/GPU/PED/gqd_qd_util.h
@@ -1,27 +1,29 @@
+// utilities for the polynomial evaluation and differentiation
+
 #ifndef UTIL
 #define UTIL
 
-#include<qd/qd_real.h>
-#include"complexD.h"
-#include"complexH.h"
-#include"cstdlib"
+#include <qd/qd_real.h>
+#include "complexD.h"
+#include "complexH.h"
+#include "cstdlib"
 
-void qd2gqd( qd_real *a, gqd_real *b );
-void gqd2qd( gqd_real *a, qd_real *b );
+void qd2gqd ( qd_real *a, gqd_real *b );
+void gqd2qd ( gqd_real *a, qd_real *b );
 void qd2gqd ( dd_real *a, gdd_real *b );
 void gqd2qd ( gdd_real *a, dd_real *b );
 void qd2gqd ( double *a, double *b );
 void gqd2qd ( double *a, double *b );
 
 template<class realD, class realH>
-void comp1_gqd2qd(complexD<realD>* a, complexH<realH>* b)
+void comp1_gqd2qd ( complexD<realD>* a, complexH<realH>* b )
 {
    gqd2qd(&(a->re),&(b->re));
    gqd2qd(&(a->im),&(b->im));
 }
 
 template<class realD, class realH>
-void comp1_gqdArr2qdArr(complexD<realD>* a, complexH<realH>* b, int dim)
+void comp1_gqdArr2qdArr ( complexD<realD>* a, complexH<realH>* b, int dim )
 {
    int i;
 
@@ -31,49 +33,78 @@ void comp1_gqdArr2qdArr(complexD<realD>* a, complexH<realH>* b, int dim)
 int parse_arguments
  ( int argc, char *argv[],
    int *BS, int *dim, int *NM, int *NV, int *deg, int *r, int *mode );
+/*
+ * Parses the command line arguments.
+ * Returns 0 if okay, otherwise the function returns 1.
+ *
+ * ON ENTRY :
+ *   argc     the number of command line arguments;
+ *   argv     the command line arguments.
+ *
+ * ON RETURN :
+ *   BS       number of threads in a block, the block size;
+ *   dim      dimension of the problem;
+ *   NM       number of monomials;
+ *   NV       number of variables;
+ *   deg      highest degree of the variables;
+ *   r        frequency of the runs;
+ *   mode     mode of execution, 0, 1, or 2. */
+
+int parse_args
+ ( int argc, char *argv[], int *prc,
+   int *BS, int *dim, int *NM, int *NV, int *deg, int *r, int *mode );
+/*
+ * Parses the command line arguments.
+ * Returns 0 if okay, otherwise the function returns 1.
+ *
+ * ON ENTRY :
+ *   argc     the number of command line arguments;
+ *   argv     the command line arguments.
+ *
+ * ON RETURN :
+ *   prc      the working precision, 0 for d, 1 for dd, 2 for qd;
+ *   BS       number of threads in a block, the block size;
+ *   dim      dimension of the problem;
+ *   NM       number of monomials;
+ *   NV       number of variables;
+ *   deg      highest degree of the variables;
+ *   r        frequency of the runs;
+ *   mode     mode of execution, 0, 1, or 2. */
 
 void random_positions ( int dim, int NM, int NV, int *p_int, char *p_char );
-// Generates random positions for the NM monomials in NV variables,
-// for a system of dimension dim, stored twice in the arrays p_int and p_char.
+/*
+ * Generates random positions for the NM monomials in NV variables, for
+ * a system of dimension dim, stored twice in the arrays p_int and p_char. */
 
 void random_exponents ( int NM, int NV, int d, int *e_int, char *e_char );
-// Generates random exponents for the NM monomials in NV variables,
-// of degree at most d, stored twice in the arrays e_int and e_char.
+/*
+ * Generates random exponents for the NM monomials in NV variables,
+ * of degree at most d, stored twice in the arrays e_int and e_char. */
 
-double random_double ( void )
-// Returns a random double in [0,1].
+double random_double ( void ) // Returns a random double in [0,1].
 {
    return rand()/((double) RAND_MAX);
 }
 
 template<class realD, class realH>
-void random_point ( int dim, complexD<realD> *x_h, complexH<realH> *x )
-// Generates a random complex point of length dim,
-// stored twice in the arrays x and x_h.
+void random_point ( int dim, complexD<realD> *x_d, complexH<realH> *x_h )
+/*
+ * Generates a random complex point of length dim,
+ * stored twice in the arrays x_d and x_h. */
 {
-/*   for(int i=0; i<dim; i++)
+   for(int i=0; i<dim; i++)
    {
       double temp = random_double()*2*M_PI;
-      x[i].re = cos(temp);   x[i].im = sin(temp);
-      x_h[i].re = cos(temp); x_h[i].im = sin(temp);
-   } */
-   for (int i=0;i<dim;i++)
-     {
-        double temp = random_double()*2*M_PI;
-        // cout << "cos=" << cos(temp) << endl;
-        // cout << "sin=" << sin(temp) << endl;
-        x_h[i].initH(cos(temp),sin(temp));
-        x[i].init(cos(temp),sin(temp));
-        // cout << "cos=" << cos(temp) << endl;
-        // cout << "sin=" << sin(temp) << endl;
-        // cout << x[i];
-     }
+      x_d[i].initH(cos(temp),sin(temp));
+      x_h[i].init(cos(temp),sin(temp));
+   }
 }
 
 template<class realD, class realH>
 void random_coefficients ( int n, complexD<realD> *c_h, complexH<realH> *c )
-// Generates n random coefficients
-// stored twice in the arrays c_h and c.
+/*
+ * Generates n random coefficients
+ * stored twice in the arrays c_h and c. */
 {
    for(int i=0; i<n; i++)
    {
@@ -86,72 +117,45 @@ void random_coefficients ( int n, complexD<realD> *c_h, complexH<realH> *c )
 template<class realD, class realH>
 void generate_system
  ( int dim, int NM, int NV, int deg, int *p_int, char *p_char,
-   int *e_int, char *e_char, complexD<realD> *c_h, complexH<realH> *c )
-// Generates a random system of dimension dim.
-// On input are the number of monomials NM, number of variables NV,
-// largest degree deg.  On returns are positions in p_int, p_char,
-// exponents e_int, e_char, and coefficients in c_h, c.
+   int *e_int, char *e_char, complexD<realD> *c_d, complexH<realH> *c_h )
+/*
+ * Generates a random system of dimension dim.
+ * On input are the number of monomials NM, number of variables NV,
+ * largest degree deg.  On returns are positions in p_int, p_char,
+ * exponents e_int, e_char, and coefficients in c_d, c_h. */
 {
    random_positions(dim,NM,NV,p_int,p_char);
    random_exponents(NM,NV,deg,e_int,e_char);
-   random_coefficients(NM,c_h,c);
+   random_coefficients(NM,c_d,c_h);
 }
 
 template<class realD, class realH>
 void error_on_factors
  ( int NM, complexD<realD> *factors_h, complexH<realH> *factors_s )
-// Compares the factors computes on the host with those computed by
-// the CPU, for a number of monomials equal to NM.
+/*
+ * Compares the factors computes on the host with those computed by
+ * the CPU, for a number of monomials equal to NM. */
 {
-
    complexH<realH> factors_error(0.0,0.0);   
    complexH<realH> temp;
 
-   for(int i=0;i<NM;i++)
-      {
-        comp1_gqd2qd(&factors_h[i], &temp);
-        factors_error=factors_error+(factors_s[i]-temp)*(factors_s[i]-temp);
-        cout << temp;
-        cout << factors_s[i]; 
-      }
-
-  // cout << scientific << setprecision(sizeof(T)*2);
-   cout << "factors_error=" << factors_error; 
-/*
-   float factors_error_inf_re = 0.0;
-   float factors_error_inf_im = 0.0;
-   int factors_re_err_ind_i = 0;
-   int factors_im_err_ind_i = 0;
-
    for(int i=0; i<NM; i++)
    {
-      if(abs(factors_h[i].re-factors_s[i].re) > factors_error_inf_re)
-      {
-         factors_error_inf_re = abs(factors_h[i].re-factors_s[i].re);
-         factors_re_err_ind_i = i;
-      }
-      if(abs(factors_h[i].im-factors_s[i].im) > factors_error_inf_im)
-      {
-         factors_error_inf_im = abs(factors_h[i].im-factors_s[i].im);
-         factors_im_err_ind_i = i;
-      }
+      comp1_gqd2qd(&factors_h[i], &temp);
+      factors_error=factors_error+(factors_s[i]-temp)*(factors_s[i]-temp);
+      cout << temp;
+      cout << factors_s[i]; 
    }
-   cout << scientific << setprecision(8);
-   cout << " factors_error_re = " << factors_error_inf_re << " "
-        << factors_re_err_ind_i << endl;
-   cout << " factors_error_im = " << factors_error_inf_im << " "
-        << factors_im_err_ind_i << endl;
-
-*/
+   cout << "factors_error=" << factors_error; 
 }
 
 template<class realD, class realH>
 void error_on_derivatives
  ( int dim, complexH<realH> **derivatives, complexD<realD> *values_h )
-// compares the derivatives of the system of dimension dim
-// computed on the GPU with those computed by the CPU
+/*
+ * Compares the derivatives of the system of dimension dim
+ * computed on the GPU with those computed by the CPU. */
 {
-
    complexH<realH> poly_error;
    complexH<realH> temp;
 
@@ -162,7 +166,6 @@ void error_on_derivatives
          poly_error=poly_error
             +(derivatives[j][i]-temp)*(derivatives[j][i]-temp);
       }
-
    cout << "poly_error=" << poly_error;
 }
 
