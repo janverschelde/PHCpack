aab737e3e0aae196eb3292cbda29489081426b0c
Jan Verschelde <janv@uic.edu>
2017-01-22 15:00:19 -0600

    reformatting code to run GPU tests

--- src/GPU/Path/Main/pieri_test.h
+++ src/GPU/Path/Main/pieri_test.h
@@ -1,9 +1,6 @@
-/*
- * pieri_test.h
- *
- *  Created on: Feb 8, 2015
- *      Author: yxc
- */
+/* The file "pieri_test.h" provide the main prototype to test path tracking
+ * in a large dimensional space.  The paths are defined by a Pieri homotopy. 
+ * The file was made by Xiangcheng Yu on 8 Febuary 2015. */
 
 #ifndef PIERI_TEST_H_
 #define PIERI_TEST_H_
@@ -12,10 +9,13 @@
 #include "path_gpu.h"
 #include "err_check.h"
 
-//#include "path_host.h"
 #include "init_test.h"
 #include "path_test.h"
 
-void Pieri_Test(int dim_start, int dim_end, Parameter path_parameter, int sys_type, int device_option);
+void Pieri_Test
+ ( int dim_start, int dim_end, Parameter path_parameter, int sys_type,
+   int device_option );
+
+// Runs a sequence of path tracking jobs defined by Pieri homotopies.
 
 #endif /* PIERI_TEST_H_ */
