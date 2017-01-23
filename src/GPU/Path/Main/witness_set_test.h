2326f405d8e69008d9bfc74ea08abd382e808bd7
Jan Verschelde <janv@uic.edu>
2017-01-22 19:51:46 -0600

    reformatting code in the test on the witness sets in the Path library

--- src/GPU/Path/Main/witness_set_test.h
+++ src/GPU/Path/Main/witness_set_test.h
@@ -1,22 +1,16 @@
-/*
- * witness_set_test.h
- *
- *  Created on: Feb 8, 2015
- *      Author: yxc
- */
+/* The file "witness_set_test.h" was written by Xiangcheng Yu
+ * on 8 February 2015. */
 
 #ifndef WITNESS_SET_TEST_H_
 #define WITNESS_SET_TEST_H_
 
+#include <vector>
 #include "eval_host.h"
 #include "path_gpu.h"
-
 #include "path_test.h"
 
-#include <vector>
-
-int witness_set_test(Workspace& workspace_cpu, CPUInstHom& cpu_inst_hom, Parameter path_parameter, CT* sol0, CT t, int device_option = 1);
-
-
+int witness_set_test
+ ( Workspace& workspace_cpu, CPUInstHom& cpu_inst_hom,
+   Parameter path_parameter, CT* sol0, CT t, int device_option = 1 );
 
 #endif /* WITNESS_SET_TEST_H_ */
