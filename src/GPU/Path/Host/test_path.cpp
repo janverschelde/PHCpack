06111ea935b7183ad4031f92526481a8337197c0
Jan Verschelde <janv@uic.edu>
2017-01-29 15:43:37 -0600

    added reading of start system and start solutions to the test of the path tracker

--- src/GPU/Path/Host/test_path.cpp
+++ src/GPU/Path/Host/test_path.cpp
@@ -3,18 +3,33 @@
 #include <iostream>
 #include <string>
 #include "complexH.h"
+#include "polysol.h"
 #include "eval_host.h"
 #include "path_host.h"
 
 using namespace std;
 
 template <class ComplexType, class RealType>
-int path_test ( PolySys<ComplexType,RealType>& polynomials );
-// calls Newton's method on the polynomials
+int test ( string targetfile, string startfile );
+// reads target and start system from file, calls path_test
 
 template <class ComplexType, class RealType>
-int test ( string filename );
-// reads a double precision system from file, calls path_test
+int path_test
+ ( PolySys<ComplexType,RealType>& targetpols,
+   PolySys<ComplexType,RealType>& startpols );
+// Calls the path tracker on a homotopy defined by the target system
+// in targetpols and the start system in startpols.
+
+string* x_var ( string x, int dim );
+// Generates a string of variables of size dim.
+
+template <class ComplexType, class RealType>
+bool read_homotopy_from_file
+ ( PolySys<ComplexType,RealType>& targetsys,
+   PolySys<ComplexType,RealType>& startsys,
+   PolySolSet<ComplexType,RealType>* sols,
+   string startname, string targetname );
+// Reads a target system, start system, and solutions from file.
 
 // Parameters
 #define N_PREDICTOR           4
@@ -39,10 +54,15 @@ int test ( string filename );
 
 int main ( void )
 {
-   cout << "Testing the polynomials ..." << endl;
+   cout << "Testing the path tracker ..." << endl;
 
-   string name; // = "/Users/jan/PHCv2/Demo/cyclic5";
-   cout << "-> give a file name : "; cin >> name;
+   string name4target; // = "/Users/jan/PHCv2/Demo/cyclic5";
+   cout << "\n-> give a file name for the target system : ";
+   cin >> name4target;
+
+   string name4start;
+   cout << "\n-> give a file name for the start system : ";
+   cin >> name4start;
 
    char choice;
 
@@ -52,16 +72,14 @@ int main ( void )
    cout << "  2. quad double precision" << endl;
    cout << "Type 0, 1, or 2 : "; cin >> choice;
 
-   cout << "\nReading from file " << name << " ..." << endl;
-
    int fail = 0;
 
    if(choice == '0')
-      fail = test<complexH<double>,double>(name);
+      fail = test<complexH<double>,double>(name4target,name4start);
    else if(choice == '1')
-      fail = test<complexH<dd_real>,dd_real>(name);
+      fail = test<complexH<dd_real>,dd_real>(name4target,name4start);
    else if(choice == '2')
-      fail = test<complexH<qd_real>,qd_real>(name);
+      fail = test<complexH<qd_real>,qd_real>(name4target,name4start);
    else
       cout << "Invalid choice " << choice << " for the precision." << endl; 
 
@@ -69,31 +87,107 @@ int main ( void )
 }
 
 template <class ComplexType, class RealType>
-int test ( string filename )
+int test ( string targetfile, string startfile )
 {
-   PolySys<ComplexType,RealType> polynomials;
+   PolySys<ComplexType,RealType> targetsys;
+   PolySys<ComplexType,RealType> startsys;
+   // PolySolSet<ComplexType,RealType> sols;
 
-   polynomials.read_file(filename);
-   cout << "The polynomials on the file " << filename << " :" << endl;
-   polynomials.print();
+   cout << "\nReading from file " << targetfile << " ..." << endl;
+   targetsys.read_file(targetfile);
+   cout << "The polynomials on the file " << targetfile << " :" << endl;
+   targetsys.print();
 
-   return path_test<ComplexType,RealType>(polynomials);
+   cout << "\nReading from file " << startfile << " ..." << endl;
+   startsys.read_file(startfile);
+   cout << "The polynomials on the file " << startfile << " :" << endl;
+   startsys.print();
+
+   ifstream solfile(startfile.c_str());
+   PolySolSet<ComplexType,RealType> sols(solfile);
+   cout << "The solutions on the file " << startfile << " :" << endl;
+   sols.print();
+
+   //bool success = read_homotopy_from_file<ComplexType,RealType>
+   //   (targetsys,startsys,&sols,targetfile,startfile);
+
+   return path_test<ComplexType,RealType>(targetsys,startsys);
+}
+
+string* x_var ( string x, int dim )
+{
+   string* var = new string[dim];
+
+   for(int i=0; i< dim; i++)
+   {
+      ostringstream ss;
+      ss << x << i;
+      var[i] = ss.str();
+   }
+   return var;
 }
 
 template <class ComplexType, class RealType>
-int path_test ( PolySys<ComplexType,RealType>& polynomials )
+bool read_homotopy_from_file
+ ( PolySys<ComplexType,RealType>& targetsys,
+   PolySys<ComplexType,RealType>& startsys,
+   PolySolSet<ComplexType,RealType>* sols,
+   string startname, string targetname )
 {
-   cout << "The dimension : " << polynomials.dim << endl;
+   ifstream targetfile(targetname.c_str());
+   if(targetfile.is_open() == false)
+   {
+      cout << "Unable to open file with name \""
+           << targetname << "\"." << endl;
+      return false;
+   }
+   ifstream startfile(startname.c_str());
+   if(startfile.is_open() == false)
+   {
+      cout << "Unable to open file with name \""
+           << startname << "\"." << endl;
+      return false;
+   }
+   VarDict pos_dict;
 
-   ComplexType* arg = new ComplexType[polynomials.dim];
-   for(int i=0; i<polynomials.dim; i++) arg[i].init(i,0.0);
+   startsys.read_file(startfile,pos_dict);
+
+   int dim = startsys.dim;
+
+   string x_name = "x";
+   string* x_names = x_var(x_name, dim);
+   startsys.pos_var = x_names;
+
+   cout << "the start system read from file" << endl;
+   startsys.print();
+
+   // if(sols != NULL) sols -> init(targetfile, pos_dict);
+
+   targetsys.read_file(targetfile,pos_dict);
+
+   string x_name_target = "x";
+   string* x_names_target = x_var(x_name_target,dim);
+   targetsys.pos_var = x_names_target;
+
+   cout << "the target system read from file" << endl;
+   targetsys.print();
+
+   return true;
+}
+
+template <class ComplexType, class RealType>
+int path_test
+ ( PolySys<ComplexType,RealType>& targetpols,
+   PolySys<ComplexType,RealType>& startpols )
+{
+   cout << "The dimension : " << targetpols.dim << endl;
 
    CPUInstHom<ComplexType,RealType> ped; // data for eval_host
    Workspace<ComplexType> wrk;
    ComplexType alpha,t;
    alpha.init(0.0,0.0); // initialize the data for eval_host
    t.init(0.0,0.0);
-   ped.init(polynomials,polynomials.dim,polynomials.n_eq,0,alpha);
+   ped.init(targetpols,startpols,targetpols.dim,targetpols.n_eq,0,alpha);
    ped.init_workspace(wrk);
 
    Parameter pars(N_PREDICTOR, MAX_STEP, MAX_IT, MAX_DELTA_T,
