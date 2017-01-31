e95cff2e49589fb5f8082de03e9f38ab02456984
Jan Verschelde <janv@uic.edu>
2017-01-30 22:29:21 -0600

    added double double and quad double versions of the wrappers to the templated code

--- src/Ada/PHCtoC/ademanypaths.cpp
+++ src/Ada/PHCtoC/ademanypaths.cpp
@@ -7,7 +7,6 @@
 #include "solcon.h"
 #include "phcpack.h"
 #include "lib2path.h"
-// #include "parameter.h"
 #include "path_host.h"
 #include "ademanypaths.h"
 
@@ -75,6 +74,88 @@ extern "C" int standard_ademanypaths
    return 0;
 }
 
+extern "C" int dobldobl_ademanypaths
+ ( int verbose, double regamma, double imgamma )
+{
+   int fail;
+   PolySys<complexH<dd_real>,dd_real> ps;
+   PolySys<complexH<dd_real>,dd_real> qs;
+   PolySolSet<complexH<dd_real>,dd_real> sols;
+
+   fail = copy_dobldobl_target_system_to_container();
+
+   if(verbose > 0)
+   {
+      int dim, len;
+
+      cout << endl;
+      cout << "Tracking many paths ..." << endl;
+      cout << "gamma = " << setprecision(16)
+           << regamma << " + i* " << imgamma << endl;
+      fail = syscon_number_of_dobldobl_polynomials(&dim);
+      cout << "number of polynomials : " << dim << endl;
+      fail = solcon_number_of_dobldobl_solutions(&len);
+      cout << "number of solutions : " << len << endl;
+   }
+
+   lib2path_read_dobldobl_sys(verbose,ps);
+
+   fail = copy_dobldobl_start_system_to_container();
+
+   lib2path_read_dobldobl_sys(verbose,qs);
+   lib2path_read_dobldobl_sols(qs,sols);
+
+   fail = dobldobl_manytrack(verbose,regamma,imgamma,ps,qs,sols);
+
+   if(verbose > 0)
+      cout << "writing the solutions to the container ..." << endl;
+
+   lib2path_write_dobldobl_sols(sols);
+
+   return 0;
+}
+
+extern "C" int quaddobl_ademanypaths
+ ( int verbose, double regamma, double imgamma )
+{
+   int fail;
+   PolySys<complexH<qd_real>,qd_real> ps;
+   PolySys<complexH<qd_real>,qd_real> qs;
+   PolySolSet<complexH<qd_real>,qd_real> sols;
+
+   fail = copy_quaddobl_target_system_to_container();
+
+   if(verbose > 0)
+   {
+      int dim, len;
+
+      cout << endl;
+      cout << "Tracking many paths ..." << endl;
+      cout << "gamma = " << setprecision(16)
+           << regamma << " + i* " << imgamma << endl;
+      fail = syscon_number_of_quaddobl_polynomials(&dim);
+      cout << "number of polynomials : " << dim << endl;
+      fail = solcon_number_of_quaddobl_solutions(&len);
+      cout << "number of solutions : " << len << endl;
+   }
+
+   lib2path_read_quaddobl_sys(verbose,ps);
+
+   fail = copy_quaddobl_start_system_to_container();
+
+   lib2path_read_quaddobl_sys(verbose,qs);
+   lib2path_read_quaddobl_sols(qs,sols);
+
+   fail = quaddobl_manytrack(verbose,regamma,imgamma,ps,qs,sols);
+
+   if(verbose > 0)
+      cout << "writing the solutions to the container ..." << endl;
+
+   lib2path_write_quaddobl_sols(sols);
+
+   return 0;
+}
+
 int standard_manytrack
  ( int verbose, double regamma, double imgamma,
    PolySys<complexH<double>,double>& p,
@@ -130,6 +211,126 @@ int standard_manytrack
          cout << "The solution " << path_idx << endl;
          for(int k=0; k<p.dim; k++)
             cout << k << " :" << setw(24) << workspace_cpu.x_last[k];
+      }
+   }
+   return 0;
+}
+
+int dobldobl_manytrack
+ ( int verbose, double regamma, double imgamma,
+   PolySys<complexH<dd_real>,dd_real>& p,
+   PolySys<complexH<dd_real>,dd_real>& q,
+   PolySolSet<complexH<dd_real>,dd_real>& s )
+{
+   double tpred,teval,tmgs;
+   bool success;
+   complexH<dd_real>* sol = s.get_sol(0);
+   complexH<dd_real> alpha,t;
+   CPUInstHom<complexH<dd_real>,dd_real> cpu_inst_hom;
+   Workspace< complexH<dd_real> > workspace_cpu;
+   Parameter pars(N_PREDICTOR, MAX_STEP, MAX_IT, MAX_DELTA_T, \
+      MAX_DELTA_T_END, MIN_DELTA_T, ERR_MAX_RES, ERR_MAX_DELTA_X, \
+      ERR_MAX_FIRST_DELTA_X, ERR_MIN_ROUND_OFF, MAX_IT_REFINE, \
+      ERR_MIN_ROUND_OFF_REFINE, STEP_INCREASE, STEP_DECREASE);
+  
+   if(verbose > 0)
+   {
+      cout << "The first solution on input :" << endl;
+      for(int k=0; k<p.dim; k++)
+      {
+         cout << k << " :";
+         cout << setw(40) << scientific << setprecision(32) << sol[k];
+      }
+   }
+   int fail,n_path;
+   fail = solcon_number_of_dobldobl_solutions(&n_path);
+
+   alpha = complexH<dd_real>(regamma,imgamma);
+   cpu_inst_hom.init(p,q,p.dim,p.n_eq,1,alpha,verbose);
+   cpu_inst_hom.init_workspace(workspace_cpu);
+
+   for(int path_idx=0; path_idx<n_path; path_idx++)
+   {
+      if(verbose > 0) cout << "tracking path " << path_idx << endl;
+      complexH<dd_real>* sol0 = s.get_sol(path_idx);
+      complexH<dd_real>* sol_new = NULL;
+      t = complexH<dd_real>(0.0,0);
+      workspace_cpu.init_x_t_idx();
+      workspace_cpu.update_x_t_value(sol0,t);
+      workspace_cpu.update_x_t_idx();
+      workspace_cpu.path_idx = path_idx;
+      tpred = 0.0; teval = 0.0; tmgs = 0.0;
+      if(verbose > 0) cout << "calling path_tracker ..." << endl;
+      success = path_tracker(workspace_cpu,cpu_inst_hom,pars,
+                             tpred,teval,tmgs,0,verbose);
+      if(verbose > 0) cout << "done with call to path_tracker." << endl;
+      s.change_sol(path_idx,workspace_cpu.x_last);
+      if(verbose > 0)
+      {
+         cout.precision(32);
+         cout << "The solution " << path_idx << endl;
+         for(int k=0; k<p.dim; k++)
+            cout << k << " :" << setw(40) << workspace_cpu.x_last[k];
+      }
+   }
+   return 0;
+}
+
+int quaddobl_manytrack
+ ( int verbose, double regamma, double imgamma,
+   PolySys<complexH<qd_real>,qd_real>& p,
+   PolySys<complexH<qd_real>,qd_real>& q,
+   PolySolSet<complexH<qd_real>,qd_real>& s )
+{
+   double tpred,teval,tmgs;
+   bool success;
+   complexH<qd_real>* sol = s.get_sol(0);
+   complexH<qd_real> alpha,t;
+   CPUInstHom<complexH<qd_real>,qd_real> cpu_inst_hom;
+   Workspace< complexH<qd_real> > workspace_cpu;
+   Parameter pars(N_PREDICTOR, MAX_STEP, MAX_IT, MAX_DELTA_T, \
+      MAX_DELTA_T_END, MIN_DELTA_T, ERR_MAX_RES, ERR_MAX_DELTA_X, \
+      ERR_MAX_FIRST_DELTA_X, ERR_MIN_ROUND_OFF, MAX_IT_REFINE, \
+      ERR_MIN_ROUND_OFF_REFINE, STEP_INCREASE, STEP_DECREASE);
+  
+   if(verbose > 0)
+   {
+      cout << "The first solution on input :" << endl;
+      for(int k=0; k<p.dim; k++)
+      {
+         cout << k << " :";
+         cout << setw(72) << scientific << setprecision(64) << sol[k];
+      }
+   }
+   int fail,n_path;
+   fail = solcon_number_of_quaddobl_solutions(&n_path);
+
+   alpha = complexH<qd_real>(regamma,imgamma);
+   cpu_inst_hom.init(p,q,p.dim,p.n_eq,1,alpha,verbose);
+   cpu_inst_hom.init_workspace(workspace_cpu);
+
+   for(int path_idx=0; path_idx<n_path; path_idx++)
+   {
+      if(verbose > 0) cout << "tracking path " << path_idx << endl;
+      complexH<qd_real>* sol0 = s.get_sol(path_idx);
+      complexH<qd_real>* sol_new = NULL;
+      t = complexH<qd_real>(0.0,0);
+      workspace_cpu.init_x_t_idx();
+      workspace_cpu.update_x_t_value(sol0,t);
+      workspace_cpu.update_x_t_idx();
+      workspace_cpu.path_idx = path_idx;
+      tpred = 0.0; teval = 0.0; tmgs = 0.0;
+      if(verbose > 0) cout << "calling path_tracker ..." << endl;
+      success = path_tracker(workspace_cpu,cpu_inst_hom,pars,
+                             tpred,teval,tmgs,0,verbose);
+      if(verbose > 0) cout << "done with call to path_tracker." << endl;
+      s.change_sol(path_idx,workspace_cpu.x_last);
+      if(verbose > 0)
+      {
+         cout.precision(64);
+         cout << "The solution " << path_idx << endl;
+         for(int k=0; k<p.dim; k++)
+            cout << k << " :" << setw(72) << workspace_cpu.x_last[k];
       }
    }
    return 0;
--- src/Ada/PHCtoC/ademanypaths.h
+++ src/Ada/PHCtoC/ademanypaths.h
@@ -1,5 +1,6 @@
 /* This file ademanypaths.h collects the prototypes to track many paths
- * using algorithmic differentiation in double precision. */
+ * using algorithmic differentiation in double, double double,
+ * and quad double precision. */
 
 #ifndef __ADEMANYPATHS_H__
 #define __ADEMANYPATHS_H__
@@ -12,6 +13,16 @@ int standard_manytrack
    PolySys<complexH<double>,double>& p,
    PolySys<complexH<double>,double>& q,
    PolySolSet<complexH<double>,double>& s );
+int dobldobl_manytrack
+ ( int verbose, double regamma, double imgamma,
+   PolySys<complexH<dd_real>,dd_real>& p,
+   PolySys<complexH<dd_real>,dd_real>& q,
+   PolySolSet<complexH<dd_real>,dd_real>& s );
+int quaddobl_manytrack
+ ( int verbose, double regamma, double imgamma,
+   PolySys<complexH<qd_real>,qd_real>& p,
+   PolySys<complexH<qd_real>,qd_real>& q,
+   PolySolSet<complexH<qd_real>,qd_real>& s );
 /*
  * DESCRIPTION :
  *   Tracks many paths defined by an artificial parameter homotopy,
@@ -26,6 +37,10 @@ int standard_manytrack
  *   s         solutions of the start system q. */
 
 extern "C" int standard_ademanypaths
+ ( int verbose, double regamma, double imgamma );
+extern "C" int dobldobl_ademanypaths
+ ( int verbose, double regamma, double imgamma );
+extern "C" int quaddobl_ademanypaths
  ( int verbose, double regamma, double imgamma );
 /*
  * DESCRIPTION :
