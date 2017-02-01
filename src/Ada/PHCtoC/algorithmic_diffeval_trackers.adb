d7cfde2147137b593ccdec72243f06f1384658a5
Jan Verschelde <janv@uic.edu>
2017-02-01 08:46:43 -0600

    refactored wrapper code from a test procedure into a package

---
+++ src/Ada/PHCtoC/algorithmic_diffeval_trackers.adb
@@ -0,0 +1,505 @@
+with text_io;                            use text_io;
+with Timing_Package;                     use Timing_Package;
+with Communications_with_User;           use Communications_with_User;
+with Standard_Natural_Numbers;           use Standard_Natural_Numbers;
+with Standard_Natural_Numbers_io;        use Standard_Natural_Numbers_io;
+with Standard_Floating_Numbers;          use Standard_Floating_Numbers;
+with Standard_Complex_Numbers_io;        use Standard_Complex_Numbers_io;
+with Standard_Random_Numbers;
+with DoblDobl_Complex_Numbers;
+with DoblDobl_Complex_Numbers_io;        use DoblDobl_Complex_Numbers_io;
+with DoblDobl_Random_Numbers;
+with QuadDobl_Complex_Numbers;
+with QuadDobl_Complex_Numbers_io;        use QuadDobl_Complex_Numbers_io;
+with QuadDobl_Random_Numbers;
+with Standard_Complex_Poly_Systems;
+with Standard_Complex_Poly_Systems_io;   use Standard_Complex_Poly_Systems_io;
+with DoblDobl_Complex_Poly_Systems;
+with DoblDobl_Complex_Poly_Systems_io;   use DoblDobl_Complex_Poly_Systems_io;
+with QuadDobl_Complex_Poly_Systems;
+with QuadDobl_Complex_Poly_Systems_io;   use QuadDobl_Complex_Poly_Systems_io;
+with Standard_Complex_Solutions;
+with Standard_Complex_Solutions_io;      use Standard_Complex_Solutions_io;
+with Standard_System_and_Solutions_io;
+with DoblDobl_Complex_Solutions;
+with DoblDobl_Complex_Solutions_io;      use DoblDobl_Complex_Solutions_io;
+with DoblDobl_System_and_Solutions_io;
+with QuadDobl_Complex_Solutions;
+with QuadDobl_Complex_Solutions_io;      use QuadDobl_Complex_Solutions_io;
+with QuadDobl_System_and_Solutions_io;
+with Standard_PolySys_Container;
+with Standard_Solutions_Container;
+with DoblDobl_PolySys_Container;
+with DoblDobl_Solutions_Container;
+with QuadDobl_PolySys_Container;
+with QuadDobl_Solutions_Container;
+with PHCpack_Operations;
+
+package body Algorithmic_DiffEval_Trackers is
+
+  function Prompt_for_Verbose return integer32 is
+
+  -- DESCRIPTION :
+  --   Asks the user if extra output is wanted and returns 0 or 1.
+
+    ans : character;
+
+  begin
+    new_line;
+    put("Extra output before and after the computations ? (y/n) ");
+    Ask_Yes_or_No(ans);
+    if ans = 'y'
+     then return 1;
+     else return 0;
+    end if;
+  end Prompt_for_Verbose;
+
+-- WRAPPERS TO THE C INTERFACE FUNCTIONS :
+
+  procedure Standard_ADE_Newton ( verbose : in integer32 ) is
+
+    return_of_call : integer32;
+
+    function newton ( v : integer32 ) return integer32;
+    pragma import(C, newton, "standard_adenewton");
+
+  begin
+    return_of_call := newton(verbose);
+  end Standard_ADE_Newton;
+
+  procedure DoblDobl_ADE_Newton ( verbose : in integer32 ) is
+
+    return_of_call : integer32;
+
+    function newton ( v : integer32 ) return integer32;
+    pragma import(C, newton, "dobldobl_adenewton");
+
+  begin
+    return_of_call := newton(verbose);
+  end DoblDobl_ADE_Newton;
+
+  procedure QuadDobl_ADE_Newton ( verbose : in integer32 ) is
+
+    return_of_call : integer32;
+
+    function newton ( v : integer32 ) return integer32;
+    pragma import(C, newton, "quaddobl_adenewton");
+
+  begin
+    return_of_call := newton(verbose);
+  end QuadDobl_ADE_Newton;
+
+  procedure Standard_ADE_Track_One
+              ( verbose : in integer32; 
+                gamma : in Standard_Complex_Numbers.Complex_Number ) is
+
+    use Standard_Complex_Numbers;
+
+    return_of_call : integer32;
+    regam : constant double_float := REAL_PART(gamma);
+    imgam : constant double_float := IMAG_PART(gamma);
+
+    function one_track ( v : integer32; r,i : double_float ) return integer32;
+    pragma import(C, one_track, "standard_adeonepath");
+
+  begin
+    return_of_call := one_track(verbose,regam,imgam);
+  end Standard_ADE_Track_One;
+
+  procedure DoblDobl_ADE_Track_One
+              ( verbose : in integer32; 
+                gamma : in Standard_Complex_Numbers.Complex_Number ) is
+
+    use Standard_Complex_Numbers;
+
+    return_of_call : integer32;
+    regam : constant double_float := REAL_PART(gamma);
+    imgam : constant double_float := IMAG_PART(gamma);
+
+    function one_track ( v : integer32; r,i : double_float ) return integer32;
+    pragma import(C, one_track, "dobldobl_adeonepath");
+
+  begin
+    return_of_call := one_track(verbose,regam,imgam);
+  end DoblDobl_ADE_Track_One;
+
+  procedure QuadDobl_ADE_Track_One
+              ( verbose : in integer32; 
+                gamma : in Standard_Complex_Numbers.Complex_Number ) is
+
+    use Standard_Complex_Numbers;
+
+    return_of_call : integer32;
+    regam : constant double_float := REAL_PART(gamma);
+    imgam : constant double_float := IMAG_PART(gamma);
+
+    function one_track ( v : integer32; r,i : double_float ) return integer32;
+    pragma import(C, one_track, "quaddobl_adeonepath");
+
+  begin
+    return_of_call := one_track(verbose,regam,imgam);
+  end QuadDobl_ADE_Track_One;
+
+  procedure Standard_ADE_Track_Many
+              ( verbose : in integer32;
+                gamma : in Standard_Complex_Numbers.Complex_Number ) is
+
+    use Standard_Complex_Numbers;
+
+    return_of_call : integer32;
+    regam : constant double_float := REAL_PART(gamma);
+    imgam : constant double_float := IMAG_PART(gamma);
+
+    function many_track ( v : integer32; r,i : double_float ) return integer32;
+    pragma import(C, many_track, "standard_ademanypaths");
+
+  begin
+    return_of_call := many_track(verbose,regam,imgam);
+  end Standard_ADE_Track_Many;
+
+  procedure DoblDobl_ADE_Track_Many
+              ( verbose : in integer32;
+                gamma : in Standard_Complex_Numbers.Complex_Number ) is
+
+    use Standard_Complex_Numbers;
+
+    return_of_call : integer32;
+    regam : constant double_float := REAL_PART(gamma);
+    imgam : constant double_float := IMAG_PART(gamma);
+
+    function many_track ( v : integer32; r,i : double_float ) return integer32;
+    pragma import(C, many_track, "dobldobl_ademanypaths");
+
+  begin
+    return_of_call := many_track(verbose,regam,imgam);
+  end DoblDobl_ADE_Track_Many;
+
+  procedure QuadDobl_ADE_Track_Many
+              ( verbose : in integer32;
+                gamma : in Standard_Complex_Numbers.Complex_Number ) is
+
+    use Standard_Complex_Numbers;
+
+    return_of_call : integer32;
+    regam : constant double_float := REAL_PART(gamma);
+    imgam : constant double_float := IMAG_PART(gamma);
+
+    function many_track ( v : integer32; r,i : double_float ) return integer32;
+    pragma import(C, many_track, "quaddobl_ademanypaths");
+
+  begin
+    return_of_call := many_track(verbose,regam,imgam);
+  end QuadDobl_ADE_Track_Many;
+
+-- INTERACTIVE DRIVERS :
+
+  procedure Standard_Newton is
+
+    use Standard_Complex_Poly_Systems;
+    use Standard_Complex_Solutions;
+
+    p : Link_to_Poly_Sys;
+    sols,newtsols : Solution_List;
+    verbose : integer32;
+
+  begin
+    new_line;
+    put_line("Reading a system with solutions ...");
+    Standard_System_and_Solutions_io.get(p,sols);
+    new_line;
+    put("Read "); put(Length_Of(sols),1); put_line(" solutions.");
+    put_line("Initializing the systems container ...");
+    Standard_PolySys_Container.Initialize(p.all);
+    put_line("Initializing the solutions container ...");
+    Standard_Solutions_Container.Initialize(sols);
+    verbose := Prompt_for_Verbose;
+    Standard_ADE_Newton(verbose);
+    newtsols := Standard_Solutions_Container.Retrieve;
+    put_line("The solutions after Newton's method :");
+    put(standard_output,Length_Of(newtsols),
+        natural32(Head_Of(newtsols).n),newtsols);
+  end Standard_Newton;
+
+  procedure DoblDobl_Newton is
+
+    use DoblDobl_Complex_Poly_Systems;
+    use DoblDobl_Complex_Solutions;
+
+    p : Link_to_Poly_Sys;
+    sols,newtsols : Solution_List;
+    verbose : integer32;
+
+  begin
+    new_line;
+    put_line("Reading a system with solutions ...");
+    DoblDobl_System_and_Solutions_io.get(p,sols);
+    new_line;
+    put("Read "); put(Length_Of(sols),1); put_line(" solutions.");
+    put_line("Initializing the systems container ...");
+    DoblDobl_PolySys_Container.Initialize(p.all);
+    put_line("Initializing the solutions container ...");
+    DoblDobl_Solutions_Container.Initialize(sols);
+    verbose := Prompt_for_Verbose;
+    DoblDobl_ADE_Newton(verbose);
+    newtsols := DoblDobl_Solutions_Container.Retrieve;
+    put_line("The solutions after Newton's method :");
+    put(standard_output,Length_Of(newtsols),
+        natural32(Head_Of(newtsols).n),newtsols);
+  end DoblDobl_Newton;
+
+  procedure QuadDobl_Newton is
+
+    use QuadDobl_Complex_Poly_Systems;
+    use QuadDobl_Complex_Solutions;
+
+    p : Link_to_Poly_Sys;
+    sols,newtsols : Solution_List;
+    verbose : integer32;
+
+  begin
+    new_line;
+    put_line("Reading a system with solutions ...");
+    QuadDobl_System_and_Solutions_io.get(p,sols);
+    new_line;
+    put("Read "); put(Length_Of(sols),1); put_line(" solutions.");
+    put_line("Initializing the systems container ...");
+    QuadDobl_PolySys_Container.Initialize(p.all);
+    put_line("Initializing the solutions container ...");
+    QuadDobl_Solutions_Container.Initialize(sols);
+    verbose := Prompt_for_Verbose;
+    QuadDobl_ADE_Newton(verbose);
+    newtsols := QuadDobl_Solutions_Container.Retrieve;
+    put_line("The solutions after Newton's method :");
+    put(standard_output,Length_Of(newtsols),
+        natural32(Head_Of(newtsols).n),newtsols);
+  end QuadDobl_Newton;
+
+  procedure Standard_Track_one_Path is
+
+    use Standard_Complex_Poly_Systems;
+    use Standard_Complex_Solutions;
+
+    target,start : Link_to_Poly_Sys;
+    sols,newtsols : Solution_List;
+    verbose : integer32;
+    gamma : constant Standard_Complex_Numbers.Complex_Number
+          := Standard_Random_Numbers.Random1;
+
+  begin
+    new_line;
+    put_line("Reading a target system ...");
+    get(target);
+    new_line;
+    put_line("Reading a start system with solutions ...");
+    Standard_System_and_Solutions_io.get(start,sols);
+    new_line;
+    put("gamma = "); put(gamma); new_line;
+    new_line;
+    put("Read "); put(Length_Of(sols),1); put_line(" solutions.");
+    put_line("Initializing the data for container copies ...");
+    PHCpack_Operations.Store_Target_System(target.all);
+    PHCpack_Operations.Store_Start_System(start.all);
+    put_line("Initializing the solutions container ...");
+    Standard_Solutions_Container.Initialize(sols);
+    verbose := Prompt_for_Verbose;
+    Standard_ADE_Track_One(verbose,gamma);
+    newtsols := Standard_Solutions_Container.Retrieve;
+    put_line("The solutions after path tracking :");
+    put(standard_output,Length_Of(newtsols),
+        natural32(Head_Of(newtsols).n),newtsols);
+  end Standard_Track_one_Path;
+
+  procedure DoblDobl_Track_one_Path is
+
+    use DoblDobl_Complex_Poly_Systems;
+    use DoblDobl_Complex_Solutions;
+
+    target,start : Link_to_Poly_Sys;
+    sols,newtsols : Solution_List;
+    verbose : integer32;
+    gamma : constant Standard_Complex_Numbers.Complex_Number
+          := Standard_Random_Numbers.Random1;
+
+  begin
+    new_line;
+    put_line("Reading a target system ...");
+    get(target);
+    new_line;
+    put_line("Reading a start system with solutions ...");
+    DoblDobl_System_and_Solutions_io.get(start,sols);
+    new_line;
+    put("gamma = "); put(gamma); new_line;
+    new_line;
+    put("Read "); put(Length_Of(sols),1); put_line(" solutions.");
+    put_line("Initializing the data for container copies ...");
+    PHCpack_Operations.Store_Target_System(target.all);
+    PHCpack_Operations.Store_Start_System(start.all);
+    put_line("Initializing the solutions container ...");
+    DoblDobl_Solutions_Container.Initialize(sols);
+    verbose := Prompt_for_Verbose;
+    DoblDobl_ADE_Track_One(verbose,gamma);
+    newtsols := DoblDobl_Solutions_Container.Retrieve;
+    put_line("The solutions after path tracking :");
+    put(standard_output,Length_Of(newtsols),
+        natural32(Head_Of(newtsols).n),newtsols);
+  end DoblDobl_Track_one_Path;
+
+  procedure QuadDobl_Track_one_Path is
+
+    use QuadDobl_Complex_Poly_Systems;
+    use QuadDobl_Complex_Solutions;
+
+    target,start : Link_to_Poly_Sys;
+    sols,newtsols : Solution_List;
+    verbose : integer32;
+    gamma : constant Standard_Complex_Numbers.Complex_Number
+          := Standard_Random_Numbers.Random1;
+
+  begin
+    new_line;
+    put_line("Reading a target system ...");
+    get(target);
+    new_line;
+    put_line("Reading a start system with solutions ...");
+    QuadDobl_System_and_Solutions_io.get(start,sols);
+    new_line;
+    put("gamma = "); put(gamma); new_line;
+    new_line;
+    put("Read "); put(Length_Of(sols),1); put_line(" solutions.");
+    put_line("Initializing the data for container copies ...");
+    PHCpack_Operations.Store_Target_System(target.all);
+    PHCpack_Operations.Store_Start_System(start.all);
+    put_line("Initializing the solutions container ...");
+    QuadDobl_Solutions_Container.Initialize(sols);
+    verbose := Prompt_for_Verbose;
+    QuadDobl_ADE_Track_One(verbose,gamma);
+    newtsols := QuadDobl_Solutions_Container.Retrieve;
+    put_line("The solutions after path tracking :");
+    put(standard_output,Length_Of(newtsols),
+        natural32(Head_Of(newtsols).n),newtsols);
+  end QuadDobl_Track_one_Path;
+
+  procedure Standard_Track_many_Paths is
+
+    use Standard_Complex_Poly_Systems;
+    use Standard_Complex_Solutions;
+
+    timer : Timing_Widget;
+    target,start : Link_to_Poly_Sys;
+    sols,newtsols : Solution_List;
+    verbose : integer32;
+    gamma : constant Standard_Complex_Numbers.Complex_Number
+          := Standard_Random_Numbers.Random1;
+
+  begin
+    new_line;
+    put_line("Reading a target system ...");
+    get(target);
+    new_line;
+    put_line("Reading a start system with solutions ...");
+    Standard_System_and_Solutions_io.get(start,sols);
+    new_line;
+    put("gamma = "); put(gamma); new_line;
+    new_line;
+    put("Read "); put(Length_Of(sols),1); put_line(" solutions.");
+    put_line("Initializing the data for container copies ...");
+    PHCpack_Operations.Store_Target_System(target.all);
+    PHCpack_Operations.Store_Start_System(start.all);
+    put_line("Initializing the solutions container ...");
+    Standard_Solutions_Container.Initialize(sols);
+    verbose := Prompt_for_Verbose;
+    tstart(timer);
+    Standard_ADE_Track_Many(verbose,gamma);
+    tstop(timer);
+    newtsols := Standard_Solutions_Container.Retrieve;
+    put_line("The solutions after path tracking :");
+    put(standard_output,Length_Of(newtsols),
+        natural32(Head_Of(newtsols).n),newtsols);
+    new_line;
+    print_times
+      (standard_output,timer,"tracking many paths in double precision");
+  end Standard_Track_many_Paths;
+
+  procedure DoblDobl_Track_many_Paths is
+
+    use DoblDobl_Complex_Poly_Systems;
+    use DoblDobl_Complex_Solutions;
+
+    timer : Timing_Widget;
+    target,start : Link_to_Poly_Sys;
+    sols,newtsols : Solution_List;
+    verbose : integer32;
+    gamma : constant Standard_Complex_Numbers.Complex_Number
+          := Standard_Random_Numbers.Random1;
+
+  begin
+    new_line;
+    put_line("Reading a target system ...");
+    get(target);
+    new_line;
+    put_line("Reading a start system with solutions ...");
+    DoblDobl_System_and_Solutions_io.get(start,sols);
+    new_line;
+    put("gamma = "); put(gamma); new_line;
+    new_line;
+    put("Read "); put(Length_Of(sols),1); put_line(" solutions.");
+    put_line("Initializing the data for container copies ...");
+    PHCpack_Operations.Store_Target_System(target.all);
+    PHCpack_Operations.Store_Start_System(start.all);
+    put_line("Initializing the solutions container ...");
+    DoblDobl_Solutions_Container.Initialize(sols);
+    verbose := Prompt_for_Verbose;
+    tstart(timer);
+    DoblDobl_ADE_Track_Many(verbose,gamma);
+    tstop(timer);
+    newtsols := DoblDobl_Solutions_Container.Retrieve;
+    put_line("The solutions after path tracking :");
+    put(standard_output,Length_Of(newtsols),
+        natural32(Head_Of(newtsols).n),newtsols);
+    new_line;
+    print_times
+      (standard_output,timer,"tracking many paths in double double precision");
+  end DoblDobl_Track_many_Paths;
+
+  procedure QuadDobl_Track_many_Paths is
+
+    use QuadDobl_Complex_Poly_Systems;
+    use QuadDobl_Complex_Solutions;
+
+    timer : Timing_Widget;
+    target,start : Link_to_Poly_Sys;
+    sols,newtsols : Solution_List;
+    verbose : integer32;
+    gamma : constant Standard_Complex_Numbers.Complex_Number
+          := Standard_Random_Numbers.Random1;
+
+  begin
+    new_line;
+    put_line("Reading a target system ...");
+    get(target);
+    new_line;
+    put_line("Reading a start system with solutions ...");
+    QuadDobl_System_and_Solutions_io.get(start,sols);
+    new_line;
+    put("gamma = "); put(gamma); new_line;
+    new_line;
+    put("Read "); put(Length_Of(sols),1); put_line(" solutions.");
+    put_line("Initializing the data for container copies ...");
+    PHCpack_Operations.Store_Target_System(target.all);
+    PHCpack_Operations.Store_Start_System(start.all);
+    put_line("Initializing the solutions container ...");
+    QuadDobl_Solutions_Container.Initialize(sols);
+    verbose := Prompt_for_Verbose;
+    tstart(timer);
+    QuadDobl_ADE_Track_Many(verbose,gamma);
+    tstop(timer);
+    newtsols := QuadDobl_Solutions_Container.Retrieve;
+    put_line("The solutions after path tracking :");
+    put(standard_output,Length_Of(newtsols),
+        natural32(Head_Of(newtsols).n),newtsols);
+    new_line;
+    print_times
+      (standard_output,timer,"tracking many paths in quad double precision");
+  end QuadDobl_Track_many_Paths;
+
+end Algorithmic_DiffEval_Trackers;
---
+++ src/Ada/PHCtoC/algorithmic_diffeval_trackers.ads
@@ -0,0 +1,112 @@
+with Standard_Integer_Numbers;           use Standard_Integer_Numbers;
+with Standard_Complex_Numbers;
+
+package Algorithmic_DiffEval_Trackers is
+
+-- DESCRIPTION :
+--   Defines the interface to the path trackers which use the reverse mode
+--   of algorithmic differentiation.  There are two types of procedures:
+--   (1) Wrappers to the C interface function to the Path library;
+--   (2) Interactive drivers to test the wrappers.
+--   Exported methods are Newton's method, a tracker for one path,
+--   and the tracking of many paths.  The levels of precision are
+--   standard double, double double, and quad double precision.
+
+-- WRAPPERS :
+
+  procedure Standard_ADE_Newton ( verbose : in integer32 );
+  procedure DoblDobl_ADE_Newton ( verbose : in integer32 );
+  procedure QuadDobl_ADE_Newton ( verbose : in integer32 );
+
+  -- DESCRIPTION :
+  --   Wraps the call to the C interface to run Newton's method in
+  --   standard double, double double, or quad double precision
+  --   on the target system stored in the systems container and the
+  --   first solution stored in the solutions container.
+  --   If verbose is positive, then output is written to screen.
+
+  -- REQUIRED :
+  --   The containers have been initialized with a target system
+  --   and at least one start solution.
+
+  procedure Standard_ADE_Track_One
+              ( verbose : in integer32; 
+                gamma : in Standard_Complex_Numbers.Complex_Number );
+  procedure DoblDobl_ADE_Track_One
+              ( verbose : in integer32; 
+                gamma : in Standard_Complex_Numbers.Complex_Number );
+  procedure QuadDobl_ADE_Track_One
+              ( verbose : in integer32; 
+                gamma : in Standard_Complex_Numbers.Complex_Number );
+
+  -- DESCRIPTION :
+  --   With the homotopy as defined by the containers, tracks one path
+  --   in standard double, double double, or quad double precision.
+
+  -- ON ENTRY :
+  --   verbose  >0 if intermediate output needs to be written to screen;
+  --   gamma    the gamma constant in the homotopy.
+
+  -- REQUIRED :
+  --   The containers have been initialized with a target system,
+  --   a start system, and at least one start solution.
+
+  procedure Standard_ADE_Track_Many
+              ( verbose : in integer32;
+                gamma : in Standard_Complex_Numbers.Complex_Number );
+  procedure DoblDobl_ADE_Track_Many
+              ( verbose : in integer32;
+                gamma : in Standard_Complex_Numbers.Complex_Number );
+  procedure QuadDobl_ADE_Track_Many
+              ( verbose : in integer32;
+                gamma : in Standard_Complex_Numbers.Complex_Number );
+
+  -- DESCRIPTION :
+  --   Wraps the C interface function to track many paths.
+  --   With the homotopy as defined by the containers, tracks paths
+  --   in standard double, double double, or quad double precision.
+
+  -- ON ENTRY :
+  --   verbose  >0 if intermediate output needs to be written to screen;
+  --   gamma    the gamma constant in the homotopy.
+
+  -- REQUIRED :
+  --   The containers have been initialized with a target system,
+  --   a start system, and a list of start solutions.
+
+-- INTERACTIVE DRIVERS :
+
+  procedure Standard_Newton;
+  procedure DoblDobl_Newton;
+  procedure QuadDobl_Newton;
+
+  -- DESCRIPTION :
+  --   This interactive procedure prompts the user for a system,
+  --   and a corresponding list of solutions.
+  --   Then the wrapper to the C interface Newton method is executed,
+  --   in standard double, double double, or quad double precision.
+
+  procedure Standard_Track_one_Path;
+  procedure DoblDobl_Track_one_Path;
+  procedure QuadDobl_Track_one_Path;
+
+  -- DESCRIPTION :
+  --   This interactive procedure prompts the user for a target system,
+  --   start system, and a corresponding start solution.
+  --   Then the wrapper to the C interface path tracker is executed,
+  --   in standard double, double double, or quad double precision.
+  --   Even if the user would give a whole list of start solutions,
+  --   only the path starting at the first solution is tracked.
+
+  procedure Standard_Track_many_Paths;
+  procedure DoblDobl_Track_many_Paths;
+  procedure QuadDobl_Track_many_Paths;
+
+  -- DESCRIPTION :
+  --   This interactive procedure prompts the user for a target system,
+  --   start system, and a corresponding list of start solutions.
+  --   Then the wrapper to the C interface path tracker is executed,
+  --   in standard double, double double, or quad double precision.
+  --   As many paths are tracked as there are start solutions.
+
+end Algorithmic_DiffEval_Trackers;
