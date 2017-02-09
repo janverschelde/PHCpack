875332718d77801289b49e2a44fc59c661cfc322
Jan Verschelde <janv@uic.edu>
2017-02-08 20:57:31 -0600

    added a tune function to set specific values of the parameters

--- src/GPU/Path/Host/test_parameter.cpp
+++ src/GPU/Path/Host/test_parameter.cpp
@@ -7,6 +7,13 @@ using namespace std;
 void write ( Parameter &pars );
 // Writes the values in pars to screen.
 
+void set_value ( Parameter &pars, int idx, double val );
+// Sets the value of the parameter with idx to the value val.
+// Required: idx is in the range from 1 to 14.
+
+void tune ( Parameter &pars );
+// Interactive tuning of the parameters in pars.
+
 int main ( void )
 {
    int precision;
@@ -16,9 +23,29 @@ int main ( void )
 
    Parameter pars(precision);
 
-   write(pars);
+   tune(pars);
 
    return 0;
+}
+
+void tune ( Parameter &pars )
+{
+   int idx = 1;
+
+   while(idx > 0)
+   {
+      write(pars);
+
+      cout << "Type integer in 1..14 to change, 0 to exit : ";
+      cin >> idx;
+
+      if(idx > 0 && idx < 14)
+      {
+         cout << "Give a value for parameter " << idx << " : ";
+         double val; cin >> val;
+         set_value(pars,idx,val);
+      }
+   }
 }
 
 void write ( Parameter &pars )
@@ -51,4 +78,36 @@ void write ( Parameter &pars )
         << pars.max_it_refine << endl;
    cout << "14. Tolerance for Newton's refinement method  : "
         << pars.err_min_round_off_refine << endl;
+}
+
+void set_value ( Parameter &pars, int idx, double val )
+{
+   if(idx == 1)
+       pars.max_step = (int) val;
+   else if(idx == 2)
+       pars.n_predictor = (int) val;
+   else if(idx == 3)
+       pars.step_increase = val;
+   else if(idx == 4)
+       pars.step_decrease = val;
+   else if(idx == 5)
+       pars.max_delta_t = val;
+   else if(idx == 6)
+       pars.max_delta_t_end = val;
+   else if(idx == 7)
+       pars.min_delta_t = val;
+   else if(idx == 8)
+       pars.err_max_res = val;
+   else if(idx == 9)
+       pars.err_max_delta_x = val;
+   else if(idx == 10)
+       pars.err_max_first_delta_x = val;
+   else if(idx == 11)
+       pars.max_it = (int) val;
+   else if(idx == 12)
+       pars.err_min_round_off = val;
+   else if(idx == 13)
+       pars.max_it_refine = (int) val;
+   else if(idx == 14)
+       pars.err_min_round_off_refine = val;
 }
