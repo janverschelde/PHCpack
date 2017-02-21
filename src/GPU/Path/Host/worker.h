d0fb8886fc6bb934698c54d1b4a05c3bef320126
Jan Verschelde <janv@uic.edu>
2017-02-21 15:10:15 -0600

    basic encapsulation of pthreads as a C++ class

---
+++ src/GPU/Path/Host/worker.h
@@ -0,0 +1,59 @@
+/* The class Worker encapsulates a pthread for shared memory multithreaded
+   execution of any given number of functions. */
+
+#ifndef _WORKER_H_
+#define _WORKER_H_
+
+#include <cstdlib>
+#include <iostream>
+#include <pthread.h>
+
+class Worker
+{
+   pthread_t thread;     // the thread of execution
+
+   public:
+
+      int idn;           // identification number
+
+      Worker ( int nbr ) // initialize the identification number
+      {
+         this->idn = nbr;
+      }
+
+      void write ( void ) // writes the identification number
+      {
+         std::cout << "Hello from thread " << this->idn
+                   << "." << std::endl;
+      }
+
+      int work ( void* do_job ( void* args ), void* args, int verbose = 1 )
+      // launches the thread
+      {
+         if(verbose > 0)
+             std::cout << "worker " << this->idn
+                       << " starts to work ..." << std::endl;
+
+         pthread_create(&(this->thread),NULL,do_job,args);
+
+         if(verbose > 0)
+             std:: cout << "worker " << this->idn
+                        << " returns from thread creation" << std::endl;
+
+         return 0;
+      }
+
+      int join ( void ) // wait for the thread to finish
+      {
+         pthread_join(this->thread,NULL);
+
+         return 0;
+      }
+
+      ~Worker ( void ) // destroys the thread
+      {
+         pthread_exit(NULL);
+      }
+};
+
+#endif /* _WORKER_H_ */
