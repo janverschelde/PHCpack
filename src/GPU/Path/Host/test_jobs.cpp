9109ae038d749116ec0e997751d0fd5a1098e372
Jan Verschelde <janv@uic.edu>
2017-02-21 19:31:21 -0600

    added test on work crew processing a job queue added

--- src/GPU/Path/Host/test_jobs.cpp
+++ src/GPU/Path/Host/test_jobs.cpp
@@ -1,12 +1,18 @@
 // Test on the operations in the Job Queue
 
+#include <unistd.h>
 #include "jobqueue.h"
+#include "worker.h"
 
 using namespace std;
 
 int process_jobs ( int nbt, JobQueue* jobs );
 /*
  * Processing the jobs with nbt threads. */
+
+void* do_job ( void* args );
+/*
+ * Processes the jobs in the job queue, given in args. */
 
 int main ( void )
 {
@@ -40,5 +46,34 @@ int main ( void )
 
 int process_jobs ( int nbt, JobQueue* jobs )
 {
+   Worker *crew = (Worker*)calloc(nbt,sizeof(Worker));
+
+   for(int i=0; i<nbt; i++) crew[i].idn = i;
+
+   for(int i=0; i<nbt; i++) crew[i].write();
+
+   for(int i=0; i<nbt; i++) crew[i].work(&do_job,(void*)jobs);
+
+   for(int i=0; i<nbt; i++) crew[i].join();
+
    return 0;
+}
+
+void* do_job ( void* args )
+{
+   JobQueue *jobs = (JobQueue*) args;
+
+   while(1)
+   {
+      int nextjob = jobs->get_next_job();
+
+      if(nextjob == -1) break;
+
+      cout << "Index of the next job : " << nextjob
+           << " with work " << jobs->work[nextjob] << endl;
+
+      sleep(jobs->work[nextjob]);
+   }
+
+   return NULL;
 }
