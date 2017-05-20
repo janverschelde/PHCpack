4013ec0228baae9f7e01c659ca4f18ee5d0bb3e2
Jan Verschelde <janv@uic.edu>
2017-05-20 13:03:26 -0500

    added definition of Pade approximations in double double precision

--- src/Ada/Deformations/Curves/ts_serpade.adb
+++ src/Ada/Deformations/Curves/ts_serpade.adb
@@ -4,12 +4,20 @@ with Standard_Integer_Numbers;          use Standard_Integer_Numbers;
 with Standard_Integer_Numbers_io;       use Standard_Integer_Numbers_io;
 with Standard_Floating_Numbers;         use Standard_Floating_Numbers;
 with Standard_Floating_Numbers_io;      use Standard_Floating_Numbers_io;
-with Standard_Complex_Numbers;          use Standard_Complex_Numbers;
+with Standard_Complex_Numbers;
 with Standard_Complex_Numbers_io;       use Standard_Complex_Numbers_io;
 with Standard_Mathematical_Functions;
+with Double_Double_Numbers;             use Double_Double_Numbers;
+with Double_Double_Numbers_io;          use Double_Double_Numbers_io;
+with DoblDobl_Complex_Numbers;
+with DoblDobl_Complex_Numbers_io;       use DoblDobl_Complex_Numbers_io;
+with DoblDobl_Mathematical_Functions;
 with Standard_Complex_Vectors;
 with Standard_Complex_Vectors_io;       use Standard_Complex_Vectors_io;
+with DoblDobl_Complex_Vectors;
+with DoblDobl_Complex_Vectors_io;       use DoblDobl_Complex_Vectors_io;
 with Standard_Rational_Approximations;
+with DoblDobl_Rational_Approximations;
 
 procedure ts_serpade is
 
@@ -17,12 +25,15 @@ procedure ts_serpade is
 --   Interactive development of the rational approximation of a function,
 --   given the power series at the origin.
 
-  function log1plusx ( dim : integer32 )
-                     return Standard_Complex_Vectors.Vector is
+  function standard_log_series
+             ( dim : integer32 ) return Standard_Complex_Vectors.Vector is
 
   -- DESCRIPTION :
   --   Returns the first dim+1 coefficients of the series of log(1+x)
-  --   as a vector of range 0..dim.
+  --   as a vector of range 0..dim as a vector of complex numbers in
+  --   standard double precision.
+
+    use Standard_Complex_Numbers;
 
     res : Standard_Complex_Vectors.Vector(0..dim);
 
@@ -35,13 +46,45 @@ procedure ts_serpade is
       end if;
     end loop;
     return res;
-  end log1plusx;
+  end standard_log_series;
 
-  function invfactorial ( n : integer32 ) return Complex_Number is
+  function dobldobl_log_series
+             ( dim : integer32 ) return DoblDobl_Complex_Vectors.Vector is
+
+  -- DESCRIPTION :
+  --   Returns the first dim+1 coefficients of the series of log(1+x)
+  --   as a vector of range 0..dim as a vector of complex numbers in
+  --   double double precision.
+
+    use DoblDobl_Complex_Numbers;
+
+    res : DoblDobl_Complex_Vectors.Vector(0..dim);
+    zero : constant double_double := create(0.0);
+    one : constant double_double := create(1.0);
+    val : double_double;
+
+  begin
+    res(0) := Create(zero);
+    for k in 1..dim loop
+      val := create(k);
+      val := one/val;
+      res(k) := Create(val);
+      if k mod 2 = 0
+       then Min(res(k));
+      end if;
+    end loop;
+    return res;
+  end dobldobl_log_series;
+
+  function standard_invfactorial
+             ( n : integer32 )
+             return Standard_Complex_Numbers.Complex_Number is
 
   -- DESCRIPTION :
   --   Returns 1/n! where n! is the factorial,
-  --   stored as a complex number.
+  --   stored as a complex number in standard double precision.
+
+    use Standard_Complex_Numbers;
 
     res : Complex_Number;
     fac : integer32 := 1;
@@ -54,31 +97,78 @@ procedure ts_serpade is
     invfac := 1.0/double_float(fac);
     res := Create(invfac);
     return res;
-  end invfactorial;
+  end standard_invfactorial;
 
-  function expseries ( dim : integer32 )
-                     return Standard_Complex_Vectors.Vector is
+  function dobldobl_invfactorial
+             ( n : integer32 )
+             return DoblDobl_Complex_Numbers.Complex_Number is
+
+  -- DESCRIPTION :
+  --   Returns 1/n! where n! is the factorial,
+  --   stored as a complex number in double double precision.
+
+    use DoblDobl_Complex_Numbers;
+
+    res : Complex_Number;
+    fac : integer32 := 1;
+    dd_fac : double_double;
+    invfac : double_double;
+
+  begin
+    for k in 2..n loop
+      fac := fac*k;
+    end loop;
+    dd_fac := create(fac);
+    invfac := 1.0/dd_fac;
+    res := Create(invfac);
+    return res;
+  end dobldobl_invfactorial;
+
+  function standard_exp_series
+             ( dim : integer32 ) return Standard_Complex_Vectors.Vector is
 
   -- DESCRIPTION :
   --   Returns a vector of range 0..dim with the coefficients
-  --   of the series expansion of exp(x) at x = 0.
+  --   of the series expansion of exp(x) at x = 0, as a vector
+  --   of complex numbers in standard double precision.
 
     res : Standard_Complex_Vectors.Vector(0..dim);
     plus : boolean := true;
 
   begin
     for k in res'range loop
-      res(k) := invfactorial(k);
+      res(k) := standard_invfactorial(k);
     end loop;
     return res;
-  end expseries;
+  end standard_exp_series;
 
-  function sinseries ( dim : integer32 )
-                     return Standard_Complex_Vectors.Vector is
+  function dobldobl_exp_series
+             ( dim : integer32 ) return DoblDobl_Complex_Vectors.Vector is
 
   -- DESCRIPTION :
   --   Returns a vector of range 0..dim with the coefficients
-  --   of the series expansion of sin(x) at x = 0.
+  --   of the series expansion of exp(x) at x = 0, as a vector
+  --   of complex numbers in double double precision.
+
+    res : DoblDobl_Complex_Vectors.Vector(0..dim);
+    plus : boolean := true;
+
+  begin
+    for k in res'range loop
+      res(k) := dobldobl_invfactorial(k);
+    end loop;
+    return res;
+  end dobldobl_exp_series;
+
+  function standard_sin_series
+             ( dim : integer32 ) return Standard_Complex_Vectors.Vector is
+
+  -- DESCRIPTION :
+  --   Returns a vector of range 0..dim with the coefficients
+  --   of the series expansion of sin(x) at x = 0, as a vector
+  --   of complex numbers in standard double precision.
+
+    use Standard_Complex_Numbers;
 
     res : Standard_Complex_Vectors.Vector(0..dim);
     plus : boolean := true;
@@ -88,7 +178,7 @@ procedure ts_serpade is
       if k mod 2 = 0 then
         res(k) := Create(0.0);
       else
-        res(k) := invfactorial(k);
+        res(k) := standard_invfactorial(k);
         if not plus
          then Min(res(k)); 
         end if;
@@ -96,14 +186,46 @@ procedure ts_serpade is
       end if;
     end loop;
     return res;
-  end sinseries;
+  end standard_sin_series;
 
-  function cosseries ( dim : integer32 )
-                     return Standard_Complex_Vectors.Vector is
+  function dobldobl_sin_series
+             ( dim : integer32 ) return DoblDobl_Complex_Vectors.Vector is
 
   -- DESCRIPTION :
   --   Returns a vector of range 0..dim with the coefficients
-  --   of the series expansion of cos(x) at x = 0.
+  --   of the series expansion of sin(x) at x = 0, as a vector
+  --   of complex numbers in double double precision.
+
+    use DoblDobl_Complex_Numbers;
+
+    res : DoblDobl_Complex_Vectors.Vector(0..dim);
+    zero : constant double_double := create(0.0);
+    plus : boolean := true;
+
+  begin
+    for k in res'range loop
+      if k mod 2 = 0 then
+        res(k) := Create(zero);
+      else
+        res(k) := dobldobl_invfactorial(k);
+        if not plus
+         then Min(res(k)); 
+        end if;
+        plus := not plus;
+      end if;
+    end loop;
+    return res;
+  end dobldobl_sin_series;
+
+  function standard_cos_series
+             ( dim : integer32 ) return Standard_Complex_Vectors.Vector is
+
+  -- DESCRIPTION :
+  --   Returns a vector of range 0..dim with the coefficients
+  --   of the series expansion of cos(x) at x = 0, as a vector
+  --   of complex numbers in standard double precision.
+
+    use Standard_Complex_Numbers;
 
     res : Standard_Complex_Vectors.Vector(0..dim);
     plus : boolean := true;
@@ -113,7 +235,7 @@ procedure ts_serpade is
       if k mod 2 = 1 then
         res(k) := Create(0.0);
       else
-        res(k) := invfactorial(k);
+        res(k) := standard_invfactorial(k);
         if not plus
          then Min(res(k)); 
         end if;
@@ -121,17 +243,48 @@ procedure ts_serpade is
       end if;
     end loop;
     return res;
-  end cosseries;
+  end standard_cos_series;
 
-  procedure Standard_Logarithm_Test ( numdeg,dendeg : in integer32 ) is
+  function dobldobl_cos_series
+             ( dim : integer32 ) return DoblDobl_Complex_Vectors.Vector is
+
+  -- DESCRIPTION :
+  --   Returns a vector of range 0..dim with the coefficients
+  --   of the series expansion of cos(x) at x = 0, as a vector
+  --   of complex numbers in double double precision.
+
+    use DoblDobl_Complex_Numbers;
+
+    res : DoblDobl_Complex_Vectors.Vector(0..dim);
+    zero : constant double_double := create(0.0);
+    plus : boolean := true;
+
+  begin
+    for k in res'range loop
+      if k mod 2 = 1 then
+        res(k) := Create(zero);
+      else
+        res(k) := dobldobl_invfactorial(k);
+        if not plus
+         then Min(res(k)); 
+        end if;
+        plus := not plus;
+      end if;
+    end loop;
+    return res;
+  end dobldobl_cos_series;
+
+  procedure Standard_log_Test ( numdeg,dendeg : in integer32 ) is
 
   -- DESCRIPTION :
   --   Tests the construction in standard floating point arithmetic
   --   on the natural logarithm of 1 + x.
 
+    use Standard_Complex_Numbers;
+
     dim : constant integer32 := numdeg + dendeg;
     cff : constant Standard_Complex_Vectors.Vector(0..dim)
-        := log1plusx(dim);
+        := standard_log_series(dim);
     num : Standard_Complex_Vectors.Vector(0..numdeg);
     den : Standard_Complex_Vectors.Vector(0..dendeg);
     pnt : constant Complex_Number := Create(0.1);
@@ -147,17 +300,48 @@ procedure ts_serpade is
     eva := Standard_Rational_Approximations.Evaluate(num,den,pnt);
     put("The value at 1.1      :"); put(eva); new_line;
     put("The value of log(1.1) :"); put(chkpnt); new_line;
-  end Standard_Logarithm_Test;
+  end Standard_log_Test;
 
-  procedure Standard_Sin_Test ( numdeg,dendeg : in integer32 ) is
+  procedure DoblDobl_log_Test ( numdeg,dendeg : in integer32 ) is
+
+  -- DESCRIPTION :
+  --   Tests the construction in double double arithmetic
+  --   on the natural logarithm of 1 + x.
+
+    use DoblDobl_Complex_Numbers;
+
+    dim : constant integer32 := numdeg + dendeg;
+    cff : constant DoblDobl_Complex_Vectors.Vector(0..dim)
+        := DoblDobl_log_series(dim);
+    num : DoblDobl_Complex_Vectors.Vector(0..numdeg);
+    den : DoblDobl_Complex_Vectors.Vector(0..dendeg);
+    nbr : constant double_double := create(0.1);
+    pnt : constant Complex_Number := Create(nbr);
+    eva : Complex_Number;
+    chkpnt : constant double_float
+           := Standard_Mathematical_Functions.LN(1.1);
+
+  begin
+    put_line("The coefficient vector of the series :"); put_line(cff);
+    DoblDobl_Rational_Approximations.Pade(numdeg,dendeg,cff,num,den);
+    put_line("The coefficients of the numerator :"); put_line(num);
+    put_line("The coefficients of the denominator :"); put_line(den);
+    eva := DoblDobl_Rational_Approximations.Evaluate(num,den,pnt);
+    put("The value at 1.1      :"); put(eva); new_line;
+    put("The value of log(1.1) :"); put(chkpnt); new_line;
+  end DoblDobl_log_Test;
+
+  procedure Standard_sin_Test ( numdeg,dendeg : in integer32 ) is
 
   -- DESCRIPTION :
   --   Tests the construction in standard floating point arithmetic
   --   on the series of sin(x) at x = 0.
 
+    use Standard_Complex_Numbers;
+
     dim : constant integer32 := numdeg + dendeg;
     cff : constant Standard_Complex_Vectors.Vector(0..dim)
-        := sinseries(dim);
+        := standard_sin_series(dim);
     num : Standard_Complex_Vectors.Vector(0..numdeg);
     den : Standard_Complex_Vectors.Vector(0..dendeg);
     pnt : constant Complex_Number := Create(0.1);
@@ -175,15 +359,46 @@ procedure ts_serpade is
     put("The value of sin(0.1) :"); put(chkpnt); new_line;
   end Standard_Sin_Test;
 
-  procedure Standard_Exp_Test ( numdeg,dendeg : in integer32 ) is
+  procedure DoblDobl_sin_Test ( numdeg,dendeg : in integer32 ) is
+
+  -- DESCRIPTION :
+  --   Tests the construction in double double arithmetic
+  --   on the series of sin(x) at x = 0.
+
+    use DoblDobl_Complex_Numbers;
+
+    dim : constant integer32 := numdeg + dendeg;
+    cff : constant DoblDobl_Complex_Vectors.Vector(0..dim)
+        := dobldobl_sin_series(dim);
+    num : DoblDobl_Complex_Vectors.Vector(0..numdeg);
+    den : DoblDobl_Complex_Vectors.Vector(0..dendeg);
+    arg : constant double_double := create(0.1);
+    pnt : constant Complex_Number := Create(arg);
+    eva : Complex_Number;
+    chkpnt : constant double_double
+           := DoblDobl_Mathematical_Functions.SIN(arg);
+
+  begin
+    put_line("The coefficient vector of the series :"); put_line(cff);
+    DoblDobl_Rational_Approximations.Pade(numdeg,dendeg,cff,num,den);
+    put_line("The coefficients of the numerator :"); put_line(num);
+    put_line("The coefficients of the denominator :"); put_line(den);
+    eva := DoblDobl_Rational_Approximations.Evaluate(num,den,pnt);
+    put("The value at 0.1      :"); put(eva); new_line;
+    put("The value of sin(0.1) :"); put(chkpnt); new_line;
+  end DoblDobl_Sin_Test;
+
+  procedure Standard_exp_Test ( numdeg,dendeg : in integer32 ) is
 
   -- DESCRIPTION :
   --   Tests the construction in standard floating point arithmetic
   --   on the series of exp(x) at x = 0.
 
+    use Standard_Complex_Numbers;
+
     dim : constant integer32 := numdeg + dendeg;
     cff : constant Standard_Complex_Vectors.Vector(0..dim)
-        := expseries(dim);
+        := standard_exp_series(dim);
     num : Standard_Complex_Vectors.Vector(0..numdeg);
     den : Standard_Complex_Vectors.Vector(0..dendeg);
     pnt : constant Complex_Number := Create(0.1);
@@ -199,17 +414,48 @@ procedure ts_serpade is
     eva := Standard_Rational_Approximations.Evaluate(num,den,pnt);
     put("The value at 0.1      :"); put(eva); new_line;
     put("The value of sin(0.1) :"); put(chkpnt); new_line;
-  end Standard_Exp_Test;
+  end Standard_exp_Test;
 
-  procedure Standard_Cos_Test ( numdeg,dendeg : in integer32 ) is
+  procedure DoblDobl_exp_Test ( numdeg,dendeg : in integer32 ) is
+
+  -- DESCRIPTION :
+  --   Tests the construction in double double arithmetic
+  --   on the series of exp(x) at x = 0.
+
+    use DoblDobl_Complex_Numbers;
+
+    dim : constant integer32 := numdeg + dendeg;
+    cff : constant DoblDobl_Complex_Vectors.Vector(0..dim)
+        := dobldobl_exp_series(dim);
+    num : DoblDobl_Complex_Vectors.Vector(0..numdeg);
+    den : DoblDobl_Complex_Vectors.Vector(0..dendeg);
+    arg : constant double_double := create(0.1);
+    pnt : constant Complex_Number := Create(arg);
+    eva : Complex_Number;
+    chkpnt : constant double_float
+           := Standard_Mathematical_Functions.EXP(0.1);
+
+  begin
+    put_line("The coefficient vector of the series :"); put_line(cff);
+    DoblDobl_Rational_Approximations.Pade(numdeg,dendeg,cff,num,den);
+    put_line("The coefficients of the numerator :"); put_line(num);
+    put_line("The coefficients of the denominator :"); put_line(den);
+    eva := DoblDobl_Rational_Approximations.Evaluate(num,den,pnt);
+    put("The value at 0.1      :"); put(eva); new_line;
+    put("The value of sin(0.1) :"); put(chkpnt); new_line;
+  end DoblDobl_exp_Test;
+
+  procedure Standard_cos_Test ( numdeg,dendeg : in integer32 ) is
 
   -- DESCRIPTION :
   --   Tests the construction in standard floating point arithmetic
   --   on the series of cos(x) at x = 0.
 
+    use Standard_Complex_Numbers;
+
     dim : constant integer32 := numdeg + dendeg;
     cff : constant Standard_Complex_Vectors.Vector(0..dim)
-        := cosseries(dim);
+        := standard_cos_series(dim);
     num : Standard_Complex_Vectors.Vector(0..numdeg);
     den : Standard_Complex_Vectors.Vector(0..dendeg);
     pnt : constant Complex_Number := Create(0.1);
@@ -225,7 +471,36 @@ procedure ts_serpade is
     eva := Standard_Rational_Approximations.Evaluate(num,den,pnt);
     put("The value at 0.1      :"); put(eva); new_line;
     put("The value of cos(0.1) :"); put(chkpnt); new_line;
-  end Standard_Cos_Test;
+  end Standard_cos_Test;
+
+  procedure DoblDobl_cos_Test ( numdeg,dendeg : in integer32 ) is
+
+  -- DESCRIPTION :
+  --   Tests the construction in double double arithmetic
+  --   on the series of cos(x) at x = 0.
+
+    use DoblDobl_Complex_Numbers;
+
+    dim : constant integer32 := numdeg + dendeg;
+    cff : constant DoblDobl_Complex_Vectors.Vector(0..dim)
+        := dobldobl_cos_series(dim);
+    num : DoblDobl_Complex_Vectors.Vector(0..numdeg);
+    den : DoblDobl_Complex_Vectors.Vector(0..dendeg);
+    arg : constant double_double := create(0.1);
+    pnt : constant Complex_Number := Create(arg);
+    eva : Complex_Number;
+    chkpnt : constant double_double
+           := DoblDobl_Mathematical_Functions.COS(arg);
+
+  begin
+    put_line("The coefficient vector of the series :"); put_line(cff);
+    DoblDobl_Rational_Approximations.Pade(numdeg,dendeg,cff,num,den);
+    put_line("The coefficients of the numerator :"); put_line(num);
+    put_line("The coefficients of the denominator :"); put_line(den);
+    eva := DoblDobl_Rational_Approximations.Evaluate(num,den,pnt);
+    put("The value at 0.1      :"); put(eva); new_line;
+    put("The value of cos(0.1) :"); put(chkpnt); new_line;
+  end DoblDobl_cos_Test;
 
   procedure Main is
 
@@ -236,7 +511,7 @@ procedure ts_serpade is
     degnum : integer32 := 0; -- degree numerator
     degden : integer32 := 0; -- degree denominator
     dim : integer32 := 0;    -- degnum + degden
-    ans : character;
+    ans,prc : character;
 
   begin
     new_line;
@@ -252,11 +527,29 @@ procedure ts_serpade is
     put("Give the degree of the denominator : "); get(degden);
     dim := degnum + degden;
     put("The dimension : "); put(dim,1); new_line;
-    case ans is 
-      when '1' => Standard_Logarithm_Test(degnum,degden);
-      when '2' => Standard_Exp_Test(degnum,degden);
-      when '3' => Standard_Sin_Test(degnum,degden);
-      when '4' => Standard_Cos_Test(degnum,degden);
+    new_line;
+    put_line("MENU for the precision :");
+    put_line("  0. standard double precision");
+    put_line("  1. double double precision");
+    put("Type 0 or 1 to select the precision : ");
+    Ask_Alternative(prc,"01");
+    case prc is
+      when '0' =>
+        case ans is 
+          when '1' => Standard_log_Test(degnum,degden);
+          when '2' => Standard_exp_Test(degnum,degden);
+          when '3' => Standard_sin_Test(degnum,degden);
+          when '4' => Standard_cos_Test(degnum,degden);
+          when others => null;
+        end case;
+      when '1' =>
+        case ans is 
+          when '1' => DoblDobl_log_Test(degnum,degden);
+          when '2' => DoblDobl_exp_Test(degnum,degden);
+          when '3' => DoblDobl_sin_Test(degnum,degden);
+          when '4' => DoblDobl_cos_Test(degnum,degden);
+          when others => null;
+        end case;
       when others => null;
     end case;
   end Main;
