87a0f70b37650f9d6ebb5310643e64f9b2490da3
Jan Verschelde <janv@uic.edu>
2017-05-19 21:49:50 -0500

    added more test cases: exp(x), sin(x), and cos(x) for the rational approximations

--- src/Ada/Deformations/Curves/ts_serpade.adb
+++ src/Ada/Deformations/Curves/ts_serpade.adb
@@ -1,4 +1,5 @@
 with text_io;                           use text_io;
+with Communications_with_User;          use Communications_with_User;
 with Standard_Integer_Numbers;          use Standard_Integer_Numbers;
 with Standard_Integer_Numbers_io;       use Standard_Integer_Numbers_io;
 with Standard_Floating_Numbers;         use Standard_Floating_Numbers;
@@ -37,6 +38,92 @@ procedure ts_serpade is
     end loop;
     return res;
   end log1plusx;
+
+  function invfactorial ( n : integer32 ) return Complex_Number is
+
+  -- DESCRIPTION :
+  --   Returns 1/n! where n! is the factorial,
+  --   stored as a complex number.
+
+    res : Complex_Number;
+    fac : integer32 := 1;
+    invfac : double_float;
+
+  begin
+    for k in 2..n loop
+      fac := fac*k;
+    end loop;
+    invfac := 1.0/double_float(fac);
+    res := Create(invfac);
+    return res;
+  end invfactorial;
+
+  function expseries ( dim : integer32 )
+                     return Standard_Complex_Vectors.Vector is
+
+  -- DESCRIPTION :
+  --   Returns a vector of range 0..dim with the coefficients
+  --   of the series expansion of exp(x) at x = 0.
+
+    res : Standard_Complex_Vectors.Vector(0..dim);
+    plus : boolean := true;
+
+  begin
+    for k in res'range loop
+      res(k) := invfactorial(k);
+    end loop;
+    return res;
+  end expseries;
+
+  function sinseries ( dim : integer32 )
+                     return Standard_Complex_Vectors.Vector is
+
+  -- DESCRIPTION :
+  --   Returns a vector of range 0..dim with the coefficients
+  --   of the series expansion of sin(x) at x = 0.
+
+    res : Standard_Complex_Vectors.Vector(0..dim);
+    plus : boolean := true;
+
+  begin
+    for k in res'range loop
+      if k mod 2 = 0 then
+        res(k) := Create(0.0);
+      else
+        res(k) := invfactorial(k);
+        if not plus
+         then Min(res(k)); 
+        end if;
+        plus := not plus;
+      end if;
+    end loop;
+    return res;
+  end sinseries;
+
+  function cosseries ( dim : integer32 )
+                     return Standard_Complex_Vectors.Vector is
+
+  -- DESCRIPTION :
+  --   Returns a vector of range 0..dim with the coefficients
+  --   of the series expansion of cos(x) at x = 0.
+
+    res : Standard_Complex_Vectors.Vector(0..dim);
+    plus : boolean := true;
+
+  begin
+    for k in res'range loop
+      if k mod 2 = 1 then
+        res(k) := Create(0.0);
+      else
+        res(k) := invfactorial(k);
+        if not plus
+         then Min(res(k)); 
+        end if;
+        plus := not plus;
+      end if;
+    end loop;
+    return res;
+  end cosseries;
 
   procedure Standard_Denominator_System
               ( numdeg,dendeg : in integer32; 
@@ -147,7 +234,8 @@ procedure ts_serpade is
 
   procedure Standard_Test
               ( numdeg,dendeg : in integer32;
-                cff : in Standard_Complex_Vectors.Vector ) is
+                cff : in Standard_Complex_Vectors.Vector;
+                numcff,dencff : out Standard_Complex_Vectors.Vector ) is
 
   -- DESCRIPTION :
   --   Tests the construction in standard floating point arithmetic
@@ -159,6 +247,10 @@ procedure ts_serpade is
   --   dendeg   degree of the denominator;
   --   cff      coefficients of the power series.
 
+  -- ON RETURN :
+  --   numcff   coefficients of the numerator;
+  --   dencff   coefficients of the denominator.
+
   -- REQUIRED : cff'range = 0..numdeg+dendeg.
 
     dim : constant integer32 := numdeg + dendeg;
@@ -166,14 +258,9 @@ procedure ts_serpade is
     rhs : Standard_Complex_Vectors.Vector(1..dendeg);
     ipvt : Standard_Integer_Vectors.Vector(1..dendeg);
     info : integer32;
-    dencff : Standard_Complex_Vectors.Vector(0..dendeg);
-    numcff : Standard_Complex_Vectors.Vector(0..numdeg);
-    pnt,eva : Complex_Number;
-    chkpnt : constant double_float
-           := Standard_Mathematical_Functions.LN(1.1);
 
   begin
-    put_line("The coefficient vector for log(1+x) :"); put_line(cff);
+    put_line("The coefficient vector of the series :"); put_line(cff);
     Standard_Denominator_System(numdeg,dendeg,cff,mat,rhs);
     lufac(mat,dendeg,ipvt,info);
     lusolve(mat,dendeg,ipvt,rhs);
@@ -184,10 +271,6 @@ procedure ts_serpade is
     put_line("The coefficients of the denominator :"); put_line(dencff);
     numcff := Standard_Numerator_Coefficients(numdeg,dendeg,dencff,cff);
     put_line("The coefficients of the numerator :"); put_line(numcff);
-    pnt := Create(0.1);
-    eva := Standard_Evaluate(numcff,dencff,pnt);
-    put("The value at 1.1      :"); put(eva); new_line;
-    put("The value of log(1.1) :"); put(chkpnt); new_line;
   end Standard_Test;
 
   procedure Standard_Logarithm_Test ( numdeg,dendeg : in integer32 ) is
@@ -199,10 +282,88 @@ procedure ts_serpade is
     dim : constant integer32 := numdeg + dendeg;
     cff : constant Standard_Complex_Vectors.Vector(0..dim)
         := log1plusx(dim);
+    num : Standard_Complex_Vectors.Vector(0..numdeg);
+    den : Standard_Complex_Vectors.Vector(0..dendeg);
+    pnt : constant Complex_Number := Create(0.1);
+    eva : Complex_Number;
+    chkpnt : constant double_float
+           := Standard_Mathematical_Functions.LN(1.1);
 
   begin
-    Standard_Test(numdeg,dendeg,cff);
+    Standard_Test(numdeg,dendeg,cff,num,den);
+    eva := Standard_Evaluate(num,den,pnt);
+    put("The value at 1.1      :"); put(eva); new_line;
+    put("The value of log(1.1) :"); put(chkpnt); new_line;
   end Standard_Logarithm_Test;
+
+  procedure Standard_Sin_Test ( numdeg,dendeg : in integer32 ) is
+
+  -- DESCRIPTION :
+  --   Tests the construction in standard floating point arithmetic
+  --   on the series of sin(x) at x = 0.
+
+    dim : constant integer32 := numdeg + dendeg;
+    cff : constant Standard_Complex_Vectors.Vector(0..dim)
+        := sinseries(dim);
+    num : Standard_Complex_Vectors.Vector(0..numdeg);
+    den : Standard_Complex_Vectors.Vector(0..dendeg);
+    pnt : constant Complex_Number := Create(0.1);
+    eva : Complex_Number;
+    chkpnt : constant double_float
+           := Standard_Mathematical_Functions.SIN(0.1);
+
+  begin
+    Standard_Test(numdeg,dendeg,cff,num,den);
+    eva := Standard_Evaluate(num,den,pnt);
+    put("The value at 0.1      :"); put(eva); new_line;
+    put("The value of sin(0.1) :"); put(chkpnt); new_line;
+  end Standard_Sin_Test;
+
+  procedure Standard_Exp_Test ( numdeg,dendeg : in integer32 ) is
+
+  -- DESCRIPTION :
+  --   Tests the construction in standard floating point arithmetic
+  --   on the series of exp(x) at x = 0.
+
+    dim : constant integer32 := numdeg + dendeg;
+    cff : constant Standard_Complex_Vectors.Vector(0..dim)
+        := expseries(dim);
+    num : Standard_Complex_Vectors.Vector(0..numdeg);
+    den : Standard_Complex_Vectors.Vector(0..dendeg);
+    pnt : constant Complex_Number := Create(0.1);
+    eva : Complex_Number;
+    chkpnt : constant double_float
+           := Standard_Mathematical_Functions.EXP(0.1);
+
+  begin
+    Standard_Test(numdeg,dendeg,cff,num,den);
+    eva := Standard_Evaluate(num,den,pnt);
+    put("The value at 0.1      :"); put(eva); new_line;
+    put("The value of sin(0.1) :"); put(chkpnt); new_line;
+  end Standard_Exp_Test;
+
+  procedure Standard_Cos_Test ( numdeg,dendeg : in integer32 ) is
+
+  -- DESCRIPTION :
+  --   Tests the construction in standard floating point arithmetic
+  --   on the series of cos(x) at x = 0.
+
+    dim : constant integer32 := numdeg + dendeg;
+    cff : constant Standard_Complex_Vectors.Vector(0..dim)
+        := cosseries(dim);
+    num : Standard_Complex_Vectors.Vector(0..numdeg);
+    den : Standard_Complex_Vectors.Vector(0..dendeg);
+    pnt : constant Complex_Number := Create(0.1);
+    eva : Complex_Number;
+    chkpnt : constant double_float
+           := Standard_Mathematical_Functions.COS(0.1);
+
+  begin
+    Standard_Test(numdeg,dendeg,cff,num,den);
+    eva := Standard_Evaluate(num,den,pnt);
+    put("The value at 0.1      :"); put(eva); new_line;
+    put("The value of cos(0.1) :"); put(chkpnt); new_line;
+  end Standard_Cos_Test;
 
   procedure Main is
 
@@ -213,15 +374,29 @@ procedure ts_serpade is
     degnum : integer32 := 0; -- degree numerator
     degden : integer32 := 0; -- degree denominator
     dim : integer32 := 0;    -- degnum + degden
+    ans : character;
 
   begin
     new_line;
-    put_line("Rational approximation for a function ...");
+    put_line("MENU to test rational approximations for a function ...");
+    put_line("  1. series for log(1+x)");
+    put_line("  2. series for exp(x)");
+    put_line("  3. series for sin(x)");
+    put_line("  4. series for cos(x)");
+    put("Type 1, 2, 3, or 4 to choose the test : ");
+    Ask_Alternative(ans,"1234");
+    new_line;
     put("Give the degree of the numerator : "); get(degnum);
     put("Give the degree of the denominator : "); get(degden);
     dim := degnum + degden;
     put("The dimension : "); put(dim,1); new_line;
-    Standard_Logarithm_Test(degnum,degden);
+    case ans is 
+      when '1' => Standard_Logarithm_Test(degnum,degden);
+      when '2' => Standard_Exp_Test(degnum,degden);
+      when '3' => Standard_Sin_Test(degnum,degden);
+      when '4' => Standard_Cos_Test(degnum,degden);
+      when others => null;
+    end case;
   end Main;
 
 begin
