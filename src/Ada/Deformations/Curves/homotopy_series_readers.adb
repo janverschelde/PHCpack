470a37fed362df56ffd6ff7962ec15aaf18711b6
Jan Verschelde <janv@uic.edu>
2017-05-21 17:07:42 -0500

    extra procedures for user given gamma constant in homotopy series readers

--- src/Ada/Deformations/Curves/homotopy_series_readers.adb
+++ src/Ada/Deformations/Curves/homotopy_series_readers.adb
@@ -1,10 +1,7 @@
 with text_io;                            use text_io;
 with Standard_Natural_Numbers;           use Standard_Natural_Numbers;
-with Standard_Complex_Numbers;
 with Standard_Random_Numbers;
-with DoblDobl_Complex_Numbers;
 with DoblDobl_Random_Numbers;
-with QuadDobl_Complex_Numbers;
 with QuadDobl_Random_Numbers;
 with Standard_Complex_Poly_Systems;
 with Standard_Complex_Poly_Systems_io;   use Standard_Complex_Poly_Systems_io;
@@ -24,12 +21,10 @@ package body Homotopy_Series_Readers is
   procedure Standard_Reader
               ( nbequ : out integer32;
                 sols : out Standard_Complex_Solutions.Solution_List;
-                tpow : in natural32 := 2 ) is
+                tpow : in natural32;
+                gamma : in Standard_Complex_Numbers.Complex_Number ) is
 
     target,start : Standard_Complex_Poly_Systems.Link_to_Poly_Sys;
-    k : constant natural32 := tpow;
-    gamma : constant Standard_Complex_Numbers.Complex_Number
-          := Standard_Random_Numbers.Random1;
 
   begin
     new_line;
@@ -40,18 +35,16 @@ package body Homotopy_Series_Readers is
     new_line;
     put_line("Reading the start system and its solutions ...");
     Standard_System_and_Solutions_io.get(start,sols);
-    Standard_Homotopy.Create(target.all,start.all,k,gamma);
+    Standard_Homotopy.Create(target.all,start.all,tpow,gamma);
   end Standard_Reader;
 
   procedure DoblDobl_Reader
               ( nbequ : out integer32;
                 sols : out DoblDobl_Complex_Solutions.Solution_List;
-                tpow : in natural32 := 2 ) is
+                tpow : in natural32;
+                gamma : in DoblDobl_Complex_Numbers.Complex_Number ) is
 
     target,start : DoblDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
-    k : constant natural32 := tpow;
-    gamma : constant DoblDobl_Complex_Numbers.Complex_Number
-          := DoblDobl_Random_Numbers.Random1;
 
   begin
     new_line;
@@ -62,18 +55,16 @@ package body Homotopy_Series_Readers is
     new_line;
     put_line("Reading the start system and its solutions ...");
     DoblDobl_System_and_Solutions_io.get(start,sols);
-    DoblDobl_Homotopy.Create(target.all,start.all,k,gamma);
+    DoblDobl_Homotopy.Create(target.all,start.all,tpow,gamma);
   end DoblDobl_Reader;
 
   procedure QuadDobl_Reader
               ( nbequ : out integer32;
                 sols : out QuadDobl_Complex_Solutions.Solution_List;
-                tpow : in natural32 := 2 ) is
+                tpow : in natural32;
+                gamma : in QuadDobl_Complex_Numbers.Complex_Number ) is
 
     target,start : QuadDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
-    k : constant natural32 := tpow;
-    gamma : constant QuadDobl_Complex_Numbers.Complex_Number
-          := QuadDobl_Random_Numbers.Random1;
 
   begin
     new_line;
@@ -84,7 +75,43 @@ package body Homotopy_Series_Readers is
     new_line;
     put_line("Reading the start system ...");
     QuadDobl_System_and_Solutions_io.get(start,sols);
-    QuadDobl_Homotopy.Create(target.all,start.all,k,gamma);
+    QuadDobl_Homotopy.Create(target.all,start.all,tpow,gamma);
+  end QuadDobl_Reader;
+
+  procedure Standard_Reader
+              ( nbequ : out integer32;
+                sols : out Standard_Complex_Solutions.Solution_List;
+                tpow : in natural32 := 2 ) is
+
+    gamma : constant Standard_Complex_Numbers.Complex_Number
+          := Standard_Random_Numbers.Random1;
+
+  begin
+    Standard_Reader(nbequ,sols,tpow,gamma);
+  end Standard_Reader;
+
+  procedure DoblDobl_Reader
+              ( nbequ : out integer32;
+                sols : out DoblDobl_Complex_Solutions.Solution_List;
+                tpow : in natural32 := 2 ) is
+
+    gamma : constant DoblDobl_Complex_Numbers.Complex_Number
+          := DoblDobl_Random_Numbers.Random1;
+
+  begin
+    DoblDobl_Reader(nbequ,sols,tpow,gamma);
+  end DoblDobl_Reader;
+
+  procedure QuadDobl_Reader
+              ( nbequ : out integer32;
+                sols : out QuadDobl_Complex_Solutions.Solution_List;
+                tpow : in natural32 := 2 ) is
+
+    gamma : constant QuadDobl_Complex_Numbers.Complex_Number
+          := QuadDobl_Random_Numbers.Random1;
+
+  begin
+    QuadDobl_Reader(nbequ,sols,tpow,gamma);
   end QuadDobl_Reader;
 
 end Homotopy_Series_Readers;
