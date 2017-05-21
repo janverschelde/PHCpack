3743d851ec07383a017134e21259add6e18e47fb
Jan Verschelde <janv@uic.edu>
2017-05-21 16:36:40 -0500

    added default value for the continuation parameter in the homotopy series readers

--- src/Ada/Deformations/Curves/homotopy_series_readers.adb
+++ src/Ada/Deformations/Curves/homotopy_series_readers.adb
@@ -23,10 +23,11 @@ package body Homotopy_Series_Readers is
 
   procedure Standard_Reader
               ( nbequ : out integer32;
-                sols : out Standard_Complex_Solutions.Solution_List ) is
+                sols : out Standard_Complex_Solutions.Solution_List;
+                tpow : in natural32 := 2 ) is
 
     target,start : Standard_Complex_Poly_Systems.Link_to_Poly_Sys;
-    k : constant natural32 := 2;
+    k : constant natural32 := tpow;
     gamma : constant Standard_Complex_Numbers.Complex_Number
           := Standard_Random_Numbers.Random1;
 
@@ -44,10 +45,11 @@ package body Homotopy_Series_Readers is
 
   procedure DoblDobl_Reader
               ( nbequ : out integer32;
-                sols : out DoblDobl_Complex_Solutions.Solution_List ) is
+                sols : out DoblDobl_Complex_Solutions.Solution_List;
+                tpow : in natural32 := 2 ) is
 
     target,start : DoblDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
-    k : constant natural32 := 2;
+    k : constant natural32 := tpow;
     gamma : constant DoblDobl_Complex_Numbers.Complex_Number
           := DoblDobl_Random_Numbers.Random1;
 
@@ -65,10 +67,11 @@ package body Homotopy_Series_Readers is
 
   procedure QuadDobl_Reader
               ( nbequ : out integer32;
-                sols : out QuadDobl_Complex_Solutions.Solution_List ) is
+                sols : out QuadDobl_Complex_Solutions.Solution_List;
+                tpow : in natural32 := 2 ) is
 
     target,start : QuadDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
-    k : constant natural32 := 2;
+    k : constant natural32 := tpow;
     gamma : constant QuadDobl_Complex_Numbers.Complex_Number
           := QuadDobl_Random_Numbers.Random1;
 
--- src/Ada/Deformations/Curves/homotopy_series_readers.ads
+++ src/Ada/Deformations/Curves/homotopy_series_readers.ads
@@ -1,3 +1,4 @@
+with Standard_Natural_Numbers;          use Standard_Natural_Numbers;
 with Standard_Integer_Numbers;          use Standard_Integer_Numbers;
 with Standard_Complex_Solutions;
 with DoblDobl_Complex_Solutions;
@@ -8,21 +9,31 @@ package Homotopy_Series_Readers is
 -- DESCRIPTION :
 --   Provides interactive procedures to setup of homotopies of series,
 --   in double, double double, and quad double precision.
+--   The homotopy is an artificial parameter homotopy.
 
   procedure Standard_Reader
               ( nbequ : out integer32;
-                sols : out Standard_Complex_Solutions.Solution_List );
+                sols : out Standard_Complex_Solutions.Solution_List;
+                tpow : in natural32 := 2 );
   procedure DoblDobl_Reader
               ( nbequ : out integer32;
-                sols : out DoblDobl_Complex_Solutions.Solution_List );
+                sols : out DoblDobl_Complex_Solutions.Solution_List;
+                tpow : in natural32 := 2 );
   procedure QuadDobl_Reader
               ( nbequ : out integer32;
-                sols : out QuadDobl_Complex_Solutions.Solution_List );
+                sols : out QuadDobl_Complex_Solutions.Solution_List;
+                tpow : in natural32 := 2 );
 
   -- DESCRIPTION :
-  --   Prompts the user for a target system, a start system with
-  --   start solutions, returned in sols.
-  --   The number of equations is returned in nbequ.
+  --   Prompts for a target system, a start system with start solutions.
   --   The target and start system are stored in the Homotopy package.
+
+  -- ON ENTRY :
+  --   tpow     power of the continuation parameter
+  --            in the artificial parameter homotopy.
+
+  -- ON RETURN :
+  --   nbequ    number of equations in the systems in the homotopy;
+  --   sols     start solutions in the homotopy.
 
 end Homotopy_Series_Readers;
