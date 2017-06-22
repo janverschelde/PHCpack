c963a2d43aede122f23c73399b8cc0f1152e6751
Jan Verschelde <janv@uic.edu>
2017-06-21 21:50:53 -0500

    refactored code for the blackbox root refiners, called by phc -v

--- src/Ada/Main/bablvali.adb
+++ src/Ada/Main/bablvali.adb
@@ -13,10 +13,25 @@ with Black_Box_Root_Refiners;
 
 procedure bablvali ( infilename,outfilename : in string ) is
 
-  lp : Link_to_Poly_Sys;
-  sysonfile : boolean;
+  procedure Read_System
+              ( file : in out file_type; filename : in string;
+                lp : out Link_to_Poly_Sys; sysonfile : out boolean ) is
 
-  procedure Read_System ( file : in out file_type; filename : in string ) is
+  -- DESCRIPTION :
+  --   If the filename is not empty, then the file is opened
+  --   and a system is read from file.
+
+  -- ON ENTRY :
+  --   filename is the name of a file, typically infilename.
+
+  -- ON RETURN :
+  --   file     opened for input is filename is not empty,
+  --            may contain also the solutions of the system;
+  --   lp       null if there was no system on file with filename,
+  --            otherwise points to the system on file;
+  --   sysonfile is false if the reading of a system did not work,
+  --            otherwise sysonfile is true.
+
   begin
     if filename /= "" then
       Open(file,in_file,filename);
@@ -31,48 +46,68 @@ procedure bablvali ( infilename,outfilename : in string ) is
                    lp := null; return;
   end Read_System;
 
-  procedure Main is
+  procedure Prompt_for_System
+              ( file : in out file_type; lp : out Link_to_Poly_Sys;
+                sysonfile : out boolean ) is
 
-    infile,outfile : file_type;
-    nbvar : natural32;
+  -- DESCRIPTION :
+  --   Prompts the user for a file name and reads the system.
+
+  -- ON RETURN :
+  --   file     will be opened for input if the reading was okay;
+  --   lp       points to a polynomial system if reading was okay,
+  --            otherwise lp remains null;
+  --   sysonfile is true if the system was read from file;
+  --            otherwise if the system was typed in.
+
     ans : character;
-    sols : Solution_List;
     n : integer32 := 0;
-    found : boolean;
 
   begin
-    Read_System(infile,infilename);
-    if lp = null then
-      new_line;
-      put("Is the system on file ? (y/n) ");
-      Ask_Yes_or_No(ans);
-      if ans = 'y' then
-        put_line("Reading the name of the input file.");
-        Read_Name_and_Open_File(infile);
-        get(infile,lp);
-        sysonfile := true;
-      else
-        put("Give the dimension : "); get(n);
-        lp := new Poly_Sys(1..n);
-        put("Give "); put(n,1); put(" "); put(n,1); 
-        put_line("-variate polynomials :");
-        get(natural32(n),lp.all);
-        skip_line;
-        sysonfile := false;
-      end if;
+    new_line;
+    put("Is the system on file ? (y/n) ");
+    Ask_Yes_or_No(ans);
+    if ans = 'y' then
+      put_line("Reading the name of the input file.");
+      Read_Name_and_Open_File(file);
+      get(file,lp);
+      sysonfile := true;
+    else
+      put("Give the dimension : "); get(n);
+      lp := new Poly_Sys(1..n);
+      put("Give "); put(n,1); put(" "); put(n,1); 
+      put_line("-variate polynomials :");
+      get(natural32(n),lp.all);
+      skip_line;
+      sysonfile := false;
     end if;
-    nbvar := Standard_Complex_Polynomials.Number_of_Unknowns(lp(lp'first));
+  end Prompt_for_System;
+
+  procedure Refine ( file : in out file_type; lp : in Link_to_Poly_Sys;
+                     sysonfile : in boolean ) is
+
+  -- DESCRIPTION :
+  --   Calls the root refiner on the system lp,
+  --   reading the solutions from file if sysonfile.
+
+    outfile : file_type;
+    sols : Solution_List;
+    found : boolean;
+    nbvar : constant natural32
+          := Standard_Complex_Polynomials.Number_of_Unknowns(lp(lp'first));
+
+  begin
     Create_Output_File(outfile,outfilename);
     if lp'last = integer32(nbvar)
      then put(outfile,natural32(lp'last),lp.all);
      else put(outfile,natural32(lp'last),nbvar,lp.all);
     end if;
     if sysonfile then
-      Scan_and_Skip(infile,"THE SOLUTIONS",found);
+      Scan_and_Skip(file,"THE SOLUTIONS",found);
       if found
-       then get(infile,sols);
+       then get(file,sols);
       end if;
-      Close(infile);
+      Close(file);
     else
       found := false;
     end if;
@@ -80,6 +115,26 @@ procedure bablvali ( infilename,outfilename : in string ) is
      then new_line; Read(sols);
     end if;
     Black_Box_Root_Refiners.Refine_Roots(outfile,lp.all,sols);
+  end Refine;
+
+  procedure Main is
+
+  -- DESCRIPTION :
+  --   Reads the system, the solutions,
+  --   and then calls the black box root refiner.
+
+    infile : file_type;
+    sysonfile : boolean;
+    lp : Link_to_Poly_Sys := null;
+
+  begin
+    Read_System(infile,infilename,lp,sysonfile);
+    if lp = null
+     then Prompt_for_System(infile,lp,sysonfile);
+    end if;
+    if lp /= null
+     then Refine(infile,lp,sysonfile);
+    end if;
   end Main;
 
 begin
