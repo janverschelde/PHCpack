bb893615a425bac9b989d46e542c8731b7f95ea3
Jan Verschelde <janv@uic.edu>
2017-06-22 11:00:13 -0500

    new package to scan, read, and prompt for systems, obtained with refactoring code

---
+++ src/Ada/Main/prompt_for_systems.adb
@@ -0,0 +1,248 @@
+with Communications_with_User;           use Communications_with_User;
+with Standard_Natural_Numbers;           use Standard_Natural_Numbers;
+with Standard_Integer_Numbers;           use Standard_Integer_Numbers;
+with Standard_Integer_Numbers_io;        use Standard_Integer_Numbers_io;
+with Standard_Complex_Poly_Systems_io;   use Standard_Complex_Poly_Systems_io;
+with DoblDobl_Complex_Poly_Systems_io;   use DoblDobl_Complex_Poly_Systems_io;
+with QuadDobl_Complex_Poly_Systems_io;   use QuadDobl_Complex_Poly_Systems_io;
+
+package body Prompt_for_Systems is
+
+  procedure Scan_System
+               ( file : in out file_type; name : in string;
+                 lp : in out Standard_Complex_Poly_Systems.Link_to_Poly_Sys;
+                 onfile : out boolean ) is
+
+  -- DESCRIPTION :
+  --   Checks whether the given file name corresponds to a file with
+  --   a polynomial system in a correct format.
+  --   If this is the case, then onfile is true on return and lp
+  --   contains the system.
+
+  begin
+    if name /= "" then
+      Open(file,in_file,name);
+      get(file,lp);
+      onfile := true;
+    else
+      onfile := false;
+    end if;
+   -- if lp /= null then
+   --   put("in Scan_System : ");
+   --   put(" #equations = "); put(lp'last,1); new_line;
+   -- end if;
+  exception
+    when others =>
+      new_line;
+      put("Could not open file with name "); put_line(name);
+      lp := null;
+      onfile := false;
+      return;
+  end Scan_System;
+
+  procedure Scan_System
+               ( file : in out file_type; name : in string;
+                 lp : in out DoblDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
+                 onfile : out boolean ) is
+
+  -- DESCRIPTION :
+  --   Checks whether the given file name corresponds to a file with
+  --   a polynomial system in a correct format.
+  --   If this is the case, then onfile is true on return and lp
+  --   contains the system.
+
+  begin
+    if name /= "" then
+      Open(file,in_file,name);
+      get(file,lp);
+      onfile := true;
+    else
+      onfile := false;
+    end if;
+   -- if lp /= null then
+   --   put("in Scan_System : ");
+   --   put(" #equations = "); put(lp'last,1); new_line;
+   -- end if;
+  exception
+    when others =>
+      new_line;
+      put("Could not open file with name "); put_line(name);
+      lp := null;
+      onfile := false;
+      return;
+  end Scan_System;
+
+  procedure Scan_System
+               ( file : in out file_type; name : in string;
+                 lp : in out QuadDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
+                 onfile : out boolean ) is
+
+  -- DESCRIPTION :
+  --   Checks whether the given file name corresponds to a file with
+  --   a polynomial system in a correct format.
+  --   If this is the case, then onfile is true on return and lp
+  --   contains the system.
+
+  begin
+    if name /= "" then
+      Open(file,in_file,name);
+      get(file,lp);
+      onfile := true;
+    else
+      onfile := false;
+    end if;
+   -- if lp /= null then
+   --   put("in Scan_System : ");
+   --   put(" #equations = "); put(lp'last,1); new_line;
+   -- end if;
+  exception
+    when others =>
+      new_line;
+      put("Could not open file with name "); put_line(name);
+      lp := null;
+      onfile := false;
+      return;
+  end Scan_System;
+
+  procedure Read_System
+              ( file : in out file_type; name : in string;
+                lp : in out Standard_Complex_Poly_Systems.Link_to_Poly_Sys;
+                onfile : out boolean ) is
+
+    use Standard_Complex_Poly_Systems;
+
+    ans : character;
+    n : integer32;
+
+  begin
+    Scan_System(file,name,lp,onfile);
+    if lp = null then
+      loop
+        new_line;
+        put("Is the system on a file ? (y/n/i=info) ");
+        Ask_Alternative(ans,"yni");
+        if ans = 'i' then
+          new_line;
+          Standard_Complex_Poly_Systems_io.Display_Format;
+          new_line;
+        end if;
+        exit when ans /= 'i';
+      end loop;
+      new_line;
+      if ans = 'y' then
+        put_line("Reading the name of the input file.");
+        Read_Name_and_Open_File(file);
+        get(file,lp);
+        onfile := true;
+        n := lp'length;
+      else
+        put("Give the dimension : "); get(n);
+        lp := new Standard_Complex_Poly_Systems.Poly_Sys(1..n);
+        put("Give "); put(n,1); put(" "); put(n,1);
+        put_line("-variate polynomials :");
+        get(natural32(n),lp.all);
+        skip_line;  -- skip end_of_line symbol
+        onfile := false;
+      end if;
+    end if;
+   -- put_line("at end of Read_System : ");
+   -- put("#equations : "); put(lp'last,1);
+   -- put(", #variables : "); put(Number_of_Unknowns(lp(lp'first)),1);
+   -- new_line;
+  end Read_System;
+
+  procedure Read_System
+              ( file : in out file_type; name : in string;
+                lp : in out DoblDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
+                onfile : out boolean ) is
+
+    use DoblDobl_Complex_Poly_Systems;
+
+    ans : character;
+    n : integer32;
+
+  begin
+    Scan_System(file,name,lp,onfile);
+    if lp = null then
+      loop
+        new_line;
+        put("Is the system on a file ? (y/n/i=info) ");
+        Ask_Alternative(ans,"yni");
+        if ans = 'i' then
+          new_line;
+          Standard_Complex_Poly_Systems_io.Display_Format;
+          new_line;
+        end if;
+        exit when ans /= 'i';
+      end loop;
+      new_line;
+      if ans = 'y' then
+        put_line("Reading the name of the input file.");
+        Read_Name_and_Open_File(file);
+        get(file,lp);
+        onfile := true;
+        n := lp'length;
+      else
+        put("Give the dimension : "); get(n);
+        lp := new DoblDobl_Complex_Poly_Systems.Poly_Sys(1..n);
+        put("Give "); put(n,1); put(" "); put(n,1);
+        put_line("-variate polynomials :");
+        get(standard_input,lp.all);
+        skip_line;  -- skip end_of_line symbol
+        onfile := false;
+      end if;
+    end if;
+   -- put_line("at end of Read_System : ");
+   -- put("#equations : "); put(lp'last,1);
+   -- put(", #variables : "); put(Number_of_Unknowns(lp(lp'first)),1);
+   -- new_line;
+  end Read_System;
+
+  procedure Read_System
+              ( file : in out file_type; name : in string;
+                lp : in out QuadDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
+                onfile : out boolean ) is
+
+    use QuadDobl_Complex_Poly_Systems;
+
+    ans : character;
+    n : integer32;
+
+  begin
+    Scan_System(file,name,lp,onfile);
+    if lp = null then
+      loop
+        new_line;
+        put("Is the system on a file ? (y/n/i=info) ");
+        Ask_Alternative(ans,"yni");
+        if ans = 'i' then
+          new_line;
+          Standard_Complex_Poly_Systems_io.Display_Format;
+          new_line;
+        end if;
+        exit when ans /= 'i';
+      end loop;
+      new_line;
+      if ans = 'y' then
+        put_line("Reading the name of the input file.");
+        Read_Name_and_Open_File(file);
+        get(file,lp);
+        onfile := true;
+        n := lp'length;
+      else
+        put("Give the dimension : "); get(n);
+        lp := new QuadDobl_Complex_Poly_Systems.Poly_Sys(1..n);
+        put("Give "); put(n,1); put(" "); put(n,1);
+        put_line("-variate polynomials :");
+        get(standard_input,lp.all);
+        skip_line;  -- skip end_of_line symbol
+        onfile := false;
+      end if;
+    end if;
+   -- put_line("at end of Read_System : ");
+   -- put("#equations : "); put(lp'last,1);
+   -- put(", #variables : "); put(Number_of_Unknowns(lp(lp'first)),1);
+   -- new_line;
+  end Read_System;
+
+end Prompt_for_Systems;
---
+++ src/Ada/Main/prompt_for_systems.ads
@@ -0,0 +1,59 @@
+with text_io;                            use text_io;
+with Standard_Complex_Poly_Systems;
+with DoblDobl_Complex_Poly_Systems;
+with QuadDobl_Complex_Poly_Systems;
+
+package Prompt_for_Systems is
+
+-- DESCRIPTION :
+--   By default, the input system is on the file, with the given file name,
+--   but otherwise the user will be prompted to provide a file name,
+--   or given the possibility to type in the polynomials in the system.
+
+  procedure Scan_System
+              ( file : in out file_type; name : in string;
+                lp : in out Standard_Complex_Poly_Systems.Link_to_Poly_Sys;
+                onfile : out boolean );
+  procedure Scan_System
+              ( file : in out file_type; name : in string;
+                lp : in out DoblDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
+                onfile : out boolean );
+  procedure Scan_System
+              ( file : in out file_type; name : in string;
+                lp : in out QuadDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
+                onfile : out boolean );
+
+  -- DESCRIPTION :
+  --   Checks whether the given file name corresponds to a file with
+  --   a polynomial system in a correct format.
+  --   If this is the case, then onfile is true on return and lp
+  --   contains the system.
+
+  procedure Read_System
+              ( file : in out file_type; name : in string;
+                lp : in out Standard_Complex_Poly_Systems.Link_to_Poly_Sys;
+                onfile : out boolean );
+  procedure Read_System
+              ( file : in out file_type; name : in string;
+                lp : in out DoblDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
+                onfile : out boolean );
+  procedure Read_System
+              ( file : in out file_type; name : in string;
+                lp : in out QuadDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
+                onfile : out boolean );
+
+  -- DESCRIPTION :
+  --   Reads a system from file or from standard input.
+  --   Coefficients are parsed in standard double, double double,
+  --   or quad double precision.
+
+  -- ON ENTRY :
+  --   name     name of the file where a system may be.
+
+  -- ON RETURN :
+  --   file     file opened for input, if onfile,
+  --            this file may contain the solutions of the system;
+  --   lp       points to a system read from file or type in;
+  --   onfile   is true if the system lp was read from file.
+
+end Prompt_for_Systems;
