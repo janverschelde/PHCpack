5077ffe1a8b71467421795f0de0f9d5e406e53fb
Jan Verschelde <janv@uic.edu>
2017-06-28 21:49:20 -0500

    test function added to check if a number is valid or not

--- src/Ada/Math_Lib/Numbers/ts_isnan.adb
+++ src/Ada/Math_Lib/Numbers/ts_isnan.adb
@@ -11,6 +11,24 @@ procedure ts_isnan is
   one : constant double_float := 1.0;
   tol : constant double_float := 1.0E-8;
 
+  function Is_Valid ( x : double_float ) return boolean is
+
+  -- DESCRIPTION :
+  --   A valid number is either positive, negative, or zero,
+  --   in which case true is returned, otherwise false is returned.
+
+  begin
+    if x > 0.0 then
+      return true;
+    elsif x < 0.0 then
+      return true;
+    elsif x = 0.0 then
+      return true;
+    else
+      return false;
+    end if;
+  end Is_Valid;
+
 begin
   put("give numerator (suggestion: 0) : "); get(num);
   put("give denominator (suggestion: 0) : "); get(den);
@@ -34,5 +52,39 @@ begin
   if abs(one - quo) < tol
    then put_line("abs(1 - quotient) < 1.0E-8");
    else put_line("abs(1 - quotient) >= 1.0E-8");
+  end if;
+  if abs(one - quo) = 0.0 then
+    put_line("abs(1 - quotient) = 0.0");
+  elsif abs(one - quo) > 0.0 then
+    put_line("abs(1 - quotient) > 0.0");
+  else
+    put_line("abs(1 - quotient) < 0.0");
+  end if;
+  if abs(quo - one) = 0.0 then
+    put_line("abs(quotient - 1) = 0.0");
+  elsif abs(quo - one) > 0.0 then
+    put_line("abs(quotient - 1) > 0.0");
+  else
+    put_line("abs(quotient - 1) < 0.0");
+  end if;
+  if quo < 0.0
+   then put_line("quo < 0");
+   else put_line("quo >= 0");
+  end if;
+  if quo > 0.0
+   then put_line("quo > 0");
+   else put_line("quo <= 0");
+  end if;
+  if quo = 0.0
+   then put_line("quo = 0");
+   else put_line("quo /= 0");
+  end if;
+  if is_valid(1.0)
+   then put_line("1 is valid");
+   else put_line("1 is not valid");
+  end if;
+  if is_valid(quo)
+   then put_line("quo is valid");
+   else put_line("quo is not valid");
   end if;
 end ts_isnan;
