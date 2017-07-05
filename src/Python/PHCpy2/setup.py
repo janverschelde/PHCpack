627bae165dc71ec2b2dd1c456fe9934567a9576b
Jan Verschelde <janv@uic.edu>
2017-07-05 17:00:57 -0500

    prepared files for version 0.6.8 of phcpy

--- src/Python/PHCpy2/setup.py
+++ src/Python/PHCpy2/setup.py
@@ -17,7 +17,7 @@ setup(
     author_email = 'janv@uic.edu' ,
     description = 'a package for Polynomial Homotopy Continuation' ,
     url = 'https://github.com/janverschelde/PHCpack' ,
-    version = '0.6.7' ,
+    version = '0.6.8' ,
     packages = ['phcpy'] ,
     py_modules = ['phcpy/interface', 'phcpy/solver', 'phcpy/solutions', \
         'phcpy/trackers', 'phcpy/sweepers', 'phcpy/tuning', 'phcpy/tropisms', \
