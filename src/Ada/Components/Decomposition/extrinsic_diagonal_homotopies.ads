e3351a92735394d97c75b1cde8be38bbc4aa7dd9
Jan Verschelde <janv@uic.edu>
2017-07-21 04:38:25 -0500

    completed extension in the definition of diagonal homotopies, for witness sets defined by Laurent systems

--- src/Ada/Components/Decomposition/extrinsic_diagonal_homotopies.ads
+++ src/Ada/Components/Decomposition/extrinsic_diagonal_homotopies.ads
@@ -158,6 +158,40 @@ package Extrinsic_Diagonal_Homotopies is
   --   target   embedded cascade to compute all components of dimension < b
   --            of the intersection of two components defined by p1e and p2e.
 
+  procedure Cascade2
+              ( p1e,p2e : in Standard_Complex_Laur_Systems.Laur_Sys;
+                a,b : in natural32;
+                start,target : out Standard_Complex_Laur_Systems.Laur_Sys );
+  procedure Cascade2
+              ( p1e,p2e : in DoblDobl_Complex_Laur_Systems.Laur_Sys;
+                a,b : in natural32;
+                start,target : out DoblDobl_Complex_Laur_Systems.Laur_Sys );
+  procedure Cascade2
+              ( p1e,p2e : in QuadDobl_Complex_Laur_Systems.Laur_Sys;
+                a,b : in natural32;
+                start,target : out QuadDobl_Complex_Laur_Systems.Laur_Sys );
+
+  -- DESCRIPTION :
+  --   Given two Laurent polynomial systems, properly embedded to sample
+  --   from positive dimensional solution components, this function returns
+  --   the cascade to get all components of their intersection,
+  --   in standard double, double double, or quad double precision.
+
+  -- ON ENTRY :
+  --   p1e      1st system with a-dimensional solution component;
+  --   p2e      2nd system with b-dimensional solution component;
+  --   a        dimension of solution component of 1st system;
+  --   b        dimension of solution component of 2nd system.
+
+  -- REQUIRED :
+  --   a >= b and a+b < k, k = ambient dimension;
+  --   start'range = target'range = 1..Cascade_Dimension(p1e,p2e,a,b).
+
+  -- ON RETURN :
+  --   start    start system to start the cascade;
+  --   target   embedded cascade to compute all components of dimension < b
+  --            of the intersection of two components defined by p1e and p2e.
+
   procedure Extrinsic_Cascade_Homotopy
               ( p1e,p2e : in Standard_Complex_Poly_Systems.Poly_Sys;
                 a,b : in natural32;
@@ -175,7 +209,43 @@ package Extrinsic_Diagonal_Homotopies is
   --   Returns homotopy to start the cascade, using Cascade1 and Cascade2,
   --   to find all components of the intersection of two witness sets, of
   --   dimensions a and b defined by p1e and p2e respectively,
-  --   in standard double, double double, or quad double precision.
+  --   in standard double, double double, or quad double precision,
+  --   for witness sets defined by ordinary polynomial systems.
+
+  -- ON ENTRY :
+  --   p1e      1st system with a-dimensional solution component;
+  --   p2e      2nd system with b-dimensional solution component;
+  --   a        dimension of solution component of 1st system;
+  --   b        dimension of solution component of 2nd system.
+
+  -- REQUIRED :
+  --   a >= b and a+b < k, k = ambient dimension;
+  --   start'range = target'range = 1..Cascade_Dimension(p1e,p2e,a,b).
+
+  -- ON RETURN :
+  --   start    start system to start the cascade;
+  --   target   embedded cascade to compute all components of dimension < b
+  --            of the intersection of two components defined by p1e and p2e.
+
+  procedure Extrinsic_Cascade_Homotopy
+              ( p1e,p2e : in Standard_Complex_Laur_Systems.Laur_Sys;
+                a,b : in natural32;
+                start,target : out Standard_Complex_Laur_Systems.Laur_Sys );
+  procedure Extrinsic_Cascade_Homotopy
+              ( p1e,p2e : in DoblDobl_Complex_Laur_Systems.Laur_Sys;
+                a,b : in natural32;
+                start,target : out DoblDobl_Complex_Laur_Systems.Laur_Sys );
+  procedure Extrinsic_Cascade_Homotopy
+              ( p1e,p2e : in QuadDobl_Complex_Laur_Systems.Laur_Sys;
+                a,b : in natural32;
+                start,target : out QuadDobl_Complex_Laur_Systems.Laur_Sys );
+
+  -- DESCRIPTION :
+  --   Returns homotopy to start the cascade, using Cascade1 and Cascade2,
+  --   to find all components of the intersection of two witness sets, of
+  --   dimensions a and b defined by p1e and p2e respectively,
+  --   in standard double, double double, or quad double precision,
+  --   for witness sets defined by Laurent polynomial systems.
 
   -- ON ENTRY :
   --   p1e      1st system with a-dimensional solution component;
@@ -252,6 +322,49 @@ package Extrinsic_Diagonal_Homotopies is
                 a,b : in natural32;
                 sols1,sols2 : in QuadDobl_Complex_Solutions.Solution_List;
                 start,target : out QuadDobl_Complex_Poly_Systems.Poly_Sys;
+                esols : out QuadDobl_Complex_Solutions.Solution_List );
+
+  -- DESCRIPTION :
+  --   Uses Cascade1 and Cascade2 to create the homotopy to start the cascade
+  --   to find all components of the intersection of two witness sets, of
+  --   dimensions a and b defined by (p1e,sols1) and (p2e,sols2) respectively,
+  --   in standard double, double double, or quad double precision.
+
+  -- ON ENTRY :
+  --   p1e      1st system with a-dimensional solution component;
+  --   p2e      2nd system with b-dimensional solution component;
+  --   sols1    witness points in 1st witness set, without the embedding;
+  --   sols2    witness points in 2nd witness set, without the embedding;
+  --   a        dimension of solution component of 1st system;
+  --   b        dimension of solution component of 2nd system.
+
+  -- REQUIRED :
+  --   a >= b and a+b < k, k = ambient dimension;
+  --   start'range = target'range = 1..Cascade_Dimension(p1e,p2e,a,b).
+
+  -- ON RETURN :
+  --   start    start system to start the cascade;
+  --   target   embedded cascade to compute all components of dimension < b
+  --            of the intersection of two components defined by p1e and p2e;
+  --   esols    embedded solutions of the start system in the cascade.
+
+  procedure Extrinsic_Cascade_Homotopy
+              ( p1e,p2e : in Standard_Complex_Laur_Systems.Laur_Sys;
+                a,b : in natural32;
+                sols1,sols2 : in Standard_Complex_Solutions.Solution_List;
+                start,target : out Standard_Complex_Laur_Systems.Laur_Sys;
+                esols : out Standard_Complex_Solutions.Solution_List );
+  procedure Extrinsic_Cascade_Homotopy
+              ( p1e,p2e : in DoblDobl_Complex_Laur_Systems.Laur_Sys;
+                a,b : in natural32;
+                sols1,sols2 : in DoblDobl_Complex_Solutions.Solution_List;
+                start,target : out DoblDobl_Complex_Laur_Systems.Laur_Sys;
+                esols : out DoblDobl_Complex_Solutions.Solution_List );
+  procedure Extrinsic_Cascade_Homotopy
+              ( p1e,p2e : in QuadDobl_Complex_Laur_Systems.Laur_Sys;
+                a,b : in natural32;
+                sols1,sols2 : in QuadDobl_Complex_Solutions.Solution_List;
+                start,target : out QuadDobl_Complex_Laur_Systems.Laur_Sys;
                 esols : out QuadDobl_Complex_Solutions.Solution_List );
 
   -- DESCRIPTION :
