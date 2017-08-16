57263c0d5e2bb3227beb4aee65735b6acefd5449
Jan Verschelde <janv@uic.edu>
2017-08-16 12:29:39 -0500

    more reformatting of code to evaluate monomials on a GPU

--- src/GPU/Path/Path_GPU/eval_mon_single.cu
+++ src/GPU/Path/Path_GPU/eval_mon_single.cu
@@ -2,126 +2,140 @@
 #define __PATH_GPU_EVAL_MON_SINGLE_CU_
 
 // Mon evalutaion and differentiation on GPU
-__global__ void eval_mon_single_kernel(GT* workspace_mon, GT* x, GT*workspace_coef, \
-                           int* mon_pos_start, unsigned short* mon_pos, \
-                           int n_mon) {
-	int idx = (gridDim.x*blockIdx.y+blockIdx.x)*blockDim.x + threadIdx.x;
+__global__ void eval_mon_single_kernel
+ ( GT* workspace_mon, GT* x, GT*workspace_coef, int* mon_pos_start,
+   unsigned short* mon_pos, int n_mon) 
+{
+   int idx = (gridDim.x*blockIdx.y+blockIdx.x)*blockDim.x + threadIdx.x;
 
-	//int tidx = threadIdx.x;
-	if(idx < n_mon) {
-		int tmp_start = mon_pos_start[idx];
-		GT* deri = workspace_mon + tmp_start;
-		unsigned short* pos = mon_pos + tmp_start;
+   // int tidx = threadIdx.x;
+   if(idx < n_mon)
+   {
+      int tmp_start = mon_pos_start[idx];
+      GT* deri = workspace_mon + tmp_start;
+      unsigned short* pos = mon_pos + tmp_start;
 
-		GT tmp = workspace_coef[idx];
-		deri[1] = tmp;
-		deri[0] = x[pos[1]]*tmp;
-	}
+      GT tmp = workspace_coef[idx];
+      deri[1] = tmp;
+      deri[0] = x[pos[1]]*tmp;
+   }
 }
 
 // Mon evalutaion and differentiation on GPU
-__global__ void eval_mon_single_kernel(GT* workspace_mon, GT* x, GT*workspace_coef, \
-                           unsigned short* mon_pos, int n_mon) {
-	int idx = (gridDim.x*blockIdx.y+blockIdx.x)*blockDim.x + threadIdx.x;
+__global__ void eval_mon_single_kernel
+ ( GT* workspace_mon, GT* x, GT*workspace_coef,
+   unsigned short* mon_pos, int n_mon )
+{
+   int idx = (gridDim.x*blockIdx.y+blockIdx.x)*blockDim.x + threadIdx.x;
 
-	//int tidx = threadIdx.x;
-	if(idx < n_mon) {
-		int tmp_start = idx*2;
-		GT* deri = workspace_mon + tmp_start;
-		unsigned short* pos = mon_pos + tmp_start;
+   // int tidx = threadIdx.x;
+   if(idx < n_mon) 
+   {
+      int tmp_start = idx*2;
+      GT* deri = workspace_mon + tmp_start;
+      unsigned short* pos = mon_pos + tmp_start;
 
-		GT tmp = workspace_coef[idx];
-		deri[1] = tmp;
-		deri[0] = x[pos[1]]*tmp;
-	}
+      GT tmp = workspace_coef[idx];
+      deri[1] = tmp;
+      deri[0] = x[pos[1]]*tmp;
+   }
 }
 
-__global__ void eval_mon_single_with_base_kernel(GT* workspace_mon, GT* x, GT*workspace_coef, \
-                           int* mon_pos_start, unsigned short* mon_pos, unsigned short* mon_exp, \
-                           int n_mon) {
-	int idx = (gridDim.x*blockIdx.y+blockIdx.x)*blockDim.x + threadIdx.x;
+__global__ void eval_mon_single_with_base_kernel
+ ( GT* workspace_mon, GT* x, GT*workspace_coef, int* mon_pos_start,
+   unsigned short* mon_pos, unsigned short* mon_exp, int n_mon )
+{
+   int idx = (gridDim.x*blockIdx.y+blockIdx.x)*blockDim.x + threadIdx.x;
 
-	//int tidx = threadIdx.x;
-	if(idx < n_mon) {
-		int tmp_start = mon_pos_start[idx];
-		GT* deri = workspace_mon + tmp_start;
-		unsigned short* pos = mon_pos + tmp_start;
-		unsigned short* exp = mon_exp + tmp_start;
+   // int tidx = threadIdx.x;
+   if(idx < n_mon)
+   {
+      int tmp_start = mon_pos_start[idx];
+      GT* deri = workspace_mon + tmp_start;
+      unsigned short* pos = mon_pos + tmp_start;
+      unsigned short* exp = mon_exp + tmp_start;
 
-		GT tmp = workspace_coef[idx];
-		deri[1] = tmp*GT(exp[1],0.0);
-		deri[0] = x[pos[1]]*tmp;
-	}
-}
-
-
-// Mon evalutaion and differentiation on GPU
-__global__ void eval_mon_single_kernel(GT* workspace_mon, GT* x, GT*workspace_coef, \
-                           int* mon_pos_start, unsigned short* mon_pos, \
-                           int n_mon, int workspace_size) {
-	int idx = (gridDim.x*blockIdx.y+blockIdx.x)*blockDim.x + threadIdx.x;
-
-	int path_idx = blockIdx.z;
-	workspace_mon += workspace_size*path_idx;
-	x += workspace_size*path_idx;
-	workspace_coef += workspace_size*path_idx;
-
-	//int tidx = threadIdx.x;
-	if(idx < n_mon) {
-		int tmp_start = mon_pos_start[idx];
-		GT* deri = workspace_mon + tmp_start;
-		unsigned short* pos = mon_pos + tmp_start;
-
-		GT tmp = workspace_coef[idx];
-		deri[1] = tmp;
-		deri[0] = x[pos[1]]*tmp;
-	}
+      GT tmp = workspace_coef[idx];
+      deri[1] = tmp*GT(exp[1],0.0);
+      deri[0] = x[pos[1]]*tmp;
+   }
 }
 
 // Mon evalutaion and differentiation on GPU
-__global__ void eval_mon_single_kernel(GT* workspace_mon, GT* x, GT*workspace_coef, \
-                           int* mon_pos_start, unsigned short* mon_pos, \
-                           int n_mon, int workspace_size, int* x_t_idx, int dim) {
-	int idx = (gridDim.x*blockIdx.y+blockIdx.x)*blockDim.x + threadIdx.x;
+__global__ void eval_mon_single_kernel
+ ( GT* workspace_mon, GT* x, GT*workspace_coef, int* mon_pos_start,
+   unsigned short* mon_pos, int n_mon, int workspace_size )
+{
+   int idx = (gridDim.x*blockIdx.y+blockIdx.x)*blockDim.x + threadIdx.x;
 
-	int path_idx = blockIdx.z;
-	workspace_mon += workspace_size*path_idx;
-	x += workspace_size*path_idx + x_t_idx[path_idx]*dim;
-	workspace_coef += workspace_size*path_idx;
+   int path_idx = blockIdx.z;
+   workspace_mon += workspace_size*path_idx;
+   x += workspace_size*path_idx;
+   workspace_coef += workspace_size*path_idx;
 
-	//int tidx = threadIdx.x;
-	if(idx < n_mon) {
-		int tmp_start = mon_pos_start[idx];
-		GT* deri = workspace_mon + tmp_start;
-		unsigned short* pos = mon_pos + tmp_start;
+   // int tidx = threadIdx.x;
+   if(idx < n_mon)
+   {
+      int tmp_start = mon_pos_start[idx];
+      GT* deri = workspace_mon + tmp_start;
+      unsigned short* pos = mon_pos + tmp_start;
 
-		GT tmp = workspace_coef[idx];
-		deri[1] = tmp;
-		deri[0] = x[pos[1]]*tmp;
-	}
+      GT tmp = workspace_coef[idx];
+      deri[1] = tmp;
+      deri[0] = x[pos[1]]*tmp;
+   }
 }
 
 // Mon evalutaion and differentiation on GPU
-__global__ void eval_mon_single_kernel(GT* workspace_mon, GT* x, GT*workspace_coef, \
-                           int* mon_pos_start, unsigned short* mon_pos, \
-                           int n_mon, int workspace_size, int* x_t_idx, int dim, int* path_idx_mult) {
-	int idx = (gridDim.x*blockIdx.y+blockIdx.x)*blockDim.x + threadIdx.x;
+__global__ void eval_mon_single_kernel
+ ( GT* workspace_mon, GT* x, GT*workspace_coef, int* mon_pos_start,
+   unsigned short* mon_pos, int n_mon, int workspace_size, int* x_t_idx,
+   int dim )
+{
+   int idx = (gridDim.x*blockIdx.y+blockIdx.x)*blockDim.x + threadIdx.x;
 
-	int path_idx = path_idx_mult[blockIdx.z];
-	workspace_mon += workspace_size*path_idx;
-	x += workspace_size*path_idx + x_t_idx[path_idx]*dim;
-	workspace_coef += workspace_size*path_idx;
+   int path_idx = blockIdx.z;
+   workspace_mon += workspace_size*path_idx;
+   x += workspace_size*path_idx + x_t_idx[path_idx]*dim;
+   workspace_coef += workspace_size*path_idx;
 
-	//int tidx = threadIdx.x;
-	if(idx < n_mon) {
-		int tmp_start = 2*idx; //mon_pos_start[idx];
-		GT* deri = workspace_mon + tmp_start;
-		unsigned short* pos = mon_pos + tmp_start;
+   // int tidx = threadIdx.x;
+   if(idx < n_mon)
+   {
+      int tmp_start = mon_pos_start[idx];
+      GT* deri = workspace_mon + tmp_start;
+      unsigned short* pos = mon_pos + tmp_start;
 
-		GT tmp = workspace_coef[idx];
-		deri[1] = tmp;
-		deri[0] = x[pos[1]]*tmp;
-	}
+      GT tmp = workspace_coef[idx];
+      deri[1] = tmp;
+      deri[0] = x[pos[1]]*tmp;
+   }
+}
+
+// Mon evalutaion and differentiation on GPU
+__global__ void eval_mon_single_kernel
+ ( GT* workspace_mon, GT* x, GT*workspace_coef, int* mon_pos_start,
+   unsigned short* mon_pos, int n_mon, int workspace_size, int* x_t_idx,
+   int dim, int* path_idx_mult )
+{
+   int idx = (gridDim.x*blockIdx.y+blockIdx.x)*blockDim.x + threadIdx.x;
+
+   int path_idx = path_idx_mult[blockIdx.z];
+   workspace_mon += workspace_size*path_idx;
+   x += workspace_size*path_idx + x_t_idx[path_idx]*dim;
+   workspace_coef += workspace_size*path_idx;
+
+   // int tidx = threadIdx.x;
+   if(idx < n_mon)
+   {
+      int tmp_start = 2*idx; //mon_pos_start[idx];
+      GT* deri = workspace_mon + tmp_start;
+      unsigned short* pos = mon_pos + tmp_start;
+
+      GT tmp = workspace_coef[idx];
+      deri[1] = tmp;
+      deri[0] = x[pos[1]]*tmp;
+   }
 }
 
 #endif /*__PATH_GPU_EVAL_MON_SINGLE_CU_*/
