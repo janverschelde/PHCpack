28d9cb411ff0a1f6f39e3b26604e5e249653112c
Jan Verschelde <janv@uic.edu>
2017-08-20 15:46:05 -0500

    prepared files for release of 2.4.43

--- src/Ada/Main/greeting_banners.adb
+++ src/Ada/Main/greeting_banners.adb
@@ -4,7 +4,7 @@ package body Greeting_Banners is
 
   function Version return string is
 
-    res : constant string := "PHCv2.4.42 released 2017-08-13";
+    res : constant string := "PHCv2.4.43 released 2017-08-20";
 
   begin
     return res;
