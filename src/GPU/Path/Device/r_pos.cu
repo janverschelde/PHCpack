98c44e2c8f82676c0c7cd01cbe61eb970a6199ed
Jan Verschelde <janv@uic.edu>
2017-08-29 22:26:34 -0500

    added #ifndef statement to prevent multiple includes

--- src/GPU/Path/Device/r_pos.cu
+++ src/GPU/Path/Device/r_pos.cu
@@ -1,4 +1,11 @@
+// auxiliary function for modified Gram-Schmidt method
+
+#ifndef __PATH_GPU_MGS_R_POS_CU__
+#define __PATH_GPU_MGS_R_POS__CU__
+
 __device__ inline int r_pos ( int x, int y, int cols )
 {
    return cols*(cols+1)/2 -1 - (y*(y+1)/2 -(x-y));
 }
+
+#endif
