4af4f77bc69d22c7b5e9c2f64f16f7cb35bc73ea
Jan Verschelde <janv@uic.edu>
2017-09-21 01:44:35 -0500

    used system readers in the main driver for reduction

--- src/Ada/Deformations/Homotopy/mainred.adb
+++ src/Ada/Deformations/Homotopy/mainred.adb
@@ -3,37 +3,20 @@ with Standard_Natural_Numbers;           use Standard_Natural_Numbers;
 with Communications_with_User;           use Communications_with_User;
 with Standard_Complex_Poly_Systems;      use Standard_Complex_Poly_Systems;
 with Standard_Complex_Poly_Systems_io;   use Standard_Complex_Poly_Systems_io;
+with Standard_System_Readers;
 with Drivers_for_Reduction;              use Drivers_for_Reduction;
 
 procedure mainred ( infilename,outfilename : in string ) is
 
-  lp : Link_to_Poly_Sys;
-
-  procedure Read_System ( filename : in string ) is
-
-    file : file_type;
-
-  begin
-    if filename /= "" then
-      Open(file,in_file,filename);
-      get(file,lp);
-      Close(file);
-    end if;
-  exception
-    when others => 
-      new_line;
-      put("Could not open file with name "); put_line(filename);
-      lp := null; return;
-  end Read_System;
-
   procedure Main is
 
-    outfile : file_type;
+    lp : Link_to_Poly_Sys;
+    infile,outfile : file_type;
     d : natural32;
     ans : character;
 
   begin
-    Read_System(infilename);
+    Standard_System_Readers.Read_System(infile,infilename,lp);
     if lp = null
      then new_line; get(lp);
     end if;
