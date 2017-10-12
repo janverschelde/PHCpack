85932cf31cfdaa6d1edac5a705356f90b9a0b195
Jan Verschelde <janv@uic.edu>
2017-10-11 22:09:57 -0500

    provided code to define for a Factor procedure in the monodromy homotopies

--- src/Ada/Components/Factorization/monodromy_homotopies.adb
+++ src/Ada/Components/Factorization/monodromy_homotopies.adb
@@ -1,4 +1,10 @@
 with Standard_Integer_Numbers;          use Standard_Integer_Numbers;
+with Standard_Complex_VecVecs;
+with Witness_Sets;
+with Sampling_Machine;
+with Sample_Point_Lists;                use Sample_Point_Lists;
+with Rectangular_Sample_Grids;
+with Monodromy_Component_Breakup;
 
 package body Monodromy_Homotopies is
 
@@ -8,8 +14,19 @@ package body Monodromy_Homotopies is
                 pts : in Standard_Complex_Solutions.Solution_List;
                 dim,nbl : in natural32; tol : in double_float;
                 f : out Standard_Natural_VecVecs.Link_to_VecVec ) is
+
+    hyp : constant Standard_Complex_VecVecs.VecVec
+        := Witness_Sets.Slices(eqs,dim);
+    sps : constant Standard_Sample_List := Create(pts,hyp);
+    grid : Array_of_Standard_Sample_Lists(0..2);
+
   begin
-    null;
+    Sampling_Machine.Initialize(eqs);
+    Sampling_Machine.Default_Tune_Sampler(0);
+    Sampling_Machine.Default_Tune_Refiner;
+    grid := Rectangular_Sample_Grids.Create1(sps,2);
+    Monodromy_Component_Breakup.Factor(eqs,dim,grid,f);
+    Sampling_Machine.Clear;
   end Witness_Factor;
 
   procedure Witness_Factor
