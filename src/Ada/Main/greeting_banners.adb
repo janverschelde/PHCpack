bbfc860870bfaec6f4b0d596ddcb5a386c4f4af5
Jan Verschelde <janv@uic.edu>
2017-12-30 16:47:05 -0600

    updated the greeting banners from version 2.4.48

--- src/Ada/Main/greeting_banners.adb
+++ src/Ada/Main/greeting_banners.adb
@@ -4,7 +4,7 @@ package body Greeting_Banners is
 
   function Version return string is
 
-    res : constant string := "PHCv2.4.47 released 2017-10-17";
+    res : constant string := "PHCv2.4.48 released 2017-12-30";
 
   begin
     return res;
