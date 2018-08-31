940869968c2d219e6f11102b3075d5ce5e9c216b
Jan Verschelde <janv@uic.edu>
2018-08-31 09:55:59 -0500

    moved code from the black box root counters into a separate package

---
+++ src/Ada/Root_Counts/Dynlift/apply_induced_permutations.adb
@@ -0,0 +1,151 @@
+with Supports_of_Polynomial_Systems;
+with Induced_Permutations;
+with Black_Mixed_Volume_Computations;
+
+package body Apply_Induced_Permutations is
+
+  procedure Compute_Permutation
+              ( sup : in Arrays_of_Integer_Vector_Lists.Array_of_Lists;
+                r : in integer32;
+                perm,mtype : in Standard_Integer_Vectors.Link_to_Vector;
+                mcc : in Mixed_Subdivision;
+                iprm : out Standard_Integer_Vectors.Link_to_Vector ) is
+
+    mix : Standard_Integer_Vectors.Vector(1..r);
+
+    use Black_Mixed_Volume_Computations;
+
+  begin
+    for i in 1..r loop
+      mix(i) := mtype(i-1);
+    end loop;
+    Make_Induced_Permutation(sup,mix,mcc,iprm);
+  end Compute_Permutation;
+
+  procedure Apply_Induced_Permutation
+              ( p : in out Standard_Complex_Poly_Systems.Poly_Sys;
+                r : in integer32;
+                perm,mtype : in Standard_Integer_Vectors.Link_to_Vector;
+                mcc : in Mixed_Subdivision ) is
+
+    iprm : Standard_Integer_Vectors.Link_to_Vector;
+    sup : Arrays_of_Integer_Vector_Lists.Array_of_Lists(p'range);
+
+    use Standard_Integer_Vectors;
+
+  begin
+    if perm /= null then
+      sup := Supports_of_Polynomial_Systems.Create(p);
+      Compute_Permutation(sup,r,perm,mtype,mcc,iprm);
+      Induced_Permutations.Permute(iprm.all,p);
+      Standard_Integer_Vectors.Clear(iprm);
+      Arrays_of_Integer_Vector_Lists.Deep_Clear(sup);
+    end if;
+  end Apply_Induced_Permutation;
+
+  procedure Apply_Induced_Permutation
+              ( p : in out DoblDobl_Complex_Poly_Systems.Poly_Sys;
+                r : in integer32;
+                perm,mtype : in Standard_Integer_Vectors.Link_to_Vector;
+                mcc : in Mixed_Subdivision ) is
+
+    iprm : Standard_Integer_Vectors.Link_to_Vector;
+    sup : Arrays_of_Integer_Vector_Lists.Array_of_Lists(p'range);
+
+    use Standard_Integer_Vectors;
+
+  begin
+    if perm /= null then
+      sup := Supports_of_Polynomial_Systems.Create(p);
+      Compute_Permutation(sup,r,perm,mtype,mcc,iprm);
+      Induced_Permutations.Permute(iprm.all,p);
+      Standard_Integer_Vectors.Clear(iprm);
+      Arrays_of_Integer_Vector_Lists.Deep_Clear(sup);
+    end if;
+  end Apply_Induced_Permutation;
+
+  procedure Apply_Induced_Permutation
+              ( p : in out QuadDobl_Complex_Poly_Systems.Poly_Sys;
+                r : in integer32;
+                perm,mtype : in Standard_Integer_Vectors.Link_to_Vector;
+                mcc : in Mixed_Subdivision ) is
+
+    iprm : Standard_Integer_Vectors.Link_to_Vector;
+    sup : Arrays_of_Integer_Vector_Lists.Array_of_Lists(p'range);
+
+    use Standard_Integer_Vectors;
+
+  begin
+    if perm /= null then
+      sup := Supports_of_Polynomial_Systems.Create(p);
+      Compute_Permutation(sup,r,perm,mtype,mcc,iprm);
+      Induced_Permutations.Permute(iprm.all,p);
+      Standard_Integer_Vectors.Clear(iprm);
+      Arrays_of_Integer_Vector_Lists.Deep_Clear(sup);
+    end if;
+  end Apply_Induced_Permutation;
+
+  procedure Apply_Induced_Permutation
+              ( p : in out Standard_Complex_Laur_Systems.Laur_Sys;
+                r : in integer32;
+                perm,mtype : in Standard_Integer_Vectors.Link_to_Vector;
+                mcc : in Mixed_Subdivision ) is
+
+    iprm : Standard_Integer_Vectors.Link_to_Vector;
+    sup : Arrays_of_Integer_Vector_Lists.Array_of_Lists(p'range);
+
+    use Standard_Integer_Vectors;
+
+  begin
+    if perm /= null then
+      sup := Supports_of_Polynomial_Systems.Create(p);
+      Compute_Permutation(sup,r,perm,mtype,mcc,iprm);
+      Induced_Permutations.Permute(iprm.all,p);
+      Standard_Integer_Vectors.Clear(iprm);
+      Arrays_of_Integer_Vector_Lists.Deep_Clear(sup);
+    end if;
+  end Apply_Induced_Permutation;
+
+  procedure Apply_Induced_Permutation
+              ( p : in out DoblDobl_Complex_Laur_Systems.Laur_Sys;
+                r : in integer32;
+                perm,mtype : in Standard_Integer_Vectors.Link_to_Vector;
+                mcc : in Mixed_Subdivision ) is
+
+    iprm : Standard_Integer_Vectors.Link_to_Vector;
+    sup : Arrays_of_Integer_Vector_Lists.Array_of_Lists(p'range);
+
+    use Standard_Integer_Vectors;
+
+  begin
+    if perm /= null then
+      sup := Supports_of_Polynomial_Systems.Create(p);
+      Compute_Permutation(sup,r,perm,mtype,mcc,iprm);
+      Induced_Permutations.Permute(iprm.all,p);
+      Standard_Integer_Vectors.Clear(iprm);
+      Arrays_of_Integer_Vector_Lists.Deep_Clear(sup);
+    end if;
+  end Apply_Induced_Permutation;
+
+  procedure Apply_Induced_Permutation
+              ( p : in out QuadDobl_Complex_Laur_Systems.Laur_Sys;
+                r : in integer32;
+                perm,mtype : in Standard_Integer_Vectors.Link_to_Vector;
+                mcc : in Mixed_Subdivision ) is
+
+    iprm : Standard_Integer_Vectors.Link_to_Vector;
+    sup : Arrays_of_Integer_Vector_Lists.Array_of_Lists(p'range);
+
+    use Standard_Integer_Vectors;
+
+  begin
+    if perm /= null then
+      sup := Supports_of_Polynomial_Systems.Create(p);
+      Compute_Permutation(sup,r,perm,mtype,mcc,iprm);
+      Induced_Permutations.Permute(iprm.all,p);
+      Standard_Integer_Vectors.Clear(iprm);
+      Arrays_of_Integer_Vector_Lists.Deep_Clear(sup);
+    end if;
+  end Apply_Induced_Permutation;
+
+end Apply_Induced_Permutations;
---
+++ src/Ada/Root_Counts/Dynlift/apply_induced_permutations.ads
@@ -0,0 +1,78 @@
+with Standard_Integer_Numbers;           use Standard_Integer_Numbers;
+with Standard_Integer_Vectors;
+with Arrays_of_Integer_Vector_Lists;
+with Standard_Complex_Poly_Systems;
+with Standard_Complex_Laur_Systems;
+with DoblDobl_Complex_Poly_Systems;
+with DoblDobl_Complex_Laur_Systems;
+with QuadDobl_Complex_Poly_Systems;
+with QuadDobl_Complex_Laur_Systems;
+with Floating_Mixed_Subdivisions;        use Floating_Mixed_Subdivisions;
+
+package Apply_Induced_Permutations is
+
+  procedure Compute_Permutation
+              ( sup : in Arrays_of_Integer_Vector_Lists.Array_of_Lists;
+                r : in integer32;
+                perm,mtype : in Standard_Integer_Vectors.Link_to_Vector;
+                mcc : in Mixed_Subdivision;
+                iprm : out Standard_Integer_Vectors.Link_to_Vector );
+
+  -- DESCRIPTION :
+  --   Computes the induced permutation for p.
+
+  -- ON ENTRY :
+  --   sup      supports of a polynomial system;
+  --   r        number of distinct supports;
+  --   perm     permutation of the supports;
+  --   mtype    type of mixture, computed by MixedVol;
+  --   mcc      a regular mixed-cell configuration.
+ 
+  -- ON RETURN :
+  --   p        permuted polynomials.
+
+  procedure Apply_Induced_Permutation
+              ( p : in out Standard_Complex_Poly_Systems.Poly_Sys;
+                r : in integer32;
+                perm,mtype : in Standard_Integer_Vectors.Link_to_Vector;
+                mcc : in Mixed_Subdivision );
+  procedure Apply_Induced_Permutation
+              ( p : in out DoblDobl_Complex_Poly_Systems.Poly_Sys;
+                r : in integer32;
+                perm,mtype : in Standard_Integer_Vectors.Link_to_Vector;
+                mcc : in Mixed_Subdivision );
+  procedure Apply_Induced_Permutation
+              ( p : in out QuadDobl_Complex_Poly_Systems.Poly_Sys;
+                r : in integer32;
+                perm,mtype : in Standard_Integer_Vectors.Link_to_Vector;
+                mcc : in Mixed_Subdivision );
+  procedure Apply_Induced_Permutation
+              ( p : in out Standard_Complex_Laur_Systems.Laur_Sys;
+                r : in integer32;
+                perm,mtype : in Standard_Integer_Vectors.Link_to_Vector;
+                mcc : in Mixed_Subdivision );
+  procedure Apply_Induced_Permutation
+              ( p : in out DoblDobl_Complex_Laur_Systems.Laur_Sys;
+                r : in integer32;
+                perm,mtype : in Standard_Integer_Vectors.Link_to_Vector;
+                mcc : in Mixed_Subdivision );
+  procedure Apply_Induced_Permutation
+              ( p : in out QuadDobl_Complex_Laur_Systems.Laur_Sys;
+                r : in integer32;
+                perm,mtype : in Standard_Integer_Vectors.Link_to_Vector;
+                mcc : in Mixed_Subdivision );
+
+  -- DESCRIPTION :
+  --   Computes the induced permutation and applies it to p.
+
+  -- ON ENTRY :
+  --   p        a polynomial system;
+  --   r        number of distinct supports;
+  --   perm     permutation of the supports;
+  --   mtype    type of mixture, computed by MixedVol;
+  --   mcc      a regular mixed-cell configuration.
+ 
+  -- ON RETURN :
+  --   p        permuted polynomials.
+
+end Apply_Induced_Permutations;
