5ee34fc0eb87cb07fd7e150f98cee35a415a401d
Jan Verschelde <janv@uic.edu>
2018-12-03 22:09:46 -0600

    extended the C test function for Pade continuation

--- src/Lib/lib_padcon.c
+++ src/Lib/lib_padcon.c
@@ -1,6 +1,9 @@
 /* simple test on running a Pade continuation */
 
 #include <stdio.h>
+#include <string.h>
+#include "solcon.h"
+#include "phcpack.h"
 #include "padcon.h"
 
 void write_homotopy_continuation_parameters ( void );
@@ -13,14 +16,66 @@ void tune_homotopy_continuation_parameters ( void );
  * DESCRIPTION :
  *   Interactive loop to tune the homotopy continuation parameters. */
 
+void prompt_for_output_file ( int* nbc, char* name );
+/*
+ * DESCRIPTION :
+ *   Prompts the user for an output file, returned in name,
+ *   with in nbc the number of characters in name. */
+
+void standard_track ( int nbc, char* name );
+/*
+ * DESCRIPTION :
+ *   Tracks in standard double precision.  On input is the name of the
+ *   output file, with its number of characters in nbc.
+ *   If nbc = 0, then no output is written to file,
+ *   otherwise, the output is written to the name with file name. */
+
+void dobldobl_track ( int nbc, char* name );
+/*
+ * DESCRIPTION :
+ *   Tracks in double double precision.  On input is the name of the
+ *   output file, with its number of characters in nbc.
+ *   If nbc = 0, then no output is written to file,
+ *   otherwise, the output is written to the name with file name. */
+
+void quaddobl_track ( int nbc, char* name );
+/*
+ * DESCRIPTION :
+ *   Tracks in quad double precision.  On input is the name of the
+ *   output file, with its number of characters in nbc.
+ *   If nbc = 0, then no output is written to file,
+ *   otherwise, the output is written to the name with file name. */
+
 int main ( void )
 {
    adainit();
 
-   printf("Tuning the homotopy continuation parameters ...\n");
+   char nlsb;
+   int precision,nbchar;
+   char filename[80];
+
+   printf("\nMENU for the precision :\n");
+   printf("  0. double precision\n");
+   printf("  1. double double precision\n");
+   printf("  2. quad double precision\n");
+   printf("Type 0, 1, or 2 to select the precision : ");
+   scanf("%d", &precision);
+
+   printf("\nTuning the homotopy continuation parameters ...\n");
 
    padcon_set_default_parameters();
    tune_homotopy_continuation_parameters();
+
+   scanf("%c", &nlsb); // swallow new line symbol
+
+   prompt_for_output_file(&nbchar,filename);
+
+   if(nbchar > 0)
+      printf("\nThe name of the output file is %s.\n", filename);
+
+   if(precision == 0) standard_track(nbchar,filename);
+   if(precision == 1) dobldobl_track(nbchar,filename);
+   if(precision == 2) quaddobl_track(nbchar,filename);
 
    adafinal();
 
@@ -152,4 +207,74 @@ void tune_homotopy_continuation_parameters ( void )
       }
    }
    while (choice != 0);
+}
+
+void prompt_for_output_file ( int* nbc, char* name )
+{
+   char ans,nlsb;
+
+   printf("\nOutput to separate file ? (y/n) ? ");
+   scanf("%c",&ans);
+   scanf("%c",&nlsb); /* skip newline symbol */
+
+   if(ans != 'y')
+      *nbc = 0;
+   else
+   {
+      printf("-> give the name of the output file : ");
+      scanf("%s",name);
+      scanf("%c",&nlsb); /* skip newline symbol */
+      *nbc = strlen(name);
+   }
+}
+
+void standard_track ( int nbc, char* name )
+{
+   int fail,length;
+
+   fail = read_standard_target_system();
+   fail = read_standard_start_system();
+   fail = copy_start_solutions_to_container();
+   fail = solcon_number_of_standard_solutions(&length);
+   printf("Read %d start solutions.\n", length);
+
+   if(nbc > 0) printf("\nSee the output file %s ...\n", name);
+
+   fail = padcon_standard_track(nbc,name);
+
+   if(nbc == 0) fail = solcon_write_standard_solutions();
+}
+
+void dobldobl_track ( int nbc, char* name )
+{
+   int fail,length;
+
+   fail = read_dobldobl_target_system();
+   fail = read_dobldobl_start_system();
+   fail = copy_dobldobl_start_solutions_to_container();
+   fail = solcon_number_of_dobldobl_solutions(&length);
+   printf("Read %d start solutions.\n", length);
+
+   if(nbc > 0) printf("\nSee the output file %s ...\n", name);
+
+   fail = padcon_dobldobl_track(nbc,name);
+
+   if(nbc == 0) fail = solcon_write_dobldobl_solutions();
+}
+
+void quaddobl_track ( int nbc, char* name )
+{
+   int fail,length;
+
+   fail = read_quaddobl_target_system();
+   fail = read_quaddobl_start_system();
+   fail = copy_quaddobl_start_solutions_to_container();
+   fail = solcon_number_of_quaddobl_solutions(&length);
+   printf("Read %d start solutions.\n", length);
+
+   if(nbc > 0) printf("\nSee the output file %s ...\n", name);
+
+   fail = padcon_quaddobl_track(nbc,name);
+
+   if(nbc == 0) fail = solcon_write_quaddobl_solutions();
 }
