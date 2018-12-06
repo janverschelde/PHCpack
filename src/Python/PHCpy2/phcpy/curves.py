6ac104a96b7ccac31c8816c1e6a02a020a258d19
Jan Verschelde <janv@uic.edu>
2018-12-05 21:39:03 -0600

    wrapped the path trackers for Pade continuation in the new module curves.py

--- src/Python/PHCpy2/phcpy/curves.py
+++ src/Python/PHCpy2/phcpy/curves.py
@@ -72,13 +72,154 @@ def tune_homotopy_continuation_parameters():
                 val = float(raw_input(valprompt))
                 set_homotopy_continuation_parameter(idx, val);
 
-def test():
+def standard_track(target, start, sols, filename="", verbose=False):
     """
-    Returns the current values of the homotopy continuation parameters.
+    Wraps the tracker for Pade continuation in standard double precision.
+    On input are a target system, a start system with solutions,
+    optionally: a string *filename* and the *verbose* flag.
+    The *target* is a list of strings representing the polynomials
+    of the target system (which has to be solved).
+    The *start* is a list of strings representing the polynomials
+    of the start system, with known solutions in *sols*.
+    The *sols* is a list of strings representing start solutions.
+    On return are the string representations of the solutions
+    computed at the end of the paths.
+    """
+    from phcpy.phcpy2c2 import py2c_copy_standard_container_to_target_system
+    from phcpy.phcpy2c2 import py2c_copy_standard_container_to_start_system
+    from phcpy.phcpy2c2 import py2c_copy_standard_container_to_start_solutions
+    from phcpy.phcpy2c2 import py2c_create_standard_homotopy
+    from phcpy.phcpy2c2 import py2c_create_standard_homotopy_with_gamma
+    from phcpy.phcpy2c2 import py2c_solve_by_standard_homotopy_continuation
+    from phcpy.phcpy2c2 import py2c_clear_standard_homotopy
+    from phcpy.phcpy2c2 import py2c_clear_standard_operations_data
+    from phcpy.interface import store_standard_system
+    from phcpy.interface import store_standard_solutions
+    from phcpy.interface import load_standard_solutions
+    from phcpy.solver import number_of_symbols
+    from phcpy.phcpy2c2 import py2c_padcon_standard_track
+    dim = number_of_symbols(start)
+    store_standard_system(target, nbvar=dim)
+    py2c_copy_standard_container_to_target_system()
+    store_standard_system(start, nbvar=dim)
+    py2c_copy_standard_container_to_start_system()
+    (regamma, imgamma) = get_homotopy_continuation_parameter(1)
+    py2c_create_standard_homotopy_with_gamma(regamma, imgamma)
+    store_standard_solutions(dim, sols)
+    py2c_copy_standard_container_to_start_solutions()
+    nbc = len(filename)
+    fail = py2c_padcon_standard_track(nbc,filename,int(verbose))
+    # py2c_clear_standard_homotopy()
+    # py2c_clear_standard_operations_data()
+    return load_standard_solutions()
+
+def dobldobl_track(target, start, sols, filename="", verbose=False):
+    """
+    Wraps the tracker for Pade continuation in double double precision.
+    On input are a target system, a start system with solutions,
+    optionally: a string *filename* and the *verbose* flag.
+    The *target* is a list of strings representing the polynomials
+    of the target system (which has to be solved).
+    The *start* is a list of strings representing the polynomials
+    of the start system, with known solutions in *sols*.
+    The *sols* is a list of strings representing start solutions.
+    On return are the string representations of the solutions
+    computed at the end of the paths.
+    """
+    from phcpy.phcpy2c2 import py2c_copy_dobldobl_container_to_target_system
+    from phcpy.phcpy2c2 import py2c_copy_dobldobl_container_to_start_system
+    from phcpy.phcpy2c2 import py2c_copy_dobldobl_container_to_start_solutions
+    from phcpy.phcpy2c2 import py2c_create_dobldobl_homotopy
+    from phcpy.phcpy2c2 import py2c_create_dobldobl_homotopy_with_gamma
+    from phcpy.phcpy2c2 import py2c_solve_by_dobldobl_homotopy_continuation
+    from phcpy.phcpy2c2 import py2c_clear_dobldobl_homotopy
+    from phcpy.phcpy2c2 import py2c_clear_dobldobl_operations_data
+    from phcpy.interface import store_dobldobl_system
+    from phcpy.interface import store_dobldobl_solutions
+    from phcpy.interface import load_dobldobl_solutions
+    from phcpy.solver import number_of_symbols
+    from phcpy.phcpy2c2 import py2c_padcon_dobldobl_track
+    dim = number_of_symbols(start)
+    store_dobldobl_system(target, nbvar=dim)
+    py2c_copy_dobldobl_container_to_target_system()
+    store_dobldobl_system(start, nbvar=dim)
+    py2c_copy_dobldobl_container_to_start_system()
+    (regamma, imgamma) = get_homotopy_continuation_parameter(1)
+    py2c_create_dobldobl_homotopy_with_gamma(regamma, imgamma)
+    store_dobldobl_solutions(dim, sols)
+    py2c_copy_dobldobl_container_to_start_solutions()
+    nbc = len(filename)
+    fail = py2c_padcon_dobldobl_track(nbc,filename,int(verbose))
+    # py2c_clear_dobldobl_homotopy()
+    # py2c_clear_dobldobl_operations_data()
+    return load_dobldobl_solutions()
+
+def quaddobl_track(target, start, sols, filename="", verbose=False):
+    """
+    Wraps the tracker for Pade continuation in quad double precision.
+    On input are a target system, a start system with solutions,
+    optionally: a string *filename* and the *verbose* flag.
+    The *target* is a list of strings representing the polynomials
+    of the target system (which has to be solved).
+    The *start* is a list of strings representing the polynomials
+    of the start system, with known solutions in *sols*.
+    The *sols* is a list of strings representing start solutions.
+    On return are the string representations of the solutions
+    computed at the end of the paths.
+    """
+    from phcpy.phcpy2c2 import py2c_copy_quaddobl_container_to_target_system
+    from phcpy.phcpy2c2 import py2c_copy_quaddobl_container_to_start_system
+    from phcpy.phcpy2c2 import py2c_copy_quaddobl_container_to_start_solutions
+    from phcpy.phcpy2c2 import py2c_create_quaddobl_homotopy
+    from phcpy.phcpy2c2 import py2c_create_quaddobl_homotopy_with_gamma
+    from phcpy.phcpy2c2 import py2c_solve_by_quaddobl_homotopy_continuation
+    from phcpy.phcpy2c2 import py2c_clear_quaddobl_homotopy
+    from phcpy.phcpy2c2 import py2c_clear_quaddobl_operations_data
+    from phcpy.interface import store_quaddobl_system
+    from phcpy.interface import store_quaddobl_solutions
+    from phcpy.interface import load_quaddobl_solutions
+    from phcpy.solver import number_of_symbols
+    from phcpy.phcpy2c2 import py2c_padcon_quaddobl_track
+    dim = number_of_symbols(start)
+    store_quaddobl_system(target, nbvar=dim)
+    py2c_copy_quaddobl_container_to_target_system()
+    store_quaddobl_system(start, nbvar=dim)
+    py2c_copy_quaddobl_container_to_start_system()
+    (regamma, imgamma) = get_homotopy_continuation_parameter(1)
+    py2c_create_quaddobl_homotopy_with_gamma(regamma, imgamma)
+    store_quaddobl_solutions(dim, sols)
+    py2c_copy_quaddobl_container_to_start_solutions()
+    nbc = len(filename)
+    fail = py2c_padcon_quaddobl_track(nbc,filename,int(verbose))
+    # py2c_clear_quaddobl_homotopy()
+    # py2c_clear_quaddobl_operations_data()
+    return load_quaddobl_solutions()
+
+def test(precision='d'):
+    """
+    Tunes the parameters and runs a simple test on the trackers.
+    The precision is either double ('d'), double double ('dd'),
+    or quad double ('qd').
     """
     pars = [get_homotopy_continuation_parameter(k) for k in range(1, 13)]
     print pars
     tune_homotopy_continuation_parameters()
+    from phcpy.families import katsura
+    k3 = katsura(3)
+    from phcpy.solver import total_degree_start_system as tdss
+    (k3q, k3qsols) = tdss(k3)
+    print 'tracking', len(k3qsols), 'paths ...'
+    if(precision == 'd'):
+        k3sols = standard_track(k3, k3q, k3qsols,"",True)
+    elif(precision == 'dd'):
+        k3sols = dobldobl_track(k3, k3q, k3qsols,"",True)
+    elif(precision == 'qd'):
+        k3sols = quaddobl_track(k3, k3q, k3qsols,"",True)
+    else:
+        print 'wrong precision'
+    for sol in k3sols:
+        print sol
 
 if __name__ == "__main__":
-    test()
+    test('dd')
+    # test('qd')
