4bd9d9d2ef87afa8c44d153f43ed22c20218be89
Jan Verschelde <janv@uic.edu>
2018-12-30 18:45:14 -0600

    updated test procedure to write start system before the solutions corresponding to failed paths

--- src/Ada/Deformations/Solutions/ts_trasols_io.adb
+++ src/Ada/Deformations/Solutions/ts_trasols_io.adb
@@ -4,17 +4,23 @@ with Standard_Natural_Numbers;           use Standard_Natural_Numbers;
 with Standard_Natural_Numbers_io;        use Standard_Natural_Numbers_io;
 with Standard_Floating_Numbers;          use Standard_Floating_Numbers;
 with Standard_Complex_Poly_Systems;
+with Standard_Complex_Poly_Systems_io;   use Standard_Complex_Poly_Systems_io;
 with Standard_Complex_Laur_Systems;
+with Standard_Complex_Laur_Systems_io;   use Standard_Complex_Laur_Systems_io;
 with Standard_Complex_Solutions;
 with Standard_Complex_Solutions_io;      use Standard_Complex_Solutions_io;
 with Standard_Solution_Filters;          use Standard_Solution_Filters;
 with DoblDobl_Complex_Poly_Systems;
+with DoblDobl_Complex_Poly_Systems_io;   use DoblDobl_Complex_Poly_Systems_io;
 with DoblDobl_Complex_Laur_Systems;
+with DoblDobl_Complex_Laur_Systems_io;   use DoblDobl_Complex_Laur_Systems_io;
 with DoblDobl_Complex_Solutions;
 with DoblDobl_Complex_Solutions_io;      use DoblDobl_Complex_Solutions_io;
 with DoblDobl_Solution_Filters;          use DoblDobl_Solution_Filters;
 with QuadDobl_Complex_Poly_Systems;
+with QuadDobl_Complex_Poly_Systems_io;   use QuadDobl_Complex_Poly_Systems_io;
 with QuadDobl_Complex_Laur_Systems;
+with QuadDobl_Complex_Laur_Systems_io;   use QuadDobl_Complex_Laur_Systems_io;
 with QuadDobl_Complex_Solutions;
 with QuadDobl_Complex_Solutions_io;      use QuadDobl_Complex_Solutions_io;
 with QuadDobl_Solution_Filters;          use QuadDobl_Solution_Filters;
@@ -27,20 +33,19 @@ procedure ts_trasols_io is
 -- DESCRIPTION :
 --   Development of processing of the output file of a path tracker.
 
-  procedure Write_to_File
-              ( sols : in Standard_Complex_Solutions.Solution_List ) is
+  procedure Prompt_for_File
+              ( file : in out file_type; len : in natural32 ) is
 
   -- DESCRIPTION :
-  --   Prompts the user for a file name to write the solutions on.
+  --   Given the length of the solution list in len,
+  --   prompts the user for a file name if len > 0
+  --   and the created file object is returned in file.
 
     ans : character;
-    file : file_type;
  
   begin
-    put("Selected ");
-    put(Standard_Complex_Solutions.Length_Of(sols),1);
-    put_line(" failed solutions.");
-    if not Standard_Complex_Solutions.Is_Null(sols) then
+    put("Selected "); put(len,1); put_line(" failed solutions.");
+    if len > 0 then
       new_line;
       put("Write start solutions corresponding to failed paths ");
       put("to file ? (y/n) ");
@@ -49,69 +54,183 @@ procedure ts_trasols_io is
         new_line;
         put_line("Reading the name of an output file ...");
         Read_Name_and_Create_File(file);
-        put(file,Standard_Complex_Solutions.Length_Of(sols),
-            natural32(Standard_Complex_Solutions.Head_Of(sols).n),sols);
-        close(file);
       end if;
     end if;
+  end Prompt_for_File;
+
+  procedure Write_to_File
+              ( p : in Standard_Complex_Poly_Systems.Poly_Sys;
+                sols : in Standard_Complex_Solutions.Solution_List ) is
+
+  -- DESCRIPTION :
+  --   If the solution list sols is not empty,
+  --   then the user is prompted for a file name.
+  --   Prompts the user for a file name to write the solutions on.
+  --   If the length of sols is positive,
+  --   then the system p and the solutions in sols are written to file.
+
+  -- ON ENTRY :
+  --   p        a start system used in a homotopy.
+  --   sols     solutions of p which correspond to failed paths.
+
+    file : file_type;
+    len : constant natural32 := Standard_Complex_Solutions.Length_Of(sols);
+ 
+  begin
+    Prompt_for_File(file,len);
+    put(file,natural32(p'last),p);
+    new_line(file);
+    put_line(file,
+      "TITLE : start system with solutions corresponding to failed paths");
+    new_line(file);
+    put_line(file,"THE SOLUTIONS :");
+    put(file,len,natural32(Standard_Complex_Solutions.Head_Of(sols).n),sols);
+    close(file);
   end Write_to_File;
 
   procedure Write_to_File
-              ( sols : in DoblDobl_Complex_Solutions.Solution_List ) is
+              ( p : in Standard_Complex_Laur_Systems.Laur_Sys;
+                sols : in Standard_Complex_Solutions.Solution_List ) is
 
   -- DESCRIPTION :
+  --   If the solution list sols is not empty,
+  --   then the user is prompted for a file name.
   --   Prompts the user for a file name to write the solutions on.
+  --   If the length of sols is positive,
+  --   then the system p and the solutions in sols are written to file.
 
-    ans : character;
+  -- ON ENTRY :
+  --   p        a start system used in a homotopy.
+  --   sols     solutions of p which correspond to failed paths.
+
     file : file_type;
+    len : constant natural32 := Standard_Complex_Solutions.Length_Of(sols);
  
   begin
-    put("Selected ");
-    put(DoblDobl_Complex_Solutions.Length_Of(sols),1);
-    put_line(" failed solutions.");
-    if not DoblDobl_Complex_Solutions.Is_Null(sols) then
-      new_line;
-      put("Write start solutions corresponding to failed paths ");
-      put("to file ? (y/n) ");
-      Ask_Yes_or_No(ans);
-      if ans = 'y' then
-        new_line;
-        put_line("Reading the name of an output file ...");
-        Read_Name_and_Create_File(file);
-        put(file,DoblDobl_Complex_Solutions.Length_Of(sols),
-            natural32(DoblDobl_Complex_Solutions.Head_Of(sols).n),sols);
-        close(file);
-      end if;
-    end if;
+    Prompt_for_File(file,len);
+    put(file,natural32(p'last),p);
+    new_line(file);
+    put_line(file,
+      "TITLE : start system with solutions corresponding to failed paths");
+    new_line(file);
+    put(file,len,natural32(Standard_Complex_Solutions.Head_Of(sols).n),sols);
+    close(file);
   end Write_to_File;
 
   procedure Write_to_File
-              ( sols : in QuadDobl_Complex_Solutions.Solution_List ) is
+              ( p : in DoblDobl_Complex_Poly_Systems.Poly_Sys;
+                sols : in DoblDobl_Complex_Solutions.Solution_List ) is
 
   -- DESCRIPTION :
+  --   If the solution list sols is not empty,
+  --   then the user is prompted for a file name.
   --   Prompts the user for a file name to write the solutions on.
+  --   If the length of sols is positive,
+  --   then the system p and the solutions in sols are written to file.
 
-    ans : character;
+  -- ON ENTRY :
+  --   p        a start system used in a homotopy.
+  --   sols     solutions of p which correspond to failed paths.
+
     file : file_type;
+    len : constant natural32 := DoblDobl_Complex_Solutions.Length_Of(sols);
+
+  begin
+    Prompt_for_File(file,len);
+    put(file,natural32(p'last),p);
+    new_line(file);
+    put_line(file,
+      "TITLE : start system with solutions corresponding to failed paths");
+    new_line(file);
+    put(file,len,natural32(DoblDobl_Complex_Solutions.Head_Of(sols).n),sols);
+    close(file);
+  end Write_to_File;
+
+  procedure Write_to_File
+              ( p : in DoblDobl_Complex_Laur_Systems.Laur_Sys;
+                sols : in DoblDobl_Complex_Solutions.Solution_List ) is
+
+  -- DESCRIPTION :
+  --   If the solution list sols is not empty,
+  --   then the user is prompted for a file name.
+  --   Prompts the user for a file name to write the solutions on.
+  --   If the length of sols is positive,
+  --   then the system p and the solutions in sols are written to file.
+
+  -- ON ENTRY :
+  --   p        a start system used in a homotopy.
+  --   sols     solutions of p which correspond to failed paths.
+
+    file : file_type;
+    len : constant natural32 := DoblDobl_Complex_Solutions.Length_Of(sols);
+
+  begin
+    Prompt_for_File(file,len);
+    put(file,natural32(p'last),p);
+    new_line(file);
+    put_line(file,
+      "TITLE : start system with solutions corresponding to failed paths");
+    new_line(file);
+    put(file,len,natural32(DoblDobl_Complex_Solutions.Head_Of(sols).n),sols);
+    close(file);
+  end Write_to_File;
+
+  procedure Write_to_File
+              ( p : in QuadDobl_Complex_Poly_Systems.Poly_Sys;
+                sols : in QuadDobl_Complex_Solutions.Solution_List ) is
+
+  -- DESCRIPTION :
+  --   If the solution list sols is not empty,
+  --   then the user is prompted for a file name.
+  --   Prompts the user for a file name to write the solutions on.
+  --   If the length of sols is positive,
+  --   then the system p and the solutions in sols are written to file.
+
+  -- ON ENTRY :
+  --   p        a start system used in a homotopy.
+  --   sols     solutions of p which correspond to failed paths.
+
+    file : file_type;
+    len : constant natural32 := QuadDobl_Complex_Solutions.Length_Of(sols);
  
   begin
-    put("Selected ");
-    put(QuadDobl_Complex_Solutions.Length_Of(sols),1);
-    put_line(" failed solutions.");
-    if not QuadDobl_Complex_Solutions.Is_Null(sols) then
-      new_line;
-      put("Write start solutions corresponding to failed paths ");
-      put("to file ? (y/n) ");
-      Ask_Yes_or_No(ans);
-      if ans = 'y' then
-        new_line;
-        put_line("Reading the name of an output file ...");
-        Read_Name_and_Create_File(file);
-        put(file,QuadDobl_Complex_Solutions.Length_Of(sols),
-            natural32(QuadDobl_Complex_Solutions.Head_Of(sols).n),sols);
-        close(file);
-      end if;
-    end if;
+    Prompt_for_File(file,len);
+    put(file,natural32(p'last),p);
+    new_line(file);
+    put_line(file,
+      "TITLE : start system with solutions corresponding to failed paths");
+    new_line(file);
+    put(file,len,natural32(QuadDobl_Complex_Solutions.Head_Of(sols).n),sols);
+    close(file);
+  end Write_to_File;
+
+  procedure Write_to_File
+              ( p : in QuadDobl_Complex_Laur_Systems.Laur_Sys;
+                sols : in QuadDobl_Complex_Solutions.Solution_List ) is
+
+  -- DESCRIPTION :
+  --   If the solution list sols is not empty,
+  --   then the user is prompted for a file name.
+  --   Prompts the user for a file name to write the solutions on.
+  --   If the length of sols is positive,
+  --   then the system p and the solutions in sols are written to file.
+
+  -- ON ENTRY :
+  --   p        a start system used in a homotopy.
+  --   sols     solutions of p which correspond to failed paths.
+
+    file : file_type;
+    len : constant natural32 := QuadDobl_Complex_Solutions.Length_Of(sols);
+ 
+  begin
+    Prompt_for_File(file,len);
+    put(file,natural32(p'last),p);
+    new_line(file);
+    put_line(file,
+      "TITLE : start system with solutions corresponding to failed paths");
+    new_line(file);
+    put(file,len,natural32(QuadDobl_Complex_Solutions.Head_Of(sols).n),sols);
+    close(file);
   end Write_to_File;
 
   procedure Standard_Poly_Read
@@ -131,7 +250,7 @@ procedure ts_trasols_io is
   begin
     Standard_Tracked_Solutions_io.get(file,lp,lq,psols,qsols,verbose);
     failed := Select_Failed_Solutions(psols,qsols,tol,verbose);
-    Write_to_File(failed);
+    Write_to_File(lq.all,failed);
   end Standard_Poly_Read;
 
   procedure Standard_Laur_Read
@@ -151,7 +270,7 @@ procedure ts_trasols_io is
   begin
     Standard_Tracked_Solutions_io.get(file,lp,lq,psols,qsols,verbose);
     failed := Select_Failed_Solutions(psols,qsols,tol,verbose);
-    Write_to_File(failed);
+    Write_to_File(lq.all,failed);
   end Standard_Laur_Read;
 
   procedure DoblDobl_Poly_Read
@@ -171,7 +290,7 @@ procedure ts_trasols_io is
   begin
     DoblDobl_Tracked_Solutions_io.get(file,lp,lq,psols,qsols,verbose);
     failed := Select_Failed_Solutions(psols,qsols,tol,verbose);
-    Write_to_File(failed);
+    Write_to_File(lq.all,failed);
   end DoblDobl_Poly_Read;
 
   procedure DoblDobl_Laur_Read
@@ -191,7 +310,7 @@ procedure ts_trasols_io is
   begin
     DoblDobl_Tracked_Solutions_io.get(file,lp,lq,psols,qsols,verbose);
     failed := Select_Failed_Solutions(psols,qsols,tol,verbose);
-    Write_to_File(failed);
+    Write_to_File(lq.all,failed);
   end DoblDobl_Laur_Read;
 
   procedure QuadDobl_Poly_Read
@@ -211,7 +330,7 @@ procedure ts_trasols_io is
   begin
     QuadDobl_Tracked_Solutions_io.get(file,lp,lq,psols,qsols,verbose);
     failed := Select_Failed_Solutions(psols,qsols,tol,verbose);
-    Write_to_File(failed);
+    Write_to_File(lq.all,failed);
   end QuadDobl_Poly_Read;
 
   procedure QuadDobl_Laur_Read
@@ -231,7 +350,7 @@ procedure ts_trasols_io is
   begin
     QuadDobl_Tracked_Solutions_io.get(file,lp,lq,psols,qsols,verbose);
     failed := Select_Failed_Solutions(psols,qsols,tol,verbose);
-    Write_to_File(failed);
+    Write_to_File(lq.all,failed);
   end QuadDobl_Laur_Read;
 
   procedure Main is
@@ -248,7 +367,7 @@ procedure ts_trasols_io is
     put_line("  0. standard double precision");
     put_line("  1. double double precision");
     put_line("  2. quad double precision");
-    put("Type 0, 1, or 2 to select the precision :");
+    put("Type 0, 1, or 2 to select the precision : ");
     Ask_Alternative(prc,"012");
     new_line;
     put_line("Reading the name of a file with output of a tracker ...");
