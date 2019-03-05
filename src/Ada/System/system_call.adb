a991abd7941138c822e8eda38db692ee943bfe7a
Jan Verschelde <janv@uic.edu>
2019-03-05 10:37:54 -0600

    dealt with obsolescent feature, replaced Interface by Import

--- src/Ada/System/system_call.adb
+++ src/Ada/System/system_call.adb
@@ -1,11 +1,12 @@
-with System;                   use System;
+with System;                             use System;
 
 package body System_Call is
 
   procedure Call ( Command: in string ) is
 
     function system(command: address) return natural;
-    pragma Interface(C, system);
+   -- pragma Interface(C, system); -- obsolescent feature
+    pragma Import(C, system);
     cmd: constant string := command & ASCII.NUL;
     ret: Natural;
 
