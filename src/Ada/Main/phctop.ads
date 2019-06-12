f32d7d52fd9b3d276efd391a03918e9f21c44190
Jan Verschelde <janv@uic.edu>
2019-06-12 17:38:28 -0500

    new top level procedure for the main executable phc

---
+++ src/Ada/Main/phctop.ads
@@ -0,0 +1,6 @@
+procedure phctop;
+
+-- DESCRIPTION :
+--   This procedure processes the command line arguments
+--   and calls the proper procedures corresponding to the options.
+--   This is the top level procedure for the executable phc.
