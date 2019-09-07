e9eae0f0ba88db7357a09d2729bb2d2fa0d0a22f
Jan Verschelde <janv@uic.edu>
2019-09-06 22:39:28 -0500

    added the name of the output file to the blackbox mode of the equation-by-equation solver

--- src/Ada/Components/Solver/bablsolve.adb
+++ src/Ada/Components/Solver/bablsolve.adb
@@ -3,7 +3,8 @@ with String_Splitters;                   use String_Splitters;
 with Communications_with_User;           use Communications_with_User;
 with Drivers_to_Eqn_by_Eqn_Solvers;      use Drivers_to_Eqn_by_Eqn_Solvers;
 
-procedure bablsolve ( p : in Poly_Sys; verbose : in integer32 := 0 ) is
+procedure bablsolve ( p : in Poly_Sys; outname : in string;
+                      verbose : in integer32 := 0 ) is
 
   file : file_type;
   name : Link_to_String;
@@ -14,8 +15,16 @@ begin
   end if;
   new_line;
   put_line("Calling the equation-by-equation solver ...");
-  new_line;
-  put_line("Reading the name of the output file ...");
-  Read_Name_and_Create_File(file,name);
-  Shuffle_Polynomials_and_Solve(file,name.all,p);
+  if outname = "" then
+    new_line;
+    put_line("Reading the name of the output file ...");
+    Read_Name_and_Create_File(file,name);
+    Shuffle_Polynomials_and_Solve(file,name.all,p);
+  else
+    Create_Output_File(file,outname,name);
+    if name /= null
+     then Shuffle_Polynomials_and_Solve(file,name.all,p);
+     else Shuffle_Polynomials_and_Solve(file,outname,p);
+    end if;
+  end if;
 end bablsolve;
--- src/Ada/Components/Solver/bablsolve.ads
+++ src/Ada/Components/Solver/bablsolve.ads
@@ -1,7 +1,8 @@
 with Standard_Integer_Numbers;           use Standard_Integer_Numbers;
 with Standard_Complex_Poly_Systems;      use Standard_Complex_Poly_Systems;
 
-procedure bablsolve ( p : in Poly_Sys; verbose : in integer32 := 0 );
+procedure bablsolve ( p : in Poly_Sys; outname : in string;
+                      verbose : in integer32 := 0 );
 
 -- DESCRIPTION :
 --   This routine is called by the blackbox solver of PHCpack,
@@ -11,4 +12,5 @@ procedure bablsolve ( p : in Poly_Sys; verbose : in integer32 := 0 );
 
 -- ON ENTRY :
 --   p              system of polynomial equations;
+--   outname        the name of the output file (may be empty);
 --   verbose        the verbose level.
