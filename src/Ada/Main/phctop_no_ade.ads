073f087148fb5d034f49916e7267780e96c4a8e1
Jan Verschelde <janv@uic.edu>
2019-10-25 18:57:33 -0500

    removed option_handlers_no_ade and phctop_no_ade

--- src/Ada/Main/phctop_no_ade.ads
+++
@@ -1,9 +0,0 @@
-procedure phctop_no_ade;
-
--- DESCRIPTION :
---   This procedure processes the command line arguments
---   and calls the proper procedures corresponding to the options.
---   This is the top level procedure for the executable phc.
---   The "no_ade" means that the QD library will not be needed during
---   the linking, as the Algorithmic Differentiation for the Path library
---   will not be linked in.
