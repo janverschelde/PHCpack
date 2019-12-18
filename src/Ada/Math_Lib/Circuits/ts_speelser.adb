db1132f2a1d622b817fa2d8b7bbfbefd5a30ae25
Jan Verschelde <janv@uic.edu>
2019-12-17 20:58:21 -0600

    added the product of the power series to the new test procedure

--- src/Ada/Math_Lib/Circuits/ts_speelser.adb
+++ src/Ada/Math_Lib/Circuits/ts_speelser.adb
@@ -1,10 +1,11 @@
 with text_io;                             use text_io;
 with Standard_Integer_Numbers;            use Standard_Integer_Numbers;
 with Standard_Integer_Numbers_io;         use Standard_Integer_Numbers_io;
+with Standard_Complex_Numbers;            use Standard_Complex_Numbers;
 with Standard_Natural_Vectors;
+with Standard_Complex_Vectors;
+with Standard_Complex_Vectors_io;         use Standard_Complex_Vectors_io;
 with Standard_Dense_Series;
---with Standard_Dense_Vector_Series2;
---with Standard_Dense_Vector_Series2_io;    use Standard_Dense_Vector_Series2_io;
 with Standard_Dense_Series_Vectors;
 with Standard_Random_Series;
 with Standard_Series_Polynomials;
@@ -17,8 +18,9 @@ procedure ts_speelser is
 --   A vectorized version of the computation of Speelpenning products
 --   for truncated power series.
 
-  function Product ( dim,deg : in integer32 )
-                   return Standard_Series_Polynomials.Poly is
+  function Standard_Product
+             ( dim,deg : in integer32 )
+             return Standard_Series_Polynomials.Poly is
 
   -- DESCRIPTION :
   --   Returns the product of the first dim variables,
@@ -46,7 +48,52 @@ procedure ts_speelser is
       end;
     end loop;
     return res;
-  end Product;
+  end Standard_Product;
+
+  procedure Standard_Evaluate_Product
+              ( x : in Standard_Dense_Series_Vectors.Vector;
+                d : in integer32 ) is
+
+  -- DESCRIPTION :
+  --   Inline computation of the coefficients of the product
+  --   of the series in x, which are all of degree d.
+  --   The range of x starts at 1 and ends at 2 or a larger value.
+
+    acc,res : Standard_Complex_Vectors.Vector(0..d);
+
+  begin
+    if x'last = 2 then
+      res(0) := x(1).cff(0)*x(2).cff(0);
+      for k in 1..d loop
+        res(k) := x(1).cff(0)*x(2).cff(k);
+        for i in 1..k loop
+          res(k) := res(k) + x(1).cff(i)*x(2).cff(k-i);
+        end loop;
+      end loop;
+    else
+      acc(0) := x(1).cff(0)*x(2).cff(0);
+      for k in 1..d loop
+        acc(k) := x(1).cff(0)*x(2).cff(k);
+        for i in 1..k loop
+          acc(k) := acc(k) + x(1).cff(i)*x(2).cff(k-i);
+        end loop;
+      end loop;
+      for j in 3..x'last loop
+        res(0) := acc(0)*x(j).cff(0);
+        for k in 1..d loop
+          res(k) := acc(0)*x(j).cff(k);
+          for i in 1..k loop
+            res(k) := res(k) + acc(i)*x(j).cff(k-i);
+          end loop;
+        end loop;
+        if j < x'last
+         then acc := res;
+        end if;
+      end loop;
+    end if;
+    put_line("The coefficients of the product : ");
+    put_line(res);
+  end Standard_Evaluate_Product;
 
   procedure Standard_Test ( dim,deg : in integer32 ) is
 
@@ -54,7 +101,8 @@ procedure ts_speelser is
   --   Generates random coefficient vectors for as many series as
   --   the value of dim, series truncated to degree deg.
 
-    prd : constant Standard_Series_Polynomials.Poly := Product(dim,deg);
+    prd : constant Standard_Series_Polynomials.Poly
+        := Standard_Product(dim,deg);
     x : constant Standard_Dense_Series_Vectors.Vector(1..dim)
       := Standard_Random_Series.Random_Series_Vector(1,dim,deg);
     y : Standard_Dense_Series.Series;
@@ -66,7 +114,8 @@ procedure ts_speelser is
     put(prd); new_line;
     y := Standard_Series_Poly_Functions.Eval(prd,x);
     put_line("The value of the product at the random series :");
-    put(y);
+    put(y); new_line;
+    Standard_Evaluate_Product(x,deg);
   end Standard_Test;
 
   procedure Main is
