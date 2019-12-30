07f7a7d8e222f51fb9fffab854a693b4f477fa5a
Jan Verschelde <janv@uic.edu>
2019-12-30 15:59:57 -0600

    added more function to compare results of algorithmic differentiation, for multitasking

--- src/Ada/Math_Lib/Circuits/evaluation_differentiation_errors.adb
+++ src/Ada/Math_Lib/Circuits/evaluation_differentiation_errors.adb
@@ -64,9 +64,66 @@ package body Evaluation_Differentiation_Errors is
     return res;
   end Difference;
 
+  function Difference ( x : Standard_Complex_Vectors.Link_to_Vector;
+                        y : Standard_Complex_Vectors.Link_to_Vector )
+                      return double_float is
+
+    use Standard_Complex_Numbers;
+
+    res : double_float := 0.0;
+    avl : double_float;
+    val : Complex_Number;
+
+  begin
+    for i in x'range loop
+      val := x(i) - y(i);
+      avl := AbsVal(val);
+      res := res + avl;
+    end loop;
+    return res;
+  end Difference;
+
+  function Difference ( x : DoblDobl_Complex_Vectors.Link_to_Vector;
+                        y : DoblDobl_Complex_Vectors.Link_to_Vector )
+                      return double_double is
+
+    use DoblDobl_Complex_Numbers;
+
+    res : double_double := create(0.0);
+    avl : double_double;
+    val : Complex_Number;
+
+  begin
+    for i in x'range loop
+      val := x(i) - y(i);
+      avl := AbsVal(val);
+      res := res + avl;
+    end loop;
+    return res;
+  end Difference;
+
+  function Difference ( x : QuadDobl_Complex_Vectors.Link_to_Vector;
+                        y : QuadDobl_Complex_Vectors.Link_to_Vector )
+                      return quad_double is
+
+    use QuadDobl_Complex_Numbers;
+
+    res : quad_double := create(0.0);
+    avl : quad_double;
+    val : Complex_Number;
+
+  begin
+    for i in x'range loop
+      val := x(i) - y(i);
+      avl := AbsVal(val);
+      res := res + avl;
+    end loop;
+    return res;
+  end Difference;
+
   function Difference ( s : Standard_Complex_Series_Vectors.Vector;
-                       c : Standard_Complex_VecVecs.VecVec )
-                     return double_float is
+                        c : Standard_Complex_VecVecs.VecVec )
+                      return double_float is
 
     res : double_float := 0.0;
     val : double_float;
@@ -80,8 +137,8 @@ package body Evaluation_Differentiation_Errors is
   end Difference;
 
   function Difference ( s : DoblDobl_Complex_Series_Vectors.Vector;
-                       c : DoblDobl_Complex_VecVecs.VecVec )
-                     return double_double is
+                        c : DoblDobl_Complex_VecVecs.VecVec )
+                      return double_double is
 
     res : double_double := create(0.0);
     val : double_double;
@@ -95,8 +152,8 @@ package body Evaluation_Differentiation_Errors is
   end Difference;
 
   function Difference ( s : QuadDobl_Complex_Series_Vectors.Vector;
-                       c : QuadDobl_Complex_VecVecs.VecVec )
-                     return quad_double is
+                        c : QuadDobl_Complex_VecVecs.VecVec )
+                      return quad_double is
 
     res : quad_double := create(0.0);
     val : quad_double;
@@ -104,6 +161,51 @@ package body Evaluation_Differentiation_Errors is
   begin
     for i in s'range loop
       val := Difference(s(i),c(i));
+      res := res + val;
+    end loop;
+    return res;
+  end Difference;
+
+  function Difference ( x : Standard_Complex_VecVecs.VecVec;
+                        y : Standard_Complex_VecVecs.VecVec )
+                      return double_float is
+
+    res : double_float := 0.0;
+    val : double_float;
+
+  begin
+    for i in x'range loop
+      val := Difference(x(i),y(i));
+      res := res + val;
+    end loop;
+    return res;
+  end Difference;
+
+  function Difference ( x : DoblDobl_Complex_VecVecs.VecVec;
+                        y : DoblDobl_Complex_VecVecs.VecVec )
+                      return double_double is
+
+    res : double_double := create(0.0);
+    val : double_double;
+
+  begin
+    for i in x'range loop
+      val := Difference(x(i),y(i));
+      res := res + val;
+    end loop;
+    return res;
+  end Difference;
+
+  function Difference ( x : QuadDobl_Complex_VecVecs.VecVec;
+                        y : QuadDobl_Complex_VecVecs.VecVec )
+                      return quad_double is
+
+    res : quad_double := create(0.0);
+    val : quad_double;
+
+  begin
+    for i in x'range loop
+      val := Difference(x(i),y(i));
       res := res + val;
     end loop;
     return res;
@@ -186,6 +288,90 @@ package body Evaluation_Differentiation_Errors is
           end;
         end loop;
       end loop;
+    end loop;
+    return res;
+  end Difference;
+
+  function Difference ( vm1 : Standard_Complex_VecMats.VecMat;
+                        vm2 : Standard_Complex_VecMats.VecMat )
+                      return double_float is
+
+    use Standard_Complex_Numbers;
+
+    res : double_float := 0.0;
+    avl : double_float;
+    val : Complex_Number;
+
+  begin
+    for k in vm1'range loop
+      declare
+        mat1 : constant Standard_Complex_Matrices.Link_to_Matrix := vm1(k);
+        mat2 : constant Standard_Complex_Matrices.Link_to_Matrix := vm2(k);
+      begin
+        for i in mat1'range(1) loop
+          for j in mat2'range(2) loop
+            val := mat1(i,j) - mat2(i,j);
+            avl := AbsVal(val);
+            res := res + avl;
+          end loop;
+        end loop;
+      end;
+    end loop;
+    return res;
+  end Difference;
+
+  function Difference ( vm1 : DoblDobl_Complex_VecMats.VecMat;
+                        vm2 : DoblDobl_Complex_VecMats.VecMat )
+                      return double_double is
+
+    use DoblDobl_Complex_Numbers;
+
+    res : double_double := create(0.0);
+    avl : double_double;
+    val : Complex_Number;
+
+  begin
+    for k in vm1'range loop
+      declare
+        mat1 : constant DoblDobl_Complex_Matrices.Link_to_Matrix := vm1(k);
+        mat2 : constant DoblDobl_Complex_Matrices.Link_to_Matrix := vm2(k);
+      begin
+        for i in mat1'range(1) loop
+          for j in mat2'range(2) loop
+            val := mat1(i,j) - mat2(i,j);
+            avl := AbsVal(val);
+            res := res + avl;
+          end loop;
+        end loop;
+      end;
+    end loop;
+    return res;
+  end Difference;
+
+  function Difference ( vm1 : QuadDobl_Complex_VecMats.VecMat;
+                        vm2 : QuadDobl_Complex_VecMats.VecMat )
+                      return quad_double is
+
+    use QuadDobl_Complex_Numbers;
+
+    res : quad_double := create(0.0);
+    avl : quad_double;
+    val : Complex_Number;
+
+  begin
+    for k in vm1'range loop
+      declare
+        mat1 : constant QuadDobl_Complex_Matrices.Link_to_Matrix := vm1(k);
+        mat2 : constant QuadDobl_Complex_Matrices.Link_to_Matrix := vm2(k);
+      begin
+        for i in mat1'range(1) loop
+          for j in mat2'range(2) loop
+            val := mat1(i,j) - mat2(i,j);
+            avl := AbsVal(val);
+            res := res + avl;
+          end loop;
+        end loop;
+      end;
     end loop;
     return res;
   end Difference;
--- src/Ada/Math_Lib/Circuits/evaluation_differentiation_errors.ads
+++ src/Ada/Math_Lib/Circuits/evaluation_differentiation_errors.ads
@@ -44,15 +44,32 @@ package Evaluation_Differentiation_Errors is
 
   -- REQUIRED : s.cff'range = c'range.
 
+  function Difference ( x : Standard_Complex_Vectors.Link_to_Vector;
+                        y : Standard_Complex_Vectors.Link_to_Vector )
+                      return double_float;
+  function Difference ( x : DoblDobl_Complex_Vectors.Link_to_Vector;
+                        y : DoblDobl_Complex_Vectors.Link_to_Vector )
+                      return double_double;
+  function Difference ( x : QuadDobl_Complex_Vectors.Link_to_Vector;
+                        y : QuadDobl_Complex_Vectors.Link_to_Vector )
+                      return quad_double;
+
+  -- DESCRIPTION :
+  --   Returns the sum of the absolute values of the differences
+  --   between the coefficients of x and those values of y,
+  --   in double, double double, or quad double precision.
+
+  -- REQUIRED : x'range = y'range.
+
   function Difference ( s : Standard_Complex_Series_Vectors.Vector;
-                       c : Standard_Complex_VecVecs.VecVec )
-                     return double_float;
+                        c : Standard_Complex_VecVecs.VecVec )
+                      return double_float;
   function Difference ( s : DoblDobl_Complex_Series_Vectors.Vector;
-                       c : DoblDobl_Complex_VecVecs.VecVec )
-                     return double_double;
+                        c : DoblDobl_Complex_VecVecs.VecVec )
+                      return double_double;
   function Difference ( s : QuadDobl_Complex_Series_Vectors.Vector;
-                       c : QuadDobl_Complex_VecVecs.VecVec )
-                     return quad_double;
+                        c : QuadDobl_Complex_VecVecs.VecVec )
+                      return quad_double;
 
   -- DESCRIPTION :
   --   Returns the sum of the absolute values of the differences
@@ -60,6 +77,23 @@ package Evaluation_Differentiation_Errors is
   --   in double, double double, or quad double precision.
 
   -- REQUIRED : s'range = c'range.
+
+  function Difference ( x : Standard_Complex_VecVecs.VecVec;
+                        y : Standard_Complex_VecVecs.VecVec )
+                      return double_float;
+  function Difference ( x : DoblDobl_Complex_VecVecs.VecVec;
+                        y : DoblDobl_Complex_VecVecs.VecVec )
+                      return double_double;
+  function Difference ( x : QuadDobl_Complex_VecVecs.VecVec;
+                        y : QuadDobl_Complex_VecVecs.VecVec )
+                      return quad_double;
+
+  -- DESCRIPTION :
+  --   Returns the sum of the absolute values of the differences
+  --   between the coefficients of x and those in y,
+  --   in double, double double, or quad double precision.
+
+  -- REQUIRED : x'range = y'range.
 
   function Difference ( jm : Standard_Complex_Series_Matrices.Matrix;
                         vm : Standard_Complex_VecMats.VecMat )
@@ -78,5 +112,22 @@ package Evaluation_Differentiation_Errors is
   -- REQUIRED : vm'range = 0..degree, where degree is the degree
   --   of all series in the matrix jm, for all k in vm'range:
   --   vm(k)'range(1) = jm'range(1) and vm(k)'range(2) = jm'range(2).
+
+  function Difference ( vm1 : Standard_Complex_VecMats.VecMat;
+                        vm2 : Standard_Complex_VecMats.VecMat )
+                      return double_float;
+  function Difference ( vm1 : DoblDobl_Complex_VecMats.VecMat;
+                        vm2 : DoblDobl_Complex_VecMats.VecMat )
+                      return double_double;
+  function Difference ( vm1 : QuadDobl_Complex_VecMats.VecMat;
+                        vm2 : QuadDobl_Complex_VecMats.VecMat )
+                      return quad_double;
+
+  -- DESCRIPTION :
+  --   Returns the sum of the absolute values of the differences
+  --   between the coefficients of vm1 and the values in vm2.
+
+  -- REQUIRED : vm1'range = vm2'range and for k in vm1'range:
+  --   vm1(k)'range(1) = vm2'range(1) and vm1(k)'range(2) = vm2(k)'range(2).
 
 end Evaluation_Differentiation_Errors;
