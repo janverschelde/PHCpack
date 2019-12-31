6e814b340be9e3df7df40192cb29aaeea002d460
Jan Verschelde <janv@uic.edu>
2019-12-30 18:01:12 -0600

    added tests and fixed indexing error

--- src/Ada/Tasking/ts_mtadcnv.adb
+++ src/Ada/Tasking/ts_mtadcnv.adb
@@ -2,16 +2,20 @@ with text_io;                            use text_io;
 with Timing_Package;                     use Timing_Package;
 with Standard_Integer_Numbers;           use Standard_Integer_Numbers;
 with Standard_Integer_Numbers_io;        use Standard_Integer_Numbers_io;
+with Standard_Floating_Numbers;          use Standard_Floating_Numbers;
+with Standard_Floating_Numbers_io;       use Standard_Floating_Numbers_io;
 with Standard_Integer_Vectors;
 with Standard_Complex_Numbers;
 with Standard_Complex_Vectors;
 with Standard_Complex_VecVecs;
+with Standard_Complex_Matrices;
 with Standard_Complex_VecMats;
 with Standard_Complex_Series_Vectors;
 with Standard_Random_Series_Vectors;
 with Series_Polynomial_Gradients;        use Series_Polynomial_Gradients;
 with Standard_Speelpenning_Convolutions;
 with Random_Convolution_Circuits;        use Random_Convolution_Circuits;
+with Evaluation_Differentiation_Errors;  use Evaluation_Differentiation_Errors;
 with Multitasking;
 
 procedure ts_mtadcnv is
@@ -96,23 +100,40 @@ procedure ts_mtadcnv is
     --   with intermediate output.
 
       idx : integer32 := i;
+      ydi : Standard_Complex_VecVecs.Link_to_VecVec;
       vleft,vright : Standard_Complex_Vectors.Link_to_Vector;
+      mleft : Standard_Complex_Matrices.Link_to_Matrix;
  
     begin
       put_line("number of circuits : " & Multitasking.to_string(c'last));
+      put_line("number of tasks : " & Multitasking.to_string(n));
       while idx <= c'last loop
         put_line("considering circuit " & Multitasking.to_string(idx));
         put_line("task " & Multitasking.to_string(i)
                          & " computes circuit "
                          & Multitasking.to_string(idx));
-        EvalDiff(c(idx).all,x,pwt,yd(idx).all);
+        EvalDiff(c(idx).all,x,pwt,yd(i).all);
+        put_line("task " & Multitasking.to_string(i)
+                         & " done computing circuit "
+                         & Multitasking.to_string(idx));
         vleft := vy(idx);
-        vright := yd(i)(x'last+1);
+        ydi := yd(i);
+        vright := ydi(x'last+1);
         for j in vleft'range loop
           vleft(j) := vright(j);
           vright(j) := Standard_Complex_Numbers.Create(0.0);
         end loop;
+        for j in 1..x'last loop
+          vright := ydi(j);
+          for k in vm'range loop       -- k-th coefficient in matrix vm(k)
+            mleft := vm(k);            -- row idx in vm(k) is the equation
+            mleft(idx,j) := vright(k); -- column j in vm(k) is the variable
+            vright(k) := Standard_Complex_Numbers.Create(0.0);
+          end loop;
+        end loop;
+        put_line("idx before increment : " & Multitasking.to_string(idx));
         idx := idx + n;
+        put_line("idx after increment : " & Multitasking.to_string(idx));
       end loop;
       put_line("task " & Multitasking.to_string(i) & " is done.");
       done(i) := true;
@@ -158,24 +179,33 @@ procedure ts_mtadcnv is
     pwt : constant Link_to_VecVecVec := Create(xcff,mxe);
     yd : constant Standard_Complex_VecVecs.VecVec(1..dim+1)
        := Allocate_Coefficients(dim+1,deg);
-    vy : constant Standard_Complex_VecVecs.VecVec(1..dim)
-       := Allocate_Coefficients(dim,deg);
-    vm : constant Standard_Complex_VecMats.VecMat(0..deg)
-       := Allocate_Coefficients(dim,dim,deg);
+    vy1 : constant Standard_Complex_VecVecs.VecVec(1..dim)
+        := Allocate_Coefficients(dim,deg);
+    vy2 : constant Standard_Complex_VecVecs.VecVec(1..dim)
+        := Allocate_Coefficients(dim,deg);
+    vm1 : constant Standard_Complex_VecMats.VecMat(0..deg)
+        := Allocate_Coefficients(dim,dim,deg);
+    vm2 : constant Standard_Complex_VecMats.VecMat(0..deg)
+        := Allocate_Coefficients(dim,dim,deg);
     timer : timing_widget;
+    err : double_float;
 
   begin
-    if nbt = 1 then
+   -- if nbt = 1 then
       tstart(timer);
-      EvalDiff(c,xcff,pwt,yd,vy,vm);
+      EvalDiff(c,xcff,pwt,yd,vy1,vm1);
       tstop(timer);
-    else
+   -- else
       tstart(timer);
-      Standard_Multitasked_EvalDiff(nbt,c,xcff,pwt,vy,vm,true);
+      Standard_Multitasked_EvalDiff(nbt,c,xcff,pwt,vy2,vm2,true);
       tstop(timer);
-    end if;
-    new_line;
-    print_times(standard_output,timer,"evaluation and differentiation");
+   -- end if;
+    err := Difference(vy1,vy2);
+    put("the error of evaluation : "); put(err,3); new_line;
+    err := Difference(vm1,vm2);
+    put("the error of differentiation : "); put(err,3); new_line;
+   -- new_line;
+   -- print_times(standard_output,timer,"evaluation and differentiation");
   end Standard_Random_Test;
 
   procedure Main is
