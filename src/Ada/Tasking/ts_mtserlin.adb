3f1cc4dc3e39504c234828c2c8f3d6767e78a278
Jan Verschelde <janv@uic.edu>
2020-01-16 12:53:25 -0600

    added computation of the sum of all errors to the test on the multitasked linearization of the matrix series equation solver

--- src/Ada/Tasking/ts_mtserlin.adb
+++ src/Ada/Tasking/ts_mtserlin.adb
@@ -2,6 +2,12 @@ with text_io;                            use text_io;
 with Communications_with_User;           use Communications_with_User;
 with Standard_Integer_Numbers;           use Standard_Integer_Numbers;
 with Standard_Integer_Numbers_io;        use Standard_Integer_Numbers_io;
+with Standard_Floating_Numbers;          use Standard_Floating_Numbers;
+with Standard_Floating_Numbers_io;       use Standard_Floating_Numbers_io;
+with Double_Double_Numbers;              use Double_Double_Numbers;
+with Double_Double_Numbers_io;           use Double_Double_Numbers_io;
+with Quad_Double_Numbers;                use Quad_Double_Numbers;
+with Quad_Double_Numbers_io;             use Quad_Double_Numbers_io;
 with Standard_Integer_Vectors;
 with Standard_Complex_Vectors;
 with Standard_Complex_Vectors_io;        use Standard_Complex_Vectors_io;
@@ -52,6 +58,7 @@ with QuadDobl_Random_Series_Vectors;
 with QuadDobl_Random_Series_Matrices;
 with QuadDobl_Series_Matrix_Solvers;
 with Series_Coefficient_Vectors;
+with Evaluation_Differentiation_Errors;  use Evaluation_Differentiation_Errors;
 with Multitasked_Series_Linearization;   use Multitasked_Series_Linearization;
 
 procedure ts_mtserlin is
@@ -59,6 +66,105 @@ procedure ts_mtserlin is
 -- DESCRIPTION :
 --   Tests the linearization of solving linear systems of truncated series
 --   with multitasking.
+
+  function Error ( xs : Standard_Complex_Vector_Series.Vector;
+                   bscff : Standard_Complex_VecVecs.VecVec;
+                   output : boolean := true ) return double_float is
+
+  -- DESCRIPTION :
+  --   Returns the sum of all differences between the coefficients in xs
+  --   and those in bscff.  If output, then the coefficient vectors are
+  --   written to screen for both xs and bscff.
+
+    err : double_float;
+
+  begin
+    if output then
+      put_line("The generated leading vector series of the solution :");
+      put_line(xs.cff(0));
+      put_line("The computed leading vector series of the solution :");
+      put_line(bscff(0));
+    end if;
+    err := Difference(xs.cff(0),bscff(0));
+    for k in 1..xs.deg loop
+      if output then
+        put("The generated term "); put(k,1);
+        put_line(" of the vector series of the solution :");
+        put_line(xs.cff(k));
+        put("The computed term "); put(k,1);
+        put_line(" of the vector series of the solution :");
+        put_line(bscff(k));
+      end if;
+      err := err + Difference(xs.cff(k),bscff(k));
+    end loop;
+    return err;
+  end Error;
+
+  function Error ( xs : DoblDobl_Complex_Vector_Series.Vector;
+                   bscff : DoblDobl_Complex_VecVecs.VecVec;
+                   output : boolean := true ) return double_double is
+
+  -- DESCRIPTION :
+  --   Returns the sum of all differences between the coefficients in xs
+  --   and those in bscff.  If output, then the coefficient vectors are
+  --   written to screen for both xs and bscff.
+
+    err : double_double;
+
+  begin
+    if output then
+      put_line("The generated leading vector series of the solution :");
+      put_line(xs.cff(0));
+      put_line("The computed leading vector series of the solution :");
+      put_line(bscff(0));
+    end if;
+    err := Difference(xs.cff(0),bscff(0));
+    for k in 1..xs.deg loop
+      if output then
+        put("The generated term "); put(k,1);
+        put_line(" of the vector series of the solution :");
+        put_line(xs.cff(k));
+        put("The computed term "); put(k,1);
+        put_line(" of the vector series of the solution :");
+        put_line(bscff(k));
+      end if;
+      err := err + Difference(xs.cff(k),bscff(k));
+    end loop;
+    return err;
+  end Error;
+
+  function Error ( xs : QuadDobl_Complex_Vector_Series.Vector;
+                   bscff : QuadDobl_Complex_VecVecs.VecVec;
+                   output : boolean := true ) return quad_double is
+
+  -- DESCRIPTION :
+  --   Returns the sum of all differences between the coefficients in xs
+  --   and those in bscff.  If output, then the coefficient vectors are
+  --   written to screen for both xs and bscff.
+
+    err : quad_double;
+
+  begin
+    if output then
+      put_line("The generated leading vector series of the solution :");
+      put_line(xs.cff(0));
+      put_line("The computed leading vector series of the solution :");
+      put_line(bscff(0));
+    end if;
+    err := Difference(xs.cff(0),bscff(0));
+    for k in 1..xs.deg loop
+      if output then
+        put("The generated term "); put(k,1);
+        put_line(" of the vector series of the solution :");
+        put_line(xs.cff(k));
+        put("The computed term "); put(k,1);
+        put_line(" of the vector series of the solution :");
+        put_line(bscff(k));
+      end if;
+      err := err + Difference(xs.cff(k),bscff(k));
+    end loop;
+    return err;
+  end Error;
 
   procedure Standard_Test ( nbt,n,d : in integer32 ) is
 
@@ -93,20 +199,24 @@ procedure ts_mtserlin is
         := new Standard_Complex_Vectors.Vector(1..n);
     info : integer32;
     ans : character;
-    output : boolean;
+    nbrotp,output : boolean;
+    err : double_float;
 
   begin
-    put_line("The coefficients of the matrix series :"); put(As);
-    put_line("The coefficient matrices : "); put(vm);
-    put_line("The exact solution x :"); put_line(sx);
-    put_line("The coefficients of the vector series x :"); put(xs);
-    put_line("The right hand side vector b :"); put_line(sb);
-    put_line("The coefficients of b : "); put_line(sbcff);
-    put_line("The coefficients of the vector series b :"); put(bs);
-    put_line("The coefficients of the vector series b :"); put_line(bscff);
-    put("Output during multitasking ? (y/n) ");
-    Ask_Yes_or_No(ans);
+    put("Output of numbers ? (y/n) "); Ask_Yes_or_No(ans);
+    nbrotp := (ans = 'y');
+    put("Output during multitasking ? (y/n) "); Ask_Yes_or_No(ans);
     output := (ans = 'y');
+    if nbrotp then
+      put_line("The coefficients of the matrix series :"); put(As);
+      put_line("The coefficient matrices : "); put(vm);
+      put_line("The exact solution x :"); put_line(sx);
+      put_line("The coefficients of the vector series x :"); put(xs);
+      put_line("The right hand side vector b :"); put_line(sb);
+      put_line("The coefficients of b : "); put_line(sbcff);
+      put_line("The coefficients of the vector series b :"); put(bs);
+      put_line("The coefficients of the vector series b :"); put_line(bscff);
+    end if;
     if nbt > 1 then
       Multitasked_Solve_by_lufac(nbt,vm,bscff,ipvt,info,output);
     elsif nbt = 1 then
@@ -118,16 +228,8 @@ procedure ts_mtserlin is
       end if;
     end if;
     put("info : "); put(info,1); new_line;
-    put_line("The generated leading vector series of the solution :");
-    put_line(xs.cff(0));
-    put_line("The computed leading vector series of the solution :");
-    put_line(bscff(0));
-    for k in 1..bs.deg loop
-      put("The generated term "); put(k,1);
-      put_line(" of the vector series of the solution :"); put_line(xs.cff(k));
-      put("The computed term "); put(k,1);
-      put_line(" of the vector series of the solution :"); put_line(bscff(k));
-    end loop;
+    err := Error(xs,bscff,nbrotp);
+    put("Sum of errors :"); put(err,3); new_line;
   end Standard_Test;
 
   procedure DoblDobl_Test ( nbt,n,d : in integer32 ) is
@@ -163,20 +265,24 @@ procedure ts_mtserlin is
         := new DoblDobl_Complex_Vectors.Vector(1..n);
     info : integer32;
     ans : character;
-    output : boolean;
+    nbrotp,output : boolean;
+    err : double_double;
 
   begin
-    put_line("The coefficients of the matrix series :"); put(As);
-    put_line("The coefficient matrices : "); put(vm);
-    put_line("The exact solution x :"); put_line(sx);
-    put_line("The coefficients of the vector series x :"); put(xs);
-    put_line("The right hand side vector b :"); put_line(sb);
-    put_line("The coefficients of b : "); put_line(sbcff);
-    put_line("The coefficients of the vector series b :"); put(bs);
-    put_line("The coefficients of the vector series b :"); put_line(bscff);
-    put("Output during multitasking ? (y/n) ");
-    Ask_Yes_or_No(ans);
+    put("Output of numbers ? (y/n) "); Ask_Yes_or_No(ans);
+    nbrotp := (ans = 'y');
+    put("Output during multitasking ? (y/n) "); Ask_Yes_or_No(ans);
     output := (ans = 'y');
+    if nbrotp then
+      put_line("The coefficients of the matrix series :"); put(As);
+      put_line("The coefficient matrices : "); put(vm);
+      put_line("The exact solution x :"); put_line(sx);
+      put_line("The coefficients of the vector series x :"); put(xs);
+      put_line("The right hand side vector b :"); put_line(sb);
+      put_line("The coefficients of b : "); put_line(sbcff);
+      put_line("The coefficients of the vector series b :"); put(bs);
+      put_line("The coefficients of the vector series b :"); put_line(bscff);
+    end if;
     if nbt > 1 then
       Multitasked_Solve_by_lufac(nbt,vm,bscff,ipvt,info,output);
     elsif nbt = 1 then
@@ -188,16 +294,8 @@ procedure ts_mtserlin is
       end if;
     end if;
     put("info : "); put(info,1); new_line;
-    put_line("The generated leading vector series of the solution :");
-    put_line(xs.cff(0));
-    put_line("The computed leading vector series of the solution :");
-    put_line(bscff(0));
-    for k in 1..bs.deg loop
-      put("The generated term "); put(k,1);
-      put_line(" of the vector series of the solution :"); put_line(xs.cff(k));
-      put("The computed term "); put(k,1);
-      put_line(" of the vector series of the solution :"); put_line(bscff(k));
-    end loop;
+    err := Error(xs,bscff,nbrotp);
+    put("Sum of errors : "); put(err,3); new_line;
   end DoblDobl_Test;
 
   procedure QuadDobl_Test ( nbt,n,d : in integer32 ) is
@@ -233,20 +331,24 @@ procedure ts_mtserlin is
         := new QuadDobl_Complex_Vectors.Vector(1..n);
     info : integer32;
     ans : character;
-    output : boolean;
+    nbrotp,output : boolean;
+    err : quad_double;
 
   begin
-    put_line("The coefficients of the matrix series :"); put(As);
-    put_line("The coefficient matrices : "); put(vm);
-    put_line("The exact solution x :"); put_line(sx);
-    put_line("The coefficients of the vector series x :"); put(xs);
-    put_line("The right hand side vector b :"); put_line(sb);
-    put_line("The coefficients of b : "); put_line(sbcff);
-    put_line("The coefficients of the vector series b :"); put(bs);
-    put_line("The coefficients of the vector series b :"); put_line(bscff);
-    put("Output during multitasking ? (y/n) ");
-    Ask_Yes_or_No(ans);
+    put("Output of numbers ? (y/n) "); Ask_Yes_or_No(ans);
+    nbrotp := (ans = 'y');
+    put("Output during multitasking ? (y/n) "); Ask_Yes_or_No(ans);
     output := (ans = 'y');
+    if nbrotp then
+      put_line("The coefficients of the matrix series :"); put(As);
+      put_line("The coefficient matrices : "); put(vm);
+      put_line("The exact solution x :"); put_line(sx);
+      put_line("The coefficients of the vector series x :"); put(xs);
+      put_line("The right hand side vector b :"); put_line(sb);
+      put_line("The coefficients of b : "); put_line(sbcff);
+      put_line("The coefficients of the vector series b :"); put(bs);
+      put_line("The coefficients of the vector series b :"); put_line(bscff);
+    end if;
     if nbt > 1 then
       Multitasked_Solve_by_lufac(nbt,vm,bscff,ipvt,info,output);
     elsif nbt = 1 then
@@ -258,16 +360,8 @@ procedure ts_mtserlin is
       end if;
     end if;
     put("info : "); put(info,1); new_line;
-    put_line("The generated leading vector series of the solution :");
-    put_line(xs.cff(0));
-    put_line("The computed leading vector series of the solution :");
-    put_line(bscff(0));
-    for k in 1..bs.deg loop
-      put("The generated term "); put(k,1);
-      put_line(" of the vector series of the solution :"); put_line(xs.cff(k));
-      put("The computed term "); put(k,1);
-      put_line(" of the vector series of the solution :"); put_line(bscff(k));
-    end loop;
+    err := Error(xs,bscff,nbrotp);
+    put("Sum of errors : "); put(err,3); new_line;
   end QuadDobl_Test;
 
   procedure Main is
@@ -287,9 +381,9 @@ procedure ts_mtserlin is
     put("  Give the number of tasks : "); get(nbt);
     new_line;
     put_line("MENU for the working precision :");
-    put_line(" 0. double precision");
-    put_line(" 1. double double precision");
-    put_line(" 2. quad double precision");
+    put_line("  0. double precision");
+    put_line("  1. double double precision");
+    put_line("  2. quad double precision");
     put("Type 0, 1, or 2 to select the precision : ");
     Ask_Alternative(ans,"012");
     new_line;
