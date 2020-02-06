daf864a4f853a362a7ac2c8266ed77c5dd77016c
Jan Verschelde <janv@uic.edu>
2020-02-06 10:20:32 -0600

    added difference computation and timers to the test procedure on the multitasked Hessian computation

--- src/Ada/Tasking/ts_mthessian.adb
+++ src/Ada/Tasking/ts_mthessian.adb
@@ -1,4 +1,7 @@
 with text_io;                            use text_io;
+with duration_io;
+with Ada.Calendar;
+with Time_Stamps;
 with Communications_with_User;           use Communications_with_User;
 with Standard_Integer_Numbers;           use Standard_Integer_Numbers;
 with Standard_Integer_Numbers_io;        use Standard_Integer_Numbers_io;
@@ -24,6 +27,7 @@ with Standard_Speelpenning_Convolutions;
 with DoblDobl_Speelpenning_Convolutions;
 with QuadDobl_Speelpenning_Convolutions;
 with Random_Convolution_Circuits;        use Random_Convolution_Circuits;
+with Evaluation_Differentiation_Errors;  use Evaluation_Differentiation_Errors;
 with Hessian_Convolution_Circuits;       use Hessian_Convolution_Circuits;
 with Multitasked_Hessian_Convolutions;   use Multitasked_Hessian_Convolutions;
 
@@ -115,31 +119,62 @@ procedure ts_mthessian is
     vx : Standard_Complex_Vectors.Vector(1..dim);
     A,U,V : Standard_Complex_Matrices.Matrix(1..dim,1..dim);
     e : Standard_Complex_Vectors.Vector(1..dim);
-    svl : Standard_Complex_VecVecs.VecVec(1..dim);
+    svl : constant Standard_Complex_VecVecs.VecVec(1..dim) := Allocate(dim,dim);
     values : Standard_Complex_VecVecs.VecVec(1..dim) := Allocate(dim,dim);
+    ans : character;
+    otp : boolean;
     nbt : integer32 := 0;
+    err : double_float;
+    seristart,seristop,multstart,multstop : Ada.Calendar.Time;
+    seri_elapsed,mult_elapsed,speedup : Duration;
+
+    use Ada.Calendar;
 
   begin
+    new_line;
+    put("Extra output wanted ? (y/n) ");
+    Ask_Yes_or_No(ans);
+    otp := (ans = 'y');
     Standard_Random_Newton_Homotopy(dim,deg,nbr,pwr,s,x);
     for i in 1..dim loop
       lnk := x(i);
       vx(i) := lnk(0);
     end loop;
+    put_line("Computing first without multitasking ...");
+    seristart := Ada.Calendar.Clock;
     for i in 1..dim loop
-      declare
-        vls : Standard_Complex_Vectors.Vector(1..dim);
-      begin
-        Singular_Values(s.crc(i),vx,A,U,V,e,vls);
-        svl(i) := new Standard_Complex_Vectors.Vector'(vls);
-      end;
+      lnk := svl(i);
+      Singular_Values(s.crc(i),vx,A,U,V,e,lnk.all);
     end loop;
-    put_line("All singular values :");
-    Write_Singular_Values(svl);
-    new_line;
-    put("Give the number of tasks : "); get(nbt);
-    Multitasked_Singular_Values(nbt,s,vx,values);
-    put_line("All singular values computed by multitasking :");
-    Write_Singular_Values(values);
+    seristop := Ada.Calendar.Clock;
+    seri_elapsed := seristop - seristart;
+    put_line("-> Elapsed time without multitasking :");
+    Time_Stamps.Write_Elapsed_Time(standard_output,seristart,seristop);
+    if otp
+     then put_line("All singular values :"); Write_Singular_Values(svl);
+    end if;
+    loop
+      new_line;
+      put("Give the number of tasks (0 to exit) : "); get(nbt);
+      exit when (nbt <= 0);
+      multstart := Ada.Calendar.Clock;
+      Multitasked_Singular_Values(nbt,s,vx,values,otp);
+      multstop := Ada.Calendar.Clock;
+      mult_elapsed := multstop - multstart;
+      put("-> Elapsed time with "); put(nbt,1); put_line(" tasks :");
+      Time_Stamps.Write_Elapsed_Time(standard_output,multstart,multstop);
+      if otp then
+        put_line("All singular values computed by multitasking :");
+        Write_Singular_Values(values);
+      end if;
+      err := Difference(svl,values);
+      put("The difference error : "); put(err,3); new_line;
+      if seri_elapsed + 1.0 /= 1.0 then
+        speedup := seri_elapsed/mult_elapsed;
+        put("The speedup : ");
+        duration_io.put(speedup,1,3); new_line;
+      end if;
+    end loop;
   end Standard_Random_Test;
 
   procedure DoblDobl_Random_Test
@@ -163,31 +198,62 @@ procedure ts_mthessian is
     vx : DoblDobl_Complex_Vectors.Vector(1..dim);
     A,U,V : DoblDobl_Complex_Matrices.Matrix(1..dim,1..dim);
     e : DoblDobl_Complex_Vectors.Vector(1..dim);
-    svl : DoblDobl_Complex_VecVecs.VecVec(1..dim);
+    svl : constant DoblDobl_Complex_VecVecs.VecVec(1..dim) := Allocate(dim,dim);
     values : DoblDobl_Complex_VecVecs.VecVec(1..dim) := Allocate(dim,dim);
+    ans : character;
+    otp : boolean;
     nbt : integer32 := 0;
+    err : double_double;
+    seristart,seristop,multstart,multstop : Ada.Calendar.Time;
+    seri_elapsed,mult_elapsed,speedup : Duration;
+
+    use Ada.Calendar;
 
   begin
+    new_line;
+    put("Extra output wanted ? (y/n) ");
+    Ask_Yes_or_No(ans);
+    otp := (ans = 'y');
     DoblDobl_Random_Newton_Homotopy(dim,deg,nbr,pwr,s,x);
     for i in 1..dim loop
       lnk := x(i);
       vx(i) := lnk(0);
     end loop;
+    put_line("Computing first without multitasking ...");
+    seristart := Ada.Calendar.Clock;
     for i in 1..dim loop
-      declare
-        vls : DoblDobl_Complex_Vectors.Vector(1..dim);
-      begin
-        Singular_Values(s.crc(i),vx,A,U,V,e,vls);
-        svl(i) := new DoblDobl_Complex_Vectors.Vector'(vls);
-      end;
+      lnk := svl(i);
+      Singular_Values(s.crc(i),vx,A,U,V,e,lnk.all);
     end loop;
-    put_line("All singular values :");
-    Write_Singular_Values(svl);
-    new_line;
-    put("Give the number of tasks : "); get(nbt);
-    Multitasked_Singular_Values(nbt,s,vx,values);
-    put_line("All singular values computed by multitasking :");
-    Write_Singular_Values(values);
+    seristop := Ada.Calendar.Clock;
+    seri_elapsed := seristop - seristart;
+    put_line("-> Elapsed time without multitasking :");
+    Time_Stamps.Write_Elapsed_Time(standard_output,seristart,seristop);
+    if otp
+     then put_line("All singular values :"); Write_Singular_Values(svl);
+    end if;
+    loop
+      new_line;
+      put("Give the number of tasks (0 to exit) : "); get(nbt);
+      exit when (nbt <= 0);
+      multstart := Ada.Calendar.Clock;
+      Multitasked_Singular_Values(nbt,s,vx,values,otp);
+      multstop := Ada.Calendar.Clock;
+      mult_elapsed := multstop - multstart;
+      put("-> Elapsed time with "); put(nbt,1); put_line(" tasks :");
+      Time_Stamps.Write_Elapsed_Time(standard_output,multstart,multstop);
+      if otp then
+        put_line("All singular values computed by multitasking :");
+        Write_Singular_Values(values);
+      end if;
+      err := Difference(svl,values);
+      put("The difference error : "); put(err,3); new_line;
+      if seri_elapsed + 1.0 /= 1.0 then
+        speedup := seri_elapsed/mult_elapsed;
+        put("The speedup : ");
+        duration_io.put(speedup,1,3); new_line;
+      end if;
+    end loop;
   end DoblDobl_Random_Test;
 
   procedure QuadDobl_Random_Test
@@ -211,31 +277,62 @@ procedure ts_mthessian is
     vx : QuadDobl_Complex_Vectors.Vector(1..dim);
     A,U,V : QuadDobl_Complex_Matrices.Matrix(1..dim,1..dim);
     e : QuadDobl_Complex_Vectors.Vector(1..dim);
-    svl : QuadDobl_Complex_VecVecs.VecVec(1..dim);
+    svl : constant QuadDobl_Complex_VecVecs.VecVec(1..dim) := Allocate(dim,dim);
     values : QuadDobl_Complex_VecVecs.VecVec(1..dim) := Allocate(dim,dim);
+    ans : character;
+    otp : boolean;
     nbt : integer32 := 0;
+    err : quad_double;
+    seristart,seristop,multstart,multstop : Ada.Calendar.Time;
+    seri_elapsed,mult_elapsed,speedup : Duration;
+
+    use Ada.Calendar;
 
   begin
+    new_line;
+    put("Extra output wanted ? (y/n) ");
+    Ask_Yes_or_No(ans);
+    otp := (ans = 'y');
     QuadDobl_Random_Newton_Homotopy(dim,deg,nbr,pwr,s,x);
     for i in 1..dim loop
       lnk := x(i);
       vx(i) := lnk(0);
     end loop;
+    put_line("Computing first without multitasking ...");
+    seristart := Ada.Calendar.Clock;
     for i in 1..dim loop
-      declare
-        vls : QuadDobl_Complex_Vectors.Vector(1..dim);
-      begin
-        Singular_Values(s.crc(i),vx,A,U,V,e,vls);
-        svl(i) := new QuadDobl_Complex_Vectors.Vector'(vls);
-      end;
+      lnk := svl(i);
+      Singular_Values(s.crc(i),vx,A,U,V,e,lnk.all);
     end loop;
-    put_line("All singular values :");
-    Write_Singular_Values(svl);
-    new_line;
-    put("Give the number of tasks : "); get(nbt);
-    Multitasked_Singular_Values(nbt,s,vx,values);
-    put_line("All singular values computed by multitasking :");
-    Write_Singular_Values(values);
+    seristop := Ada.Calendar.Clock;
+    seri_elapsed := seristop - seristart;
+    put_line("-> Elapsed time without multitasking :");
+    Time_Stamps.Write_Elapsed_Time(standard_output,seristart,seristop);
+    if otp
+     then put_line("All singular values :"); Write_Singular_Values(svl);
+    end if;
+    loop
+      new_line;
+      put("Give the number of tasks (0 to exit) : "); get(nbt);
+      exit when (nbt <= 0);
+      multstart := Ada.Calendar.Clock;
+      Multitasked_Singular_Values(nbt,s,vx,values,otp);
+      multstop := Ada.Calendar.Clock;
+      mult_elapsed := multstop - multstart;
+      put("-> Elapsed time with "); put(nbt,1); put_line(" tasks :");
+      Time_Stamps.Write_Elapsed_Time(standard_output,multstart,multstop);
+      if otp then
+        put_line("All singular values computed by multitasking :");
+        Write_Singular_Values(values);
+      end if;
+      err := Difference(svl,values);
+      put("The difference error : "); put(err,3); new_line;
+      if seri_elapsed + 1.0 /= 1.0 then
+        speedup := seri_elapsed/mult_elapsed;
+        put("The speedup : ");
+        duration_io.put(speedup,1,3); new_line;
+      end if;
+    end loop;
   end QuadDobl_Random_Test;
 
   procedure Main is
