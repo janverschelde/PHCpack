b2ba2e3c403e264413567b3ed193270d65aab2e4
Jan Verschelde <janv@uic.edu>
2020-02-07 19:01:21 -0600

    added benchmark test for all precision levels

--- src/Ada/Tasking/ts_mthessian.adb
+++ src/Ada/Tasking/ts_mthessian.adb
@@ -23,9 +23,11 @@ with DoblDobl_Complex_Matrices;
 with QuadDobl_Complex_Vectors;
 with QuadDobl_Complex_VecVecs;
 with QuadDobl_Complex_Matrices;
+with QuadDobl_Complex_Vectors_cv;
 with Standard_Speelpenning_Convolutions;
 with DoblDobl_Speelpenning_Convolutions;
 with QuadDobl_Speelpenning_Convolutions;
+with System_Convolution_Circuits;
 with Random_Convolution_Circuits;        use Random_Convolution_Circuits;
 with Evaluation_Differentiation_Errors;  use Evaluation_Differentiation_Errors;
 with Hessian_Convolution_Circuits;       use Hessian_Convolution_Circuits;
@@ -369,6 +371,125 @@ procedure ts_mthessian is
     end loop;
   end QuadDobl_Random_Test;
 
+  function to_double_double
+             ( v : QuadDobl_Complex_VecVecs.Link_to_VecVec )
+             return DoblDobl_Complex_VecVecs.Link_to_VecVec is
+
+  -- DESCRIPTION :
+  --   Returns the vector v converted to double double precision.
+
+  -- REQUIRED : v /= null;
+
+    res : DoblDobl_Complex_VecVecs.Link_to_VecVec;
+    ddv : DoblDobl_Complex_VecVecs.VecVec(v'range);
+
+    use QuadDobl_Complex_Vectors_cv;
+
+  begin
+    for i in ddv'range loop
+      declare
+        vec : constant DoblDobl_Complex_Vectors.Vector
+            := QuadDobl_Complex_to_DoblDobl(v(i).all);
+      begin
+        ddv(i) := new DoblDobl_Complex_Vectors.Vector'(vec);
+      end;
+    end loop;
+    res := new DoblDobl_Complex_VecVecs.VecVec'(ddv);
+    return res;
+  end to_double_double;
+
+  function to_double
+             ( v : QuadDobl_Complex_VecVecs.Link_to_VecVec )
+             return Standard_Complex_VecVecs.Link_to_VecVec is
+
+  -- DESCRIPTION :
+  --   Returns the vector v converted to double precision.
+
+  -- REQUIRED : v /= null;
+
+    res : Standard_Complex_VecVecs.Link_to_VecVec;
+    ddv : Standard_Complex_VecVecs.VecVec(v'range);
+
+    use QuadDobl_Complex_Vectors_cv;
+
+  begin
+    for i in ddv'range loop
+      declare
+        vec : constant Standard_Complex_Vectors.Vector
+            := QuadDobl_Complex_to_Standard(v(i).all);
+      begin
+        ddv(i) := new Standard_Complex_Vectors.Vector'(vec);
+      end;
+    end loop;
+    res := new Standard_Complex_VecVecs.VecVec'(ddv);
+    return res;
+  end to_double;
+
+  procedure Benchmark ( dim,deg,nbr,pwr : in integer32 ) is
+
+  -- DESCRIPTION :
+  --   Generates a random Newton homotopy in quad double precision,
+  --   and runs benchmark tests in all three precisions.
+
+  -- ON ENTRY :
+  --   dim      dimension of the exponent vectors;
+  --   deg      degree of the power series;
+  --   nbr      number of products;
+  --   pwr      largest power of the variables.
+
+    qds : QuadDobl_Speelpenning_Convolutions.Link_to_System;
+    dds : DoblDobl_Speelpenning_Convolutions.Link_to_System;
+    d_s : Standard_Speelpenning_Convolutions.Link_to_System;
+    qdx : QuadDobl_Complex_VecVecs.Link_to_VecVec;
+    ddx : DoblDobl_Complex_VecVecs.Link_to_VecVec;
+    d_x : Standard_Complex_VecVecs.Link_to_VecVec;
+    qd_values : QuadDobl_Complex_VecVecs.VecVec(1..dim) := Allocate(dim,dim);
+    dd_values : DoblDobl_Complex_VecVecs.VecVec(1..dim) := Allocate(dim,dim);
+    d_values : Standard_Complex_VecVecs.VecVec(1..dim) := Allocate(dim,dim);
+    qd_jmvls : QuadDobl_Complex_Vectors.Vector(1..dim);
+    dd_jmvls : DoblDobl_Complex_Vectors.Vector(1..dim);
+    d_jmvls : Standard_Complex_Vectors.Vector(1..dim);
+    qdlnk : QuadDobl_Complex_Vectors.Link_to_Vector;
+    ddlnk : DoblDobl_Complex_Vectors.Link_to_Vector;
+    dlnk : Standard_Complex_Vectors.Link_to_Vector;
+    qdvx : QuadDobl_Complex_Vectors.Vector(1..dim);
+    ddvx : DoblDobl_Complex_Vectors.Vector(1..dim);
+    dvx : Standard_Complex_Vectors.Vector(1..dim);
+    nbt : integer32 := 0;
+    start,stop : Ada.Calendar.Time;
+
+  begin
+    new_line;
+    put("Give the number of tasks : "); get(nbt);
+    QuadDobl_Random_Newton_Homotopy(dim,deg,nbr,pwr,qds,qdx);
+    dds := System_Convolution_Circuits.to_double_double(qds);
+    ddx := to_double_double(qdx);
+    d_s := System_Convolution_Circuits.to_double(qds);
+    d_x := to_double(qdx);
+    for i in 1..dim loop
+      qdlnk := qdx(i); ddlnk := ddx(i); dlnk := d_x(i);
+      qdvx(i) := qdlnk(0); ddvx(i) := ddlnk(0); dvx(i) := dlnk(0);
+    end loop;
+    put_line("running in double precision ...");
+    start := Ada.Calendar.Clock;
+    Multitasked_Singular_Values(nbt,d_s,dvx,d_jmvls,d_values,false);
+    stop := Ada.Calendar.Clock;
+    put("-> Elapsed time with "); put(nbt,1); put_line(" tasks :");
+    Time_Stamps.Write_Elapsed_Time(standard_output,start,stop);
+    put_line("running in double double precision ...");
+    start := Ada.Calendar.Clock;
+    Multitasked_Singular_Values(nbt,dds,ddvx,dd_jmvls,dd_values,false);
+    stop := Ada.Calendar.Clock;
+    put("-> Elapsed time with "); put(nbt,1); put_line(" tasks :");
+    Time_Stamps.Write_Elapsed_Time(standard_output,start,stop);
+    put_line("running in quad double precision ...");
+    start := Ada.Calendar.Clock;
+    Multitasked_Singular_Values(nbt,qds,qdvx,qd_jmvls,qd_values,false);
+    stop := Ada.Calendar.Clock;
+    put("-> Elapsed time with "); put(nbt,1); put_line(" tasks :");
+    Time_Stamps.Write_Elapsed_Time(standard_output,start,stop);
+  end Benchmark;
+
   procedure Main is
 
   -- DESCRIPTION :
@@ -376,7 +497,7 @@ procedure ts_mthessian is
   --   to generate a random problem.
 
     dim,deg,nbr,pwr : integer32 := 0;
-    precision : character;
+    ans,precision : character;
 
   begin
     new_line;
@@ -387,18 +508,25 @@ procedure ts_mthessian is
     put("Give the number of terms in each circuit : "); get(nbr);
     put("Give the largest power of the variables : "); get(pwr);
     new_line;
-    put_line("MENU for the working precision :");
-    put_line("  0. double precision");
-    put_line("  1. double double precision");
-    put_line("  2. quad double precision");
-    put("Type 0, 1, or 2 to select the precision : ");
-    Ask_Alternative(precision,"012");
-    case precision is
-      when '0' => Standard_Random_Test(dim,deg,nbr,pwr);
-      when '1' => DoblDobl_Random_Test(dim,deg,nbr,pwr);
-      when '2' => QuadDobl_Random_Test(dim,deg,nbr,pwr);
-      when others => null;
-    end case;
+    put("Benchmark for all precisions ? (y/n) ");
+    Ask_Yes_or_No(ans);
+    if ans = 'y' then
+      Benchmark(dim,deg,nbr,pwr);
+    else
+      new_line;
+      put_line("MENU for the working precision :");
+      put_line("  0. double precision");
+      put_line("  1. double double precision");
+      put_line("  2. quad double precision");
+      put("Type 0, 1, or 2 to select the precision : ");
+      Ask_Alternative(precision,"012");
+      case precision is
+        when '0' => Standard_Random_Test(dim,deg,nbr,pwr);
+        when '1' => DoblDobl_Random_Test(dim,deg,nbr,pwr);
+        when '2' => QuadDobl_Random_Test(dim,deg,nbr,pwr);
+        when others => null;
+      end case;
+    end if;
   end Main;
 
 begin
