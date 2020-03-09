e16e3ff1f7ad62e095b2636bd3690f51bd8f54ec
Jan Verschelde <janv@uic.edu>
2020-03-08 21:05:15 -0500

    added the application of the SVD in the test on the Newton-Fabry-Pade predictor on convolutions

--- src/Ada/Deformations/Curves/ts_padepcnv.adb
+++ src/Ada/Deformations/Curves/ts_padepcnv.adb
@@ -18,10 +18,16 @@ with QuadDobl_Complex_Numbers;
 with QuadDobl_Complex_Numbers_io;        use QuadDobl_Complex_Numbers_io;
 with Standard_Complex_Vectors;
 with Standard_Complex_Vectors_io;        use Standard_Complex_Vectors_io;
+with Standard_Complex_VecVecs;
+with Standard_Complex_Matrices;
 with DoblDobl_Complex_Vectors;
 with DoblDobl_Complex_Vectors_io;        use DoblDobl_Complex_Vectors_io;
+with DoblDobl_Complex_VecVecs;
+with DoblDobl_Complex_Matrices;
 with QuadDobl_Complex_Vectors;
 with QuadDobl_Complex_Vectors_io;        use QuadDobl_Complex_Vectors_io;
+with QuadDobl_Complex_VecVecs;
+with QuadDobl_Complex_Matrices;
 with Standard_Integer_VecVecs_io;
 with Standard_Complex_Poly_Systems;
 with DoblDobl_Complex_Poly_Systems;
@@ -61,6 +67,7 @@ procedure ts_padepcnv is
               ( hom : in Standard_Speelpenning_Convolutions.Link_to_System;
                 prd : in Standard_Predictor_Convolutions.Link_to_Predictor;
                 maxit : in integer32; tol : in double_float;
+                usesvd : in boolean;
                 fail : out boolean; output : in boolean ) is
 
   -- DESCRIPTION :
@@ -68,26 +75,64 @@ procedure ts_padepcnv is
   --   applies Fabry's theorem and constructs Pade approximants
   --   to predict the next solution, in double precision.
 
+  -- ON ENTRY :
+  --   hom      homotopy convolution circuit system
+  --   prd      predictor data for LU Newton and Pade approximants;
+  --   maxit    maximum number of iterations in Newton's method;
+  --   tol      tolerance on the correction term;
+  --   usesvd   flag to indicate if the SVD has to be used;
+  --   output   flag to indicate extra output during computations.
+
+  -- ON RETURN :
+  --   prd      contains solution series and Pade approximants;
+  --   fail     indicates failure status.
+
     use Standard_Rational_Approximations;
     use Standard_Speelpenning_Convolutions;
     use Standard_Predictor_Convolutions;
 
     z : Standard_Complex_Numbers.Complex_Number;
-    r,err,absdx : double_float;
+    r,err,absdx,rcond : double_float;
     eva : Standard_Complex_Vectors.Vector(1..prd.dim);
     res : Standard_Complex_Vectors.Vector(hom.crc'range);
     info,nbrit : integer32;
+    dim : constant integer32 := prd.dim;
+    deg : constant integer32 := prd.deg;
+    neq : constant integer32 := hom.crc'last;
+    dx : Standard_Complex_VecVecs.VecVec(1..dim);
+    xd : Standard_Complex_VecVecs.VecVec(0..deg);
+    svl : Standard_Complex_Vectors.Vector(1..dim+1);
+    U : Standard_Complex_Matrices.Matrix(1..neq,1..neq);
+    V : Standard_Complex_Matrices.Matrix(1..dim,1..dim);
+    ewrk : constant Standard_Complex_Vectors.Link_to_Vector
+         := new Standard_Complex_Vectors.Vector(1..dim);
 
   begin
     if output then
-      LU_Newton_Steps
-        (standard_output,hom,prd.sol,maxit,nbrit,tol,absdx,fail,
-         info,prd.newtpiv,prd.wrk,false);
+      if usesvd then
+        dx := Standard_Speelpenning_Convolutions.Allocate_Coefficients(dim,deg);
+        xd := Standard_Speelpenning_Convolutions.Linearized_Allocation(dim,deg);
+        SVD_Newton_Steps
+          (standard_output,hom,prd.sol,dx,xd,maxit,nbrit,tol,absdx,fail,
+           svl,U,V,info,rcond,ewrk,prd.wrk,false);
+      else
+        LU_Newton_Steps
+          (standard_output,hom,prd.sol,maxit,nbrit,tol,absdx,fail,
+           info,prd.newtpiv,prd.wrk,false);
+      end if;
       Convergence_Radius_Estimates.Fabry(prd.sol,z,r,err,fail);
     else
-      LU_Newton_Steps
-        (hom,prd.sol,maxit,nbrit,tol,absdx,fail,
-         info,prd.newtpiv,prd.wrk,false,false);
+      if usesvd then
+        dx := Standard_Speelpenning_Convolutions.Allocate_Coefficients(dim,deg);
+        xd := Standard_Speelpenning_Convolutions.Linearized_Allocation(dim,deg);
+        SVD_Newton_Steps
+          (hom,prd.sol,dx,xd,maxit,nbrit,tol,absdx,fail,
+           svl,U,V,info,rcond,ewrk,prd.wrk,false,false);
+      else
+        LU_Newton_Steps
+          (hom,prd.sol,maxit,nbrit,tol,absdx,fail,
+           info,prd.newtpiv,prd.wrk,false,false);
+      end if;
       Convergence_Radius_Estimates.Fabry(prd.sol,z,r,err,fail,false);
     end if;
     if not fail then
@@ -108,6 +153,7 @@ procedure ts_padepcnv is
               ( hom : in DoblDobl_Speelpenning_Convolutions.Link_to_System;
                 prd : in DoblDobl_Predictor_Convolutions.Link_to_Predictor;
                 maxit : in integer32; tol : in double_float;
+                usesvd : in boolean;
                 fail : out boolean; output : in boolean ) is
 
   -- DESCRIPTION :
@@ -115,26 +161,69 @@ procedure ts_padepcnv is
   --   applies Fabry's theorem and constructs Pade approximants
   --   to predict the next solution, in double double precision.
 
+  -- DESCRIPTION :
+  --   Runs Newton's method on the power series in prd,
+  --   applies Fabry's theorem and constructs Pade approximants
+  --   to predict the next solution, in double precision.
+
+  -- ON ENTRY :
+  --   hom      homotopy convolution circuit system
+  --   prd      predictor data for LU Newton and Pade approximants;
+  --   maxit    maximum number of iterations in Newton's method;
+  --   tol      tolerance on the correction term;
+  --   usesvd   flag to indicate if the SVD has to be used;
+  --   output   flag to indicate extra output during computations.
+
+  -- ON RETURN :
+  --   prd      contains solution series and Pade approximants;
+  --   fail     indicates failure status.
+
     use DoblDobl_Rational_Approximations;
     use DoblDobl_Speelpenning_Convolutions;
     use DoblDobl_Predictor_Convolutions;
 
     z : DoblDobl_Complex_Numbers.Complex_Number;
-    r,err,absdx : double_double;
+    r,err,absdx,rcond : double_double;
     eva : DoblDobl_Complex_Vectors.Vector(1..prd.dim);
     res : DoblDobl_Complex_Vectors.Vector(hom.crc'range);
     info,nbrit : integer32;
+    dim : constant integer32 := prd.dim;
+    deg : constant integer32 := prd.deg;
+    neq : constant integer32 := hom.crc'last;
+    dx : DoblDobl_Complex_VecVecs.VecVec(1..dim);
+    xd : DoblDobl_Complex_VecVecs.VecVec(0..deg);
+    svl : DoblDobl_Complex_Vectors.Vector(1..dim+1);
+    U : DoblDobl_Complex_Matrices.Matrix(1..neq,1..neq);
+    V : DoblDobl_Complex_Matrices.Matrix(1..dim,1..dim);
+    ewrk : constant DoblDobl_Complex_Vectors.Link_to_Vector
+         := new DoblDobl_Complex_Vectors.Vector(1..dim);
 
   begin
     if output then
-      LU_Newton_Steps
-        (standard_output,hom,prd.sol,maxit,nbrit,tol,absdx,fail,
-         info,prd.newtpiv,prd.wrk,false);
+      if usesvd then
+        dx := DoblDobl_Speelpenning_Convolutions.Allocate_Coefficients(dim,deg);
+        xd := DoblDobl_Speelpenning_Convolutions.Linearized_Allocation(dim,deg);
+        SVD_Newton_Steps
+          (standard_output,hom,prd.sol,dx,xd,maxit,nbrit,tol,absdx,fail,
+           svl,U,V,info,rcond,ewrk,prd.wrk,false);
+      else
+        LU_Newton_Steps
+          (standard_output,hom,prd.sol,maxit,nbrit,tol,absdx,fail,
+           info,prd.newtpiv,prd.wrk,false);
+      end if;
       Convergence_Radius_Estimates.Fabry(prd.sol,z,r,err,fail);
     else
-      LU_Newton_Steps
-        (hom,prd.sol,maxit,nbrit,tol,absdx,fail,
-         info,prd.newtpiv,prd.wrk,false,false);
+      if usesvd then
+        dx := DoblDobl_Speelpenning_Convolutions.Allocate_Coefficients(dim,deg);
+        xd := DoblDobl_Speelpenning_Convolutions.Linearized_Allocation(dim,deg);
+        SVD_Newton_Steps
+          (hom,prd.sol,dx,xd,maxit,nbrit,tol,absdx,fail,
+           svl,U,V,info,rcond,ewrk,prd.wrk,false,false);
+      else
+        LU_Newton_Steps
+          (hom,prd.sol,maxit,nbrit,tol,absdx,fail,
+           info,prd.newtpiv,prd.wrk,false,false);
+      end if;
       Convergence_Radius_Estimates.Fabry(prd.sol,z,r,err,fail,false);
     end if;
     if not fail then
@@ -155,6 +244,7 @@ procedure ts_padepcnv is
               ( hom : in QuadDobl_Speelpenning_Convolutions.Link_to_System;
                 prd : in QuadDobl_Predictor_Convolutions.Link_to_Predictor;
                 maxit : in integer32; tol : in double_float;
+                usesvd : in boolean;
                 fail : out boolean; output : in boolean ) is
 
   -- DESCRIPTION :
@@ -162,32 +252,75 @@ procedure ts_padepcnv is
   --   applies Fabry's theorem and constructs Pade approximants
   --   to predict the next solution, in quad double precision.
 
+  -- DESCRIPTION :
+  --   Runs Newton's method on the power series in prd,
+  --   applies Fabry's theorem and constructs Pade approximants
+  --   to predict the next solution, in double precision.
+
+  -- ON ENTRY :
+  --   hom      homotopy convolution circuit system
+  --   prd      predictor data for LU Newton and Pade approximants;
+  --   maxit    maximum number of iterations in Newton's method;
+  --   tol      tolerance on the correction term;
+  --   usesvd   flag to indicate if the SVD has to be used;
+  --   output   flag to indicate extra output during computations.
+
+  -- ON RETURN :
+  --   prd      contains solution series and Pade approximants;
+  --   fail     indicates failure status.
+
     use QuadDobl_Rational_Approximations;
     use QuadDobl_Speelpenning_Convolutions;
     use QuadDobl_Predictor_Convolutions;
 
     z : QuadDobl_Complex_Numbers.Complex_Number;
-    r,err,absdx : quad_double;
+    r,err,absdx,rcond : quad_double;
     eva : QuadDobl_Complex_Vectors.Vector(1..prd.dim);
     res : QuadDobl_Complex_Vectors.Vector(hom.crc'range);
     info,nbrit : integer32;
+    dim : constant integer32 := prd.dim;
+    deg : constant integer32 := prd.deg;
+    neq : constant integer32 := hom.crc'last;
+    dx : QuadDobl_Complex_VecVecs.VecVec(1..dim);
+    xd : QuadDobl_Complex_VecVecs.VecVec(0..deg);
+    svl : QuadDobl_Complex_Vectors.Vector(1..dim+1);
+    U : QuadDobl_Complex_Matrices.Matrix(1..neq,1..neq);
+    V : QuadDobl_Complex_Matrices.Matrix(1..dim,1..dim);
+    ewrk : constant QuadDobl_Complex_Vectors.Link_to_Vector
+         := new QuadDobl_Complex_Vectors.Vector(1..dim);
 
   begin
     if output then
-      LU_Newton_Steps
-        (standard_output,hom,prd.sol,maxit,nbrit,tol,absdx,fail,
-         info,prd.newtpiv,prd.wrk,false);
+      if usesvd then
+        dx := QuadDobl_Speelpenning_Convolutions.Allocate_Coefficients(dim,deg);
+        xd := QuadDobl_Speelpenning_Convolutions.Linearized_Allocation(dim,deg);
+        SVD_Newton_Steps
+          (standard_output,hom,prd.sol,dx,xd,maxit,nbrit,tol,absdx,fail,
+           svl,U,V,info,rcond,ewrk,prd.wrk,false);
+      else
+        LU_Newton_Steps
+          (standard_output,hom,prd.sol,maxit,nbrit,tol,absdx,fail,
+           info,prd.newtpiv,prd.wrk,false);
+      end if;
       Convergence_Radius_Estimates.Fabry(prd.sol,z,r,err,fail);
     else
-      LU_Newton_Steps
-        (hom,prd.sol,maxit,nbrit,tol,absdx,fail,
-         info,prd.newtpiv,prd.wrk,false,false);
+      if usesvd then
+        dx := QuadDobl_Speelpenning_Convolutions.Allocate_Coefficients(dim,deg);
+        xd := QuadDobl_Speelpenning_Convolutions.Linearized_Allocation(dim,deg);
+        SVD_Newton_Steps
+          (hom,prd.sol,dx,xd,maxit,nbrit,tol,absdx,fail,
+           svl,U,V,info,rcond,ewrk,prd.wrk,false,false);
+      else
+        LU_Newton_Steps
+          (hom,prd.sol,maxit,nbrit,tol,absdx,fail,
+           info,prd.newtpiv,prd.wrk,false,false);
+      end if;
       Convergence_Radius_Estimates.Fabry(prd.sol,z,r,err,fail,false);
     end if;
     if not fail then
       put("z : "); put(z); 
-      put("  error estimate :"); put(err,3); new_line;
-      put("estimated radius :"); put(r,3); new_line;
+      put("  error estimate : "); put(err,3); new_line;
+      put("estimated radius : "); put(r,3); new_line;
     end if;
     Pade_Vector(prd.numdeg,prd.dendeg,prd.sol,prd.numcff,prd.dencff,
                 prd.mat,prd.rhs,prd.padepiv,info,false);
@@ -215,19 +348,20 @@ procedure ts_padepcnv is
     ls : Link_to_Solution;
     maxit : integer32 := 0; 
     tol : constant double_float := 1.0E-12;
-    fail,otp : boolean;
+    fail,otp,usesvd : boolean;
     ans : character;
 
   begin
     put("Give the maximum number of iterations : "); get(maxit);
     put("Output during Newton's method ? (y/n) "); Ask_Yes_or_No(ans);
     otp := (ans = 'y');
+    put("Use SVD ? (y/n) "); Ask_Yes_or_No(ans); usesvd := (ans = 'y');
     for k in 1..Length_Of(sols) loop
       ls := Head_Of(tmp);
       declare
         prd : Link_to_Predictor := Create(ls.v,neq,deg,numdeg,dendeg);
       begin
-        Standard_Prediction(chom,prd,maxit,tol,fail,otp);
+        Standard_Prediction(chom,prd,maxit,tol,usesvd,fail,otp);
         Clear(prd);
       end;
       put("Continue to the next solution ? (y/n) ");
@@ -254,19 +388,20 @@ procedure ts_padepcnv is
     ls : Link_to_Solution;
     maxit : integer32 := 0; 
     tol : constant double_float := 1.0E-24;
-    fail,otp : boolean;
+    fail,otp,usesvd : boolean;
     ans : character;
 
   begin
     put("Give the maximum number of iterations : "); get(maxit);
     put("Output during Newton's method ? (y/n) "); Ask_Yes_or_No(ans);
     otp := (ans = 'y');
+    put("Use SVD ? (y/n) "); Ask_Yes_or_No(ans); usesvd := (ans = 'y');
     for k in 1..Length_Of(sols) loop
       ls := Head_Of(tmp);
       declare
         prd : Link_to_Predictor := Create(ls.v,neq,deg,numdeg,dendeg);
       begin
-        DoblDobl_Prediction(chom,prd,maxit,tol,fail,otp);
+        DoblDobl_Prediction(chom,prd,maxit,tol,usesvd,fail,otp);
         Clear(prd);
       end;
       put("Continue to the next solution ? (y/n) ");
@@ -293,19 +428,20 @@ procedure ts_padepcnv is
     ls : Link_to_Solution;
     maxit : integer32 := 0; 
     tol : constant double_float := 1.0E-48;
-    fail,otp : boolean;
+    fail,otp,usesvd : boolean;
     ans : character;
 
   begin
     put("Give the maximum number of iterations : "); get(maxit);
     put("Output during Newton's method ? (y/n) "); Ask_Yes_or_No(ans);
     otp := (ans = 'y');
+    put("Use SVD ? (y/n) "); Ask_Yes_or_No(ans); usesvd := (ans = 'y');
     for k in 1..Length_Of(sols) loop
       ls := Head_Of(tmp);
       declare
         prd : Link_to_Predictor := Create(ls.v,neq,deg,numdeg,dendeg);
       begin
-        QuadDobl_Prediction(chom,prd,maxit,tol,fail,otp);
+        QuadDobl_Prediction(chom,prd,maxit,tol,usesvd,fail,otp);
         Clear(prd);
       end;
       put("Continue to the next solution ? (y/n) ");
