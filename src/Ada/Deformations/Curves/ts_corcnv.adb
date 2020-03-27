ac628d20b8523cb75902bbefce3314843c2e0cde
Jan Verschelde <janv@uic.edu>
2020-03-26 22:20:00 -0500

    added procedures to evaluate series coefficients at a positive step size

--- src/Ada/Deformations/Curves/ts_corcnv.adb
+++ src/Ada/Deformations/Curves/ts_corcnv.adb
@@ -2,8 +2,12 @@ with text_io;                            use text_io;
 with Communications_with_User;           use Communications_with_User;
 with Standard_Integer_Numbers;           use Standard_Integer_Numbers;
 with Standard_Floating_Numbers;          use Standard_Floating_Numbers;
+with Standard_Floating_Numbers_io;       use Standard_Floating_Numbers_io;
 with Double_Double_Numbers;              use Double_Double_Numbers;
 with Quad_Double_Numbers;                use Quad_Double_Numbers;
+with Standard_Complex_Numbers;
+with DoblDobl_Complex_Numbers;
+with QuadDobl_Complex_Numbers;
 with Standard_Integer_Vectors;
 with Standard_Integer_VecVecs_io;
 with Standard_Complex_Vectors;
@@ -25,6 +29,165 @@ procedure ts_corcnv is
 
 -- DESCRIPTION :
 --   Development of the corrector convolutions.
+
+  function Step_Coefficient
+              ( c : Standard_Complex_Vectors.Vector; t : double_float )
+              return Standard_Complex_Numbers.Complex_Number is
+
+  -- DESCRIPTION :
+  --   Given in c is the coefficient vector of a series
+  --   and in t is some step size, t > 0.
+  --   Returns the value of the series c evaluated at t.
+
+    use Standard_Complex_Numbers;
+
+    res : Complex_Number := c(c'last);
+
+  begin
+    for k in reverse 0..c'last-1 loop
+      res := res*t + c(k);
+    end loop;
+    return res;
+  end Step_Coefficient;
+
+  function Step_Coefficient
+              ( c : DoblDobl_Complex_Vectors.Vector; t : double_double )
+              return DoblDobl_Complex_Numbers.Complex_Number is
+
+  -- DESCRIPTION :
+  --   Given in c is the coefficient vector of a series
+  --   and in t is some step size, t > 0.
+  --   Returns the value of the series c evaluated at t.
+
+    use DoblDobl_Complex_Numbers;
+
+    res : Complex_Number := c(c'last);
+
+  begin
+    for k in reverse 0..c'last-1 loop
+      res := res*t + c(k);
+    end loop;
+    return res;
+  end Step_Coefficient;
+
+  function Step_Coefficient
+              ( c : QuadDobl_Complex_Vectors.Vector; t : quad_double )
+              return QuadDobl_Complex_Numbers.Complex_Number is
+
+  -- DESCRIPTION :
+  --   Given in c is the coefficient vector of a series
+  --   and in t is some step size, t > 0.
+  --   Returns the value of the series c evaluated at t.
+
+    use QuadDobl_Complex_Numbers;
+
+    res : Complex_Number := c(c'last);
+
+  begin
+    for k in reverse 0..c'last-1 loop
+      res := res*t + c(k);
+    end loop;
+    return res;
+  end Step_Coefficient;
+
+  procedure Step_Coefficient
+              ( c : in Standard_Speelpenning_Convolutions.Link_to_Circuit;
+                t : in double_float ) is
+
+  -- DESCRIPTION :
+  --   Replaces the leading coefficient of all series in hom by
+  --   the series coefficient evaluated at t.
+
+    use Standard_Complex_Vectors;
+
+  begin
+    if c.cst /= null
+     then c.cst(0) := Step_Coefficient(c.cst.all,t);
+    end if;
+    for k in c.cff'range loop
+      c.cff(k)(0) := Step_Coefficient(c.cff(k).all,t);
+    end loop;
+  end Step_Coefficient;
+
+  procedure Step_Coefficient
+              ( c : in DoblDobl_Speelpenning_Convolutions.Link_to_Circuit;
+                t : in double_double ) is
+
+  -- DESCRIPTION :
+  --   Replaces the leading coefficient of all series in hom by
+  --   the series coefficient evaluated at t.
+
+    use DoblDobl_Complex_Vectors;
+
+  begin
+    if c.cst /= null
+     then c.cst(0) := Step_Coefficient(c.cst.all,t);
+    end if;
+    for k in c.cff'range loop
+      c.cff(k)(0) := Step_Coefficient(c.cff(k).all,t);
+    end loop;
+  end Step_Coefficient;
+
+  procedure Step_Coefficient
+              ( c : in QuadDobl_Speelpenning_Convolutions.Link_to_Circuit;
+                t : in quad_double ) is
+
+  -- DESCRIPTION :
+  --   Replaces the leading coefficient of all series in hom by
+  --   the series coefficient evaluated at t.
+
+    use QuadDobl_Complex_Vectors;
+
+  begin
+    if c.cst /= null
+     then c.cst(0) := Step_Coefficient(c.cst.all,t);
+    end if;
+    for k in c.cff'range loop
+      c.cff(k)(0) := Step_Coefficient(c.cff(k).all,t);
+    end loop;
+  end Step_Coefficient;
+
+  procedure Step_Coefficient
+              ( hom : in Standard_Speelpenning_Convolutions.Link_to_System;
+                t : in double_float ) is
+
+  -- DESCRIPTION :
+  --   Replaces the leading coefficient of all series in hom by
+  --   the series coefficient evaluated at t.
+ 
+  begin
+    for k in hom.crc'range loop
+      Step_Coefficient(hom.crc(k),t);
+    end loop;
+  end Step_Coefficient;
+
+  procedure Step_Coefficient
+              ( hom : in DoblDobl_Speelpenning_Convolutions.Link_to_System;
+                t : in double_double ) is
+
+  -- DESCRIPTION :
+  --   Replaces the leading coefficient of all series in hom by
+  --   the series coefficient evaluated at t.
+ 
+  begin
+    for k in hom.crc'range loop
+      Step_Coefficient(hom.crc(k),t);
+    end loop;
+  end Step_Coefficient;
+
+  procedure Step_Coefficient
+              ( hom : in QuadDobl_Speelpenning_Convolutions.Link_to_System;
+                t : in quad_double ) is
+
+  -- DESCRIPTION :
+  --   Replaces the leading coefficient of all series in hom by
+  --   the series coefficient evaluated at t.
+ 
+  begin
+    for k in hom.crc'range loop
+      Step_Coefficient(hom.crc(k),t);
+    end loop;
+  end Step_Coefficient;
 
   procedure Standard_Run_Newton
               ( chom : in Standard_Speelpenning_Convolutions.Link_to_System;
@@ -131,9 +294,10 @@ procedure ts_corcnv is
     cnvhom : Standard_Speelpenning_Convolutions.Link_to_System;
     idxpar : integer32;
     ans : character;
+    step : double_float := 0.0;
 
   begin
-    Standard_Homotopy_Convolutions_io.get(0,cnvhom,sols,idxpar);
+    Standard_Homotopy_Convolutions_io.get(2,cnvhom,sols,idxpar);
     put_line("The exponents in the circuits :");
     for k in cnvhom.crc'range loop
       Standard_Integer_VecVecs_io.put(cnvhom.crc(k).xps);
@@ -141,6 +305,11 @@ procedure ts_corcnv is
     put("Check all start solutions ? (y/n) "); Ask_Yes_or_No(ans);
     if ans = 'y' then
       Test_Predictor_Convolutions.Standard_Check_Solutions(cnvhom,sols);
+    end if;
+    put("Evaluate for a positive step ? (y/n) "); Ask_Yes_or_No(ans);
+    if ans = 'y' then
+      put("Give the step : "); get(step);
+      Step_Coefficient(cnvhom,step);
     end if;
     Standard_Run_Newton(cnvhom,sols);
   end Standard_Test_Correction;
@@ -154,9 +323,11 @@ procedure ts_corcnv is
     cnvhom : DoblDobl_Speelpenning_Convolutions.Link_to_System;
     idxpar : integer32;
     ans : character;
+    step : double_float := 0.0;
+    ddstep : double_double;
 
   begin
-    DoblDobl_Homotopy_Convolutions_io.get(0,cnvhom,sols,idxpar);
+    DoblDobl_Homotopy_Convolutions_io.get(2,cnvhom,sols,idxpar);
     put_line("The exponents in the circuits :");
     for k in cnvhom.crc'range loop
       Standard_Integer_VecVecs_io.put(cnvhom.crc(k).xps);
@@ -164,6 +335,16 @@ procedure ts_corcnv is
     put("Check all start solutions ? (y/n) "); Ask_Yes_or_No(ans);
     if ans = 'y' then
       Test_Predictor_Convolutions.DoblDobl_Check_Solutions(cnvhom,sols);
+    end if;
+    put_line("The exponents in the circuits :");
+    for k in cnvhom.crc'range loop
+      Standard_Integer_VecVecs_io.put(cnvhom.crc(k).xps);
+    end loop;
+    put("Evaluate for a positive step ? (y/n) "); Ask_Yes_or_No(ans);
+    if ans = 'y' then
+      put("Give the step : "); get(step);
+      ddstep := Create(step);
+      Step_Coefficient(cnvhom,ddstep);
     end if;
     DoblDobl_Run_Newton(cnvhom,sols);
   end DoblDobl_Test_Correction;
@@ -177,9 +358,11 @@ procedure ts_corcnv is
     cnvhom : QuadDobl_Speelpenning_Convolutions.Link_to_System;
     idxpar : integer32;
     ans : character;
+    step : double_float := 0.0;
+    qdstep : quad_double;
 
   begin
-    QuadDobl_Homotopy_Convolutions_io.get(0,cnvhom,sols,idxpar);
+    QuadDobl_Homotopy_Convolutions_io.get(2,cnvhom,sols,idxpar);
     put_line("The exponents in the circuits :");
     for k in cnvhom.crc'range loop
       Standard_Integer_VecVecs_io.put(cnvhom.crc(k).xps);
@@ -187,6 +370,12 @@ procedure ts_corcnv is
     put("Check all start solutions ? (y/n) "); Ask_Yes_or_No(ans);
     if ans = 'y' then
       Test_Predictor_Convolutions.QuadDobl_Check_Solutions(cnvhom,sols);
+    end if;
+    put("Evaluate for a positive step ? (y/n) "); Ask_Yes_or_No(ans);
+    if ans = 'y' then
+      put("Give the step : "); get(step);
+      qdstep := Create(step);
+      Step_Coefficient(cnvhom,qdstep);
     end if;
     QuadDobl_Run_Newton(cnvhom,sols);
   end QuadDobl_Test_Correction;
