aeb9c0d7485940329bd76ad37bd33c272661e7e2
Jan Verschelde <janv@uic.edu>
2020-05-02 15:58:21 -0500

    added tests on the performance of the complex inner products

--- src/Ada/Math_Lib/Circuits/ts_perfconv.adb
+++ src/Ada/Math_Lib/Circuits/ts_perfconv.adb
@@ -4,7 +4,9 @@ with Timing_Package;                      use Timing_Package;
 with Standard_Integer_Numbers;            use Standard_Integer_Numbers;
 with Standard_Integer_Numbers_io;         use Standard_Integer_Numbers_io;
 with Standard_Floating_Numbers;           use Standard_Floating_Numbers;
+with Standard_Floating_Numbers_io;        use Standard_Floating_Numbers_io;
 with Standard_Complex_Numbers;
+with Standard_Complex_Numbers_io;         use Standard_Complex_Numbers_io;
 with Standard_Floating_Vectors;
 with Standard_Complex_Vectors;
 with Standard_Complex_Vectors_io;         use Standard_Complex_Vectors_io;
@@ -23,7 +25,7 @@ procedure ts_perfconv is
   --   and stores the results in the product.
 
   -- REQUIRED :
-  --   All vectors have the same range.
+  --   All vectors have the same range and start at 0.
 
     deg : constant integer32 := first'last;
 
@@ -43,6 +45,9 @@ procedure ts_perfconv is
   --   Real parts of x, y, and z are stored in xr, yr, and zr.
   --   Imaginary parts of x, y, and z are stored in xi, yi, and zi.
 
+  -- REQUIRED :
+  --   All vectors have the same range and start at 0.
+
     deg : constant integer32 := xr'last;
 
   begin
@@ -51,6 +56,62 @@ procedure ts_perfconv is
       zi(k) := xi(k) + yi(k);
     end loop;
   end Add;
+
+  procedure Inner_Product
+              ( first,second : in Standard_Complex_Vectors.Link_to_Vector;
+                product : out Standard_Complex_Numbers.Complex_Number ) is
+
+  -- DESCRIPTION :
+  --   Computes the inner product of the vectors first and second
+  --   and stores the result in product.
+
+  -- REQUIRED :
+  --   All vectors have the same range and start at 0.
+
+    deg : constant integer32 := first'last;
+
+    use Standard_Complex_Numbers;
+
+  begin
+    product := Create(0.0);
+    for k in 0..deg loop
+      product := product + first(k)*second(k);
+    end loop;
+  end Inner_Product;
+
+  procedure Inner_Product
+              ( xr,xi,yr,yi : in Standard_Floating_Vectors.Link_to_Vector;
+                zr,zi : out double_float ) is
+
+  -- DESCRIPTION :
+  --   Computes the inner product of the vectors x and y,
+  --   with real parts in xr, yr, and imaginary parts in xi, yi.
+  --   The real part of the inner product is returned in zr
+  --   and the imaginary part is returned in zi.
+
+  -- REQUIRED :
+  --   All vectors have the same range and start at 0.
+
+    deg : constant integer32 := xr'last;
+    xrk,xik,yrk,yik,p1,p2,p3,p4 : double_float;
+
+    use Standard_Complex_Numbers;
+
+  begin
+   -- product := Create(0.0);
+    zr := 0.0; zi := 0.0;
+    for k in 0..deg loop
+     -- product := product + first(k)*second(k);
+      xrk := xr(k); xik := xi(k);
+      yrk := yr(k); yik := yi(k);
+      p1 := xrk*yrk;
+      p2 := xik*yik;
+      zr := zr + p1 - p2;
+      p3 := xik*yrk;
+      p4 := xrk*yik;
+      zi := zi + p3 + p4;
+    end loop;
+  end Inner_Product;
 
   procedure Multiply
               ( first,second : in Standard_Complex_Vectors.Link_to_Vector;
@@ -284,6 +345,120 @@ procedure ts_perfconv is
     put_line("recomputed : "); put_line(v);
   end Test_Add;
 
+  procedure Test_Inner ( deg : in integer32 ) is
+
+  -- DESCRIPTION :
+  --   Generates two random coefficients vectors of degree deg
+  --   and tests the inner product.
+
+    cx : constant Standard_Complex_Vectors.Vector(0..deg)
+       := Standard_Random_Vectors.Random_Vector(0,deg);
+    cy : constant Standard_Complex_Vectors.Vector(0..deg)
+       := Standard_Random_Vectors.Random_Vector(0,deg);
+    x : constant Standard_Complex_Vectors.Link_to_Vector
+      := new Standard_Complex_Vectors.Vector'(cx);
+    y : constant Standard_Complex_Vectors.Link_to_Vector
+      := new Standard_Complex_Vectors.Vector'(cy);
+    z : Standard_Complex_Numbers.Complex_Number;
+    rx : constant Standard_Floating_Vectors.Link_to_Vector := Real_Part(x);
+    ix : constant Standard_Floating_Vectors.Link_to_Vector := Imag_Part(x);
+    ry : constant Standard_Floating_Vectors.Link_to_Vector := Real_Part(y);
+    iy : constant Standard_Floating_Vectors.Link_to_Vector := Imag_Part(y);
+    rz,iz : double_float;
+
+  begin
+    Inner_Product(x,y,z);
+    put_line("the result : "); put(z); new_line;
+    Inner_Product(rx,ix,ry,iy,rz,iz);
+    put_line("recomputed : ");
+    put(rz); put("  "); put(iz); new_line;
+  end Test_Inner;
+
+  procedure Timing_Test_Add ( deg,frq : in integer32 ) is
+
+  -- DESCRIPTION :
+  --   Does as many additions as freq on random coefficient vectors
+  --   of series truncated to degree deg.
+
+    cx : constant Standard_Complex_Vectors.Vector(0..deg)
+       := Standard_Random_Vectors.Random_Vector(0,deg);
+    cy : constant Standard_Complex_Vectors.Vector(0..deg)
+       := Standard_Random_Vectors.Random_Vector(0,deg);
+    zero : constant Standard_Complex_Numbers.Complex_Number
+         := Standard_Complex_Numbers.Create(0.0);
+    cz : constant Standard_Complex_Vectors.Vector(0..deg)
+       := Standard_Complex_Vectors.Vector'(0..deg => zero);
+    x : constant Standard_Complex_Vectors.Link_to_Vector
+      := new Standard_Complex_Vectors.Vector'(cx);
+    y : constant Standard_Complex_Vectors.Link_to_Vector
+      := new Standard_Complex_Vectors.Vector'(cy);
+    z : constant Standard_Complex_Vectors.Link_to_Vector
+      := new Standard_Complex_Vectors.Vector'(cz);
+    rx : constant Standard_Floating_Vectors.Link_to_Vector := Real_Part(x);
+    ix : constant Standard_Floating_Vectors.Link_to_Vector := Imag_Part(x);
+    ry : constant Standard_Floating_Vectors.Link_to_Vector := Real_Part(y);
+    iy : constant Standard_Floating_Vectors.Link_to_Vector := Imag_Part(y);
+    rz : constant Standard_Floating_Vectors.Link_to_Vector := Real_Part(z);
+    iz : constant Standard_Floating_Vectors.Link_to_Vector := Imag_Part(z);
+    timer : Timing_Widget;
+
+  begin
+    tstart(timer);
+    for k in 1..frq loop
+      Add(x,y,z);
+    end loop;
+    tstop(timer);
+    new_line;
+    print_times(standard_output,timer,"complex add");
+    tstart(timer);
+    for k in 1..frq loop
+      Add(rx,ix,ry,iy,rz,iz);
+    end loop;
+    tstop(timer);
+    new_line;
+    print_times(standard_output,timer,"real add");
+  end Timing_Test_Add;
+
+  procedure Timing_Test_Inner ( deg,frq : in integer32 ) is
+
+  -- DESCRIPTION :
+  --   Does as many inner products as freq on random coefficient vectors
+  --   of series truncated to degree deg.
+
+    cx : constant Standard_Complex_Vectors.Vector(0..deg)
+       := Standard_Random_Vectors.Random_Vector(0,deg);
+    cy : constant Standard_Complex_Vectors.Vector(0..deg)
+       := Standard_Random_Vectors.Random_Vector(0,deg);
+    x : constant Standard_Complex_Vectors.Link_to_Vector
+      := new Standard_Complex_Vectors.Vector'(cx);
+    y : constant Standard_Complex_Vectors.Link_to_Vector
+      := new Standard_Complex_Vectors.Vector'(cy);
+    z : Standard_Complex_Numbers.Complex_Number;
+    rx : constant Standard_Floating_Vectors.Link_to_Vector := Real_Part(x);
+    ix : constant Standard_Floating_Vectors.Link_to_Vector := Imag_Part(x);
+    ry : constant Standard_Floating_Vectors.Link_to_Vector := Real_Part(y);
+    iy : constant Standard_Floating_Vectors.Link_to_Vector := Imag_Part(y);
+    rz,iz : double_float;
+    timer : Timing_Widget;
+
+  begin
+    tstart(timer);
+    for k in 1..frq loop
+      Inner_Product(x,y,z);
+    end loop;
+    tstop(timer);
+    new_line;
+    print_times(standard_output,timer,"complex inner product");
+    tstart(timer);
+    for k in 1..frq loop
+      Inner_Product(rx,ix,ry,iy,rz,iz);
+      z := Standard_Complex_Numbers.Create(rz,iz);
+    end loop;
+    tstop(timer);
+    new_line;
+    print_times(standard_output,timer,"real inner product");
+  end Timing_Test_Inner;
+
   procedure Timing_Test_Multiply ( deg,frq : in integer32 ) is
 
   -- DESCRIPTION :
@@ -336,52 +511,6 @@ procedure ts_perfconv is
     print_times(standard_output,timer,"real multiply 2");
   end Timing_Test_Multiply;
 
-  procedure Timing_Test_Add ( deg,frq : in integer32 ) is
-
-  -- DESCRIPTION :
-  --   Does as many convolutions as freq on random coefficient vectors
-  --   of series truncated to degree deg.
-
-    cx : constant Standard_Complex_Vectors.Vector(0..deg)
-       := Standard_Random_Vectors.Random_Vector(0,deg);
-    cy : constant Standard_Complex_Vectors.Vector(0..deg)
-       := Standard_Random_Vectors.Random_Vector(0,deg);
-    zero : constant Standard_Complex_Numbers.Complex_Number
-         := Standard_Complex_Numbers.Create(0.0);
-    cz : constant Standard_Complex_Vectors.Vector(0..deg)
-       := Standard_Complex_Vectors.Vector'(0..deg => zero);
-    x : constant Standard_Complex_Vectors.Link_to_Vector
-      := new Standard_Complex_Vectors.Vector'(cx);
-    y : constant Standard_Complex_Vectors.Link_to_Vector
-      := new Standard_Complex_Vectors.Vector'(cy);
-    z : constant Standard_Complex_Vectors.Link_to_Vector
-      := new Standard_Complex_Vectors.Vector'(cz);
-    rx : constant Standard_Floating_Vectors.Link_to_Vector := Real_Part(x);
-    ix : constant Standard_Floating_Vectors.Link_to_Vector := Imag_Part(x);
-    ry : constant Standard_Floating_Vectors.Link_to_Vector := Real_Part(y);
-    iy : constant Standard_Floating_Vectors.Link_to_Vector := Imag_Part(y);
-    rz : constant Standard_Floating_Vectors.Link_to_Vector := Real_Part(z);
-    iz : constant Standard_Floating_Vectors.Link_to_Vector := Imag_Part(z);
-    timer : Timing_Widget;
-
-  begin
-    tstart(timer);
-    for k in 1..frq loop
-      Add(x,y,z);
-    end loop;
-    tstop(timer);
-    new_line;
-    print_times(standard_output,timer,"complex add");
-    tstart(timer);
-    for k in 1..frq loop
-      Add(rx,ix,ry,iy,rz,iz);
-    end loop;
-    tstop(timer);
-    new_line;
-    print_times(standard_output,timer,"real add");
-    tstart(timer);
-  end Timing_Test_Add;
-
   procedure Main is
 
   -- DESCRIPTION :
@@ -389,32 +518,39 @@ procedure ts_perfconv is
   --   and then launches the test.
 
     deg : integer32 := 0;
-    ans,addtest : character;
+    ans,opertest : character;
     freq : integer32 := 0;
 
   begin
     new_line;
     put("Give the degree of the series : "); get(deg);
     new_line;
-    put("Test addition ? (y/n) "); Ask_Yes_or_No(addtest);
+    put_line("MENU for the operations :");
+    put_line("  0. addition of two vectors");
+    put_line("  1. inner product of two vectors");
+    put_line("  2. convolution of two vectors");
+    put("Type 0, 1, or 2 to select the operation : ");
+    Ask_Alternative(opertest,"012");
     new_line;
     put("Interactive test ? (y/n) "); Ask_Yes_or_No(ans);
-    if addtest = 'y' then
-      if ans = 'y' then
-        Test_Add(deg);
-      else
-        new_line;
-        put("Give the frequency : "); get(freq);
-        Timing_Test_Add(deg,freq);
-      end if;
+    if ans /= 'y'
+     then put("Give the frequency : "); get(freq);
+    end if;
+    if freq = 0 then
+      new_line;
+      case opertest is
+        when '0' => Test_Add(deg);
+        when '1' => Test_Inner(deg);
+        when '2' => Test_Multiply(deg);
+        when others => null;
+      end case;
     else
-      if ans = 'y' then
-        Test_Multiply(deg);
-      else
-        new_line;
-        put("Give the frequency : "); get(freq);
-        Timing_Test_Multiply(deg,freq);
-      end if;
+      case opertest is
+        when '0' => Timing_Test_Add(deg,freq);
+        when '1' => Timing_Test_Inner(deg,freq);
+        when '2' => Timing_Test_Multiply(deg,freq);
+        when others => null;
+      end case;
     end if;
   end Main;
 
