882a55def025bc00e2b1761571b04da2169bbc2c
Jan Verschelde <janv@uic.edu>
2020-05-03 15:53:52 -0500

    new package to split/merge vectors of complex numbers

---
+++ src/Ada/Math_Lib/Vectors/standard_vector_splitters.adb
@@ -0,0 +1,215 @@
+with Standard_Integer_Numbers;            use Standard_Integer_Numbers;
+with Standard_Floating_Numbers;           use Standard_Floating_Numbers;
+with Standard_Complex_Numbers;
+
+package body Standard_Vector_Splitters is
+
+  function Allocate_Floating_Coefficients
+             ( deg : integer32 )
+             return Standard_Floating_Vectors.Link_to_Vector is
+
+    cff : Standard_Floating_Vectors.Vector(0..deg) := (0..deg => 0.0);
+    res : constant Standard_Floating_Vectors.Link_to_Vector
+        := new Standard_Floating_Vectors.Vector'(cff);
+
+  begin
+    return res;
+  end Allocate_Floating_Coefficients;
+
+  function Allocate_Complex_Coefficients
+             ( deg : integer32 )
+             return Standard_Complex_Vectors.Link_to_Vector is
+
+    zero : constant Standard_Complex_Numbers.Complex_Number
+         := Standard_Complex_Numbers.Create(0.0);
+    cff : Standard_Complex_Vectors.Vector(0..deg) := (0..deg => zero);
+    res : constant Standard_Complex_Vectors.Link_to_Vector
+        := new Standard_Complex_Vectors.Vector'(cff);
+
+  begin
+    return res;
+  end Allocate_Complex_Coefficients;
+
+  function Allocate_Floating_Coefficients
+             ( dim,deg : integer32 )
+             return Standard_Floating_VecVecs.Link_to_VecVec is
+
+    res : Standard_Floating_VecVecs.Link_to_VecVec;
+    cff : Standard_Floating_VecVecs.VecVec(1..dim);
+
+  begin
+    for k in cff'range loop
+      cff(k) := Allocate_Floating_Coefficients(deg);
+    end loop;
+    res := new Standard_Floating_VecVecs.VecVec'(cff);
+    return res;
+  end Allocate_Floating_Coefficients;
+
+  function Allocate_Complex_Coefficients
+             ( dim,deg : integer32 )
+             return Standard_Complex_VecVecs.Link_to_VecVec is
+
+    res : Standard_Complex_VecVecs.Link_to_VecVec;
+    cff : Standard_Complex_VecVecs.VecVec(1..dim);
+
+  begin
+    for k in cff'range loop
+      cff(k) := Allocate_Complex_Coefficients(deg);
+    end loop;
+    res := new Standard_Complex_VecVecs.VecVec'(cff);
+    return res;
+  end Allocate_Complex_Coefficients;
+
+  function Real_Part ( x : Standard_Complex_Vectors.Link_to_Vector )
+                     return Standard_Floating_Vectors.Link_to_Vector is
+
+    res : Standard_Floating_Vectors.Link_to_Vector;
+    rpx : Standard_Floating_Vectors.Vector(x'range);
+
+  begin
+    for k in x'range loop
+      rpx(k) := Standard_Complex_Numbers.REAL_PART(x(k));
+    end loop;
+    res := new Standard_Floating_Vectors.Vector'(rpx);
+    return res;
+  end Real_Part;
+
+  function Real_Part ( x : Standard_Complex_VecVecs.Link_to_VecVec )
+                     return Standard_Floating_VecVecs.Link_to_VecVec is
+
+    res : Standard_Floating_VecVecs.Link_to_VecVec;
+    rpx : Standard_Floating_VecVecs.VecVec(x'range);
+
+  begin
+    for k in x'range loop
+      rpx(k) := Real_Part(x(k));
+    end loop;
+    res := new Standard_Floating_VecVecs.VecVec'(rpx);
+    return res;
+  end Real_Part;
+
+  function Imag_Part ( x : Standard_Complex_Vectors.Link_to_Vector )
+                     return Standard_Floating_Vectors.Link_to_Vector is
+
+    res : Standard_Floating_Vectors.Link_to_Vector;
+    ipx : Standard_Floating_Vectors.Vector(x'range);
+
+  begin
+    for k in x'range loop
+      ipx(k) := Standard_Complex_Numbers.IMAG_PART(x(k));
+    end loop;
+    res := new Standard_Floating_Vectors.Vector'(ipx);
+    return res;
+  end Imag_Part;
+
+  function Imag_Part ( x : Standard_Complex_VecVecs.Link_to_VecVec )
+                     return Standard_Floating_VecVecs.Link_to_VecVec is
+
+    res : Standard_Floating_VecVecs.Link_to_VecVec;
+    ipx : Standard_Floating_VecVecs.VecVec(x'range);
+
+  begin
+    for k in x'range loop
+      ipx(k) := Imag_Part(x(k));
+    end loop;
+    res := new Standard_Floating_VecVecs.VecVec'(ipx);
+    return res;
+  end Imag_Part;
+
+  procedure Split_Complex
+              ( x : in Standard_Complex_Vectors.Link_to_Vector;
+                rpx,ipx : out Standard_Floating_Vectors.Link_to_Vector ) is
+
+    rx,ix : Standard_Floating_Vectors.Vector(x'range);
+
+  begin
+    for k in x'range loop
+      rx(k) := Standard_Complex_Numbers.REAL_PART(x(k));
+      ix(k) := Standard_Complex_Numbers.IMAG_PART(x(k));
+    end loop;
+    rpx := new Standard_Floating_Vectors.Vector'(rx);
+    ipx := new Standard_Floating_Vectors.Vector'(ix);
+  end Split_Complex;
+
+  procedure Split_Complex
+              ( x : in Standard_Complex_VecVecs.Link_to_VecVec;
+                rpx,ipx : out Standard_Floating_VecVecs.Link_to_VecVec ) is
+
+    rx,ix : Standard_Floating_VecVecs.VecVec(x'range);
+
+  begin
+    for k in x'range loop
+      Split_Complex(x(k),rx(k),ix(k));
+    end loop;
+    rpx := new Standard_Floating_VecVecs.VecVec'(rx);
+    ipx := new Standard_Floating_VecVecs.VecVec'(ix);
+  end Split_Complex;
+
+  procedure Complex_Parts
+              ( x : in Standard_Complex_Vectors.Link_to_Vector;
+                rpx,ipx : in Standard_Floating_Vectors.Link_to_Vector ) is
+  begin
+    for k in x'range loop
+      rpx(k) := Standard_Complex_Numbers.REAL_PART(x(k));
+      ipx(k) := Standard_Complex_Numbers.IMAG_PART(x(k));
+    end loop;
+  end Complex_Parts;
+
+  procedure Complex_Parts
+              ( x : in Standard_Complex_VecVecs.Link_to_VecVec;
+                rpx,ipx : in Standard_Floating_VecVecs.Link_to_VecVec ) is
+  begin
+    for k in x'range loop
+      Split_Complex(x(k),rpx(k),ipx(k));
+    end loop;
+  end Complex_Parts;
+
+  function Make_Complex
+             ( rpx,ipx : Standard_Floating_Vectors.Link_to_Vector )
+             return Standard_Complex_Vectors.Link_to_Vector is
+
+    res : Standard_Complex_Vectors.Link_to_Vector;
+    cvx : Standard_Complex_Vectors.Vector(rpx'range);
+
+  begin
+    for k in cvx'range loop
+      cvx(k) := Standard_Complex_Numbers.Create(rpx(k),ipx(k));
+    end loop;
+    res := new Standard_Complex_Vectors.Vector'(cvx);
+    return res;
+  end Make_Complex;
+
+  function Make_Complex
+             ( rpx,ipx : Standard_Floating_VecVecs.Link_to_VecVec )
+             return Standard_Complex_VecVecs.Link_to_VecVec is
+
+    res : Standard_Complex_VecVecs.Link_to_VecVec;
+    cvx : Standard_Complex_VecVecs.VecVec(rpx'range);
+
+  begin
+    for k in cvx'range loop
+      cvx(k) := Make_Complex(rpx(k),ipx(k));
+    end loop;
+    res := new Standard_Complex_VecVecs.VecVec'(cvx);
+    return res;
+  end Make_Complex;
+
+  procedure Complex_Merge
+             ( rpx,ipx : in Standard_Floating_Vectors.Link_to_Vector;
+               cvx : in Standard_Complex_Vectors.Link_to_Vector ) is
+  begin
+    for k in cvx'range loop
+      cvx(k) := Standard_Complex_Numbers.Create(rpx(k),ipx(k));
+    end loop;
+  end Complex_Merge;
+
+  procedure Complex_Merge
+             ( rpx,ipx : in Standard_Floating_VecVecs.Link_to_VecVec;
+               cvx : in Standard_Complex_VecVecs.Link_to_VecVec ) is
+  begin
+    for k in cvx'range loop
+      Complex_Merge(rpx(k),ipx(k),cvx(k));
+    end loop;
+  end Complex_Merge;
+
+end Standard_Vector_Splitters;
---
+++ src/Ada/Math_Lib/Vectors/standard_vector_splitters.ads
@@ -0,0 +1,119 @@
+with Standard_Integer_Numbers;            use Standard_Integer_Numbers;
+with Standard_Floating_Vectors;
+with Standard_Floating_VecVecs;
+with Standard_Complex_Vectors;
+with Standard_Complex_VecVecs;
+
+package Standard_Vector_Splitters is
+
+-- DESCRIPTION :
+--   A vector splitter separates real and imaginary parts of complex vectors,
+--   for more efficient computations with the vectors.
+
+  function Allocate_Floating_Coefficients
+             ( deg : integer32 )
+             return Standard_Floating_Vectors.Link_to_Vector;
+
+  -- DESCRIPTION :
+  --   Returns allocated space for the real or imaginary parts of
+  --   complex coefficients of a series truncated to degree deg.
+
+  function Allocate_Complex_Coefficients
+             ( deg : integer32 )
+             return Standard_Complex_Vectors.Link_to_Vector;
+
+  -- DESCRIPTION :
+  --   Returns allocated space for the complex coefficients
+  --   of a series truncated to degree deg.
+
+  function Allocate_Floating_Coefficients
+             ( dim,deg : integer32 )
+             return Standard_Floating_VecVecs.Link_to_VecVec;
+
+  -- DESCRIPTION :
+  --   Returns allocated space for the real or imaginary parts of
+  --   coefficients of a vector of series, all truncated to degree deg.
+  --   The vector on return has range 1..dim.
+
+  function Allocate_Complex_Coefficients
+             ( dim,deg : integer32 )
+             return Standard_Complex_VecVecs.Link_to_VecVec;
+
+  -- DESCRIPTION :
+  --   Returns allocated space for the coefficients of a vector 
+  --   of series, all truncated to degree deg.
+  --   The vector on return has range 1..dim.
+
+  function Real_Part ( x : Standard_Complex_Vectors.Link_to_Vector )
+                     return Standard_Floating_Vectors.Link_to_Vector;
+  function Real_Part ( x : Standard_Complex_VecVecs.Link_to_VecVec )
+                     return Standard_Floating_VecVecs.Link_to_VecVec;
+
+  -- DESCRIPTION :
+  --   Returns the vector (or vector of vectors) of the real parts
+  --   of the complex vector x.  Memory is allocated.
+
+  function Imag_Part ( x : Standard_Complex_Vectors.Link_to_Vector )
+                     return Standard_Floating_Vectors.Link_to_Vector;
+  function Imag_Part ( x : Standard_Complex_VecVecs.Link_to_VecVec )
+                     return Standard_Floating_VecVecs.Link_to_VecVec;
+
+  -- DESCRIPTION :
+  --   Returns the vector (or vector of vectors) of the imaginary parts
+  --   of the complex vector x.  Memory is allocated.
+
+  procedure Split_Complex
+              ( x : in Standard_Complex_Vectors.Link_to_Vector;
+                rpx,ipx : out Standard_Floating_Vectors.Link_to_Vector );
+  procedure Split_Complex
+              ( x : in Standard_Complex_VecVecs.Link_to_VecVec;
+                rpx,ipx : out Standard_Floating_VecVecs.Link_to_VecVec );
+
+  -- DESCRIPTION :
+  --   Splits the complex vector (of vectors) x into two real vectors,
+  --   with its real and imaginary parts of the complex numbers in x.
+  --   Memory is allocated for the resulting vectors.
+
+  procedure Complex_Parts
+              ( x : in Standard_Complex_Vectors.Link_to_Vector;
+                rpx,ipx : in Standard_Floating_Vectors.Link_to_Vector );
+  procedure Complex_Parts
+              ( x : in Standard_Complex_VecVecs.Link_to_VecVec;
+                rpx,ipx : in Standard_Floating_VecVecs.Link_to_VecVec );
+
+  -- DESCRIPTION :
+  --   Parts the complex vector (of vectors) x into two real vectors,
+  --   with its real and imaginary parts of the complex numbers in x.
+
+  -- REQUIRED :
+  --   The vectors rpx and ipx are completely allocated
+  --   and have the same ranges as x.
+
+  function Make_Complex
+             ( rpx,ipx : Standard_Floating_Vectors.Link_to_Vector )
+             return Standard_Complex_Vectors.Link_to_Vector;
+  function Make_Complex
+             ( rpx,ipx : Standard_Floating_VecVecs.Link_to_VecVec )
+             return Standard_Complex_VecVecs.Link_to_VecVec;
+
+  -- DESCRIPTION :
+  --   Returns the vector (of vectors) of complex numbers,
+  --   with real and imaginary parts given in the vectors rpx and ipx.
+  --   Memory is allocated for the resulting vectors.
+
+  procedure Complex_Merge
+             ( rpx,ipx : in Standard_Floating_Vectors.Link_to_Vector;
+               cvx : in Standard_Complex_Vectors.Link_to_Vector );
+  procedure Complex_Merge
+             ( rpx,ipx : in Standard_Floating_VecVecs.Link_to_VecVec;
+               cvx : in Standard_Complex_VecVecs.Link_to_VecVec );
+
+  -- DESCRIPTION :
+  --   Merges the real and imaginary parts in rpx and ipx
+  --   into the vector (of vectors) of complex numbers.
+
+  -- REQUIRED :
+  --   The vector cvx is completely allocated
+  --   and has the same ranges as rpx and ipx.
+
+end Standard_Vector_Splitters;
