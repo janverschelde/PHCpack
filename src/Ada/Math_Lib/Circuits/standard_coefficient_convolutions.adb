9587d865d7e2fd08d4c656a5c3e99269b4e156c0
Jan Verschelde <janv@uic.edu>
2020-05-03 20:12:24 -0500

    extended the package to compute the convolutions more efficiently with procedure to compute the power table and to apply algorithmic differentiation

--- src/Ada/Math_Lib/Circuits/standard_coefficient_convolutions.adb
+++ src/Ada/Math_Lib/Circuits/standard_coefficient_convolutions.adb
@@ -1,51 +1,51 @@
+with unchecked_deallocation;
 with Standard_Integer_Numbers;            use Standard_Integer_Numbers;
 with Standard_Floating_Numbers;           use Standard_Floating_Numbers;
 with Standard_Complex_Numbers;
+with Standard_Vector_Splitters;
 
 package body Standard_Coefficient_Convolutions is
 
-  function Real_Part ( x : Standard_Complex_Vectors.Link_to_Vector )
-                     return Standard_Floating_Vectors.Link_to_Vector is
+  function Allocate ( mxe : Standard_Integer_Vectors.Vector;
+                      deg : integer32 )
+                    return Link_to_VecVecVec is
 
-    res : Standard_Floating_Vectors.Link_to_Vector;
-    rpx : Standard_Floating_Vectors.Vector(x'range);
+    res : Link_to_VecVecVec;
+    pwt : VecVecVec(mxe'range);
 
-  begin
-    for k in x'range loop
-      rpx(k) := Standard_Complex_Numbers.REAL_PART(x(k));
-    end loop;
-    res := new Standard_Floating_Vectors.Vector'(rpx);
-    return res;
-  end Real_Part;
-
-  function Imag_Part ( x : Standard_Complex_Vectors.Link_to_Vector )
-                     return Standard_Floating_Vectors.Link_to_Vector is
-
-    res : Standard_Floating_Vectors.Link_to_Vector;
-    ipx : Standard_Floating_Vectors.Vector(x'range);
+    use Standard_Vector_Splitters;
 
   begin
-    for k in x'range loop
-      ipx(k) := Standard_Complex_Numbers.IMAG_PART(x(k));
+    for i in mxe'range loop
+      if mxe(i) > 2
+       then pwt(i) := Allocate_Floating_Coefficients(mxe(i)-2,deg);
+      end if;
     end loop;
-    res := new Standard_Floating_Vectors.Vector'(ipx);
+    res := new VecVecVec'(pwt);
     return res;
-  end Imag_Part;
+  end Allocate;
 
-  function Make_Complex
-             ( rpx,ipx : Standard_Floating_Vectors.Link_to_Vector )
-             return Standard_Complex_Vectors.Link_to_Vector is
-
-    res : Standard_Complex_Vectors.Link_to_Vector;
-    cvx : Standard_Complex_Vectors.Vector(rpx'range);
-
+  procedure Create ( rx,ix : in Standard_Floating_VecVecs.VecVec;
+                     mxe : in Standard_Integer_Vectors.Vector;
+                     deg : in integer32;
+                     rpwt,ipwt : out Link_to_VecVecVec ) is
   begin
-    for k in cvx'range loop
-      cvx(k) := Standard_Complex_Numbers.Create(rpx(k),ipx(k));
+    rpwt := Allocate(mxe,deg);
+    ipwt := Allocate(mxe,deg);
+    Compute(rpwt,ipwt,mxe,rx,ix);
+  end Create;
+
+-- BASIC COMPUTATIONAL PROCEDURES :
+
+  procedure Update ( rvl,ivl : in Standard_Floating_Vectors.Link_to_Vector;
+                     rnc,inc : in Standard_Floating_Vectors.Link_to_Vector ) is
+  begin
+    for i in rvl'range loop
+      exit when (i > rnc'last);
+      rvl(i) := rvl(i) + rnc(i);
+      ivl(i) := ivl(i) + inc(i);
     end loop;
-    res := new Standard_Complex_Vectors.Vector'(cvx);
-    return res;
-  end Make_Complex;
+  end Update;
 
   procedure Multiply
               ( xr,xi,yr,yi : in Standard_Floating_Vectors.Link_to_Vector;
@@ -75,5 +75,226 @@ package body Standard_Coefficient_Convolutions is
       zr(k) := rpa; zi(k) := ipa;
     end loop;
   end Multiply;
+
+-- REVERSE MODE OF ALGORITHMIC DIFFERENTIATION :
+
+  procedure Speel ( rx,ix : in Standard_Floating_VecVecs.VecVec;
+                    rfwd,ifwd : in Standard_Floating_VecVecs.VecVec;
+                    rbck,ibck : in Standard_Floating_VecVecs.VecVec;
+                    rcrs,icrs : in Standard_Floating_VecVecs.VecVec ) is
+  begin
+    Multiply(rx(1),ix(1),rx(2),ix(2),rfwd(1),ifwd(1));
+    for k in 3..rx'last loop
+      Multiply(rfwd(k-2),ifwd(k-2),rx(k),ix(k),rfwd(k-1),ifwd(k-1));
+    end loop;
+    if rx'last > 2 then
+      Multiply(rx(rx'last),ix(ix'last),
+               rx(rx'last-1),ix(ix'last-1),rbck(1),ibck(1));
+      for k in 2..rx'last-2 loop
+        Multiply(rbck(k-1),ibck(k-1),rx(rx'last-k),ix(ix'last-k),
+                 rbck(k),ibck(k));
+      end loop;
+      if rx'last = 3 then
+        Multiply(rx(1),ix(1),rx(3),ix(3),rcrs(1),icrs(1));
+      else
+        Multiply(rx(1),ix(1),rbck(rx'last-3),ibck(ix'last-3),
+                 rcrs(1),icrs(1));
+        for k in 2..rx'last-3 loop
+          Multiply(rfwd(k-1),ifwd(k-1),rbck(rx'last-2-k),ibck(ix'last-2-k),
+                   rcrs(k),icrs(k));
+        end loop;
+        Multiply(rfwd(rx'last-3),ifwd(ix'last-3),rx(rx'last),ix(ix'last),
+                 rcrs(rx'last-2),icrs(ix'last-2));
+      end if;
+    end if;
+  end Speel;
+
+  procedure Speel ( rx,ix : in Standard_Floating_VecVecs.VecVec;
+                    idx : in Standard_Integer_Vectors.Vector;
+                    rfwd,ifwd : in Standard_Floating_VecVecs.VecVec;
+                    rbck,ibck : in Standard_Floating_VecVecs.VecVec;
+                    rcrs,icrs : in Standard_Floating_VecVecs.VecVec ) is
+
+    p,q,r : integer32;
+
+  begin
+    p := idx(1); q := idx(2);
+    Multiply(rx(p),ix(p),rx(q),ix(q),rfwd(1),ifwd(1));
+    for k in 3..idx'last loop
+      p := k-2; q := idx(k); r := k-1;
+      Multiply(rfwd(p),ifwd(p),rx(q),ix(q),rfwd(r),ifwd(r));
+    end loop;
+    if idx'last > 2 then
+      p := idx(idx'last); q := idx(idx'last-1);
+      Multiply(rx(p),ix(p),rx(q),ix(q),rbck(1),ibck(1));
+      for k in 2..idx'last-2 loop
+        p := k-1; q := idx(idx'last-k);
+        Multiply(rbck(p),ibck(p),rx(q),ix(q),rbck(k),ibck(k));
+      end loop;
+      if idx'last = 3 then
+        p := idx(1); q := idx(3);
+        Multiply(rx(p),ix(p),rx(q),ix(q),rcrs(1),icrs(1));
+      else
+        p := idx(1); q := idx'last-3;
+        Multiply(rx(p),ix(p),rbck(q),ibck(q),rcrs(1),icrs(1));
+        for k in 2..idx'last-3 loop
+          p := k-1; q := idx'last-2-k;
+          Multiply(rfwd(p),ifwd(p),rbck(q),ibck(q),rcrs(k),icrs(k));
+        end loop;
+        p := idx'last-3; q := idx(idx'last); r := idx'last-2;
+        Multiply(rfwd(p),ifwd(p),rx(q),ix(q),rcrs(r),icrs(r));
+      end if;
+    end if;
+  end Speel;
+
+  procedure Speel ( idx : in Standard_Integer_VecVecs.VecVec;
+                    rx,ix : in Standard_Floating_VecVecs.VecVec;
+                    rfwd,ifwd : in Standard_Floating_VecVecs.VecVec;
+                    rbck,ibck : in Standard_Floating_VecVecs.VecVec;
+                    rcrs,icrs : in Standard_Floating_VecVecs.VecVec;
+                    ryd,iyd : in Standard_Floating_VecVecs.VecVec ) is
+
+    use Standard_Integer_Vectors;
+
+    idk : Standard_Integer_Vectors.Link_to_Vector;
+    ryptr : constant Standard_Floating_Vectors.Link_to_Vector := ryd(ryd'last);
+    iyptr : constant Standard_Floating_Vectors.Link_to_Vector := iyd(iyd'last);
+    p : Standard_Floating_Vectors.Link_to_Vector;
+    q,r : integer32;
+
+  begin
+    for k in idx'range loop
+      idk := idx(k);
+      if idk /= null then
+        if idk'last = 1 then
+          q := idk(1);
+          Update(ryptr,iyptr,rx(q),ix(q));
+          p := ryd(q); p(0) := p(0) + 1.0;
+        else
+          Speel(rx,ix,idk.all,rfwd,ifwd,rbck,ibck,rcrs,icrs);
+          q := idk'last-1;
+          Update(ryptr,iyptr,rfwd(q),ifwd(q));
+          if idk'last = 2 then
+            q := idk(2); r := idk(1);
+            Update(ryd(q),iyd(q),rx(r),ix(r));
+            Update(ryd(r),iyd(r),rx(q),ix(q));
+          else -- idk'last > 2 
+            q := idk(1); r := idk'last-2;
+            Update(ryd(q),iyd(q),rbck(r),ibck(r));
+            for j in idk'first+1..idk'last-1 loop
+              q := idk(j); r := j-1;
+              Update(ryd(q),iyd(q),rcrs(r),icrs(r));
+            end loop;
+            q := idk(idk'last); r := idk'last-2;
+            Update(ryd(q),iyd(q),rfwd(r),ifwd(r));
+          end if;
+        end if;
+      end if;
+    end loop;
+  end Speel;
+
+  procedure Speel ( idx : in Standard_Integer_VecVecs.VecVec;
+                    rcff,icff : in Standard_Floating_VecVecs.VecVec;
+                    rx,ix : in Standard_Floating_VecVecs.VecVec;
+                    rfwd,ifwd : in Standard_Floating_VecVecs.VecVec;
+                    rbck,ibck : in Standard_Floating_VecVecs.VecVec;
+                    rcrs,icrs : in Standard_Floating_VecVecs.VecVec;
+                    ryd,iyd : in Standard_Floating_VecVecs.VecVec;
+                    rwrk : in Standard_Floating_Vectors.Link_to_Vector;
+                    iwrk : in Standard_Floating_Vectors.Link_to_Vector ) is
+
+    use Standard_Integer_Vectors;
+
+    idk : Standard_Integer_Vectors.Link_to_Vector;
+    ryptr : constant Standard_Floating_Vectors.Link_to_Vector := ryd(ryd'last);
+    iyptr : constant Standard_Floating_Vectors.Link_to_Vector := iyd(iyd'last);
+    rp,ip : Standard_Floating_Vectors.Link_to_Vector;
+    q,r : integer32;
+
+  begin
+    for k in idx'range loop
+      idk := idx(k);
+      if idk /= null then
+        rp := rcff(k); ip := icff(k);
+        if idk'last = 1 then
+          q := idk(1);
+          Multiply(rp,ip,rx(q),ix(q),rwrk,iwrk);
+          Update(ryptr,iyptr,rwrk,iwrk);
+          Update(ryd(q),iyd(q),rp,ip);
+        else
+          Speel(rx,ix,idk.all,rfwd,ifwd,rbck,ibck,rcrs,icrs);
+          q := idk'last-1;
+          Multiply(rp,ip,rfwd(q),ifwd(q),rwrk,iwrk);
+          Update(ryptr,iyptr,rwrk,iwrk);
+          if idk'last = 2 then
+            q := idk(2); r := idk(1);
+            Multiply(rp,ip,rx(r),ix(r),rwrk,iwrk);
+            Update(ryd(q),iyd(q),rwrk,iwrk);
+            Multiply(rp,ip,rx(q),ix(q),rwrk,iwrk);
+            Update(ryd(r),iyd(r),rwrk,iwrk);
+          else -- idk'last > 2 
+            q := idk(1); r := idk'last-2;
+            Multiply(rp,ip,rbck(r),ibck(r),rwrk,iwrk);
+            Update(ryd(q),iyd(q),rwrk,iwrk);
+            for j in idk'first+1..idk'last-1 loop
+              q := idk(j); r := j-1;
+              Multiply(rp,ip,rcrs(r),icrs(r),rwrk,iwrk);
+              Update(ryd(q),iyd(q),rwrk,iwrk);
+            end loop;
+            q := idk(idk'last); r := idk'last-2;
+            Multiply(rp,ip,rfwd(r),ifwd(r),rwrk,iwrk);
+            Update(ryd(q),iyd(q),rwrk,iwrk);
+          end if;
+        end if;
+      end if;
+    end loop;
+  end Speel;
+
+-- COMPUTING THE POWER TABLE :
+
+  procedure Compute ( rpwt,ipwt : in Link_to_VecVecVec;
+                      mxe : in Standard_Integer_Vectors.Vector;
+                      rx,ix : in Standard_Floating_VecVecs.VecVec ) is
+
+    rxpw,ixpw : Standard_Floating_VecVecs.Link_to_VecVec;
+
+  begin
+    for i in rx'range loop
+      if mxe(i) > 2 then
+        rxpw := rpwt(i); ixpw := ipwt(i);
+        Multiply(rx(i),ix(i),rx(i),ix(i),rxpw(1),ixpw(1));
+        for k in 2..(mxe(i)-2) loop
+          Multiply(rxpw(k-1),ixpw(k-1),rx(i),ix(i),rxpw(k),ixpw(k));
+        end loop;
+      end if;
+    end loop;
+  end Compute;
+
+-- DEALLOCATORS :
+
+  procedure Clear ( pwt : in out VecVecVec ) is
+  begin
+    for k in pwt'range loop
+      Standard_Floating_VecVecs.Deep_Clear(pwt(k));
+    end loop;
+  end Clear;
+
+  procedure Clear ( pwt : in out Link_to_VecVecVec ) is
+
+    procedure free is new unchecked_deallocation(VecVecVec,Link_to_VecVecVec);
+
+  begin
+    if pwt /= null then
+      Clear(pwt.all);
+      free(pwt);
+    end if;
+  end Clear;
+
+  procedure Clear ( pwt : in out VecVecVec_Array ) is
+  begin
+    for k in pwt'range loop
+      Clear(pwt(k));
+    end loop;
+  end Clear;
 
 end Standard_Coefficient_Convolutions;
--- src/Ada/Math_Lib/Circuits/standard_coefficient_convolutions.ads
+++ src/Ada/Math_Lib/Circuits/standard_coefficient_convolutions.ads
@@ -1,27 +1,59 @@
+with Standard_Integer_Numbers;            use Standard_Integer_Numbers;
+with Standard_Integer_Vectors;
+with Standard_Integer_VecVecs;
 with Standard_Floating_Vectors;
+with Standard_Floating_VecVecs;
 with Standard_Complex_Vectors;
+with Standard_Complex_VecVecs;
 
 package Standard_Coefficient_Convolutions is
 
-  function Real_Part ( x : Standard_Complex_Vectors.Link_to_Vector )
-                     return Standard_Floating_Vectors.Link_to_Vector;
+-- DESCRIPTION :
+--   This package offers more efficient vector computations on 
+--   separated real and imaginary parts, splitted from complex vectors.
+
+-- DATA STRUCTURES :
+
+  type VecVecVec is
+    array ( integer32 range <> ) of Standard_Floating_VecVecs.Link_to_VecVec;
+  -- A three dimensional structure to store the coefficient vectors,
+  -- real or imaginary parts, of powers of series.
+ 
+  type Link_to_VecVecVec is access VecVecVec; -- to store the power table
+
+  type VecVecVec_Array is array ( integer32 range <> ) of Link_to_VecVecVec;
+
+  function Allocate ( mxe : Standard_Integer_Vectors.Vector;
+                      deg : integer32 )
+                    return Link_to_VecVecVec;
 
   -- DESCRIPTION :
-  --   Returns the vector of the real parts of the complex vector x.
+  --   Allocates space for the power table, given the exponent maxima
+  --   for each variable in mxe and the degrees of the power series in deg.
 
-  function Imag_Part ( x : Standard_Complex_Vectors.Link_to_Vector )
-                     return Standard_Floating_Vectors.Link_to_Vector;
-
-  -- DESCRIPTION :
-  --   Returns the vector of the imaginary parts of the complex vector x.
-
-  function Make_Complex
-             ( rpx,ipx : Standard_Floating_Vectors.Link_to_Vector )
-             return Standard_Complex_Vectors.Link_to_Vector;
+  procedure Create ( rx,ix : in Standard_Floating_VecVecs.VecVec;
+                     mxe : in Standard_Integer_Vectors.Vector;
+                     deg : in integer32;
+                     rpwt,ipwt : out Link_to_VecVecVec );
 
   -- DESCRIPTION :
-  --   Returns the vector of complex numbers,
-  --   with real and imaginary parts given in the vectors rpx and ipx.
+  --   Stores all powers x(i)^k for k ranging from 2 to d(i),
+  --   for i in d'range = x'range, in the power table.
+  --   The i-th entry in the power table contains the powers of x(i),
+  --   if d(i) > 1, starting with x(i)^2 at the first position.
+  --   This Create procedure combines the functions Allocate(mxe,deg) 
+  --   and the Compute(rpwt,ipwt,d,rx,ix) procedure below.
+
+-- BASIC COMPUTATIONAL PROCEDURES :
+
+  procedure Update ( rvl,ivl : in Standard_Floating_Vectors.Link_to_Vector;
+                     rnc,inc : in Standard_Floating_Vectors.Link_to_Vector );
+
+  -- DESCRIPTION :
+  --   Adds the numbers in rnc to the rvl and 
+  --   adds the numbers in inc to the ivl.
+
+  -- REQUIRED : all vectors have the same range.
 
   procedure Multiply
               ( xr,xi,yr,yi : in Standard_Floating_Vectors.Link_to_Vector;
@@ -35,5 +67,154 @@ package Standard_Coefficient_Convolutions is
 
   -- REQUIRED :
   --   All vectors have the same range.
+
+-- COMPUTING THE POWER TABLE :
+
+  procedure Compute ( rpwt,ipwt : in Link_to_VecVecVec;
+                      mxe : in Standard_Integer_Vectors.Vector;
+                      rx,ix : in Standard_Floating_VecVecs.VecVec );
+
+  -- DESCRIPTION :
+  --   Computes the powers in the allocated power table for the
+  --   coefficients in the power series in x.
+
+  -- REQUIRED : rpwt = Allocate(mxe,deg) and ipwt = Allocate(mxe,deg)
+  --   have been executed, and rpwt'range = x'range = ipwt'range.
+
+  -- ON ENTRY :
+  --   rpwt     allocated power table for the exponent maxima in mxe
+  --            and for the real parts of the coefficients of series
+  --            of the degree of the series in x;
+  --   ipwt     allocated power table for the exponent maxima in mxe
+  --            and for the imaginary parts of the coefficients of series
+  --            of the degree of the series in x;
+  --   mxe      exponent maxima for each variable in the system;
+  --   rx       real parts of coefficients of power series;
+  --   ix       imaginary parts of coefficients of power series.
+
+  -- ON RETURN :
+  --   rpwt     real parts of the coefficients of the power table;
+  --   ipwt     imaginary parts of the coefficients of the power table.
+
+-- REVERSE MODE OF ALGORITHMIC DIFFERENTIATION :
+
+  procedure Speel ( rx,ix : in Standard_Floating_VecVecs.VecVec;
+                    rfwd,ifwd : in Standard_Floating_VecVecs.VecVec;
+                    rbck,ibck : in Standard_Floating_VecVecs.VecVec;
+                    rcrs,icrs : in Standard_Floating_VecVecs.VecVec );
+  procedure Speel ( rx,ix : in Standard_Floating_VecVecs.VecVec;
+                    idx : in Standard_Integer_Vectors.Vector;
+                    rfwd,ifwd : in Standard_Floating_VecVecs.VecVec;
+                    rbck,ibck : in Standard_Floating_VecVecs.VecVec;
+                    rcrs,icrs : in Standard_Floating_VecVecs.VecVec );
+
+  -- DESCRIPTION :
+  --   Inline computation of the coefficients of the product
+  --   of the series in rx, ix, which are all of the same degree.
+
+  -- REQUIRED :
+  --   The range of rx and ix starts at 1 and ends at 2 or higher.
+  --   All vectors are allocated and of the range 0..degree,
+  --   for some same fixed degree.
+
+  -- ON ENTRY :
+  --   rx           a vector with range starting at 1, ending at 2 or higher,
+  --                with the real parts of the coefficients of series;
+  --   ix           a vector with range starting at 1, ending at 2 or higher,
+  --                with the imaginary parts of the coefficients of series;
+  --   idx          if provided, then only those indices of x with values
+  --                in idx participate and dim = idx'last,
+  --                otherwise, all values of x participate and dim = x'last;
+  --   rfwd,ifwd    space allocated for dim-1 coefficient vectors,
+  --                for series of the same fixed degree;
+  --   backward     has space reserved for dim-2 coefficient vectors,
+  --                for series of the same fixed degree;
+  --   cross        has space reserved for dim-2 coefficient vectors,
+  --                for series of the same fixed degree.
+
+  -- ON RETURN :
+  --   rfwd         accumulates the real parts of the forward products,
+  --                rfwd(dim-1) holds the coefficients for the product,
+  --                rfwd(dim-2) holds the coefficients for the value
+  --                of the last partial derivative in x;
+  --   ifwd         accumulates the imaginary parts of the forward products,
+  --                ifwd(dim-1) holds the coefficients for the product,
+  --                ifwd(dim-2) holds the coefficients for the value
+  --                of the last partial derivative in x;
+  --   rbck         accumulates the real parts of the backward products,
+  --                rbck(dim-2) holds the coefficients of the first
+  --                partial derivative of the product;
+  --   ibck         accumulates the imaginary parts of the backward products,
+  --                rbck(dim-2) holds the coefficients of the first
+  --                partial derivative of the product;
+  --   rcrs         stores the real parts of the cross products,
+  --                rcrs(k) contains the real parts of the coefficients
+  --                of the partial derivative w.r.t. k+1;
+  --   icrs         stores the imaginary parts of the cross products,
+  --                icrs(k) contains the imaginary parts of coefficients
+  --                of the partial derivative w.r.t. k+1.
+
+  procedure Speel ( idx : in Standard_Integer_VecVecs.VecVec;
+                    rx,ix : in Standard_Floating_VecVecs.VecVec;
+                    rfwd,ifwd : in Standard_Floating_VecVecs.VecVec;
+                    rbck,ibck : in Standard_Floating_VecVecs.VecVec;
+                    rcrs,icrs : in Standard_Floating_VecVecs.VecVec;
+                    ryd,iyd : in Standard_Floating_VecVecs.VecVec );
+  procedure Speel ( idx : in Standard_Integer_VecVecs.VecVec;
+                    rcff,icff : in Standard_Floating_VecVecs.VecVec;
+                    rx,ix : in Standard_Floating_VecVecs.VecVec;
+                    rfwd,ifwd : in Standard_Floating_VecVecs.VecVec;
+                    rbck,ibck : in Standard_Floating_VecVecs.VecVec;
+                    rcrs,icrs : in Standard_Floating_VecVecs.VecVec;
+                    ryd,iyd : in Standard_Floating_VecVecs.VecVec;
+                    rwrk,iwrk : in Standard_Floating_Vectors.Link_to_Vector );
+
+  -- DESCRIPTION :
+  --   Evaluation and differentiation of the sum of products,
+  --   given in indexed format at a power series.
+
+  -- ON ENTRY :
+  --   idx          indexed representation of a sum of products of variables;
+  --   rcff         real parts of coefficients of the products,
+  --                if omitted, then all coefficients are considered as one;
+  --   icff         imaginary parts of coefficients of the products,
+  --                if omitted, then all coefficients are considered as one;
+  --   rx           real parts of coefficients of series of same degree;
+  --   ix           imaginary parts of coefficients of series of same degree;
+  --   rfwd         work space allocated for rx'last-1 coefficient vectors
+  --                of the same fixed degree as the series in rx;
+  --   ifwd         work space allocated for ix'last-1 coefficient vectors
+  --                of the same fixed degree as the series in ix;
+  --   rbck         work space allocated for rx'last-2 coefficient vectors
+  --                of the same fixed degree as the series in rx;
+  --   ibck         work space allocated for ix'last-2 coefficient vectors
+  --                of the same fixed degree as the series in ix;
+  --   rcrs         work space allocated for rx'last-2 coefficient vectors
+  --                of the same fixed degree as the series in x;
+  --   icrs         work space allocated for ix'last-2 coefficient vectors
+  --                of the same fixed degree as the series in x;
+  --   ryd          vector of range 0..rx'last with space allocated for the
+  --                coefficients of power series of the same fixed degree;
+  --   iyd          vector of range 0..ix'last with space allocated for the
+  --                coefficients of power series of the same fixed degree;
+  --   rwrk         work space for the coefficients of the same fixed degree;
+  --   iwrk         work space for the coefficients of the same fixed degree.
+
+  -- ON RETURN :
+  --   ryd          ryd(rx'last+1) contains the real parts of coefficients
+  --                of the value of the sum of products, evaluated at x,
+  --                ryd(k) is the real part of the k-th partial derivative;
+  --   iyd          iyd(ix'last+1) contains the real parts of coefficients
+  --                of the value of the sum of products, evaluated at x,
+  --                iyd(k) is the real part of the k-th partial derivative.
+
+-- DEALLOCATORS :
+
+  procedure Clear ( pwt : in out VecVecVec );
+  procedure Clear ( pwt : in out Link_to_VecVecVec );
+  procedure Clear ( pwt : in out VecVecVec_Array );
+
+  -- DESCRIPTION :
+  --   Deallocates the space occupied by the power table(s) pwt.
 
 end Standard_Coefficient_Convolutions;
