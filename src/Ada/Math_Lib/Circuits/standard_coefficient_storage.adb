1e352d6cd57f5ece608f075eb8e2a10fb7f32683
Jan Verschelde <janv@uic.edu>
2020-06-20 14:54:20 -0500

    new package to store and restore the coefficient of series in circuits

---
+++ src/Ada/Math_Lib/Circuits/standard_coefficient_storage.adb
@@ -0,0 +1,117 @@
+with Standard_Floating_Vectors;
+
+package body Standard_Coefficient_Storage is
+
+  procedure Allocate_and_Store
+              ( c : in Link_to_Circuit;
+                rcf : out Standard_Floating_VecVecs.Link_to_VecVec;
+                icf : out Standard_Floating_VecVecs.Link_to_VecVec ) is
+
+    rpc,ipc : Standard_Floating_VecVecs.VecVec(0..c.nbr);
+    lnk : Standard_Floating_Vectors.Link_to_Vector;
+
+    use Standard_Floating_Vectors;
+
+  begin
+    if c.rct /= null
+     then rpc(0) := new Standard_Floating_Vectors.Vector'(c.rct.all);
+    end if;
+    if c.ict /= null
+     then ipc(0) := new Standard_Floating_Vectors.Vector'(c.ict.all);
+    end if;
+    for k in 1..c.nbr loop
+      if c.rcf(k) /= null then
+        lnk := c.rcf(k);
+        declare
+          vck : Standard_Floating_Vectors.Vector(lnk'range);
+        begin
+          for i in lnk'range loop
+            vck(i) := lnk(i);
+          end loop;
+          rpc(k) := new Standard_Floating_Vectors.Vector'(vck);
+        end;
+      end if;
+      if c.icf(k) /= null then
+        lnk := c.icf(k);
+        declare
+          vck : Standard_Floating_Vectors.Vector(lnk'range);
+        begin
+          for i in lnk'range loop
+            vck(i) := lnk(i);
+          end loop;
+          ipc(k) := new Standard_Floating_Vectors.Vector'(vck);
+        end;
+      end if;
+    end loop;
+    rcf := new Standard_Floating_VecVecs.VecVec'(rpc);
+    icf := new Standard_Floating_VecVecs.VecVec'(ipc);
+  end Allocate_and_Store;
+
+  procedure Allocate_and_Store
+              ( c : in Circuits; rcf,icf : out Link_to_VecVecVec ) is
+
+    rcp,icp : VecVecVec(c'range);
+
+  begin
+    for k in c'range loop
+      if c(k) /= null
+       then Allocate_and_Store(c(k),rcp(k),icp(k));
+      end if;
+    end loop;
+    rcf := new VecVecVec'(rcp);
+    icf := new VecVecVec'(icp);
+  end Allocate_and_Store;
+
+  procedure Restore
+              ( rcf : in Standard_Floating_VecVecs.Link_to_VecVec;
+                icf : in Standard_Floating_VecVecs.Link_to_VecVec;
+                c : in Link_to_Circuit ) is
+
+    crclnk,storelnk : Standard_Floating_Vectors.Link_to_Vector;
+
+    use Standard_Floating_Vectors;
+
+  begin
+    if rcf(0) /= null then
+      storelnk := rcf(0);
+      crclnk := c.rct;
+      for k in storelnk'range loop
+        crclnk(k) := storelnk(k);
+      end loop;
+    end if;
+    if icf(0) /= null then
+      storelnk := icf(0);
+      crclnk := c.ict;
+      for k in storelnk'range loop
+        crclnk(k) := storelnk(k);
+      end loop;
+    end if;
+    for k in 1..c.nbr loop
+      if rcf(k) /= null then
+        storelnk := rcf(k);
+        crclnk := c.rcf(k);
+        for i in storelnk'range loop
+          crclnk(i) := storelnk(i);
+        end loop;
+      end if;
+      if icf(k) /= null then
+        storelnk := icf(k);
+        crclnk := c.icf(k);
+        for i in storelnk'range loop
+          crclnk(i) := storelnk(i);
+        end loop;
+      end if;
+    end loop;
+  end Restore;
+
+  procedure Restore
+              ( rcf,icf : in Link_to_VecVecVec; c : in Circuits ) is
+  begin
+    for k in c'range loop
+      if c(k) /= null
+       then Restore(rcf(k),icf(k),c(k));
+      end if;
+    end loop;
+  end Restore;
+
+end Standard_Coefficient_Storage;
---
+++ src/Ada/Math_Lib/Circuits/standard_coefficient_storage.ads
@@ -0,0 +1,79 @@
+with Standard_Floating_VecVecs;
+with Standard_Coefficient_Convolutions;  use Standard_Coefficient_Convolutions;
+
+package Standard_Coefficient_Storage is
+
+-- DESCRIPTION :
+--   The coefficients of the power series of circuits are shifted
+--   during the tracking on one path.  This package allocates space,
+--   stores, and restores the coefficients of the circuits.
+
+  procedure Allocate_and_Store
+              ( c : in Link_to_Circuit;
+                rcf : out Standard_Floating_VecVecs.Link_to_VecVec;
+                icf : out Standard_Floating_VecVecs.Link_to_VecVec );
+
+  -- DESCRIPTION :
+  --   Allocates space for the real and imaginary parts of the coefficients
+  --   of the circuit c and stores the coefficients in vectors of vectors.
+
+  -- ON ENTRY :
+  --   c        a convolution circuit with well defined coefficients.
+
+  -- ON RETURN :
+  --   rcf      real parts of the coefficients of the series in c,
+  --            with the real part of the constant series at index 0;
+  --   icf      imaginary parts of the coefficients of the series in c,
+  --            with the imaginary part of the constant series at index 0.
+
+  procedure Allocate_and_Store
+              ( c : in Circuits; rcf,icf : out Link_to_VecVecVec );
+
+  -- DESCRIPTION :
+  --   Allocates space for the real and imaginary parts of the coefficients
+  --   of the circuits in c and stores all coefficients of the circuits
+  --   in c into vectors of vectors of vectors.
+
+  -- ON ENTRY :
+  --   c        convolution circuits with well defined coefficients.
+
+  -- ON RETURN :
+  --   rcf      vector of vectors of vectors, where rcf(k) contains
+  --            the real parts of the coefficients of the series in c(k),
+  --            with the real part of the constant series at index 0,
+  --            for all k in c'range;
+  --   icf      vector of vectors of vectors, where icf(k) contains
+  --            the imaginary parts of the coefficients of the series in c(k),
+  --            with the imaginary part of the constant series at index 0,
+  --            for all k in c'range.
+
+  procedure Restore
+              ( rcf : in Standard_Floating_VecVecs.Link_to_VecVec;
+                icf : in Standard_Floating_VecVecs.Link_to_VecVec;
+                c : in Link_to_Circuit );
+
+  -- DESCRIPTION :
+  --   Given allocated space for all coefficients of the circuit c,
+  --   restores the values to their original values.
+
+  -- REQUIRED : rcf'range = icf'range = 0..c.nbr,
+  --   and for all k in rcf'range, rcf(k) and icf(k) is allocated.
+
+  -- ON ENTRY :
+  --   rcf      real parts of the coefficients of series for c,
+  --            with the real part of the constant series at index 0;
+  --   icf      imaginary parts of the coefficients of series for c,
+  --            with the imaginary part of the constant series at index 0.
+
+  -- ON RETURN :
+  --   c        a convolution circuit with coefficients restored
+  --            with the values in rcf and icf.
+
+  procedure Restore
+              ( rcf,icf : in Link_to_VecVecVec; c : in Circuits );
+
+  -- DESCRIPTION :
+  --   Restores all coefficients in c to the values
+  --   stored in the rcf and icf.
+
+end Standard_Coefficient_Storage;
