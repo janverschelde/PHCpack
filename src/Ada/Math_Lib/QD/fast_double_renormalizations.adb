3b2c8c221ce6752d82732628fafa94a6489464ba
Jan Verschelde <janv@uic.edu>
2020-08-27 11:49:57 -0500

    bug fix in the renormalization to add a double to a penta double

--- src/Ada/Math_Lib/QD/fast_double_renormalizations.adb
+++ src/Ada/Math_Lib/QD/fast_double_renormalizations.adb
@@ -135,6 +135,141 @@ package body Fast_Double_Renormalizations is
     end if;
   end renorm_add1;
 
+  procedure renorm5
+              ( f0,f1,f2,f3,f4,f5 : in double_float;
+                pr : in out double_float;
+                r0,r1,r2,r3,r4 : out double_float ) is
+
+    ptr : integer;
+
+  begin
+    if f1 = 0.0 then
+      pr := f0;
+      ptr := 0;
+      Double_Double_Basics.quick_two_sum(pr,f2,r0,pr);
+    else
+      r0 := f0;
+      pr := f1;
+      ptr := 1;
+      Double_Double_Basics.quick_two_sum(pr,f2,r1,pr);
+    end if;
+    if pr = 0.0 then
+      if ptr = 0
+       then pr := r0;
+       else pr := r1;
+      end if;
+    else
+      ptr := ptr + 1;
+    end if;
+    case ptr is
+      when 0 => Double_Double_Basics.quick_two_sum(pr,f3,r0,pr);
+      when 1 => Double_Double_Basics.quick_two_sum(pr,f3,r1,pr);
+      when 2 => Double_Double_Basics.quick_two_sum(pr,f3,r2,pr);
+      when others => null;
+    end case;
+    if pr = 0.0 then
+      case ptr is
+        when 0 => pr := r0;
+        when 1 => pr := r1;
+        when 2 => pr := r2;
+        when others => null;
+      end case;
+    else
+      ptr := ptr + 1;
+    end if;
+    case ptr is
+      when 0 => Double_Double_Basics.quick_two_sum(pr,f4,r0,pr);
+      when 1 => Double_Double_Basics.quick_two_sum(pr,f4,r1,pr);
+      when 2 => Double_Double_Basics.quick_two_sum(pr,f4,r2,pr);
+      when 3 => Double_Double_Basics.quick_two_sum(pr,f4,r3,pr);
+      when others => null;
+    end case;
+    if pr = 0.0 then
+      case ptr is
+        when 0 => pr := r0;
+        when 1 => pr := r1;
+        when 2 => pr := r2;
+        when 3 => pr := r3;
+        when others => null;
+      end case;
+    else
+      ptr := ptr + 1;
+    end if;
+    case ptr is
+      when 0 => Double_Double_Basics.quick_two_sum(pr,f5,r0,pr);
+      when 1 => Double_Double_Basics.quick_two_sum(pr,f5,r1,pr);
+      when 2 => Double_Double_Basics.quick_two_sum(pr,f5,r2,pr);
+      when 3 => Double_Double_Basics.quick_two_sum(pr,f5,r3,pr);
+      when 4 => Double_Double_Basics.quick_two_sum(pr,f5,r4,pr);
+      when others => null;
+    end case;
+    if pr = 0.0 then
+      case ptr is
+        when 0 => pr := r0;
+        when 1 => pr := r1;
+        when 2 => pr := r2;
+        when 3 => pr := r3;
+        when 4 => pr := r4;
+        when others => null;
+      end case;
+    else
+      ptr := ptr + 1;
+    end if;
+    if ptr < 5 and pr /= 0.0 then
+      case ptr is
+        when 0 => r0 := pr;
+        when 1 => r1 := pr;
+        when 2 => r2 := pr;
+        when 3 => r3 := pr;
+        when 4 => r4 := pr;
+        when others => null;
+      end case;
+      ptr := ptr + 1;
+    end if;
+    if ptr < 1 then
+      r4 := 0.0; r3 := 0.0; r2 := 0.0; r1 := 0.0; r0 := 0.0;
+    elsif ptr < 2 then
+      r4 := 0.0; r3 := 0.0; r2 := 0.0; r1 := 0.0;
+    elsif ptr < 3 then
+      r4 := 0.0; r3 := 0.0; r2 := 0.0;
+    elsif ptr < 4 then
+      r4 := 0.0; r3 := 0.0;
+    elsif ptr < 5 then
+      r4 := 0.0;
+    end if;
+  end renorm5;
+
+  procedure fast_renorm
+              ( x0,x1,x2,x3,x4,x5 : in double_float;
+                r0,r1,r2,r3,r4 : out double_float ) is
+
+    f0,f1,f2,f3,f4,f5,pr : double_float;
+
+  begin
+    Double_Double_Basics.quick_two_sum(x4,x5,pr,f5);
+    Double_Double_Basics.quick_two_sum(x3,pr,pr,f4);
+    Double_Double_Basics.quick_two_sum(x2,pr,pr,f3);
+    Double_Double_Basics.quick_two_sum(x1,pr,pr,f2);
+    Double_Double_Basics.quick_two_sum(x0,pr,f0,f1);
+    renorm5(f0,f1,f2,f3,f4,f5,pr,r0,r1,r2,r3,r4);
+  end fast_renorm;
+
+  procedure renorm_add1
+              ( x0,x1,x2,x3,x4 : in double_float;
+                y : in double_float;
+                r0,r1,r2,r3,r4 : out double_float ) is
+
+    f0,f1,f2,f3,f4,f5,pr : double_float;
+
+  begin
+    Double_Double_Basics.two_sum(x4,y,pr,f5);
+    Double_Double_Basics.two_sum(x3,pr,pr,f4);
+    Double_Double_Basics.two_sum(x2,pr,pr,f3);
+    Double_Double_Basics.two_sum(x1,pr,pr,f2);
+    Double_Double_Basics.two_sum(x0,pr,f0,f1);
+    renorm5(f0,f1,f2,f3,f4,f5,pr,r0,r1,r2,r3,r4);
+  end renorm_add1;
+
   procedure renorm8
               ( f0,f1,f2,f3,f4,f5,f6,f7,f8 : in double_float;
                 pr : in out double_float;
