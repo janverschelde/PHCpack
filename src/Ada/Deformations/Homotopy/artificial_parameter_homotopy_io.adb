481e866775c5e5c37af24f4baf0157634153ea99
Jan Verschelde <janv@uic.edu>
2020-09-24 20:46:14 -0500

    extended input of artificial-parameter homotopies to penta, octo, and deca double precision

--- src/Ada/Deformations/Homotopy/artificial_parameter_homotopy_io.adb
+++ src/Ada/Deformations/Homotopy/artificial_parameter_homotopy_io.adb
@@ -3,10 +3,16 @@ with Standard_Complex_Poly_Systems_io;   use Standard_Complex_Poly_Systems_io;
 with DoblDobl_Complex_Poly_Systems_io;   use DoblDobl_Complex_Poly_Systems_io;
 with TripDobl_Complex_Poly_Systems_io;   use TripDobl_Complex_Poly_Systems_io;
 with QuadDobl_Complex_Poly_Systems_io;   use QuadDobl_Complex_Poly_Systems_io;
+with PentDobl_Complex_Poly_Systems_io;   use PentDobl_Complex_Poly_Systems_io;
+with OctoDobl_Complex_Poly_Systems_io;   use OctoDobl_Complex_Poly_Systems_io;
+with DecaDobl_Complex_Poly_Systems_io;   use DecaDobl_Complex_Poly_Systems_io;
 with Standard_System_and_Solutions_io;
 with DoblDobl_System_and_Solutions_io;
 with TripDobl_System_and_Solutions_io;
 with QuadDobl_System_and_Solutions_io;
+with PentDobl_System_and_Solutions_io;
+with OctoDobl_System_and_Solutions_io;
+with DecaDobl_System_and_Solutions_io;
 
 package body Artificial_Parameter_Homotopy_io is
 
@@ -52,6 +58,39 @@ package body Artificial_Parameter_Homotopy_io is
     new_line;
     put_line("Reading the start system and its solutions ...");
     QuadDobl_System_and_Solutions_io.get(start,sols);
+  end get;
+
+  procedure get ( target : out PentDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
+                  start : out PentDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
+                  sols : out PentDobl_Complex_Solutions.Solution_List ) is
+  begin
+    new_line;
+    put_line("Reading the target system ..."); get(target);
+    new_line;
+    put_line("Reading the start system and its solutions ...");
+    PentDobl_System_and_Solutions_io.get(start,sols);
+  end get;
+
+  procedure get ( target : out OctoDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
+                  start : out OctoDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
+                  sols : out OctoDobl_Complex_Solutions.Solution_List ) is
+  begin
+    new_line;
+    put_line("Reading the target system ..."); get(target);
+    new_line;
+    put_line("Reading the start system and its solutions ...");
+    OctoDobl_System_and_Solutions_io.get(start,sols);
+  end get;
+
+  procedure get ( target : out DecaDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
+                  start : out DecaDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
+                  sols : out DecaDobl_Complex_Solutions.Solution_List ) is
+  begin
+    new_line;
+    put_line("Reading the target system ..."); get(target);
+    new_line;
+    put_line("Reading the start system and its solutions ...");
+    DecaDobl_System_and_Solutions_io.get(start,sols);
   end get;
 
 end Artificial_Parameter_Homotopy_io;
--- src/Ada/Deformations/Homotopy/artificial_parameter_homotopy_io.ads
+++ src/Ada/Deformations/Homotopy/artificial_parameter_homotopy_io.ads
@@ -2,10 +2,16 @@ with Standard_Complex_Poly_Systems;
 with DoblDobl_Complex_Poly_Systems;
 with TripDobl_Complex_Poly_Systems;
 with QuadDobl_Complex_Poly_Systems;
+with PentDobl_Complex_Poly_Systems;
+with OctoDobl_Complex_Poly_Systems;
+with DecaDobl_Complex_Poly_Systems;
 with Standard_Complex_Solutions;
 with DoblDobl_Complex_Solutions;
 with TripDobl_Complex_Solutions;
 with QuadDobl_Complex_Solutions;
+with PentDobl_Complex_Solutions;
+with OctoDobl_Complex_Solutions;
+with DecaDobl_Complex_Solutions;
 
 package Artificial_Parameter_Homotopy_io is
 
@@ -13,7 +19,7 @@ package Artificial_Parameter_Homotopy_io is
 --   Provides procedures to prompt the user to provide file names
 --   for the target, start system, and start solutions in an 
 --   artificial-parameter homotopy, in double, double double,
---   triple double, and quad double precision.
+--   triple double, quad double, octo double, and deca double precision.
 
   procedure get ( target : out Standard_Complex_Poly_Systems.Link_to_Poly_Sys;
                   start : out Standard_Complex_Poly_Systems.Link_to_Poly_Sys;
@@ -27,6 +33,15 @@ package Artificial_Parameter_Homotopy_io is
   procedure get ( target : out QuadDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
                   start : out QuadDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
                   sols : out QuadDobl_Complex_Solutions.Solution_List );
+  procedure get ( target : out PentDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
+                  start : out PentDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
+                  sols : out PentDobl_Complex_Solutions.Solution_List );
+  procedure get ( target : out OctoDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
+                  start : out OctoDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
+                  sols : out OctoDobl_Complex_Solutions.Solution_List );
+  procedure get ( target : out DecaDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
+                  start : out DecaDobl_Complex_Poly_Systems.Link_to_Poly_Sys;
+                  sols : out DecaDobl_Complex_Solutions.Solution_List );
 
   -- DESCRIPTION :
   --   Prompts the user for a target system and for a start system with
