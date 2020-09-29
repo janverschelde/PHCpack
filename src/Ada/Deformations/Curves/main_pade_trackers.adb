6d3512e1dadeb619df8bd96266ca8706934389ce
Jan Verschelde <janv@uic.edu>
2020-09-29 15:00:10 -0500

    moved main procedures into separate package

---
+++ src/Ada/Deformations/Curves/main_pade_trackers.adb
@@ -0,0 +1,247 @@
+with text_io;                            use text_io;
+with Communications_with_User;           use Communications_with_User;
+with Run_Power_Series_Methods;          use Run_Power_Series_Methods;
+with Regular_Newton_Puiseux;
+with Series_Path_Trackers;
+with Interactive_Pade_Trackers;
+with Track_Path_Convolutions;
+with Multitasked_Path_Convolutions;
+
+package body Main_Pade_Trackers is
+
+  procedure Run_Regular_Newton_Puiseux ( valprc : in character ) is
+  begin
+    put_line("Using as lifting the powers of the first variable,");
+    put_line("assuming coefficients are sufficiently generic ...");
+    case valprc is
+      when '1' =>
+        put_line("The working precision is double precision");
+        Regular_Newton_Puiseux.Standard_Main;
+      when '2' =>
+        put_line("The working precision is double double precision.");
+        Regular_Newton_Puiseux.DoblDobl_Main;
+      when '4' =>
+        put_line("The working precision is quad double precision.");
+        Regular_Newton_Puiseux.QuadDobl_Main;
+      when others => null;
+    end case;
+  end Run_Regular_Newton_Puiseux;
+
+  procedure Run_Power_Series_Newton
+              ( infilename,outfilename : in string;
+                valprc : in character; vrb : in integer32 := 0 ) is
+
+    ans : character;
+
+  begin
+    new_line;
+    put("Start Newton's method at a constant term ? (y/n) ");
+    Ask_Yes_or_No(ans);
+    case valprc is
+      when '1' =>
+        put_line("The working precision is double precision");
+        if ans = 'y'
+         then Standard_Main_at_Constant(infilename,outfilename,vrb-1);
+         else Standard_Main_at_Series(infilename,outfilename,vrb-1);
+        end if;
+      when '2' =>
+        put_line("The working precision is double double precision.");
+        if ans = 'y'
+         then DoblDobl_Main_at_Constant(infilename,outfilename,vrb-1);
+         else DoblDobl_Main_at_Series(infilename,outfilename,vrb-1);
+        end if;
+      when '4' =>
+        put_line("The working precision is quad double precision.");
+        if ans = 'y'
+         then QuadDobl_Main_at_Constant(infilename,outfilename,vrb-1);
+         else QuadDobl_Main_at_Series(infilename,outfilename,vrb-1);
+        end if;
+      when others => null;
+    end case;
+  end Run_Power_Series_Newton;
+
+  procedure Run_Path_Trackers
+              ( valprc : in character; vrb : in integer32 := 0 ) is
+
+    ans : character;
+
+  begin
+    new_line;
+    put("Step-by-step interactive execution ? (y/n) ");
+    Ask_Yes_or_No(ans);
+    if ans = 'y' then
+      case valprc is
+        when '1' => Interactive_Pade_Trackers.Standard_Main(vrb-1);
+        when '2' => Interactive_Pade_Trackers.DoblDobl_Main(vrb-1);
+        when '4' => Interactive_Pade_Trackers.QuadDobl_Main(vrb-1);
+        when others => null;
+      end case;
+    else
+      case valprc is
+        when '1' => Series_Path_Trackers.Standard_Main(vrb-1);
+        when '2' => Series_Path_Trackers.DoblDobl_Main(vrb-1);
+        when '4' => Series_Path_Trackers.QuadDobl_Main(vrb-1);
+        when others => null;
+      end case;
+    end if;
+  end Run_Path_Trackers;
+
+  procedure Run_Path_Convolution_Trackers
+              ( nbtasks : in natural32; valprc : in character;
+                vrb : in integer32 := 0 ) is
+
+    ans : character;
+
+  begin
+    if nbtasks > 0 then
+      case valprc is
+        when '1' => Multitasked_Path_Convolutions.Standard_Main(nbtasks,vrb-1);
+        when '2' => Multitasked_Path_Convolutions.DoblDobl_Main(nbtasks,vrb-1);
+        when '4' => Multitasked_Path_Convolutions.QuadDobl_Main(nbtasks,vrb-1);
+        when others => null;
+      end case;
+    else
+      new_line;
+      put("Apply multitasking ? (y/n) "); Ask_Yes_or_No(ans);
+      if ans = 'n' then
+        case valprc is
+          when '1' => Track_Path_Convolutions.Standard_Main(vrb-1);
+          when '2' => Track_Path_Convolutions.DoblDobl_Main(vrb-1);
+          when '4' => Track_Path_Convolutions.QuadDobl_Main(vrb-1);
+          when others => null;
+        end case;
+      else
+        case valprc is
+          when '1' => Multitasked_Path_Convolutions.Standard_Main(0,vrb-1);
+          when '2' => Multitasked_Path_Convolutions.DoblDobl_Main(0,vrb-1);
+          when '4' => Multitasked_Path_Convolutions.QuadDobl_Main(0,vrb-1);
+          when others => null;
+        end case;
+      end if;
+    end if;
+  end Run_Path_Convolution_Trackers;
+
+  function Prompt_for_Precision_Level return character is
+
+    res : character;
+
+  begin
+    new_line;
+    put_line("MENU for the precision :");
+    put_line("  1. double precision");
+    put_line("  2. double double precision");
+    put_line("  4. quad double precision");
+    put("Type 1, 2, or 4 to select the precision : ");
+    Ask_Alternative(res,"124");
+    return res;
+  end Prompt_for_Precision_Level;
+
+  procedure Run_Regular_Newton_Puiseux is
+
+    prc : constant character := Prompt_for_Precision_Level;
+
+  begin
+    Run_Regular_Newton_Puiseux(prc);
+  end Run_Regular_Newton_Puiseux;
+
+  procedure Run_Power_Series_Newton
+              ( infilename,outfilename : in string;
+                vrb : in integer32 := 0 ) is
+
+    prc : constant character := Prompt_for_Precision_Level;
+
+  begin
+    Run_Power_Series_Newton(infilename,outfilename,prc,vrb-1);
+  end Run_Power_Series_Newton;
+
+  procedure Run_Path_Trackers ( vrb : in integer32 := 0 ) is
+
+    prc : constant character := Prompt_for_Precision_Level;
+
+  begin
+    Run_Path_Trackers(prc,vrb-1);
+  end Run_Path_Trackers;
+
+  procedure Run_Path_Convolution_Trackers
+              ( nbt : in natural32; vrb : in integer32 := 0 ) is
+
+    prc : constant character := Prompt_for_Precision_Level;
+
+  begin
+    Run_Path_Convolution_Trackers(nbt,prc,vrb-1);
+  end Run_Path_Convolution_Trackers;
+
+  function Prompt_for_Method return character is
+
+    res : character;
+
+  begin
+    new_line;
+    put_line("MENU for power series methods :");
+    put_line("  1. apply polyhedral methods for tropisms;");
+    put_line("  2. run Newton's method starting at a series or a point;");
+    put_line("  3. track paths with Pade approximants as predictor;");
+    put_line("  4. run a faster Newton-Fabry-Pade-Hesse path tracker.");
+    put("Type 1, 2, 3, or 4 to select the method : ");
+    Ask_Alternative(res,"1234");
+    return res;
+  end Prompt_for_Method;
+
+  procedure Nonzero_Precision_Main
+              ( infilename,outfilename : in string;
+                nbtasks : in natural32; valprc : in character;
+                vrb : in integer32 := 0 ) is
+
+    method : character;
+
+  begin 
+    if vrb > 0 then
+      put_line("-> in main_pade_trackers.Nonzero_Precision_Main ...");
+    end if;
+    method := Prompt_for_Method;
+    case method is
+      when '1' => Run_Regular_Newton_Puiseux(valprc);
+      when '2' => Run_Power_Series_Newton(infilename,outfilename,valprc,vrb-1);
+      when '3' => Run_Path_Trackers(valprc,vrb-1);
+      when '4' => Run_Path_Convolution_Trackers(nbtasks,valprc,vrb-1);
+      when others => null;
+    end case;
+  end Nonzero_Precision_Main;
+
+  procedure Zero_Precision_Main
+              ( infilename,outfilename : in string;
+                nbtasks : in natural32; vrb : in integer32 := 0 ) is
+
+    method : character;
+
+  begin 
+    if vrb > 0 then
+      put_line("-> in main_pade_trackers.Nonzero_Precision_Main ...");
+    end if;
+    method := Prompt_for_Method;
+    case method is
+      when '1' => Run_Regular_Newton_Puiseux;
+      when '2' => Run_Power_Series_Newton(infilename,outfilename,vrb-1);
+      when '3' => Run_Path_Trackers(vrb-1);
+      when '4' => Run_Path_Convolution_Trackers(nbtasks,vrb-1);
+      when others => null;
+    end case;
+  end Zero_Precision_Main;
+
+
+  procedure Main ( infilename,outfilename : in string;
+                   nbtasks : in natural32; precision : in character;
+                   verbose : in integer32 := 0 ) is
+  begin
+    if verbose > 0 then
+      put_line("-> in main_pade_trackers.Main ...");
+    end if;
+    if precision = '0' then
+      Zero_Precision_Main(infilename,outfilename,nbtasks,verbose-1);
+    else
+      Nonzero_Precision_Main
+        (infilename,outfilename,nbtasks,precision,verbose-1);
+    end if;
+  end Main;
+
+end Main_Pade_Trackers;
---
+++ src/Ada/Deformations/Curves/main_pade_trackers.ads
@@ -0,0 +1,129 @@
+with Standard_Natural_Numbers;           use Standard_Natural_Numbers;
+with Standard_Integer_Numbers;           use Standard_Integer_Numbers;
+
+package Main_Pade_Trackers is
+
+-- DESCRIPTION :
+--   The main procedures for phc -u are defined by this package.
+
+  procedure Run_Regular_Newton_Puiseux ( valprc : in character );
+
+  -- DESCRIPTION :
+  --   Runs the Newton-Puiseux algorithm for the generic case,
+  --   in double, double double, or quad double precision,
+  --   when valprc is respectively '0', '1', or '4'.
+
+  procedure Run_Power_Series_Newton
+              ( infilename,outfilename : in string;
+                valprc : in character; vrb : in integer32 := 0 );
+
+  -- DESCRIPTION :
+  --   Computes power series expansions with Newton's method,
+  --   in double, double double, or quad double precision,
+  --   when valprc is respectively '0', '1', or '4'.
+  --   The value of the verbose level is vrb.
+
+  procedure Run_Path_Trackers
+              ( valprc : in character; vrb : in integer32 := 0 );
+
+  -- DESCRIPTION :
+  --   Tracks paths in double, double double, or quad double precision,
+  --   when valprc is respectively '0', '1', or '4'.
+  --   The value of the verbose level is vrb.
+
+  procedure Run_Path_Convolution_Trackers
+              ( nbtasks : in natural32; valprc : in character;
+                vrb : in integer32 := 0 );
+
+  -- DESCRIPTION :
+  --   Tracks paths with convolution circuits 
+  --   and multitasking if nbtasks > 0,
+  --   If nbtasks equals zero, then the number of tasks is prompted for.
+  --   in double, double double, or quad double precision,
+  --   when valprc is respectively '0', '1', or '4'.
+  --   The value of the verbose level is vrb.
+
+  function Prompt_for_Precision_Level return character;
+
+  -- DESCRIPTION :
+  --   Prompts for the precision and returns '1', '2', or '4',
+  --   respectively for double, double double, or quad double precision.
+
+  procedure Run_Regular_Newton_Puiseux;
+
+  -- DESCRIPTION :
+  --   Prompts for the precision and then runs the Newton-Puiseux
+  --   algorithm for the (sufficiently) generic case.
+  --   The verbose level is given in vrb.
+
+  procedure Run_Power_Series_Newton
+              ( infilename,outfilename : in string; vrb : in integer32 := 0 );
+
+  -- DESCRIPTION :
+  --   Prompts for the precision and then runs Newton's method
+  --   to compute power series expansions.
+  --   The verbose level is given in vrb.
+
+  procedure Run_Path_Trackers ( vrb : in integer32 := 0 );
+
+  -- DESCRIPTION :
+  --   Prompts for the precision and then tracks paths.
+  --   The verbose level is given in vrb.
+
+  procedure Run_Path_Convolution_Trackers
+              ( nbt : in natural32; vrb : in integer32 := 0 );
+
+  -- DESCRIPTION :
+  --   Prompts for the precision and then tracks paths.
+  --   The number of tasks is given in nbt.
+  --   The verbose level is given in vrb.
+
+  function Prompt_for_Method return character;
+
+  -- DESCRIPTION :
+  --   Displays the menu with methods and returns the character
+  --   that corresponds with the selected method.
+
+  procedure Nonzero_Precision_Main
+              ( infilename,outfilename : in string;
+                nbtasks : in natural32; valprc : in character;
+                vrb : in integer32 := 0 );
+
+  -- DESCRIPTION :
+  --   This main procedure is called when the precision is determined,
+  --   either interactively or via the value of precision in valprc.
+  --   The value for the precision in valprc is not '0', it is
+  --   '1' for double, '2' for double double, or '4' for quad double.
+  --   The procedure can be called immediately if the precision is
+  --   set at the command line.
+
+  procedure Zero_Precision_Main
+              ( infilename,outfilename : in string;
+                nbtasks : in natural32; vrb : in integer32 := 0 );
+
+  -- DESCRIPTION :
+  --   This main procedure is called when the precision is undetermined,
+  --   The value for the precision will be set after the prompt
+  --   for the kind of power series method.
+
+  procedure Main ( infilename,outfilename : in string;
+                   nbtasks : in natural32; precision : in character;
+                   verbose : in integer32 := 0 );
+
+  -- DESCRIPTION :
+  --   Defines what phc -u does.
+
+  -- ON ENTRY :
+  --   nbtasks      number of tasks, 0 for no multitasking;
+  --   precision    indicates the precision, is one of the following:
+  --                '0' : the user will be prompted for the precision,
+  --                '1' : standard double precision,
+  --                '2' : double double precision,
+  --                '4' : quad double precision;
+  --   infilename   name of the input file, if "", then the user will
+  --                be prompted to provide the name of the input file;
+  --   outfilename  name of the output file, if "", then the user will
+  --                be prompted to provide the name of the output file;
+  --   verbose      the verbose level.
+
+end Main_Pade_Trackers;
