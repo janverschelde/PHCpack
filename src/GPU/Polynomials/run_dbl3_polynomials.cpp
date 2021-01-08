1991ef117be8f1834f28320739c3ba8288b028f9
Jan Verschelde <janv@uic.edu>
2021-01-08 13:29:15 -0600

    added more instances of a specific test, for more precisions and more degrees

--- src/GPU/Polynomials/run_dbl3_polynomials.cpp
+++ src/GPU/Polynomials/run_dbl3_polynomials.cpp
@@ -9,15 +9,43 @@ using namespace std;
 
 int main ( void )
 {
-   int seed,dim,nva,nbr,pwr,deg,vrb;
+   int seed,dim,nva,nbr,pwr,deg,vrb,fail;
 
    cout << "Give the seed (0 for time) : "; cin >> seed;
 
-   dim = 16; nva = 4; nbr = products_count(dim,nva); pwr = 1; deg = 31;
+   dim = 16; nva = 4; nbr = products_count(dim,nva); pwr = 1; vrb = 2;
 
-   vrb = 2;
-
-   int fail = main_dbl3_test_polynomial(seed,dim,nbr,nva,pwr,deg,vrb);
+   deg = 15;
+   fail = main_dbl3_test_polynomial
+             (seed,dim,nbr,nva,pwr,deg,vrb,1.0e-40,true);
+   deg = 31;
+   cout << "---> running for degree 31 ..." << endl;
+   fail += main_dbl3_test_polynomial
+              (seed,dim,nbr,nva,pwr,deg,vrb,1.0e-40,false);
+   deg = 63;
+   cout << "---> running for degree 63 ..." << endl;
+   fail += main_dbl3_test_polynomial
+              (seed,dim,nbr,nva,pwr,deg,vrb,1.0e-40,false);
+   deg = 95;
+   cout << "---> running for degree 95 ..." << endl;
+   fail += main_dbl3_test_polynomial
+              (seed,dim,nbr,nva,pwr,deg,vrb,1.0e-40,false);
+   deg = 127;
+   cout << "---> running for degree 127 ..." << endl;
+   fail += main_dbl3_test_polynomial
+              (seed,dim,nbr,nva,pwr,deg,vrb,1.0e-40,false);
+   deg = 152;
+   cout << "---> running for degree 152 ..." << endl;
+   fail += main_dbl3_test_polynomial
+              (seed,dim,nbr,nva,pwr,deg,vrb,1.0e-40,false);
+   deg = 159;
+   cout << "---> running for degree 159 ..." << endl;
+   fail += main_dbl3_test_polynomial
+             (seed,dim,nbr,nva,pwr,deg,vrb,1.0e-40,false);
+   deg = 191;
+   cout << "---> running for degree 191 ..." << endl;
+   fail += main_dbl3_test_polynomial
+             (seed,dim,nbr,nva,pwr,deg,vrb,1.0e-40,false);
 
    if(fail == 0)
       cout << "All tests passed." << endl;
