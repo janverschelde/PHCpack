ea5a2e4072d2dddd70c555efd591b9d1c13482ad
Jan Verschelde <janv@uic.edu>
2021-01-10 19:45:26 -0600

    improved a test program

--- src/GPU/Polynomials/test_polynomials.cpp
+++ src/GPU/Polynomials/test_polynomials.cpp
@@ -20,6 +20,8 @@ int main ( void )
 
    prompt_for_setup(&seed,&dim,&nbr,&nva,&pwr,&deg,&vrb,&mode);
 
+   cout << endl << "running in double precision ..." << endl;
+
    int fail = main_dbl_test_polynomial
                  (seed,dim,nbr,nva,pwr,deg,vrb,1.0e-8,true,mode);
    if(mode == 2)
@@ -30,6 +32,8 @@ int main ( void )
          cout << "Number of failed tests in double precision : "
               << fail << endl;
    }
+   cout << endl << "running in double double precision ..." << endl;
+
    int fail2 = main_dbl2_test_polynomial
                   (seed,dim,nbr,nva,pwr,deg,vrb,1.0e-24,false,mode);
    if(mode == 2)
@@ -40,6 +44,8 @@ int main ( void )
          cout << "Number of failed tests in double double precision : "
               << fail2 << endl;
    }
+   cout << endl << "running in triple double precision ..." << endl;
+
    int fail3 = main_dbl3_test_polynomial
                   (seed,dim,nbr,nva,pwr,deg,vrb,1.0e-40,false,mode);
    if(mode == 2)
@@ -50,6 +56,8 @@ int main ( void )
          cout << "Number of failed tests in triple double precision : "
               << fail3 << endl;
    }
+   cout << endl << "running in quad double precision ..." << endl;
+
    int fail4 = main_dbl4_test_polynomial
                   (seed,dim,nbr,nva,pwr,deg,vrb,1.0e-56,false,mode);
    if(mode == 2)
@@ -60,6 +68,8 @@ int main ( void )
          cout << "Number of failed tests in quad double precision : "
               << fail4 << endl;
    }
+   cout << endl << "running in penta double precision ..." << endl;
+
    int fail5 = main_dbl5_test_polynomial
                   (seed,dim,nbr,nva,pwr,deg,vrb,1.0e-72,false,mode);
    if(mode == 2)
@@ -70,6 +80,8 @@ int main ( void )
          cout << "Number of failed tests in penta double precision : "
               << fail5 << endl;
    }
+   cout << endl << "running in octo double precision ..." << endl;
+
    int fail8 = main_dbl8_test_polynomial
                   (seed,dim,nbr,nva,pwr,deg,vrb,1.0e-120,false,mode);
    if(mode == 2)
@@ -80,6 +92,8 @@ int main ( void )
          cout << "Number of failed tests in octo double precision : "
               << fail8 << endl;
    }
+   cout << endl << "running in deca double precision ..." << endl;
+
    int fail10 = main_dbl10_test_polynomial
                    (seed,dim,nbr,nva,pwr,deg,vrb,1.0e-156,false,mode);
    if(mode == 2)
