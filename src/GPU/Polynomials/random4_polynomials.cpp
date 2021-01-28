3a56673df9c34815ed30a806e3f9fb32479d3c11
Jan Verschelde <janv@uic.edu>
2021-01-27 20:46:03 -0600

    added functions to generate polynomials with random complex coefficients in quad double precision

--- src/GPU/Polynomials/random4_polynomials.cpp
+++ src/GPU/Polynomials/random4_polynomials.cpp
@@ -3,6 +3,7 @@
 
 #include <cstdlib>
 #include <iostream>
+#include "quad_double_functions.h"
 #include "random4_vectors.h"
 #include "random4_monomials.h"
 #include "random_polynomials.h"
@@ -21,6 +22,51 @@ bool make_real4_polynomial
    {
       fail = make_real4_monomial(dim,nvr[i],pwr,deg,idx[i],exp[i],
                                  cffhihi[i],cfflohi[i],cffhilo[i],cfflolo[i]);
+      if(fail) return true;
+   }
+   return fail;
+}
+
+void random_quad_double_complex
+ ( double *rehihi, double *relohi, double *rehilo, double *relolo,
+   double *imhihi, double *imlohi, double *imhilo, double *imlolo )
+{
+   double rndhihi,rndlohi,rndhilo,rndlolo;
+   double sinhihi,sinlohi,sinhilo,sinlolo;
+
+   random_quad_double(rehihi,relohi,rehilo,relolo);    // random cos
+
+   qdf_sqr(*rehihi,*relohi,*rehilo,*relolo,
+           &sinhihi,&sinlohi,&sinhilo,&sinlolo);       // cos^2(angle)
+   qdf_minus(&sinhihi,&sinlohi,&sinhilo,&sinlolo);     // -cos^2(angle)
+   qdf_inc_d(&sinhihi,&sinlohi,&sinhilo,&sinlolo,1.0); // 1-cos^2(angle)
+   qdf_sqrt(sinhihi,sinlohi,sinhilo,sinlolo,
+            imhihi,imlohi,imhilo,imlolo);              // sin is sqrt
+}
+
+bool make_complex4_polynomial
+ ( int dim, int nbr, int pwr, int deg, int *nvr, int **idx, int **exp,
+   double *cstrehihi, double *cstrelohi, double *cstrehilo, double *cstrelolo,
+   double *cstimhihi, double *cstimlohi, double *cstimhilo, double *cstimlolo,
+   double **cffrehihi, double **cffrelohi,
+   double **cffrehilo, double **cffrelolo,
+   double **cffimhihi, double **cffimlohi,
+   double **cffimhilo, double **cffimlolo )
+{
+   bool fail = false;
+
+   for(int i=0; i<=deg; i++)
+      random_quad_double_complex
+         (&cstrehihi[i],&cstrelohi[i],&cstrehilo[i],&cstrelolo[i],
+          &cstimhihi[i],&cstimlohi[i],&cstimhilo[i],&cstimlolo[i]);
+
+   for(int i=0; i<nbr; i++)
+   {
+      fail = make_complex4_monomial
+                (dim,nvr[i],pwr,deg,idx[i],exp[i],
+                 cffrehihi[i],cffrelohi[i],cffrehilo[i],cffrelolo[i],
+                 cffimhihi[i],cffimlohi[i],cffimhilo[i],cffimlolo[i]);
+
       if(fail) return true;
    }
    return fail;
@@ -47,6 +93,37 @@ void make_real4_products
    free(accu);
 }
 
+void make_complex4_products
+ ( int dim, int nbr, int nva, int deg, int **idx,
+   double *cstrehihi, double *cstrelohi, double *cstrehilo, double *cstrelolo,
+   double *cstimhihi, double *cstimlohi, double *cstimhilo, double *cstimlolo,
+   double **cffrehihi, double **cffrelohi,
+   double **cffrehilo, double **cffrelolo,
+   double **cffimhihi, double **cffimlohi,
+   double **cffimhilo, double **cffimlolo )
+{
+   for(int i=0; i<=deg; i++)
+      random_quad_double_complex
+         (&cstrehihi[i],&cstrelohi[i],&cstrehilo[i],&cstrelolo[i],
+          &cstimhihi[i],&cstimlohi[i],&cstimhilo[i],&cstimlolo[i]);
+
+   for(int k=0; k<nbr; k++)
+      for(int i=0; i<=deg; i++)
+         random_quad_double_complex
+            (&cffrehihi[k][i],&cffrelohi[k][i],
+             &cffrehilo[k][i],&cffrelolo[k][i],
+             &cffimhihi[k][i],&cffimlohi[k][i],
+             &cffimhilo[k][i],&cffimlolo[k][i]);
+
+   int moncnt = 0;
+   int *accu = new int[nva];
+
+   make_product_exponents(0,dim,nva,accu,&moncnt,idx);
+
+   free(accu);
+}
+
+
 void make_real4_cyclic
  ( int dim, int nva, int deg, int **idx,
    double *csthihi, double *cstlohi, double *csthilo, double *cstlolo,
@@ -59,6 +136,31 @@ void make_real4_cyclic
       for(int i=0; i<=deg; i++)
          random_quad_double(&cffhihi[k][i],&cfflohi[k][i],
                             &cffhilo[k][i],&cfflolo[k][i]);
+
+   make_cyclic_exponents(dim,nva,idx);
+}
+
+void make_complex4_cyclic
+ ( int dim, int nva, int deg, int **idx,
+   double *cstrehihi, double *cstrelohi, double *cstrehilo, double *cstrelolo,
+   double *cstimhihi, double *cstimlohi, double *cstimhilo, double *cstimlolo,
+   double **cffrehihi, double **cffrelohi,
+   double **cffrehilo, double **cffrelolo,
+   double **cffimhihi, double **cffimlohi,
+   double **cffimhilo, double **cffimlolo )
+{
+   for(int i=0; i<=deg; i++)
+      random_quad_double_complex
+         (&cstrehihi[i],&cstrelohi[i],&cstrehilo[i],&cstrelolo[i],
+          &cstimhihi[i],&cstimlohi[i],&cstimhilo[i],&cstimlolo[i]);
+
+   for(int k=0; k<dim; k++)
+      for(int i=0; i<=deg; i++)
+         random_quad_double_complex
+            (&cffrehihi[k][i],&cffrelohi[k][i],
+             &cffrehilo[k][i],&cffrelolo[k][i],
+             &cffimhihi[k][i],&cffimlohi[k][i],
+             &cffimhilo[k][i],&cffimlolo[k][i]);
 
    make_cyclic_exponents(dim,nva,idx);
 }
--- src/GPU/Polynomials/random4_polynomials.h
+++ src/GPU/Polynomials/random4_polynomials.h
@@ -52,6 +52,111 @@ bool make_real4_polynomial
  *   cfflolo   the lowest parts of the coefficient series for each monomial,
  *             cfflolo[k] has the deg+1 lowest coefficients of monomial k. */
 
+void random_quad_double_complex
+ ( double *rehihi, double *relohi, double *rehilo, double *relolo,
+   double *imhihi, double *imlohi, double *imhilo, double *imlolo );
+/*
+ * DESCRIPTION :
+ *   Generates the high, middle, and low double of a random cosine c
+ *   and computes the corresponding sine as sqrt(1-c*c),
+ *   so the randomly generated complex number has modulus one.
+ *
+ * ON RETURN :
+ *   rehihi   the highest double of the real part,
+ *   relohi   the second highest double of the real part,
+ *   rehilo   the second lowest double of the real part,
+ *   relolo   the lowest double of the real part,
+ *   imhihi   the highest double of the imaginary part,
+ *   imlohi   the second highest double of the imaginary part,
+ *   imhilo   the second lowest double of the imaginary part,
+ *   imlolo   the lowest double of the imaginary part
+ *            of the randomly generated complex number. */
+
+bool make_complex4_polynomial
+ ( int dim, int nbr, int pwr, int deg, int *nvr, int **idx, int **exp,
+   double *cstrehihi, double *cstrelohi, double *cstrehilo, double *cstrelolo,
+   double *cstimhihi, double *cstimlohi, double *cstimhilo, double *cstimlolo,
+   double **cffrehihi, double **cffrelohi,
+   double **cffrehilo, double **cffrelolo,
+   double **cffimhihi, double **cffimlohi,
+   double **cffimhilo, double **cffimlolo );
+/*
+ * DESCRIPTION :
+ *   Makes a polynomial in several variables, with power series coefficients,
+ *   generating random exponents and complex coefficients.
+ *   Writes an error message and returns true if for one k, nvr[k] > dim.
+ *
+ * ON ENTRY :
+ *   dim       dimension, total number of variables;
+ *   nbr       number of monomials, excluding the constant term,
+ *             nbr is the number of monomials that have at least one variable;
+ *   pwr       largest power of a variable;
+ *   deg       degree of the power series coefficient;
+ *   nvr       nbr integers, with nvr[k] the number of variables in monomial k;
+ *   exp       space allocated for nbr arrays of nvr[k] integers;
+ *   cstrehihi has space allocated for deg+1 doubles;
+ *   cstrelohi has space allocated for deg+1 doubles;
+ *   cstrehilo has space allocated for deg+1 doubles;
+ *   cstrelolo has space allocated for deg+1 doubles;
+ *   cstimhihi has space allocated for deg+1 doubles;
+ *   cstimlohi has space allocated for deg+1 doubles;
+ *   cstimhilo has space allocated for deg+1 doubles;
+ *   cstimlolo has space allocated for deg+1 doubles;
+ *   cffrehihi has space allocated for nbr arrays of deg+1 doubles;
+ *   cffrelohi has space allocated for nbr arrays of deg+1 doubles;
+ *   cffrehilo has space allocated for nbr arrays of deg+1 doubles;
+ *   cffrelolo has space allocated for nbr arrays of deg+1 doubles;
+ *   cffimhihi has space allocated for nbr arrays of deg+1 doubles;
+ *   cffimlohi has space allocated for nbr arrays of deg+1 doubles;
+ *   cffimhilo has space allocated for nbr arrays of deg+1 doubles.
+ *   cffimlolo has space allocated for nbr arrays of deg+1 doubles.
+ *
+ * ON RETURN :
+ *   idx       the participating variables in each monomial,
+ *             idx[k] is an array of nvr[k] integers,
+ *             idx[k][i] is the index of variable i in monomial k;
+ *   exp       the exponents of the variables in each monomial,
+ *             exp[k] is an array of nvr[k] integers,
+ *             exp[k][i] is the exponent of variable i in monomial k;
+ *   cstrehihi has the deg+1 highest coefficients of the real parts
+ *             of the constant monomial;
+ *   cstrelohi has the deg+1 second highest coefficients of the real parts
+ *             of the constant monomial;
+ *   cstrelolo has the deg+1 second lowest coefficients of the real parts
+ *             of the constant monomial;
+ *   cstrelolo has the deg+1 lowest coefficients of the real parts
+ *             of the constant monomial;
+ *   cstimhihi has the deg+1 highest coefficients of the imaginary parts
+ *             of the constant monomial;
+ *   cstimlohi has the deg+1 second highest coefficients of the imaginary parts
+ *             of the constant monomial;
+ *   cstimhilo has the deg+1 second lowest coefficients of the imaginary parts
+ *             of the constant monomial;
+ *   cstimlolo has the deg+1 lowest coefficients of the imaginary parts
+ *             of the constant monomial;
+ *   cffrehihi has the highest doubles of the real parts of the coefficients,
+ *             cffrehihi[k] has deg+1 highest coefficients of monomial k;
+ *   cffrelohi has the second highest doubles of the real parts
+ *             of the coefficients, cffrelohi[k] has deg+1 second highest
+ *             coefficients of monomial k;
+ *   cffrehilo has the second lowest doubles of the real parts
+ *             of the coefficients, cffrehilo[k] has deg+1 second lowest
+ *             coefficients of monomial k;
+ *   cffrelolo has the lowest doubles of the real parts of the coefficients,
+ *             cffrelolo[k] has deg+1 lowest coefficients of monomial k;
+ *   cffimhihi has the highest doubles of the imaginary parts
+ *             of the coefficients, cffimhihi[k] has deg+1 highest
+ *             coefficients of monomial k;
+ *   cffimlohi has the second highest doubles of the imaginary parts
+ *             of the coefficients, cffimlohi[k] has deg+1 second highest
+ *             coefficients of monomial k;
+ *   cffimhilo has the second lowest doubles of the imaginary parts
+ *             of the coefficient, cffimlolo[k] has the deg+1 second lowest
+ *             coefficients of monomial k;
+ *   cffimlolo has the lowest doubles of the imaginary parts
+ *             of the coefficient, cffimlolo[k] has the deg+1 lowest
+ *             coefficients of monomial k. */
+
 void make_real4_products
  ( int dim, int nbr, int nva, int deg, int **idx,
    double *csthihi, double *cstlohi, double *csthilo, double *cstlolo,
@@ -95,6 +200,92 @@ void make_real4_products
  *   cfflolo   the lowest parts of the coefficient series for each monomial,
  *             cfflolo[k] has the deg+1 lowest coefficients of monomial k. */
 
+void make_complex4_products
+ ( int dim, int nbr, int nva, int deg, int **idx,
+   double *cstrehihi, double *cstrelohi, double *cstrehilo, double *cstrelolo,
+   double *cstimhihi, double *cstimlohi, double *cstimhilo, double *cstimlolo,
+   double **cffrehihi, double **cffrelohi,
+   double **cffrehilo, double **cffrelolo,
+   double **cffimhihi, double **cffimlohi,
+   double **cffimhilo, double **cffimlolo );
+/*
+ * DESCRIPTION :
+ *   Returns the sum of all products of size nbr out of dimension dim,
+ *   with random complex power series coefficients truncated to degree deg.
+ *
+ * REQUIRED : dim > nbr.
+ *
+ * ON ENTRY :
+ *   dim       dimension, total number of variables;
+ *   nbr       number of monomials, excluding the constant term,
+ *             nbr is the number of monomials that have at least one variable;
+ *   pwr       largest power of a variable;
+ *   deg       degree of the power series coefficient;
+ *   nvr       nbr integers, with nvr[k] the number of variables in monomial k;
+ *   exp       space allocated for nbr arrays of nvr[k] integers;
+ *   cstrehihi has space allocated for deg+1 doubles;
+ *   cstrelohi has space allocated for deg+1 doubles;
+ *   cstrehilo has space allocated for deg+1 doubles;
+ *   cstrelolo has space allocated for deg+1 doubles;
+ *   cstimhihi has space allocated for deg+1 doubles;
+ *   cstimlohi has space allocated for deg+1 doubles;
+ *   cstimhilo has space allocated for deg+1 doubles;
+ *   cstimlolo has space allocated for deg+1 doubles;
+ *   cffrehihi has space allocated for nbr arrays of deg+1 doubles;
+ *   cffrelohi has space allocated for nbr arrays of deg+1 doubles;
+ *   cffrehilo has space allocated for nbr arrays of deg+1 doubles;
+ *   cffrelolo has space allocated for nbr arrays of deg+1 doubles;
+ *   cffimhihi has space allocated for nbr arrays of deg+1 doubles;
+ *   cffimlohi has space allocated for nbr arrays of deg+1 doubles;
+ *   cffimhilo has space allocated for nbr arrays of deg+1 doubles.
+ *   cffimlolo has space allocated for nbr arrays of deg+1 doubles.
+ *
+ * ON RETURN :
+ *   idx       the participating variables in each monomial,
+ *             idx[k] is an array of nvr[k] integers,
+ *             idx[k][i] is the index of variable i in monomial k;
+ *   exp       the exponents of the variables in each monomial,
+ *             exp[k] is an array of nvr[k] integers,
+ *             exp[k][i] is the exponent of variable i in monomial k;
+ *   cstrehihi has the deg+1 highest coefficients of the real parts
+ *             of the constant monomial;
+ *   cstrelohi has the deg+1 second highest coefficients of the real parts
+ *             of the constant monomial;
+ *   cstrelolo has the deg+1 second lowest coefficients of the real parts
+ *             of the constant monomial;
+ *   cstrelolo has the deg+1 lowest coefficients of the real parts
+ *             of the constant monomial;
+ *   cstimhihi has the deg+1 highest coefficients of the imaginary parts
+ *             of the constant monomial;
+ *   cstimlohi has the deg+1 second highest coefficients of the imaginary parts
+ *             of the constant monomial;
+ *   cstimhilo has the deg+1 second lowest coefficients of the imaginary parts
+ *             of the constant monomial;
+ *   cstimlolo has the deg+1 lowest coefficients of the imaginary parts
+ *             of the constant monomial;
+ *   cffrehihi has the highest doubles of the real parts of the coefficients,
+ *             cffrehihi[k] has deg+1 highest coefficients of monomial k;
+ *   cffrelohi has the second highest doubles of the real parts
+ *             of the coefficients, cffrelohi[k] has deg+1 second highest
+ *             coefficients of monomial k;
+ *   cffrehilo has the second lowest doubles of the real parts
+ *             of the coefficients, cffrehilo[k] has deg+1 second lowest
+ *             coefficients of monomial k;
+ *   cffrelolo has the lowest doubles of the real parts of the coefficients,
+ *             cffrelolo[k] has deg+1 lowest coefficients of monomial k;
+ *   cffimhihi has the highest doubles of the imaginary parts
+ *             of the coefficients, cffimhihi[k] has deg+1 highest
+ *             coefficients of monomial k;
+ *   cffimlohi has the second highest doubles of the imaginary parts
+ *             of the coefficients, cffimlohi[k] has deg+1 second highest
+ *             coefficients of monomial k;
+ *   cffimhilo has the second lowest doubles of the imaginary parts
+ *             of the coefficient, cffimlolo[k] has the deg+1 second lowest
+ *             coefficients of monomial k;
+ *   cffimlolo has the lowest doubles of the imaginary parts
+ *             of the coefficient, cffimlolo[k] has the deg+1 lowest
+ *             coefficients of monomial k. */
+
 void make_real4_cyclic
  ( int dim, int nva, int deg, int **idx,
    double *csthihi, double *cstlohi, double *csthilo, double *cstlolo,
@@ -136,5 +327,91 @@ void make_real4_cyclic
  *             has the deg+1 second lowest coefficients of monomial k;
  *   cfflolo   the lowest parts of the coefficient series for each monomial,
  *             cfflolo[k] has the deg+1 lowest coefficients of monomial k. */
+
+void make_complex4_cyclic
+ ( int dim, int nva, int deg, int **idx,
+   double *cstrehihi, double *cstrelohi, double *cstrehilo, double *cstrelolo,
+   double *cstimhihi, double *cstimlohi, double *cstimhilo, double *cstimlolo,
+   double **cffrehihi, double **cffrelohi,
+   double **cffrehilo, double **cffrelolo,
+   double **cffimhihi, double **cffimlohi,
+   double **cffimhilo, double **cffimlolo );
+/*
+ * DESCRIPTION :
+ *   Returns the cyclic polynomial with nva variables in dimension dim,
+ *   with random complex power series coefficients truncated to degree deg.
+ *
+ * REQUIRED : dim > nbr.
+ *
+ * ON ENTRY :
+ *   dim       dimension, total number of variables;
+ *   nbr       number of monomials, excluding the constant term,
+ *             nbr is the number of monomials that have at least one variable;
+ *   pwr       largest power of a variable;
+ *   deg       degree of the power series coefficient;
+ *   nvr       nbr integers, with nvr[k] the number of variables in monomial k;
+ *   exp       space allocated for nbr arrays of nvr[k] integers;
+ *   cstrehihi has space allocated for deg+1 doubles;
+ *   cstrelohi has space allocated for deg+1 doubles;
+ *   cstrehilo has space allocated for deg+1 doubles;
+ *   cstrelolo has space allocated for deg+1 doubles;
+ *   cstimhihi has space allocated for deg+1 doubles;
+ *   cstimlohi has space allocated for deg+1 doubles;
+ *   cstimhilo has space allocated for deg+1 doubles;
+ *   cstimlolo has space allocated for deg+1 doubles;
+ *   cffrehihi has space allocated for nbr arrays of deg+1 doubles;
+ *   cffrelohi has space allocated for nbr arrays of deg+1 doubles;
+ *   cffrehilo has space allocated for nbr arrays of deg+1 doubles;
+ *   cffrelolo has space allocated for nbr arrays of deg+1 doubles;
+ *   cffimhihi has space allocated for nbr arrays of deg+1 doubles;
+ *   cffimlohi has space allocated for nbr arrays of deg+1 doubles;
+ *   cffimhilo has space allocated for nbr arrays of deg+1 doubles.
+ *   cffimlolo has space allocated for nbr arrays of deg+1 doubles.
+ *
+ * ON RETURN :
+ *   idx       the participating variables in each monomial,
+ *             idx[k] is an array of nvr[k] integers,
+ *             idx[k][i] is the index of variable i in monomial k;
+ *   exp       the exponents of the variables in each monomial,
+ *             exp[k] is an array of nvr[k] integers,
+ *             exp[k][i] is the exponent of variable i in monomial k;
+ *   cstrehihi has the deg+1 highest coefficients of the real parts
+ *             of the constant monomial;
+ *   cstrelohi has the deg+1 second highest coefficients of the real parts
+ *             of the constant monomial;
+ *   cstrelolo has the deg+1 second lowest coefficients of the real parts
+ *             of the constant monomial;
+ *   cstrelolo has the deg+1 lowest coefficients of the real parts
+ *             of the constant monomial;
+ *   cstimhihi has the deg+1 highest coefficients of the imaginary parts
+ *             of the constant monomial;
+ *   cstimlohi has the deg+1 second highest coefficients of the imaginary parts
+ *             of the constant monomial;
+ *   cstimhilo has the deg+1 second lowest coefficients of the imaginary parts
+ *             of the constant monomial;
+ *   cstimlolo has the deg+1 lowest coefficients of the imaginary parts
+ *             of the constant monomial;
+ *   cffrehihi has the highest doubles of the real parts of the coefficients,
+ *             cffrehihi[k] has deg+1 highest coefficients of monomial k;
+ *   cffrelohi has the second highest doubles of the real parts
+ *             of the coefficients, cffrelohi[k] has deg+1 second highest
+ *             coefficients of monomial k;
+ *   cffrehilo has the second lowest doubles of the real parts
+ *             of the coefficients, cffrehilo[k] has deg+1 second lowest
+ *             coefficients of monomial k;
+ *   cffrelolo has the lowest doubles of the real parts of the coefficients,
+ *             cffrelolo[k] has deg+1 lowest coefficients of monomial k;
+ *   cffimhihi has the highest doubles of the imaginary parts
+ *             of the coefficients, cffimhihi[k] has deg+1 highest
+ *             coefficients of monomial k;
+ *   cffimlohi has the second highest doubles of the imaginary parts
+ *             of the coefficients, cffimlohi[k] has deg+1 second highest
+ *             coefficients of monomial k;
+ *   cffimhilo has the second lowest doubles of the imaginary parts
+ *             of the coefficient, cffimlolo[k] has the deg+1 second lowest
+ *             coefficients of monomial k;
+ *   cffimlolo has the lowest doubles of the imaginary parts
+ *             of the coefficient, cffimlolo[k] has the deg+1 lowest
+ *             coefficients of monomial k. */
 
 #endif
