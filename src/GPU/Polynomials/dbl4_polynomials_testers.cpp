b478a3c3aa114b2c50afcaf337c116681abe0609
Jan Verschelde <janv@uic.edu>
2021-02-01 20:27:49 -0600

    extended testers on complex data in quad double precision

--- src/GPU/Polynomials/dbl4_polynomials_testers.cpp
+++ src/GPU/Polynomials/dbl4_polynomials_testers.cpp
@@ -401,7 +401,6 @@ double cmplx4_error_sum
               << results2imlohi_h[dim][i] << endl
               << results2imhilo_h[dim][i] << "  "
               << results2imlolo_h[dim][i] << endl;
-       /*
          cout << resultsrehihi_d[dim][i] << "  "
               << resultsrehilo_d[dim][i] << endl
               << resultsrelohi_d[dim][i] << "  "
@@ -409,7 +408,7 @@ double cmplx4_error_sum
          cout << resultsimhihi_d[dim][i] << "  "
               << resultsimlohi_d[dim][i] << endl
               << resultsimhilo_d[dim][i] << "  "
-              << resultsimlolo_d[dim][i] << endl; */
+              << resultsimlolo_d[dim][i] << endl;
       }
       err = err + abs(results1rehihi_h[dim][i] - results2rehihi_h[dim][i])
                 + abs(results1relohi_h[dim][i] - results2relohi_h[dim][i])
@@ -419,7 +418,6 @@ double cmplx4_error_sum
                 + abs(results1imlohi_h[dim][i] - results2imlohi_h[dim][i])
                 + abs(results1imhilo_h[dim][i] - results2imhilo_h[dim][i])
                 + abs(results1imlolo_h[dim][i] - results2imlolo_h[dim][i])
-             /*
                 + abs(results1rehihi_h[dim][i] - resultsrehihi_d[dim][i])
                 + abs(results1relohi_h[dim][i] - resultsrelohi_d[dim][i])
                 + abs(results1rehilo_h[dim][i] - resultsrehilo_d[dim][i])
@@ -427,7 +425,7 @@ double cmplx4_error_sum
                 + abs(results1imhihi_h[dim][i] - resultsimhihi_d[dim][i])
                 + abs(results1imlohi_h[dim][i] - resultsimlohi_d[dim][i])
                 + abs(results1imhilo_h[dim][i] - resultsimhilo_d[dim][i])
-                + abs(results1imlolo_h[dim][i] - resultsimlolo_d[dim][i]) */;
+                + abs(results1imlolo_h[dim][i] - resultsimlolo_d[dim][i]);
    }
    if(verbose) cout << "error : " << err << endl;
 
@@ -457,14 +455,14 @@ double cmplx4_error_sum
                  << results2imlohi_h[k][i] << endl
                  << results2imhilo_h[k][i] << "  "
                  << results2imlolo_h[k][i] << endl;
-          /*  cout << resultsrehihi_d[k][i] << "  "
+            cout << resultsrehihi_d[k][i] << "  "
                  << resultsrelohi_d[k][i] << endl
                  << resultsrehilo_d[k][i] << "  "
                  << resultsrelolo_d[k][i] << endl;
             cout << resultsimhihi_d[k][i] << "  "
                  << resultsimlohi_d[k][i] << endl
                  << resultsimhilo_d[k][i] << "  "
-                 << resultsimlolo_d[k][i] << endl; */
+                 << resultsimlolo_d[k][i] << endl;
          }
          err = err + abs(results1rehihi_h[k][i] - results2rehihi_h[k][i])
                    + abs(results1relohi_h[k][i] - results2relohi_h[k][i])
@@ -474,7 +472,6 @@ double cmplx4_error_sum
                    + abs(results1imlohi_h[k][i] - results2imlohi_h[k][i])
                    + abs(results1imhilo_h[k][i] - results2imhilo_h[k][i])
                    + abs(results1imlolo_h[k][i] - results2imlolo_h[k][i])
-               /*
                    + abs(results1rehihi_h[k][i] - resultsrehihi_d[k][i])
                    + abs(results1relohi_h[k][i] - resultsrelohi_d[k][i])
                    + abs(results1rehilo_h[k][i] - resultsrehilo_d[k][i])
@@ -482,7 +479,7 @@ double cmplx4_error_sum
                    + abs(results1imhihi_h[k][i] - resultsimhihi_d[k][i])
                    + abs(results1imlohi_h[k][i] - resultsimlohi_d[k][i])
                    + abs(results1imhilo_h[k][i] - resultsimhilo_d[k][i])
-                   + abs(results1imlolo_h[k][i] - resultsimlolo_d[k][i])*/;
+                   + abs(results1imlolo_h[k][i] - resultsimlolo_d[k][i]);
       }
       if(verbose) cout << "error : " << err << endl;
       sumerr = sumerr + err;
@@ -841,7 +838,6 @@ double test_cmplx4_real_polynomial
              output2imhihi_h,output2imlohi_h,output2imhilo_h,output2imlolo_h,
              cnvjobs,addjobs,&timelapsec2_h,vrb);
       }
-/*
       if((mode == 0) || (mode == 2))
       {
          if(vrb) cout << "Computing on the device ..." << endl;
@@ -858,7 +854,6 @@ double test_cmplx4_real_polynomial
              cnvjobs,addjobs,&cnvlapms,&addlapms,&timelapms_d,
              &walltimes_d,vrb);
       }
- */
       double sumerr = 0.0;
       if(mode == 2)
          sumerr = cmplx4_error_sum(dim,deg,
