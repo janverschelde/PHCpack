549cd11578326b06099cb83fcea94306923ca4c8
Jan Verschelde <janv@uic.edu>
2021-02-05 20:24:20 -0600

    added the inner product to test vectors on series vectors

--- src/GPU/Matrices/test_dbl_series_vectors.cpp
+++ src/GPU/Matrices/test_dbl_series_vectors.cpp
@@ -2,9 +2,29 @@
 
 #include <iostream>
 #include <iomanip>
+#include <cstdlib>
 #include "random_matrices.h"
+#include "dbl_convolutions_host.h"
 
 using namespace std;
+
+void inner_product
+ ( int dim, int deg, double **x, double **y, double *z );
+/*
+ * DESCRIPTION :
+ *   Computes the product of two vectors x and y of power series
+ *   and assigns the result to z.
+ *
+ * ON ENTRY :
+ *   dim      dimension of the vectors x and y;
+ *   deg      truncation degree of the series;
+ *   x        dim series truncated to degree deg;
+ *   y        dim series truncated to degree deg;
+ *   z        space for deg+1 doubles.
+ *
+ * ON RETURN :
+ *   z        the sum of all x[k]*y[k] for k from 0 to dim-1,
+ *            as a power series truncated to the degree deg. */
 
 int main ( void )
 {
@@ -35,5 +55,29 @@ int main ( void )
       cout << "series for exp(-x) :" << endl; 
       for(int i=0; i<=deg; i++) cout << mx[k][i] << endl;
    }
+
+   double *ip = new double[deg+1];
+
+   inner_product(dim,deg,px,mx,ip);
+
+   cout << "the inner product :" << endl;
+   for(int i=0; i<=deg; i++) cout << ip[i] << endl;
+  
    return 0;
+}
+
+void inner_product
+ ( int dim, int deg, double **x, double **y, double *z )
+{
+   double *prod = new double[deg+1];
+
+   for(int i=0; i<=deg; i++) z[i] = 0.0;
+
+   for(int k=0; k<dim; k++)
+   {
+      CPU_dbl_product(deg,x[k],y[k],prod);
+      for(int i=0; i<=deg; i++)
+         z[i] = z[i] + prod[i];
+   }
+   free(prod);
 }
