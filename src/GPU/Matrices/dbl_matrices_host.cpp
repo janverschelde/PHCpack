0fa36a0a9265b9928611320d1b1fac807781d0fa
Jan Verschelde <janv@uic.edu>
2021-02-10 10:35:58 -0600

    moved code from the test function into new files

---
+++ src/GPU/Matrices/dbl_matrices_host.cpp
@@ -0,0 +1,60 @@
+/* The file dbl_matrices_host.cpp defines the function specified
+ * in dbl_matrices_host.h. */
+
+#include <cstdlib>
+#include "dbl_convolutions_host.h"
+
+void real_inner_product
+ ( int dim, int deg, double **x, double **y, double *z )
+{
+   double *prod = new double[deg+1];
+
+   for(int i=0; i<=deg; i++) z[i] = 0.0;
+
+   for(int k=0; k<dim; k++)
+   {
+      CPU_dbl_product(deg,x[k],y[k],prod);
+      for(int i=0; i<=deg; i++)
+         z[i] = z[i] + prod[i];
+   }
+   free(prod);
+}
+
+void cmplx_inner_product
+ ( int dim, int deg,
+   double **xre, double **xim, double **yre, double **yim,
+   double *zre, double *zim )
+{
+   double *prodre = new double[deg+1];
+   double *prodim = new double[deg+1];
+
+   for(int i=0; i<=deg; i++)
+   {
+      zre[i] = 0.0; zim[i] = 0.0;
+   }
+   for(int k=0; k<dim; k++)
+   {
+      CPU_cmplx_product(deg,xre[k],xim[k],yre[k],yim[k],prodre,prodim);
+      for(int i=0; i<=deg; i++)
+      {
+         zre[i] = zre[i] + prodre[i];
+         zim[i] = zim[i] + prodim[i];
+      }
+   }
+   free(prodre); free(prodim);
+}
+
+void real_matrix_vector_product
+ ( int rows, int cols, int deg, double ***A, double **x, double **y )
+{
+   for(int k=0; k<rows; k++)
+      real_inner_product(cols,deg,A[k],x,y[k]);
+}
+
+void cmplx_matrix_vector_product
+ ( int rows, int cols, int deg, double ***Are, double ***Aim,
+   double **xre, double **xim, double **yre, double **yim )
+{
+   for(int k=0; k<rows; k++)
+      cmplx_inner_product(cols,deg,Are[k],Aim[k],xre,xim,yre[k],yim[k]);
+}
---
+++ src/GPU/Matrices/dbl_matrices_host.h
@@ -0,0 +1,102 @@
+/* The file dbl_matrices_host specifies functions on vectors and matrices
+ * of series truncated to the same degree in double precision. */
+
+#ifndef __dbl_matrices_host_h__
+#define __dbl_matrices_host_h__
+
+void real_inner_product
+ ( int dim, int deg, double **x, double **y, double *z );
+/*
+ * DESCRIPTION :
+ *   Computes the product of two real vectors x and y of power series
+ *   and assigns the result to z.
+ *
+ * ON ENTRY :
+ *   dim      dimension of the vectors x and y;
+ *   deg      truncation degree of the series;
+ *   x        dim series truncated to degree deg;
+ *   y        dim series truncated to degree deg;
+ *   z        space for deg+1 doubles.
+ *
+ * ON RETURN :
+ *   z        the sum of all x[k]*y[k] for k from 0 to dim-1,
+ *            as a power series truncated to the degree deg. */
+
+void cmplx_inner_product
+ ( int dim, int deg,
+   double **xre, double **xim, double **yre, double **yim,
+   double *zre, double *zim );
+/*
+ * DESCRIPTION :
+ *   Computes the product of two complex vectors x and y of power series
+ *   and assigns the result to z.
+ *
+ * ON ENTRY :
+ *   dim      dimension of the vectors x and y;
+ *   deg      truncation degree of the series;
+ *   xre      real parts of dim series truncated to degree deg;
+ *   xim      imaginary parts of dim series truncated to degree deg;
+ *   yre      real parts of dim series truncated to degree deg;
+ *   yim      imaginary parts of dim series truncated to degree deg;
+ *   zre      space for deg+1 doubles;
+ *   zim      space for deg+1 doubles.
+ *
+ * ON RETURN :
+ *   zre      real parts of the sum of all x[k]*y[k] for k from 0 to dim-1,
+ *            as a power series truncated to the degree deg;
+ *   zim      imaginary parts of the sum of all x[k]*y[k] for k from 0
+ *            to dim-1, as a power series truncated to the degree deg. */
+
+void real_matrix_vector_product
+ ( int rows, int cols, int deg, double ***A, double **x, double **y );
+/*
+ * DESCRIPTION :
+ *   Computes the product y of the matrix A with x on real data.
+ *
+ * ON ENTRY :
+ *   rows     the number of rows in the matrix A
+ *            and the dimension of y;
+ *   cols     the number of columns in the matrix A
+ *            and the dimension of x;
+ *   deg      truncation degree of the series;
+ *   A        matrix of dimensions rows and cols,
+ *            of power series truncated at the degree deg;
+ *   x        vector of dimension cols
+ *            of power series truncated at the degree deg;
+ *   y        space allocated for a vector of dimension rows 
+ *            of power series truncated at the degree deg.
+ *
+ * ON RETURN :
+ *   y        product of A with x. */
+
+void cmplx_matrix_vector_product
+ ( int rows, int cols, int deg, double ***Are, double ***Aim,
+   double **xre, double **xim, double **yre, double **yim );
+/*
+ * DESCRIPTION :
+ *   Computes the product y of the matrix A with x on complex data.
+ *
+ * ON ENTRY :
+ *   rows     the number of rows in the matrix A
+ *            and the dimension of y;
+ *   cols     the number of columns in the matrix A
+ *            and the dimension of x;
+ *   deg      truncation degree of the series;
+ *   Are      real parts of a matrix of dimensions rows and cols,
+ *            of power series truncated at the degree deg;
+ *   Aim      imaginary parts of a matrix of dimensions rows and cols,
+ *            of power series truncated at the degree deg;
+ *   xre      real parts of a vector of dimension cols
+ *            of power series truncated at the degree deg;
+ *   xim      imaginary parts of a vector of dimension cols
+ *            of power series truncated at the degree deg;
+ *   yre      space allocated for a vector of dimension rows for
+ *            the real parts of series truncated at the degree deg;
+ *   yim      space allocated for a vector of dimension rows for
+ *            the imaginary parts of series truncated at the degree deg.
+ *
+ * ON RETURN :
+ *   yre      real parts of the product of A with x;
+ *   yim      imaginary parts of the product of A with x. */
+
+#endif
