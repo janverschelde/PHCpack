e4c7f7f08aebfc5fa37639cd620b556a17183610
Jan Verschelde <janv@uic.edu>
2021-02-16 20:48:35 -0600

    added separate procedure to get start system and solutions from file

--- src/Ada/Deformations/Solutions/ts_getstart.adb
+++ src/Ada/Deformations/Solutions/ts_getstart.adb
@@ -15,6 +15,57 @@ procedure ts_getstart is
 --   Tests the scanning of start system and start solutions
 --   from an output file of the blackbox solver of phc.
 
+  procedure Scan_for_Start_System 
+              ( infile : file_type; name : in Link_to_String;
+                q : out Standard_Complex_Poly_Systems.Link_to_Poly_Sys;
+                qsols : out Standard_Complex_Solutions.Solution_List;
+                found : out boolean; verbose : in boolean := true ) is
+
+  -- DESCRIPTION :
+  --   Scans the infile for a start system and start solutions.
+
+  -- ON ENTRY :
+  --   infile   must be opened for input;
+  --   name     name of the infile for error messages;
+  --   verbose  if true, then error messages are displayed
+  --            when a start system or its solutions are not found.
+
+  -- ON RETURN :
+  --   q        a start system, if found;
+  --   qsols    start solutions, if found;
+  --   found    if true, then both q and qsols are found,
+  --            otherwise, either q and/or qsols were not present.
+
+    len,dim : natural32 := 0;
+
+  begin
+    File_Scanning.Scan_and_Skip(infile,"START SYSTEM",found);
+    if not found then
+      if verbose then
+        new_line;
+        put_line("no start system found in " & name.all);
+      end if;
+    else
+      get(infile,q);
+      File_Scanning.Scan_and_skip(infile,"START SOLUTIONS",found);
+      if not found then
+        if verbose then
+          new_line;
+          put_line("no start solutions found in " & name.all);
+        end if;
+      else
+        get(infile,len);
+        get(infile,dim);
+        get(infile,len,dim,qsols);
+        if verbose then
+          new_line;
+          put("Read "); put(len,1);
+          put(" solutions of dimension "); put(dim,1); put_line(".");
+        end if;
+      end if;
+    end if;
+  end Scan_for_Start_System;
+
   procedure Main is
 
   -- DESCRIPTION :
@@ -22,40 +73,28 @@ procedure ts_getstart is
   --   and scans for the start system and start solutions.
 
     infile,outfile : file_type;
-    found : boolean;
     name : Link_to_String;
+    found : boolean;
     lp : Link_to_Poly_Sys;
     sols : Solution_List;
-    len,dim : natural32 := 0;
 
   begin
     new_line;
     put_line("Reading the name of an input file ...");
     Read_Name_and_Open_File(infile,name);
-    File_Scanning.Scan_and_Skip(infile,"START SYSTEM",found);
+    Scan_for_Start_System(infile,name,lp,sols,found);
     if not found then
       put_line("no start system found in " & name.all);
     else
       new_line;
       put_line("Reading the name of an output file ...");
       Read_Name_and_Create_File(outfile);
-      get(infile,lp);
       put(outfile,natural32(lp'last),lp.all);
       new_line(outfile);
       put_line(outfile,"TITLE : start system in file " & name.all);
-      File_Scanning.Scan_and_skip(infile,"START SOLUTIONS",found);
-      if not found then
-        put_line("no start solutions found in " & name.all);
-      else
-        get(infile,len);
-        get(infile,dim);
-        get(infile,len,dim,sols);
-        put("Read "); put(len,1);
-        put(" solutions of dimension "); put(dim,1); put_line(".");
-        new_line(outfile);
-        put_line(outfile,"THE SOLUTIONS :");
-        put(outfile,len,dim,sols);
-      end if;
+      new_line(outfile);
+      put_line(outfile,"THE SOLUTIONS :");
+      put(outfile,Length_Of(sols),natural32(Head_Of(sols).n),sols);
       close(outfile);
     end if;
   end Main;
