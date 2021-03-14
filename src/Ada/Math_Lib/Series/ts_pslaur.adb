ccec20faca13ba3bf1c6659907837df488e854af
Jan Verschelde <janv@uic.edu>
2021-03-13 20:42:23 -0600

    defined addition and subtraction on Laurent series, with normalization

--- src/Ada/Math_Lib/Series/ts_pslaur.adb
+++ src/Ada/Math_Lib/Series/ts_pslaur.adb
@@ -1,6 +1,7 @@
 with text_io;                           use text_io;
 with Standard_Integer_Numbers;          use Standard_Integer_Numbers;
 with Standard_Integer_Numbers_io;       use Standard_Integer_Numbers_io;
+with Standard_Floating_Numbers;         use Standard_Floating_Numbers;
 with Standard_Complex_Numbers;          use Standard_Complex_Numbers;
 with Standard_Complex_Numbers_io;       use Standard_Complex_Numbers_io;
 with Standard_Complex_Vectors;
@@ -128,42 +129,238 @@ procedure ts_pslaur is
     Multiply(d,xe,iye,xc,iyc,ze,zc);
   end Divide;
 
+  procedure Normalize ( d : in integer32; e : in out integer32;
+                        c : in out Standard_Complex_Vectors.Vector;
+                        tol : in double_float := 1.0E-15 ) is
+
+  -- DESCRIPTION :
+  --   Normalizes the representation of the Laurent series
+  --   so that the leading coefficient is larger than tol.
+
+  -- ON ENTRY :
+  --   d       only coefficients in the range 0 to d are considered;
+  --   e       leading exponent of the series;
+  --   c       coefficient vector of the series;
+  --   tol     tolerance to decide the leading degree
+  --           when the first coefficient becomes too small.
+
+  -- ON RETURN :
+  --   e       augmented exponent for each shift,
+  --           if all coefficients are zero, then e is zero as well;
+  --   c       shifted coefficient vector so the leading coefficient
+  --           is nonzero, or zero for a zero series.
+
+    allzero : boolean := true;
+
+  begin
+    for i in 0..d loop
+      if (AbsVal(c(0)) > tol)
+       then allzero := false; exit;
+      end if;
+      e := e + 1;
+      for k in 1..d-i loop -- shift the coefficients
+        c(k-1) := c(k);
+      end loop;
+    end loop;
+    if allzero
+     then e := 0;
+    end if;
+  end Normalize;
+
+  procedure Add ( d,xe,ye : in integer32;
+                  xc,yc : in Standard_Complex_Vectors.Vector;
+                  ze : out integer32;
+                  zc : out Standard_Complex_Vectors.Vector;
+                  tol : in double_float := 1.0E-15 ) is
+
+  -- DESCRIPTION :
+  --   Adds two Laurent series, with a tolerance to determine
+  --   the leading degree when the leading coefficient is too small.
+
+  -- REQUIRED :
+  --   All coefficient vectors range between 0 and d,
+  --   where d is the same constant for all series.
+
+  -- ON ENTRY :
+  --   d       only coefficients in the range 0 to d are considered;
+  --   xe      leading exponent of the first series x;
+  --   ye      leading exponent of the second series y;
+  --   xc      coefficient vector of the first series x;
+  --   yc      coefficient vector of the second series y;
+  --   tol     tolerance to decide the leading degree
+  --           when the first coefficient becomes too small.
+
+  -- ON RETURN :
+  --   ze      leading exponent of the sum of x and y;
+  --   zc      coefficient vector of the sum of x and y.
+
+    gap : integer32; -- gap between the leading coefficients
+
+  begin
+    if xe < ye then
+      ze := xe;
+      gap := abs(ye - xe);
+      for i in 0..gap-1 loop
+        zc(i) := xc(i);
+      end loop;
+      for i in gap..d loop
+        zc(i) := xc(i) + yc(i-gap);
+      end loop;
+    elsif xe > ye then
+      ze := ye;
+      gap := abs(xe - ye);
+      for i in 0..gap-1 loop
+        zc(i) := yc(i);
+      end loop;
+      for i in gap..d loop
+        zc(i) := yc(i) + xc(i-gap);
+      end loop;
+    else -- xe = ye
+      ze := xe;
+      for i in 0..d loop
+        zc(i) := xc(i) + yc(i);
+      end loop;
+      Normalize(d,ze,zc,tol);
+    end if;
+  end Add;
+
+  procedure Subtract ( d,xe,ye : in integer32;
+                       xc,yc : in Standard_Complex_Vectors.Vector;
+                       ze : out integer32;
+                       zc : out Standard_Complex_Vectors.Vector;
+                       tol : in double_float := 1.0E-15 ) is
+
+  -- DESCRIPTION :
+  --   Subtracts two Laurent series, with a tolerance to determine
+  --   the leading degree when the leading coefficient is too small.
+
+  -- REQUIRED :
+  --   All coefficient vectors range between 0 and d,
+  --   where d is the same constant for all series.
+
+  -- ON ENTRY :
+  --   d       only coefficients in the range 0 to d are considered;
+  --   xe      leading exponent of the first series x;
+  --   ye      leading exponent of the second series y;
+  --   xc      coefficient vector of the first series x;
+  --   yc      coefficient vector of the second series y;
+  --   tol     tolerance to decide the leading degree
+  --           when the first coefficient becomes too small.
+
+  -- ON RETURN :
+  --   ze      leading exponent of the difference x - y;
+  --   zc      coefficient vector of the difference x - y.
+
+    gap : integer32; -- gap between the leading coefficients
+
+  begin
+    if xe < ye then
+      ze := xe;
+      gap := abs(ye - xe);
+      for i in 0..gap-1 loop
+        zc(i) := xc(i);
+      end loop;
+      for i in gap..d loop
+        zc(i) := xc(i) - yc(i-gap);
+      end loop;
+    elsif xe > ye then
+      ze := ye;
+      gap := abs(xe - ye);
+      for i in 0..gap-1 loop
+        zc(i) := -yc(i);
+      end loop;
+      for i in gap..d loop
+        zc(i) := xc(i-gap) - yc(i);
+      end loop;
+    else -- xe = ye
+      ze := xe;
+      for i in 0..d loop
+        zc(i) := xc(i) - yc(i);
+      end loop;
+      Normalize(d,ze,zc,tol);
+    end if;
+  end Subtract;
+
+  procedure Test_Multiply_Inverse_Divide ( deg : in integer32 ) is
+
+  -- DESCRIPTION :
+  --   Generates coefficient vectors for truncated Laurent series
+  --   to degree deg, to test multiplication, inverse, and division.
+
+    ale,ble,prodle,invble,quotle : integer32 := 0;
+    a : constant Standard_Complex_Vectors.Vector(0..deg)
+      := Standard_Random_Vectors.Random_Vector(0,deg);
+    b : constant Standard_Complex_Vectors.Vector(0..deg)
+      := Standard_Random_Vectors.Random_Vector(0,deg);
+    prod : Standard_Complex_Vectors.Vector(0..deg);
+    invb : Standard_Complex_Vectors.Vector(0..deg);
+    quot : Standard_Complex_Vectors.Vector(0..deg);
+
+  begin
+    new_line;
+    put("Give the leading exponent of a : "); get(ale);
+    put("Give the leading exponent of b : "); get(ble);
+    new_line;
+    put_line("A random series a :"); Write(ale,a);
+    new_line;
+    put_line("A random series b :"); Write(ble,b);
+    Multiply(deg,ale,ble,a,b,prodle,prod);
+    new_line;
+    put_line("The product of a with b :"); Write(prodle,prod);
+    Divide(deg,prodle,ble,prod,b,quotle,quot,invb);
+    new_line;
+    put_line("The quotient of a*b with b :"); Write(quotle,quot);
+    invble := -ble;
+    Multiply(deg,ble,invble,b,invb,prodle,prod);
+    put_line("The product of b with 1/b :"); Write(prodle,prod);
+  end Test_Multiply_Inverse_Divide;
+
+  procedure Test_Add_and_Subtract ( deg : in integer32 ) is
+
+  -- DESCRIPTION :
+  --   Generates coefficient vectors for truncated Laurent series
+  --   to degree deg, to test addition and subtraction.
+
+    ale,ble,sumle,difle : integer32 := 0;
+    a : constant Standard_Complex_Vectors.Vector(0..deg)
+      := Standard_Random_Vectors.Random_Vector(0,deg);
+    b : constant Standard_Complex_Vectors.Vector(0..deg)
+      := Standard_Random_Vectors.Random_Vector(0,deg);
+    sum : Standard_Complex_Vectors.Vector(0..deg);
+    dif : Standard_Complex_Vectors.Vector(0..deg);
+
+  begin
+    new_line;
+    put("Give the leading exponent of a : "); get(ale);
+    put("Give the leading exponent of b : "); get(ble);
+    new_line;
+    put_line("A random series a :"); Write(ale,a);
+    new_line;
+    put_line("A random series b :"); Write(ble,b);
+    Add(deg,ale,ble,a,b,sumle,sum);
+    new_line;
+    put_line("The sum of a and b :"); Write(sumle,sum);
+    Subtract(deg,sumle,ble,sum,b,difle,dif);
+    new_line;
+    put_line("The result of (a + b) - b :"); Write(difle,dif);
+    Subtract(deg,ale,ale,a,a,difle,dif);
+    new_line;
+    put_line("The result of a - a :"); Write(difle,dif);
+  end Test_Add_and_Subtract;
+
   procedure Main is
 
   -- DESCRIPTION :
-  --   Prompts for the degree of truncation, leading exponents,
+  --   Prompts for the degree of truncation,
   --   and tests some basic arithmetical operations.
 
-     d,ale,ble,prodle,invble,quotle : integer32 := 0;
+     d : integer32 := 0;
 
   begin
     new_line;
     put("Give the truncation degree : "); get(d);
-    put("Give the leading exponent of a : "); get(ale);
-    put("Give the leading exponent of b : "); get(ble);
-    declare
-      a : constant Standard_Complex_Vectors.Vector(0..d)
-        := Standard_Random_Vectors.Random_Vector(0,d);
-      b : constant Standard_Complex_Vectors.Vector(0..d)
-        := Standard_Random_Vectors.Random_Vector(0,d);
-      prod : Standard_Complex_Vectors.Vector(0..d);
-      invb : Standard_Complex_Vectors.Vector(0..d);
-      quot : Standard_Complex_Vectors.Vector(0..d);
-    begin
-      new_line;
-      put_line("A random series a :"); Write(ale,a);
-      new_line;
-      put_line("A random series b :"); Write(ble,b);
-      Multiply(d,ale,ble,a,b,prodle,prod);
-      new_line;
-      put_line("The product of a with b :"); Write(prodle,prod);
-      Divide(d,prodle,ble,prod,b,quotle,quot,invb);
-      new_line;
-      put_line("The quotient of a*b with b :"); Write(quotle,quot);
-      invble := -ble;
-      Multiply(d,ble,invble,b,invb,prodle,prod);
-      put_line("The product of b with 1/b :"); Write(prodle,prod);
-    end;
+    Test_Multiply_Inverse_Divide(d);
+    Test_Add_and_Subtract(d);
   end Main;
 
 begin
