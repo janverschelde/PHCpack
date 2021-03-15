5ea060da875dca161b8ef5ce8eed0139df88bdac
Jan Verschelde <janv@uic.edu>
2021-03-14 20:19:25 -0500

    new package with Laurent series arithmetic in double precision

---
+++ src/Ada/Math_Lib/Series/standard_laurent_series.adb
@@ -0,0 +1,156 @@
+with text_io;                           use text_io;
+with Standard_Integer_Numbers_io;       use Standard_Integer_Numbers_io;
+with Standard_Complex_Numbers;          use Standard_Complex_Numbers;
+with Standard_Complex_Numbers_io;       use Standard_Complex_Numbers_io;
+
+package body Standard_Laurent_Series is
+
+  procedure Write ( e : in integer32;
+                    c : in Standard_Complex_Vectors.Vector ) is
+  begin
+    for i in c'range loop
+      if i > c'first
+       then put(" + (");
+       else put("   (");
+      end if;
+      put(c(i)); put(")*t^"); put(e+i,1); new_line;
+    end loop;
+  end Write;
+
+  procedure Multiply ( d,xe,ye : in integer32;
+                       xc,yc : in Standard_Complex_Vectors.Vector;
+                       ze : out integer32;
+                       zc : out Standard_Complex_Vectors.Vector ) is
+  begin
+    ze := xe + ye;
+    for i in 0..d loop
+       zc(i) := xc(0)*yc(i);
+       for j in 1..i loop
+         zc(i) := zc(i) + xc(j)*yc(i-j);
+       end loop;
+    end loop;
+  end Multiply;
+
+  procedure Inverse ( d,xe : in integer32;
+                      xc : in Standard_Complex_Vectors.Vector;
+                      ye : out integer32;
+                      yc : out Standard_Complex_Vectors.Vector ) is
+  begin
+    ye := -xe;
+    yc(0) := 1.0/xc(0);
+    for i in 1..d loop
+      yc(i) := -xc(1)*yc(i-1);
+      for j in 2..i loop
+        yc(i) := yc(i) - xc(j)*yc(i-j);
+      end loop;
+      yc(i) := yc(i)/xc(0);
+    end loop;
+  end Inverse;
+
+  procedure Divide ( d,xe,ye : in integer32;
+                     xc,yc : in Standard_Complex_Vectors.Vector;
+                     ze : out integer32;
+                     zc : out Standard_Complex_Vectors.Vector;
+                     iyc : out Standard_Complex_Vectors.Vector ) is
+
+    iye : integer32;
+
+  begin
+    Inverse(d,ye,yc,iye,iyc);
+    Multiply(d,xe,iye,xc,iyc,ze,zc);
+  end Divide;
+
+  procedure Normalize ( d : in integer32; e : in out integer32;
+                        c : in out Standard_Complex_Vectors.Vector;
+                        tol : in double_float := 1.0E-15 ) is
+
+    allzero : boolean := true;
+
+  begin
+    for i in 0..d loop
+      if (AbsVal(c(0)) > tol)
+       then allzero := false; exit;
+      end if;
+      e := e + 1;
+      for k in 1..d-i loop -- shift the coefficients
+        c(k-1) := c(k);
+      end loop;
+    end loop;
+    if allzero
+     then e := 0;
+    end if;
+  end Normalize;
+
+  procedure Add ( d,xe,ye : in integer32;
+                  xc,yc : in Standard_Complex_Vectors.Vector;
+                  ze : out integer32;
+                  zc : out Standard_Complex_Vectors.Vector;
+                  tol : in double_float := 1.0E-15 ) is
+
+    gap : integer32; -- gap between the leading coefficients
+
+  begin
+    if xe < ye then
+      ze := xe;
+      gap := abs(ye - xe);
+      for i in 0..gap-1 loop
+        zc(i) := xc(i);
+      end loop;
+      for i in gap..d loop
+        zc(i) := xc(i) + yc(i-gap);
+      end loop;
+    elsif xe > ye then
+      ze := ye;
+      gap := abs(xe - ye);
+      for i in 0..gap-1 loop
+        zc(i) := yc(i);
+      end loop;
+      for i in gap..d loop
+        zc(i) := yc(i) + xc(i-gap);
+      end loop;
+    else -- xe = ye
+      ze := xe;
+      for i in 0..d loop
+        zc(i) := xc(i) + yc(i);
+      end loop;
+      Normalize(d,ze,zc,tol);
+    end if;
+  end Add;
+
+  procedure Subtract ( d,xe,ye : in integer32;
+                       xc,yc : in Standard_Complex_Vectors.Vector;
+                       ze : out integer32;
+                       zc : out Standard_Complex_Vectors.Vector;
+                       tol : in double_float := 1.0E-15 ) is
+
+    gap : integer32; -- gap between the leading coefficients
+
+  begin
+    if xe < ye then
+      ze := xe;
+      gap := abs(ye - xe);
+      for i in 0..gap-1 loop
+        zc(i) := xc(i);
+      end loop;
+      for i in gap..d loop
+        zc(i) := xc(i) - yc(i-gap);
+      end loop;
+    elsif xe > ye then
+      ze := ye;
+      gap := abs(xe - ye);
+      for i in 0..gap-1 loop
+        zc(i) := -yc(i);
+      end loop;
+      for i in gap..d loop
+        zc(i) := xc(i-gap) - yc(i);
+      end loop;
+    else -- xe = ye
+      ze := xe;
+      for i in 0..d loop
+        zc(i) := xc(i) - yc(i);
+      end loop;
+      Normalize(d,ze,zc,tol);
+    end if;
+  end Subtract;
+
+end Standard_Laurent_Series;
---
+++ src/Ada/Math_Lib/Series/standard_laurent_series.ads
@@ -0,0 +1,165 @@
+with Standard_Integer_Numbers;          use Standard_Integer_Numbers;
+with Standard_Floating_Numbers;         use Standard_Floating_Numbers;
+with Standard_Complex_Vectors;
+
+package Standard_Laurent_Series is
+
+-- DESCRIPTION :
+--   Defines operations on Laurent series with complex coefficients.
+--   A Laurent series is defined by a leading exponent and a coefficient
+--   vector, with a typical range from 0 to some degree d, d > 0.
+--   In this intermediate proof-of-concept package,
+--   the data are not encapsulated by types.
+
+  procedure Write ( e : in integer32;
+                    c : in Standard_Complex_Vectors.Vector );
+
+  -- DESCRIPTION :
+  --   Writes the power series with leading exponent e
+  --   and coefficients in c.
+
+  procedure Multiply ( d,xe,ye : in integer32;
+                       xc,yc : in Standard_Complex_Vectors.Vector;
+                       ze : out integer32;
+                       zc : out Standard_Complex_Vectors.Vector );
+
+  -- DESCRIPTION :
+  --   Multiplies two Laurent series.
+
+  -- REQUIRED :
+  --   All coefficient vectors range between 0 and d,
+  --   where d is the same constant for all series.
+
+  -- ON ENTRY :
+  --   d       only coefficients in the range 0 to d are considered;
+  --   xe      leading exponent of the first series x;
+  --   ye      leading exponent of the second series y;
+  --   xc      coefficient vector of the first series x;
+  --   yc      coefficient vector of the second series y.
+
+  -- ON RETURN :
+  --   ze      leading exponent of the product of x with y;
+  --   zc      coefficient vector of the product of x with y.
+
+  procedure Inverse ( d,xe : in integer32;
+                      xc : in Standard_Complex_Vectors.Vector;
+                      ye : out integer32;
+                      yc : out Standard_Complex_Vectors.Vector );
+
+  -- DESCRIPTION :
+  --   Computes the inverse of a Laurent series.
+  --   Any nonzero Laurent series has a nonzero leading coefficient.
+
+  -- REQUIRED : xc'first = 0 = yc'first and xc'last = yc'last.
+
+  -- ON ENTRY :
+  --   d       truncation degree of the series;
+  --   xe      leading exponent of a nonzero series x;
+  --   xc      coefficient vector of the series x.
+
+  -- ON RETURN :
+  --   ye      leading exponent of the inverse of x;
+  --   yc      coefficient vector of the inverse of x.
+
+  procedure Divide ( d,xe,ye : in integer32;
+                     xc,yc : in Standard_Complex_Vectors.Vector;
+                     ze : out integer32;
+                     zc : out Standard_Complex_Vectors.Vector;
+                     iyc : out Standard_Complex_Vectors.Vector );
+
+  -- DESCRIPTION :
+  --   Divides two Laurent series.
+
+  -- REQUIRED :
+  --   All coefficient vectors range between 0 and d,
+  --   where d is the same constant for all series.
+  --   The second series y should be nonzero.
+
+  -- ON ENTRY :
+  --   d       only coefficients in the range 0 to d are considered;
+  --   xe      leading exponent of the first series x;
+  --   ye      leading exponent of the second series y;
+  --   xc      coefficient vector of the first series x;
+  --   yc      coefficient vector of the second series y;
+
+  -- ON RETURN :
+  --   ze      leading exponent of the product of x with y;
+  --   zc      coefficient vector of the product of x with y;
+  --   iyc     coefficient vector of the inverse of y, as work space.
+
+  procedure Normalize ( d : in integer32; e : in out integer32;
+                        c : in out Standard_Complex_Vectors.Vector;
+                        tol : in double_float := 1.0E-15 );
+
+  -- DESCRIPTION :
+  --   Normalizes the representation of the Laurent series
+  --   so that the leading coefficient is larger than tol.
+
+  -- ON ENTRY :
+  --   d       only coefficients in the range 0 to d are considered;
+  --   e       leading exponent of the series;
+  --   c       coefficient vector of the series;
+  --   tol     tolerance to decide the leading degree
+  --           when the first coefficient becomes too small.
+
+  -- ON RETURN :
+  --   e       augmented exponent for each shift,
+  --           if all coefficients are zero, then e is zero as well;
+  --   c       shifted coefficient vector so the leading coefficient
+  --           is nonzero, or zero for a zero series.
+
+  procedure Add ( d,xe,ye : in integer32;
+                  xc,yc : in Standard_Complex_Vectors.Vector;
+                  ze : out integer32;
+                  zc : out Standard_Complex_Vectors.Vector;
+                  tol : in double_float := 1.0E-15 );
+
+  -- DESCRIPTION :
+  --   Adds two Laurent series, with a tolerance to determine
+  --   the leading degree when the leading coefficient is too small.
+
+  -- REQUIRED :
+  --   All coefficient vectors range between 0 and d,
+  --   where d is the same constant for all series.
+
+  -- ON ENTRY :
+  --   d       only coefficients in the range 0 to d are considered;
+  --   xe      leading exponent of the first series x;
+  --   ye      leading exponent of the second series y;
+  --   xc      coefficient vector of the first series x;
+  --   yc      coefficient vector of the second series y;
+  --   tol     tolerance to decide the leading degree
+  --           when the first coefficient becomes too small.
+
+  -- ON RETURN :
+  --   ze      leading exponent of the sum of x and y;
+  --   zc      coefficient vector of the sum of x and y.
+
+  procedure Subtract ( d,xe,ye : in integer32;
+                       xc,yc : in Standard_Complex_Vectors.Vector;
+                       ze : out integer32;
+                       zc : out Standard_Complex_Vectors.Vector;
+                       tol : in double_float := 1.0E-15 );
+
+  -- DESCRIPTION :
+  --   Subtracts two Laurent series, with a tolerance to determine
+  --   the leading degree when the leading coefficient is too small.
+
+  -- REQUIRED :
+  --   All coefficient vectors range between 0 and d,
+  --   where d is the same constant for all series.
+
+  -- ON ENTRY :
+  --   d       only coefficients in the range 0 to d are considered;
+  --   xe      leading exponent of the first series x;
+  --   ye      leading exponent of the second series y;
+  --   xc      coefficient vector of the first series x;
+  --   yc      coefficient vector of the second series y;
+  --   tol     tolerance to decide the leading degree
+  --           when the first coefficient becomes too small.
+
+  -- ON RETURN :
+  --   ze      leading exponent of the difference x - y;
+  --   zc      coefficient vector of the difference x - y.
+
+end Standard_Laurent_Series;
