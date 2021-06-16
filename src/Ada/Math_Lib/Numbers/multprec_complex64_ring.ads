d16e0d3e1010a9d89e90195a4e774217c97895a0
Jan Verschelde <janv@uic.edu>
2021-06-15 19:06:43 -0500

    forgot to push an update

--- src/Ada/Math_Lib/Numbers/multprec_complex64_ring.ads
+++ src/Ada/Math_Lib/Numbers/multprec_complex64_ring.ads
@@ -2,7 +2,7 @@ with Multprec_Complex64_Numbers;         use Multprec_Complex64_Numbers;
 with Abstract_Ring;
 
 package Multprec_Complex64_Ring is
-  new Abstract_Ring(Complex_Number,Create(0),Create(1),
+  new Abstract_Ring(Complex_Number,Create(integer(0)),Create(integer(1)),
                     Create,Equal,Copy,"+","+","-","-","*",
                     Add,Sub,Min,Mul,Clear);
 
