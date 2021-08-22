e1812ddb928ab3908dee7894d1dae0579749231d
Jan Verschelde <janv@uic.edu>
2021-08-21 21:38:49 -0500

    added functions to generate real and complex matrices of quad doubles

---
+++ src/GPU/Matrices/random4_matrices.cpp
@@ -0,0 +1,110 @@
+// The file random4_matrices.cpp defines the functions specified in
+// the file random4_matrices.h.
+
+#include "random4_vectors.h"
+#include "random4_matrices.h"
+#include "quad_double_functions.h"
+
+void random_dbl4_upper_matrix
+ ( int rows, int cols,
+   double **Ahihi, double **Alohi, double **Ahilo, double **Alolo )
+{
+   for(int i=0; i<rows; i++)
+   {
+      for(int j=0; j<i; j++)
+      {
+         Ahihi[i][j] = 0.0; Alohi[i][j] = 0.0;
+         Ahilo[i][j] = 0.0; Alolo[i][j] = 0.0;
+      }
+      for(int j=i; j<cols; j++)
+         random_quad_double
+            (&Ahihi[i][j],&Alohi[i][j],&Ahilo[i][j],&Alolo[i][j]);
+   }
+}
+
+void random_cmplx4_upper_matrix
+ ( int rows, int cols,
+   double **Arehihi, double **Arelohi, double **Arehilo, double **Arelolo,
+   double **Aimhihi, double **Aimlohi, double **Aimhilo, double **Aimlolo )
+{
+   double rnd_hihi,rnd_lohi,rnd_hilo,rnd_lolo;
+   double cosrnd_hihi,cosrnd_lohi,cosrnd_hilo,cosrnd_lolo;
+   double sinrnd_hihi,sinrnd_lohi,sinrnd_hilo,sinrnd_lolo;
+
+   for(int i=0; i<rows; i++)
+   {
+      for(int j=0; j<i; j++)
+      {
+         Arehihi[i][j] = 0.0; Arelohi[i][j] = 0.0;
+         Arehilo[i][j] = 0.0; Arelolo[i][j] = 0.0;
+         Aimhihi[i][j] = 0.0; Aimlohi[i][j] = 0.0;
+         Aimhilo[i][j] = 0.0; Aimlolo[i][j] = 0.0;
+      }
+      for(int j=i; j<cols; j++)
+      {
+         random_quad_double(&rnd_hihi,&rnd_lohi,&rnd_hilo,&rnd_lolo);
+         sinrnd_hihi = rnd_hihi; sinrnd_lohi = rnd_lohi;
+         sinrnd_hilo = rnd_hilo; sinrnd_lolo = rnd_lolo;
+
+         double y_hihi,y_lohi,y_hilo,y_lolo;  // work around to compute cos
+
+         qdf_sqr(sinrnd_hi,sinrnd_lo,&y_hi,&y_lo);
+         qdf_minus(&y_hihi,&y_lohi,&y_hilo,&y_lolo);       // y = -sin^2
+         qdf_inc_d(&y_hihi,&y_lohi,&y_hilo,&y_lolo,1.0);   // y = 1 - sin^2
+                                                      // cos is sqrt(1-sin^2)
+         qdf_sqrt(y_hihi,y_lohi,y_hilo,y_lolo,
+                  &cosrnd_hihi,&cosrnd_lohi,&cosrnd_hilo,&cosrnd_lolo);
+
+         Arehihi[i][j] = cosrnd_hihi; Arelohi[i][j] = cosrnd_lohi;
+         Arehilo[i][j] = cosrnd_hilo; Arelolo[i][j] = cosrnd_lolo;
+         Aimhihi[i][j] = sinrnd_hihi; Aimlohi[i][j] = sinrnd_lohi;
+         Aimhilo[i][j] = sinrnd_hilo; Aimlolo[i][j] = sinrnd_lolo;
+      }
+   }
+}
+
+void random_dbl4_matrix
+ ( int rows, int cols,
+   double **Ahihi, double **Alohi, double **Ahilo, double **Alolo )
+{
+   for(int i=0; i<rows; i++)
+   {
+      for(int j=0; j<cols; j++)
+         random_quad_double
+            (&Ahihi[i][j],&Alohi[i][j],&Ahilo[i][j],&Alolo[i][j]);
+   }
+}
+
+void random_cmplx4_matrix
+ ( int rows, int cols,
+   double **Arehihi, double **Arelohi, double **Arehilo, double **Arelolo,
+   double **Aimhihi, double **Aimlohi, double **Aimhilo, double **Aimlolo )
+{
+   double rnd_hihi,rnd_lohi,rnd_hilo,rnd_lolo;
+   double cosrnd_hihi,cosrnd_lohi,cosrnd_hilo,cosrnd_lolo;
+   double sinrnd_hihi,sinrnd_lohi,sinrnd_hilo,sinrnd_lolo;
+
+   for(int i=0; i<rows; i++)
+   {
+      for(int j=0; j<cols; j++)
+      {
+         random_quad_double(&rnd_hihi,&rnd_lohi,&rnd_hilo,&rnd_lolo);
+         sinrnd_hihi = rnd_hihi; sinrnd_lohi = rnd_lohi;
+         sinrnd_hilo = rnd_hilo; sinrnd_lolo = rnd_lolo;
+
+         double y_hihi,y_lohi,y_hilo,y_lolo;  // work around to compute cos
+
+         qdf_sqr(sinrnd_hi,sinrnd_lo,&y_hi,&y_lo);
+         qdf_minus(&y_hihi,&y_lohi,&y_hilo,&y_lolo);       // y = -sin^2
+         qdf_inc_d(&y_hihi,&y_lohi,&y_hilo,&y_lolo,1.0);   // y = 1 - sin^2
+                                                      // cos is sqrt(1-sin^2)
+         qdf_sqrt(y_hihi,y_lohi,y_hilo,y_lolo,
+                  &cosrnd_hihi,&cosrnd_lohi,&cosrnd_hilo,&cosrnd_lolo);
+
+         Arehihi[i][j] = cosrnd_hihi; Arelohi[i][j] = cosrnd_lohi;
+         Arehilo[i][j] = cosrnd_hilo; Arelolo[i][j] = cosrnd_lolo;
+         Aimhihi[i][j] = sinrnd_hihi; Aimlohi[i][j] = sinrnd_lohi;
+         Aimhilo[i][j] = sinrnd_hilo; Aimlolo[i][j] = sinrnd_lolo;
+      }
+   }
+}
---
+++ src/GPU/Matrices/random4_matrices.h
@@ -0,0 +1,119 @@
+// The file random4_matrices.h specifies functions define tests to work
+// with matrices and vectors in quad double precision.
+
+#ifndef __random4_matrices_h__
+#define __random4_matrices_h__
+
+void random_dbl4_upper_matrix
+ ( int rows, int cols,
+   double **Ahihi, double **Alohi, double **Ahilo, double **Alolo );
+/*
+ * DESCRIPTION :
+ *   Returns in (Ahihi, Alohi, Ahilo, Alolo) a real upper triangular matrix
+ *   of dimensions rows and cols, of randomly generated quad doubles.
+ *
+ * ON ENTRY :
+ *   rows     the number of rows of all matrices;
+ *   cols     the number of columns of all matrices;
+ *   Ahi      space for a matrix, of dimensions rows and cols;
+ *   Alo      space for a matrix, of dimensions rows and cols;
+ *   Ahi      space for a matrix, of dimensions rows and cols;
+ *   Alo      space for a matrix, of dimensions rows and cols.
+ *
+ * ON RETURN :
+ *   Ahihi    the highest doubles of an upper triangular matrix
+ *            of randomly generated quad doubles;
+ *   Alohi    the second highest doubles of an upper triangular matrix
+ *            of randomly generated quad doubles;
+ *   Ahilo    the second lowes doubles of an upper triangular matrix
+ *            of randomly generated quad doubles;
+ *   Alolo    thees low doubles of an upper triangular matrix
+ *            of randomly generated quad doubles. */
+
+void random_cmplx4_upper_matrix
+ ( int rows, int cols,
+   double **Arehihi, double **Arelohi, double **Arehilo, double **Arelolo,
+   double **Aimhihi, double **Aimlohi, double **Aimhilo, double **Aimlolo );
+/*
+ * DESCRIPTION :
+ *   Returns in (Arehihi, Arelohi, Arehilo, Arelolo, Aimhihi, Aimlohi,
+ *   Aimhilo, Aimlolo) a complex upper triangular matrix of dimensions
+ *   rows and cols, of randomly generated quad doubles.
+ *
+ * ON ENTRY :
+ *   rows     the number of rows of all matrices;
+ *   cols     the number of columns of all matrices;
+ *   Arehihi  space for a matrix, of dimensions rows and cols;
+ *   Arelohi  space for a matrix, of dimensions rows and cols;
+ *   Arehilo  space for a matrix, of dimensions rows and cols;
+ *   Arelolo  space for a matrix, of dimensions rows and cols;
+ *   Aimhihi  space for a matrix, of dimensions rows and cols;
+ *   Aimlohi  space for a matrix, of dimensions rows and cols.
+ *   Aimhilo  space for a matrix, of dimensions rows and cols;
+ *   Aimlolo  space for a matrix, of dimensions rows and cols.
+ *
+ * ON RETURN :
+ *   Arehihi    highest doubles of the real parts of a random matrix;
+ *   Arelohi    second highest doubles of the real parts of a random matrix;
+ *   Arehilo    second low doubles of the real parts of a random matrix;
+ *   Arelolo    lowest doubles of the real parts of a random matrix;
+ *   Aimhihi    highest doubles of the imaginary parts of a random matrix;
+ *   Aimlohi    second highest doubles of the imaginary parts;
+ *   Aimhilo    second lowest doubles of the imaginary parts;
+ *   Aimlolo    lowest doubles of the imaginary parts of a random matrix. */
+
+void random_dbl4_matrix
+ ( int rows, int cols,
+   double **Ahihi, double **Alohi, double **Ahilo, double **Alolo );
+/*
+ * DESCRIPTION :
+ *   Returns in (Ahihi, Alohi, Ahilo, Alolo) a real matrix
+ *   of dimensions rows and cols, of randomly generated quad doubles.
+ *
+ * ON ENTRY :
+ *   rows     the number of rows of all matrices;
+ *   cols     the number of columns of all matrices;
+ *   Ahihi    space for a matrix, of dimensions rows and cols;
+ *   Alohi    space for a matrix, of dimensions rows and cols;
+ *   Ahilo    space for a matrix, of dimensions rows and cols.
+ *   Alolo    space for a matrix, of dimensions rows and cols.
+ *
+ * ON RETURN :
+ *   Ahihi    highest doubles of a random matrix;
+ *   Alohi    second highest doubles of a random matrix;
+ *   Alohi    second lowest doubles of a random matrix;
+ *   Alolo    lowest doubles of a random matrix. */
+
+void random_cmplx4_matrix
+ ( int rows, int cols,
+   double **Arehihi, double **Arelohi, double **Arehilo, double **Arelolo,
+   double **Aimhihi, double **Aimlohi, double **Aimhilo, double **Aimlolo );
+/*
+ * DESCRIPTION :
+ *   Returns in (Arehihi, Arelohi, Arehilo, Arelolo, Aimhihi, Aimlohi,
+ *   Aimhilo, Aimlolo) a complex matrix of dimensions rows and cols,
+ *   of randomly generated quad doubles.
+ *
+ * ON ENTRY :
+ *   rows     the number of rows of all matrices;
+ *   cols     the number of columns of all matrices;
+ *   Arehihi  space for a matrix, of dimensions rows and cols;
+ *   Arelohi  space for a matrix, of dimensions rows and cols;
+ *   Arehilo  space for a matrix, of dimensions rows and cols;
+ *   Arelolo  space for a matrix, of dimensions rows and cols;
+ *   Aimhihi  space for a matrix, of dimensions rows and cols;
+ *   Aimlohi  space for a matrix, of dimensions rows and cols.
+ *   Aimhilo  space for a matrix, of dimensions rows and cols;
+ *   Aimlolo  space for a matrix, of dimensions rows and cols.
+ *
+ * ON RETURN :
+ *   Arehihi    highest doubles of the real parts of a random matrix;
+ *   Arelohi    second highest doubles of the real parts of a random matrix;
+ *   Arehilo    second low doubles of the real parts of a random matrix;
+ *   Arelolo    lowest doubles of the real parts of a random matrix;
+ *   Aimhihi    highest doubles of the imaginary parts of a random matrix;
+ *   Aimlohi    second highest doubles of the imaginary parts;
+ *   Aimhilo    second lowest doubles of the imaginary parts;
+ *   Aimlolo    lowest doubles of the imaginary parts of a random matrix. */
+
+#endif
