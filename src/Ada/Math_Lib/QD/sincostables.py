9d47b2068200ef38fd3ff287e2e47e5d63349c6f
Jan Verschelde <janv@uic.edu>
2021-12-05 16:42:09 -0600

    updated the Python3 script to generate the sine and cosine tables for 256 decimal places

--- src/Ada/Math_Lib/QD/sincostables.py
+++ src/Ada/Math_Lib/QD/sincostables.py
@@ -1,6 +1,6 @@
 """
-Python script to compute with sympy the sines and cosines
-of multiples of pi/1024 with 160 decimal places.
+Python3 script to compute with sympy the sines and cosines
+of multiples of pi/1024 with 256 decimal places.
 The output is formatted as Ada assignments of constant strings.
 """
 from sympy import pi, sin, cos, evalf
@@ -8,25 +8,29 @@ S = []
 C = []
 for k in range(1, 257):
     p = k*pi/1024
-    s = sin(p).evalf(160)
+    s = sin(p).evalf(256)
     S.append(s)
-    c = cos(p).evalf(160)
+    c = cos(p).evalf(256)
     C.append(c)
 print('sine table :')
 for k in range(len(S)):
     print('    sint%03d : constant string' % k)
     s = str(S[k])
-    L = len(s)//3
-    (s0, s1, s2) = (s[0:L], s[L:2*L], s[2*L:])
+    L = len(s)//5
+    (s0, s1, s2, s3, s4) = (s[0:L], s[L:2*L], s[2*L:3*L], s[3*L:4*L], s[4*L:])
     print('            := \"%s\"' % s0)
     print('             & \"%s\"' % s1)
-    print('             & \"%s\";' % s2)
+    print('             & \"%s\"' % s2)
+    print('             & \"%s\"' % s3)
+    print('             & \"%s\";' % s4)
 print('cosine table :')
 for k in range(len(C)):
     print('    cost%03d : constant string' % k)
     c = str(C[k])
-    L = len(s)//3
-    (c0, c1, c2) = (c[0:L], c[L:2*L], c[2*L:])
+    L = len(s)//5
+    (c0, c1, c2, c3, c4) = (c[0:L], c[L:2*L], c[2*L:3*L], c[3*L:4*L], c[4*L:])
     print('            := \"%s\"' % c0)
     print('             & \"%s\"' % c1)
-    print('             & \"%s\";' % c2)
+    print('             & \"%s\"' % c2)
+    print('             & \"%s\"' % c3)
+    print('             & \"%s\";' % c4)
