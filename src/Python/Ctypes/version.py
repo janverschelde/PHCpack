55187d9c3c6378e7aac7f29dd19ed28a26d7c815
Jan Verschelde <janv@uic.edu>
2023-01-15 17:44:24 -0600

    added a function to get the library module for any platform

--- src/Python/Ctypes/version.py
+++ src/Python/Ctypes/version.py
@@ -1,28 +1,39 @@
 """
 Tests the retrieval of the version string of the PHCpack library.
+The function getPHCmod() returns the library module for the three
+supported platforms.
 """
 
 import ctypes
 from ctypes import c_int, c_double, pointer
 from ctypes import create_string_buffer, sizeof
-import sys
 
-if 'linux' in sys.platform:
-    LIBPHCPACK = "../../lib/libPHCpack.so"
-    modPHCpack = ctypes.CDLL(LIBPHCPACK)
-elif 'darwin' in sys.platform:
-    LIBPHCPACK = "../../lib/libPHCpack.dylib"
-    modPHCpack = ctypes.CDLL(LIBPHCPACK)
-elif 'win' in sys.platform:
-    LIBPHCPACK = "../../lib/libPHCpack"
-    modPHCpack = ctypes.WinDLL(LIBPHCPACK, winmode=0)
-else:
-    print('The platform', sys.platform, 'is not supported.')
+def getPHCmod():
+    """
+    Returns the proper module according to the platform.
+    """
+    import sys
+    if 'linux' in sys.platform:
+        LIBPHCPACK = "../../lib/libPHCpack.so"
+        modPHCpack = ctypes.CDLL(LIBPHCPACK)
+        return modPHCpack
+    elif 'darwin' in sys.platform:
+        LIBPHCPACK = "../../lib/libPHCpack.dylib"
+        modPHCpack = ctypes.CDLL(LIBPHCPACK)
+        return modPHCpack
+    elif 'win' in sys.platform:
+        LIBPHCPACK = "../../lib/libPHCpack"
+        modPHCpack = ctypes.WinDLL(LIBPHCPACK, winmode=0)
+        return modPHCpack
+    else:
+        print('The platform', sys.platform, 'is not supported.')
+        return None
 
 def version(verbose=True):
     """
     Returns the version string of PHCpack.
     """
+    modPHCpack = getPHCmod()
     f = modPHCpack._ada_use_c2phc
     a = pointer(c_int(0))
     b = create_string_buffer(30*4)
@@ -42,4 +53,5 @@ def version(verbose=True):
          print(result)
     return result
 
-print(version(False))
+if __name__=="__main__":
+    print(version(False))
