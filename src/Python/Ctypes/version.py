8f68a7d300ef991bf7f421184ad3b3f87fb8745b
Jan Verschelde <janv@uic.edu>
2023-01-15 17:19:09 -0600

    made the version.py to be platform independent

--- src/Python/Ctypes/version.py
+++ src/Python/Ctypes/version.py
@@ -1,15 +1,29 @@
+"""
+Tests the retrieval of the version string of the PHCpack library.
+"""
+
 import ctypes
 from ctypes import c_int, c_double, pointer
 from ctypes import create_string_buffer, sizeof
+import sys
 
-LIBPHCPACK = "../lib/libPHCpack"
+if 'linux' in sys.platform:
+    LIBPHCPACK = "../../lib/libPHCpack.so"
+    modPHCpack = ctypes.CDLL(LIBPHCPACK)
+elif 'darwin' in sys.platform:
+    LIBPHCPACK = "../../lib/libPHCpack.dylib"
+    modPHCpack = ctypes.CDLL(LIBPHCPACK)
+elif 'win' in sys.platform:
+    LIBPHCPACK = "../../lib/libPHCpack"
+    modPHCpack = ctypes.WinDLL(LIBPHCPACK, winmode=0)
+else:
+    print('The platform', sys.platform, 'is not supported.')
 
 def version(verbose=True):
     """
     Returns the version string of PHCpack.
     """
-    d = ctypes.WinDLL(LIBPHCPACK, winmode=0)
-    f = d._ada_use_c2phc
+    f = modPHCpack._ada_use_c2phc
     a = pointer(c_int(0))
     b = create_string_buffer(30*4)
     c = pointer(c_double(0.0))
