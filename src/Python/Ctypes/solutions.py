c9f2cb205aab96a7ce97239b8b338314f092729a
Jan Verschelde <janv@uic.edu>
2023-01-20 21:28:00 -0600

    extended the solutions module with functions to write all solutions

--- src/Python/Ctypes/solutions.py
+++ src/Python/Ctypes/solutions.py
@@ -1,8 +1,8 @@
 """
 Exports operations on solutions.
 """
-from ctypes import c_int, c_double, pointer
-from version import getPHCmod
+from ctypes import c_int, c_double, pointer, create_string_buffer
+from version import getPHCmod, int4a2str
 from polynomials import set_double_system
 from solver import solve_double_system, write_double_solutions
 
@@ -24,6 +24,48 @@ def number_double_solutions(vrblvl=0):
         print('-> number_double_solutions, return value :', retval)
     return bbb[0]
 
+def get_next_double_solution(idx, vrblvl=0):
+    """
+    Returns the string representation of the next solution
+    in double precision, at the index idx.
+    The vrblvl is the verbose level.
+    """
+    if vrblvl > 0:
+        print("-> get_next_double_solution, vrblvl =", vrblvl)
+    phcpack = getPHCmod()
+    phc = phcpack._ada_use_c2phc
+    aaa = pointer(c_int(idx)) # at the given index
+    bbb = pointer(c_int(0))
+    ccc = pointer(c_double(0.0))
+    vrb = c_int(vrblvl)
+    retval = phc(525, aaa, bbb, ccc, vrb)
+    size = bbb[0];
+    if vrblvl > 0:
+        print('-> get_first_double_solution, size :', size)
+    soldata = create_string_buffer(4*size)
+    retval = phc(533, bbb, soldata, ccc, vrb)
+    if vrblvl > 0:
+        print("-> get_next_double_solution, return value :", retval)
+    result = int4a2str(soldata, False)
+    return result
+
+def move_double_solution_cursor(idx, vrblvl=0):
+    """
+    Moves the cursor to the next solution, following the index.
+    """
+    if vrblvl > 0:
+        print("-> move_double_solution_cursor, vrblvl =", vrblvl)
+    phcpack = getPHCmod()
+    phc = phcpack._ada_use_c2phc
+    aaa = pointer(c_int(idx)) # at the given index
+    bbb = pointer(c_int(0))
+    ccc = pointer(c_double(0.0))
+    vrb = c_int(vrblvl)
+    retval = phc(454, aaa, bbb, ccc, vrb)
+    if vrblvl > 0:
+        print("-> move_double_solution_cursor, return value :", retval)
+    return aaa[0]
+
 def show_solutions():
     """
     Solves a simple system and tests the operations
@@ -38,6 +80,16 @@ def show_solutions():
     write_double_solutions(lvl)
     nbrsols = number_double_solutions(lvl)
     print("number of solutions retrieved :", nbrsols)
+    sol = get_next_double_solution(1, lvl)
+    print("the first solution :")
+    print(sol)
+    idx = 1
+    for k in range(1, nbrsols):
+        idx = move_double_solution_cursor(idx, lvl)
+        print("the next index :", idx)
+        sol = get_next_double_solution(idx, lvl)
+        print("the solution at index", idx, ":")
+        print(sol)
 
 if __name__=="__main__":
     show_solutions()
