e1b005bcd243b77a864a21dbb178666b045bdd12
Jan Verschelde <janv@uic.edu>
2023-03-21 11:59:11 -0500

    fixed a test procedure and removed three obsolete tests

--- src/Ada/Components/Decomposition/ts_diahom.adb
+++ src/Ada/Components/Decomposition/ts_diahom.adb
@@ -1,6 +1,5 @@
 with text_io;                           use text_io;
 with Communications_with_User;          use Communications_with_User;
-with Drivers_to_Intrinsic_Solvers;      use Drivers_to_Intrinsic_Solvers;
 with Extrinsic_Diagonal_Solvers;        use Extrinsic_Diagonal_Solvers;
 
 procedure ts_diahom is
@@ -23,17 +22,12 @@ procedure ts_diahom is
     put_line("  Manipulate diagonal homotopies extrinsically : ");
     put_line("    2. Build a diagonal cascade from two embedded systems.");
     put_line("    3. Collapse a diagonal system eliminating the diagonal.");
-    put_line("  Intrinsic version provides incremental solver :");
-    put_line("    4. Intersect two hypersurfaces, given by two polynomials.");
-    put_line("    5. Compute witness points for given polynomial system.");
     put("Type 1, 2, 3, 4, or 5 to make your choice : ");
     Ask_Alternative(ans,"12345");
     case ans is
       when '1' => Randomize_System;
       when '2' => Build_Diagonal_Cascade;
       when '3' => Collapse_Diagonal_System;
-      when '4' => Intersection_of_Two_Hypersurfaces;
-      when '5' => Driver_to_Hypersurface_Solvers;
       when others => null;
     end case;
   end Main;
