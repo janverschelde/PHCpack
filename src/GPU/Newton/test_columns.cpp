1d71e629d56d2567ce0d63e3069cc3cc51990d26
Jan Verschelde <janv@uic.edu>
2023-05-07 13:52:51 -0500

    added value of #columns in test function

--- src/GPU/Newton/test_columns.cpp
+++ src/GPU/Newton/test_columns.cpp
@@ -20,7 +20,7 @@ int main ( void )
    int **nvr = new int*[dim];
    for(int i=0; i<dim; i++) nvr[i] = new int[dim];
 
-   make_cyclic_variables(dim,nvr);
+   make_cyclic_variables(dim,dim,nvr);
 
    int ***idx = new int**[dim]; // we have dim columns and
    for(int i=0; i<dim; i++)     // dim monomials in each column
@@ -28,9 +28,9 @@ int main ( void )
       idx[i] = new int*[dim];
       for(int j=0; j<dim; j++) idx[i][j] = new int[nvr[i][j]];
    }
-   make_cyclic_columns(dim,nvr,idx);
+   make_cyclic_columns(dim,dim,nvr,idx);
 
-   write_cyclic_columns(dim,nvr,idx);
+   write_cyclic_columns(dim,dim,nvr,idx);
 
    return 0;
 }
