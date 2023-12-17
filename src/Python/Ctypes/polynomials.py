acb6228bd6b468bc0da4c422d45ac858a7ea44ea
Jan Verschelde <janv@uic.edu>
2023-12-17 12:52:44 -0600

    extensions for nonsquare polynomial systems

--- src/Python/Ctypes/polynomials.py
+++ src/Python/Ctypes/polynomials.py
@@ -250,6 +250,51 @@ def get_quad_double_system(vrblvl=0):
         result.append(pol)
     return result
 
+def string_of_symbols(maxlen=100, vrblvl=0):
+    """
+    Returns the list of all symbols (as strings),
+    defined when storing polynomials.
+    The maxlen on entry equals the maximum number of characters
+    in the symbol string, that is: the sequence of all string
+    representations of the symbols, separated by one space.
+    """
+    if vrblvl > 0:
+        print('-> string_of_symbols, maxlen :', maxlen)
+    phc = get_phcfun()
+    slen = pointer(c_int(0))
+    ssym = create_string_buffer(maxlen*4)
+    ccc = pointer(c_double(0.0))
+    vrb = c_int(vrblvl)
+    retval = phc(295, slen, ssym, ccc, vrb)
+    if vrblvl > 0:
+        print('-> string_of_symbols, return value :', retval)
+        print('number of characters :', slen[0])
+    vars = int4a2str(ssym, (vrblvl > 0))
+    result = vars[0:slen[0]].split(' ')
+    return result
+
+def number_of_symbols(pols, vrblvl=0):
+    """
+    Returns the number of symbols that appear as variables
+    in the polynomials, givein in the list of strings pols.
+    Useful to determine whether a system is square or not.
+    """
+    if vrblvl > 0:
+        print('-> string_of_symbols, pols :')
+        for pol in pols:
+            print(pol)
+    inpols = ''.join(pols)
+    phc = get_phcfun()
+    slen = pointer(c_int(len(inpols)))
+    bpol = str2int4a(inpols)
+    ccc = pointer(c_double(0.0))
+    vrb = c_int(vrblvl)
+    retval = phc(439, slen, bpol, ccc, vrb)
+    if vrblvl > 0:
+        print('-> number_of_symbols, return value :', retval)
+        print('-> number_of_symbols, result :', slen[0])
+    return slen[0]
+
 def test_double_polynomial():
     """
     Tests the setting and getting of a polynomial,
@@ -264,6 +309,8 @@ def test_double_polynomial():
     set_double_polynomial(idx, dim, org, lvl)
     pol = get_double_polynomial(idx, lvl)
     print('the retrieved polynomial :', pol)
+    smb = string_of_symbols(100,lvl)
+    print('the list of symbols :', smb)
 
 def test_double_double_polynomial():
     """
@@ -301,8 +348,16 @@ def test_double_system():
     in double precision.
     """
     lvl = 10
-    polynomials = ["x^3 + 2*x*y - 1;", "x + y - 1;"]
-    set_double_system(2, polynomials, lvl)
+    polynomials = ["x^3 + 2*x*y - 1;", "x + y - 1;", "x - 1;"]
+    dim = number_of_symbols(polynomials, lvl)
+    print('number of symbols :', dim)
+    if dim == len(polynomials):
+        print('The system is square.')
+    else:
+        print('number of polynomials :', len(polynomials))
+        print('  number of variables :', dim)
+        print('The system is not square.')
+    set_double_system(dim, polynomials, lvl)
     pols = get_double_system(lvl)
     print('the retrieved polynomials :')
     for pol in pols:
@@ -314,8 +369,16 @@ def test_double_double_system():
     in double double precision.
     """
     lvl = 10
-    polynomials = ["x^3 + 2*x*y - 1;", "x + y - 1/3;"]
-    set_double_double_system(2, polynomials, lvl)
+    polynomials = ["x^3 + 2*x*y - 1;", "x + y - 1/3;", "x - 1;"]
+    dim = number_of_symbols(polynomials, lvl)
+    print('number of symbols :', dim)
+    if dim == len(polynomials):
+        print('The system is square.')
+    else:
+        print('number of polynomials :', len(polynomials))
+        print('  number of variables :', dim)
+        print('The system is not square.')
+    set_double_double_system(dim, polynomials, lvl)
     pols = get_double_double_system(lvl)
     print('the retrieved polynomials :')
     for pol in pols:
@@ -327,8 +390,16 @@ def test_quad_double_system():
     in quad double precision.
     """
     lvl = 10
-    polynomials = ["x^3 + 2*x*y - 1;", "x + y - 1/3;"]
-    set_quad_double_system(2, polynomials, lvl)
+    polynomials = ["x^3 + 2*x*y - 1;", "x + y - 1/3;", "y - 1;"]
+    dim = number_of_symbols(polynomials, lvl)
+    print('number of symbols :', dim)
+    if dim == len(polynomials):
+        print('The system is square.')
+    else:
+        print('number of polynomials :', len(polynomials))
+        print('  number of variables :', dim)
+        print('The system is not square.')
+    set_quad_double_system(dim, polynomials, lvl)
     pols = get_quad_double_system(lvl)
     print('the retrieved polynomials :')
     for pol in pols:
@@ -337,5 +408,5 @@ def test_quad_double_system():
 if __name__=="__main__":
     # test_double_polynomial()
     # test_double_system()
-    # test_double_double_system()
-    test_quad_double_system()
+    test_double_double_system()
+    # test_quad_double_system()
