9364be1d4a055ba1847b1392e4e94346189a2d04
Jan Verschelde <janv@uic.edu>
2023-12-21 16:31:43 -0600

    improved dimension module with better tests and core count function

--- src/Python/Ctypes/dimension.py
+++ src/Python/Ctypes/dimension.py
@@ -11,15 +11,17 @@ def set_double_dimension(dim, vrblvl=0):
     to the value of the first parameter dim.
     """
     if vrblvl > 0:
-        print("in set_double_dimension, dim = ", dim)
+        print('in set_double_dimension, dim :', dim)
     phc = get_phcfun()
     adim = pointer(c_int(dim))
     bbb = pointer(c_int(0))
     ccc = pointer(c_double(0.0))
     vrb = c_int(vrblvl)
+    if vrblvl > 0:
+        print('-> set_double_dimension calls phc', end='')
     retval = phc(23, adim, bbb, ccc, vrb)
     if vrblvl > 0:
-        print('the return value of set_double_dimension :', retval)
+        print(', return value :', retval)
     return retval
 
 def set_double_double_dimension(dim, vrblvl=0):
@@ -28,15 +30,17 @@ def set_double_double_dimension(dim, vrblvl=0):
     to the value of the first parameter dim.
     """
     if vrblvl > 0:
-        print("in set_double_double_dimension, dim = ", dim)
+        print('in set_double_double_dimension, dim :', dim)
     phc = get_phcfun()
     adim = pointer(c_int(dim))
     bbb = pointer(c_int(0))
     ccc = pointer(c_double(0.0))
     vrb = c_int(vrblvl)
+    if vrblvl > 0:
+        print('-> set_double_double_dimension calls phc', end='')
     retval = phc(333, adim, bbb, ccc, vrb)
     if vrblvl > 0:
-        print('the return value of set_double_double_dimension :', retval)
+        print(', return value :', retval)
     return retval
 
 def set_quad_double_dimension(dim, vrblvl=0):
@@ -45,15 +49,17 @@ def set_quad_double_dimension(dim, vrblvl=0):
     to the value of the first parameter dim.
     """
     if vrblvl > 0:
-        print("in set_quad_double_dimension, dim = ", dim)
+        print('in set_quad_double_dimension, dim :', dim)
     phc = get_phcfun()
     adim = pointer(c_int(dim))
     bbb = pointer(c_int(0))
     ccc = pointer(c_double(0.0))
     vrb = c_int(vrblvl)
+    if vrblvl > 0:
+        print('-> set_quad_double_dimension calls phc', end='')
     retval = phc(383, adim, bbb, ccc, vrb)
     if vrblvl > 0:
-        print('the return value of set_quad_double_dimension :', retval)
+        print(', return value :', retval)
     return retval
 
 def get_double_dimension(vrblvl=0):
@@ -67,9 +73,11 @@ def get_double_dimension(vrblvl=0):
     bbb = pointer(c_int(0))
     ccc = pointer(c_double(0.0))
     vrb = c_int(vrblvl)
+    if vrblvl > 0:
+        print('-> get_double_dimension calls phc', end='')
     retval = phc(22, adim, bbb, ccc, vrb)
     if vrblvl > 0:
-        print('the return value of get_double_dimension :', retval)
+        print(', return value :', retval)
         print('the retrieved dimension :', adim[0])
     return adim[0]
 
@@ -84,9 +92,11 @@ def get_double_double_dimension(vrblvl=0):
     bbb = pointer(c_int(0))
     ccc = pointer(c_double(0.0))
     vrb = c_int(vrblvl)
+    if vrblvl > 0:
+        print('-> get_double_double_dimension calls phc', end='')
     retval = phc(332, adim, bbb, ccc, vrb)
     if vrblvl > 0:
-        print('the return value of get_double_double_dimension :', retval)
+        print(', return value :', retval)
         print('the retrieved dimension :', adim[0])
     return adim[0]
 
@@ -101,28 +111,152 @@ def get_quad_double_dimension(vrblvl=0):
     bbb = pointer(c_int(0))
     ccc = pointer(c_double(0.0))
     vrb = c_int(vrblvl)
+    if vrblvl > 0:
+        print('-> get_quad_double_dimension calls phc', end='')
     retval = phc(382, adim, bbb, ccc, vrb)
     if vrblvl > 0:
-        print('the return value of get_quad_double_dimension :', retval)
+        print(', return value :', retval)
         print('the retrieved dimension :', adim[0])
     return adim[0]
 
-def test_dimension():
+def set_seed(seed, vrblvl=0):
     """
-    Test on setting and getting the dimension.
+    Sets the seed for the random number generators to seed.
     """
-    print("testing in double precision ...")
-    set_double_dimension(12345, 10)
-    dim = get_double_dimension(10)
-    print('the dimension :', dim)
-    print("testing in double double precision ...")
-    set_double_double_dimension(12345, 10)
-    dim = get_double_double_dimension(10)
-    print('the dimension :', dim)
-    print("testing in quad double precision ...")
-    set_quad_double_dimension(12345, 10)
-    dim = get_quad_double_dimension(10)
-    print('the dimension :', dim)
+    if vrblvl > 0:
+        print('in set_seed, seed :', seed)
+    phc = get_phcfun()
+    aseed = pointer(c_int(seed))
+    bbb = pointer(c_int(0))
+    ccc = pointer(c_double(0.0))
+    vrb = c_int(vrblvl)
+    if vrblvl > 0:
+        print('-> set_seed calls phc', end='')
+    retval = phc(998, aseed, bbb, ccc, vrb)
+    if vrblvl > 0:
+        print(', return value :', retval)
+    return retval
 
-if __name__=="__main__":
-    test_dimension()
+def get_seed(vrblvl=0):
+    """
+    Returns the seed used to generate random numbers.
+    """
+    if vrblvl > 0:
+        print('in get_seed ...')
+    phc = get_phcfun()
+    aseed = pointer(c_int(0))
+    bbb = pointer(c_int(0))
+    ccc = pointer(c_double(0.0))
+    vrb = c_int(vrblvl)
+    if vrblvl > 0:
+        print('-> get_seed calls phc', end='')
+    retval = phc(997, aseed, bbb, ccc, vrb)
+    if vrblvl > 0:
+        print(', return value :', retval)
+        print('the retrieved seed :', aseed[0])
+    return aseed[0]
+
+def get_core_count(vrblvl=0):
+    """
+    Returns the number of available cores.
+    """
+    if vrblvl > 0:
+        print('in get_core_count ...')
+    phc = get_phcfun()
+    acores = pointer(c_int(0))
+    bbb = pointer(c_int(0))
+    ccc = pointer(c_double(0.0))
+    vrb = c_int(vrblvl)
+    if vrblvl > 0:
+        print('-> get_core_count calls phc', end='')
+    retval = phc(994, acores, bbb, ccc, vrb)
+    if vrblvl > 0:
+        print(', return value :', retval)
+        print('the core count :', acores[0])
+    return acores[0]
+
+def test_dimension(vrblvl=0):
+    """
+    Tests setting and getting the dimension.
+    The verbose level is defined by vrblvl.
+    """
+    if vrblvl > 0:
+        print("testing in double precision ...")
+    set_double_dimension(12345, vrblvl)
+    dim = get_double_dimension(vrblvl)
+    if vrblvl > 0:
+        print('the dimension :', dim)
+    fail = int(dim != 12345)
+    if vrblvl > 0:
+        print("testing in double double precision ...")
+    set_double_double_dimension(12345, vrblvl)
+    dim = get_double_double_dimension(vrblvl)
+    if vrblvl > 0:
+        print('the dimension :', dim)
+    fail = fail + int(dim != 12345)
+    if vrblvl > 0:
+        print("testing in quad double precision ...")
+    set_quad_double_dimension(12345, vrblvl)
+    dim = get_quad_double_dimension(vrblvl)
+    if vrblvl > 0:
+        print('the dimension :', dim)
+    fail = fail + int(dim != 12345)
+    if vrblvl > 0:
+        if fail == 0:
+            print('=> All tests on set/get dimension passed.')
+        else:
+            print('Number of failed tests on set/get dimension :', fail)
+    return fail
+
+def test_seed(vrblvl=0):
+    """
+    Tests the setting and getting of the seed.
+    The verbose level is defined by vrblvl.
+    """
+    seed = get_seed(vrblvl)
+    if vrblvl > 0:
+        print('The current seed :', seed)
+        print('Setting the seed to 12345 ...')
+    set_seed(12345, vrblvl)
+    seed = get_seed(vrblvl)
+    if vrblvl > 0:
+        print('The current seed :', seed)
+    fail = int(seed != 12345)
+    if vrblvl > 0:
+        if fail == 0:
+            print('=> Tests on set/get seed passed.')
+        else:
+            print('Test on set/get seed failed!')
+    return fail
+
+def test_core_count(vrblvl=0):
+    """
+    Tests if the number of available cores is positive.
+    The verbose level is defined by vrblvl.
+    """
+    cores = get_core_count(vrblvl)
+    if vrblvl > 0:
+        print('The number of available cores :', cores)
+    fail = int(cores <= 0)
+    if vrblvl > 0:
+        if fail == 0:
+            print('=> Test on get core count passed.')
+        else:
+            print('Test on get core count failed!')
+    return fail
+
+def main():
+    """
+    Runs tests on set/get dimension and set/get seed.
+    """
+    lvl = 10
+    fail = test_dimension(lvl)
+    fail = fail + test_seed(lvl)
+    fail = fail + test_core_count(lvl)
+    if fail == 0:
+        print('=> All tests passed.')
+    else:
+        print('Number of failed tests :', fail)
+
+if __name__== '__main__':
+    main()
