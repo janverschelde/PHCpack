822efb272095ac52f1c9f28b50888635885f4214
Jan Verschelde <janv@uic.edu>
2023-12-22 16:48:46 -0600

    improved tests on mixed and stable mixed volumes

--- src/Python/Ctypes/volumes.py
+++ src/Python/Ctypes/volumes.py
@@ -13,69 +13,90 @@ def mixed_volume(demics=True, vrblvl=0):
     The verbose level is given by vrblvl.
     """
     if vrblvl > 0:
-        print('-> mixed_volume, demics flag :', demics)
+        print('in mixed_volume, demics flag :', demics)
     phc = get_phcfun()
     mixvol = pointer(c_int(0))
     bbb = pointer(c_int(0))
     ccc = pointer(c_double(0.0))
     vrb = c_int(vrblvl)
+    if vrblvl > 0:
+        print('-> mixed_volume calls phc', end='')
     if demics:
         retval = phc(843, mixvol, bbb, ccc, vrb)
     else:
         retval = phc(78, mixvol, bbb, ccc, vrb)
     if vrblvl > 0:
-        print('-> mixed_volume, return value :', retval)
+        print(', return value :', retval)
     return mixvol[0]
 
 def stable_mixed_volume(demics=True, vrblvl=0):
     """
-    Returns the stable mixed volume of the polynomial system
+    Returns the mixed and the stable mixed volume of the polynomial system
     in double precision.
     The demics flag indicates if dynamic enumeration as implemented by
     the software DEMiCs will be used, otherwise MixedVol is called.
     The verbose level is given by vrblvl.
     """
     if vrblvl > 0:
-        print('-> stable_mixed_volume, demics flag :', demics)
+        print('in stable_mixed_volume, demics flag :', demics)
     phc = get_phcfun()
     mixvol = pointer(c_int(0))
     stablemv = pointer(c_int(0))
     ccc = pointer(c_double(0.0))
     vrb = c_int(vrblvl)
+    if vrblvl > 0:
+        print('-> stable_mixed_volume calls phc', end='')
     if demics:
         retval = phc(844, mixvol, stablemv, ccc, vrb)
     else:
         retval = phc(79, mixvol, stablemv, ccc, vrb)
     if vrblvl > 0:
-        print('-> stable_mixed_volume, return value :', retval)
+        print(', return value :', retval)
     return (mixvol[0], stablemv[0])
 
-def show_mixed_volume():
+def test_mixed_volume(vrblvl=0):
     """
     Computes the mixed volume of a simple example.
+    The verbose level is given by vrblvl.
     """
-    lvl = 10
     polynomials = ["x^3 + 2*x*y - 1;", "x + y - 1;"]
-    set_double_system(2, polynomials, lvl)
-    mvl = mixed_volume(True, lvl)
-    print('the mixed volume by DEMiCs :', mvl)
-    mvl = mixed_volume(False, lvl)
-    print('the mixed volume by MixedVol :', mvl)
+    set_double_system(2, polynomials, vrblvl)
+    mvl = mixed_volume(True, vrblvl)
+    if vrblvl > 0:
+        print('the mixed volume by DEMiCs :', mvl)
+    mvl = mixed_volume(False, vrblvl)
+    if vrblvl > 0:
+        print('the mixed volume by MixedVol :', mvl)
+    return int(mvl != 3)
 
-def show_stable_mixed_volume():
+def test_stable_mixed_volume(vrblvl=0):
     """
     Computes the stable mixed volume of a simple example.
+    The verbose level is given by vrblvl.
     """
-    lvl = 10
     polynomials = ["x^3 + 2*x*y - x^2*y;", "x + y - x^3;"]
-    set_double_system(2, polynomials, lvl)
-    mvl, smv = stable_mixed_volume(True, lvl)
-    print('the mixed volume by DEMiCs :', mvl)
-    print('the stable mixed volume by DEMiCs :', smv)
-    mvl, smv = stable_mixed_volume(False, lvl)
-    print('the mixed volume by MixedVol :', mvl)
-    print('the stable mixed volume by MixedVol :', smv)
+    set_double_system(2, polynomials, vrblvl)
+    mvl, smv = stable_mixed_volume(True, vrblvl)
+    if vrblvl > 0:
+        print('the mixed volume by DEMiCs :', mvl)
+        print('the stable mixed volume by DEMiCs :', smv)
+    mvl, smv = stable_mixed_volume(False, vrblvl)
+    if vrblvl > 0:
+        print('the mixed volume by MixedVol :', mvl)
+        print('the stable mixed volume by MixedVol :', smv)
+    return int(mvl != 3) + int(smv != 5)
 
-if __name__=="__main__":
-    show_mixed_volume()
-    show_stable_mixed_volume()
+def main():
+    """
+    Runs tests on mixed volumes and stable mixed volumes.
+    """
+    lvl = 1
+    fail = test_mixed_volume(lvl)
+    fail = fail + test_stable_mixed_volume(lvl)
+    if fail == 0:
+        print('=> All tests passed.')
+    else:
+        print('Number of failed tests :', fail)
+
+if __name__=='__main__':
+    main()
