bc36472b878da15d920ac19b2ae01d13c293f706
Jan Verschelde <janv@uic.edu>
2024-01-01 18:58:44 -0600

    added m-homogeneous Bezout numbers and start systems

--- src/Python/PHCpy/phcpy/homotopies.py
+++ src/Python/PHCpy/phcpy/homotopies.py
@@ -2,11 +2,14 @@
 A homotopy is a family of polynomial systems which connects a given target
 system to a start system.  This module exports several start systems.
 """
-from ctypes import c_int32, c_double, pointer
+from ctypes import c_int32, c_double, pointer, create_string_buffer
+from version import int4a2str, str2int4a
 from phcpy.version import get_phcfun
 from phcpy.polynomials import set_double_system, string_of_symbols
+from phcpy.polynomials import get_double_system
 from phcpy.polynomials import degree_of_double_polynomial
-from phcpy.examples import noon3
+from phcpy.solutions import get_double_solutions
+from phcpy.examples import noon3, game4two
 from phcpy.solver import solve_checkin, solve
 
 def total_degree(pols, vrblvl=0):
@@ -61,15 +64,136 @@ def total_degree_start_system(pols, checkin=True, vrblvl=0):
         result.append(svars[ind]+'^'+str(degrees[ind])+' - 1;')
     return (result, solve(result))
 
-def main():
+def m_homogeneous_bezout_number(pols, vrblvl=0):
+    r"""
+    Given in *pols* a list of string representations of polynomials,
+    in as many variables as the elements in the list,
+    this function applies a heuristic to generate a partition of the
+    set of unknowns to exploit the product structure of the system.
+    On return are the m-homogeneous Bezout number and the partition
+    of the set of unknowns.  If the partition equals the entire
+    set of unknowns, then the 1-homogeneous Bezout number equals
+    the total degree of the system.
     """
-    Runs some tests.
+    if vrblvl > 0:
+        print('in m_homogeneous_bezout_number, pols :')
+        for pol in pols:
+            print(pol)
+    dim = len(pols)
+    set_double_system(dim, pols, vrblvl)
+    phc = get_phcfun()
+    deg = pointer(c_int32(0))
+    pbuffer = create_string_buffer(b"", 4*256)
+    ccc = pointer(c_double(0.0))
+    vrb = c_int32(vrblvl)
+    if vrblvl > 0:
+        print('-> m_homogeneous_bezout_number calls phc', end='')
+    retval = phc(530, deg, pbuffer, ccc, vrb)
+    if vrblvl > 0:
+        print(', return value :', retval)
+        print('an m-homogeneous Bezout number :', deg[0])
+    partition = int4a2str(pbuffer, (vrblvl > 0))
+    if vrblvl > 0:
+        print('the partition :', partition)
+    return (deg[0], partition)
+
+def m_partition_bezout_number(pols, partition, vrblvl=0):
+    r"""
+    There are as many m-homogeneous Bezout numbers as there are
+    partitions of the set of unknowns of a polynomial system.
+    Given in *pols* the string representations of a polynomial system
+    in as many variables as equations, and a string representation of
+    a *partition* of the set of unknowns, this function returns the
+    m-homogeneous Bezout number corresponding to the given partition.
     """
-    lvl = 10
+    if vrblvl > 0:
+        print('in m_partition_bezout_number, pols :')
+        for pol in pols:
+            print(pol)
+        print('the partition :', partition)
+    dim = len(pols)
+    set_double_system(dim, pols, vrblvl)
+    phc = get_phcfun()
+    deg = pointer(c_int32(len(partition)))
+    pbuffer = str2int4a(partition, (vrblvl > 0))
+    ccc = pointer(c_double(0.0))
+    vrb = c_int32(vrblvl)
+    if vrblvl > 0:
+        print('-> m_partition_bezout_number calls phc', end='')
+    retval = phc(531, deg, pbuffer, ccc, vrb)
+    if vrblvl > 0:
+        print(', return value :', retval)
+        print('the m-homogeneous Bezout number :', deg[0])
+    return deg[0]
+
+def m_homogeneous_start_system(pols, partition, checkin=True, vrblvl=0):
+    r"""
+    For an m-homogeneous Bezout number of a polynomial system defined by
+    a *partition* of the set of unknowns, one can define a linear-product
+    system that has exactly as many regular solutions as the Bezount number.
+    This linear-product system can then be used as start system in a
+    homotopy to compute all isolated solutions of any polynomial system
+    with the same m-homogeneous structure.
+    This function returns a linear-product start system with random
+    coefficients and its solutions for the given polynomials in pols
+    and the partition.
+    If *checkin*, then the list *pols* is tested to see if *pols* defines
+    a square polynomial system.  If the input system is not square,
+    then an error message is printed and None is returned.
+    """
+    if vrblvl > 0:
+        print('in m_homogeneous_start_system, pols :')
+        for pol in pols:
+            print(pol)
+        print('the partition :', partition)
+    if checkin:
+        errmsg = 'Start systems are defined only for square systems,'
+        if not solve_checkin(pols, errmsg):
+            return None
+    dim = len(pols)
+    set_double_system(dim, pols, vrblvl)
+    phc = get_phcfun()
+    deg = pointer(c_int32(len(partition)))
+    pbuffer = str2int4a(partition, (vrblvl > 0))
+    ccc = pointer(c_double(0.0))
+    vrb = c_int32(vrblvl)
+    if vrblvl > 0:
+        print('-> m_partition_start_system calls phc', end='')
+    retval = phc(532, deg, pbuffer, ccc, vrb)
+    if vrblvl > 0:
+        print(', return value :', retval)
+        print('the m-homogeneous Bezout number :', deg[0])
+    startsys = get_double_system(vrblvl)
+    if vrblvl > 0:
+        print('the start system :')
+        for pol in startsys:
+            print(pol)
+    aaa = pointer(c_int32(0))
+    bbb = pointer(c_int32(0))
+    ccc = pointer(c_double(0.0))
+    vrb = c_int32(vrblvl)
+    if vrblvl > 0:
+        print('-> m_partition_start_system calls phc', end='')
+        print(' to solve system')
+    retval = phc(114, aaa, bbb, ccc, vrb)
+    if vrblvl > 0:
+        print(', return value :', retval)
+    startsols = get_double_solutions(vrblvl)
+    if vrblvl > 0:
+        print('the start solutions :')
+        for (idx, sol) in enumerate(startsols):
+            print('solution', idx+1, ':')
+            print(sol)
+    return (startsys, startsols)
+
+def test_total_degree(vrblvl=0):
+    """
+    Tests the total degree and the start system.
+    """
     pols = noon3()
-    totdeg = total_degree(pols, lvl)
+    totdeg = total_degree(pols, vrblvl)
     print('the total degree of noon3 :', totdeg)
-    (start, startsols) = total_degree_start_system(pols, vrblvl=lvl)
+    (start, startsols) = total_degree_start_system(pols, vrblvl)
     print('the start system :')
     for pol in start:
         print(pol)
@@ -77,6 +201,33 @@ def main():
     for (idx, sol) in enumerate(startsols):
         print('Solution', idx+1, ':')
         print(sol)
+    return int(len(startsols) != 27)
+
+def test_m_homogeneous_degree(vrblvl=0):
+    """
+    Tests m-homogeneous Bezout number.
+    """
+    pols = game4two()
+    deg, partition = m_homogeneous_bezout_number(pols, vrblvl)
+    fail = int(deg != 9)
+    deg = m_partition_bezout_number(pols, partition, vrblvl=vrblvl)
+    fail = fail + int(deg != 9)
+    print('********************************************************')
+    q, qsols = m_homogeneous_start_system(pols, partition, vrblvl=vrblvl)
+    fail = fail + int(len(qsols) != 9)
+    return fail
+
+def main():
+    """
+    Runs some tests.
+    """
+    lvl = 10
+    fail = test_total_degree(lvl)
+    fail = fail + test_m_homogeneous_degree(lvl)
+    if fail == 0:
+        print('=> All tests passed.')
+    else:
+        print('Number of failed tests :', fail)
 
 if __name__=='__main__':
     main()
