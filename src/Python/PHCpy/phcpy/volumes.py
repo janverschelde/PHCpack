49ee9707f10667a430891317d57d946cce77c28c
Jan Verschelde <janv@uic.edu>
2024-01-25 18:22:34 -0600

    polyhedral homotopies with double doubles and quad doubles

--- src/Python/PHCpy/phcpy/volumes.py
+++ src/Python/PHCpy/phcpy/volumes.py
@@ -4,8 +4,16 @@ Exports functions to compute mixed volumes and stable mixed volumes.
 from ctypes import c_int32, c_double, pointer
 from phcpy.version import get_phcfun
 from phcpy.polynomials import set_double_system, get_double_system
+from phcpy.polynomials import set_double_double_system
+from phcpy.polynomials import get_double_double_system
+from phcpy.polynomials import set_quad_double_system
+from phcpy.polynomials import get_quad_double_system
 from phcpy.solutions import get_double_solutions, verify
 from phcpy.solutions import clear_double_solutions
+from phcpy.solutions import get_double_double_solutions
+from phcpy.solutions import clear_double_double_solutions
+from phcpy.solutions import get_quad_double_solutions
+from phcpy.solutions import clear_quad_double_solutions
 
 def mixed_volume(demics=True, vrblvl=0):
     """
@@ -198,6 +206,46 @@ def double_random_coefficient_system(vrblvl=0):
         print(', return value :', retval)
     return retval
 
+def double_double_random_coefficient_system(vrblvl=0):
+    """
+    Makes a random coefficient system using the type of mixture
+    and the supports used to compute the mixed volume.
+    Sets the system in double double precision.
+    """
+    if vrblvl > 0:
+        print('in double_double_random_coefficient_system ...')
+    phc = get_phcfun(vrblvl-1)
+    aaa = pointer(c_int32(0))
+    bbb = pointer(c_int32(0))
+    ccc = pointer(c_double(0.0))
+    vrb = c_int32(vrblvl-1)
+    if vrblvl > 0:
+        print('-> double_double_random_coefficient_system calls phc', end='')
+    retval = phc(460, aaa, bbb, ccc, vrb)
+    if vrblvl > 0:
+        print(', return value :', retval)
+    return retval
+
+def quad_double_random_coefficient_system(vrblvl=0):
+    """
+    Makes a random coefficient system using the type of mixture
+    and the supports used to compute the mixed volume.
+    Sets the system in quad double precision.
+    """
+    if vrblvl > 0:
+        print('in quad_double_random_coefficient_system ...')
+    phc = get_phcfun(vrblvl-1)
+    aaa = pointer(c_int32(0))
+    bbb = pointer(c_int32(0))
+    ccc = pointer(c_double(0.0))
+    vrb = c_int32(vrblvl-1)
+    if vrblvl > 0:
+        print('-> quad_double_random_coefficient_system calls phc', end='')
+    retval = phc(470, aaa, bbb, ccc, vrb)
+    if vrblvl > 0:
+        print(', return value :', retval)
+    return retval
+
 def set_double_coefficient_system(vrblvl=0):
     """
     Sets the constructed random coefficient system to the system
@@ -213,6 +261,44 @@ def set_double_coefficient_system(vrblvl=0):
     if vrblvl > 0:
         print('-> set_double_coefficient_system calls phc', end='')
     retval = phc(99, aaa, bbb, ccc, vrb)
+    if vrblvl > 0:
+        print(', return value :', retval)
+    return retval
+
+def set_double_double_coefficient_system(vrblvl=0):
+    """
+    Sets the constructed random coefficient system to the system
+    in double double precision.
+    """
+    if vrblvl > 0:
+        print('in set_double_double_coefficient_system ...')
+    phc = get_phcfun(vrblvl-1)
+    aaa = pointer(c_int32(0))
+    bbb = pointer(c_int32(0))
+    ccc = pointer(c_double(0.0))
+    vrb = c_int32(vrblvl-1)
+    if vrblvl > 0:
+        print('-> set_double_double_coefficient_system calls phc', end='')
+    retval = phc(463, aaa, bbb, ccc, vrb)
+    if vrblvl > 0:
+        print(', return value :', retval)
+    return retval
+
+def set_quad_double_coefficient_system(vrblvl=0):
+    """
+    Sets the constructed random coefficient system to the system
+    in quad double precision.
+    """
+    if vrblvl > 0:
+        print('in set_quad_double_coefficient_system ...')
+    phc = get_phcfun(vrblvl-1)
+    aaa = pointer(c_int32(0))
+    bbb = pointer(c_int32(0))
+    ccc = pointer(c_double(0.0))
+    vrb = c_int32(vrblvl-1)
+    if vrblvl > 0:
+        print('-> set_quad_double_coefficient_system calls phc', end='')
+    retval = phc(473, aaa, bbb, ccc, vrb)
     if vrblvl > 0:
         print(', return value :', retval)
     return retval
@@ -237,11 +323,52 @@ def set_double_polyhedral_homotopy(vrblvl=0):
         print(', return value :', retval)
     return retval
 
+def set_double_double_polyhedral_homotopy(vrblvl=0):
+    """
+    Sets the polyhedral homotopy in double double precision based
+    on the lifted supports used to compute the mixed volume.
+    Initializes the data to store the solutions.
+    """
+    if vrblvl > 0:
+        print('in set_double_double_polyhedral_homotopy ...')
+    phc = get_phcfun(vrblvl-1)
+    aaa = pointer(c_int32(0))
+    bbb = pointer(c_int32(0))
+    ccc = pointer(c_double(0.0))
+    vrb = c_int32(vrblvl-1)
+    if vrblvl > 0:
+        print('-> set_double_double_polyhedral_homotopy calls phc', end='')
+    retval = phc(465, aaa, bbb, ccc, vrb)
+    if vrblvl > 0:
+        print(', return value :', retval)
+    return retval
+
+def set_quad_double_polyhedral_homotopy(vrblvl=0):
+    """
+    Sets the polyhedral homotopy in quad double precision based
+    on the lifted supports used to compute the mixed volume.
+    Initializes the data to store the solutions.
+    """
+    if vrblvl > 0:
+        print('in set_quad_double_polyhedral_homotopy ...')
+    phc = get_phcfun(vrblvl-1)
+    aaa = pointer(c_int32(0))
+    bbb = pointer(c_int32(0))
+    ccc = pointer(c_double(0.0))
+    vrb = c_int32(vrblvl-1)
+    if vrblvl > 0:
+        print('-> set_quad_double_polyhedral_homotopy calls phc', end='')
+    retval = phc(475, aaa, bbb, ccc, vrb)
+    if vrblvl > 0:
+        print(', return value :', retval)
+    return retval
+
 def double_solve_start_system(idx, vrblvl=0):
     """
     Solves the start system corresponding to the cell with index idx,
-    using double precision arithmetic.  Return the number of solutions
-    found, which must equal the mixed volume of the cell.
+    using double precision arithmetic.
+    Returns the number of solutions found,
+    which must equal the mixed volume of the cell.
     """
     if vrblvl > 0:
         print('in double_solve_start_system, idx :', idx)
@@ -259,11 +386,57 @@ def double_solve_start_system(idx, vrblvl=0):
         print('number of solutions found :', result)
     return result
 
+def double_double_solve_start_system(idx, vrblvl=0):
+    """
+    Solves the start system corresponding to the cell with index idx,
+    using double double precision arithmetic.
+    Returns the number of solutions found,
+    which must equal the mixed volume of the cell.
+    """
+    if vrblvl > 0:
+        print('in double_double_solve_start_system, idx :', idx)
+    phc = get_phcfun(vrblvl-1)
+    aidx = pointer(c_int32(idx))
+    bnbr = pointer(c_int32(0))
+    ccc = pointer(c_double(0.0))
+    vrb = c_int32(vrblvl-1)
+    if vrblvl > 0:
+        print('-> double_double_solve_start_system calls phc', end='')
+    retval = phc(466, aidx, bnbr, ccc, vrb)
+    result = bnbr[0]
+    if vrblvl > 0:
+        print(', return value :', retval)
+        print('number of solutions found :', result)
+    return result
+
+def quad_double_solve_start_system(idx, vrblvl=0):
+    """
+    Solves the start system corresponding to the cell with index idx,
+    using quad double precision arithmetic.
+    Returns the number of solutions found,
+    which must equal the mixed volume of the cell.
+    """
+    if vrblvl > 0:
+        print('in quad_double_solve_start_system, idx :', idx)
+    phc = get_phcfun(vrblvl-1)
+    aidx = pointer(c_int32(idx))
+    bnbr = pointer(c_int32(0))
+    ccc = pointer(c_double(0.0))
+    vrb = c_int32(vrblvl-1)
+    if vrblvl > 0:
+        print('-> quad_double_solve_start_system calls phc', end='')
+    retval = phc(476, aidx, bnbr, ccc, vrb)
+    result = bnbr[0]
+    if vrblvl > 0:
+        print(', return value :', retval)
+        print('number of solutions found :', result)
+    return result
+
 def double_track_path(cellidx, pathidx, vrblvl=0):
     """
     Tracks path with index pathidx defined by cell with index cellidx.
     Prior to calling this function, the start system corresponding
-    to the cell must have been solved.
+    to the cell must have been solved, in double precision.
     """
     if vrblvl > 0:
         print('in double_track_path, cell :', cellidx, end='')
@@ -279,6 +452,54 @@ def double_track_path(cellidx, pathidx, vrblvl=0):
     if vrblvl > 0:
         print('-> double_track_path calls phc', end='')
     retval = phc(103, aidx, bnbrs, ccc, vrb)
+    if vrblvl > 0:
+        print(', return value :', retval)
+    return retval
+
+def double_double_track_path(cellidx, pathidx, vrblvl=0):
+    """
+    Tracks path with index pathidx defined by cell with index cellidx.
+    Prior to calling this function, the start system corresponding
+    to the cell must have been solved, in double double precision.
+    """
+    if vrblvl > 0:
+        print('in double_double_track_path, cell :', cellidx, end='')
+        print(', path :', pathidx)
+    phc = get_phcfun(vrblvl-1)
+    aidx = pointer(c_int32(cellidx))
+    nbrs = (c_int32 * 2)()
+    nbrs[0] = pathidx
+    nbrs[1] = vrblvl
+    bnbrs = pointer(nbrs)
+    ccc = pointer(c_double(0.0))
+    vrb = c_int32(vrblvl-1)
+    if vrblvl > 0:
+        print('-> double_double_track_path calls phc', end='')
+    retval = phc(467, aidx, bnbrs, ccc, vrb)
+    if vrblvl > 0:
+        print(', return value :', retval)
+    return retval
+
+def quad_double_track_path(cellidx, pathidx, vrblvl=0):
+    """
+    Tracks path with index pathidx defined by cell with index cellidx.
+    Prior to calling this function, the start system corresponding
+    to the cell must have been solved, in quad double precision.
+    """
+    if vrblvl > 0:
+        print('in quad_double_track_path, cell :', cellidx, end='')
+        print(', path :', pathidx)
+    phc = get_phcfun(vrblvl-1)
+    aidx = pointer(c_int32(cellidx))
+    nbrs = (c_int32 * 2)()
+    nbrs[0] = pathidx
+    nbrs[1] = vrblvl
+    bnbrs = pointer(nbrs)
+    ccc = pointer(c_double(0.0))
+    vrb = c_int32(vrblvl-1)
+    if vrblvl > 0:
+        print('-> quad_double_track_path calls phc', end='')
+    retval = phc(477, aidx, bnbrs, ccc, vrb)
     if vrblvl > 0:
         print(', return value :', retval)
     return retval
@@ -303,15 +524,54 @@ def set_double_start_solution(cellidx, solidx, vrblvl=0):
         print(', return value :', retval)
     return retval
 
+def set_double_double_start_solution(cellidx, solidx, vrblvl=0):
+    """
+    Sets the start solution of index solidx, with cell with index cellidx,
+    to the solutions in double double precision.
+    """ 
+    if vrblvl > 0:
+        print('in set_double_double_start_solution, cell :', cellidx, end='')
+        print(', solidx :', solidx)
+    phc = get_phcfun(vrblvl-1)
+    aidx = pointer(c_int32(cellidx))
+    bidx = pointer(c_int32(solidx))
+    ccc = pointer(c_double(0.0))
+    vrb = c_int32(vrblvl-1)
+    if vrblvl > 0:
+        print('-> set_double_double_start_solution calls phc', end='')
+    retval = phc(468, aidx, bidx, ccc, vrb)
+    if vrblvl > 0:
+        print(', return value :', retval)
+    return retval
+
+def set_quad_double_start_solution(cellidx, solidx, vrblvl=0):
+    """
+    Sets the start solution of index solidx, with cell with index cellidx,
+    to the solutions in quad double precision.
+    """ 
+    if vrblvl > 0:
+        print('in set_quad_double_start_solution, cell :', cellidx, end='')
+        print(', solidx :', solidx)
+    phc = get_phcfun(vrblvl-1)
+    aidx = pointer(c_int32(cellidx))
+    bidx = pointer(c_int32(solidx))
+    ccc = pointer(c_double(0.0))
+    vrb = c_int32(vrblvl-1)
+    if vrblvl > 0:
+        print('-> set_quad_double_start_solution calls phc', end='')
+    retval = phc(478, aidx, bidx, ccc, vrb)
+    if vrblvl > 0:
+        print(', return value :', retval)
+    return retval
+
 def double_polyhedral_homotopies(vrblvl=0):
-    r"""
+    """
     Runs the polyhedral homotopies and returns a random coefficient
-    system based on the contents of the cell container,
-    in standard double precision arithmetic.
-    For this to work, the function **mixed_volume()** must be called first.
+    system, in double precision arithmetic,
+    based on the mixed volume computation
     """
     if vrblvl > 0:
-        print('in double_random_coefficient_system ...')
+        print('in double_polyhedral_homotopies ...')
     double_random_coefficient_system(vrblvl)
     set_double_coefficient_system(vrblvl)
     rndcffsys = get_double_system(vrblvl-1)
@@ -334,6 +594,70 @@ def double_polyhedral_homotopies(vrblvl=0):
             double_track_path(cell, idxsol, vrblvl)
             set_double_start_solution(cell, idxsol, vrblvl)
     sols = get_double_solutions(vrblvl-1)
+    return (rndcffsys, sols)
+
+def double_double_polyhedral_homotopies(vrblvl=0):
+    """
+    Runs the polyhedral homotopies and returns a random coefficient
+    system, in double double precision arithmetic,
+    based on the mixed volume computation
+    """
+    if vrblvl > 0:
+        print('in double_double_polyhedral_homotopies ...')
+    double_double_random_coefficient_system(vrblvl)
+    set_double_double_coefficient_system(vrblvl)
+    rndcffsys = get_double_double_system(vrblvl-1)
+    set_double_double_polyhedral_homotopy(vrblvl)
+    is_stable = are_cells_stable(vrblvl)
+    if is_stable:
+        nbcells = number_of_original_cells(vrblvl)
+    else:
+        nbcells = number_of_cells(vrblvl)
+    if vrblvl > 0:
+         print('the number of cells :', nbcells)
+    clear_double_double_solutions(vrblvl-1)
+    for cell in range(1, nbcells+1):
+        nbsols = double_double_solve_start_system(cell, vrblvl)
+        if vrblvl > 0:
+            print(f'system {cell} has {nbsols} solutions')
+        for idxsol in range(1, nbsols+1):
+            if vrblvl > 0:
+                print(f'-> tracking path {idxsol} out of {nbsols}')
+            double_double_track_path(cell, idxsol, vrblvl)
+            set_double_double_start_solution(cell, idxsol, vrblvl)
+    sols = get_double_double_solutions(vrblvl-1)
+    return (rndcffsys, sols)
+
+def quad_double_polyhedral_homotopies(vrblvl=0):
+    """
+    Runs the polyhedral homotopies and returns a random coefficient
+    system, in quad double precision arithmetic,
+    based on the mixed volume computation
+    """
+    if vrblvl > 0:
+        print('in quad_double_polyhedral_homotopies ...')
+    quad_double_random_coefficient_system(vrblvl)
+    set_quad_double_coefficient_system(vrblvl)
+    rndcffsys = get_quad_double_system(vrblvl-1)
+    set_quad_double_polyhedral_homotopy(vrblvl)
+    is_stable = are_cells_stable(vrblvl)
+    if is_stable:
+        nbcells = number_of_original_cells(vrblvl)
+    else:
+        nbcells = number_of_cells(vrblvl)
+    if vrblvl > 0:
+         print('the number of cells :', nbcells)
+    clear_quad_double_solutions(vrblvl-1)
+    for cell in range(1, nbcells+1):
+        nbsols = quad_double_solve_start_system(cell, vrblvl)
+        if vrblvl > 0:
+            print(f'system {cell} has {nbsols} solutions')
+        for idxsol in range(1, nbsols+1):
+            if vrblvl > 0:
+                print(f'-> tracking path {idxsol} out of {nbsols}')
+            quad_double_track_path(cell, idxsol, vrblvl)
+            set_quad_double_start_solution(cell, idxsol, vrblvl)
+    sols = get_quad_double_solutions(vrblvl-1)
     return (rndcffsys, sols)
 
 def test_mixed_volume(vrblvl=0):
@@ -438,6 +762,58 @@ def test_double_polyhedral_homotopies(vrblvl=0):
     fail = fail + int(abs(err.real) > 1.0e-8) + int(abs(err.imag) > 1.0e-8)
     return fail
 
+def test_double_double_polyhedral_homotopies(vrblvl=0):
+    """
+    Test the polyhedral homotopies in double double precision
+    to compute and solve a random coefficient system.
+    The verbose level is given by vrblvl.
+    """
+    if vrblvl > 0:
+        print('in test_double_double_polyhedral_homotopies ...')
+    polynomials = ["x^3 + 2*x*y - 1;", "x + y - 1;"]
+    mv = mixed_volume(True, vrblvl)
+    rndcffsys, sols = double_double_polyhedral_homotopies(vrblvl)
+    fail = int(mv != len(sols))
+    if vrblvl > 0:
+        print('a random coefficient system :')
+        for pol in rndcffsys:
+            print(pol)
+        print('its solutions :')
+        for (idx, sol) in enumerate(sols):
+            print('Solution', idx+1, ':')
+            print(sol)
+    err = verify(rndcffsys, sols, vrblvl-1)
+    if vrblvl > 0:
+        print('the sum of errors :', err)
+    fail = fail + int(abs(err.real) > 1.0e-8) + int(abs(err.imag) > 1.0e-8)
+    return fail
+
+def test_quad_double_polyhedral_homotopies(vrblvl=0):
+    """
+    Test the polyhedral homotopies in quad double precision
+    to compute and solve a random coefficient system.
+    The verbose level is given by vrblvl.
+    """
+    if vrblvl > 0:
+        print('in test_quad_double_polyhedral_homotopies ...')
+    polynomials = ["x^3 + 2*x*y - 1;", "x + y - 1;"]
+    mv = mixed_volume(True, vrblvl)
+    rndcffsys, sols = quad_double_polyhedral_homotopies(vrblvl)
+    fail = int(mv != len(sols))
+    if vrblvl > 0:
+        print('a random coefficient system :')
+        for pol in rndcffsys:
+            print(pol)
+        print('its solutions :')
+        for (idx, sol) in enumerate(sols):
+            print('Solution', idx+1, ':')
+            print(sol)
+    err = verify(rndcffsys, sols, vrblvl-1)
+    if vrblvl > 0:
+        print('the sum of errors :', err)
+    fail = fail + int(abs(err.real) > 1.0e-8) + int(abs(err.imag) > 1.0e-8)
+    return fail
+
 def main():
     """
     Runs tests on mixed volumes and stable mixed volumes.
@@ -446,6 +822,8 @@ def main():
     fail = test_mixed_volume(lvl)
     fail = fail + test_stable_mixed_volume(lvl)
     fail = fail + test_double_polyhedral_homotopies(lvl)
+    fail = fail + test_double_double_polyhedral_homotopies(lvl)
+    fail = fail + test_quad_double_polyhedral_homotopies(lvl)
     if fail == 0:
         print('=> All tests passed.')
     else:
