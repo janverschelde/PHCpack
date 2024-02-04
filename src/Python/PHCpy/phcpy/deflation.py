fdb91bbef44d35309cad2965aa54b65a6b5fa4e3
Jan Verschelde <janv@uic.edu>
2024-02-04 10:55:22 -0600

    added multiplicity structure to the deflation module

--- src/Python/PHCpy/phcpy/deflation.py
+++ src/Python/PHCpy/phcpy/deflation.py
@@ -30,14 +30,14 @@ def double_newton_step(pols, sols, vrblvl=0):
         for (idx, sol) in enumerate(sols):
             print('Solution', idx+1, ':')
             print(sol)
-    nvr = number_of_symbols(pols)
+    nvr = number_of_symbols(pols, vrblvl-1)
     set_double_system(nvr, pols, vrblvl-1)
     set_double_solutions(nvr, sols, vrblvl-1)
     phc = get_phcfun(vrblvl-1)
     aaa = pointer(c_int32(0))
     bbb = pointer(c_int32(0))
     ccc = pointer(c_double(0.0))
-    vrb = c_int32(vrblvl)
+    vrb = c_int32(vrblvl-1)
     if vrblvl > 0:
         print('-> double_newton_step calls phc', end='')
     retval = phc(199, aaa, bbb, ccc, vrb)
@@ -60,14 +60,14 @@ def double_double_newton_step(pols, sols, vrblvl=0):
         for (idx, sol) in enumerate(sols):
             print('Solution', idx+1, ':')
             print(sol)
-    nvr = number_of_symbols(pols)
+    nvr = number_of_symbols(pols, vrblvl-1)
     set_double_double_system(nvr, pols, vrblvl-1)
     set_double_double_solutions(nvr, sols, vrblvl-1)
     phc = get_phcfun(vrblvl-1)
     aaa = pointer(c_int32(0))
     bbb = pointer(c_int32(0))
     ccc = pointer(c_double(0.0))
-    vrb = c_int32(vrblvl)
+    vrb = c_int32(vrblvl-1)
     if vrblvl > 0:
         print('-> double_double_newton_step calls phc', end='')
     retval = phc(198, aaa, bbb, ccc, vrb)
@@ -90,14 +90,14 @@ def quad_double_newton_step(pols, sols, vrblvl=0):
         for (idx, sol) in enumerate(sols):
             print('Solution', idx+1, ':')
             print(sol)
-    nvr = number_of_symbols(pols)
+    nvr = number_of_symbols(pols, vrblvl-1)
     set_quad_double_system(nvr, pols, vrblvl-1)
     set_quad_double_solutions(nvr, sols, vrblvl-1)
     phc = get_phcfun(vrblvl-1)
     aaa = pointer(c_int32(0))
     bbb = pointer(c_int32(0))
     ccc = pointer(c_double(0.0))
-    vrb = c_int32(vrblvl)
+    vrb = c_int32(vrblvl-1)
     if vrblvl > 0:
         print('-> quad_double_newton_step calls phc', end='')
     retval = phc(197, aaa, bbb, ccc, vrb)
@@ -134,7 +134,7 @@ def double_deflate(pols, sols, maxitr=3, maxdef=3, \
         for (idx, sol) in enumerate(sols):
             print('Solution', idx+1, ':')
             print(sol)
-    nvr = number_of_symbols(pols)
+    nvr = number_of_symbols(pols, vrblvl-1)
     set_double_system(nvr, pols, vrblvl-1)
     set_double_solutions(nvr, sols, vrblvl-1)
     phc = get_phcfun(vrblvl-1)
@@ -145,7 +145,7 @@ def double_deflate(pols, sols, maxitr=3, maxdef=3, \
     tols[1] = tolres
     tols[2] = tolrnk
     ctols = pointer(tols)
-    vrb = c_int32(vrblvl)
+    vrb = c_int32(vrblvl-1)
     if vrblvl > 0:
         print('-> double_deflate calls phc', end='')
     retval = phc(196, amaxitr, bmaxdef, ctols, vrb)
@@ -182,7 +182,7 @@ def double_double_deflate(pols, sols, maxitr=3, maxdef=3, \
         for (idx, sol) in enumerate(sols):
             print('Solution', idx+1, ':')
             print(sol)
-    nvr = number_of_symbols(pols)
+    nvr = number_of_symbols(pols, vrblvl-1)
     set_double_double_system(nvr, pols, vrblvl-1)
     set_double_double_solutions(nvr, sols, vrblvl-1)
     phc = get_phcfun(vrblvl-1)
@@ -193,7 +193,7 @@ def double_double_deflate(pols, sols, maxitr=3, maxdef=3, \
     tols[1] = tolres
     tols[2] = tolrnk
     ctols = pointer(tols)
-    vrb = c_int32(vrblvl)
+    vrb = c_int32(vrblvl-1)
     if vrblvl > 0:
         print('-> double_double_deflate calls phc', end='')
     retval = phc(249, amaxitr, bmaxdef, ctols, vrb)
@@ -230,7 +230,7 @@ def quad_double_deflate(pols, sols, maxitr=3, maxdef=3, \
         for (idx, sol) in enumerate(sols):
             print('Solution', idx+1, ':')
             print(sol)
-    nvr = number_of_symbols(pols)
+    nvr = number_of_symbols(pols, vrblvl-1)
     set_quad_double_system(nvr, pols, vrblvl-1)
     set_quad_double_solutions(nvr, sols, vrblvl-1)
     phc = get_phcfun(vrblvl-1)
@@ -241,7 +241,7 @@ def quad_double_deflate(pols, sols, maxitr=3, maxdef=3, \
     tols[1] = tolres
     tols[2] = tolrnk
     ctols = pointer(tols)
-    vrb = c_int32(vrblvl)
+    vrb = c_int32(vrblvl-1)
     if vrblvl > 0:
         print('-> quad_double_deflate calls phc', end='')
     retval = phc(250, amaxitr, bmaxdef, ctols, vrb)
@@ -249,6 +249,150 @@ def quad_double_deflate(pols, sols, maxitr=3, maxdef=3, \
         print(', return value :', retval)
     result = get_quad_double_solutions(vrblvl-1)
     return result
+
+def double_multiplicity(system, solution, \
+    order=5, tol=1.0e-8, vrblvl=0):
+    r"""
+    Computes the multiplicity structure in double precision
+    of an isolated solution (in the string *solution*)
+    of a polynomial system (in the list *system*).
+    The other parameters are
+
+    *order*: the maximum order of differentiation,
+
+    *tol*: tolerance on the numerical rank,
+
+    *vrblvl*: is the verbose level.
+
+    On return is the computed multiplicity.
+    """
+    if vrblvl > 0:
+        print('in double_multiplicity ...')
+        print('the polynomial system :')
+        for pol in system:
+            print(pol)
+        print('the solution :')
+        print(solution)
+    dim = number_of_symbols(system, vrblvl-1)
+    set_double_system(dim, system, vrblvl-1)
+    set_double_solutions(dim, [solution], vrblvl-1)
+    phc = get_phcfun(vrblvl-1)
+    amlt = pointer(c_int32(order))
+    size = order + 1
+    bhlb = (c_int32 * size)()
+    bhlb[0] = vrblvl
+    phlb = pointer(bhlb)
+    ctol = pointer(c_double(tol))
+    vrb = c_int32(vrblvl-1)
+    if vrblvl > 0:
+        print('-> double_multiplicity calls phc', end='')
+    retval = phc(732, amlt, phlb, ctol, vrb)
+    if vrblvl > 0:
+        print(', return value :', retval)
+    vals = phlb[0:size]
+    hlbfun = []
+    for idx in range(size):
+        hlbfun.append(int(vals[0][idx]))
+    if vrblvl > 0:
+        print('The values of the Hilbert function :', hlbfun)
+        print('The multiplicity :', amlt[0])
+    return (amlt[0], hlbfun)
+
+def double_double_multiplicity(system, solution, \
+    order=5, tol=1.0e-8, vrblvl=0):
+    r"""
+    Computes the multiplicity structure in double double precision
+    of an isolated solution (in the string *solution*)
+    of a polynomial system (in the list *system*).
+    The other parameters are
+
+    *order*: the maximum order of differentiation,
+
+    *tol*: tolerance on the numerical rank,
+
+    *vrblvl*: is the verbose level.
+
+    On return is the computed multiplicity.
+    """
+    if vrblvl > 0:
+        print('in double_double_multiplicity ...')
+        print('the polynomial system :')
+        for pol in system:
+            print(pol)
+        print('the solution :')
+        print(solution)
+    dim = number_of_symbols(system, vrblvl-1)
+    set_double_double_system(dim, system, vrblvl-1)
+    set_double_double_solutions(dim, [solution], vrblvl-1)
+    phc = get_phcfun(vrblvl-1)
+    amlt = pointer(c_int32(order))
+    size = order + 1
+    bhlb = (c_int32 * size)()
+    bhlb[0] = vrblvl
+    phlb = pointer(bhlb)
+    ctol = pointer(c_double(tol))
+    vrb = c_int32(vrblvl-1)
+    if vrblvl > 0:
+        print('-> double_double_multiplicity calls phc', end='')
+    retval = phc(733, amlt, phlb, ctol, vrb)
+    if vrblvl > 0:
+        print(', return value :', retval)
+    vals = phlb[0:size]
+    hlbfun = []
+    for idx in range(size):
+        hlbfun.append(int(vals[0][idx]))
+    if vrblvl > 0:
+        print('The values of the Hilbert function :', hlbfun)
+        print('The multiplicity :', amlt[0])
+    return (amlt[0], hlbfun)
+
+def quad_double_multiplicity(system, solution, \
+    order=5, tol=1.0e-8, vrblvl=0):
+    r"""
+    Computes the multiplicity structure in quad double precision
+    of an isolated solution (in the string *solution*)
+    of a polynomial system (in the list *system*).
+    The other parameters are
+
+    *order*: the maximum order of differentiation,
+
+    *tol*: tolerance on the numerical rank,
+
+    *vrblvl*: is the verbose level.
+
+    On return is the computed multiplicity.
+    """
+    if vrblvl > 0:
+        print('in quad_double_multiplicity ...')
+        print('the polynomial system :')
+        for pol in system:
+            print(pol)
+        print('the solution :')
+        print(solution)
+    dim = number_of_symbols(system, vrblvl-1)
+    set_quad_double_system(dim, system, vrblvl-1)
+    set_quad_double_solutions(dim, [solution], vrblvl-1)
+    phc = get_phcfun(vrblvl-1)
+    amlt = pointer(c_int32(order))
+    size = order + 1
+    bhlb = (c_int32 * size)()
+    bhlb[0] = vrblvl
+    phlb = pointer(bhlb)
+    ctol = pointer(c_double(tol))
+    vrb = c_int32(vrblvl-1)
+    if vrblvl > 0:
+        print('-> quad_double_multiplicity calls phc', end='')
+    retval = phc(734, amlt, phlb, ctol, vrb)
+    if vrblvl > 0:
+        print(', return value :', retval)
+    vals = phlb[0:size]
+    hlbfun = []
+    for idx in range(size):
+        hlbfun.append(int(vals[0][idx]))
+    if vrblvl > 0:
+        print('The values of the Hilbert function :', hlbfun)
+        print('The multiplicity :', amlt[0])
+    return (amlt[0], hlbfun)
 
 def test_double_newton_step(vrblvl=0):
     """
@@ -406,6 +550,50 @@ def test_quad_double_deflate(vrblvl=0):
         print(f'backward error : {res:.3e}')
     return int(res > 1.0e-12)
 
+def test_double_multiplicity(vrblvl=0):
+    """
+    Tests the multiplicity structure in double precision.
+    """
+    if vrblvl > 0:
+        print('in test_double_multiplicity ...')
+    pols = [ 'x**2+y-3;', 'x+0.125*y**2-1.5;']
+    sol = make_solution(['x', 'y'], [1, 2])
+    multiplicity, hilfun = double_multiplicity(pols, sol, vrblvl=vrblvl)
+    if vrblvl > 0:
+        print('the multiplicity :', multiplicity)
+        print('the Hibert function :', hilfun)
+    return int(multiplicity != 3)
+
+def test_double_double_multiplicity(vrblvl=0):
+    """
+    Tests the multiplicity structure in double double precision.
+    """
+    if vrblvl > 0:
+        print('in test_double_double_multiplicity ...')
+    pols = [ 'x**2+y-3;', 'x+0.125*y**2-1.5;']
+    sol = make_solution(['x', 'y'], [1, 2])
+    multiplicity, hilfun = double_double_multiplicity(pols, sol, \
+        vrblvl=vrblvl)
+    if vrblvl > 0:
+        print('the multiplicity :', multiplicity)
+        print('the Hibert function :', hilfun)
+    return int(multiplicity != 3)
+
+def test_quad_double_multiplicity(vrblvl=0):
+    """
+    Tests the multiplicity structure in quad double precision.
+    """
+    if vrblvl > 0:
+        print('in test_quad_double_multiplicity ...')
+    pols = [ 'x**2+y-3;', 'x+0.125*y**2-1.5;']
+    sol = make_solution(['x', 'y'], [1, 2])
+    multiplicity, hilfun = quad_double_multiplicity(pols, sol, \
+        vrblvl=vrblvl)
+    if vrblvl > 0:
+        print('the multiplicity :', multiplicity)
+        print('the Hibert function :', hilfun)
+    return int(multiplicity != 3)
+
 def main():
     """
     Runs some tests.
@@ -417,6 +605,9 @@ def main():
     fail = fail + test_double_deflate(lvl)
     fail = fail + test_double_double_deflate(lvl)
     fail = fail + test_quad_double_deflate(lvl)
+    fail = fail + test_double_multiplicity(lvl)
+    fail = fail + test_double_double_multiplicity(lvl)
+    fail = fail + test_quad_double_multiplicity(lvl)
     if fail == 0:
         print('=> All tests passed.')
     else:
