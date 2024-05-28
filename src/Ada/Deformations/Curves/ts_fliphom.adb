83fb577b23344f1e410c2b49ec83c3c8b2d37e5c
Jan Verschelde <janv@uic.edu>
2024-05-28 18:10:26 -0500

    added the Fabry ratio computation to the new homotopy

--- src/Ada/Deformations/Curves/ts_fliphom.adb
+++ src/Ada/Deformations/Curves/ts_fliphom.adb
@@ -9,10 +9,20 @@ with Standard_Random_Numbers;
 with Standard_Natural_Vectors;
 with Standard_Floating_Vectors;
 with Standard_Floating_Vectors_io;       use Standard_Floating_Vectors_io;
+with Standard_Integer_Vectors;
+with Standard_Complex_Vectors;
+with Standard_Complex_Vectors_io;        use Standard_Complex_Vectors_io;
+with Standard_Complex_Matrices;
+with Standard_Complex_Matrices_io;       use Standard_Complex_Matrices_io;
+with Standard_Complex_Linear_Solvers;    use Standard_Complex_Linear_Solvers;
 with Symbol_Table;
 with Standard_Complex_Polynomials;       use Standard_Complex_Polynomials;
 with Standard_Complex_Poly_Systems;      use Standard_Complex_Poly_Systems;
 with Standard_Complex_Poly_Systems_io;   use Standard_Complex_Poly_Systems_io;
+with Standard_Complex_Poly_SysFun;
+with Standard_Complex_Solutions;
+with Standard_Homotopy;
+with Standard_Fabry_on_Homotopy;
 with Double_Taylor_Developments;         use Double_Taylor_Developments;
 
 procedure ts_fliphom is
@@ -76,6 +86,54 @@ procedure ts_fliphom is
     return res;
   end Random_Square_System;
 
+  procedure Solve_Linear_System
+              ( p : in Poly_Sys; s : out Standard_Complex_Vectors.Vector ) is 
+
+  -- DESCRIPTION :
+  --   Extracts the linear part of the system in p
+  --   and return the solution of the linear system in s.
+
+    mat : Standard_Complex_Matrices.Matrix(1..2,1..2);
+    rhs : Standard_Complex_Vectors.Vector(1..2);
+    deg : Standard_Complex_Polynomials.Degrees
+        := new Standard_Natural_Vectors.Vector(1..3);
+    piv : Standard_Integer_Vectors.Vector(1..2);
+    info : integer32;
+    wrk : Standard_Complex_Vectors.Vector(1..3);
+    eva : Standard_Complex_Vectors.Vector(1..2);
+
+    use Standard_Complex_Numbers;
+
+  begin
+    deg(1) := 0;
+    deg(2) := 0;
+    deg(3) := 0;
+    rhs(1) := -Coeff(p(1),deg);
+    rhs(2) := -Coeff(p(2),deg);
+    put_line("The right hand side vector :");
+    put_line(rhs);
+    deg(2) := 1;
+    mat(1,1) := Coeff(p(1),deg);
+    mat(2,1) := Coeff(p(2),deg);
+    deg(2) := 0;
+    deg(3) := 1;
+    mat(1,2) := Coeff(p(1),deg);
+    mat(2,2) := Coeff(p(2),deg);
+    put_line("The coefficient matrix :"); put(mat);
+    lufac(mat,2,piv,info);
+    lusolve(mat,2,piv,rhs);
+    Clear(deg);
+    wrk(1) := Standard_Complex_Numbers.Create(0.0);
+    wrk(2) := rhs(1);
+    wrk(3) := rhs(2);
+    put_line("The solution :");
+    put_line(rhs);
+    eva := Standard_Complex_Poly_SysFun.Eval(p,wrk);
+    put_line("The value at the system :");
+    put_line(eva);
+    s := rhs;
+  end Solve_Linear_System;
+
   procedure Double_Test
               ( deg : in integer32; alpha, point : in double_float ) is
 
@@ -85,6 +143,8 @@ procedure ts_fliphom is
     cff : constant Standard_Floating_Vectors.Vector(0..deg)
         := Double_Taylor_Coefficients(deg,alpha,point);
     sys : constant Poly_Sys := Random_Square_System(cff);
+    sol : Standard_Complex_Solutions.Solution(2);
+    sols : Standard_Complex_Solutions.Solution_List;
 
   begin
     Symbol_Table.Init(3);
@@ -95,6 +155,12 @@ procedure ts_fliphom is
     put_line(cff);
     put_line("The homotopy :");
     put_line(sys);
+    Standard_Homotopy.Create(sys,1);
+    Solve_Linear_System(sys,sol.v);
+    sol.m := 1;
+    sol.t := Standard_Complex_Numbers.Create(point);
+    Standard_Complex_Solutions.Add(sols,sol);
+    Standard_Fabry_on_Homotopy.Run(0,2,1,deg,sols);
   end Double_Test;
 
   procedure Main is
