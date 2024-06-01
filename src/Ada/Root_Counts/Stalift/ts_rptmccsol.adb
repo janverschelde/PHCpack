9e2ad9fc50912f450d7c903575bb9bf72b27eca3
Jan Verschelde <janv@uic.edu>
2024-06-01 17:55:42 -0500

    added code for Taylor homotopies for mixed cells

--- src/Ada/Root_Counts/Stalift/ts_rptmccsol.adb
+++ src/Ada/Root_Counts/Stalift/ts_rptmccsol.adb
@@ -5,10 +5,15 @@ with Standard_Natural_Numbers_io;        use Standard_Natural_Numbers_io;
 with Standard_Integer_Numbers;           use Standard_Integer_Numbers;
 with Standard_Floating_Numbers;          use Standard_Floating_Numbers;
 with Standard_Floating_Numbers_io;       use Standard_Floating_Numbers_io;
+with Standard_Complex_Numbers;           use Standard_Complex_Numbers;
+with Standard_Complex_Numbers_io;        use Standard_Complex_Numbers_io;
 with Standard_Integer_Vectors;
 with Standard_Floating_Vectors;
+with Standard_Complex_Vectors;
+with Standard_Complex_VecVecs;
 with Lists_of_Floating_Vectors;          use Lists_of_Floating_Vectors;
 with Arrays_of_Floating_Vector_Lists;
+with Standard_Complex_Laur_Functions;
 with Standard_Complex_Laur_Systems;      use Standard_Complex_Laur_Systems;
 with Standard_Complex_Laur_Systems_io;   use Standard_Complex_Laur_Systems_io;
 with Floating_Lifting_Utilities;
@@ -19,6 +24,8 @@ with Random_Coefficient_Systems;
 with Standard_Complex_Solutions;         use Standard_Complex_Solutions;
 with Standard_Complex_Solutions_io;      use Standard_Complex_Solutions_io;
 with Standard_Simpomial_Solvers;
+with Double_Taylor_Homotopies;           use Double_Taylor_Homotopies;
+with Double_Taylor_Homotopies_io;        use Double_Taylor_Homotopies_io;
 
 procedure ts_rptmccsol is
 
@@ -52,6 +59,27 @@ procedure ts_rptmccsol is
     put(file,n,mix.all,mcc,mv);
   end Confirm_Input;
 
+  function Coefficient_Vectors
+             ( q : Laur_Sys ) return Standard_Complex_VecVecs.VecVec is
+
+  -- DESCRIPTION :
+  --   Returns the coefficient vectors of the polynomials in q.
+
+    res : Standard_Complex_VecVecs.VecVec(q'range);
+
+    use Standard_Complex_Laur_Functions;
+
+  begin
+    for i in q'range loop
+      declare
+        cff : constant Standard_Complex_Vectors.Vector := Coeff(q(i));
+      begin
+        res(i) := new Standard_Complex_Vectors.Vector'(cff);
+      end;
+    end loop;
+    return res;
+  end Coefficient_Vectors;
+
   function Offset ( mic : Mixed_Cell; idx : integer32 )
                   return double_float is
 
@@ -70,10 +98,72 @@ procedure ts_rptmccsol is
     return res;
   end Offset;
 
+  procedure Make_Homotopy
+              ( file : in file_type; deg : in integer32;
+                point : double_float;
+                cq : in Standard_Complex_VecVecs.VecVec;
+                mix : in Standard_Integer_Vectors.Link_to_Vector;
+                lif : in Arrays_of_Floating_Vector_Lists.Array_of_Lists;
+                mic : in Mixed_Cell; thm : out Taylor_Homotopy ) is
+
+  -- DESCRIPTION :
+  --   Defines the homotopy for the mixed cell and the lifting,
+  --   using the coefficients in the system q.
+
+    dim : constant integer32 := cq'last;
+    tmp : Lists_of_Floating_Vectors.List;
+    lpt : Standard_Floating_Vectors.Link_to_Vector;
+    pwr : double_float;
+    idx : integer32 := 0; -- index of polynomial
+    monidx : integer32; -- index of a monomial coefficient
+    cff : Standard_Complex_Numbers.Complex_Number;
+    tol_zero : constant double_float := 1.0e-12;
+    exp : Standard_Integer_Vectors.Vector(1..dim);
+
+  begin
+    for k in lif'range loop
+      for i in 1..mix(k) loop
+        tmp := lif(k);
+        idx := idx + 1;
+        declare
+          len : constant integer32 := integer32(Length_Of(lif(k)));
+          tmv : Taylor_Monomial_Vector(1..len);
+        begin
+          monidx := 0;
+          while not Is_Null(tmp) loop
+             monidx := monidx + 1;
+             cff := cq(idx)(monidx);
+             put(file,cff);
+             lpt := Head_Of(tmp);
+             pwr := lpt(lpt'last) - Offset(mic,k);
+             for i in cq'range loop
+               put(file," "); put(file,natural32(lpt(i)),1);
+               pwr := pwr + lpt(i)*mic.nor(i);
+               exp(i) := integer32(lpt(i));
+             end loop;
+             put(file," : "); put(file,pwr); new_line(file);
+             if pwr < tol_zero
+              then pwr := 0.0;
+             end if;
+             declare
+               tm : constant Link_to_Taylor_Monomial
+                  := Make(deg,pwr,point,cff,exp);
+             begin
+              -- put(file,tm);
+               tmv(monidx) := tm;
+             end;
+             tmp := Tail_Of(tmp);
+           end loop;
+           thm(idx) := new Taylor_Monomial_Vector'(tmv);
+         end;
+       end loop;
+    end loop;
+  end Make_Homotopy;
+
   procedure Track
               ( file : in file_type; q : in Laur_Sys;
+                cfq : in Standard_Complex_VecVecs.VecVec;
                 qsols,qsols_last : in out Solution_List;
-                n : in natural32;
                 mix : in Standard_Integer_Vectors.Link_to_Vector;
                 lif : in Arrays_of_Floating_Vector_Lists.Array_of_Lists;
                 mic : in Mixed_Cell ) is
@@ -83,8 +173,8 @@ procedure ts_rptmccsol is
  
   -- ON ENTRY :
   --   file     must be opened for input;
-  --   p        a polynomial system;
-  --   n        number of variables;
+  --   q        a random coefficient system;
+  --   cfq      coefficient vectors of q;
   --   mv       the mixed volume.
   --   mix      type of mixture;
   --   lif      lifted supports;
@@ -99,12 +189,12 @@ procedure ts_rptmccsol is
     sqsols : Solution_List;
     tol_zero : constant double_float := 1.0e-12;
     fail,zero_y : boolean;
-    tmp : Lists_of_Floating_Vectors.List;
-    lpt : Standard_Floating_Vectors.Link_to_Vector;
-    pwr : double_float;
+    deg : constant integer32 := 4;
+    point : constant double_float := 0.01;
+    thm : Taylor_Homotopy(q'range);
 
   begin
-    put(file,n,sq);
+    put(file,natural32(sq'last),sq);
     Standard_Simpomial_Solvers.Solve
       (sq,tol_zero,sqsols,fail,zero_y,false);
     if fail then
@@ -112,20 +202,10 @@ procedure ts_rptmccsol is
     else
       put(file,"Computed "); put(file,Length_Of(sqsols),1);
       put_line(file," start solutions.");
-      put(file,Length_Of(sqsols),n,sqsols);
-      for k in lif'range loop
-        tmp := lif(k);
-        while not Is_Null(tmp) loop
-          lpt := Head_Of(tmp);
-          pwr := lpt(lpt'last) - Offset(mic,k);
-          for i in 1..integer32(n) loop
-            put(file," "); put(file,natural32(lpt(i)),1);
-            pwr := pwr + lpt(i)*mic.nor(i);
-          end loop;
-          put(file," : "); put(file,pwr); new_line(file);
-          tmp := Tail_Of(tmp);
-        end loop;
-      end loop;
+      put(file,Length_Of(sqsols),natural32(sq'last),sqsols);
+      Make_Homotopy(file,deg,point,cfq,mix,lif,mic,thm);
+      put_line(file,"The Taylor monomial homotopy :");
+      put(file,thm);
       Concat(qsols,qsols_last,sqsols);
     end if;
     Clear(sq);
@@ -153,6 +233,7 @@ procedure ts_rptmccsol is
   --   q        a random coefficient system;
   --   qsols    solutions of q.
 
+    cfq : Standard_Complex_VecVecs.VecVec(1..integer32(n));
     lif : constant Arrays_of_Floating_Vector_Lists.Array_of_Lists(mix'range)
         := Floating_Lifting_Utilities.Lifted_Supports(mix'last,mcc);
     tmp : Mixed_Subdivision := mcc;
@@ -164,6 +245,7 @@ procedure ts_rptmccsol is
     put_line(file,"THE LIFTED SUPPORTS :");
     put(file,lif);
     q := new Laur_Sys'(Random_Coefficient_Systems.Create(n,mix.all,lif));
+    cfq := Coefficient_Vectors(q.all);
     put_line(file,"A RANDOM COEFFICIENT SYSTEM :");
     put(file,n,q.all);
     new_line(file);
@@ -175,9 +257,10 @@ procedure ts_rptmccsol is
       put(file,"PROCESSING CELL "); put(file,cnt,1);
       put_line(file," :");
       mic := Head_Of(tmp);
-      Track(file,q.all,qsols,qsols_last,n,mix,lif,mic);
+      Track(file,q.all,cfq,qsols,qsols_last,mix,lif,mic);
       tmp := Tail_of(tmp);
     end loop;
+    Standard_Complex_VecVecs.Clear(cfq);
   end Polyhedral_Continuation;
 
   procedure Main is
