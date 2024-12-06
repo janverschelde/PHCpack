2bc5b6c40d99d655e203f71d1c4e1fc414d42e17
Jan Verschelde <janv@uic.edu>
2024-12-06 11:24:13 -0600

    main procedure to test double integer arithmetic

---
+++ src/Ada/Math_Lib/Words/ts_dblint.adb
@@ -0,0 +1,10 @@
+with Test_Double_Integers;
+
+procedure ts_dblint is
+
+-- DESCRIPTION :
+--   Tests on multiword arithmetic with 64-bit integers.
+
+begin
+  Test_Double_Integers.Main;
+end ts_dblint;
