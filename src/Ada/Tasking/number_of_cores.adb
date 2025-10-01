abda20b159bc512c0b64b3579398fae7e1bb8b3b
Jan Verschelde <janv@uic.edu>
2025-10-01 17:15:40 -0500

    removed C files to count the available cores

--- src/Ada/Tasking/number_of_cores.adb
+++ src/Ada/Tasking/number_of_cores.adb
@@ -2,10 +2,6 @@ with System.Multiprocessors;
 
 function Number_of_Cores return integer32 is
 
- -- function core_count return integer32;
- -- pragma import(C, core_count, "corecount");
- -- res : constant integer32 := core_count;
-
   res : constant integer32
       := integer32(System.Multiprocessors.Number_of_CPUs);
 
