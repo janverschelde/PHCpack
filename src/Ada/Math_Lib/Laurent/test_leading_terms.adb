2f3b2b140a65a560192199bce2da33f8bfcb566a
Jan Verschelde <janv@uic.edu>
2025-10-25 18:14:36 -0500

    extended code to work with solution series

--- src/Ada/Math_Lib/Laurent/test_leading_terms.adb
+++ src/Ada/Math_Lib/Laurent/test_leading_terms.adb
@@ -1,19 +1,49 @@
 with Ada.Text_IO;                       use Ada.Text_IO;
 with Standard_Natural_Numbers;          use Standard_Natural_Numbers;
 with Standard_Integer_Numbers_io;       use Standard_Integer_Numbers_io;
-with Standard_Floating_Numbers;         use Standard_Floating_Numbers;
+with Standard_Integer_Vectors_io;       use Standard_Integer_Vectors_io;
 with Standard_Floating_Numbers_io;      use Standard_Floating_Numbers_io;
 with Standard_Complex_Numbers;          use Standard_Complex_Numbers;
 with Standard_Complex_Numbers_io;       use Standard_Complex_Numbers_io;
 with Standard_Floating_Vectors_io;      use Standard_Floating_Vectors_io;
-with Standard_Integer_Vectors;
 with Standard_Integer_VecVecs;
 with Standard_Floating_Matrices_io;     use Standard_Floating_Matrices_io;
+with Standard_Random_Numbers;
 with Standard_Random_Vectors;
 with Standard_Random_Matrices;
 with Double_Puiseux_Operations;
 
 package body Test_Leading_Terms is
+
+  procedure Sort ( B : in out Standard_Floating_Matrices.Matrix;
+                   cB : in out Standard_Complex_Matrices.Matrix;
+                   nbrcols : in integer32 ) is
+
+    minval : double_float;
+    minidx : integer32;
+    nbr : Complex_Number;
+
+  begin
+    for i in B'range(1) loop              -- sort i-th row
+      for j in B'first(2)..nbrcols-1 loop -- select minimum
+        minval := B(i,j);
+        minidx := j;
+        for k in j+1..nbrcols loop
+          if B(i,k) < minval then
+            minval := B(i,k);
+            minidx := k;
+          end if;
+        end loop;
+        if minidx /= j then -- swap if minimum not at position j
+          B(i,minidx) := B(i,j);
+          B(i,j) := minval;
+          nbr := cB(i,minidx);  
+          cB(i,minidx) := cB(i,j);
+          cB(i,j) := nbr;
+        end if;
+      end loop;
+    end loop;
+  end Sort;
 
   procedure Series_Product
               ( A : in Standard_Floating_Matrices.Matrix;
@@ -22,11 +52,6 @@ package body Test_Leading_Terms is
                 cx : in Standard_Complex_Vectors.Vector;
                 B : out Standard_Floating_Matrices.Matrix;
                 cB : out Standard_Complex_Matrices.Matrix ) is
-
-    minb : double_float;
-    minidx : integer32;
-    nbr : Complex_Number;
-
   begin
     for i in A'range(1) loop
       for j in A'range(2) loop
@@ -34,21 +59,7 @@ package body Test_Leading_Terms is
         cB(i,j) := cA(i,j)*cx(j);
       end loop;
     end loop;
-    for i in B'range(1) loop                -- sort i-th row
-      for j in B'first(2)..B'last(2)-1 loop -- select minimum
-        minb := B(i,j); minidx := j;
-        for k in j+1..B'last(2) loop
-          if B(i,k) < minb
-           then minb := B(i,k); minidx := k;
-          end if;
-        end loop;
-        if minidx /= j then -- swap if minimum not at position j
-          B(i,minidx) := B(i,j); B(i,j) := minb;
-          nbr := cB(i,minidx);  
-          cB(i,minidx) := cB(i,j); cB(i,j) := nbr;
-        end if;
-      end loop;
-    end loop;
+    Sort(B,cB,B'last(2));
   end Series_Product;
 
   procedure Series_Product
@@ -61,9 +72,6 @@ package body Test_Leading_Terms is
                 cB : out Standard_Complex_Matrices.Matrix;
                 nbrcols : out integer32 ) is
 
-    minb : double_float;
-    minidx : integer32;
-    nbr : Complex_Number;
     colidx : integer32 := 0;
 
   begin
@@ -77,24 +85,57 @@ package body Test_Leading_Terms is
       end if;
     end loop;
     nbrcols := colidx;
-    for i in B'range(1) loop             -- sort i-th row
-      for j in B'first(2)..colidx-1 loop -- select minimum
-        minb := B(i,j); minidx := j;
-        for k in j+1..colidx loop
-          if B(i,k) < minb
-           then minb := B(i,k); minidx := k;
-          end if;
-        end loop;
-        if minidx /= j then -- swap if minimum not at position j
-          B(i,minidx) := B(i,j); B(i,j) := minb;
-          nbr := cB(i,minidx);  
-          cB(i,minidx) := cB(i,j); cB(i,j) := nbr;
-        end if;
-      end loop;
-    end loop;
+    Sort(B,cB,nbrcols);
   end Series_Product;
 
-  procedure Random_Input
+  procedure Series_Product
+              ( A,X : in Standard_Floating_Matrices.Matrix;
+                cA,cX : in Standard_Complex_Matrices.Matrix;
+                B : out Standard_Floating_Matrices.Matrix;
+                cB : out Standard_Complex_Matrices.Matrix ) is
+
+    offset : integer32 := 0; -- column offset for B
+
+  begin
+    for k in cX'range(2) loop -- run over the columns of X
+      for i in A'range(1) loop
+        for j in A'range(2) loop
+          B(i,j+offset) := A(i,j) + X(j,k);
+          cB(i,j+offset) := cA(i,j)*cX(j,k);
+        end loop;
+      end loop;
+      offset := offset + A'last(1);
+    end loop;
+    Sort(B,cB,B'last(2));
+  end Series_Product;
+
+  procedure Series_Product
+              ( A,X : in Standard_Floating_Matrices.Matrix;
+                cA,cX : in Standard_Complex_Matrices.Matrix;
+                skip : in Standard_Integer_Vectors.Vector;
+                B : out Standard_Floating_Matrices.Matrix;
+                cB : out Standard_Complex_Matrices.Matrix;
+                nbrcols : out integer32 ) is
+
+    colidx : integer32 := 0;
+    xidx : integer32;
+
+  begin
+    for j in X'range(1) loop -- run over the rows of X
+      xidx := skip(j) + 1;
+      if xidx <= X'last(2) then -- otherwise skip element
+        colidx := colidx + 1;
+        for i in A'range(1) loop
+          B(i,colidx) := A(i,j) + X(j,xidx);
+          cB(i,colidx) := cA(i,j)*cX(j,xidx);
+        end loop;
+      end if;
+    end loop;
+    nbrcols := colidx;
+    Sort(B,cB,nbrcols);
+  end Series_Product;
+
+  procedure Random_Vector
               ( dim : in integer32;
                 A,B : out Standard_Floating_Matrices.Matrix;
                 x : out Standard_Floating_Vectors.Vector;
@@ -120,9 +161,134 @@ package body Test_Leading_Terms is
     put_line("-dimensional power vector :"); put(x,3); new_line;
     Series_Product(A,x,cA,cx,B,cB);
     put_line("Power matrix after product :"); put(B,3);
-  end Random_Input;
+  end Random_Vector;
 
-  procedure Test_Random_Input ( dim : in integer32 ) is
+  procedure Random_Series
+              ( dim,nbr : in integer32;
+                X : out Standard_Floating_Matrices.Matrix;
+                cX : out Standard_Complex_Matrices.Matrix ) is
+
+    nrows : constant natural32 := natural32(dim);
+    ncols : constant natural32 := natural32(nbr);
+    rnd : double_float;
+
+  begin
+    cX := Standard_Random_Matrices.Random_Matrix(nrows,ncols);
+    for i in 1..dim loop
+      rnd := Standard_Random_Numbers.Random;
+      X(i,1) := abs(rnd);
+      for j in 2..nbr loop
+        rnd := Standard_Random_Numbers.Random;
+        X(i,j) := X(i,j-1) + 0.1 + abs(rnd);
+      end loop;
+    end loop;
+  end Random_Series;
+
+  procedure Random_Series_System
+              ( dim,nbr : in integer32;
+                A,X,B : out Standard_Floating_Matrices.Matrix;
+                cA,cX,cB : out Standard_Complex_Matrices.Matrix ) is
+
+    n : constant natural32 := natural32(dim);
+
+  begin
+    A := Standard_Random_Matrices.Random_Matrix(n,n);
+    cA := Standard_Random_Matrices.Random_Matrix(n,n);
+    for i in A'range(1) loop
+      for j in A'range(2) loop
+        A(i,j) := abs(A(i,j));
+      end loop;
+    end loop;
+    put("A random "); put(dim,1);
+    put_line("-dimensional power matrix :"); put(A,3);
+    Random_Series(dim,nbr,X,cX);
+    put("A random "); put(dim,1);
+    put_line("-dimensional series exponents :"); put(X,3);
+    Series_Product(A,X,cA,cX,B,cB);
+    put_line("Right hand side exponents :"); put(B,3);
+  end Random_Series_System;
+
+  procedure Next_Coefficients
+              ( cy : in out Standard_Complex_Vectors.Vector;
+                cA,cB : in Standard_Complex_Matrices.Matrix;
+                idx1 : in Standard_Integer_Vectors.Vector;
+                prev,next : in Boolean_Vectors.Vector;
+                cx : in Standard_Complex_Vectors.Vector ) is
+
+    rowidx : integer32;
+    err : double_float;
+
+  begin
+    for i in next'range loop
+      if not prev(i) and next(i) then -- found new correct power
+        rowidx := 0;
+        for j in idx1'range loop -- look for row index
+          if idx1(j) = i
+           then rowidx := j; exit;
+          end if;
+        end loop;
+        cy(i) := cB(rowidx,cB'first(2))/cA(rowidx,i);
+        put("cy("); put(i,1); put(") : "); put(cy(i)); new_line;
+        put("cx("); put(i,1); put(") : "); put(cx(i));
+        err := AbsVal(cy(i) - cx(i));
+        put(", err :"); put(err,3); new_line;
+      end if;
+    end loop;
+  end Next_Coefficients;
+
+  procedure Next_Series_Coefficients
+              ( cy : in out Standard_Complex_Vectors.Vector;
+                cA,cB : in Standard_Complex_Matrices.Matrix;
+                idx1 : in Standard_Integer_Vectors.Vector;
+                prev,next : in Boolean_Vectors.Vector;
+                correct : in out Standard_Integer_Vectors.Vector;
+                cx : in Standard_Complex_Vectors.Vector ) is
+
+    rowidx : integer32;
+    err : double_float;
+
+  begin
+    for i in next'range loop
+      if not prev(i) and next(i) then -- found new correct power
+        rowidx := 0;
+        for j in idx1'range loop -- look for row index
+          if idx1(j) = i
+           then rowidx := j; exit;
+          end if;
+        end loop;
+        cy(i) := cB(rowidx,cB'first(2))/cA(rowidx,i);
+        put("cy("); put(i,1); put(") : "); put(cy(i)); new_line;
+        put("cx("); put(i,1); put(") : "); put(cx(i));
+        err := AbsVal(cy(i) - cx(i));
+        put(", err :"); put(err,3); new_line;
+        correct(i) := correct(i) + 1;
+      end if;
+    end loop;
+  end Next_Series_Coefficients;
+
+  procedure Coefficient_Check
+              ( tol : in double_float;
+                cx,cy : in Standard_Complex_Vectors.Vector ) is
+
+    err : double_float;
+    sumerr : double_float := 0.0;
+
+  begin
+    for i in cy'range loop
+      put("cy("); put(i,1); put(") : "); put(cy(i)); new_line;
+      put("cx("); put(i,1); put(") : "); put(cx(i));
+      err := AbsVal(cy(i) - cx(i));
+      put(", err :"); put(err,3); new_line;
+      sumerr := sumerr + err;
+    end loop;
+    put("Sum of errors :"); put(sumerr,3);
+    if sumerr < tol
+     then put_line(", okay.");
+     else put_line(", failure!");
+    end if;
+  end Coefficient_Check;
+
+  procedure Test_Random_Vector ( dim : in integer32 ) is
 
     rA,rB : Standard_Floating_Matrices.Matrix(1..dim,1..dim);
     cA,cB : Standard_Complex_Matrices.Matrix(1..dim,1..dim);
@@ -132,13 +298,12 @@ package body Test_Leading_Terms is
     idx1,idx2 : Standard_Integer_Vectors.Vector(1..dim);
     fail,done : boolean;
     prev,next : Boolean_Vectors.Vector(1..dim) := (1..dim => false);
-    rowidx,nbrcols : integer32;
-    err,sumerr : double_float;
+    nbrcols : integer32;
+    tol : constant double_float := 1.0E-12;
 
   begin
     put_line("-> generating random data ...");
-    Random_Input(dim,rA,rB,rx,cA,cB,cx);
-    put_line("-> computing the tropical Cramer vector ...");
+    Random_Vector(dim,rA,rB,rx,cA,cB,cx);
     for i in m'range loop
       m(i) := new Standard_Integer_Vectors.Vector'(1..dim => 0);
     end loop;
@@ -147,28 +312,15 @@ package body Test_Leading_Terms is
       for i in vb'range loop
         vb(i) := rB(i,rB'first(2));
       end loop;
+      put_line("-> computing the tropical Cramer vector ...");
       Double_Puiseux_Operations.Leading_Powers
-        (dim,1.0E-12,rA,vb,m,ry,idx1,idx2,fail,2);
+        (dim,tol,rA,vb,m,ry,idx1,idx2,fail,2);
       Double_Puiseux_Operations.Check_Correctness
-        (dim,1.0E-12,rx,ry,idx1,idx2,next,rz,1);
+        (dim,tol,rx,ry,idx1,idx2,next,rz,1);
+      Next_Coefficients(cy,cA,cB,idx1,prev,next,cx);
       done := true;
       for i in next'range loop
         done := done and next(i);
-      end loop;
-      for i in next'range loop
-        if not prev(i) and next(i) then -- found new correct power
-          rowidx := 0;
-          for j in idx1'range loop -- look for row index
-            if idx1(j) = i
-             then rowidx := j; exit;
-            end if;
-          end loop;
-          cy(i) := cB(rowidx,cB'first(2))/cA(rowidx,i);
-          put("cy("); put(i,1); put(") : "); put(cy(i)); new_line;
-          put("cx("); put(i,1); put(") : "); put(cx(i));
-          err := AbsVal(cy(i) - cx(i));
-          put(", err :"); put(err,3); new_line;
-        end if;
       end loop;
       if done then
         put("At step "); put(step,1);
@@ -177,31 +329,81 @@ package body Test_Leading_Terms is
       prev := next; -- for the next round
       Series_Product(rA,rx,cA,cx,next,rB,cB,nbrcols);
     end loop;
-    put_line("checking final coefficients :");
-    sumerr := 0.0;
-    for i in cy'range loop
-      put("cy("); put(i,1); put(") : "); put(cy(i)); new_line;
-      put("cx("); put(i,1); put(") : "); put(cx(i));
-      err := AbsVal(cy(i) - cx(i));
-      put(", err :"); put(err,3); new_line;
-      sumerr := sumerr + err;
+    put_line("-> checking final coefficients :");
+    Coefficient_Check(tol,cx,cy);
+  end Test_Random_Vector;
+
+  procedure Test_Random_Series ( dim,nbr : in integer32 ) is
+
+    prd : constant integer32 := dim*nbr;
+    eA : Standard_Floating_Matrices.Matrix(1..dim,1..dim);
+    eX : Standard_Floating_Matrices.Matrix(1..dim,1..nbr);
+    eB : Standard_Floating_Matrices.Matrix(1..dim,1..prd);
+    cA : Standard_Complex_Matrices.Matrix(1..dim,1..dim);
+    cX : Standard_Complex_Matrices.Matrix(1..dim,1..nbr);
+    cB : Standard_Complex_Matrices.Matrix(1..dim,1..prd);
+    vB,ry,rX,rz : Standard_Floating_Vectors.Vector(1..dim);
+    cy,vX : Standard_Complex_Vectors.Vector(1..dim);
+    m : Standard_Integer_VecVecs.VecVec(0..dim);
+    idx1,idx2,correct : Standard_Integer_Vectors.Vector(1..dim);
+    prev,next : Boolean_Vectors.Vector(1..dim) := (1..dim => false);
+    fail,done : boolean;
+    tol : constant double_float := 1.0E-12;
+    nbrcols : integer32;
+
+  begin
+    put_line("-> generating random data ...");
+    Random_Series_System(dim,nbr,eA,eX,eB,cA,cX,cB);
+    for i in m'range loop
+      m(i) := new Standard_Integer_Vectors.Vector'(1..dim => 0);
     end loop;
-    put("Sum of errors :"); put(sumerr,3);
-    if sumerr < 1.0E-12
-     then put_line(", okay.");
-     else put_line(", failure!");
-    end if;
-  end Test_Random_Input;
+    correct := (1..dim => 0); -- indices in X that are correct
+    for step in 1..prd loop
+      put("*** running step "); put(step,1); put_line(" ***");
+      for i in 1..dim loop
+        vB(i) := eB(i,eB'first(2));
+        if correct(i) < eX'last(2) then
+          rX(i) := eX(i,correct(i)+1);
+          vX(i) := cX(i,correct(i)+1);
+        else
+          rX(i) := 1.0E+99;
+        end if;
+      end loop;
+      put_line("-> computing the tropical Cramer vector ...");
+      Double_Puiseux_Operations.Leading_Powers
+        (dim,tol,eA,vB,m,rY,idx1,idx2,fail,2);
+      Double_Puiseux_Operations.Check_Correctness
+        (dim,tol,rX,rY,idx1,idx2,next,rz,1);
+      Next_Series_Coefficients(cy,cA,cB,idx1,prev,next,correct,vX);
+      done := true;
+      for i in next'range loop
+        done := done and (correct(i) = eX'last(2));
+      end loop;
+      if done then
+        put("At step "); put(step,1);
+        put_line(", all values are correct, done!");
+      end if;
+      put("correct indices :"); put(correct); new_line;
+      exit when done;
+      prev := (1..dim => false);
+      next := (1..dim => false);
+      Series_Product(eA,eX,cA,cX,correct,eB,cB,nbrcols);
+    end loop;
+  end Test_Random_Series;
 
   procedure Main is
 
-    dim : integer32 := 0;
+    dim,nbr : integer32 := 0;
 
   begin
     new_line;
     put_line("Testing leading terms computation ...");
     put("-> Give the dimension : "); get(dim);
-    Test_Random_Input(dim);
+    put("-> Give the number of terms : "); get(nbr);
+    if nbr = 1
+     then Test_Random_Vector(dim);
+     else Test_Random_Series(dim,nbr);
+    end if;
   end Main;
 
 end Test_Leading_Terms;
--- src/Ada/Math_Lib/Laurent/test_leading_terms.ads
+++ src/Ada/Math_Lib/Laurent/test_leading_terms.ads
@@ -1,5 +1,7 @@
 with Standard_Integer_Numbers;          use Standard_Integer_Numbers;
+with Standard_Floating_Numbers;         use Standard_Floating_Numbers;
 with Boolean_Vectors;
+with Standard_Integer_Vectors;
 with Standard_Floating_Vectors;
 with Standard_Complex_Vectors;
 with Standard_Floating_Matrices;
@@ -16,6 +18,15 @@ package Test_Leading_Terms is
 --   of the form C(A)*[t^A]*c(x)*[t^x] = C(B)*[t^B],
 --   where A and B are matrices of floating-point numbers,
 --   and C(A) and C(B) are complex matrices.
+
+  procedure Sort ( B : in out Standard_Floating_Matrices.Matrix;
+                   cB : in out Standard_Complex_Matrices.Matrix;
+                   nbrcols : in integer32 );
+
+  -- DESCRIPTION :
+  --   Sorts the rows in B in increasing order,
+  --   also swapping the corresponding coefficients in cB.
+  --   The number of valid columns in B is nbrcols.
 
   procedure Series_Product
               ( A : in Standard_Floating_Matrices.Matrix;
@@ -61,7 +72,36 @@ package Test_Leading_Terms is
   --   skip(k) is true are skipped.  The number of columns nbrcols in
   --   B and cB that count may be less than B'last(2).
 
-  procedure Random_Input
+  procedure Series_Product
+              ( A,X : in Standard_Floating_Matrices.Matrix;
+                cA,cX : in Standard_Complex_Matrices.Matrix;
+                B : out Standard_Floating_Matrices.Matrix;
+                cB : out Standard_Complex_Matrices.Matrix );
+
+  -- DESCRIPTION :
+  --   Makes the product of the series cA*[t^A] with cX*[t^X]
+  --   and stores the result in cB*[t^B].
+ 
+  -- REQUIRED :
+  --   If A is a dim-by-dim matrix and X is a dim-by-nbr matrix,
+  --   then B is a dim-by-(dim*nbr) matrix.
+  --   The coefficient matrices cA, cX, and cB have the same ranges
+  --   as the exponent matrices A, X, and B.
+
+  procedure Series_Product
+              ( A,X : in Standard_Floating_Matrices.Matrix;
+                cA,cX : in Standard_Complex_Matrices.Matrix;
+                skip : in Standard_Integer_Vectors.Vector;
+                B : out Standard_Floating_Matrices.Matrix;
+                cB : out Standard_Complex_Matrices.Matrix;
+                nbrcols : out integer32 );
+
+  -- DESCRIPTION :
+  --   Similar to Series_Product(A,X,cA,cX,B,cB),
+  --   skipping the columns according to the indices in skip.
+  --   The number of valid columns in B and cB is in nbrcols.
+
+  procedure Random_Vector
               ( dim : in integer32;
                 A,B : out Standard_Floating_Matrices.Matrix;
                 x : out Standard_Floating_Vectors.Vector;
@@ -70,15 +110,129 @@ package Test_Leading_Terms is
 
   -- DESCRIPTION :
   --   Generates a random matrix A of positive numbers in [0, 1],
-  --   and leading powers in x, also in [0, 1].
+  --   and leading powers in x, also in [0, 1], in one vector.
   --   The right hand side vectors in B are then computed.
   --   Random complex coefficients are generated in cA and cx,
   --   where cB is the result of a computation using cA and cx.
 
-  procedure Test_Random_Input ( dim : in integer32 );
+  procedure Random_Series
+              ( dim,nbr : in integer32;
+                X : out Standard_Floating_Matrices.Matrix;
+                cX : out Standard_Complex_Matrices.Matrix );
+
+  -- DESCRIPION :
+  --   Generates dim many random series of nbr terms.
+
+  -- ON ENTRY :
+  --   dim      dimension of the vector of series;
+  --   nbr      number of terms in each series;
+  --   X        space for dim-by-nbr real matrix;
+  --   cX       space for dim-by-nbr complex matrix.
+
+  -- ON RETURN :
+  --   X        matrix of exponents, range 1..dim and 1..nbr,
+  --            where the rows are increasing;
+  --   cX       a random dim-by-nbr matrix of coefficients.
+
+  procedure Random_Series_System
+              ( dim,nbr : in integer32;
+                A,X,B : out Standard_Floating_Matrices.Matrix;
+                cA,cX,cB : out Standard_Complex_Matrices.Matrix );
 
   -- DESCRIPTION :
-  --   Generates random input data of dimension dim.
+  --   Generates a linear system of series with real powers 
+  --   of dimension dim for a series with nbr terms.
+
+  -- ON ENTRY :
+  --   dim      dimension of the vector of series;
+  --   nbr      number of terms in each series;
+  --   A        space for dim-by-dim real matrix;
+  --   cA       space for dim-by-dim complex matrix;
+  --   X        space for dim-by-nbr real matrix;
+  --   cX       space for dim-by-nbr complex matrix;
+  --   B        space for dim-by-(nbr*dim) real matrix;
+  --   cB       space for dim-by-(nbr*dim) complex matrix.
+
+  -- ON RETURN :
+  --   A        real powers of the leading terms of series,
+  --            as the coefficients of a linear system;
+  --   cA       coefficients of the leading terms, corresponding to A;
+  --   X        matrix of exponents, range 1..dim and 1..nbr,
+  --            where the rows are increasing;
+  --   cX       a random dim-by-nbr matrix of coefficients.
+  --   B        sorted exponents of the right hand sides,
+  --            computed so that cX*[t^X] is a solution;
+  --   cB       corresponding coefficients of the right hand sides.
+
+  procedure Next_Coefficients
+              ( cy : in out Standard_Complex_Vectors.Vector;
+                cA,cB : in Standard_Complex_Matrices.Matrix;
+                idx1 : in Standard_Integer_Vectors.Vector;
+                prev,next : in Boolean_Vectors.Vector;
+                cx : in Standard_Complex_Vectors.Vector );
+
+  -- DESCRIPTION :
+  --   Computes the next coefficients of the leading power series.
+
+  -- ON ENTRY :
+  --   cy       current values of the leading coefficients;
+  --   cA       coefficients of the matrix of the linear system;
+  --   cB       coefficients of the right hand side of the system;
+  --   idx1     first set of indices of the tropical Cramer vector;
+  --   prev     previously correct indices;
+  --   next     current set of correct indices;
+  --   cx       correct values of the leading coefficients,
+  --            used for comparison.
+
+  -- ON RETURN :
+  --   cy       updated vector of coefficients.
+
+  procedure Next_Series_Coefficients
+              ( cy : in out Standard_Complex_Vectors.Vector;
+                cA,cB : in Standard_Complex_Matrices.Matrix;
+                idx1 : in Standard_Integer_Vectors.Vector;
+                prev,next : in Boolean_Vectors.Vector;
+                correct : in out Standard_Integer_Vectors.Vector;
+                cx : in Standard_Complex_Vectors.Vector );
+
+  -- DESCRIPTION :
+  --   Computes the next coefficients of the power series.
+
+  -- ON ENTRY :
+  --   cy       current values of the leading coefficients;
+  --   cA       coefficients of the matrix of the linear system;
+  --   cB       coefficients of the right hand side of the system;
+  --   idx1     first set of indices of the tropical Cramer vector;
+  --   prev     previously correct indices;
+  --   next     current set of correct indices;
+  --   correct  correct(i) indicates the number of correct values
+  --            in the series cX*[t^X];
+  --   cx       correct values of the leading coefficients,
+  --            used for comparison.
+
+  -- ON RETURN :
+  --   correct  updated indices of correct values;
+  --   cy       updated vector of coefficients.
+
+  procedure Coefficient_Check
+              ( tol : in double_float;
+                cx,cy : in Standard_Complex_Vectors.Vector );
+
+  -- DESCRIPTION :
+  --   Compares the computed coefficients cy to the original cx,
+  --   using tol to decide whether a number is small enough.
+
+  procedure Test_Random_Vector ( dim : in integer32 );
+
+  -- DESCRIPTION :
+  --   Generates random input data of dimension dim,
+  --   for one leading vector of a series.
+
+  procedure Test_Random_Series ( dim,nbr : in integer32 );
+
+  -- DESCRIPTION :
+  --   Generates a random linear system of dimension dim,
+  --   for a series with nbr terms.
 
   procedure Main;
 
