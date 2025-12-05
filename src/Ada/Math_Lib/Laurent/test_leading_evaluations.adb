86677d4845fe726837123d0b48689586c7f96e45
Jan Verschelde <janv@uic.edu>
2025-12-05 17:18:37 -0600

    added random homotopy with series solution for testing

--- src/Ada/Math_Lib/Laurent/test_leading_evaluations.adb
+++ src/Ada/Math_Lib/Laurent/test_leading_evaluations.adb
@@ -1,4 +1,5 @@
 with Ada.Text_IO;                       use Ada.Text_IO;
+with Communications_with_User;          use Communications_with_User;
 with Standard_Integer_Numbers_io;       use Standard_Integer_Numbers_io;
 with Standard_Floating_Numbers_io;      use Standard_Floating_Numbers_io;
 with Standard_Complex_Numbers;          use Standard_Complex_Numbers;
@@ -10,13 +11,11 @@ with Standard_Random_Vectors;
 with Standard_Integer_Vectors_io;       use Standard_Integer_Vectors_io;
 with Standard_Floating_Vectors_io;      use Standard_Floating_Vectors_io;
 with Standard_Complex_Vectors_io;       use Standard_Complex_Vectors_io;
-with Standard_Floating_VecVecs;
-with Standard_Complex_VecVecs;
 with Double_Leading_Evaluations;
 
 package body Test_Leading_Evaluations is
 
-  function Random_Monomial
+  function Random_Monomial_Support
              ( dim,low,upp : integer32 )
              return Standard_Integer_Vectors.Vector is
 
@@ -27,9 +26,9 @@ package body Test_Leading_Evaluations is
       res(i) := Standard_Random_Numbers.Random(low,upp);
     end loop;
     return res;
-  end Random_Monomial;
+  end Random_Monomial_Support;
 
-  function Random_Polynomial
+  function Random_Polynomial_Support
              ( nbr,dim,low,upp : integer32 )
              return Standard_Integer_VecVecs.VecVec is
 
@@ -39,13 +38,36 @@ package body Test_Leading_Evaluations is
     for i in 1..nbr loop
       declare
         mon : constant Standard_Integer_Vectors.Vector(1..dim)
-            := Random_Monomial(dim,low,upp);
+            := Random_Monomial_Support(dim,low,upp);
       begin
         res(i) := new Standard_Integer_Vectors.Vector'(mon);
       end;
     end loop;
     return res;
-  end Random_Polynomial;
+  end Random_Polynomial_Support;
+
+  procedure Random_Laurent_System
+              ( nbp,dim,low,upp : in integer32;
+                nbm : in Standard_Integer_Vectors.Vector;
+                deg : out Standard_Integer_VecVecs.Array_of_VecVecs;
+                cff : out Standard_Complex_VecVecs.VecVec;
+                tpw : out Standard_Floating_VecVecs.VecVec ) is
+  begin
+    for i in 1..nbp loop
+      declare
+        dpi : constant Standard_Integer_VecVecs.VecVec(1..nbm(i))
+            := Random_Polynomial_Support(nbm(i),dim,low,upp);
+        cfi : constant Standard_Complex_Vectors.Vector(1..nbm(i))
+            := Standard_Random_Vectors.Random_Vector(1,nbm(i));
+        cti : constant Standard_Floating_Vectors.Vector(1..nbm(i))
+            := Random_Leading_Powers(nbm(i));
+      begin
+        deg(i) := new Standard_Integer_VecVecs.VecVec'(dpi);
+        cff(i) := new Standard_Complex_Vectors.Vector'(cfi);
+        tpw(i) := new Standard_Floating_Vectors.Vector'(cti);
+      end;
+    end loop;
+  end Random_Laurent_System;
 
   function Random_Leading_Powers
              ( dim : integer32 ) return Standard_Floating_Vectors.Vector is
@@ -60,6 +82,170 @@ package body Test_Leading_Evaluations is
     end loop;
     return res;
   end Random_Leading_Powers;
+
+  procedure Random_Power_Series
+              ( dim : in integer32;
+                nbt : in Standard_Integer_Vectors.Vector;
+                cff : out Standard_Complex_VecVecs.VecVec;
+                pwr : out Standard_Floating_VecVecs.VecVec ) is
+
+    rnd : double_float;
+
+  begin
+    for i in 1..dim loop
+      declare
+        cfi : constant Standard_Complex_Vectors.Vector(0..nbt(i))
+            := Standard_Random_Vectors.Random_Vector(0,nbt(i));
+        pwi : Standard_Floating_Vectors.Vector(1..nbt(i));
+      begin
+        cff(i) := new Standard_Complex_Vectors.Vector'(cfi);
+        rnd := Standard_Random_Numbers.Random;
+        pwi(1) := abs(rnd);
+        for j in 2..nbt(i) loop
+          rnd := Standard_Random_Numbers.Random;
+          pwi(j) := pwi(j-1) + 0.01 + abs(rnd);
+        end loop;
+        pwr(i) := new Standard_Floating_Vectors.Vector'(pwi);
+      end;
+    end loop;
+  end Random_Power_Series;
+
+  procedure Random_Homotopy
+              ( pdg : in Standard_Integer_VecVecs.Array_of_VecVecs;
+                pcf : in Standard_Complex_VecVecs.VecVec;
+                ptp : in Standard_Floating_VecVecs.VecVec;
+                scf : in Standard_Complex_VecVecs.VecVec;
+                spw : in Standard_Floating_VecVecs.VecVec;
+                hdg : out Standard_Integer_VecVecs.Array_of_VecVecs;
+                hcf : out Standard_Complex_VecVecs.VecVec;
+                htp : out Standard_Floating_VecVecs.VecVec ) is
+
+    dim : constant integer32 := pdg'last;
+    nbt,nbm,size,idx : integer32;
+
+  begin
+    for i in pdg'range loop
+      nbt := spw(i)'last;  -- number of powers in i-th series
+      nbm := pdg(i)'last;  -- number of monomials in i-th polynomial
+      size := (nbt+2)*nbm; -- size of i-th homotopy polynomial
+      declare
+        hdi : Standard_Integer_VecVecs.VecVec(1..size);
+        hci : Standard_Complex_Vectors.Vector(1..size);
+        hti : Standard_Floating_Vectors.Vector(1..size);
+      begin
+        idx := 0;
+        for j in pdg(i)'range loop -- consider j-th monomial
+          declare
+            deg : Standard_Integer_Vectors.Vector(1..dim) := pdg(i)(j).all;
+          begin
+            deg(i) := deg(i) + 1; -- multiply by x(i)
+            idx := idx + 1;
+            hdi(idx) := new Standard_Integer_Vectors.Vector'(deg);
+            hci(idx) := pcf(i)(j);
+            hti(idx) := ptp(i)(j);
+            deg(i) := deg(i) - 1; -- multiply by series
+            idx := idx + 1;
+            hdi(idx) := new Standard_Integer_Vectors.Vector'(deg);
+            hci(idx) := -scf(i)(0)*pcf(i)(j); -- constant of series
+            hti(idx) := ptp(i)(j);
+            for k in 1..nbt loop -- run over all terms of series
+              idx := idx + 1;
+              hdi(idx) := new Standard_Integer_Vectors.Vector'(deg);
+              hci(idx) := -scf(i)(k)*pcf(i)(j); -- k-th term of series
+              hti(idx) := spw(i)(k) + ptp(i)(j);
+            end loop;
+          end;
+        end loop;
+        hdg(i) := new Standard_Integer_VecVecs.VecVec'(hdi);
+        hcf(i) := new Standard_Complex_Vectors.Vector'(hci);
+        htp(i) := new Standard_Floating_Vectors.Vector'(hti);
+      end;
+    end loop;
+  end Random_Homotopy;
+
+  function Evaluate_Series
+             ( cff : Standard_Complex_VecVecs.VecVec;
+               pwr : Standard_Floating_VecVecs.VecVec; tpt : double_float )
+             return Standard_Complex_Vectors.Vector is
+
+    res : Standard_Complex_Vectors.Vector(cff'range);
+
+  begin
+    for i in res'range loop
+      res(i) := cff(i)(0);
+      for j in 1..cff(i)'last loop
+        res(i) := res(i) + cff(i)(j)*(tpt**pwr(i)(j));
+      end loop;
+    end loop;
+    return res;
+  end Evaluate_Series;
+
+  function Evaluate_Monomial
+             ( deg : Standard_Integer_Vectors.Vector;
+               cff : Complex_Number; tpw : double_float;
+               zpt : Standard_Complex_Vectors.Vector; tpt : double_float )
+             return Complex_Number is
+
+  -- DESCRIPTION :
+  --   Evaluates the monomial with exponents in deg, coefficient in cff,
+  --   and leading power of t in tpw at point zpt and parameter tpt.
+
+    pwr : constant double_float := tpt**tpw;
+    res : Complex_Number := pwr*cff;
+
+  begin
+    for i in zpt'range loop
+      if deg(i) > 0 then
+        for j in 1..deg(i) loop
+          res := res*zpt(i);
+        end loop;
+      elsif deg(i) < 0 then
+        for j in 1..(-deg(i)) loop
+          res := res/zpt(i);
+        end loop;
+      end if;
+    end loop;
+    return res;
+  end Evaluate_Monomial;
+
+  function Evaluate_Homotopy
+             ( deg : Standard_Integer_VecVecs.Array_of_VecVecs;
+               cff : Standard_Complex_VecVecs.VecVec;
+               tpw : Standard_Floating_VecVecs.VecVec;
+               zpt : Standard_Complex_Vectors.Vector; tpt : double_float )
+             return Standard_Complex_Vectors.Vector is
+
+    res : Standard_Complex_Vectors.Vector(cff'range);
+
+  begin
+    for i in res'range loop
+      res(i) := create(0.0);
+      for j in cff'range loop
+        res(i) := res(i)
+          + Evaluate_Monomial(deg(i)(j).all,cff(i)(j),tpw(i)(j),zpt,tpt);
+      end loop;
+    end loop;
+    return res;
+  end Evaluate_Homotopy;
+
+  procedure Test_Random_Homotopy
+              ( hdg : in Standard_Integer_VecVecs.Array_of_VecVecs;
+                hcf : in Standard_Complex_VecVecs.VecVec;
+                htp : in Standard_Floating_VecVecs.VecVec;
+                scf : in Standard_Complex_VecVecs.VecVec;
+                spw : in Standard_Floating_VecVecs.VecVec ) is
+
+    tpt : constant double_float := abs(Standard_Random_Numbers.Random);
+    zpt : constant Standard_Complex_Vectors.Vector(scf'range)
+        := Evaluate_Series(scf,spw,tpt);
+    hpt : constant Standard_Complex_Vectors.Vector(hcf'range)
+        := Evaluate_Homotopy(hdg,hcf,htp,zpt,tpt);
+
+  begin
+    put("random t : "); put(tpt); new_line;
+    put_line("evaluated series :"); put_line(zpt);
+    put_line("evaluated homotopy :"); put_line(hpt);
+  end Test_Random_Homotopy;
 
   procedure Test_Monomial_Derivative
               ( deg : in Standard_Integer_Vectors.Vector;
@@ -103,7 +289,7 @@ package body Test_Leading_Evaluations is
   procedure Test_Monomial ( dim : in integer32 ) is
                
     deg : constant Standard_Integer_Vectors.Vector(1..dim)
-        := Random_Monomial(dim,-9,9);
+        := Random_Monomial_Support(dim,-9,9);
     pwr : constant Standard_Floating_Vectors.Vector(1..dim)
         := Random_Leading_Powers(dim);
     cff : constant Standard_Complex_Vectors.Vector(1..dim)
@@ -169,7 +355,7 @@ package body Test_Leading_Evaluations is
   procedure Test_Polynomial ( nbr,dim : in integer32 ) is
 
     deg : constant Standard_Integer_VecVecs.VecVec(1..nbr)
-        := Random_Polynomial(nbr,dim,-9,9);
+        := Random_Polynomial_Support(nbr,dim,-9,9);
     pwr : constant Standard_Floating_Vectors.Vector(1..dim)
         := Random_Leading_Powers(dim);
     val : Standard_Floating_Vectors.Vector(1..nbr);
@@ -220,20 +406,7 @@ package body Test_Leading_Evaluations is
         := Standard_Random_Vectors.Random_Vector(1,dim);
 
   begin
-    for i in 1..nbp loop
-      declare
-        dpi : constant Standard_Integer_VecVecs.VecVec(1..nbm(i))
-            := Random_Polynomial(nbm(i),dim,-9,9);
-        cfi : constant Standard_Complex_Vectors.Vector(1..nbm(i))
-            := Standard_Random_Vectors.Random_Vector(1,nbm(i));
-        cti : constant Standard_Floating_Vectors.Vector(1..nbm(i))
-            := Random_Leading_Powers(nbm(i));
-      begin
-        pdg(i) := new Standard_Integer_VecVecs.VecVec'(dpi);
-        pcf(i) := new Standard_Complex_Vectors.Vector'(cfi);
-        pct(i) := new Standard_Floating_Vectors.Vector'(cti);
-      end;
-    end loop;
+    Random_Laurent_System(nbp,dim,-9,9,nbm,pdg,pcf,pct);
     for i in 1..nbp loop
       put("-> coefficients and degrees of polynomial ");
       put(i,1); put_line(" :");
@@ -279,9 +452,55 @@ package body Test_Leading_Evaluations is
     end;   
   end Test_System;
 
+  procedure Test_Homotopy
+              ( dim : in integer32;
+                nbm,nbt : in Standard_Integer_Vectors.Vector ) is
+
+    pdg : Standard_Integer_VecVecs.Array_of_VecVecs(1..dim);
+    pcf : Standard_Complex_VecVecs.VecVec(1..dim);
+    pct : Standard_Floating_VecVecs.VecVec(1..dim);
+    cff : Standard_Complex_VecVecs.VecVec(1..dim);
+    pwr : Standard_Floating_VecVecs.VecVec(1..dim);
+    hdg : Standard_Integer_VecVecs.Array_of_VecVecs(1..dim);
+    hcf : Standard_Complex_VecVecs.VecVec(1..dim);
+    hct : Standard_Floating_VecVecs.VecVec(1..dim);
+
+  begin
+    Random_Laurent_System(dim,dim,0,9,nbm,pdg,pcf,pct);
+    for i in 1..dim loop
+      put("-> coefficients and degrees of polynomial ");
+      put(i,1); put_line(" :");
+      for j in 1..nbm(i) loop
+        put(pcf(i)(j)); 
+        put(" t^"); put(pct(i)(j));
+        put("  "); put(pdg(i)(j)); new_line;
+      end loop;
+    end loop;
+    Random_Power_Series(dim,nbt,cff,pwr);
+    for i in 1..dim loop
+      put("-> a random power series "); put(i,1); put_line(" : ");
+      put(cff(i)(0)); new_line;
+      for j in 1..nbt(i) loop
+        put(cff(i)(j)); put("  t^"); put(pwr(i)(j)); new_line;
+      end loop;
+    end loop;
+    Random_Homotopy(pdg,pcf,pct,cff,pwr,hdg,hcf,hct);
+    for i in 1..dim loop
+      put("-> coefficients and degrees of homotopy ");
+      put(i,1); put_line(" :");
+      for j in hcf(i)'range loop
+        put(hcf(i)(j)); 
+        put(" t^"); put(hct(i)(j));
+        put("  "); put(hdg(i)(j)); new_line;
+      end loop;
+    end loop;
+    Test_Random_Homotopy(hdg,hcf,hct,cff,pwr);
+  end Test_Homotopy;
+
   procedure Main is
 
     nbr,dim,npol : integer32 := 0;
+    ans : character;
 
   begin
     new_line;
@@ -297,12 +516,27 @@ package body Test_Leading_Evaluations is
       put_line("Reading the number of monomials for every polynomial ...");
       declare
         nbm : Standard_Integer_Vectors.Vector(1..npol) := (1..npol => 0);
+        nbt : Standard_Integer_Vectors.Vector(1..dim) := (1..dim => 0);
       begin
         for i in 1..npol loop
           put("  Give the number of monomials in polynomial "); put(i,1);
           put(" : "); get(nbm(i));
         end loop;
-        Test_System(npol,dim,nbm);
+        if npol /= dim then
+          Test_System(npol,dim,nbm);
+        else
+          put("Test homotopy ? (y/n) ");
+          Ask_Yes_or_No(ans);
+          if ans = 'n' then
+            Test_System(npol,dim,nbm);
+          else
+            for i in 1..dim loop
+              put("  Give the number of terms in series "); put(i,1);
+              put(" : "); get(nbt(i));
+            end loop;
+            Test_Homotopy(dim,nbm,nbt);
+          end if;
+        end if;
       end;
     end if;
   end Main;
--- src/Ada/Math_Lib/Laurent/test_leading_evaluations.ads
+++ src/Ada/Math_Lib/Laurent/test_leading_evaluations.ads
@@ -3,7 +3,9 @@ with Standard_Floating_Numbers;         use Standard_Floating_Numbers;
 with Standard_Integer_Vectors;
 with Standard_Integer_VecVecs;
 with Standard_Floating_Vectors;
+with Standard_Floating_VecVecs;
 with Standard_Complex_Vectors;
+with Standard_Complex_VecVecs;
 
 package Test_Leading_Evaluations is
 
@@ -11,21 +13,49 @@ package Test_Leading_Evaluations is
 --   Tests the evaluation of Laurent monomials at leading terms
 --   of series with real powers.
 
-  function Random_Monomial
+  function Random_Monomial_Support
              ( dim,low,upp : integer32 )
              return Standard_Integer_Vectors.Vector;
 
   -- DESCRIPTION :
   --   Returns a vector of exponents of dimension dim,
-  --   with values randomly generated between low and upp.
+  --   with values randomly generated between low and upp,
+  --   as the support of a random monomial.
 
-  function Random_Polynomial
+  function Random_Polynomial_Support
              ( nbr,dim,low,upp : integer32 )
              return Standard_Integer_VecVecs.VecVec;
 
   -- DESCRIPTION :
   --   Returns as many as nbr random exponents in a vector of range 1..nbr,
-  --   of dimension dim, with values between low and upp.
+  --   of dimension dim, with values between low and upp,
+  --   as the support of a random Laurent polynomial.
+
+  procedure Random_Laurent_System
+              ( nbp,dim,low,upp : in integer32;
+                nbm : in Standard_Integer_Vectors.Vector;
+                deg : out Standard_Integer_VecVecs.Array_of_VecVecs;
+                cff : out Standard_Complex_VecVecs.VecVec;
+                tpw : out Standard_Floating_VecVecs.VecVec );
+
+  -- DESCRIPTION :
+  --   Generates a random Laurent polynomial system,
+  --   where the coefficients are leading powers of series.
+
+  -- REQUIRED :
+  --   nbm'range = deg'range = cff'range = ctp'range = 1..nbp.
+
+  -- ON ENTRY :
+  --   nbp      number of polynomials in the system;
+  --   dim      number of variables in the polynomials;
+  --   low      lower bound for the exponents in the monomials;
+  --   upp      upper bound for the exponents in the monomials,
+  --   nbm      nbm(i) equals the number of monomials in the i-th polynomial.
+
+  -- ON RETURN :
+  --   deg      supports of the monomials in the system;
+  --   cff      coefficients of the monomials;
+  --   tpw      powers of t in the coefficients of the homotopy.      
 
   function Random_Leading_Powers
              ( dim : integer32 ) return Standard_Floating_Vectors.Vector;
@@ -33,6 +63,87 @@ package Test_Leading_Evaluations is
   -- DESCRIPTION :
   --   Returns a vector of range 1..dim with random powers of series
   --   with real positive powers. 
+
+  procedure Random_Power_Series
+              ( dim : in integer32;
+                nbt : in Standard_Integer_Vectors.Vector;
+                cff : out Standard_Complex_VecVecs.VecVec;
+                pwr : out Standard_Floating_VecVecs.VecVec );
+
+  -- DESCRIPTION :
+  --   Generates dim power series with random coefficients and real powers.
+
+  -- REQUIRED :
+  --   cff'range = nbt'range = pwr'range = 1..dim.
+
+  -- ON ENTRY :
+  --   dim      number of power series;
+  --   nbt      nbt(i) equals the number of terms in the i-th series;
+  --   cff      coefficients of the power series;
+  --   pwr      powers of the series.
+
+  procedure Random_Homotopy
+              ( pdg : in Standard_Integer_VecVecs.Array_of_VecVecs;
+                pcf : in Standard_Complex_VecVecs.VecVec;
+                ptp : in Standard_Floating_VecVecs.VecVec;
+                scf : in Standard_Complex_VecVecs.VecVec;
+                spw : in Standard_Floating_VecVecs.VecVec;
+                hdg : out Standard_Integer_VecVecs.Array_of_VecVecs;
+                hcf : out Standard_Complex_VecVecs.VecVec;
+                htp : out Standard_Floating_VecVecs.VecVec );
+
+  -- DESCRIPTION :
+  --   Given a Laurent polynomial system and a power series,
+  --   makes a homotopy that has this power series as a solution.
+
+  -- REQUIRED :
+  --   All vectors have 1..dim as the same range,
+  --   where dim is the number of polynomials and the number of variables.
+
+  -- ON ENTRY :
+  --   pdg      exponents of the monomials in the system;
+  --   pcf      coefficients of the system;
+  --   ptp      leading powers of the series coefficients of the system;
+  --   scf      coefficients of the power series solution;
+  --   spw      real powers of the series solution.
+
+  -- ON RETURN :
+  --   hdg      exponents of the monomials in the homotopy;
+  --   hcf      coefficients of the homotopy;
+  --   htp      leading powers of the series coefficients of the homotopy.
+
+  function Evaluate_Series
+             ( cff : Standard_Complex_VecVecs.VecVec;
+               pwr : Standard_Floating_VecVecs.VecVec; tpt : double_float )
+             return Standard_Complex_Vectors.Vector;
+
+  -- DESCRIPTION :
+  --   Given in cff and pwr are the coefficients and real powers of a series,
+  --   and in tpt a value for t.  Returns the value of the series.
+
+  function Evaluate_Homotopy
+             ( deg : Standard_Integer_VecVecs.Array_of_VecVecs;
+               cff : Standard_Complex_VecVecs.VecVec;
+               tpw : Standard_Floating_VecVecs.VecVec;
+               zpt : Standard_Complex_Vectors.Vector; tpt : double_float )
+             return Standard_Complex_Vectors.Vector;
+
+  -- DESCRIPTION :
+  --   Returns the evaluation of the homotopy defined by supports
+  --   in deg, coefficients in cff, and leading powers in tpw,
+  --   at the point zpt and parameter tpt.
+
+  procedure Test_Random_Homotopy
+              ( hdg : in Standard_Integer_VecVecs.Array_of_VecVecs;
+                hcf : in Standard_Complex_VecVecs.VecVec;
+                htp : in Standard_Floating_VecVecs.VecVec;
+                scf : in Standard_Complex_VecVecs.VecVec;
+                spw : in Standard_Floating_VecVecs.VecVec );
+
+  -- DESCRIPTION :
+  --   Evaluates the series with coefficients in scf and powers in spw
+  --   at a random value for t into the homotopy, with supports in hdg,
+  --   coefficients in hcf, and leading powers in htp.
 
   procedure Test_Monomial_Derivative
               ( deg : in Standard_Integer_Vectors.Vector;
@@ -64,10 +175,20 @@ package Test_Leading_Evaluations is
                 nbm : in Standard_Integer_Vectors.Vector );
 
   -- DESCRIPTION :
-  --   Tests polynomial evaluation and differentiation in dim many variables,
+  --   Tests polynomial evaluation in dim many variables,
   --   of a system of nbp polynomials, where the i-th polynomial
   --   has nbm(i) many terms, at the leading terms of a series with 
   --   real positive powers.
+
+  procedure Test_Homotopy
+              ( dim : in integer32;
+                nbm,nbt : in Standard_Integer_Vectors.Vector );
+
+  -- DESCRIPTION :
+  --   Tests generation of a Laurent homotopy of dim polynomials
+  --   in dim variables, with number of monomials in nbm,
+  --   with a generated power series solution, where the number
+  --   of terms in each series is defined by nbt.
 
   procedure Main;
 
