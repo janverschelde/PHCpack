76ee7b17d4f38f35e3ed57b71ba2d46d986c4905
Jan Verschelde <janv@uic.edu>
2025-12-09 15:51:57 -0600

    extended tests on Newton-Puiseux for real powers

--- src/Ada/Math_Lib/Laurent/test_newton_puiseux.adb
+++ src/Ada/Math_Lib/Laurent/test_newton_puiseux.adb
@@ -1,36 +1,177 @@
 with Ada.Text_IO;                       use Ada.Text_IO;
 with Standard_Integer_Numbers_io;       use Standard_Integer_Numbers_io;
 with Standard_Floating_Numbers_io;      use Standard_Floating_Numbers_io;
+with Standard_Complex_Numbers;          use Standard_Complex_Numbers;
 with Standard_Complex_Numbers_io;       use Standard_Complex_Numbers_io;
 with Standard_Integer_Vectors_io;       use Standard_Integer_Vectors_io;
-with Standard_Floating_Vectors;
-with Standard_Complex_Vectors;
+with Standard_Floating_Vectors_io;      use Standard_Floating_Vectors_io;
 with Double_Leading_Evaluations;
+with Double_Puiseux_Operations;
 with Random_Laurent_Homotopy;
 
 package body Test_Newton_Puiseux is
+
+  procedure Evaluate_and_Differentiate
+              ( hcf : in Standard_Complex_VecVecs.VecVec;
+                hct : in Standard_Floating_VecVecs.VecVec;
+                hdg : in Standard_Integer_VecVecs.Array_of_VecVecs;
+                zt0 : in Standard_Complex_Vectors.Vector;
+                ycf : out Standard_Complex_Vectors.Vector;
+                ydg : out Standard_Floating_Vectors.Vector;
+		cjm : out Standard_Complex_Matrices.Matrix;
+		ejm : out Standard_Floating_Matrices.Matrix;
+                vrblvl : in integer32 := 0 ) is
+
+    dim : constant integer32 := hcf'last;
+    tol : constant double_float := 1.0e-12;
+    dfc : double_float;
+
+    use Double_Leading_Evaluations;
+
+  begin
+    if vrblvl > 0 then
+      put_line("-> in Test_Newton_Puiseux.evaluate_and_differentiate ...");
+    end if;
+    for i in 1..dim loop -- run over all polynomials
+      if vrblvl > 0 then
+        put("at monomial 1");
+        put(" of polynomial "); put(i,1); put_line(" ...");
+      end if;
+      ydg(i) := hct(i)(1); -- initialize with first monomial
+      ycf(i) := hcf(i)(1)*Leading_Coefficient(hdg(i)(1).all,zt0);
+      for k in 1..dim loop
+        ejm(i,k) := hct(i)(1);
+        cjm(i,k) := hcf(i)(1)*Leading_Coefficient(hdg(i)(1).all,zt0,k);
+      end loop;
+      for j in 2..hcf(i)'last loop -- run over all monomials
+        if vrblvl > 0 then
+          put("at monomial "); put(j,1);
+          put(" of polynomial "); put(i,1); put_line(" ...");
+        end if;
+        dfc := abs(ydg(i) - hct(i)(j));
+        if dfc < tol then
+          ycf(i) := ycf(i) + hcf(i)(j)*Leading_Coefficient(hdg(i)(j).all,zt0);
+          for k in 1..dim loop
+            cjm(i,k) := cjm(i,k)
+              + hcf(i)(j)*Leading_Coefficient(hdg(i)(j).all,zt0,k);
+          end loop;
+        elsif hct(i)(j) < ydg(i) then
+          ydg(i) := hct(i)(j);
+          ycf(i) := hcf(i)(j)*Leading_Coefficient(hdg(i)(j).all,zt0);
+          for k in 1..dim loop
+            ejm(i,k) := hct(i)(j);
+            cjm(i,k) := hcf(i)(j)*Leading_Coefficient(hdg(i)(j).all,zt0,k);
+          end loop;
+        end if;
+      end loop;
+    end loop;
+  end Evaluate_and_Differentiate;
+
+  function Positive_Minimum
+             ( v : Standard_Floating_Vectors.Vector ) return double_float is
+
+    res : double_float;
+    tol : constant double_float := 1.0E-12;
+    idx : integer32;
+
+  begin
+    for i in v'range loop -- find first positive number
+      if v(i) > tol then
+        idx := i;
+        res := v(i);
+        exit;
+      end if;
+    end loop;
+    for i in idx+1..v'last loop
+      if v(i) > tol and then v(i) < res
+       then res := v(i);
+      end if;
+    end loop;
+    return res;
+  end Positive_Minimum;
 
   procedure Run_Newton_Step
               ( hcf : in Standard_Complex_VecVecs.VecVec;
                 hct : in Standard_Floating_VecVecs.VecVec;
                 hdg : in Standard_Integer_VecVecs.Array_of_VecVecs;
                 cff : in Standard_Complex_VecVecs.VecVec;
-                pwr : in Standard_Floating_VecVecs.VecVec ) is
+                pwr : in Standard_Floating_VecVecs.VecVec;
+                vrblvl : in integer32 := 0 ) is
 
-    ycf : Standard_Complex_VecVecs.VecVec(hcf'range);
-    ydg : Standard_Floating_VecVecs.VecVec(hcf'range);
+    dim : constant integer32 := hcf'last;
+    ycf : Standard_Complex_Vectors.Vector(hcf'range);
+    ydg : Standard_Floating_Vectors.Vector(hcf'range);
     lcf : Standard_Complex_Vectors.Vector(hcf'range);
     lpw : Standard_Floating_Vectors.Vector(hcf'range);
+    cA : Standard_Complex_Matrices.Matrix(hcf'range,hcf'range);
+    eA : Standard_Floating_Matrices.Matrix(hcf'range,hcf'range);
+    tol : constant double_float := 1.0e-12;
+    idx1,idx2 : Standard_Integer_Vectors.Vector(hcf'range);
+    work : Standard_Integer_VecVecs.VecVec(0..dim);
+    sol : Standard_Floating_Vectors.Vector(hcf'range);
+    fail : boolean;
+    posmin,err,sumerr : double_float;
 
   begin
+    if vrblvl > 0 then
+      put_line("-> in Test_Newton_Puiseux.run_newton_step ...");
+    end if;
     for i in hcf'range loop
-      ycf(i) := new Standard_Complex_Vectors.Vector(hcf(i)'range);
-      ydg(i) := new Standard_Floating_Vectors.Vector(hcf(i)'range);
       lcf(i) := cff(i)(cff(i)'first); -- get leading coefficients
-      lpw(i) := 0.0; -- pwr(i)(pwr(i)'first); -- evaluate at constant!
+      lpw(i) := pwr(i)(pwr(i)'first); -- get leading exponents
     end loop;
-    Double_Leading_Evaluations.Evaluate_System(hcf,hct,hdg,lcf,lpw,ycf,ydg,1);
+    sumerr := 0.0;
+    for i in hct'range loop
+      posmin := Positive_minimum(hct(i).all);
+      put("p : "); put(posmin);
+      put(", lpw : "); put(lpw(i)); err := abs(posmin - lpw(i));
+      put(", err : "); put(err,3); new_line;
+      sumerr := sumerr + err;
+    end loop;
+    put("error sum : "); put(sumerr,3); new_line;
+    Evaluate_and_Differentiate(hcf,hct,hdg,lcf,ycf,ydg,cA,eA,1);
+    if vrblvl > 0 then
+      put_line("the function value :");
+      for i in ycf'range loop
+        put(ycf(i)); put(" t^"); put(ydg(i)); new_line;
+      end loop;
+      put_line("the Jacobian :");
+      for i in cA'range(1) loop
+        for j in cA'range(2) loop
+          put("A("); put(i,1); put(","); put(j,1); put(") : ");
+          put(cA(i,j)); put(" t^"); put(eA(i,j)); new_line;
+        end loop;
+      end loop;
+    end if;
+    put_line("-> computing the tropical Cramer vector ...");
+    for i in work'range loop
+      work(i) := new Standard_Integer_Vectors.Vector'(1..dim => 0);
+    end loop;
+    Double_Puiseux_Operations.Leading_Powers
+      (dim,tol,eA,ydg,work,sol,idx1,idx2,fail,2);
+    put_line("the solution :"); put_line(sol);
   end Run_Newton_Step;
+
+  procedure Scale_Homotopy_Powers
+              ( hct : in Standard_Floating_VecVecs.VecVec ) is
+
+    powers : Standard_Floating_Vectors.Link_to_Vector;
+    minpwr : double_float;
+
+  begin
+    for i in hct'range loop
+      powers := hct(i);
+      minpwr := powers(powers'first);
+      for j in powers'first+1..powers'last loop
+        if powers(j) < minpwr
+         then minpwr := powers(j);
+        end if;
+      end loop;
+      for j in powers'range loop
+        powers(j) := powers(j) - minpwr;
+      end loop;
+    end loop;
+  end Scale_Homotopy_Powers;
 
   procedure Define_Homotopy
               ( dim : in integer32;
@@ -39,7 +180,8 @@ package body Test_Newton_Puiseux is
                 pwr : out Standard_Floating_VecVecs.VecVec;
                 hdg : out Standard_Integer_VecVecs.Array_of_VecVecs;
                 hcf : out Standard_Complex_VecVecs.VecVec;
-                hct : out Standard_Floating_VecVecs.VecVec ) is
+                hct : out Standard_Floating_VecVecs.VecVec;
+                vrblvl : in integer32 := 0 ) is
 
     pdg : Standard_Integer_VecVecs.Array_of_VecVecs(1..dim);
     pcf : Standard_Complex_VecVecs.VecVec(1..dim);
@@ -48,33 +190,40 @@ package body Test_Newton_Puiseux is
   begin
     Random_Laurent_Homotopy.Random_Laurent_System
       (dim,dim,-9,9,nbm,pdg,pcf,pct);
-    for i in 1..dim loop
-      put("-> coefficients and degrees of polynomial ");
-      put(i,1); put_line(" :");
-      for j in 1..nbm(i) loop
-        put(pcf(i)(j)); 
-        put(" t^"); put(pct(i)(j));
-        put("  "); put(pdg(i)(j)); new_line;
+    if vrblvl > 0 then
+      for i in 1..dim loop
+        put("-> coefficients and degrees of polynomial ");
+        put(i,1); put_line(" :");
+        for j in 1..nbm(i) loop
+          put(pcf(i)(j)); 
+          put(" t^"); put(pct(i)(j));
+          put("  "); put(pdg(i)(j)); new_line;
+        end loop;
       end loop;
-    end loop;
+    end if;
     Random_Laurent_Homotopy.Random_Power_Series(dim,nbt,cff,pwr);
-    for i in 1..dim loop
-      put("-> a random power series "); put(i,1); put_line(" : ");
-      put(cff(i)(0)); new_line;
-      for j in 1..nbt(i) loop
-        put(cff(i)(j)); put("  t^"); put(pwr(i)(j)); new_line;
+    if vrblvl > 0 then
+      for i in 1..dim loop
+        put("-> a random power series "); put(i,1); put_line(" : ");
+        put(cff(i)(0)); new_line;
+        for j in 1..nbt(i) loop
+          put(cff(i)(j)); put("  t^"); put(pwr(i)(j)); new_line;
+        end loop;
       end loop;
-    end loop;
+    end if;
     Random_Laurent_Homotopy.Random_Homotopy(pdg,pcf,pct,cff,pwr,hdg,hcf,hct);
-    for i in 1..dim loop
-      put("-> coefficients and degrees of homotopy ");
-      put(i,1); put_line(" :");
-      for j in hcf(i)'range loop
-        put(hcf(i)(j)); 
-        put(" t^"); put(hct(i)(j));
-        put("  "); put(hdg(i)(j)); new_line;
+    Scale_Homotopy_Powers(hct);
+    if vrblvl > 0 then
+      for i in 1..dim loop
+        put("-> coefficients and degrees of homotopy ");
+        put(i,1); put_line(" :");
+        for j in hcf(i)'range loop
+          put(hcf(i)(j)); 
+          put(" t^"); put(hct(i)(j));
+          put("  "); put(hdg(i)(j)); new_line;
+        end loop;
       end loop;
-    end loop;
+    end if;
     Random_Laurent_Homotopy.Test_Random_Homotopy(hdg,hcf,hct,cff,pwr);
   end Define_Homotopy;
 
@@ -99,8 +248,8 @@ package body Test_Newton_Puiseux is
       put("  Give the number of terms in series "); put(i,1);
       put(" : "); get(nbt(i));
     end loop;
-    Define_Homotopy(dim,nbm,nbt,cff,pwr,hdg,hcf,hct);
-    Run_Newton_Step(hcf,hct,hdg,cff,pwr);
+    Define_Homotopy(dim,nbm,nbt,cff,pwr,hdg,hcf,hct,1);
+    Run_Newton_Step(hcf,hct,hdg,cff,pwr,1);
   end Test;
 
   procedure Main is
--- src/Ada/Math_Lib/Laurent/test_newton_puiseux.ads
+++ src/Ada/Math_Lib/Laurent/test_newton_puiseux.ads
@@ -1,13 +1,78 @@
 with Standard_Integer_Numbers;          use Standard_Integer_Numbers;
+with Standard_Floating_Numbers;         use Standard_Floating_Numbers;
 with Standard_Integer_Vectors;
 with Standard_Integer_VecVecs;
+with Standard_Floating_Vectors;
 with Standard_Floating_VecVecs;
+with Standard_Complex_Vectors;
 with Standard_Complex_VecVecs;
+with Standard_Floating_Matrices;
+with Standard_Complex_Matrices;
 
 package Test_Newton_Puiseux is
 
 -- DESCRIPTION :
 --   Tests the Newton-Puiseux algorithm on a random Laurent homotopy.
+
+  procedure Evaluate_and_Differentiate
+              ( hcf : in Standard_Complex_VecVecs.VecVec;
+                hct : in Standard_Floating_VecVecs.VecVec;
+                hdg : in Standard_Integer_VecVecs.Array_of_VecVecs;
+                zt0 : in Standard_Complex_Vectors.Vector;
+                ycf : out Standard_Complex_Vectors.Vector;
+                ydg : out Standard_Floating_Vectors.Vector;
+                cjm : out Standard_Complex_Matrices.Matrix;
+                ejm : out Standard_Floating_Matrices.Matrix;
+                vrblvl : in integer32 := 0 );
+
+  -- DESCRIPTION :
+  --   Evaluates the Laurent homotopy at the leading constants
+  --   and computes the Jacobian matrix.
+ 
+  -- ON ENTRY :
+  --   hdg      supports of the Laurent homotopy;
+  --   hcf      coefficients of the polynomials in the homotopy;
+  --   hct      powers of t in the homotopy for each monomial;
+  --   zt0      leading, constant coefficients of the series;
+  --   vrblvl   is the verbose level.
+
+  -- ON RETURN :
+  --   ycf      leading coefficients of the value;
+  --   ydg      leading exponents of the value;
+  --   cjm      leading coefficients of the Jacobian matrix;
+  --   ejm      leading exponents of the Jacobian matrix.
+
+  function Positive_Minimum
+             ( v : Standard_Floating_Vectors.Vector ) return double_float;
+
+  -- DESCRIPTION :
+  --   Returns the smallest positive number in v.
+
+  procedure Run_Newton_Step
+              ( hcf : in Standard_Complex_VecVecs.VecVec;
+                hct : in Standard_Floating_VecVecs.VecVec;
+                hdg : in Standard_Integer_VecVecs.Array_of_VecVecs;
+                cff : in Standard_Complex_VecVecs.VecVec;
+                pwr : in Standard_Floating_VecVecs.VecVec;
+                vrblvl : in integer32 := 0 );
+
+  -- DESCRIPTION :
+  --   Runs one step with Newton's method on a Laurent homotopy,
+  --   starting at the leading coefficients of a power series.
+
+  -- ON ENTRY :
+  --   hdg      supports of the Laurent homotopy;
+  --   hcf      coefficients of the polynomials in the homotopy;
+  --   hct      powers of t in the homotopy for each monomial;
+  --   cff      coefficients of a power series solution;
+  --   pwr      exponents of a power series solution;
+  --   vrblvl   is the verbose level.
+
+  procedure Scale_Homotopy_Powers
+              ( hct : in Standard_Floating_VecVecs.VecVec );
+
+  -- DESCRIPTION :
+  --   Subtracts the minimum power from all other powers in hct.
 
   procedure Define_Homotopy
               ( dim : in integer32;
@@ -16,7 +81,8 @@ package Test_Newton_Puiseux is
                 pwr : out Standard_Floating_VecVecs.VecVec;
                 hdg : out Standard_Integer_VecVecs.Array_of_VecVecs;
                 hcf : out Standard_Complex_VecVecs.VecVec;
-                hct : out Standard_Floating_VecVecs.VecVec );
+                hct : out Standard_Floating_VecVecs.VecVec;
+                vrblvl : in integer32 := 0 );
 
   -- DESCRIPTION :
   --   Given the dimension, number of monomials and number of terms,
@@ -25,7 +91,8 @@ package Test_Newton_Puiseux is
   -- ON ENTRY :
   --   dim      number of equations and variables in the homotopy;
   --   nbm      nbm(i) equals the number of monomials in the system;
-  --   nbt      nbt(i) equals the number of terms in the i-th series.
+  --   nbt      nbt(i) equals the number of terms in the i-th series;
+  --   vrblvl   is the verbose level.
 
   -- ON RETURN :
   --   cff      coefficients of the power series solution;
